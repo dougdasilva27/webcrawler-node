11913516321f02584cef4c11157a8342ab98f19f
samirleao <samirleao@lett.com.br>
2016-08-12 11:34:26 -0300

    atualizando javadoc
    Former-commit-id: 3dd62db43c81fd74730b621e9bf0da4c2035f2ac

--- src/br/com/lett/crawlernode/queue/QueueHandler.java
+++ src/br/com/lett/crawlernode/queue/QueueHandler.java
@@ -15,6 +15,14 @@ import br.com.lett.crawlernode.kernel.ExecutionParameters;
 import br.com.lett.crawlernode.main.Main;
 import br.com.lett.crawlernode.util.Logging;
 
+/**
+ * This class only performs authentication on the desired Amazon SQS queue and
+ * creates the queue instance.
+ * When running in development environment, only the development queue is created.
+ * Analogous for modes insights and discovery in production environment.
+ * @author Samir Leao
+ *
+ */
 public class QueueHandler {
 
 	protected static final Logger logger = LoggerFactory.getLogger(QueueHandler.class);
