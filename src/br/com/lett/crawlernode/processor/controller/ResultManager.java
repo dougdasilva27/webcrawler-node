409350adb813df106272685d5943634a672b17c0
samirleao <samirleao@lett.com.br>
2016-09-12 12:23:54 -0300

    Traduzindo comentários e organizando sintaxe
    Former-commit-id: 86272e899eec374bfab3b0034f15d882f27049d2

--- src/br/com/lett/crawlernode/processor/controller/ResultManager.java
+++ src/br/com/lett/crawlernode/processor/controller/ResultManager.java
@@ -57,39 +57,37 @@ public class ResultManager {
 
 	private static final Logger logger = LoggerFactory.getLogger(ResultManager.class);
 
-	// Mongo para o SIFT similarity
+	// reference to mongo, to be used on SIFT calculations
 	private MongoDatabase mongo;
 
-	// Banco de dados, para usar Postgres
 	private DatabaseManager db;
 
-	// Mapas de Substituição
+	// substitution maps
 	private Map<String, String> unitsReplaceMap;
 	private Map<String, String> recipientsReplaceMap;
 
-	// Listas de identificação
+	// identification lists
 	private List<String> recipientsList;
 	private List<String> unitsList;
 	private List<ClassModel> classModelList;
 	private List<BrandModel> brandModelList;
 
-	// Variáveis de controle
+	// log messages on/off
 	private boolean logActivated;
 
-	// Variáveis de apoio
 	private DateFormat isoDateFormat;
 	private Map<Integer, String> cityNameInfo;
 	private Map<Integer, String> marketNameInfo;
 
-	// Variável usada no teste de processer de um determinado supermercado
 	private ArrayList<Integer> marketid;
 
 	/**
-	 * Construtor do ResultManager chamado pelo Crawler
+	 * Called on crawler main method.
 	 * 
-	 * @category Construtor
-	 * @author fabricio
-	 * @param activateLogging - Define a ativação ou não dos logs
+	 * @param activateLogging
+	 * @param mongo
+	 * @param db
+	 * @throws NullPointerException
 	 */
 	public ResultManager(
 			boolean activateLogging, 
@@ -100,7 +98,7 @@ public class ResultManager {
 		this.db = db;
 		this.mongo = mongo;
 
-		this.initialize(activateLogging);
+		this.init(activateLogging);
 	}
 
 	/**
@@ -109,7 +107,7 @@ public class ResultManager {
 	 * @param activateLogging display log messages
 	 */
 	public ResultManager(boolean activateLogging) throws NullPointerException {
-		this.initialize(activateLogging);
+		this.init(activateLogging);
 	}
 
 	/**
@@ -117,9 +115,7 @@ public class ResultManager {
 	 * 
 	 * @author fabricio
 	 */
-	private void initialize(boolean activateLogging) throws NullPointerException {
-
-		// Busca hora e data atual no sistema
+	private void init(boolean activateLogging) throws NullPointerException {
 		this.isoDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 		isoDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
 
@@ -128,32 +124,29 @@ public class ResultManager {
 		// Cria as informaçẽos do supermercado
 		this.createMarketInfo();
 
-		// Inicializa os mapas de substituição
+		// initializa substitution maps
 		this.unitsReplaceMap = new LinkedHashMap<String, String>();
 		this.recipientsReplaceMap = new LinkedHashMap<String, String>();
 
-		// Inicializa os mapas de identificação
+		// initialize identification maps
 		this.recipientsList = new ArrayList<String>();
 		this.unitsList = new ArrayList<String>();
 		this.classModelList = new ArrayList<ClassModel>();
 		this.brandModelList = new ArrayList<BrandModel>();
 
-		// Cria Modelo de manipulação de Marcas para substituição e identificação
+		// create model for brand manipulation for identification and substitution
 		try {
-
-			// ResultSet com resultados da consulta das classes
 			ResultSet rs = this.db.runSqlConsult(Queries.queryForLettBrandProducts);
-
-			// Enquanto houver linhas...
 			while(rs.next()) {
 
-				// Verificação se a marca deve ser ignorada
-				// Exemplo: papel higiênico leve 3 pague 2 PAMPERS
-				// LEVE também é uma marca, mas queremos PAMPERS, por isso LEVE não será incluída na nossa lista de marcas
-				if(!rs.getBoolean("ignored")){		
+				// verify if the brand should be ignored
+				// e.g: papel higiênico leve 3 pague 2 PAMPERS
+				// LEVE is also a brand, but we want PAMPERS
+				// so LEVE is not included on our brand list
+				if(!rs.getBoolean("ignored")) {		
 					BrandModel aux = null;
 
-					// Procurando se já existe a marca na lista de marcas
+					// see if this brand already exists on the brand list
 					for(BrandModel bm: brandModelList) {
 						if(bm.getBrand().equals(rs.getString("denomination"))) {
 							aux = bm;
@@ -161,14 +154,14 @@ public class ResultManager {
 						}
 					}
 
-					// Se não existe, cria um novo BrandModel e o incluí na lista de brandsModelList
+					// if it doesn't exists yet, we create a new Brand Model and add it on the brandsModelList
 					if(aux == null) {
 						aux = new BrandModel(rs.getString("denomination"), rs.getString("lett_supplier"));
 						aux.putOnList(rs.getString("mistake"));
 						brandModelList.add(aux);
 					}
 
-					// Caso já exista uma incidência, apenas adiciona o erro no mapa
+					// if an occurrence exists, only add this error on the map
 					else {
 						aux.putOnList(rs.getString("mistake"));
 					}
@@ -182,13 +175,14 @@ public class ResultManager {
 			Logging.printLogError(logger, CommonMethods.getStackTraceString(e));
 		}
 
-		// Completa marcas ou corrige marcas através da lista de marcas	
+		// completion or correction of brands through brands list
 		for(BrandModel bm : brandModelList) {
 
-			// Automação 1: marcas sem espaço
-			// Exemplo: Marca de nome: jack daniels
-			//          Nós adicionaremos automáticamente com brandsReplaceMap e suas combinações: 
-			//          jackdaniels (errado) -> jack daniels (correto)
+			// automation 1: brands is no blank spaces
+			// eg: "jack daniels"
+			//
+			// automatically add with brandsReplaceMap and its combinations:
+			// jackdaniels (wrong) -> jack daniels (correct)
 			if(bm.getBrand().contains(" ")) {
 				bm.putOnList(bm.getBrand().replace(" ", ""));
 
@@ -199,11 +193,12 @@ public class ResultManager {
 				}
 			}
 
-			// Automação 2: marcas com hifén.
-			// Exemplo: marca de nome: arco-íris
-			//          Nós adicionaremos automáticamente com brandsReplaceMap e suas combinações: 
-			//          arco íris (errado) -> arco-íris (correto)
-			//          arcoíris (errado)  -> arco-íris (correto)
+			// automation 2: brands that contains hifen (-).
+			// e.g: "arco-íris"
+			//
+			// automatically add with brandsReplaceMap and its combinations:
+			// "arco íris" (wrong) -> "arco-íris" (correct)
+			// "arcoíris" (wrong)  -> "arco-íris" (correct)
 			if(bm.getBrand().contains("-")) {
 				bm.putOnList(bm.getBrand().replace("-", ""));
 				bm.putOnList(bm.getBrand().replace("-", " "));
@@ -217,30 +212,26 @@ public class ResultManager {
 			}
 		}
 
-		// Cria listas de identificação de recipientes
-		this.recipientsList = IdentificationLists.recipientsList;
-
-		// Cria listas de identificação de unidades
+		// create unit identification lists
 		this.unitsList = IdentificationLists.unitsList;
 
-		// Cria listas de substituição de unidades
+		// create unit substitution lists
 		this.unitsReplaceMap = ReplacementMaps.unitsReplaceMap;
 
-		// Cria listas de substituição de recipientes
+		// create recipient identification lists
+		this.recipientsList = IdentificationLists.recipientsList;
+
+		// create recipient substitution lists
 		this.recipientsReplaceMap = ReplacementMaps.recipientsReplaceMap;
 
-		// Cria Modelo de manipulação da classes para torná-las LettClasses
+		// create manipulation models for lett classes
 		try{
-
-			// ResultSet com resultados da consulta das classes
 			ResultSet rs = this.db.runSqlConsult(Queries.queryForLettClassProducts);
-
-			// Enquanto houver linhas...
 			while (rs.next()) {
 
 				ClassModel aux = null;
 
-				// Procurando se já existe na lista a classe
+				// see if it already exists on class list
 				for(ClassModel lcm: classModelList) {
 					if(lcm.getLettName().equals(rs.getString("denomination"))) {
 						aux = lcm;
@@ -248,36 +239,36 @@ public class ResultManager {
 					}
 				}
 
-				// Se não existe, cria um nome lettClassesModel e adiciona na lista
+				// if it not exists, creates a new class model and add it to the list
 				if (aux == null) {
 					aux = new ClassModel(rs.getString("denomination"));
 					aux.putOnMap(rs.getString("mistake"), rs.getString("extra"));
 					classModelList.add(aux);
 				} 
-				// Caso já exista uma incidência, apenas adiciona o erro no mapa
+
+				// if an occurrence exists, only add this error on the map
 				else {
 					aux.putOnMap(rs.getString("mistake"), rs.getString("extra"));
 				}
 			}
-			// Encerra o ResultSet
+
+			// close the result set
 			rs.close();
 
-			// Trata exceções do ResultSet
 		}	catch (Exception e) {
 			Logging.printLogError(logger, CommonMethods.getStackTraceString(e));
 		}
 
-		// Completa classes ou corrige classes através da lista de classes	
+		// complete or apply correction upon classes
 		for (ClassModel cm : classModelList){
-
 			Pattern space = Pattern.compile(" ");
 
-			// Automação 1: marcas sem espaço
-			// Exemplo: Marca de nome: jack daniels
-			//          Nós adicionaremos automaticamente com brandsReplaceMap e suas combinações: 
-			//          jackdaniels (errado) -> jack daniels (correto)
+			// automation 1: brands is no blank spaces
+			// eg: "jack daniels"
+			//
+			// automatically add with brandsReplaceMap and its combinations:
+			// jackdaniels (wrong) -> jack daniels (correct)
 			if (cm.getLettName().contains(" ")) {
-
 				if(!cm.getMistakes().containsKey(cm.getLettName().replace(" ", ""))) 	cm.putOnMap(cm.getLettName().replace(" ", ""), "");
 				if(!cm.getMistakes().containsKey(cm.getLettName().replace(" ", "-"))) 	cm.putOnMap(cm.getLettName().replace(" ", "-"), "");
 
@@ -287,13 +278,13 @@ public class ResultManager {
 				}
 			}
 
-			// Automação 2: marcas com hifén.
-			// Exemplo: marca de nome: arco-íris
-			//          Nós adicionaremos automáticamente com brandsReplaceMap e suas combinações: 
-			//          arco íris (errado) -> arco-íris (correto)
-			//          arcoíris (errado)  -> arco-íris (correto)
+			// automation 2: brands that contains hifen (-).
+			// e.g: "arco-íris"
+			//
+			// automatically add with brandsReplaceMap and its combinations:
+			// "arco íris" (wrong) -> "arco-íris" (correct)
+			// "arcoíris" (wrong)  -> "arco-íris" (correct)
 			if (cm.getLettName().contains("-")) {
-
 				if(!cm.getMistakes().containsKey(cm.getLettName().replace("-", ""))) 	cm.putOnMap(cm.getLettName().replace("-", ""), "");
 				if(!cm.getMistakes().containsKey(cm.getLettName().replace("-", " "))) 	cm.putOnMap(cm.getLettName().replace("-", " "), "");
 
@@ -308,8 +299,8 @@ public class ResultManager {
 	}
 
 	/**
-	 * Extrai informações a partir dos campos "original_" do ProcessModel, e por fim
-	 * salva os dados recebidos dentro do ProcessModel a ser retornado.
+	 * Extract informations from all originals fields on ProcessedModel and then
+	 * saves the data gattered from the ProcessedModel to be returned.
 	 * 
 	 * @author Fabricio
 	 * @param cm Recebe valores do Crawler e os transfere para o ProcessModel
@@ -385,7 +376,7 @@ public class ResultManager {
 		if(pic.has("primary") ) {
 			picPrimary = pic.getJSONObject("primary");
 		}
-		
+
 		// assembling path to primary image stored on Amazon S3
 		// this image is the last downloaded image in the image crawler
 		StringBuilder primaryImageAmazonKey = new StringBuilder();
@@ -394,7 +385,7 @@ public class ResultManager {
 		primaryImageAmazonKey.append(this.marketNameInfo.get(pm.getMarket()) + "/");
 		primaryImageAmazonKey.append(pm.getInternalId());
 		primaryImageAmazonKey.append("/1-original.jpg");
-		
+
 		// assembling path to the desired primary image on Amazon S3
 		// this image is the one that was previously stored as the image that goes to insights
 		StringBuilder desiredPrimaryImageAmazonKey = new StringBuilder();
@@ -402,10 +393,10 @@ public class ResultManager {
 		desiredPrimaryImageAmazonKey.append("lett/");
 		desiredPrimaryImageAmazonKey.append(pm.getLettId());
 		desiredPrimaryImageAmazonKey.append("/1-original.jpg");
-		
+
 		// fetch md5 of the desired image
 		String desiredPrimaryMd5 = S3Service.fetchMd5FromAmazon(session, desiredPrimaryImageAmazonKey.toString());
-		
+
 		// fetch md5 for the supposed new image
 		String primaryMd5 = S3Service.fetchMd5FromAmazon(session, primaryImageAmazonKey.toString());
 
@@ -474,7 +465,7 @@ public class ResultManager {
 	}
 
 	/**
-	 * Fetch all markets from database.
+	 * Fetch market informations.
 	 */
 	private void createMarketInfo() {
 		this.cityNameInfo = new HashMap<Integer, String>();
@@ -487,10 +478,10 @@ public class ResultManager {
 
 			while(rs.next()) {
 
-				// Mapa de informaçẽos da cidade
+				// city information
 				this.cityNameInfo.put(rs.getInt("id"), rs.getString("city"));
 
-				// Mapa de informaçẽos do Supermercado
+				// market information
 				this.marketNameInfo.put(rs.getInt("id"), rs.getString("name"));
 
 				// Contém ids dos supermercados para teste do processer
