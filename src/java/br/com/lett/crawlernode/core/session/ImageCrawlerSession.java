eeaebf2671d661f3b2b80cfd3ad67310db128933
samirleao <samirleao@lett.com.br>
2016-09-26 17:28:09 -0300

    Preparando classe de session para tarefa de download de imagens.
    Former-commit-id: 76e52c0ce9a92edfafb2627d9ef047b130cb3a46

--- src/java/br/com/lett/crawlernode/core/session/ImageCrawlerSession.java
+++ src/java/br/com/lett/crawlernode/core/session/ImageCrawlerSession.java
@@ -1,13 +1,46 @@
 package br.com.lett.crawlernode.core.session;
 
+import java.util.List;
+import java.util.Map;
+
 import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.MessageAttributeValue;
 
 import br.com.lett.crawlernode.core.models.Markets;
 
 public class ImageCrawlerSession extends CrawlerSession {
 	
+	/** Primary image URL */
+	private String primaryImage;
+	
+	/** Array of secondary images */
+	private List<String> secondaryImages;
+	
 	public ImageCrawlerSession(Message message, String queueName, Markets markets) {
 		super(message, queueName, markets);
+		
+		Map<String, MessageAttributeValue> attrMap = message.getMessageAttributes();
+		
+		// the primary image url is the message body, that is already set up on the superclass CrawlerSession
+		this.primaryImage = super.url;
+		
+		// get the list of secondary images
+		String secondaryImagesString = attrMap.get("secondary").getStringValue();
+		
+		
+		
 	}
+	
+	public String getPrimaryImage() {
+		return super.url;
+	}
+	
+	private void parseSecondaryImagesURL(String secondaryImagesString) {
+		String[] urls = secondaryImagesString.replace("[", "").replaceAll("]", "").split(",");
+	}
+	
+	
+	
+	
 	
 }
