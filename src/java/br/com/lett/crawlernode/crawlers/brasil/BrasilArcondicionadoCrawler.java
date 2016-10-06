7544908ea0421d6919f53b521ad84c6f6ba66bd7
samirleao <samirleao@lett.com.br>
2016-10-06 14:43:01 -0300

    Novos crawlers.
    Former-commit-id: 4f124300b2881ec886796829a752b051196282da

---
+++ src/java/br/com/lett/crawlernode/crawlers/brasil/BrasilArcondicionadoCrawler.java
@@ -0,0 +1,299 @@
+package br.com.lett.crawlernode.crawlers.brasil;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.session.CrawlerSession;
+import br.com.lett.crawlernode.core.task.Crawler;
+import br.com.lett.crawlernode.util.Logging;
+
+/************************************************************************************************************************************************************************************
+ * Crawling notes (25/08/2016):
+ * 
+ * 1) For this crawler, we have one url per each sku. There is no page is more than one sku in it.
+ *  
+ * 2) There is no stock information for skus in this ecommerce by the time this crawler was made.
+ * 
+ * 3) There is no marketplace in this ecommerce by the time this crawler was made.
+ * 
+ * 4) The sku page identification is done simply looking for an specific html element.
+ * 
+ * 5) Even if a product is unavailable, its price is displayed.
+ * 
+ * 6) There is internalPid for skus in this ecommerce. 
+ * 
+ * 7) The primary image is the first image in the secondary images selector.
+ * 
+ * Examples:
+ * ex1 (available): http://www.arcondicionado.com.br/produto/ar-condicionado-split-9000-btus-frio-220v-lg-smile-ts-c092tnw6-68635
+ * ex2 (unavailable): http://www.arcondicionado.com.br/produto/cortina-de-ar-90-cm-220v-springer-acs09s5-68690
+ *
+ * Optimizations notes:
+ * No optimizations.
+ *
+ ************************************************************************************************************************************************************************************/
+public class BrasilArcondicionadoCrawler extends Crawler {
+	
+	private final String HOME_PAGE = "http://www.arcondicionado.com.br/";
+
+	public BrasilArcondicionadoCrawler(CrawlerSession session) {
+		super(session);
+	}
+
+	@Override
+	public boolean shouldVisit() {
+		String href = session.getOriginalURL().toLowerCase();
+		return !FILTERS.matcher(href).matches() && (href.startsWith(HOME_PAGE));
+	}
+
+
+	@Override
+	public List<Product> extractInformation(Document doc) throws Exception {
+		super.extractInformation(doc);
+		List<Product> products = new ArrayList<Product>();
+
+		if ( isProductPage(session.getOriginalURL()) ) {
+			Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
+
+			/* ***********************************
+			 * crawling data of only one product *
+			 *************************************/
+
+			// InternalId
+			String internalId = crawlInternalId(doc);
+
+			// Pid
+			String internalPid = crawlInternalPid(doc);
+
+			// Name
+			String name = crawlName(doc);
+
+			// Price
+			Float price = crawlMainPagePrice(doc);
+			
+			// Availability
+			boolean available = crawlAvailability(doc);
+
+			// Categories
+			ArrayList<String> categories = crawlCategories(doc);
+			String category1 = getCategory(categories, 0);
+			String category2 = getCategory(categories, 1);
+			String category3 = getCategory(categories, 2);
+
+			// Primary image
+			String primaryImage = crawlPrimaryImage(doc);
+
+			// Secondary images
+			String secondaryImages = crawlSecondaryImages(doc);
+
+			// Description
+			String description = crawlDescription(doc);
+
+			// Stock
+			Integer stock = null;
+
+			// Marketplace map
+			Map<String, Float> marketplaceMap = crawlMarketplace(doc);
+
+			// Marketplace
+			JSONArray marketplace = assembleMarketplaceFromMap(marketplaceMap);
+
+			// Creating the product
+			Product product = new Product();
+			product.setUrl(session.getOriginalURL());
+			product.setInternalId(internalId);
+			product.setInternalPid(internalPid);
+			product.setName(name);
+			product.setPrice(price);
+			product.setAvailable(available);
+			product.setCategory1(category1);
+			product.setCategory2(category2);
+			product.setCategory3(category3);
+			product.setPrimaryImage(primaryImage);
+			product.setSecondaryImages(secondaryImages);
+			product.setDescription(description);
+			product.setStock(stock);
+			product.setMarketplace(marketplace);
+
+			products.add(product);
+
+		} else {
+			Logging.printLogDebug(logger, session, "Not a product page" + this.session.getOriginalURL());
+		}
+		
+		return products;
+
+	}
+
+
+
+	/*******************************
+	 * Product page identification *
+	 *******************************/
+
+	private boolean isProductPage(String url) {
+		if ( url.startsWith(HOME_PAGE + "produto/") ) return true;
+		return false;
+	}
+	
+	
+	/*******************
+	 * General methods *
+	 *******************/
+	
+	private String crawlInternalId(Document document) {
+		String internalId = null;
+		Element internalIdElement = document.select("#hdnProdutoVarianteId").first();
+
+		if (internalIdElement != null) {
+			internalId = internalIdElement.attr("value").toString().trim();			
+		}
+
+		return internalId;
+	}
+
+	private String crawlInternalPid(Document document) {
+		String internalPid = null;
+
+		Element internalPidElement = document.select("#hdnProdutoId").first();
+
+		if (internalPidElement != null) {
+			internalPid = internalPidElement.attr("value").toString().trim();			
+		}
+		
+		return internalPid;
+	}
+	
+	private String crawlName(Document document) {
+		String name = null;
+		Element nameElement = document.select(".prodTitle").first();
+
+		if (nameElement != null) {
+			name = nameElement.text().toString().trim();
+		}
+
+		return name;
+	}
+
+	private Float crawlMainPagePrice(Document document) {
+		Float price = null;
+		Element specialPrice = document.select(".produtoInfo .precoPor").first();		
+		
+		if (specialPrice != null) {
+			price = Float.parseFloat( specialPrice.text().toString().replaceAll("[^0-9,]+", "").replaceAll("\\.", "").replaceAll(",", ".") );
+		} 
+
+		return price;
+	}
+	
+	private boolean crawlAvailability(Document document) {
+		Element notifyMeElement = document.select(".avisoIndisponivel:not([style])").first();
+		
+		if (notifyMeElement != null) {
+			return false;
+		}
+		
+		return true;
+	}
+
+	private Map<String, Float> crawlMarketplace(Document document) {
+		return new HashMap<String, Float>();
+	}
+	
+	private JSONArray assembleMarketplaceFromMap(Map<String, Float> marketplaceMap) {
+		return new JSONArray();
+	}
+
+	private String crawlPrimaryImage(Document document) {
+		String primaryImage = null;
+		Element primaryImageElement = document.select(".fbits-componente-imagem img").first();
+
+		if (primaryImageElement != null) {
+			String image = primaryImageElement.attr("data-zoom-image").trim();
+			
+			if(!image.startsWith("http")){
+				image = primaryImageElement.attr("src").trim();
+			}
+			
+			if(image.contains("?")){
+				image = image.split("\\?")[0];
+			}
+			
+			primaryImage = image;
+		}
+
+		return primaryImage;
+	}
+
+	private String crawlSecondaryImages(Document document) {
+		String secondaryImages = null;
+		JSONArray secondaryImagesArray = new JSONArray();
+
+		Elements imagesElement = document.select(".fbits-produto-imagens a");
+
+		for (int i = 1; i < imagesElement.size(); i++) { // start with index 1 because the first image is the primary image
+			String image = imagesElement.get(i).attr("data-zoom-image").trim();
+			
+			if(image.startsWith("http")){
+				image = imagesElement.get(i).attr("data-image").trim();
+			}
+			
+			if(image.contains("?")){
+				image = image.split("\\?")[0];
+			}
+				
+			secondaryImagesArray.put( image );	
+			
+		}
+
+		if (secondaryImagesArray.length() > 0) {
+			secondaryImages = secondaryImagesArray.toString();
+		}
+
+		return secondaryImages;
+	}
+
+	private ArrayList<String> crawlCategories(Document document) {
+		ArrayList<String> categories = new ArrayList<String>();
+		Elements elementCategories = document.select("#fbits-breadcrumb span[itemprop=name]");
+
+		for (int i = 1; i < elementCategories.size(); i++) { // starting from index 1, because the first is the market name
+			categories.add( elementCategories.get(i).text().trim() );
+		}
+
+		return categories;
+	}
+
+	private String getCategory(ArrayList<String> categories, int n) {
+		if (n < categories.size()) {
+			return categories.get(n);
+		}
+
+		return "";
+	}
+
+	private String crawlDescription(Document document) {
+		String description = "";
+		Element descriptionElement = document.select("#descricao-produto").first();
+		Element princElement = document.select("#carac-principais").first();
+		Element specElement = document.select("#carac-tecnicas").first();
+		Element dimensionsElement = document.select("#dimensoes").first();
+
+		if (descriptionElement != null) description = description + descriptionElement.html();
+		if (princElement != null) description = description + princElement.html();
+		if (specElement != null) description = description + specElement.html();
+		if (dimensionsElement != null) description = description + dimensionsElement.html();
+
+		return description;
+	}
+
+}
---
+++ src/java/br/com/lett/crawlernode/crawlers/brasil/BrasilLilianiCrawler.java
@@ -0,0 +1,349 @@
+package br.com.lett.crawlernode.crawlers.brasil;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.session.CrawlerSession;
+import br.com.lett.crawlernode.core.task.Crawler;
+import br.com.lett.crawlernode.util.Logging;
+
+/************************************************************************************************************************************************************************************
+ * Crawling notes (29/08/2016):
+ * 
+ * 1) For this crawler, we have one url per mutiple skus.
+ *  
+ * 2) There is no stock information for skus in this ecommerce by the time this crawler was made.
+ * 
+ * 3) There is no marketplace in this ecommerce by the time this crawler was made.
+ * 
+ * 4) The sku page identification is done simply looking for an specific html element.
+ * 
+ * 5) Even if a product is unavailable, its price is not displayed.
+ * 
+ * 6) There is internalPid for skus in this ecommerce. The internalPid is a number that is the same for all
+ * the variations of a given sku.
+ * 
+ * 7) The primary image is the first image in the secondary images selector.
+ * 
+ * 8) The url of images are changed to bigger dimensions manually.
+ * 
+ * Examples:
+ * ex1 (available): http://www.liliani.com.br/product.aspx?idproduct=343987
+ * ex2 (unavailable): http://www.liliani.com.br/product.aspx?idproduct=324446
+ * ex3 (variations): http://www.liliani.com.br/product.aspx?idproduct=222262
+ *
+ * Optimizations notes:
+ * No optimizations.
+ *
+ ************************************************************************************************************************************************************************************/
+
+public class BrasilLilianiCrawler extends Crawler {
+	
+	private final String HOME_PAGE = "http://www.liliani.com.br/";
+
+	public BrasilLilianiCrawler(CrawlerSession session) {
+		super(session);
+	}
+
+	@Override
+	public boolean shouldVisit() {
+		String href = session.getOriginalURL().toLowerCase();
+		return !FILTERS.matcher(href).matches() && (href.startsWith(HOME_PAGE));
+	}
+
+
+	@Override
+	public List<Product> extractInformation(Document doc) throws Exception {
+		super.extractInformation(doc);
+		List<Product> products = new ArrayList<Product>();
+
+		if ( isProductPage(session.getOriginalURL()) ) {
+			Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
+			
+			// Pid
+			String internalPid = crawlInternalPid(session.getOriginalURL());
+
+			// Name
+			String nameMainPage = crawlName(doc);
+
+			// Availability
+			boolean available = crawlAvailability(doc);
+					
+			// Price
+			Float price = crawlMainPagePrice(doc, available);
+			
+			// Categories
+			ArrayList<String> categories = crawlCategories(doc);
+			String category1 = getCategory(categories, 0);
+			String category2 = getCategory(categories, 1);
+			String category3 = getCategory(categories, 2);
+
+			// Primary image
+			String primaryImage = crawlPrimaryImage(doc);
+
+			// Secondary images
+			String secondaryImages = crawlSecondaryImages(doc);
+
+			// Description
+			String description = crawlDescription(doc);
+
+			// Stock
+			Integer stock = null;
+
+			// Marketplace map
+			Map<String, Float> marketplaceMap = crawlMarketplace(doc);
+
+			// Marketplace
+			JSONArray marketplace = assembleMarketplaceFromMap(marketplaceMap);
+			
+			
+			Elements variations = doc.select(".sku table");
+
+			if(variations.size() > 0){
+				
+				/* ***********************************
+				 * crawling data of mutiple products *
+				 *************************************/
+				
+				for(Element e : variations){
+					
+					// InternalID
+					String internalId = this.crawlInternalIdVariation(e);
+					
+					// Name Variation
+					String nameVariation = e.select("strong").first().text().trim().replaceAll("[^0-9V]", "");
+
+					// Name
+					String name = nameMainPage + " (" + nameVariation + ")";
+
+					// Avaiability
+					boolean availableVariation = this.crawlAvailabilityVariation(e);
+					
+					// Price
+					Float priceVariation = crawlMainPagePrice(doc, availableVariation);
+
+					// Creating the product
+					Product product = new Product();
+					product.setUrl(session.getOriginalURL());
+					product.setInternalId(internalId);
+					product.setInternalPid(internalPid);
+					product.setName(name);
+					product.setPrice(priceVariation);
+					product.setAvailable(availableVariation);
+					product.setCategory1(category1);
+					product.setCategory2(category2);
+					product.setCategory3(category3);
+					product.setPrimaryImage(primaryImage);
+					product.setSecondaryImages(secondaryImages);
+					product.setDescription(description);
+					product.setStock(stock);
+					product.setMarketplace(marketplace);
+
+					products.add(product);
+				}
+			}
+			
+			else {
+				
+				/* ***********************************
+				 * crawling data of only one product *
+				 *************************************/
+
+				// InternalId
+				String internalId = internalPid;
+				
+				// Creating the product
+				Product product = new Product();
+				product.setUrl(session.getOriginalURL());
+				product.setInternalId(internalId);
+				product.setInternalPid(internalPid);
+				product.setName(nameMainPage);
+				product.setPrice(price);
+				product.setAvailable(available);
+				product.setCategory1(category1);
+				product.setCategory2(category2);
+				product.setCategory3(category3);
+				product.setPrimaryImage(primaryImage);
+				product.setSecondaryImages(secondaryImages);
+				product.setDescription(description);
+				product.setStock(stock);
+				product.setMarketplace(marketplace);
+
+				products.add(product);
+				
+			}
+
+		} else {
+			Logging.printLogDebug(logger, session, "Not a product page" + this.session.getOriginalURL());
+		}
+		
+		return products;
+
+	}
+
+
+
+	/*******************************
+	 * Product page identification *
+	 *******************************/
+
+	private boolean isProductPage(String url) {
+		if ( url.startsWith(HOME_PAGE + "product") ) return true;
+		return false;
+	}
+	
+	/*********************
+	 * Variation product *
+	 *********************/	
+	
+	private boolean crawlAvailabilityVariation(Element sku) {	
+		Element e = sku.select(".arial_12.pading_botom_6").first();
+		
+		if(e != null){
+			String status = e.ownText().trim().toLowerCase();
+			
+			if(status.equals("disponibilidade imediata")){
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	private String crawlInternalIdVariation(Element sku) {
+		String internalId = null;
+		
+		Element internalIdElement = sku.select("td a").first();
+
+		if(internalIdElement != null){
+			internalId = internalIdElement.attr("href").replaceAll("[^0-9]", "").trim();
+		}
+		
+		return internalId;
+	}
+	
+	/*******************
+	 * General methods *
+	 *******************/
+
+	private String crawlInternalPid(String url) {
+		String internalPid = null;
+		
+		String[] tokens = url.split("=");
+		internalPid = tokens[tokens.length-1];
+		
+		return internalPid;
+	}
+	
+	private String crawlName(Document document) {
+		String name = null;
+		Element nameElement = document.select(".text_prod_detalhe_prod h1").first();
+
+		if (nameElement != null) {
+			name = nameElement.text().toString().trim();
+		}
+
+		return name;
+	}
+
+	private Float crawlMainPagePrice(Document document, boolean available) {
+		Float price = null;
+		Element specialPrice = document.select("#ctl00_ContentSite_dvAvailable span strong").first();		
+		
+		if (specialPrice != null && available) {
+			price = Float.parseFloat( specialPrice.text().toString().replaceAll("[^0-9,]+", "").replaceAll("\\.", "").replaceAll(",", ".") );
+		} 
+
+		return price;
+	}
+	
+	private boolean crawlAvailability(Document document) {
+		Element notifyMeElement = document.select("#ctl00_ContentSite_dvAvailable span strong").first();
+		
+		if (notifyMeElement != null) {
+			return true;
+		}
+		
+		return false;
+	}
+
+	private Map<String, Float> crawlMarketplace(Document document) {
+		return new HashMap<String, Float>();
+	}
+	
+	private JSONArray assembleMarketplaceFromMap(Map<String, Float> marketplaceMap) {
+		return new JSONArray();
+	}
+
+	private String crawlPrimaryImage(Document document) {
+		String primaryImage = null;
+		Element primaryImageElement = document.select(".img_prod img#ctl00_ContentSite_imgProductImgBig").first();
+
+		if (primaryImageElement != null) {
+			primaryImage = modifyImageUrlToLarge(primaryImageElement.attr("src")); // montando imagem com zoom
+		}
+
+		return primaryImage;
+	}
+
+	private String crawlSecondaryImages(Document document) {
+		String secondaryImages = null;
+		JSONArray secondaryImagesArray = new JSONArray();
+
+		Elements imagesElement = document.select("#ctl00_ContentSite_dltAdditionalImages img");
+
+		for (int i = 1; i < imagesElement.size(); i++) { //start with index 1 because the first image is the primary image
+			secondaryImagesArray.put( imagesElement.get(i).attr("src").trim().replaceAll("36", "500") );
+		}
+
+		if (secondaryImagesArray.length() > 0) {
+			secondaryImages = secondaryImagesArray.toString();
+		}
+
+		return secondaryImages;
+	}
+	
+	private String modifyImageUrlToLarge(String image){
+		String urlImage = image;
+		
+		String[] tokens = urlImage.split("_");
+		String temp = tokens[tokens.length-1];
+		
+		urlImage = image.replace(temp, "500.jpg");
+		
+		return urlImage;
+	}
+
+	private ArrayList<String> crawlCategories(Document document) {
+		ArrayList<String> categories = new ArrayList<String>();
+		
+		return categories;
+	}
+
+	private String getCategory(ArrayList<String> categories, int n) {
+		if (n < categories.size()) {
+			return categories.get(n);
+		}
+
+		return "";
+	}
+
+	private String crawlDescription(Document document) {
+		String description = "";
+		Element descriptionElement = document.select(".conteudo_box_detalhe_prod").first();
+	
+		if (descriptionElement != null) description = description + descriptionElement.html();
+
+		return description;
+	}
+
+}
