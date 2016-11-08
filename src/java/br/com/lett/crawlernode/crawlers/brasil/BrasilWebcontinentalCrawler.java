a52f4dfe2bbb35f45fdcdf4ccf7fd1e6c0035593
gabriel <gabriel@gabriel-lett>
2016-11-08 11:58:17 -0200

    Finalizei prices webcontinetal.
    Former-commit-id: eacb628222cde464b9587323a757d6317b87108e

--- src/java/br/com/lett/crawlernode/crawlers/brasil/BrasilWebcontinentalCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/brasil/BrasilWebcontinentalCrawler.java
@@ -1,7 +1,9 @@
 package br.com.lett.crawlernode.crawlers.brasil;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.json.JSONArray;
 import org.jsoup.Jsoup;
@@ -11,8 +13,11 @@ import org.jsoup.select.Elements;
 
 import br.com.lett.crawlernode.core.crawler.Crawler;
 import br.com.lett.crawlernode.core.fetcher.DataFetcher;
+import br.com.lett.crawlernode.core.models.Card;
+import br.com.lett.crawlernode.core.models.Prices;
 import br.com.lett.crawlernode.core.models.Product;
 import br.com.lett.crawlernode.core.session.CrawlerSession;
+import br.com.lett.crawlernode.util.CommonMethods;
 import br.com.lett.crawlernode.util.Logging;
 
 /************************************************************************************************************************************************************************************
@@ -218,6 +223,9 @@ public class BrasilWebcontinentalCrawler extends Crawler {
 
 		// Marketplace
 		JSONArray marketplace = null;
+		
+		// Prices 
+		Prices prices = crawlPrices(document, price);
 
 		Product product = new Product();
 		product.setUrl(url);
@@ -225,6 +233,7 @@ public class BrasilWebcontinentalCrawler extends Crawler {
 		product.setInternalPid(internalPid);
 		product.setName(name);
 		product.setPrice(price);
+		product.setPrices(prices);
 		product.setCategory1(category1);
 		product.setCategory2(category2);
 		product.setCategory3(category3);
@@ -342,4 +351,41 @@ public class BrasilWebcontinentalCrawler extends Crawler {
 		return secondaryImages;
 	}
 
+	private Prices crawlPrices(Document doc, Float price){
+		Prices prices = new Prices();
+		
+		if(price != null){
+			Element priceElement = doc.select(".box-preco-produto .valor-por .valor-avista").first();
+
+			if (priceElement != null) {
+				Float bankTicketPrice = Float.parseFloat(priceElement.text().replaceAll("[^0-9,]+", "").replaceAll("\\.", "").replaceAll(",", "."));
+				prices.insertBankTicket(bankTicketPrice);
+			}
+			
+			Map<Integer, Float> installmentPriceMap = new HashMap<>();
+			Elements installments = doc.select("#payment-methods-modal .box-installment tr");
+			
+			for(Element e : installments){
+				String text = e.text().toLowerCase();
+				
+				if(text.contains("x")){
+					int x = text.indexOf("x");
+					
+					Integer installment = Integer.parseInt(text.substring(0, x).trim());
+					Float value = CommonMethods.parseFloat(text.substring(x+1));
+					
+					installmentPriceMap.put(installment, value);
+				}
+			}
+			
+			prices.insertCardInstallment(Card.VISA.toString(), installmentPriceMap);
+			prices.insertCardInstallment(Card.MASTERCARD.toString(), installmentPriceMap);
+			prices.insertCardInstallment(Card.DINERS.toString(), installmentPriceMap);
+			prices.insertCardInstallment(Card.AMEX.toString(), installmentPriceMap);
+			prices.insertCardInstallment(Card.ELO.toString(), installmentPriceMap);
+			prices.insertCardInstallment(Card.HIPERCARD.toString(), installmentPriceMap);
+		}
+		
+		return prices;
+	}
 }
