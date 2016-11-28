353459418fa0ff14b75df8d6719618390a3f42f8
gabriel <gabriel@gabriel-lett>
2016-11-28 14:51:22 -0200

    Finalizei superama.
    Former-commit-id: 3076cd8a53f3fcd1b14cd48f984f78477f7f464a

---
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/mexico/MexicoSuperamaCrawler.java
@@ -0,0 +1,253 @@
+package br.com.lett.crawlernode.crawlers.corecontent.mexico;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.json.JSONArray;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import br.com.lett.crawlernode.core.crawler.Crawler;
+import br.com.lett.crawlernode.core.models.Card;
+import br.com.lett.crawlernode.core.models.CategoryCollection;
+import br.com.lett.crawlernode.core.models.Prices;
+import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.models.ProductBuilder;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.util.Logging;
+
+/**
+ * Date: 28/11/2016
+ * 
+ * 1) Only one sku per page.
+ * 
+ * Price crawling notes:
+ * 1) In time crawler was made, there no product unnavailable.
+ * 2) There is no bank slip (boleto bancario) payment option.
+ * 3) There is no installments for card payment. So we only have 
+ * 1x payment, and to this value we use the cash price crawled from
+ * the sku page. (nao existe divisao no cartao de credito).
+ * 
+ * @author Gabriel Dornelas
+ *
+ */
+public class MexicoSuperamaCrawler extends Crawler {
+
+	private final String HOME_PAGE = "https://www.superama.com.mx/";
+
+	public MexicoSuperamaCrawler(Session session) {
+		super(session);
+	}
+
+	@Override
+	public boolean shouldVisit() {
+		String href = session.getOriginalURL().toLowerCase();
+		return !FILTERS.matcher(href).matches() && (href.startsWith(HOME_PAGE));
+	}
+
+	@Override
+	public List<Product> extractInformation(Document doc) throws Exception {
+		super.extractInformation(doc);
+		List<Product> products = new ArrayList<Product>();
+
+		if ( isProductPage(doc) ) {
+			Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
+
+			String internalId = crawlInternalId(doc);
+			String internalPid = crawlInternalPid(doc);
+			String name = crawlName(doc);
+			Float price = crawlPrice(doc);
+			Prices prices = crawlPrices(price);
+			boolean available = crawlAvailability(doc);
+			CategoryCollection categories = crawlCategories(doc);
+			String primaryImage = crawlPrimaryImage(doc);
+			String secondaryImages = crawlSecondaryImages(doc);
+			String description = crawlDescription(doc);
+			Integer stock = null;
+			JSONArray marketplace = crawlMarketplace(doc);
+
+			// Creating the product
+			Product product = ProductBuilder.create()
+					.setUrl(session.getOriginalURL())
+					.setInternalId(internalId)
+					.setInternalPid(internalPid)
+					.setName(name)
+					.setPrice(price)
+					.setPrices(prices)
+					.setAvailable(available)
+					.setCategory1(categories.getCategory(0))
+					.setCategory2(categories.getCategory(1))
+					.setCategory3(categories.getCategory(2))
+					.setPrimaryImage(primaryImage)
+					.setSecondaryImages(secondaryImages)
+					.setDescription(description)
+					.setStock(stock)
+					.setMarketplace(marketplace)
+					.build();
+
+			products.add(product);
+
+		} else {
+			Logging.printLogDebug(logger, session, "Not a product page" + this.session.getOriginalURL());
+		}
+
+		return products;
+
+	}
+
+	private boolean isProductPage(Document doc) {
+		if (doc.select("#detalleProductoContainer").first() != null) return true;
+		return false;
+	}
+
+	private String crawlInternalId(Document document) {
+		String internalId = null;
+
+		Element internalIdElement = document.select("#upcProducto").first();
+		if (internalIdElement != null) {
+			internalId = internalIdElement.attr("value");
+		}
+
+		return internalId;
+	}
+
+	/**
+	 * There is no internalPid.
+	 * 
+	 * @param document
+	 * @return
+	 */
+	private String crawlInternalPid(Document document) {
+		String internalPid = null;
+
+		return internalPid;
+	}
+
+	private String crawlName(Document document) {
+		String name = null;
+		Element nameElement = document.select("#nombreProductoDetalle").first();
+
+		if (nameElement != null) {
+			name = nameElement.text().trim();
+		}
+
+		return name;
+	}
+
+	private Float crawlPrice(Document document) {
+		Float price = null;
+
+		String priceText = null;
+		Element salePriceElement = document.select(".detail-description-price span").first();		
+
+		if (salePriceElement != null) {
+			priceText = salePriceElement.ownText();
+			price = Float.parseFloat(priceText.replaceAll("\\$", "").replaceAll(",", ""));
+		}
+
+		return price;
+	}
+
+	private boolean crawlAvailability(Document document) {
+		boolean available = false;
+
+		Element outOfStockElement = document.select(".botonAgregarProductoCarritoDetalle").first();
+		if (outOfStockElement != null) {
+			available = true;
+		}
+
+		return available;
+	}
+
+	private JSONArray crawlMarketplace(Document document) {
+		return new JSONArray();
+	}
+
+
+	private String crawlPrimaryImage(Document document) {
+		String primaryImage = null;
+		Element primaryImageElement = document.select(".sp-wrap > a").first();
+
+		if (primaryImageElement != null) {			
+			primaryImage = "https://www.superama.com.mx" + primaryImageElement.attr("href").trim();
+		}
+
+		return primaryImage;
+	}
+
+	private String crawlSecondaryImages(Document document) {
+		String secondaryImages = null;
+		JSONArray secondaryImagesArray = new JSONArray();
+
+		Elements imagesElement = document.select(".sp-wrap > a");
+
+		for (int i = 1; i < imagesElement.size(); i++) { // first index is the primary image
+			String image = "https://www.superama.com.mx" + imagesElement.get(i).attr("href").trim();
+			secondaryImagesArray.put( image );	
+		}
+
+		if (secondaryImagesArray.length() > 0) {
+			secondaryImages = secondaryImagesArray.toString();
+		}
+
+		return secondaryImages;
+	}
+
+	private CategoryCollection crawlCategories(Document document) {
+		CategoryCollection categories = new CategoryCollection();
+
+		Elements elementCategories = document.select(".breadcrumb a");
+		for (int i = 1; i < elementCategories.size(); i++) { // starting from index 1, because the first is the market name
+			categories.add( elementCategories.get(i).text().trim() );
+		}
+
+		return categories;
+	}
+
+	private String crawlDescription(Document document) {
+		StringBuilder description = new StringBuilder();
+		
+		Element descriptionElement = document.select(".detail-description-content").first();
+		Element ingredientElement = document.select(".content__ingredientes").first();
+		Element nutritionalElement = document.select(".content__nutricional").first();
+		Element caracteristicaElement = document.select(".content__caracteristicas").first();
+	
+		if(descriptionElement != null) description.append(descriptionElement.html());
+		if(ingredientElement != null) description.append(ingredientElement.html());
+		if(nutritionalElement != null) description.append(nutritionalElement.html());
+		if(caracteristicaElement != null) description.append(caracteristicaElement.html());
+		
+		return description.toString();
+	}
+
+	/**
+	 * There is no bankSlip price.
+	 * 
+	 * There is no card payment options, other than cash price.
+	 * So for installments, we will have only one installment for each
+	 * card brand, and it will be equals to the price crawled on the sku
+	 * main page.
+	 * 
+	 * @param doc
+	 * @param price
+	 * @return
+	 */
+	private Prices crawlPrices(Float price) {
+		Prices prices = new Prices();
+		
+		if(price != null){
+			Map<Integer,Float> installmentPriceMap = new TreeMap<Integer, Float>();
+			installmentPriceMap.put(1, price);
+	
+			prices.insertCardInstallment(Card.VISA.toString(), installmentPriceMap);
+			prices.insertCardInstallment(Card.MASTERCARD.toString(), installmentPriceMap);
+			prices.insertCardInstallment(Card.AMEX.toString(), installmentPriceMap);
+		}
+		
+		return prices;
+	}
+
+}
