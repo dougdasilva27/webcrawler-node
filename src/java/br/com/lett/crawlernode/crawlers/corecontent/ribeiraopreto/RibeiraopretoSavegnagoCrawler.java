1570f9b1cf24bda75f5c8065b9e2729184789cfb
gabriel <gabriel@gabriel-lett>
2016-12-01 14:09:19 -0200

    Finalizei savegnago.
    Former-commit-id: 64b42f685753e6011ff7710f3ad70a3bd482f7c6

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/ribeiraopreto/RibeiraopretoSavegnagoCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/ribeiraopreto/RibeiraopretoSavegnagoCrawler.java
@@ -3,7 +3,9 @@ package br.com.lett.crawlernode.crawlers.corecontent.ribeiraopreto;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URIBuilder;
@@ -15,6 +17,8 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import br.com.lett.crawlernode.core.crawler.Crawler;
+import br.com.lett.crawlernode.core.models.Card;
+import br.com.lett.crawlernode.core.models.Prices;
 import br.com.lett.crawlernode.core.models.Product;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.util.Logging;
@@ -168,6 +172,9 @@ public class RibeiraopretoSavegnagoCrawler extends Crawler {
 			// Marketplace
 			JSONArray marketplace = null;
 
+			// Prices
+			Prices prices = crawlPrices(doc, price);
+			
 			Product product = new Product();
 			
 			product.setUrl(session.getOriginalURL());
@@ -175,6 +182,7 @@ public class RibeiraopretoSavegnagoCrawler extends Crawler {
 			product.setInternalPid(internalPid);
 			product.setName(name);
 			product.setPrice(price);
+			product.setPrices(prices);
 			product.setCategory1(category1);
 			product.setCategory2(category2);
 			product.setCategory3(category3);
@@ -203,4 +211,26 @@ public class RibeiraopretoSavegnagoCrawler extends Crawler {
 		return ((url.startsWith(HOME_PAGE_SPECIAL) || url.startsWith(HOME_PAGE)) && url.contains("/p?sc="));
 	}
 
+	/**
+	 * In this market, installments not appear in product page
+	 * 
+	 * @param doc
+	 * @param price
+	 * @return
+	 */
+	private Prices crawlPrices(Document doc, Float price){
+		Prices prices = new Prices();
+
+		if(price != null){
+			Map<Integer,Float> installmentPriceMap = new HashMap<>();
+
+			installmentPriceMap.put(1, price);
+			prices.insertBankTicket(price);
+
+			prices.insertCardInstallment(Card.VISA.toString(), installmentPriceMap);
+			prices.insertCardInstallment(Card.MASTERCARD.toString(), installmentPriceMap);
+		}
+
+		return prices;
+	}
 }
