cdbc86fb94ac10272d637bcf29a30d59e4cef3b1
gabriel <gabriel@gabriel-lett>
2017-01-18 10:33:14 -0200

    Colocando novamente query do jooq no processor. [deploy:Production]
    Former-commit-id: 34a0578f450af5a23ad5b590563ecf97fe94d03f

--- src/java/br/com/lett/crawlernode/processor/base/Processor.java
+++ src/java/br/com/lett/crawlernode/processor/base/Processor.java
@@ -466,15 +466,15 @@ public class Processor {
 				// estava falhando aqui
 				// voltei do jeito antigo pra apagar o fogo, estava dando problema na fastshop
 				// averiguar o motivo
-				StringBuilder query = new StringBuilder();
-								query.append("SELECT * FROM processed WHERE market = ");
-								query.append(session.getMarket().getNumber());
-								query.append(" AND internal_id = '");
-								query.append(internalId);
-								query.append("'");
-				
-				//ResultSet rs = Main.dbManager.runSelectJooq(processedTable, null, conditions);
-				ResultSet rs = Main.dbManager.runSqlConsult(query.toString());
+//				StringBuilder query = new StringBuilder();
+//								query.append("SELECT * FROM processed WHERE market = ");
+//								query.append(session.getMarket().getNumber());
+//								query.append(" AND internal_id = '");
+//								query.append(internalId);
+//								query.append("'");
+//				
+				ResultSet rs = Main.dbManager.runSelectJooq(processedTable, null, conditions);
+//				ResultSet rs = Main.dbManager.runSqlConsult(query.toString());
 
 				while(rs.next()) {
 					
