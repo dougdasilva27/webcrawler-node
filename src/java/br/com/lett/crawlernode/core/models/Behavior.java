60b6039d9ca4626ab9acdfbd7d07764c73359ef7
samirleao <samirleao@lett.com.br>
2017-03-07 18:46:48 -0300

    Modelo inicial do Behavior
    Former-commit-id: 123a8d468f5a9c9b9a0759d8086dab4a327d9a0f

---
+++ src/java/br/com/lett/crawlernode/core/models/Behavior.java
@@ -0,0 +1,54 @@
+package br.com.lett.crawlernode.core.models;
+
+public class Behavior {
+	
+	private String date;
+	private Double price;
+	private Prices prices;
+	private Boolean available;
+	private String status;
+	
+	public Behavior() {
+		super();
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Prices getPrices() {
+		return prices;
+	}
+
+	public void setPrices(Prices prices) {
+		this.prices = prices;
+	}
+
+	public Boolean getAvailable() {
+		return available;
+	}
+
+	public void setAvailable(Boolean available) {
+		this.available = available;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
