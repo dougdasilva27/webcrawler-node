c8c2cebfbae03b1c58904978ae9f9ea5b388797d
samirleao <samirleao@lett.com.br>
2017-03-22 18:12:44 -0300

    [deploy: Production]
    Former-commit-id: 2060ba3893f23323715275dca586b46e2efb6352

--- src/java/br/com/lett/crawlernode/main/Main.java
+++ src/java/br/com/lett/crawlernode/main/Main.java
@@ -107,6 +107,7 @@ public class Main {
 		// create the scheduled task to check the executor status
 		serverExecutorStatusAgent = new ServerExecutorStatusAgent();
 		serverExecutorStatusAgent.executeScheduled(new ServerExecutorStatusCollector(server), 5);
+		
 	}
 
 	private static void initProxies() {
