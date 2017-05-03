f4169e08859a505b8e60c9c0e9305e777ad2fca4
samirleao <samirleao@lett.com.br>
2017-05-03 12:08:40 -0300

    1) Rodando testes e mergindo o branch master. 2) Mergindo branch com código para identificação de formato de imagem e conversão da imagem para jpeg, caso ela seja png. [deploy: Production]
    Former-commit-id: 4037f2c3152030a6fcfd3858adcbff49bab067fd

--- src/java/br/com/lett/crawlernode/core/imgprocessing/ImageConverter.java
+++ src/java/br/com/lett/crawlernode/core/imgprocessing/ImageConverter.java
@@ -28,9 +28,9 @@ import br.com.lett.crawlernode.util.Logging;
 public class ImageConverter {
 
 	private static final Logger logger = LoggerFactory.getLogger(ImageConverter.class);
-	
+
 	private static final float ORIGINAL_IMAGE_COMPRESSION_QUALITY = 1.0f;
-	
+
 	private ImageConverter() {
 		super();
 	}
@@ -48,47 +48,54 @@ public class ImageConverter {
 	public static File createTransformedImageFile(
 			File localOriginalFile,
 			Session session) throws IOException {
-		
+
 		if (localOriginalFile == null) {
 			Logging.printLogError(logger, session, "Image downloaded is null...returning...");
 			return null;
 		}
-		
+
 		ImageCrawlerSession imageCrawlerSession = (ImageCrawlerSession)session;
-		
+
 		String imageFormatName = getImageFormatName(localOriginalFile, session);
-		
+
 		Logging.printLogDebug(logger, imageCrawlerSession, "Image format: " + imageFormatName);
+
+		File convertedToJPEG = null;
 		
 		boolean converted = false;
-		
+
 		if ( !imageFormatName.isEmpty() ) {
 			if ( imageFormatName.equals("png") ) {
 				Logging.printLogDebug(logger, imageCrawlerSession, "Converting to jpeg...");
-				pngToJPEG(localOriginalFile);
+				convertedToJPEG = pngToJPEG(localOriginalFile, session);
 				converted = true;
 			}
 		}
-		
-		BufferedImage originalBufferedImage = ImageIO.read(localOriginalFile);
-		
+
+		BufferedImage originalBufferedImage;
+		if (convertedToJPEG == null) {
+			originalBufferedImage = ImageIO.read(localOriginalFile);
+		} else {
+			originalBufferedImage = ImageIO.read(convertedToJPEG);
+		}
+
 		// compute dimensions
 		Dimension originalDimension = new Dimension(originalBufferedImage.getWidth(), originalBufferedImage.getHeight());
-		
+
 		BufferedImage transformedBufferedImage = new BufferedImage(originalDimension.width, originalDimension.height, originalBufferedImage.getType());
-		
+
 		Graphics2D graphics2dOriginal = transformedBufferedImage.createGraphics();
-		
+
 		if (!converted) {
 			graphics2dOriginal.setColor(Color.WHITE);
 		}
-		
+
 		graphics2dOriginal.fillRect(
 				0, 
 				0, 
 				originalDimension.width, 
 				originalDimension.height);
-		
+
 		graphics2dOriginal.drawImage(
 				originalBufferedImage, 
 				0, 
@@ -96,20 +103,20 @@ public class ImageConverter {
 				originalDimension.width, 
 				originalDimension.height, 
 				null);
-		
+
 		graphics2dOriginal.dispose();
 
 		// write final image to the tmp converted image file
 		ImageWriter writer = ImageIO.getImageWritersByFormatName("jpeg").next();
 		ImageWriteParam param = writer.getDefaultWriteParam();
-		
+
 		writeImage(param, writer, imageCrawlerSession.getLocalTransformedFileDir(), transformedBufferedImage);
 
 		writer.dispose();
-		
+
 		return new File(imageCrawlerSession.getLocalTransformedFileDir());	
 	}
-	
+
 	/**
 	 * Write the image to a file.
 	 * 
@@ -125,50 +132,62 @@ public class ImageConverter {
 			ImageWriter writer,
 			String fileDir, 
 			BufferedImage outputImage) throws IOException {
-				
+
 		param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
 		param.setCompressionQuality(ORIGINAL_IMAGE_COMPRESSION_QUALITY);
-		
+
 		writer.setOutput(new FileImageOutputStream(new File(fileDir)));
 		writer.write(null, new IIOImage(outputImage, null, null), param);
 	}
-	
+
 	/**
 	 * Converts a png image file to jpeg. The original file
-	 * will be overrided. 
+	 * will not be overrided, because it is the original image downloaded
+	 * from the market. This one we have to save as it is. 
 	 * <br>Remember that in our application, even
 	 * if the original file is'n a jpeg file, after the product
 	 * image on the market is downloaded, we consider it is jpeg and
-	 * automatically use extension .jpg on the file. 
-	 * <br>So, this method will automatically override the original file
-	 * with the same name and extension (which is .jpg).
+	 * automatically use extension .jpg on the file. TODO this will be changed. 
+	 * <br>This method will create the transformed file, which wil be the
+	 * final file.
 	 * 
 	 * @param pngImageFile
+	 * @param Session
+	 * @return 	<br>the converted jpg file
+	 * 			<br>null if any error occurred during conversion
 	 */
-	public static void pngToJPEG(File pngImageFile) {
+	public static File pngToJPEG(File pngImageFile, Session session) {
 		try {
 
-		  //read image file
-		  BufferedImage pngBufferedImage = ImageIO.read(pngImageFile);
+			ImageCrawlerSession imageCrawlerSession = (ImageCrawlerSession)session;
+			
+			File jpgImageFile = new File(imageCrawlerSession.getLocalTransformedFileDir());
 
-		  // create a blank, RGB, same width and height, and a white background
-		  BufferedImage newBufferedImage = new BufferedImage(
-				  pngBufferedImage.getWidth(),
-				  pngBufferedImage.getHeight(), 
-				  BufferedImage.TYPE_INT_RGB);
-		  
-		  newBufferedImage.createGraphics().drawImage(pngBufferedImage, 0, 0, Color.WHITE, null);
+			//read image file
+			BufferedImage pngBufferedImage = ImageIO.read(pngImageFile);
 
-		  // write to jpeg file
-		  ImageIO.write(newBufferedImage, "jpg", new File(pngImageFile.getAbsolutePath()));
+			// create a blank, RGB, same width and height, and a white background
+			BufferedImage newBufferedImage = new BufferedImage(
+					pngBufferedImage.getWidth(),
+					pngBufferedImage.getHeight(), 
+					BufferedImage.TYPE_INT_RGB);
+
+			newBufferedImage.createGraphics().drawImage(pngBufferedImage, 0, 0, Color.WHITE, null);
+
+			// write to jpeg file
+			ImageIO.write(newBufferedImage, "jpg", jpgImageFile);
+			
+			return jpgImageFile;
 
 		} catch (IOException e) {
 
-		  e.printStackTrace();
-
+			e.printStackTrace();
+			
+			return null;
 		}
+		
 	}
-	
+
 	/**
 	 * Get the image file format name.
 	 * 
