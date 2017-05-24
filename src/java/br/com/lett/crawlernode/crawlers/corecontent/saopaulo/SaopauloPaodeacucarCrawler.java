65781b8db8d5e9f5b47935507bf5fb518755a30c
gabriel <gabrieldornelas@lett.com.br>
2017-05-24 13:02:38 -0300

    Crawler products do pao de açucar finalizado.
    Former-commit-id: f08861fd2dc936c36c0693a038f0d8e163869fef

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/saopaulo/SaopauloPaodeacucarCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/saopaulo/SaopauloPaodeacucarCrawler.java
@@ -1,21 +1,25 @@
 package br.com.lett.crawlernode.crawlers.corecontent.saopaulo;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.http.impl.cookie.BasicClientCookie;
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
+import br.com.lett.crawlernode.core.fetcher.DataFetcher;
 import br.com.lett.crawlernode.core.models.Card;
+import br.com.lett.crawlernode.core.models.CategoryCollection;
 import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.models.ProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.Crawler;
 import br.com.lett.crawlernode.util.Logging;
+import br.com.lett.crawlernode.util.MathCommonsMethods;
 import models.Marketplace;
 import models.Prices;
 
@@ -32,12 +36,15 @@ public class SaopauloPaodeacucarCrawler extends Crawler {
 		String href = this.session.getOriginalURL().toLowerCase();
 		return !FILTERS.matcher(href).matches() && (href.startsWith(HOME_PAGE));
 	}
-
+	
+	// Loja 501 sp
+	private static final String STORE_ID = "501";
+	
 	@Override
 	public void handleCookiesBeforeFetch() {
 
-		// Criando cookie da loja 3 = São Paulo capital
-		BasicClientCookie cookie = new BasicClientCookie("ep.selected_store", "501");
+		// Criando cookie da loja 501 = São Paulo capital
+		BasicClientCookie cookie = new BasicClientCookie("ep.selected_store", STORE_ID);
 		cookie.setDomain(".paodeacucar.com.br");
 		cookie.setPath("/");
 		this.cookies.add(cookie);
@@ -50,58 +57,65 @@ public class SaopauloPaodeacucarCrawler extends Crawler {
 		super.extractInformation(doc);
 		List<Product> products = new ArrayList<>();
 
-		if ( isProductPage(this.session.getOriginalURL()) ) {
+		if ( isProductPage(session.getOriginalURL()) ) {
 			Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
 
-			// Descobrindo se produto existe na loja pão de açúcar selecionada
-			Element doNotExist = doc.select("#productArea .message404").first();
+			JSONObject jsonSku = crawlProductInformatioFromGPAApi(session.getOriginalURL());
 
-			if(doNotExist != null) {
-				Logging.printLogDebug(logger, session, "Produto " + this.session.getOriginalURL() + " não existe nessa loja do Pão de Açúcar.");
-				return products;
-			}
-
-			String internalId = crawlInternalId(doc);
-			String internalPid = crawlInternalPid(doc);
-			String name = crawlName(doc);
-			Float price = crawlPrice(doc);
-
-			// Categorias
-			Elements elementCategory = doc.select("ul.breadcrumbs__items > li > a");
-			String category1 = "";
-			String category2 = "";
-			String category3 = "";
-			if(elementCategory.size() > 1) category1 = elementCategory.get(1).text().trim();
-			if(elementCategory.size() > 2) category2 = elementCategory.get(2).text().trim();
-
-			String primaryImage = crawlMainImage(doc);
-			String secondaryImages = crawlSecondaryImages(doc);
-			String description = crawlDescription(doc);
-			boolean available = crawlAvailability(doc);
-			Integer stock = null;
-			Marketplace marketplace = new Marketplace();
-			Prices prices = crawlPrices(doc, price);
+			// InternalId 
+			String internalId = crawlInternalId(jsonSku);
 			
-			Product product = new Product();
-			product.setUrl(this.session.getOriginalURL());
-			product.setInternalId(internalId);
-			product.setInternalPid(internalPid);
-			product.setName(name);
-			product.setPrice(price);
-			product.setPrices(prices);
-			product.setCategory1(category1);
-			product.setCategory2(category2);
-			product.setCategory3(category3);
-			product.setPrimaryImage(primaryImage);
-			product.setSecondaryImages(secondaryImages);
-			product.setDescription(description);
-			product.setStock(stock);
-			product.setMarketplace(marketplace);
-			product.setAvailable(available);
+			// Pid
+			String internalPid = crawlInternalPid(jsonSku);
 
-			if ( !(product.getAvailable() == false && product.getPrice() != null) ) {
-				products.add(product);
-			}
+			// Categories
+			CategoryCollection categories = crawlCategories(jsonSku);
+
+			// Description
+			String description = crawlDescription(jsonSku);
+			
+			// Availability
+			boolean available = crawlAvailability(jsonSku);
+			
+			// Price
+			Float price = crawlPrice(jsonSku);
+
+			// Primary image
+			String primaryImage = crawlPrimaryImage(jsonSku);
+
+			// Name
+			String name = crawlName(jsonSku);
+
+			// Secondary images
+			String secondaryImages = crawlSecondaryImages(jsonSku, primaryImage);
+
+			// Prices
+			Prices prices = crawlPrices(price);
+			
+			// Stock
+			Integer stock = null;
+
+			// Creating the product
+			Product product = ProductBuilder.create()
+					.setUrl(session.getOriginalURL())
+					.setInternalId(internalId)
+					.setInternalPid(internalPid)
+					.setName(name)
+					.setPrice(price)
+					.setPrices(prices)
+					.setAvailable(available)
+					.setCategory1(categories.getCategory(0))
+					.setCategory2(categories.getCategory(1))
+					.setCategory3(categories.getCategory(2))
+					.setPrimaryImage(primaryImage)
+					.setSecondaryImages(secondaryImages)
+					.setDescription(description)
+					.setStock(stock)
+					.setMarketplace(new Marketplace())
+					.build();
+
+			products.add(product);
+		
 
 		} else {
 			Logging.printLogDebug(logger, session, "Not a product page" + this.session.getOriginalURL());
@@ -115,77 +129,145 @@ public class SaopauloPaodeacucarCrawler extends Crawler {
 	 *******************************/
 
 	private boolean isProductPage(String url) {
-		return url.contains("/produto/");
-	}
-	
-	private String crawlInternalId(Document doc) {
-		Element elementInternalId = doc.select("input[name=productId]").first();
-		if (elementInternalId != null) {
-			return elementInternalId.attr("value").trim();
+		if (url.contains("paodeacucar.com/produto/")) {
+			return true;
 		}
-		return null;
+		return false;
 	}
-	
-	private String crawlInternalPid(Document doc) {
-		return null;
+
+	/*******************
+	 * General methods *
+	 *******************/
+
+	private String crawlInternalId(JSONObject json) {
+		String internalId = null;
+
+		if (json.has("id")) {
+			internalId = json.get("id").toString();			
+		}
+
+		return internalId;
+	}	
+
+
+	private String crawlInternalPid(JSONObject json) {
+		String internalPid = null;
+
+		if (json.has("id")) {
+			internalPid = json.getString("sku");			
+		}
+
+		return internalPid;
 	}
-	
-	private String crawlName(Document doc) {
-		Elements elementName = doc.select("h1.product-header__heading");
-		return elementName.text().replace("'", "").trim();
+
+	private String crawlName(JSONObject json) {
+		String name = null;
+		
+		if (json.has("name")) {
+			name = json.getString("name");			
+		}
+
+		return name;
 	}
-	
-	private Float crawlPrice(Document doc) {
+
+	private Float crawlPrice(JSONObject json) {
 		Float price = null;
-		Element elementPrice = doc.select("div.product-control__price.price_per > span.value").last();
-		if(elementPrice != null) {
-			price = Float.parseFloat( elementPrice.text().trim().replaceAll("[^0-9,]+", "").replaceAll("\\.", "").replaceAll(",", ".") );
-		} else {
-			elementPrice = doc.select("div.product-control__price > span.value").first();
-			if (elementPrice != null) {
-				price = Float.parseFloat( elementPrice.text().trim().replaceAll("[^0-9,]+", "").replaceAll("\\.", "").replaceAll(",", ".") );
-			}
+
+		if (json.has("currentPrice")) {
+			Double pDouble = json.getDouble("currentPrice");
+			price = MathCommonsMethods.normalizeTwoDecimalPlaces(pDouble.floatValue());
 		}
+
 		return price;
 	}
-	
-	private String crawlMainImage(Document doc) {
-		Elements elementPrimaryImage = doc.select("#product-image > a.zoomImage");
-		String primaryImage = elementPrimaryImage.attr("href");
 
-		// Se não tem a foto grande, pega a miniatura mesmo
-		if(primaryImage.equals("#")) {
-			elementPrimaryImage = doc.select("#product-image > a.zoomImage > img");
-			primaryImage = elementPrimaryImage.attr("src");
-		}
-		primaryImage = "http://www.paodeacucar.com.br" + primaryImage;
-		if(primaryImage.contains("nome_da_imagem_do_sem_foto.gif")) {
-			primaryImage = ""; //TODO: Verificar o nome da foto genérica
+	private boolean crawlAvailability(JSONObject json) {
+		if(json.has("stock") && json.getBoolean("stock")) {
+			return true;
 		}
 		
+		return false;
+	}
+
+	private String crawlPrimaryImage(JSONObject json) {
+		String primaryImage = null;
+
+		if(json.has("mapOfImages")) {
+			JSONObject images = json.getJSONObject("mapOfImages");
+			
+			for(int i = 0; i < images.length(); i++) {
+				if(images.length() > 0 && images.has(Integer.toString(i))) {
+					JSONObject imageObj = images.getJSONObject(Integer.toString(i));
+					
+					if(imageObj.has("BIG") && !imageObj.getString("BIG").isEmpty()) {
+						String image = HOME_PAGE + imageObj.getString("BIG");
+						
+						if(image.contains("img")) {
+							primaryImage = HOME_PAGE + imageObj.getString("BIG");
+						}
+					} else if(imageObj.has("MEDIUM") && !imageObj.getString("MEDIUM").isEmpty()) {
+						String image = HOME_PAGE + imageObj.getString("MEDIUM");;
+						
+						if(image.contains("img")) {
+							primaryImage = HOME_PAGE + imageObj.getString("MEDIUM");
+						}
+					} else if(imageObj.has("SMALL") && !imageObj.getString("SMALL").isEmpty()) {
+						String image = HOME_PAGE + imageObj.getString("SMALL");;
+						
+						if(image.contains("img")) {
+							primaryImage = HOME_PAGE + imageObj.getString("SMALL");
+						}
+					}
+				}
+				
+				if(primaryImage != null) {
+					break;
+				}
+			}
+		}
+
 		return primaryImage;
 	}
-	
-	private String crawlSecondaryImages(Document doc) {
-		String secondaryImages = "";
+
+	private String crawlSecondaryImages(JSONObject json, String primaryImage) {
+		String secondaryImages = null;
 		JSONArray secondaryImagesArray = new JSONArray();
 
-		Elements secondaryImageElement = doc.select(".product-image__gallery--holder ul li a");
-
-		if(secondaryImageElement.size() > 1) {
-			for(int i = 1; i < secondaryImageElement.size(); i++) { // a partir da segunda, porque a primeira é imagem primária
-				String tmp;
-
-				// tentar pegar a imagem grande
-				tmp = secondaryImageElement.get(i).attr("data-zoom").toString();
-
-				if(tmp.equals("#")) { // se não tem a grande, pegar a que tiver
-					tmp = secondaryImageElement.get(i).attr("href");
+		String primaryImageId = getImageId(primaryImage);
+		
+		if(json.has("mapOfImages")) {
+			JSONObject images = json.getJSONObject("mapOfImages");
+			
+			for(int i = 1; i < images.length(); i++) { // index 0 may be a primary Image
+				if(images.length() > 0 && images.has(Integer.toString(i))) {
+					JSONObject imageObj = images.getJSONObject(Integer.toString(i));
+					
+					if(imageObj.has("BIG") && !imageObj.getString("BIG").isEmpty()) {
+						String image = HOME_PAGE + imageObj.getString("BIG");
+						String imageId = getImageId(image);
+						
+						if(image.contains("img") && !imageId.equals(primaryImageId)) {
+							secondaryImagesArray.put(HOME_PAGE + imageObj.getString("BIG"));
+						}
+					} else if(imageObj.has("MEDIUM") && !imageObj.getString("MEDIUM").isEmpty()) {
+						String image = HOME_PAGE + imageObj.getString("MEDIUM");
+						String imageId = getImageId(image);
+						
+						if(image.contains("img") && !imageId.equals(primaryImageId)) {
+							secondaryImagesArray.put(HOME_PAGE + imageObj.getString("MEDIUM"));
+						}
+					} else if(imageObj.has("SMALL") && !imageObj.getString("SMALL").isEmpty()) {
+						String image = HOME_PAGE + imageObj.getString("SMALL");
+						String imageId = getImageId(image);
+						
+						if(image.contains("img") && !imageId.equals(primaryImageId)) {
+							secondaryImagesArray.put(HOME_PAGE + imageObj.getString("SMALL"));
+						}
+					}
 				}
-				tmp = "http://www.paodeacucar.com.br" + tmp;
-				secondaryImagesArray.put(tmp);
-			}						
+			}
 		}
+
 		if(secondaryImagesArray.length() > 0) {
 			secondaryImages = secondaryImagesArray.toString();
 		}
@@ -193,45 +275,166 @@ public class SaopauloPaodeacucarCrawler extends Crawler {
 		return secondaryImages;
 	}
 	
-	private boolean crawlAvailability(Document doc) {
-		boolean available = false;
-		Element elementAvailable = doc.select(".btnComprarProd").first();
-		if (elementAvailable != null) {
-			available = true;
+	private String getImageId(String imageUrl) {
+		if(imageUrl != null) {
+			return imageUrl.replace(HOME_PAGE, "").split("/")[4];
 		}
-		return available;
+		
+		return null;
+	}
+
+	private CategoryCollection crawlCategories(JSONObject json) {
+		CategoryCollection categories = new CategoryCollection();
+		
+		if(json.has("shelfList")) {
+			JSONArray shelfList = json.getJSONArray("shelfList");
+			
+			List<String> listCategories = new ArrayList<>(); // It is a "set" because it has been noticed that there are repeated categories
+			
+			for(int i = shelfList.length() - 1; i >= 0; i--) { // the last item is the first category and the first item is the last category
+				JSONObject cat = shelfList.getJSONObject(i);
+				
+				if(cat.has("name") && !listCategories.contains(cat.getString("name"))) {
+					listCategories.add(cat.getString("name"));
+				}
+			}
+			
+			for(String category : listCategories) {
+				categories.add(category);
+			}
+		}		
+
+		return categories;
+	}
+
+
+	private String crawlDescription(JSONObject json) {
+		String description = "";
+		
+		if(json.has("shortDescription")){
+			description += json.getString("shortDescription");
+		}
+		
+		// This key in json has a map of attributes -> {label: "", value = ""} , For crawl niutritional table we make the html and put the values in html
+		if(json.has("nutritionalMap") && json.getJSONObject("nutritionalMap").length() > 0) {
+			JSONObject nutritionalJson = json.getJSONObject("nutritionalMap");
+			
+			StringBuilder  str = new StringBuilder();
+			
+			str.append (
+				"<div class=\"product-nutritional-table\">\n" +
+				"	<p class=\"title\">Tabela nutricional</p>\n" +
+				"	<!-- ngIf: productDetailCtrl.product.nutritionalMap.cabecalho -->" +
+						"<div class=\"main-infos ng-scope\" ng-if=\"productDetailCtrl.product.nutritionalMap.cabecalho\">\n" +
+				"			<p ng-bind-html=\"productDetailCtrl.product.nutritionalMap.cabecalho || " +
+								"productDetailCtrl.product.nutritionalMap.cabecalho.value\" class=\"ng-binding\"></p>\n" +
+				"		</div><!-- end ngIf: productDetailCtrl.product.nutritionalMap.cabecalho -->\n" +
+				"		<table class=\"table table-responsive\">\n" +
+				"			<thead>\n" +
+				"				<tr>\n" +
+				"					<th>Item</th>\n" +
+				"					<th>Quantidade por porção</th>\n" +
+				"					<th>Valores diários</th>\n" +
+				"				</tr>\n" +
+				"			</thead>\n"
+			);
+			str.append(crawlNutritionalTableAttributes(nutritionalJson));
+			str.append("</table>\n</div>");
+			
+			description += str.toString();
+		}
+
+		return description;
 	}
 	
-	private String crawlDescription(Document doc) {
-		Element elementDescriptionText = doc.select("#nutritionalChart").first();
-		if(elementDescriptionText != null) {
-			return elementDescriptionText.html();
+	private String crawlNutritionalTableAttributes(JSONObject nutritionalMap) {
+		StringBuilder str = new StringBuilder();
+		str.append("<tbody>");
+		
+		List<String> attributesList = Arrays.asList("valor_energetico", "carboidratos", "acucares", 
+													"proteinas", "gorduras_totais", "gorduras_saturadas",
+													"gorduras_trans", "fibra_alimentar", "sodio", "rodape");
+		
+		for(String attribute : attributesList){
+			if(nutritionalMap.has(attribute)) {
+				JSONObject attributeJson = nutritionalMap.getJSONObject(attribute);
+				
+				if(attributeJson.has("value") && attributeJson.has("label")) {
+					str.append(putAttribute(attributeJson.getString("value"), attributeJson.getString("label")));
+				}
+			}
 		}
+		
+		str.append("</tbody");
+		return str.toString();
+	}
+	
+	private String putAttribute(String value, String label) {
+		if(label != null) {
+			if(label.equalsIgnoreCase("rodape")) {
+				return
+						"<tfoot>\n" +
+						"	<tr>\n" +
+						"		<td colspan=\"3\" ng-bind-html=\"productDetailCtrl.product.nutritionalMap.rodape.value\"" +
+									"class=\"last ng-binding\">"+ value + "</td>\n" +
+						"	</tr>\n" +
+						"</tfoot>\n";
+			} else {
+				return
+				"	<tr ng-repeat=\"(key, item) in productDetailCtrl.product.nutritionalMap \" ng-if=\"[ 'cabecalho', 'rodape'].indexOf(key) === -1 \" class=\"ng-scope\">\n" +
+				"		<td class=\"ng-binding\">"+ label +"</td>\n" +
+				"		<td class=\"ng-binding\">"+ value +"</td>\n" +
+				"		<td class=\"ng-binding\"></td>\n" +
+				"	</tr><!-- end ngIf: [ 'cabecalho', 'rodape'].indexOf(key) === -1 --><!-- end ngRepeat: "
+						+ "(key, item) in productDetailCtrl.product.nutritionalMap --><!-- ngIf: [ 'cabecalho', 'rodape'].indexOf(key) === -1 -->"
+						+ "<tr ng-repeat=\"(key, item) in productDetailCtrl.product.nutritionalMap \" ng-if=\"[ 'cabecalho', 'rodape'].indexOf(key) === -1 "
+						+ "\" class=\"ng-scope\">\n";
+			}
+		}
+		
 		return "";
 	}
-	
+
 	/**
-	 * In this market, installments not appear in product page
-	 * 
-	 * @param doc
+	 * In this site has no information of installments
 	 * @param price
 	 * @return
 	 */
-	private Prices crawlPrices(Document doc, Float price){
-		Prices prices = new Prices();
+	private Prices crawlPrices(Float price){
+		Prices p = new Prices();
 
-		if(price != null){
-			Map<Integer,Float> installmentPriceMap = new HashMap<>();
-
+		if(price != null) {
+			Map<Integer, Float> installmentPriceMap = new HashMap<>();
 			installmentPriceMap.put(1, price);
-			prices.setBankTicketPrice(price);
+			
+			p.setBankTicketPrice(price);
+			
+			p.insertCardInstallment(Card.VISA.toString(), installmentPriceMap);
+			p.insertCardInstallment(Card.MASTERCARD.toString(), installmentPriceMap);
 
-			prices.insertCardInstallment(Card.VISA.toString(), installmentPriceMap);
-			prices.insertCardInstallment(Card.MASTERCARD.toString(), installmentPriceMap);
-			prices.insertCardInstallment(Card.AMEX.toString(), installmentPriceMap);
-			prices.insertCardInstallment(Card.DINERS.toString(), installmentPriceMap);
+		}	
+
+		return p;
+	}
+
+
+	/**
+	 * Get the json of gpa api, 
+	 * this api has all info of product
+	 * @return
+	 */
+	private JSONObject crawlProductInformatioFromGPAApi(String productUrl) { 
+		JSONObject productsInfo = new JSONObject();
+		
+		String id = productUrl.replace(HOME_PAGE, "").split("/")[2];
+		String url = "https://api.gpa.digital/pa/products/"+ id +"?storeId="+ STORE_ID +"&isClienteMais=false";
+		
+		JSONObject apiGPA = DataFetcher.fetchJSONObject(DataFetcher.GET_REQUEST, session, url, null, cookies);
+		
+		if(apiGPA.has("content")) {
+			productsInfo = apiGPA.getJSONObject("content");
 		}
-
-		return prices;
+		
+		return productsInfo;
 	}
 }
