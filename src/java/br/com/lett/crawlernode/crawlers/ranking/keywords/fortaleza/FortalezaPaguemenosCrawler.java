c17ed31d1a61a416e55d9acd088ce466b5e0199b
gabriel <gabrieldornelas@lett.com.br>
2017-07-17 09:22:43 -0300

    lançando rabking paguemenos fortaleza [deploy:Production]
    Former-commit-id: 05bc71942d29ad22d0bc44b5b2ad4a5f487f114e

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/fortaleza/FortalezaPaguemenosCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/fortaleza/FortalezaPaguemenosCrawler.java
@@ -19,77 +19,8 @@ public class FortalezaPaguemenosCrawler extends CrawlerRankingKeywords {
 
 	private static final String COOKIE_ESTADO = "StoreCodePagueMenos";
 	private static final String COOKIE_VALUE = "52";
-	private List<Cookie> cookies = new ArrayList<Cookie>();
-	
-	@Override
-	protected void processBeforeFetch() {
-		if(this.cookies.size() < 1){
-			// Criando cookie da loja 52 = Fortaleza
-		    BasicClientCookie cookie = new BasicClientCookie(COOKIE_ESTADO, COOKIE_VALUE);
-		    cookie.setDomain("loja.paguemenos.com.br");
-		    cookie.setPath("/");
-		    
-		    this.cookies.add(cookie);
-		}
-	}
-	
-	@Override
-	public void extractProductsFromCurrentPage() {
-		//número de produtos por página do market
-		this.pageSize = 20;
+	private List<Cookie> cookies = new ArrayList<>();
 
-		this.log("Página "+ this.currentPage);
-		
-		String key = this.keywordWithoutAccents.replaceAll(" ", "%20");
-		
-		//monta a url com a keyword e a página
-		String url = "http://loja.paguemenos.com.br/busca/3/0/0/Nome/Crescente/20/"+ this.currentPage +"////"+ key +".aspx";
-		this.log("Link onde são feitos os crawlers: "+url);		
-		
-		//chama função de pegar a url
-		this.currentDoc = fetchDocument(url, cookies);
-			
-		Elements products =  this.currentDoc.select("#listProduct > li");
-		
-		//se essa página tiver resultado faça:	
-		if(products.size() >= 1) {			
-			for(Element e: products) {
-				// InternalPid
-				String internalPid 	= crawlInternalPid(e);
-				
-				// InternalId
-				String internalId 	= crawlInternalId(e);
-				
-				// Url do produto
-				String urlProduct = crawlProductUrl(e);
-				
-				saveDataProduct(internalId, internalPid, urlProduct);
-				
-				this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + internalPid + " - Url: " + urlProduct);
-				if(this.arrayProducts.size() == productsLimit) break;
-			}
-		} else {
-			setTotalProducts();
-			this.result = false;
-			this.log("Keyword sem resultado!");
-		}
-			
-		this.log("Finalizando Crawler de produtos da página "+this.currentPage+" - até agora "+this.arrayProducts.size()+" produtos crawleados");
-		if(!(hasNextPage())) setTotalProducts();
-	}
-	
-	@Override
-	protected boolean hasNextPage(){
-		Element page = this.currentDoc.select(".set-next.off").first();
-		
-		if(page != null){
-			return false;
-		}
-		
-		return true;
-		
-	}
-	
 	private String crawlInternalId(Element e) {
 		String internalId = null;
 		
@@ -115,16 +46,89 @@ public class FortalezaPaguemenosCrawler extends CrawlerRankingKeywords {
 		
 		return internalPid;
 	}
-	
-	private String crawlProductUrl(Element e){
+
+	private String crawlProductUrl(Element e) {
 		String urlProduct = null;
-		Element eUrl =  e.select(" a.link.url[title]").first();
-			
-		if(eUrl != null){
+		Element eUrl = e.select(" a.link.url[title]").first();
+
+		if (eUrl != null) {
 			urlProduct = eUrl.attr("href");
 		}
-		
-		
+
 		return urlProduct;
+	}
+
+	@Override
+	public void extractProductsFromCurrentPage() {
+		// número de produtos por página do market
+		this.pageSize = 20;
+
+		this.log("Página " + this.currentPage);
+
+		String key = this.keywordWithoutAccents.replaceAll(" ", "%20");
+
+		// monta a url com a keyword e a página
+		String url = "http://loja.paguemenos.com.br/busca/3/0/0/Nome/Crescente/20/" + this.currentPage + "////" + key
+				+ ".aspx";
+		this.log("Link onde são feitos os crawlers: " + url);
+
+		// chama função de pegar a url
+		this.currentDoc = fetchDocument(url, cookies);
+
+		Elements products = this.currentDoc.select("#listProduct > li");
+
+		// se essa página tiver resultado faça:
+		if (products.size() >= 1) {
+			for (Element e : products) {
+				// InternalPid
+				String internalPid = crawlInternalPid(e);
+
+				// InternalId
+				String internalId = crawlInternalId(e);
+
+				// Url do produto
+				String urlProduct = crawlProductUrl(e);
+
+				saveDataProduct(internalId, internalPid, urlProduct);
+
+				this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: "
+						+ internalPid + " - Url: " + urlProduct);
+				if (this.arrayProducts.size() == productsLimit)
+					break;
+			}
+		} else {
+			setTotalProducts();
+			this.result = false;
+			this.log("Keyword sem resultado!");
+		}
+
+		this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora "
+				+ this.arrayProducts.size() + " produtos crawleados");
+		if (!(hasNextPage()))
+			setTotalProducts();
+	}
+
+	@Override
+	protected boolean hasNextPage() {
+		Element page = this.currentDoc.select(".set-next.off").first();
+
+		if (page != null) {
+			return false;
+		}
+
+		return true;
+
+	}
+
+	@Override
+	protected void processBeforeFetch() {
+		if (this.cookies.size() < 1) {
+			// Criando cookie da loja 52 = Fortaleza
+			BasicClientCookie cookie = new BasicClientCookie(COOKIE_ESTADO, COOKIE_VALUE);
+			cookie.setDomain("loja.paguemenos.com.br");
+			cookie.setPath("/");
+
+			this.cookies.add(cookie);
+		}
 	}
 }
