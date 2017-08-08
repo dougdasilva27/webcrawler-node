7306f29fb64e4fb4c393feeb149f08fc08c9171a
gabriel <gabrieldornelas@lett.com.br>
2017-08-08 17:11:53 -0300

    Finalizei crawler drogarias povao.
    Former-commit-id: c35b231a58cfba04f976653f2a5c57240a4678ef

---
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilDrogariaspovaoCrawler.java
@@ -0,0 +1,256 @@
+package br.com.lett.crawlernode.crawlers.corecontent.brasil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import br.com.lett.crawlernode.core.models.Card;
+import br.com.lett.crawlernode.core.models.CategoryCollection;
+import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.models.ProductBuilder;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.Crawler;
+import br.com.lett.crawlernode.util.CommonMethods;
+import br.com.lett.crawlernode.util.Logging;
+import br.com.lett.crawlernode.util.MathCommonsMethods;
+import models.Marketplace;
+import models.prices.Prices;
+
+/**
+ * Date: 08/08/2017
+ * 
+ * @author Gabriel Dornelas
+ *
+ */
+public class BrasilDrogariaspovaoCrawler extends Crawler {
+
+	private final String HOME_PAGE = "http://www.drogariaspovao.com.br";
+
+	public BrasilDrogariaspovaoCrawler(Session session) {
+		super(session);
+	}
+
+	@Override
+	public boolean shouldVisit() {
+		String href = session.getOriginalURL().toLowerCase();
+		return !FILTERS.matcher(href).matches() && (href.startsWith(HOME_PAGE));
+	}
+
+	@Override
+	public List<Product> extractInformation(Document doc) throws Exception {
+		super.extractInformation(doc);
+		List<Product> products = new ArrayList<>();
+
+		if ( isProductPage(doc) ) {
+			Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
+
+			String internalId = crawlInternalId(doc);
+			String internalPid = null;
+			String name = crawlName(doc);
+			Float price = crawlPrice(doc);
+			Prices prices = crawlPrices(price, doc);
+			boolean available = crawlAvailability(price);
+			CategoryCollection categories = crawlCategories(doc);
+			String primaryImage = crawlPrimaryImage(doc);
+			String secondaryImages = crawlSecondaryImages(doc);
+			String description = crawlDescription(doc);
+			Integer stock = null;
+			Marketplace marketplace = crawlMarketplace();
+
+			// Creating the product
+			Product product = ProductBuilder.create()
+					.setUrl(session.getOriginalURL())
+					.setInternalId(internalId)
+					.setInternalPid(internalPid)
+					.setName(name)
+					.setPrice(price)
+					.setPrices(prices)
+					.setAvailable(available)
+					.setCategory1(categories.getCategory(0))
+					.setCategory2(categories.getCategory(1))
+					.setCategory3(categories.getCategory(2))
+					.setPrimaryImage(primaryImage)
+					.setSecondaryImages(secondaryImages)
+					.setDescription(description)
+					.setStock(stock)
+					.setMarketplace(marketplace)
+					.build();
+
+			products.add(product);
+
+		} else {
+			Logging.printLogDebug(logger, session, "Not a product page" + this.session.getOriginalURL());
+		}
+
+		return products;
+
+	}
+
+	private boolean isProductPage(Document doc) {
+		if (doc.select(".arial24_666").first() != null) {
+			return true;
+		}
+		return false;
+	}
+
+	private String crawlInternalId(Document doc) {
+		String internalId = null;
+
+		Elements scripts = doc.select("script");
+		
+		for(Element e : scripts) {
+			String script = e.outerHtml();
+			
+			if(script.contains("dataLayer.push")) {				
+				int x = script.indexOf('(') + 1;
+				int y = script.indexOf(");", x)+1;
+				
+				try {
+					JSONObject datalayer = new JSONObject(script.substring(x, y));
+					
+					if(datalayer.has("google_tag_params")) {
+						JSONObject google = datalayer.getJSONObject("google_tag_params");
+						
+						if(google.has("ecomm_prodid")) {
+							internalId = google.getString("ecomm_prodid").trim();
+						}
+					}
+				} catch (Exception e1) {
+					Logging.printLogError(logger, session, CommonMethods.getStackTrace(e1));
+				}
+				
+				break;
+			}
+		}
+
+		return internalId;
+	}
+
+	private String crawlName(Document document) {
+		String name = null;
+		Element nameElement = document.select(".arial24_666").first();
+
+		if (nameElement != null) {
+			name = nameElement.ownText().trim();
+		}
+
+		return name;
+	}
+
+	private Float crawlPrice(Document document) {
+		Float price = null;
+
+		String priceText = null;
+		Element salePriceElement = document.select(".arial24red").first();
+
+		if (salePriceElement != null) {
+			priceText = salePriceElement.text();
+			price = MathCommonsMethods.parseFloat(priceText);
+		}
+
+		return price;
+	}
+
+	private Marketplace crawlMarketplace() {
+		return new Marketplace();
+	}
+
+
+	private String crawlPrimaryImage(Document doc) {
+		String primaryImage = null;
+		Element elementPrimaryImage = doc.select("td[width=\"26%\"] img").first();
+		
+		if(elementPrimaryImage != null ) {
+			primaryImage = HOME_PAGE + "/" + elementPrimaryImage.attr("src");
+		} 
+		
+		return primaryImage;
+	}
+
+	/**
+	 * In the time when this crawler was made, this market hasn't secondary Images
+	 * @param doc
+	 * @return
+	 */
+	private String crawlSecondaryImages(Document doc) {
+		String secondaryImages = null;
+		JSONArray secondaryImagesArray = new JSONArray();
+
+		if (secondaryImagesArray.length() > 0) {
+			secondaryImages = secondaryImagesArray.toString();
+		}
+
+		return secondaryImages;
+	}
+
+	/**
+	 * @param document
+	 * @return
+	 */
+	private CategoryCollection crawlCategories(Document document) {
+		CategoryCollection categories = new CategoryCollection();
+		Elements elementCategories = document.select("a.bread");
+		
+		for (int i = 1; i < elementCategories.size(); i++) { 
+			String cat = elementCategories.get(i).ownText().trim();
+			
+			if(!cat.isEmpty()) {
+				categories.add( cat );
+			}
+		}
+
+		return categories;
+	}
+
+	private String crawlDescription(Document doc) {
+		StringBuilder description = new StringBuilder();
+		
+		Element elementDescription = doc.select(".arial13black").first();
+		
+		if (elementDescription != null) {
+			description.append(elementDescription.html());		
+		}
+		
+		return description.toString();
+	}
+	
+	/**
+	 * In the time this crawler was made, every product was available
+	 * @param doc
+	 * @return
+	 */
+	private boolean crawlAvailability(Float price) {
+		return price != null;		
+	}
+
+	/**
+	 * 
+	 * @param doc
+	 * @param price
+	 * @return
+	 */
+	private Prices crawlPrices(Float price, Document doc) {
+		Prices prices = new Prices();
+		
+		if (price != null) {
+			Map<Integer,Float> installmentPriceMap = new TreeMap<>();
+			installmentPriceMap.put(1, price);
+			prices.setBankTicketPrice(price);
+			
+			prices.insertCardInstallment(Card.VISA.toString(), installmentPriceMap);
+			prices.insertCardInstallment(Card.MASTERCARD.toString(), installmentPriceMap);
+			prices.insertCardInstallment(Card.AMEX.toString(), installmentPriceMap);
+			prices.insertCardInstallment(Card.HIPERCARD.toString(), installmentPriceMap);
+		}
+		
+		return prices;
+	}
+
+}
---
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilDrogariaspovaoCrawler.java
@@ -0,0 +1,125 @@
+package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
+
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+
+public class BrasilDrogariaspovaoCrawler extends CrawlerRankingKeywords{
+
+	public BrasilDrogariaspovaoCrawler(Session session) {
+		super(session);
+	}
+
+	@Override
+	protected void extractProductsFromCurrentPage() {
+		//número de produtos por página do market
+		this.pageSize = 24;
+	
+		this.log("Página "+ this.currentPage);
+		
+		String keyword = this.keywordWithoutAccents.replace(" ", "%20");
+		
+		//monta a url com a keyword e a página
+		String url = "http://www.drogariaspovao.com.br/index.php?controle=004_shopping_busca&cart_buscar="+ keyword +"&pagenum="+ this.currentPage;
+		this.log("Link onde são feitos os crawlers: "+url);	
+		
+		//chama função de pegar o html
+		this.currentDoc = fetchDocument(url);
+
+		Elements products =  this.currentDoc.select("table[bgcolor=\"#ffffff\"] tbody tr[align=center]");
+		
+		//se obter 1 ou mais links de produtos e essa página tiver resultado faça:
+		if(products.size() >= 1) {
+			if(totalProducts == 0) {
+				setTotalProducts();
+			}
+			
+			for(Element e : products) {
+				// InternalPid
+				String internalPid = null;
+				
+				// InternalId
+				String internalId = crawlInternalId(e);
+				
+				// Url do produto
+				String productUrl = crawlProductUrl(e);
+				
+				saveDataProduct(internalId, internalPid, productUrl);
+				
+				this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + internalPid + " - Url: " + productUrl);
+				if(this.arrayProducts.size() == productsLimit) {
+					break;
+				}
+				
+			}
+		} else {
+			this.result = false;
+			this.log("Keyword sem resultado!");
+		}
+		
+		this.log("Finalizando Crawler de produtos da página "+this.currentPage+" - até agora "+this.arrayProducts.size()+" produtos crawleados");
+	}
+
+	@Override
+	protected boolean hasNextPage() {
+		Element lastPageElement = this.currentDoc.select(".arial12_666").last();
+		
+		if(lastPageElement != null) {
+			String textPage = lastPageElement.text().replaceAll("[^0-9]", "").trim();
+			Integer lastPage = textPage.isEmpty() ? 0 : Integer.parseInt(textPage);
+			
+			if(lastPage > this.currentPage) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	@Override
+	protected void setTotalProducts() {
+		Element total = this.currentDoc.select("td[align=left] .arial12_666").first();
+		
+		if(total == null) {
+			total = this.currentDoc.select("td[align=left] .textmiddle").first();
+		}
+		
+		if(total != null) {
+			String totalText = total.ownText().trim().replaceAll("[^0-9]", "");
+			
+			if(!totalText.isEmpty()) {
+				this.totalProducts = Integer.parseInt(totalText);
+			}
+		}
+		
+		this.log("Total products: " + this.totalProducts);
+	}
+	
+	private String crawlInternalId(Element e){
+		String internalId = null;
+		Element idElement = e.select("input.qtytextbox[id]").first();
+		
+		if(idElement != null) {
+			internalId = idElement.attr("id");
+		}
+		
+		return internalId;
+	}
+	
+	private String crawlProductUrl(Element e){
+		String productUrl = null;
+		Element url = e.select("a").first();
+		
+		if(url != null) {
+			productUrl = url.attr("href");
+			
+			if(!productUrl.startsWith("http://www.drogariaspovao.com.br/")) {
+				productUrl = "http://www.drogariaspovao.com.br/" + productUrl;
+			}
+		}
+		
+		return productUrl;
+	}
+}
