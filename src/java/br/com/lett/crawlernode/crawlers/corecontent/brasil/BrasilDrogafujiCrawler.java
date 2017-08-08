240a279f616d8fb32c0085921175e009f8d9a3c0
gabriel <gabrieldornelas@lett.com.br>
2017-08-08 11:54:42 -0300

    Finalizei crawler drogafuji
    Former-commit-id: cf6f6ff7a1c46a6bac22415e1d6b48c046ebeee2

---
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilDrogafujiCrawler.java
@@ -0,0 +1,270 @@
+package br.com.lett.crawlernode.crawlers.corecontent.brasil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.json.JSONArray;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import br.com.lett.crawlernode.core.models.Card;
+import br.com.lett.crawlernode.core.models.CategoryCollection;
+import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.models.ProductBuilder;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.Crawler;
+import br.com.lett.crawlernode.util.Logging;
+import br.com.lett.crawlernode.util.MathCommonsMethods;
+import models.Marketplace;
+import models.prices.Prices;
+
+/**
+ * Date: 08/08/2017
+ * 
+ * @author Gabriel Dornelas
+ *
+ */
+public class BrasilDrogafujiCrawler extends Crawler {
+
+	private final String HOME_PAGE = "https://www.drogafuji.com.br/";
+
+	public BrasilDrogafujiCrawler(Session session) {
+		super(session);
+	}
+
+	@Override
+	public boolean shouldVisit() {
+		String href = session.getOriginalURL().toLowerCase();
+		return !FILTERS.matcher(href).matches() && (href.startsWith(HOME_PAGE));
+	}
+
+	@Override
+	public List<Product> extractInformation(Document doc) throws Exception {
+		super.extractInformation(doc);
+		List<Product> products = new ArrayList<>();
+
+		if ( isProductPage(doc) ) {
+			Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
+
+			String internalId = crawlInternalId(doc);
+			String internalPid = crawlInternalPid(doc);
+			String name = crawlName(doc);
+			Float price = crawlPrice(doc, internalId);
+			Prices prices = crawlPrices(price, doc);
+			boolean available = crawlAvailability(doc);
+			CategoryCollection categories = crawlCategories(doc);
+			String primaryImage = crawlPrimaryImage(doc);
+			String secondaryImages = crawlSecondaryImages(doc);
+			String description = crawlDescription(doc);
+			Integer stock = null;
+			Marketplace marketplace = crawlMarketplace();
+
+			// Creating the product
+			Product product = ProductBuilder.create()
+					.setUrl(session.getOriginalURL())
+					.setInternalId(internalId)
+					.setInternalPid(internalPid)
+					.setName(name)
+					.setPrice(price)
+					.setPrices(prices)
+					.setAvailable(available)
+					.setCategory1(categories.getCategory(0))
+					.setCategory2(categories.getCategory(1))
+					.setCategory3(categories.getCategory(2))
+					.setPrimaryImage(primaryImage)
+					.setSecondaryImages(secondaryImages)
+					.setDescription(description)
+					.setStock(stock)
+					.setMarketplace(marketplace)
+					.build();
+
+			products.add(product);
+
+		} else {
+			Logging.printLogDebug(logger, session, "Not a product page" + this.session.getOriginalURL());
+		}
+
+		return products;
+
+	}
+
+	private boolean isProductPage(Document doc) {
+		if (doc.select(".product-view").first() != null) {
+			return true;
+		}
+		return false;
+	}
+
+	private String crawlInternalId(Document doc) {
+		String internalId = null;
+
+		Element internalIdElement = doc.select("input[name=product]").first();
+		if (internalIdElement != null) {
+			internalId = internalIdElement.val();
+		}
+
+		return internalId;
+	}
+	
+	private String crawlInternalPid(Document doc) {
+		String internalPid = null;
+		Element pdi = doc.select("#product-attribute-specs-table td.data").first();
+		
+		if(pdi != null) {
+			internalPid = pdi.ownText().trim();
+		}
+		
+		return internalPid;
+	}
+
+	private String crawlName(Document document) {
+		String name = null;
+		Element nameElement = document.select(".product-name h1").first();
+
+		if (nameElement != null) {
+			name = nameElement.ownText().trim();
+		}
+
+		return name;
+	}
+
+	private Float crawlPrice(Document document, String internalId) {
+		Float price = null;
+
+		String priceText = null;
+		Element salePriceElement = document.select("#product-price-" + internalId).first();		
+
+		if (salePriceElement != null) {
+			priceText = salePriceElement.text();
+			price = MathCommonsMethods.parseFloat(priceText);
+		}
+
+		return price;
+	}
+
+	private Marketplace crawlMarketplace() {
+		return new Marketplace();
+	}
+
+
+	private String crawlPrimaryImage(Document doc) {
+		String primaryImage = null;
+		Element elementPrimaryImage = doc.select(".product-img-box >a").first();
+		
+		if(elementPrimaryImage != null ) {
+			primaryImage = elementPrimaryImage.attr("href");
+		} 
+		
+		return primaryImage;
+	}
+
+	/**
+	 * In the time when this crawler was made, this market hasn't secondary Images
+	 * @param doc
+	 * @return
+	 */
+	private String crawlSecondaryImages(Document doc) {
+		String secondaryImages = null;
+		JSONArray secondaryImagesArray = new JSONArray();
+
+		Elements images = doc.select(".more-views-list > li:not([class=active]) a");
+		
+		for(Element e : images) {
+			secondaryImagesArray.put(e.attr("href"));
+		}
+		
+		if (secondaryImagesArray.length() > 0) {
+			secondaryImages = secondaryImagesArray.toString();
+		}
+
+		return secondaryImages;
+	}
+
+	/**
+	 * No momento que o crawler foi feito não foi achado
+	 * produtos com categorias
+	 * @param document
+	 * @return
+	 */
+	private CategoryCollection crawlCategories(Document document) {
+		CategoryCollection categories = new CategoryCollection();
+		Elements elementCategories = document.select(".breadcrumbs li[class^=category] a");
+		
+		for (int i = 0; i < elementCategories.size(); i++) { 
+			String cat = elementCategories.get(i).ownText().trim();
+			
+			if(!cat.isEmpty()) {
+				categories.add( cat );
+			}
+		}
+
+		return categories;
+	}
+
+	private String crawlDescription(Document doc) {
+		StringBuilder description = new StringBuilder();
+		
+		Element elementDescription = doc.select(".box-collateral.box-description").first();
+		
+		if (elementDescription != null) {
+			description.append(elementDescription.html());		
+		}
+		
+		Element elementAditional = doc.select(".box-collateral.box-additional").first();
+		
+		if (elementAditional != null) {
+			description.append(elementAditional.html());		
+		}
+		
+		return description.toString();
+	}
+	
+	private boolean crawlAvailability(Document doc) {
+		return doc.select("div.add-to-box .btn-cart").first() != null;		
+	}
+
+	/**
+	 * 
+	 * @param doc
+	 * @param price
+	 * @return
+	 */
+	private Prices crawlPrices(Float price, Document doc) {
+		Prices prices = new Prices();
+		
+		if (price != null) {
+			Map<Integer,Float> installmentPriceMap = new TreeMap<>();
+			installmentPriceMap.put(1, price);
+			prices.setBankTicketPrice(price);
+
+			Elements installmentsElement = doc.select("#parcelamento table tr");
+			
+			for(Element e : installmentsElement) {
+				Elements values = e.select("td");
+				
+				if(values.size() > 1) {
+					String textInstallment = values.get(0).ownText().replaceAll("[^0-9]", "").trim();
+					Integer installment = !textInstallment.isEmpty() ? Integer.parseInt(textInstallment) : 1;
+					
+					String textValue = values.get(1).ownText().replaceAll("[^0-9,]", "").replaceAll(",", ".").trim();
+					Float value = !textValue.isEmpty() ? Float.parseFloat(textValue) : null;
+					
+					if(value != null) {
+						installmentPriceMap.put(installment, value);
+					}
+				}
+			}
+			
+			prices.insertCardInstallment(Card.VISA.toString(), installmentPriceMap);
+			prices.insertCardInstallment(Card.MASTERCARD.toString(), installmentPriceMap);
+			prices.insertCardInstallment(Card.AMEX.toString(), installmentPriceMap);
+			prices.insertCardInstallment(Card.HIPERCARD.toString(), installmentPriceMap);
+		}
+		
+		return prices;
+	}
+
+}
---
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilDrogafujiCrawler.java
@@ -0,0 +1,117 @@
+package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
+
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+
+public class BrasilDrogafujiCrawler extends CrawlerRankingKeywords{
+
+	public BrasilDrogafujiCrawler(Session session) {
+		super(session);
+	}
+
+	@Override
+	protected void extractProductsFromCurrentPage() {
+		//número de produtos por página do market
+		this.pageSize = 24;
+	
+		this.log("Página "+ this.currentPage);
+		
+		//monta a url com a keyword e a página
+		String url = "https://www.drogafuji.com.br/catalogsearch/result/index/?p="+ this.currentPage +"&q=" + this.keywordEncoded;
+		this.log("Link onde são feitos os crawlers: "+url);	
+		
+		//chama função de pegar o html
+		this.currentDoc = fetchDocument(url);
+
+		Elements products =  this.currentDoc.select(".products-grid .item");
+		
+		//se obter 1 ou mais links de produtos e essa página tiver resultado faça:
+		if(products.size() >= 1) {
+			if(totalProducts == 0) {
+				setTotalProducts();
+			}
+			
+			for(Element e : products) {
+				// InternalPid
+				String internalPid = null;
+				
+				// InternalId
+				String internalId = crawlInternalId(e);
+				
+				// Url do produto
+				String productUrl = crawlProductUrl(e);
+				
+				saveDataProduct(internalId, internalPid, productUrl);
+				
+				this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + internalPid + " - Url: " + productUrl);
+				if(this.arrayProducts.size() == productsLimit) {
+					break;
+				}
+				
+			}
+		} else {
+			this.result = false;
+			this.log("Keyword sem resultado!");
+		}
+		
+		this.log("Finalizando Crawler de produtos da página "+this.currentPage+" - até agora "+this.arrayProducts.size()+" produtos crawleados");
+	}
+
+	@Override
+	protected boolean hasNextPage() {
+		if(this.arrayProducts.size() < this.totalProducts) {
+			return true;
+		}
+			
+		return false;
+	}
+	
+	@Override
+	protected void setTotalProducts() {
+		Element total = this.currentDoc.select(".amount").first();
+		
+		if(total != null) {
+			String totalText = total.ownText().trim().split(" ")[0].replaceAll("[^0-9]", "");
+			
+			if(!totalText.isEmpty()) {
+				this.totalProducts = Integer.parseInt(totalText);
+			}
+		}
+		
+		this.log("Total products: " + this.totalProducts);
+	}
+	
+	private String crawlInternalId(Element e){
+		String internalId = null;
+		Element idElement = e.select("span[id^=product-price-]").first();
+		
+		if(idElement != null) {
+			String[] tokens = idElement.attr("id").split("-");
+			String id = tokens[tokens.length-1].replaceAll("[^0-9]", "").trim();
+			
+			if(!id.isEmpty()) {
+				internalId = id;
+			}
+		}
+		
+		return internalId;
+	}
+	
+	private String crawlProductUrl(Element e){
+		String productUrl = null;
+		Element url = e.select("> a").first();
+		
+		if(url != null) {
+			productUrl = url.attr("href");
+			
+			if(!productUrl.startsWith("https://www.drogafuji.com.br/")) {
+				productUrl = "https://www.drogafuji.com.br/" + productUrl;
+			}
+		}
+		
+		return productUrl;
+	}
+}
