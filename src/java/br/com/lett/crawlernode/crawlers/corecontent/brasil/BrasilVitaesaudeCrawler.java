c9e255d2d692a9f800ea6b9c767951c7e80a0e43
gabriel <gabrieldornelas@lett.com.br>
2017-08-09 17:49:13 -0300

    Finalizei ranking vitae, mas o crawler products não esta ok
    Former-commit-id: 80e90bc4e1e6dc5df02e12c5b288425241a01e1e

---
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilVitaesaudeCrawler.java
@@ -0,0 +1,428 @@
+package br.com.lett.crawlernode.crawlers.corecontent.brasil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import br.com.lett.crawlernode.core.fetcher.DataFetcher;
+import br.com.lett.crawlernode.core.models.Card;
+import br.com.lett.crawlernode.core.models.CategoryCollection;
+import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.models.ProductBuilder;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.Crawler;
+import br.com.lett.crawlernode.util.Logging;
+import br.com.lett.crawlernode.util.MathCommonsMethods;
+import models.Marketplace;
+import models.prices.Prices;
+
+/**
+ * Date: 09/08/2017
+ * 
+ * @author Gabriel Dornelas
+ *
+ */
+public class BrasilVitaesaudeCrawler extends Crawler {
+
+	private final String HOME_PAGE = "http://www.vitaesaude.com.br/";
+
+	public BrasilVitaesaudeCrawler(Session session) {
+		super(session);
+	}
+
+	@Override
+	public boolean shouldVisit() {
+		String href = session.getOriginalURL().toLowerCase();
+		return !FILTERS.matcher(href).matches() && (href.startsWith(HOME_PAGE));
+	}
+
+	@Override
+	public List<Product> extractInformation(Document doc) throws Exception {
+		super.extractInformation(doc);
+		List<Product> products = new ArrayList<>();
+
+		if ( isProductPage(doc) ) {
+			Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
+			
+			String internalPid = crawlInternalPid(doc);
+			String name = crawlName(doc);
+			CategoryCollection categories = crawlCategories(doc);
+			String primaryImage = crawlPrimaryImage(doc);
+			String secondaryImages = crawlSecondaryImages(doc);
+			String description = crawlDescription(doc);
+			Integer stock = null;
+			Marketplace marketplace = crawlMarketplace();
+
+			Elements variations = doc.select(".ProductOptionList li label");
+			
+			if(variations.size() > 1) {
+				for(Element e : variations) {
+					//Id variation
+					String variationId = e.select("input").val();
+					
+					// Variation info
+					JSONObject variationInfo = crawlVariationsInfo(internalPid, variationId);
+					
+					String internalId = crawlVariationInternalId(variationInfo);
+					String variationName = name + " " + e.ownText().trim();
+					Float price = crawlVariationPrice(variationInfo);
+					Prices prices = crawlPrices(price, variationInfo);
+					boolean available = crawlAvailability(variationInfo);
+					
+					// Creating the product
+					Product product = ProductBuilder.create()
+							.setUrl(session.getOriginalURL())
+							.setInternalId(internalPid + "-" + internalId)
+							.setInternalPid(internalPid)
+							.setName(variationName)
+							.setPrice(price)
+							.setPrices(prices)
+							.setAvailable(available)
+							.setCategory1(categories.getCategory(0))
+							.setCategory2(categories.getCategory(1))
+							.setCategory3(categories.getCategory(2))
+							.setPrimaryImage(primaryImage)
+							.setSecondaryImages(secondaryImages)
+							.setDescription(description)
+							.setStock(stock)
+							.setMarketplace(marketplace)
+							.build();
+
+					products.add(product);
+				} 
+			} else {
+				name += variations.size() == 1 ? " " + variations.get(0).ownText().trim() : "";
+				
+				String internalId = crawlInternalId(doc);
+				Float price = crawlPrice(doc);
+				Prices prices = crawlPrices(price, doc);
+				boolean available = crawlAvailability(doc);
+				
+				// Creating the product
+				Product product = ProductBuilder.create()
+						.setUrl(session.getOriginalURL())
+						.setInternalId(internalPid + "-" + internalId)
+						.setInternalPid(internalPid)
+						.setName(name)
+						.setPrice(price)
+						.setPrices(prices)
+						.setAvailable(available)
+						.setCategory1(categories.getCategory(0))
+						.setCategory2(categories.getCategory(1))
+						.setCategory3(categories.getCategory(2))
+						.setPrimaryImage(primaryImage)
+						.setSecondaryImages(secondaryImages)
+						.setDescription(description)
+						.setStock(stock)
+						.setMarketplace(marketplace)
+						.build();
+
+				products.add(product);
+			}
+			
+		} else {
+			Logging.printLogDebug(logger, session, "Not a product page" + this.session.getOriginalURL());
+		}
+
+		return products;
+
+	}
+
+	private boolean isProductPage(Document doc) {
+		if (doc.select("input[name=product_id]").first() != null) {
+			return true;
+		}
+		return false;
+	}
+
+	private String crawlInternalId(Document doc) {
+		String internalId = null;
+
+		Element internalIdElement = doc.select(".VariationProductSKU").first();
+		if (internalIdElement != null) {
+			internalId = internalIdElement.ownText().trim();
+		}
+		
+		if(internalId == null || internalId.isEmpty()) {
+			internalIdElement = doc.select(".CartVariationId").first();
+			
+			if (internalIdElement != null) {
+				internalId = internalIdElement.val();
+			}
+		}
+
+		return internalId;
+	}
+	
+	private String crawlVariationInternalId(JSONObject variationInfo) {
+		String internalId = null;
+		
+		if(variationInfo.has("sku")) {
+			internalId = variationInfo.getString("sku").trim();
+		}
+		
+		return internalId;
+	}
+	
+	private String crawlInternalPid(Document doc) {
+		String internalPid = null;
+		Element pdi = doc.select("input[name=product_id]").first();
+		
+		if(pdi != null) {
+			internalPid = pdi.val();
+		}
+		
+		return internalPid;
+	}
+
+	private String crawlName(Document document) {
+		String name = null;
+		Element nameElement = document.select("h1.ProdName").first();
+
+		if (nameElement != null) {
+			name = nameElement.ownText().trim();
+		}
+
+		return name;
+	}
+
+	private Float crawlPrice(Document document) {
+		Float price = null;
+
+		String priceText = null;
+		Element salePriceElement = document.select(".VariationProductDiscount").first();		
+
+		if (salePriceElement != null) {
+			priceText = salePriceElement.text();
+			price = MathCommonsMethods.parseFloat(priceText);
+		}
+		
+		// Casos que não tem preço a vista em destaque
+		if(price == null) {
+			Element priceOriginal = document.select("meta[itemprop=price]").first();
+			
+			if(priceOriginal != null) {
+				price = Float.parseFloat(priceOriginal.attr("content"));
+			}
+		}
+
+		return price;
+	}
+	
+	private Float crawlVariationPrice(JSONObject variationInfo) {
+		Float price = null;
+		
+		if(variationInfo.has("desconto")) {
+			String desconto = variationInfo.get("desconto").toString().replaceAll("[^0-9,]", "").replace(",", ".").trim();
+			
+			if(!desconto.isEmpty()) {
+				price = Float.parseFloat(desconto);
+			}
+		}
+		
+		if(price == null && variationInfo.has("unformattedPrice")) {
+			Double pDouble = variationInfo.getDouble("unformattedPrice");
+			price = MathCommonsMethods.normalizeTwoDecimalPlaces(pDouble.floatValue());
+		}
+		
+		return price;
+	}
+
+	private Marketplace crawlMarketplace() {
+		return new Marketplace();
+	}
+
+
+	private String crawlPrimaryImage(Document doc) {
+		String primaryImage = null;
+		Element elementPrimaryImage = doc.select(".ProductThumbImage > a").first();
+		
+		if(elementPrimaryImage != null ) {
+			primaryImage = elementPrimaryImage.attr("href");
+		} 
+		
+		return primaryImage;
+	}
+
+	/**
+	 * @param doc
+	 * @return
+	 */
+	private String crawlSecondaryImages(Document doc) {
+		String secondaryImages = null;
+		JSONArray secondaryImagesArray = new JSONArray();
+
+		Elements images = doc.select(".ProductTinyImageList li");
+		
+		if(images.size() > 1) {
+			Elements scripts = doc.select("script[type=text/javascript]");
+			
+			for(Element e : scripts) {
+				String script = e.outerHtml();
+				
+				if(script.contains("ThumbURLs")) {
+					String[] tokens = script.split(";");
+					
+					for(String token : tokens) {
+						if(token.trim().contains("ThumbURLs[") && !token.contains("ThumbURLs[0]")) {
+							secondaryImagesArray.put(token.split("=")[1].trim().replace("//", "/").replace("\"", ""));
+						}
+					}
+						
+					break;
+				}
+			}
+		}
+		
+		for(Element e : images) {
+			secondaryImagesArray.put(e.attr("href"));
+		}
+		
+		if (secondaryImagesArray.length() > 0) {
+			secondaryImages = secondaryImagesArray.toString();
+		}
+
+		return secondaryImages;
+	}
+
+	/**
+	 * @param document
+	 * @return
+	 */
+	private CategoryCollection crawlCategories(Document document) {
+		CategoryCollection categories = new CategoryCollection();
+		Elements elementCategories = document.select(".Breadcrumb ul li:not([class=itemCurrent]) a");
+		
+		for (int i = 1; i < elementCategories.size(); i++) { // primeiro item é a home
+			String cat = elementCategories.get(i).ownText().trim();
+			
+			if(!cat.isEmpty()) {
+				categories.add( cat );
+			}
+		}
+
+		return categories;
+	}
+
+	private String crawlDescription(Document doc) {
+		StringBuilder description = new StringBuilder();
+		
+		Element elementDescription = doc.select("#ProductDescription").first();
+		
+		if (elementDescription != null) {
+			description.append(elementDescription.html());		
+		}
+		
+		return description.toString();
+	}
+	
+	private boolean crawlAvailability(Document doc) {
+		return doc.select(".avisemeContent[style=\"display:none;\"]").first() != null;		
+	}
+	
+	private boolean crawlAvailability(JSONObject variationInfo) {
+		return variationInfo.has("instock") && variationInfo.getBoolean("instock");		
+	}
+
+	/**
+	 * 
+	 * @param doc
+	 * @param price
+	 * @return
+	 */
+	private Prices crawlPrices(Float price, Document doc) {
+		Prices prices = new Prices();
+		
+		if (price != null) {
+			Map<Integer,Float> installmentPriceMap = new TreeMap<>();
+			installmentPriceMap.put(1, price);
+			prices.setBankTicketPrice(price);
+
+			Element installmentsElement = doc.select(".ProdParcelas strong").first();
+			
+			if(installmentsElement != null) {
+				String installmentText = installmentsElement.ownText().replaceAll("[^0-9]", "").trim();
+				
+				if(!installmentText.isEmpty()) {
+					Integer installment = Integer.parseInt(installmentText);
+					Element valueElement = installmentsElement.select(".ValorProduto").first();
+					
+					if(valueElement != null) {
+						Float value = MathCommonsMethods.parseFloat(valueElement.ownText());
+						
+						if(value != null) {
+							installmentPriceMap.put(installment, value);
+						}
+					}
+				}
+			}
+			
+			prices.insertCardInstallment(Card.VISA.toString(), installmentPriceMap);
+			prices.insertCardInstallment(Card.MASTERCARD.toString(), installmentPriceMap);
+			prices.insertCardInstallment(Card.DINERS.toString(), installmentPriceMap);
+			prices.insertCardInstallment(Card.ELO.toString(), installmentPriceMap);
+		}
+		
+		return prices;
+	}
+	
+	/**
+	 * 
+	 * @param doc
+	 * @param price
+	 * @return
+	 */
+	private Prices crawlPrices(Float price, JSONObject variationInfo) {
+		Prices prices = new Prices();
+		
+		if (price != null) {
+			Map<Integer,Float> installmentPriceMap = new TreeMap<>();
+			installmentPriceMap.put(1, price);
+			prices.setBankTicketPrice(price);
+
+			if(variationInfo.has("parcelas")) {
+				Document doc = Jsoup.parse(variationInfo.getString("parcelas"));
+				
+				Element installmentsElement = doc.select(".ProdParcelas strong").first();
+				
+				if(installmentsElement != null) {
+					String installmentText = installmentsElement.ownText().replaceAll("[^0-9]", "").trim();
+					
+					if(!installmentText.isEmpty()) {
+						Integer installment = Integer.parseInt(installmentText);
+						Element valueElement = installmentsElement.select(".ValorProduto").first();
+						
+						if(valueElement != null) {
+							Float value = MathCommonsMethods.parseFloat(valueElement.ownText());
+							
+							if(value != null) {
+								installmentPriceMap.put(installment, value);
+							}
+						}
+					}
+				}
+				
+				prices.insertCardInstallment(Card.VISA.toString(), installmentPriceMap);
+				prices.insertCardInstallment(Card.MASTERCARD.toString(), installmentPriceMap);
+				prices.insertCardInstallment(Card.DINERS.toString(), installmentPriceMap);
+				prices.insertCardInstallment(Card.ELO.toString(), installmentPriceMap);
+			}
+		}
+			
+		
+		return prices;
+	}
+
+	private JSONObject crawlVariationsInfo(String pid, String variationId) {
+		String url = "http://www.vitaesaude.com.br/remote.php?w=GetVariationOptions&productId=" + pid + "&options=" + variationId;
+		return DataFetcher.fetchJSONObject(DataFetcher.GET_REQUEST, session, url, null, cookies);
+	}
+}
---
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilVitaesaudeCrawler.java
@@ -0,0 +1,109 @@
+package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
+
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+
+public class BrasilVitaesaudeCrawler extends CrawlerRankingKeywords{
+
+	public BrasilVitaesaudeCrawler(Session session) {
+		super(session);
+	}
+
+	@Override
+	protected void extractProductsFromCurrentPage() {
+		//número de produtos por página do market
+		this.pageSize = 9;
+	
+		this.log("Página "+ this.currentPage);
+		
+		//monta a url com a keyword e a página
+		String url = "http://www.vitaesaude.com.br/search?search_query="+ this.keywordEncoded +"&section=product&page="+ this.currentPage +"&ajax=1";
+		this.log("Link onde são feitos os crawlers: "+url);	
+		
+		//chama função de pegar o html
+		this.currentDoc = fetchDocument(url);
+
+		Elements products =  this.currentDoc.select(".ProductList li ");
+		
+		//se obter 1 ou mais links de produtos e essa página tiver resultado faça:
+		if(products.size() >= 1) {			
+			for(Element e : products) {
+				// InternalPid
+				String internalPid = crawlInternalPid(e);
+				
+				// InternalId
+				String internalId = null;
+				
+				// Url do produto
+				String productUrl = crawlProductUrl(e);
+				
+				saveDataProduct(internalId, internalPid, productUrl);
+				
+				this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + internalPid + " - Url: " + productUrl);
+				if(this.arrayProducts.size() == productsLimit) {
+					break;
+				}
+				
+			}
+		} else {
+			setTotalProducts();
+			this.result = false;
+			this.log("Keyword sem resultado!");
+		}
+		
+		if(!hasNextPage()) {
+			setTotalProducts();
+		}
+		
+		this.log("Finalizando Crawler de produtos da página "+this.currentPage+" - até agora "+this.arrayProducts.size()+" produtos crawleados");
+	}
+
+	@Override
+	protected boolean hasNextPage() {
+		Element lastPageElement = this.currentDoc.select(".PagingList > li ").last();
+		
+		if(lastPageElement != null) {
+			String text = lastPageElement.text().replaceAll("[^0-9]", "");
+			
+			if(!text.isEmpty() && (this.currentPage < Integer.parseInt(text))) {
+				return true;
+			}
+		}
+			
+		return false;
+	}
+	
+	private String crawlInternalPid(Element e){
+		String internalPid = null;
+		Element idElement = e.select(".ProductActionAdd a").first();
+		
+		if(idElement != null) {
+			String[] tokens = idElement.attr("href").split("=");
+			String id = tokens[tokens.length-1].replaceAll("[^0-9]", "").trim();
+			
+			if(!id.isEmpty()) {
+				internalPid = id;
+			}
+		}
+		
+		return internalPid;
+	}
+	
+	private String crawlProductUrl(Element e){
+		String productUrl = null;
+		Element url = e.select(".prod_nome > a").first();
+		
+		if(url != null) {
+			productUrl = url.attr("href");
+			
+			if(!productUrl.startsWith("http://www.vitaesaude.com.br/")) {
+				productUrl = "http://www.vitaesaude.com.br/" + productUrl;
+			}
+		}
+		
+		return productUrl;
+	}
+}
