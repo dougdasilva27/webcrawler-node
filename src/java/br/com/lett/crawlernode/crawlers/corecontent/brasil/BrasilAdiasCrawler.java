401fb96fdd0196080c6f3ee51d8cb557d9dea56a
gabriel <gabrieldornelas@lett.com.br>
2017-08-14 13:08:37 -0300

    Corrigi Home page Adias. [deploy:Production]
    Former-commit-id: 06ab49759f02916f129bf83e1332d2542fb47610

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilAdiasCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilAdiasCrawler.java
@@ -22,37 +22,41 @@ import models.prices.Prices;
 /************************************************************************************************************************************************************************************
  * Crawling notes (08/07/2016):
  * 
- * 1) For this crawler, we have one url per each sku. There is no page is more than one sku in it.
- *  
- * 2) There is no stock information for skus in this ecommerce by the time this crawler was made.
+ * 1) For this crawler, we have one url per each sku. There is no page is more
+ * than one sku in it.
  * 
- * 3) There is no marketplace in this ecommerce by the time this crawler was made.
+ * 2) There is no stock information for skus in this ecommerce by the time this
+ * crawler was made.
+ * 
+ * 3) There is no marketplace in this ecommerce by the time this crawler was
+ * made.
  * 
  * 4) The sku page identification is done simply looking the URL format.
  * 
  * 5) Even if a product is unavailable, its price is displayed.
  * 
- * 6) There is no internalPid for skus in this ecommerce. The internalPid must be a number that is the same for all
- * the variations of a given sku.
+ * 6) There is no internalPid for skus in this ecommerce. The internalPid must
+ * be a number that is the same for all the variations of a given sku.
  * 
- * 7) We have one method for each type of information for a sku (please carry on with this pattern).
+ * 7) We have one method for each type of information for a sku (please carry on
+ * with this pattern).
  * 
- * Examples:
- * ex1 (available): http://www.adias.com.br/produto/ar-condicionado-split-janela-9000-btus-elgin-compact-110v-frio-hcfi09a1na-68152
- * ex2 (unavailable): http://www.adias.com.br/produto/ar-condicionado-split-janela-7000-btus-elgin-compact-220v-frio-sqfic-7000-2-68221 
+ * Examples: ex1 (available):
+ * http://www.adias.com.br/produto/ar-condicionado-split-janela-9000-btus-elgin-compact-110v-frio-hcfi09a1na-68152
+ * ex2 (unavailable):
+ * http://www.adias.com.br/produto/ar-condicionado-split-janela-7000-btus-elgin-compact-220v-frio-sqfic-7000-2-68221
  *
- * Optimizations notes:
- * No optimizations
+ * Optimizations notes: No optimizations
  *
  ************************************************************************************************************************************************************************************/
 
 public class BrasilAdiasCrawler extends Crawler {
-	
+
 	private final String HOME_PAGE = "https://www.adias.com.br/";
 
 	public BrasilAdiasCrawler(Session session) {
 		super(session);
-	}	
+	}
 
 	@Override
 	public boolean shouldVisit() {
@@ -65,7 +69,7 @@ public class BrasilAdiasCrawler extends Crawler {
 		super.extractInformation(doc);
 		List<Product> products = new ArrayList<>();
 
-		if ( isProductPage(this.session.getOriginalURL()) ) {
+		if (isProductPage(this.session.getOriginalURL())) {
 			Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
 
 			// InternalId
@@ -79,10 +83,10 @@ public class BrasilAdiasCrawler extends Crawler {
 
 			// Price
 			Float price = crawlMainPagePrice(doc);
-			
+
 			// Price options
 			Prices prices = crawlPrices(doc, internalId);
-			
+
 			// Availability
 			boolean available = crawlAvailability(doc);
 
@@ -133,32 +137,31 @@ public class BrasilAdiasCrawler extends Crawler {
 		} else {
 			Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
 		}
-		
+
 		return products;
 	}
-
-
 
 	/*******************************
 	 * Product page identification *
 	 *******************************/
 
 	private boolean isProductPage(String url) {
-		if ( url.startsWith(HOME_PAGE + "produto/") ) return true;
+		if (url.startsWith(HOME_PAGE + "produto/")) {
+			return true;
+		}
 		return false;
 	}
-	
-	
+
 	/*******************
 	 * General methods *
 	 *******************/
-	
+
 	private String crawlInternalId(Document document) {
 		String internalId = null;
 		Element internalIdElement = document.select("#hdnProdutoId").first();
 
 		if (internalIdElement != null) {
-			internalId = internalIdElement.attr("value").toString().trim();			
+			internalId = internalIdElement.attr("value").toString().trim();
 		}
 
 		return internalId;
@@ -169,7 +172,7 @@ public class BrasilAdiasCrawler extends Crawler {
 
 		return internalPid;
 	}
-	
+
 	private String crawlName(Document document) {
 		String name = null;
 		Element nameElement = document.select(".fbits-produto-nome.prodTitle.title").first();
@@ -186,51 +189,55 @@ public class BrasilAdiasCrawler extends Crawler {
 		Element mainPagePriceElement = document.select("#fbits-forma-pagamento .precoPor").first();
 
 		if (mainPagePriceElement != null) {
-			price = Float.parseFloat( mainPagePriceElement.text().toString().replaceAll("[^0-9,]+", "").replaceAll("\\.", "").replaceAll(",", ".") );
+			price = Float.parseFloat(mainPagePriceElement.text().toString().replaceAll("[^0-9,]+", "")
+					.replaceAll("\\.", "").replaceAll(",", "."));
 		}
 
 		return price;
 	}
-	
+
 	private Prices crawlPrices(Document doc, String internalId) {
 		Prices prices = new Prices();
-		
+
 		// crawl the bank ticket price
 		Float bankTicketPrice = null;
-		Element bankTicketPriceElement = doc.select("#divFormaPagamento .precoVista .fbits-boleto-preco").first();		
+		Element bankTicketPriceElement = doc.select("#divFormaPagamento .precoVista .fbits-boleto-preco").first();
 		if (bankTicketPriceElement != null) {
-			bankTicketPrice = Float.parseFloat( bankTicketPriceElement.text().trim().replaceAll("[^0-9,]+", "").replaceAll("\\.", "").replaceAll(",", ".") );
+			bankTicketPrice = Float.parseFloat(bankTicketPriceElement.text().trim().replaceAll("[^0-9,]+", "")
+					.replaceAll("\\.", "").replaceAll(",", "."));
 		}
-		
+
 		// crawl the card payment options
 		Map<Integer, Float> installments = new TreeMap<Integer, Float>();
-		Elements installmentsElements = doc.select("#produto-pagamentoparcelamento-" + internalId + " .details-content p");
+		Elements installmentsElements = doc
+				.select("#produto-pagamentoparcelamento-" + internalId + " .details-content p");
 		if (installmentsElements.size() > 0) {
 			for (Element installmentElement : installmentsElements) {
 				Element installmentNumberElement = installmentElement.select("b").first();
 				Element installmentValueElement = installmentElement.select("b").last();
 				Integer installmentNumber = null;
 				Float installmentValue = null;
-								
+
 				if (installmentNumberElement != null) {
 					installmentNumber = Integer.parseInt(installmentNumberElement.text().trim());
 				}
 				if (installmentValueElement != null) {
-					installmentValue = Float.parseFloat( installmentValueElement.text().trim().replaceAll("[^0-9,]+", "").replaceAll("\\.", "").replaceAll(",", ".") );
+					installmentValue = Float.parseFloat(installmentValueElement.text().trim().replaceAll("[^0-9,]+", "")
+							.replaceAll("\\.", "").replaceAll(",", "."));
 				}
-				
+
 				// insert the installment on the map
 				if (installmentNumber != null && installmentValue != null) {
 					installments.put(installmentNumber, installmentValue);
 				}
 			}
 		}
-		
+
 		// insert the payment options
-		
+
 		// bank ticket
 		prices.setBankTicketPrice(bankTicketPrice);
-		
+
 		// all card payment options are the same for all card brands
 		// the available card brands are displayed once we hit the buy button
 		prices.insertCardInstallment(Card.VISA.toString(), installments);
@@ -240,24 +247,25 @@ public class BrasilAdiasCrawler extends Crawler {
 		prices.insertCardInstallment(Card.DISCOVER.toString(), installments);
 		prices.insertCardInstallment(Card.BNDES.toString(), installments);
 		prices.insertCardInstallment(Card.AMEX.toString(), installments);
-		
+
 		return prices;
 	}
-	
+
 	private boolean crawlAvailability(Document document) {
 		Element notifyMeElement = document.select(".avisoIndisponivel").first();
-		
+
 		if (notifyMeElement != null) {
-			if (notifyMeElement.attr("style").equals("display:none;")) return true;
+			if (notifyMeElement.attr("style").equals("display:none;"))
+				return true;
 		}
-		
+
 		return false;
 	}
 
 	private Map<String, Float> crawlMarketplace(Document document) {
 		return new HashMap<String, Float>();
 	}
-	
+
 	private Marketplace assembleMarketplaceFromMap(Map<String, Float> marketplaceMap) {
 		return new Marketplace();
 	}
@@ -277,10 +285,14 @@ public class BrasilAdiasCrawler extends Crawler {
 		String secondaryImages = null;
 		JSONArray secondaryImagesArray = new JSONArray();
 
-		Elements imagesElement = document.select(".fbits-componente-imagemvariantethumb.orientacao-vertical.fbits-produto-imagens .jcarousel ul li a");
+		Elements imagesElement = document.select(
+				".fbits-componente-imagemvariantethumb.orientacao-vertical.fbits-produto-imagens .jcarousel ul li a");
 
-		for (int i = 1; i < imagesElement.size(); i++) { // starting from index 1, because the first is the primary image
-			secondaryImagesArray.put( imagesElement.get(i).attr("data-zoom-image").trim() );
+		for (int i = 1; i < imagesElement.size(); i++) { // starting from index
+															// 1, because the
+															// first is the
+															// primary image
+			secondaryImagesArray.put(imagesElement.get(i).attr("data-zoom-image").trim());
 		}
 
 		if (secondaryImagesArray.length() > 0) {
@@ -294,8 +306,12 @@ public class BrasilAdiasCrawler extends Crawler {
 		ArrayList<String> categories = new ArrayList<String>();
 		Elements elementCategories = document.select("#fbits-breadcrumb ol li a span");
 
-		for (int i = 1; i < elementCategories.size(); i++) { // starting from index 1, because the first is the market name
-			categories.add( elementCategories.get(i).text().trim() );
+		for (int i = 1; i < elementCategories.size(); i++) { // starting from
+																// index 1,
+																// because the
+																// first is the
+																// market name
+			categories.add(elementCategories.get(i).text().trim());
 		}
 
 		return categories;
@@ -314,8 +330,10 @@ public class BrasilAdiasCrawler extends Crawler {
 		Element descriptionElement = document.select(".informacao-abas #conteudo-0").first();
 		Element specElement = document.select(".informacao-abas #conteudo-1").first();
 
-		if (descriptionElement != null) description = description + descriptionElement.html();
-		if (specElement != null) description = description + specElement.html();
+		if (descriptionElement != null)
+			description = description + descriptionElement.html();
+		if (specElement != null)
+			description = description + specElement.html();
 
 		return description;
 	}
--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilAdiasCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilAdiasCrawler.java
@@ -6,7 +6,7 @@ import org.jsoup.select.Elements;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
 
-public class BrasilAdiasCrawler extends CrawlerRankingKeywords{
+public class BrasilAdiasCrawler extends CrawlerRankingKeywords {
 
 	public BrasilAdiasCrawler(Session session) {
 		super(session);
@@ -14,85 +14,87 @@ public class BrasilAdiasCrawler extends CrawlerRankingKeywords{
 
 	@Override
 	protected void extractProductsFromCurrentPage() {
-		//número de produtos por página do market
+		// número de produtos por página do market
 		this.pageSize = 24;
-			
-		this.log("Página "+ this.currentPage);
-		
-		//monta a url com a keyword e a página
-		String url = "http://www.adias.com.br/busca?busca=" + this.keywordEncoded + "&pagina="+this.currentPage;
-		this.log("Link onde são feitos os crawlers: "+url);	
-			
-		//chama função de pegar a url
+
+		this.log("Página " + this.currentPage);
+
+		// monta a url com a keyword e a página
+		String url = "https://www.adias.com.br/busca?busca=" + this.keywordEncoded + "&pagina=" + this.currentPage;
+		this.log("Link onde são feitos os crawlers: " + url);
+
+		// chama função de pegar a url
 		this.currentDoc = fetchDocument(url);
-		
-		Elements id =  this.currentDoc.select("div.spot");
-		
-		int count=0;
-		
-		
-		//se obter 1 ou mais links de produtos e essa página tiver resultado faça:
-		if(id.size() >= 1)
-		{			
-			//se o total de busca não foi setado ainda, chama a função para setar
-			if(this.totalProducts == 0) setTotalProducts();
-			
-			for(Element e: id)
-			{
+
+		Elements id = this.currentDoc.select("div.spot");
+
+		int count = 0;
+
+		// se obter 1 ou mais links de produtos e essa página tiver resultado
+		// faça:
+		if (id.size() >= 1) {
+			// se o total de busca não foi setado ainda, chama a função para
+			// setar
+			if (this.totalProducts == 0) {
+				setTotalProducts();
+			}
+
+			for (Element e : id) {
 				count++;
-				//seta o id com o seletor
+				// seta o id com o seletor
 				String[] tokens = e.attr("id").split("-");
-				String internalPid 	= null;
-				String internalId 	= tokens[tokens.length-1];
-				
-				//monta a url
+				String internalPid = null;
+				String internalId = tokens[tokens.length - 1];
+
+				// monta a url
 				Element eUrl = e.select("div.spotContent > a").first();
 				String productUrl = eUrl.attr("href");
-				
-				if(!productUrl.contains("adias")) productUrl = "http://www.adias.com.br" + productUrl;
-				
+
+				if (!productUrl.contains("adias")) {
+					productUrl = ("https://www.adias.com.br/" + productUrl).replace("br//", "br/");
+				}
+
 				saveDataProduct(internalId, internalPid, productUrl);
 
-				this.log("InternalId do produto da "+count+" da página "+ this.currentPage+ ": " + internalId);
-				if(this.arrayProducts.size() == productsLimit) break;
+				this.log("InternalId do produto da " + count + " da página " + this.currentPage + ": " + internalId);
+				if (this.arrayProducts.size() == productsLimit) {
+					break;
+				}
 			}
-		}
-		else
-		{
+		} else {
 			this.result = false;
 			this.log("Keyword sem resultado!");
 		}
 
-		this.log("Finalizando Crawler de produtos da página "+this.currentPage+" - até agora "+this.arrayProducts.size()+" produtos crawleados");
+		this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora "
+				+ this.arrayProducts.size() + " produtos crawleados");
 	}
 
 	@Override
 	protected boolean hasNextPage() {
-		if(this.arrayProducts.size() < this.totalProducts) return true;
-		else 											return false;
+		if (this.arrayProducts.size() < this.totalProducts) {
+			return true;
+		}
+
+		return false;
 	}
-	
+
 	@Override
-	protected void setTotalProducts()
-	{
+	protected void setTotalProducts() {
 		Element totalElement = this.currentDoc.select("div.mostrando.left.fbits-info-bottom > span").first();
-		
-		if(totalElement != null)
-		{
-			try
-			{
+
+		if (totalElement != null) {
+			try {
 				int x = totalElement.text().indexOf("produto");
-				
+
 				String token = totalElement.text().substring(0, x).trim();
-				
+
 				this.totalProducts = Integer.parseInt(token);
-			}
-			catch(Exception e)
-			{
+			} catch (Exception e) {
 				this.logError(e.getMessage());
 			}
-			
-			this.log("Total da busca: "+this.totalProducts);
+
+			this.log("Total da busca: " + this.totalProducts);
 		}
 	}
 }
