72993b72456e3c89820177951bccd10d542cef16
gabriel <gabrieldornelas@lett.com.br>
2017-08-30 14:36:21 -0300

    Drogaria nissei rating
    Former-commit-id: bc799c215c4f3079cec2d5f24d96cc5c54791887

---
+++ src/java/br/com/lett/crawlernode/crawlers/ratingandreviews/brasil/BrasilDrogarianisseiRatingReviewCrawler.java
@@ -0,0 +1,113 @@
+package br.com.lett.crawlernode.crawlers.ratingandreviews.brasil;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import br.com.lett.crawlernode.core.models.RatingReviewsCollection;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.RatingReviewCrawler;
+import br.com.lett.crawlernode.util.Logging;
+import models.RatingsReviews;
+
+/**
+ * Date: 13/12/16
+ * @author gabriel
+ *
+ */
+public class BrasilDrogarianisseiRatingReviewCrawler extends RatingReviewCrawler {
+
+	public BrasilDrogarianisseiRatingReviewCrawler(Session session) {
+		super(session);
+	}
+
+	@Override
+	protected RatingReviewsCollection extractRatingAndReviews(Document document) throws Exception {
+		RatingReviewsCollection ratingReviewsCollection = new RatingReviewsCollection();
+
+		if (isProductPage(document)) {
+			Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
+
+			RatingsReviews ratingReviews = new RatingsReviews();			
+			ratingReviews.setDate(session.getDate());
+
+			String internalId = crawlInternalId(document);
+
+			Integer totalNumOfEvaluations = getTotalNumOfRatings(document);			
+			Double avgRating = getTotalAvgRating(document);
+
+			ratingReviews.setInternalId(internalId);
+			ratingReviews.setTotalRating(totalNumOfEvaluations);
+			ratingReviews.setTotalWrittenReviews(totalNumOfEvaluations);
+			ratingReviews.setAverageOverallRating(avgRating);
+
+			ratingReviewsCollection.addRatingReviews(ratingReviews);
+		}
+
+		return ratingReviewsCollection;
+
+	}
+
+	private String crawlInternalId(Document doc){
+		String internalId = null;
+		Element internalIdElement = doc.select("input[name=Id_Produto_SKU]").first();
+		
+		if (internalIdElement != null) {
+			internalId = internalIdElement.val();
+		} else {
+			internalIdElement = doc.select(".button-comprar[data-compra]").first();
+			
+			if(internalIdElement != null) {
+				internalId = internalIdElement.attr("data-compra");
+			}
+		}
+
+		return internalId;
+	}
+
+	/**
+	 * Avg appear in html element
+	 * 
+	 * @param document
+	 * @return
+	 */
+	private Double getTotalAvgRating(Document doc) {
+		Double avgRating = 0d;
+		Element reviews = doc.select(".rate-geral > .star-avaliacao").first();
+
+		if(reviews != null) {
+			String text = reviews.attr("rate-avaliacao").replaceAll("[^0-9.]", "").trim();
+			
+			if(!text.isEmpty()) {
+				avgRating = Double.parseDouble(text);
+			}
+		}
+
+		return avgRating;
+	}
+
+	/**
+	 * Number of ratings appear in html element 
+	 * @param doc
+	 * @return
+	 */
+	private Integer getTotalNumOfRatings(Document doc) {
+		Integer ratingNumber = 0;
+		Element reviews = doc.select(".rate-geral > small").first();
+
+		if(reviews != null) {
+			String text = reviews.ownText().replaceAll("[^0-9]", "").trim();
+			
+			if(!text.isEmpty()) {
+				ratingNumber = Integer.parseInt(text);
+			}
+		}
+		
+		return ratingNumber;
+	}
+
+
+	private boolean isProductPage(Document doc) {
+		return doc.select(".prod-index").first() != null;
+	}
+	
+}
