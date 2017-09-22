755fb56111b0186e64f642f8787934ea72621922
gabriel <gabrieldornelas@lett.com.br>
2017-09-22 11:38:59 -0300

    Corrigi crawler farmacity [deploy:Production]
    Former-commit-id: ef4babbb070e2221e79cfd36afa4ece4a7675916

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/argentina/ArgentinaFarmacityCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/argentina/ArgentinaFarmacityCrawler.java
@@ -1,13 +1,14 @@
 package br.com.lett.crawlernode.crawlers.corecontent.argentina;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
+import java.util.regex.Pattern;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
+import org.jsoup.nodes.DataNode;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -18,21 +19,19 @@ import br.com.lett.crawlernode.core.models.Product;
 import br.com.lett.crawlernode.core.models.ProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.Crawler;
-import br.com.lett.crawlernode.util.CommonMethods;
 import br.com.lett.crawlernode.util.Logging;
-import br.com.lett.crawlernode.util.MathCommonsMethods;
 import models.Marketplace;
 import models.prices.Prices;
 
 /**
- * Date: 20/09/2017
+ * Date: 22/09/2017
  * 
  * @author Gabriel Dornelas
  *
  */
 public class ArgentinaFarmacityCrawler extends Crawler {
 
-	private final String HOME_PAGE = "https://www.farmacity.com/";
+	private static final String HOME_PAGE = "https://www.farmacity.com/";
 
 	public ArgentinaFarmacityCrawler(Session session) {
 		super(session);
@@ -45,142 +44,171 @@ public class ArgentinaFarmacityCrawler extends Crawler {
 	}
 
 	@Override
-	public List<Product> extractInformation(Document doc) throws Exception {
+	public List<Product>  extractInformation(Document doc) throws Exception {
 		super.extractInformation(doc);
 		List<Product> products = new ArrayList<>();
 
 		if ( isProductPage(doc) ) {
 			Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
 
-			JSONObject dataLayer = crawlDatalayer(doc);
-			
-			String internalId = crawlInternalId(dataLayer);
-			String internalPid = crawlInternalPid(dataLayer);
-			String name = crawlName(doc);
-			Float price = crawlPrice(doc);
-			Prices prices = crawlPrices(price, doc);
-			boolean available = price != null;
+			JSONObject productJson = crawlProductJson(doc);	
+			String internalPid = crawlInternalPid(productJson);
 			CategoryCollection categories = crawlCategories(doc);
-			String primaryImage = crawlPrimaryImage(doc);
-			String secondaryImages = crawlSecondaryImages(doc);
 			String description = crawlDescription(doc);
 			Integer stock = null;
-			Marketplace marketplace = crawlMarketplace();
 
-			// Creating the product
-			Product product = ProductBuilder.create()
-					.setUrl(session.getOriginalURL())
-					.setInternalId(internalId)
-					.setInternalPid(internalPid)
-					.setName(name)
-					.setPrice(price)
-					.setPrices(prices)
-					.setAvailable(available)
-					.setCategory1(categories.getCategory(0))
-					.setCategory2(categories.getCategory(1))
-					.setCategory3(categories.getCategory(2))
-					.setPrimaryImage(primaryImage)
-					.setSecondaryImages(secondaryImages)
-					.setDescription(description)
-					.setStock(stock)
-					.setMarketplace(marketplace)
-					.build();
+			JSONArray arraySkus = productJson.has("variants") ? productJson.getJSONArray("variants") : new JSONArray();
+			
+			for(int i = 0; i < arraySkus.length(); i++){
+				JSONObject jsonSku = arraySkus.getJSONObject(i);
 
-			products.add(product);
+				String internalId = crawlInternalId(jsonSku);
+				String name = crawlName(jsonSku);
+				boolean available = crawlAvailability(jsonSku);
+				Float price = crawlMainPagePrice(jsonSku, available);
+				String primaryImage = crawlPrimaryImage(jsonSku);
+				String secondaryImages = crawlSecondaryImages(jsonSku);
+				Prices prices = crawlPrices(price);
+
+				// Creating the product
+				Product product = ProductBuilder.create()
+						.setUrl(session.getOriginalURL())
+						.setInternalId(internalId)
+						.setInternalPid(internalPid)
+						.setName(name)
+						.setPrice(price)
+						.setPrices(prices)
+						.setAvailable(available)
+						.setCategory1(categories.getCategory(0))
+						.setCategory2(categories.getCategory(1))
+						.setCategory3(categories.getCategory(2))
+						.setPrimaryImage(primaryImage)
+						.setSecondaryImages(secondaryImages)
+						.setDescription(description)
+						.setStock(stock)
+						.setMarketplace(new Marketplace())
+						.build();
+
+				products.add(product);
+			}
 
 		} else {
 			Logging.printLogDebug(logger, session, "Not a product page" + this.session.getOriginalURL());
 		}
 
 		return products;
-
 	}
+
+	/*******************************
+	 * Product page identification *
+	 *******************************/
 
 	private boolean isProductPage(Document doc) {
 		return doc.select(".main-title > a").first() != null;
 	}
 
-	private String crawlInternalId(JSONObject dataLayer) {
+	/*******************
+	 * General methods *
+	 *******************/
+
+	private String crawlInternalId(JSONObject json) {
 		String internalId = null;
 
-		if (dataLayer.has("product")) {
-			JSONObject product = dataLayer.getJSONObject("product");
-			
-			if(product.has("sku")) {
-				internalId = product.get("sku").toString();
-			}
+		if (json.has("sku")) {
+			internalId = Integer.toString(json.getInt("sku")).trim();			
 		}
 
 		return internalId;
-	}
-	
-	private String crawlInternalPid(JSONObject dataLayer) {
+	}	
+
+
+	private String crawlInternalPid(JSONObject json) {
 		String internalPid = null;
 
-		if (dataLayer.has("product")) {
-			JSONObject product = dataLayer.getJSONObject("product");
-			
-			if(product.has("id")) {
-				internalPid = product.get("id").toString();
-			}
+		if (json.has("id")) {
+			internalPid = json.get("id").toString();
 		}
 
 		return internalPid;
 	}
 
-	private String crawlName(Document document) {
+	private String crawlName(JSONObject jsonSku) {
 		String name = null;
-		Element nameElement = document.select(".main-title > a").first();
 
-		if (nameElement != null) {
-			name = nameElement.text().trim();
-		}
+		if (jsonSku.has("name")) {
+			name = jsonSku.getString("name");
+
+			if(jsonSku.has("option_values")) {
+				JSONArray properties = jsonSku.getJSONArray("option_values");
+				
+				for(int i = 0; i < properties.length(); i++) {
+					JSONObject p = properties.getJSONObject(i);
+					
+					if(p.has("presentation")) {
+						name += " " + p.get("presentation");
+					}
+				}
+			}
+ 		}
 
 		return name;
 	}
 
-	private Float crawlPrice(Document document) {
+	private Float crawlMainPagePrice(JSONObject json, boolean available) {
 		Float price = null;
-		Element salePriceElement = document.select(".final-price .display-price").first();		
 
-		if (salePriceElement != null) {
-			price = MathCommonsMethods.parseFloat(salePriceElement.text());
-		} 
+		if (json.has("price") && available) {
+			price = Float.parseFloat(json.getString("price"));
+		}
 
 		return price;
 	}
 
-	private Marketplace crawlMarketplace() {
-		return new Marketplace();
+	private boolean crawlAvailability(JSONObject json) {
+		if(json.has("in_stock")) {
+			return json.getBoolean("in_stock");
+		}
+		return false;
 	}
 
-
-	private String crawlPrimaryImage(Document doc) {
+	private String crawlPrimaryImage(JSONObject skuJson) {
 		String primaryImage = null;
-		Element elementPrimaryImage = doc.select(".main-pic-container > a").first();
 		
-		if(elementPrimaryImage != null ) {
-			primaryImage = elementPrimaryImage.attr("href");
-		} 
-		
+		if(skuJson.has("images") && skuJson.getJSONArray("images").length() > 0) {
+			JSONObject image = skuJson.getJSONArray("images").getJSONObject(0);
+			
+			if(image.has("gallery_large_url") && image.get("gallery_large_url").toString().startsWith("http")) {
+				primaryImage = image.get("gallery_large_url").toString();
+			} else if(image.has("gallery_small_url") && image.get("gallery_small_url").toString().startsWith("http")) {
+				primaryImage = image.get("gallery_small_url").toString();
+			} else if(image.has("gallery_thumb_url") && image.get("gallery_thumb_url").toString().startsWith("http")) {
+				primaryImage = image.get("gallery_thumb_url").toString();
+			}
+		}
+
 		return primaryImage;
 	}
 
-	/**
-	 * 
-	 * @param doc
-	 * @return
-	 */
-	private String crawlSecondaryImages(Document doc) {
+	private String crawlSecondaryImages(JSONObject skuJson) {
 		String secondaryImages = null;
 		JSONArray secondaryImagesArray = new JSONArray();
-		
-		Elements images = doc.select(".list-gallery-thumbs > li:not(.active) > a");
-		
-		for(Element image : images) {
-			secondaryImagesArray.put(image.attr("href"));
+
+		if(skuJson.has("images") && skuJson.getJSONArray("images").length() > 1) {
+			JSONArray images = skuJson.getJSONArray("images");
+			
+			for (int i = 1; i < images.length(); i++) { //starts with index 1, because the first image is the primary image
+				JSONObject image = images.getJSONObject(i);
+				
+				if(image.has("gallery_large_url") && image.get("gallery_large_url").toString().startsWith("http")) {
+					secondaryImagesArray.put(image.get("gallery_large_url").toString());
+				} else if(image.has("gallery_small_url") && image.get("gallery_small_url").toString().startsWith("http")) {
+					secondaryImagesArray.put(image.get("gallery_small_url").toString());
+				} else if(image.has("gallery_thumb_url") && image.get("gallery_thumb_url").toString().startsWith("http")) {
+					secondaryImagesArray.put(image.get("gallery_thumb_url").toString());
+				}
+			}
 		}
-		
+
 		if (secondaryImagesArray.length() > 0) {
 			secondaryImages = secondaryImagesArray.toString();
 		}
@@ -188,10 +216,6 @@ public class ArgentinaFarmacityCrawler extends Crawler {
 		return secondaryImages;
 	}
 
-	/**
-	 * @param document
-	 * @return
-	 */
 	private CategoryCollection crawlCategories(Document document) {
 		CategoryCollection categories = new CategoryCollection();
 		Elements elementCategories = document.select(".breadcrumb li[itemprop=child] a span");
@@ -206,6 +230,7 @@ public class ArgentinaFarmacityCrawler extends Crawler {
 
 		return categories;
 	}
+
 
 	private String crawlDescription(Document doc) {
 		StringBuilder description = new StringBuilder();
@@ -224,28 +249,32 @@ public class ArgentinaFarmacityCrawler extends Crawler {
 		
 		return description.toString();
 	}
-	
+
 	/**
+	 * There is no bank slip payment method
+	 * Has no informations of installments
 	 * 
-	 * @param doc
+	 * @param internalId
 	 * @param price
 	 * @return
 	 */
-	private Prices crawlPrices(Float price, Document doc) {
+	private Prices crawlPrices(Float price) {
 		Prices prices = new Prices();
-		
-		if (price != null) {
-			Map<Integer,Float> installmentPriceMap = new TreeMap<>();
-			installmentPriceMap.put(1, price);
-			
-			prices.insertCardInstallment(Card.VISA.toString(), installmentPriceMap);
-			prices.insertCardInstallment(Card.MASTERCARD.toString(), installmentPriceMap);
-			prices.insertCardInstallment(Card.AMEX.toString(), installmentPriceMap);
-			prices.insertCardInstallment(Card.CABAL.toString(), installmentPriceMap);
-			prices.insertCardInstallment(Card.NARANJA.toString(), installmentPriceMap);
-			prices.insertCardInstallment(Card.SHOP_CARD.toString(), installmentPriceMap);
+
+		if(price != null){
+			Map<Integer,Float> mapInstallments = new HashMap<>();
+			mapInstallments.put(1, price);
+
+			prices.insertCardInstallment(Card.VISA.toString(), mapInstallments);
+			prices.insertCardInstallment(Card.MASTERCARD.toString(), mapInstallments);
+			prices.insertCardInstallment(Card.DINERS.toString(), mapInstallments);
+			prices.insertCardInstallment(Card.SHOP_CARD.toString(), mapInstallments);
+			prices.insertCardInstallment(Card.NARANJA.toString(), mapInstallments);
+			prices.insertCardInstallment(Card.NATIVA.toString(), mapInstallments);
+			prices.insertCardInstallment(Card.AMEX.toString(), mapInstallments);
+
 		}
-		
+
 		return prices;
 	}
 
@@ -253,31 +282,26 @@ public class ArgentinaFarmacityCrawler extends Crawler {
 	 * Get the script having a json with the availability information
 	 * @return
 	 */
-	private JSONObject crawlDatalayer(Document document) {
+	private JSONObject crawlProductJson(Document document) {
 		Elements scriptTags = document.getElementsByTag("script");
-		JSONObject dataLayerJson = null;
-		
-		for (Element tag : scriptTags){   
-			
-			String script = tag.html();
-			
-			if(script.trim().startsWith("var dataLayer = ")) {
+		JSONObject skuJson = new JSONObject();
 
-				int x = script.indexOf("[{")+1;
-				int y = script.indexOf("}]", x);
-				
-				String dataLayer = script.substring(x, y+1);
-				
-				try {
-					dataLayerJson = new JSONObject(dataLayer);
-				} catch (JSONException e) {
-					Logging.printLogError(logger, session, CommonMethods.getStackTrace(e));
+		for (Element tag : scriptTags) {                
+			for (DataNode node : tag.dataNodes()) {
+				if(tag.html().trim().startsWith("var data = ")) {
+					skuJson = new JSONObject
+						(
+							node.getWholeData().split(Pattern.quote("var data = "))[1] +
+							node.getWholeData().split(Pattern.quote("var data = "))[1].split(Pattern.quote("};"))[0]
+						);
 				}
-
-				break;
-			}       
+			}        
 		}
-		
-		return dataLayerJson;
+
+		if(skuJson.has("product") && skuJson.getJSONObject("product").has("variants")) {
+			return skuJson.getJSONObject("product");
+		}
+
+		return new JSONObject();
 	}
 }
