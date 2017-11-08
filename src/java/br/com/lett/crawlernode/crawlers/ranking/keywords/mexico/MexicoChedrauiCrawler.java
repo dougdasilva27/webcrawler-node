d3bf265f2c2c9b1f9fb074a8e8f16994ab0be8a9
gabriel <gabrieldornelas@lett.com.br>
2017-11-08 16:46:25 -0200

    finalizei ranking/discover do chedraui [deploy:Production]
    Former-commit-id: bdf6555ed44c8f1798d89ada42b8c150ef74bf53

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/mexico/MexicoChedrauiCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/mexico/MexicoChedrauiCrawler.java
@@ -1,203 +1,118 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.mexico;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import br.com.lett.crawlernode.core.session.Session;
-import br.com.lett.crawlernode.core.session.ranking.RankingSession;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
 
-public class MexicoChedrauiCrawler extends CrawlerRankingKeywords{
-	
+public class MexicoChedrauiCrawler extends CrawlerRankingKeywords {
+
 	public MexicoChedrauiCrawler(Session session) {
 		super(session);
 	}
 
-	private static final String TIENDA = "mexicociudadlabor";
-//	private static final String TIENDA_ID = "4294967169";
-//	private static final String REMOTE_ADDRESS = "74.205.80.195:8080";
-//	private final static String REMOTE_ADDRESS = "174.143.32.125:8080";
-//	private final static String REMOTE_ADDRESS = "contenido.chedraui.com.mx";
-	
+	private boolean isCategory;
+	private String urlCategory;
+
 	@Override
 	protected void extractProductsFromCurrentPage() {
-		//número de produtos por página do market
-		this.pageSize = 24;
-			
-		this.log("Página "+ this.currentPage);
-		
-		//monta a url com a keyword e a página
-		// primeira página começa em 0 e assim vai.
-		String url = "https://contenido.chedraui.com.mx/endeca-assembler/json/search?"
-				+ "N=4294967169+&Ntt="+ this.keywordEncoded +"&Nrpp=24&No=" + this.arrayProducts.size();
-		
-		this.log("Link onde são feitos os crawlers: " + url);	
-		
-		if(((RankingSession)session).mustTakeAScreenshot() && this.currentPage <= 2) {
-			String printUrl = "http://www.chedraui.com.mx/index.php/mexicociudadlabor/endeca/result/?&N=4294967169+&Ntt="
-					+ this.keywordEncoded +"&No=" + this.arrayProducts.size();
-			takeAScreenshot(printUrl);
+		this.log("Página " + this.currentPage);
+
+		// monta a url com a keyword e a página
+
+		String url = "https://www.chedraui.com.mx/chedrauistorefront/chedraui/es/search/?text="
+				+ this.keywordEncoded;
+
+		if (this.currentPage > 1 && isCategory) {
+			url = this.urlCategory + "?q=%3Arelevance&page=" + (this.currentPage - 1) + "&pageSize=24";
+		} else if (this.currentPage > 1) {
+			url = "https://www.chedraui.com.mx/chedrauistorefront/chedraui/es/search?q="
+					+ this.keywordEncoded + "%3Arelevance&page=" + (this.currentPage - 1) + "&pageSize=24";
 		}
-		
-		// pega o json da api
-		JSONObject jsonSearch = fetchJSONObject(url);
-		
-		// pega as informações importantes
-		JSONObject productsInfo = getImportantInformations(jsonSearch);
-		
-		if(productsInfo.has("products")){
-			JSONArray products = productsInfo.getJSONArray("products");
-			
-			//se obter 1 ou mais links de produtos e essa página tiver resultado faça:
-			if(products.length() >= 1) {
-				//se o total de busca não foi setado ainda, chama a função para setar
-				if(this.totalProducts == 0) {
-					setTotalBusca(productsInfo);
-				}
-				
-				for(int i = 0; i < products.length(); i++) {
-					JSONObject product = products.getJSONObject(i);
-	
-					// InternalPid
-					String internalPid = crawlInternalPid();
-					
-					// InternalId
-					String internalId = crawlInternalId(product);
-					
-					// Url do produto
-					String productUrl = crawlProductUrl(product);
-					
-					saveDataProduct(internalId, internalPid, productUrl);
-					
-					this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + internalPid + " - Url: " + productUrl);
-					if(this.arrayProducts.size() == productsLimit) {
-						break;
-					}
-				}
+
+		this.log("Link onde são feitos os crawlers: " + url);
+
+		this.currentDoc = fetchDocument(url);
+
+		if (this.currentPage == 1) {
+			String redirectUrl = this.session.getRedirectedToURL(url);
+
+			if (redirectUrl != null && currentDoc.select(".breadcrumb li").size() > 1) {
+				isCategory = true;
+				this.urlCategory = redirectUrl.split("\\?")[0];
 			} else {
-				this.result = false;
-				this.log("Keyword sem resultado!");
+				isCategory = false;
+			}
+		}
+
+		Elements products = this.currentDoc.select(".product__list--item > a[title]");
+
+		if (!products.isEmpty()) {
+
+			if (this.totalProducts == 0) {
+				setTotalProducts();
+			}
+
+			for (Element e : products) {
+				String productUrl = crawlProductUrl(e);
+				String internalPid = crawlInternalPid(productUrl);
+				String internalId = null;
+
+				saveDataProduct(internalId, internalPid, productUrl);
+
+				this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: "
+						+ internalPid + " - Url: " + productUrl);
+				if (this.arrayProducts.size() == productsLimit)
+					break;
 			}
 		} else {
 			this.result = false;
 			this.log("Keyword sem resultado!");
 		}
 
-		this.log("Finalizando Crawler de produtos da página "+this.currentPage+" - até agora "+this.arrayProducts.size()+" produtos crawleados");
+		this.pageSize = 24;
+
+		this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora "
+				+ this.arrayProducts.size() + " produtos crawleados");
+
 	}
 
 	@Override
 	protected boolean hasNextPage() {
-		// se não atingiu o total da busca ainda tem mais páginas.
-		if(this.arrayProducts.size() < this.totalProducts){
-			return true;
-		}
-		
-		return false;
+		return this.arrayProducts.size() < this.totalProducts;
 	}
-	
 
-	protected void setTotalBusca(JSONObject productsInfo) {
-		if(productsInfo.has("totalBusca")) { 	
-			try {
-				this.totalProducts = productsInfo.getInt("totalBusca");
-			} catch(Exception e) {
-				this.logError(e.getMessage());
+	@Override
+	protected void setTotalProducts() {
+		Element totalElement = this.currentDoc.select(".pagination-bar-results").first();
+
+		if (totalElement != null) {
+			String text = totalElement.ownText().replaceAll("[^0-9]", "").trim();
+
+			if (!text.isEmpty()) {
+				this.totalProducts = Integer.parseInt(text);
 			}
-			
-			this.log("Total da busca: "+this.totalProducts);
 		}
+
+		this.log("Total da busca: " + this.totalProducts);
 	}
-	
-	private String crawlInternalId(JSONObject product){
-		String internalId = null;
-		
-		if(product.has("id")){
-			internalId = product.getString("id");
-		}
-		
-		return internalId;
-	}
-	
-	private String crawlInternalPid(){
+
+	private String crawlInternalPid(String url) {
 		String internalPid = null;
-		
+
+		String[] tokens = url.split("\\?")[0].split("/p");
+		internalPid = tokens[tokens.length - 1].replaceAll("[^0-9]", "");
+
 		return internalPid;
 	}
-	
-	private String crawlProductUrl(JSONObject product){
-		String urlProduct = null;
-		
-		if(product.has("idUrl")){
-			urlProduct = "http://www.chedraui.com.mx/index.php/"+ TIENDA +"/catalog/product/view/id/"+ product.getString("idUrl") +"/";
+
+	private String crawlProductUrl(Element e) {
+		String productUrl = e.attr("href");
+
+		if (!productUrl.contains("http")) {
+			productUrl = ("https://www.chedraui.com.mx/" + productUrl).replace(".mx//", ".mx/");
 		}
-		
-		return urlProduct;
-	}
-	
-	private JSONObject getImportantInformations(JSONObject jsonSearch){
-		JSONObject products = new JSONObject();
-		
-		if(jsonSearch.has("contents")){
-			JSONArray contents = jsonSearch.getJSONArray("contents");
-			
-			if(contents.length() > 0){
-				JSONObject content = contents.getJSONObject(0);
-				
-				if(content.has("mainContent")){
-					JSONArray mainContent = content.getJSONArray("mainContent");
-					
-					if(mainContent.length() > 1){
-						JSONObject info = mainContent.getJSONObject(1);
-						
-						if(info.has("totalNumRecs")){
-							products.put("totalBusca", info.getInt("totalNumRecs"));
-						}
-						
-						products.put("products", getJsonProducts(info));
-					}
-				}
-			}
-		}
-		
-		return products;
-	}
-	
-	private JSONArray getJsonProducts(JSONObject jsonInfo){
-		JSONArray products = new JSONArray();
-		
-		if(jsonInfo.has("records")){
-			JSONArray records = jsonInfo.getJSONArray("records");
-			
-			for(int i = 0; i < records.length(); i++){
-				JSONObject putJson = new JSONObject();
-				JSONObject product = records.getJSONObject(i);
-				
-				if(product.has("attributes")){
-					JSONObject attributes = product.getJSONObject("attributes");
-					
-					if(attributes.has("P_product_id")){
-						JSONArray pid = attributes.getJSONArray("P_product_id");
-						
-						if(pid.length() > 0){
-							putJson.put("idUrl", pid.getString(0));
-						}
-					}
-					
-					if(attributes.has("P_sku_id")){
-						JSONArray sku = attributes.getJSONArray("P_sku_id");
-						
-						if(sku.length() > 0){
-							putJson.put("id", sku.getString(0));
-						}
-					}
-					
-					products.put(putJson);
-				}
-				
-			}
-			
-		}
-		
-		return products;
+
+		return productUrl;
 	}
 }
