7a60515b3d10aac37b697416f9453eeb919bafc3
gabriel <gabriel@gabriel>
2017-12-11 12:04:24 -0200

    Criando crawler rating extra delivery [deploy:Production]
    Former-commit-id: 0e5ca1356641741acee8ac5b735e1ed6ed7a0a65

---
+++ src/java/br/com/lett/crawlernode/crawlers/ratingandreviews/saopaulo/SaopauloExtraRatingReviewCrawler.java
@@ -0,0 +1,149 @@
+package br.com.lett.crawlernode.crawlers.ratingandreviews.saopaulo;
+
+import org.apache.http.impl.cookie.BasicClientCookie;
+import org.json.JSONObject;
+import org.jsoup.nodes.Document;
+
+import br.com.lett.crawlernode.core.fetcher.DataFetcher;
+import br.com.lett.crawlernode.core.models.RatingReviewsCollection;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.RatingReviewCrawler;
+import br.com.lett.crawlernode.util.Logging;
+import models.RatingsReviews;
+
+public class SaopauloExtraRatingReviewCrawler extends RatingReviewCrawler {
+	
+	public SaopauloExtraRatingReviewCrawler(Session session) {
+		super(session);
+	}
+	
+	// Loja 501 sp
+	private static final String STORE_ID = "241";
+	
+	private static final String HOME_PAGE = "https://www.deliveryextra.com.br";
+	
+	@Override
+	public void handleCookiesBeforeFetch() {
+
+		// Criando cookie da loja 3 = São Paulo capital
+		BasicClientCookie cookie = new BasicClientCookie("ep.selected_store", STORE_ID);
+		cookie.setDomain(".deliveryextra.com");
+		cookie.setPath("/");
+		this.cookies.add(cookie);
+
+	}
+
+	@Override
+	protected RatingReviewsCollection extractRatingAndReviews(Document doc) throws Exception {
+		RatingReviewsCollection ratingReviewsCollection = new RatingReviewsCollection();
+
+		if (isProductPage(session.getOriginalURL())) {
+			Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
+			
+			RatingsReviews ratingReviews = new RatingsReviews();			
+			ratingReviews.setDate(session.getDate());
+
+			JSONObject rating = crawlProductInformatioFromGPAApi(session.getOriginalURL());
+
+			Integer totalNumOfEvaluations = getTotalNumOfRatings(rating);			
+			Double avgRating = getTotalAvgRating(rating);
+
+			ratingReviews.setTotalRating(totalNumOfEvaluations);
+			ratingReviews.setAverageOverallRating(avgRating);
+			ratingReviews.setInternalId(crawlInternalId(session.getOriginalURL()));
+			ratingReviewsCollection.addRatingReviews(ratingReviews);
+
+		}
+
+		return ratingReviewsCollection;
+
+	}
+
+
+	private String crawlInternalId(String productUrl){
+		return productUrl.replace(HOME_PAGE, "").split("/")[2];
+	}
+	
+	/**
+	 * Average is in html element 
+	 * @param document
+	 * @return
+	 */
+	private Double getTotalAvgRating(JSONObject rating) {
+		Double avgRating = 0d;
+		
+		if(rating.has("average")) {
+			avgRating = rating.getDouble("average");
+		}
+		
+		return avgRating;
+	}
+
+	/**
+	 * Number of ratings appear in key rating in json
+	 * @param docRating
+	 * @return
+	 */
+	private Integer getTotalNumOfRatings(JSONObject rating) {
+		Integer totalRating = 0;
+		
+		if(rating.has("rating")) {
+			JSONObject ratingValues = rating.getJSONObject("rating");
+			
+			totalRating = 0;
+			
+			for(int i = 1; i <= ratingValues.length(); i++) {
+				if(ratingValues.has(Integer.toString(i))) {
+					totalRating += ratingValues.getInt(Integer.toString(i));
+				}
+			}
+		}
+
+		return totalRating;
+	}
+
+
+
+	private boolean isProductPage(String url) {
+		if (url.contains("deliveryextra.com.br/produto/")) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Get the json of gpa api, 
+	 * this api has all info rating product
+	 * @return
+	 */
+	private JSONObject crawlProductInformatioFromGPAApi(String productUrl) { 
+		JSONObject productsInfo = new JSONObject();
+		
+		String id;
+		
+		if(productUrl.contains("?")) {
+			int x = productUrl.indexOf("produto/") + "produto/".length();
+			int y = productUrl.indexOf("?", x);
+			
+			id = productUrl.substring(x, y);
+		} else {
+			int x = productUrl.indexOf("produto/") + "produto/".length();
+			id = productUrl.substring(x);
+		}
+		
+		if(id != null && id.contains("/")) {
+			id = id.split("/")[0];
+		}
+		
+		String url = "https://api.gpa.digital/pa/products/"+ id +"/review";
+		
+		JSONObject apiGPA = DataFetcher.fetchJSONObject(DataFetcher.GET_REQUEST, session, url, null, cookies);
+		
+		if(apiGPA.has("content")) {
+			productsInfo = apiGPA.getJSONObject("content");
+		}
+		
+		return productsInfo;
+	}
+
+}
