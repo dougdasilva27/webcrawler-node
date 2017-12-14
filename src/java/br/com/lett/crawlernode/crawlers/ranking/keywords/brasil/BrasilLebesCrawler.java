55589f480c2841a4d04b7002792e9c5c912f0e1e
Gabriel Dornelas <gabrieldornelas@lett.com.br>
2017-12-14 11:52:54 -0200

    Bugfi pagesize ranking lebes [CSI-1089]
    Former-commit-id: ad0526a397142c79e4dbc4fc058b72a3c0b65bf3

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilLebesCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilLebesCrawler.java
@@ -2,120 +2,121 @@ package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
 
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
 
-public class BrasilLebesCrawler extends CrawlerRankingKeywords{
+public class BrasilLebesCrawler extends CrawlerRankingKeywords {
 
-	public BrasilLebesCrawler(Session session) {
-		super(session);
-	}
+  public BrasilLebesCrawler(Session session) {
+    super(session);
+  }
 
-	@Override
-	protected void extractProductsFromCurrentPage() {
-		this.log("Página "+ this.currentPage);
+  @Override
+  protected void extractProductsFromCurrentPage() {
+    this.log("Página " + this.currentPage);
 
-		//número de produtos por página do market
-		this.pageSize = 16;
+    // número de produtos por página do market
+    this.pageSize = 24;
 
-		String keyword = this.keywordWithoutAccents.replaceAll(" ", "%20");
+    String keyword = this.keywordWithoutAccents.replaceAll(" ", "%20");
 
-		//monta a url com a keyword e a página
-		String url = "http://www.lebes.com.br/"+keyword+"?PageNumber="+this.currentPage;
-		this.log("Link onde são feitos os crawlers: "+url);	
+    // monta a url com a keyword e a página
+    String url = "http://www.lebes.com.br/" + keyword + "?PageNumber=" + this.currentPage;
+    this.log("Link onde são feitos os crawlers: " + url);
 
-		//chama função de pegar a url
-		this.currentDoc = fetchDocument(url);
+    // chama função de pegar a url
+    this.currentDoc = fetchDocument(url);
 
-		Elements products = this.currentDoc.select("div.shelf--products:not([id]) ul > li[layout]");
+    Elements products = this.currentDoc.select("div.shelf--products:not([id]) ul > li[layout]");
 
-		//se obter 1 ou mais links de produtos e essa página tiver resultado faça:
-		if(products.size() >= 1) {
-			//se o total de busca não foi setado ainda, chama a função para setar
-			if(this.totalProducts == 0) {
-				setTotalProducts();
-			}
+    // se obter 1 ou mais links de produtos e essa página tiver resultado faça:
+    if (products.size() >= 1) {
+      // se o total de busca não foi setado ainda, chama a função para setar
+      if (this.totalProducts == 0) {
+        setTotalProducts();
+      }
 
-			for(Element e: products) {
-				// InternalPid
-				String internalPid 	= crawlInternalPid(e);
+      for (Element e : products) {
+        // InternalPid
+        String internalPid = crawlInternalPid(e);
 
-				if(internalPid != null){
-					// InternalId
-					String internalId 	= crawlInternalId(e);
+        if (internalPid != null) {
+          // InternalId
+          String internalId = crawlInternalId(e);
 
-					// Url do produto
-					String urlProduct = crawlProductUrl(e);
+          // Url do produto
+          String urlProduct = crawlProductUrl(e);
 
-					saveDataProduct(internalId, internalPid, urlProduct);
+          saveDataProduct(internalId, internalPid, urlProduct);
 
-					this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + internalPid + " - Url: " + urlProduct);
-					
-					if(this.arrayProducts.size() == productsLimit) {
-						break;
-					}
-				}
+          this.log("Position: " + this.position + " - InternalId: " + internalId
+              + " - InternalPid: " + internalPid + " - Url: " + urlProduct);
 
-			}
-		} else {
-			this.result = false;
-			this.log("Keyword sem resultado!");
-		}
+          if (this.arrayProducts.size() == productsLimit) {
+            break;
+          }
+        }
 
-		this.log("Finalizando Crawler de produtos da página "+this.currentPage+" - até agora "+this.arrayProducts.size()+" produtos crawleados");
-	}
+      }
+    } else {
+      this.result = false;
+      this.log("Keyword sem resultado!");
+    }
 
-	@Override
-	protected boolean hasNextPage() {
-		
-		if(arrayProducts.size() < this.totalProducts ){
-			return true;
-		}
+    this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora "
+        + this.arrayProducts.size() + " produtos crawleados");
+  }
 
-		return false;
-	}
+  @Override
+  protected boolean hasNextPage() {
 
-	@Override
-	protected void setTotalProducts() {
-		Element totalElement = this.currentDoc.select("span.resultado-busca-numero span.value").first();
+    if (arrayProducts.size() < this.totalProducts) {
+      return true;
+    }
 
-		try {
-			if(totalElement != null) {
-				this.totalProducts = Integer.parseInt(totalElement.text());
-			}
-		} catch(Exception e) {
-			this.logError(e.getMessage());
-		}
+    return false;
+  }
 
-		this.log("Total da busca: "+this.totalProducts);
-	}
+  @Override
+  protected void setTotalProducts() {
+    Element totalElement = this.currentDoc.select("span.resultado-busca-numero span.value").first();
 
-	private String crawlInternalId(Element e){
-		String internalId = null;
+    try {
+      if (totalElement != null) {
+        this.totalProducts = Integer.parseInt(totalElement.text());
+      }
+    } catch (Exception e) {
+      this.logError(e.getMessage());
+    }
 
-		return internalId;
-	}
+    this.log("Total da busca: " + this.totalProducts);
+  }
 
-	private String crawlInternalPid(Element e){
-		String internalPid = null;
-		Element pidElement = e.select(".vtex-cpProdId").first();
+  private String crawlInternalId(Element e) {
+    String internalId = null;
 
-		if(pidElement != null){
-			internalPid = pidElement.attr("value");
-		}
+    return internalId;
+  }
 
-		return internalPid;
-	}
+  private String crawlInternalPid(Element e) {
+    String internalPid = null;
+    Element pidElement = e.select(".vtex-cpProdId").first();
 
-	private String crawlProductUrl(Element e){
-		String urlProduct = null;
-		Element urlElement = e.select(".vtex-cpUri").first();
+    if (pidElement != null) {
+      internalPid = pidElement.attr("value");
+    }
 
-		if(urlElement != null){
-			urlProduct = urlElement.val();
-		}
+    return internalPid;
+  }
 
-		return urlProduct;
-	}
+  private String crawlProductUrl(Element e) {
+    String urlProduct = null;
+    Element urlElement = e.select(".vtex-cpUri").first();
+
+    if (urlElement != null) {
+      urlProduct = urlElement.val();
+    }
+
+    return urlProduct;
+  }
 }
