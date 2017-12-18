ded1a93677acc615b237e41d30f4dad56dc838ef
Gabriel Dornelas <gabrieldornelas@lett.com.br>
2017-12-18 14:40:49 -0200

    Bugfix rating nutrii
    Former-commit-id: 2027246b623470f84b5d68054437fac614fe7853

--- src/java/br/com/lett/crawlernode/crawlers/ratingandreviews/brasil/BrasilNutriiRatingReviewCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ratingandreviews/brasil/BrasilNutriiRatingReviewCrawler.java
@@ -2,7 +2,6 @@ package br.com.lett.crawlernode.crawlers.ratingandreviews.brasil;
 
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-
 import br.com.lett.crawlernode.core.models.RatingReviewsCollection;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.RatingReviewCrawler;
@@ -11,93 +10,96 @@ import models.RatingsReviews;
 
 /**
  * Date: 25/08/17
+ * 
  * @author gabriel
  *
  */
 public class BrasilNutriiRatingReviewCrawler extends RatingReviewCrawler {
 
-	public BrasilNutriiRatingReviewCrawler(Session session) {
-		super(session);
-	}
+  public BrasilNutriiRatingReviewCrawler(Session session) {
+    super(session);
+  }
 
-	@Override
-	protected RatingReviewsCollection extractRatingAndReviews(Document document) throws Exception {
-		RatingReviewsCollection ratingReviewsCollection = new RatingReviewsCollection();
+  @Override
+  protected RatingReviewsCollection extractRatingAndReviews(Document document) throws Exception {
+    RatingReviewsCollection ratingReviewsCollection = new RatingReviewsCollection();
 
-		if (isProductPage(document)) {
-			Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
+    if (isProductPage(document)) {
+      Logging.printLogDebug(logger, session,
+          "Product page identified: " + this.session.getOriginalURL());
 
-			RatingsReviews ratingReviews = new RatingsReviews();			
-			ratingReviews.setDate(session.getDate());
+      RatingsReviews ratingReviews = new RatingsReviews();
+      ratingReviews.setDate(session.getDate());
 
-			String internalId = crawlInternalId(document);
+      String internalId = crawlInternalId(document);
 
-			Integer totalNumOfEvaluations = getTotalNumOfRatings(document);			
-			Double avgRating = getTotalAvgRating(document);
+      Integer totalNumOfEvaluations = getTotalNumOfRatings(document);
+      Double avgRating = getTotalAvgRating(document);
 
-			ratingReviews.setInternalId(internalId);
-			ratingReviews.setTotalRating(totalNumOfEvaluations);
-			ratingReviews.setTotalWrittenReviews(totalNumOfEvaluations);
-			ratingReviews.setAverageOverallRating(avgRating);
+      ratingReviews.setInternalId(internalId);
+      ratingReviews.setTotalRating(totalNumOfEvaluations);
+      ratingReviews.setTotalWrittenReviews(totalNumOfEvaluations);
+      ratingReviews.setAverageOverallRating(avgRating);
 
-			ratingReviewsCollection.addRatingReviews(ratingReviews);
-		}
+      ratingReviewsCollection.addRatingReviews(ratingReviews);
+    }
 
-		return ratingReviewsCollection;
+    return ratingReviewsCollection;
 
-	}
+  }
 
-	private String crawlInternalId(Document doc){
-		String internalId = null;
+  private String crawlInternalId(Document doc) {
+    String internalId = null;
 
-		Element internalIdElement = doc.select("input[name=product]").first();
-		if (internalIdElement != null) {
-			internalId = internalIdElement.val();
-		}
+    Element internalIdElement = doc.select("input[name=product]").first();
+    if (internalIdElement != null) {
+      internalId = internalIdElement.val();
+    }
 
-		return internalId;
-	}
+    return internalId;
+  }
 
-	/**
-	 * Avg appear in html element
-	 * 
-	 * @param document
-	 * @return
-	 */
-	private Double getTotalAvgRating(Document doc) {
-		Double avgRating = 0d;
-		Element avg = doc.select("div[itemprop=ratingValue]").first();
-		
-		if(avg != null) {
-			avgRating = Double.parseDouble(avg.ownText());
-		}
-		
-		return avgRating;
-	}
+  /**
+   * Avg appear in html element
+   * 
+   * @param document
+   * @return
+   */
+  private Double getTotalAvgRating(Document doc) {
+    Double avgRating = 0d;
+    Element avg = doc.select("div.rating").first();
 
-	/**
-	 * Number of ratings appear in html element 
-	 * @param doc
-	 * @return
-	 */
-	private Integer getTotalNumOfRatings(Document doc) {
-		Integer ratingNumber = 0;
-		Element reviews = doc.select("span[itemprop=reviewCount]").first();
+    if (avg != null) {
+      avgRating = Double.parseDouble(avg.ownText());
+    }
 
-		if(reviews != null) {
-			String text = reviews.ownText().replaceAll("[^0-9]", "").trim();
-			
-			if(!text.isEmpty()) {
-				ratingNumber = Integer.parseInt(text);
-			}
-		}
-		
-		return ratingNumber;
-	}
+    return avgRating;
+  }
+
+  /**
+   * Number of ratings appear in html element
+   * 
+   * @param doc
+   * @return
+   */
+  private Integer getTotalNumOfRatings(Document doc) {
+    Integer ratingNumber = 0;
+    Element reviews = doc.select(".rating-links span.cor").first();
+
+    if (reviews != null) {
+      String text = reviews.ownText().replaceAll("[^0-9]", "").trim();
+
+      if (!text.isEmpty()) {
+        ratingNumber = Integer.parseInt(text);
+      }
+    }
+
+    return ratingNumber;
+  }
 
 
-	private boolean isProductPage(Document doc) {
-		return doc.select("input[name=product]").first() != null;
-	}
-	
+  private boolean isProductPage(Document doc) {
+    return doc.select("input[name=product]").first() != null;
+  }
+
 }
