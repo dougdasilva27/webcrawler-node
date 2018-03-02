2b2163c5072d75f1e2c332580898594f3244dd28
Gabriel Dornelas <gabrieldornelas@lett.com.br>
2018-03-02 13:01:58 -0300

    Bugfix image zonasul
    Former-commit-id: 94ef5c3de3b8f5b6f3db97e1cbddc4434396ec83

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/riodejaneiro/RiodejaneiroZonasulCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/riodejaneiro/RiodejaneiroZonasulCrawler.java
@@ -5,12 +5,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.json.JSONArray;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import br.com.lett.crawlernode.core.models.Card;
 import br.com.lett.crawlernode.core.models.CategoryCollection;
 import br.com.lett.crawlernode.core.models.Product;
@@ -22,235 +20,224 @@ import models.Marketplace;
 import models.prices.Prices;
 
 public class RiodejaneiroZonasulCrawler extends Crawler {
-	
-	private final String HOME_PAGE = "https://www.zonasul.com.br/";
 
-	public RiodejaneiroZonasulCrawler(Session session) {
-		super(session);
-	}
+  private final String HOME_PAGE = "https://www.zonasul.com.br/";
 
-	@Override
-	public boolean shouldVisit() {
-		String href = this.session.getOriginalURL().toLowerCase();
-		return !FILTERS.matcher(href).matches() && (href.startsWith(HOME_PAGE));
-	}
+  public RiodejaneiroZonasulCrawler(Session session) {
+    super(session);
+  }
 
-	@Override
+  @Override
+  public boolean shouldVisit() {
+    String href = this.session.getOriginalURL().toLowerCase();
+    return !FILTERS.matcher(href).matches() && (href.startsWith(HOME_PAGE));
+  }
 
-	public List<Product> extractInformation(Document doc) throws Exception {
-		super.extractInformation(doc);
-		List<Product> products = new ArrayList<>();
+  @Override
 
-		if ( isProductPage(this.session.getOriginalURL()) ) {
-			Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
+  public List<Product> extractInformation(Document doc) throws Exception {
+    super.extractInformation(doc);
+    List<Product> products = new ArrayList<>();
 
-			// ID interno
-			String internalId = null;
-			Element elementID = doc.select(".codigo").first();
-			if (elementID != null) {
-				if(!elementID.text().trim().isEmpty()){
-					internalId = Integer.toString(Integer.parseInt(elementID.text().replaceAll("[^0-9,]+", ""))).trim();
-				}
-			}
+    if (isProductPage(this.session.getOriginalURL())) {
+      Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
 
-			// Pid
-			String internalPid = internalId;
+      // ID interno
+      String internalId = null;
+      Element elementID = doc.select(".codigo").first();
+      if (elementID != null) {
+        if (!elementID.text().trim().isEmpty()) {
+          internalId = Integer.toString(Integer.parseInt(elementID.text().replaceAll("[^0-9,]+", ""))).trim();
+        }
+      }
 
-			// Nome
-			String name = null;
-			Element elementName = doc.select(".top h3").first();
-			if (elementName != null) {
-				name = elementName.text().trim();
-			}
+      // Pid
+      String internalPid = internalId;
 
-			// Disponibilidade
-			boolean available = true;
-			Element elementUnavailable = doc.select("#ctl00_cphMasterPage1_pnlProdutoIndisponivel").first();
-			if (elementUnavailable != null) {
-				available = false;
-			}
+      // Nome
+      String name = null;
+      Element elementName = doc.select(".top h3").first();
+      if (elementName != null) {
+        name = elementName.text().trim();
+      }
 
-			// Preço
-			Float price = null;
-			if (available) {
-				Elements elementPrice = doc.select("#ctl00_cphMasterPage1_pnlPreco p.preco");
-				if(elementPrice.size() == 0) {
-					elementPrice = doc.select("#ctl00_cphMasterPage1_pnlPrecoRebaixa p.preco_por");
-				}
+      // Disponibilidade
+      boolean available = true;
+      Element elementUnavailable = doc.select("#ctl00_cphMasterPage1_pnlProdutoIndisponivel").first();
+      if (elementUnavailable != null) {
+        available = false;
+      }
 
-				try {
-					price = Float.parseFloat(elementPrice.first().text().replaceAll("[^0-9,]+", "").replaceAll("\\.", "").replaceAll(",", "."));
-				} catch (Exception e) {
-					Logging.printLogError(logger, session, "Preço não encontrado para o produto: " + this.session.getOriginalURL());
-				}
-			}
+      // Preço
+      Float price = null;
+      if (available) {
+        Elements elementPrice = doc.select("#ctl00_cphMasterPage1_pnlPreco p.preco");
+        if (elementPrice.size() == 0) {
+          elementPrice = doc.select("#ctl00_cphMasterPage1_pnlPrecoRebaixa p.preco_por");
+        }
 
-			// Categoires
-			CategoryCollection categories = crawlCategories(doc);
+        try {
+          price = Float.parseFloat(elementPrice.first().text().replaceAll("[^0-9,]+", "").replaceAll("\\.", "").replaceAll(",", "."));
+        } catch (Exception e) {
+          Logging.printLogError(logger, session, "Preço não encontrado para o produto: " + this.session.getOriginalURL());
+        }
+      }
 
-			String primaryImage = crawlPrimaryImage(doc);
-			String secondaryImages = crawlSecondaryImages(doc);
+      // Categoires
+      CategoryCollection categories = crawlCategories(doc);
 
-			// Descrição
-			String description = "";
+      String primaryImage = crawlPrimaryImage(doc);
+      String secondaryImages = crawlSecondaryImages(doc);
 
-			Element elementInfoUL = doc.select(".info ul").first();
-			if(elementInfoUL != null) 	{
-				description = description + elementInfoUL.html();
-			}
-			
-			Element elementInfoDET = doc.select(".info .det").first();
-			if(elementInfoDET != null) 	{
-				description = description + elementInfoDET.html();
-			}
+      // Descrição
+      String description = "";
 
-			// Estoque
-			Integer stock = null;
+      Element elementInfoUL = doc.select(".info ul").first();
+      if (elementInfoUL != null) {
+        description = description + elementInfoUL.html();
+      }
 
-			// Marketplace
-			Marketplace marketplace = new Marketplace();
+      Element elementInfoDET = doc.select(".info .det").first();
+      if (elementInfoDET != null) {
+        description = description + elementInfoDET.html();
+      }
 
-			// Prices
-			Prices prices = crawlPrices(doc, price);
-			
-			// Creating the product
-			Product product = ProductBuilder.create()
-					.setUrl(session.getOriginalURL())
-					.setInternalId(internalId)
-					.setInternalPid(internalPid)
-					.setName(name)
-					.setPrice(price)
-					.setPrices(prices)
-					.setAvailable(available)
-					.setCategory1(categories.getCategory(0))
-					.setCategory2(categories.getCategory(1))
-					.setCategory3(categories.getCategory(2))
-					.setPrimaryImage(primaryImage)
-					.setSecondaryImages(secondaryImages)
-					.setDescription(description)
-					.setStock(stock)
-					.setMarketplace(marketplace)
-					.build();
+      // Estoque
+      Integer stock = null;
 
-			products.add(product);
-			
-		} else {
-			Logging.printLogDebug(logger, session, "Not a product page" + this.session.getOriginalURL());
-		}
-		
-		return products;
-	}
-	
-	
-	/*******************************
-	 * Product page identification *
-	 *******************************/
+      // Marketplace
+      Marketplace marketplace = new Marketplace();
 
-	private boolean isProductPage(String url) {
-		return url.startsWith("https://www.zonasul.com.br/Produto/") || url.startsWith("http://www.zonasulatende.com.br/Produto/");
-	}
-	
-	private String crawlPrimaryImage(Document doc) {
-		String primaryImage = null;
-		Element elementPrimaryImage = doc.select(".img_principal").first();
-		if (elementPrimaryImage != null) {
-			String onClickAttr = elementPrimaryImage.attr("onclick");
-			
-			int beginIndex = onClickAttr.indexOf('\'');
-			int endIndex = onClickAttr.lastIndexOf('\'');
-			
-			primaryImage = onClickAttr.substring(beginIndex + 1, endIndex);
-		}
-		
-		return primaryImage;
-	}
-	
-	private String crawlSecondaryImages(Document doc) throws MalformedURLException {
-		String primaryImage = crawlPrimaryImage(doc);
-		String secondaryImages = null;
+      // Prices
+      Prices prices = crawlPrices(doc, price);
 
-		JSONArray secondaryImagesArray = new JSONArray();
-		Elements elementSecondaryImage = doc.select("#ctl00_cphMasterPage1_dlFotos td > a");
+      // Creating the product
+      Product product = ProductBuilder.create().setUrl(session.getOriginalURL()).setInternalId(internalId).setInternalPid(internalPid).setName(name)
+          .setPrice(price).setPrices(prices).setAvailable(available).setCategory1(categories.getCategory(0)).setCategory2(categories.getCategory(1))
+          .setCategory3(categories.getCategory(2)).setPrimaryImage(primaryImage).setSecondaryImages(secondaryImages).setDescription(description)
+          .setStock(stock).setMarketplace(marketplace).build();
 
-		if (elementSecondaryImage.size() > 1) {
-			for(Element e : elementSecondaryImage) {
-				String onClickAttr = e.attr("onclick");
-				
-				int beginIndex = onClickAttr.indexOf('\'');
-				int endIndex = onClickAttr.lastIndexOf('\'');
-				
-				String imageUrl = onClickAttr.substring(beginIndex + 1, endIndex);
-				
-				if (imageUrl != null && !imageUrl.equals(primaryImage)) {
-					
-					String primaryImageSize = parseImageDimensions(primaryImage);
-					if (primaryImageSize != null) {
-						imageUrl = imageUrl.replaceFirst("280_280", primaryImageSize);
-					}
-					
-					if (!imageUrl.equals(primaryImage)) { // check again, because now the imageUrl could have been modified
-						secondaryImagesArray.put(imageUrl);
-					}
-				}
-			}
-		}
-		
-		if(secondaryImagesArray.length() > 0) {
-			secondaryImages = secondaryImagesArray.toString();
-		}
-		
-		return secondaryImages;
-	}
-	
-	private String parseImageDimensions(String imageUrl) {
-		String[] tokens = imageUrl.split("/"); // https://www.zonasul.com.br/ImgProdutos/280_280/2015422_37827.jpg
-		if (tokens.length >= 5) {
-			return tokens[4];
-		}
-		return null;
-	}
-	
-	/**
-	 * In this market, installments not appear in product page
-	 * 
-	 * @param doc
-	 * @param price
-	 * @return
-	 */
-	private Prices crawlPrices(Document doc, Float price){
-		Prices prices = new Prices();
+      products.add(product);
 
-		if(price != null){
-			Map<Integer,Float> installmentPriceMap = new HashMap<>();
+    } else {
+      Logging.printLogDebug(logger, session, "Not a product page" + this.session.getOriginalURL());
+    }
 
-			installmentPriceMap.put(1, price);
-			prices.setBankTicketPrice(price);
+    return products;
+  }
 
-			prices.insertCardInstallment(Card.VISA.toString(), installmentPriceMap);
-			prices.insertCardInstallment(Card.MASTERCARD.toString(), installmentPriceMap);
-			prices.insertCardInstallment(Card.AMEX.toString(), installmentPriceMap);
-			prices.insertCardInstallment(Card.DINERS.toString(), installmentPriceMap);
-			prices.insertCardInstallment(Card.ELO.toString(), installmentPriceMap);
-			prices.insertCardInstallment(Card.SHOP_CARD.toString(), installmentPriceMap);
-		}
 
-		return prices;
-	}
-	
-	/**
-	 * No momento que o crawler foi feito não foi achado
-	 * produtos com categorias
-	 * @param document
-	 * @return
-	 */
-	private CategoryCollection crawlCategories(Document doc) {
-		CategoryCollection categories = new CategoryCollection();
-		Element elementCategories = doc.select(".nomeComTopo h2").first();
-		
-		if(elementCategories != null) {
-			categories.add(elementCategories.ownText());
-		}
+  /*******************************
+   * Product page identification *
+   *******************************/
 
-		return categories;
-	}
+  private boolean isProductPage(String url) {
+    return url.startsWith("https://www.zonasul.com.br/Produto/") || url.startsWith("http://www.zonasulatende.com.br/Produto/");
+  }
+
+  private String crawlPrimaryImage(Document doc) {
+    String primaryImage = null;
+    Element elementPrimaryImage = doc.select(".img_principal").first();
+    if (elementPrimaryImage != null) {
+      String onClickAttr = elementPrimaryImage.attr("onclick").trim();
+
+      if (!onClickAttr.isEmpty() && onClickAttr.contains("'")) {
+        int beginIndex = onClickAttr.indexOf('\'');
+        int endIndex = onClickAttr.lastIndexOf('\'');
+
+        primaryImage = onClickAttr.substring(beginIndex + 1, endIndex);
+      }
+    }
+
+    return primaryImage;
+  }
+
+  private String crawlSecondaryImages(Document doc) throws MalformedURLException {
+    String primaryImage = crawlPrimaryImage(doc);
+    String secondaryImages = null;
+
+    JSONArray secondaryImagesArray = new JSONArray();
+    Elements elementSecondaryImage = doc.select("#ctl00_cphMasterPage1_dlFotos td > a");
+
+    if (elementSecondaryImage.size() > 1) {
+      for (Element e : elementSecondaryImage) {
+        String onClickAttr = e.attr("onclick");
+
+        int beginIndex = onClickAttr.indexOf('\'');
+        int endIndex = onClickAttr.lastIndexOf('\'');
+
+        String imageUrl = onClickAttr.substring(beginIndex + 1, endIndex);
+
+        if (imageUrl != null && !imageUrl.equals(primaryImage)) {
+
+          String primaryImageSize = parseImageDimensions(primaryImage);
+          if (primaryImageSize != null) {
+            imageUrl = imageUrl.replaceFirst("280_280", primaryImageSize);
+          }
+
+          if (!imageUrl.equals(primaryImage)) { // check again, because now the imageUrl could have been modified
+            secondaryImagesArray.put(imageUrl);
+          }
+        }
+      }
+    }
+
+    if (secondaryImagesArray.length() > 0) {
+      secondaryImages = secondaryImagesArray.toString();
+    }
+
+    return secondaryImages;
+  }
+
+  private String parseImageDimensions(String imageUrl) {
+    String[] tokens = imageUrl.split("/"); // https://www.zonasul.com.br/ImgProdutos/280_280/2015422_37827.jpg
+    if (tokens.length >= 5) {
+      return tokens[4];
+    }
+    return null;
+  }
+
+  /**
+   * In this market, installments not appear in product page
+   * 
+   * @param doc
+   * @param price
+   * @return
+   */
+  private Prices crawlPrices(Document doc, Float price) {
+    Prices prices = new Prices();
+
+    if (price != null) {
+      Map<Integer, Float> installmentPriceMap = new HashMap<>();
+
+      installmentPriceMap.put(1, price);
+      prices.setBankTicketPrice(price);
+
+      prices.insertCardInstallment(Card.VISA.toString(), installmentPriceMap);
+      prices.insertCardInstallment(Card.MASTERCARD.toString(), installmentPriceMap);
+      prices.insertCardInstallment(Card.AMEX.toString(), installmentPriceMap);
+      prices.insertCardInstallment(Card.DINERS.toString(), installmentPriceMap);
+      prices.insertCardInstallment(Card.ELO.toString(), installmentPriceMap);
+      prices.insertCardInstallment(Card.SHOP_CARD.toString(), installmentPriceMap);
+    }
+
+    return prices;
+  }
+
+  /**
+   * No momento que o crawler foi feito não foi achado produtos com categorias
+   * 
+   * @param document
+   * @return
+   */
+  private CategoryCollection crawlCategories(Document doc) {
+    CategoryCollection categories = new CategoryCollection();
+    Element elementCategories = doc.select(".nomeComTopo h2").first();
+
+    if (elementCategories != null) {
+      categories.add(elementCategories.ownText());
+    }
+
+    return categories;
+  }
 }
