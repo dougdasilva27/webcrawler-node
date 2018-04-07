296fe6ae3d7242e107e12a9779f4eeb935002748
Gabriel Dornelas <gabrieldornelas@lett.com.br>
2018-04-07 12:38:07 -0300

    Adding a try catch block when webdriver is terminated [deploy:Production]
    Former-commit-id: 1a6848d91d294c285c39871054e45e3cabcb78f4

--- src/java/br/com/lett/crawlernode/core/fetcher/CrawlerWebdriver.java
+++ src/java/br/com/lett/crawlernode/core/fetcher/CrawlerWebdriver.java
@@ -2,7 +2,6 @@ package br.com.lett.crawlernode.core.fetcher;
 
 import java.io.File;
 import java.util.List;
-
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -16,196 +15,200 @@ import org.openqa.selenium.remote.Augmenter;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.session.crawler.TestCrawlerSession;
 import br.com.lett.crawlernode.core.session.ranking.TestRankingSession;
 import br.com.lett.crawlernode.main.Main;
+import br.com.lett.crawlernode.util.CommonMethods;
 import br.com.lett.crawlernode.util.Logging;
 
 /**
- * This class encapsulates an instance of a Remote WebDriver
- * that uses a PhantomJS as backend for Selenium WebDriver.
- * This class also provide methods to manipulate web elements
- * and take web pages screenshots.
+ * This class encapsulates an instance of a Remote WebDriver that uses a PhantomJS as backend for
+ * Selenium WebDriver. This class also provide methods to manipulate web elements and take web pages
+ * screenshots.
  *
  * @author Samir Leao
  * 
  */
 public class CrawlerWebdriver {
-	
-	protected static final Logger logger = LoggerFactory.getLogger(CrawlerWebdriver.class);
-	
-	public static final String PHANTOMJS_EXECUTABLE_PATH = "/home/samirleao/Downloads/phantomjs-2.1.1-linux-i686/bin/phantomjs"; 
 
-	public WebDriver driver;
+  protected static final Logger logger = LoggerFactory.getLogger(CrawlerWebdriver.class);
 
-	private Session session;
-		
-	public CrawlerWebdriver(DesiredCapabilities caps, Session session) {
-		driver = new  PhantomJSDriver(caps);
-		this.session = session;	
-	}
+  public static final String PHANTOMJS_EXECUTABLE_PATH = "/home/samirleao/Downloads/phantomjs-2.1.1-linux-i686/bin/phantomjs";
 
-//	public void addHeaders(Map<String, String> headers) {
-//		driver.get("chrome-extension://idgpnmonknjnojddfkpgkljpfnnfcklj/icon.png");
-//		
-//		StringBuilder headersOptions = new StringBuilder();
-//		for (Entry<String, String> entry : headers.entrySet()) {
-//			headersOptions.append("    {enabled: true, name: '" + entry.getKey() + "', value: '" + entry.getValue() + "', comment: ''}, ");
-//		}
-//		
-//		((JavascriptExecutor)driver).executeScript(
-//				"localStorage.setItem('profiles', JSON.stringify([{                " +
-//						"  title: 'Selenium', hideComment: true, appendMode: '',           " +
-//						"  headers: [                                                      " +
-//						headersOptions.toString()											 +
-//						"  ],                                                              " +
-//						"  respHeaders: [],                                                " +
-//						"  filters: []                                                     " +
-//				"}]));");
-//		
-//	}
-	
-//	public void addCookie(String url) {
-//		try {
-//			URI uri = new URI(url);			
-//			Cookie c = new Cookie("x-a", "xulambis", "." + uri.getHost(), "/", null);
-//			driver.manage().addCookie(c);
-//		} catch (Exception e) {
-//			Logging.printLogError(logger, session, "Cookie could not be set.");
-//			Logging.printLogError(logger, session, CommonMethods.getStackTraceString(e));
-//		}
-//	}
+  public WebDriver driver;
 
-	public WebElement findElementByCssSelector(String selector) {
-		return driver.findElement(By.cssSelector(selector));
-	}
+  private Session session;
 
-	public List<WebElement> findElementsByCssSelector(String selector) {
-		return driver.findElements(By.cssSelector(selector));
-	}
+  public CrawlerWebdriver(DesiredCapabilities caps, Session session) {
+    driver = new PhantomJSDriver(caps);
+    this.session = session;
+  }
 
-	/**
-	 * Get the html source of the current page loaded in the webdriver.
-	 * 
-	 * @return
-	 */
-	public String getCurrentPageSource() {
-		return driver.getPageSource();
-	}
+  // public void addHeaders(Map<String, String> headers) {
+  // driver.get("chrome-extension://idgpnmonknjnojddfkpgkljpfnnfcklj/icon.png");
+  //
+  // StringBuilder headersOptions = new StringBuilder();
+  // for (Entry<String, String> entry : headers.entrySet()) {
+  // headersOptions.append(" {enabled: true, name: '" + entry.getKey() + "', value: '" +
+  // entry.getValue() + "', comment: ''}, ");
+  // }
+  //
+  // ((JavascriptExecutor)driver).executeScript(
+  // "localStorage.setItem('profiles', JSON.stringify([{ " +
+  // " title: 'Selenium', hideComment: true, appendMode: '', " +
+  // " headers: [ " +
+  // headersOptions.toString() +
+  // " ], " +
+  // " respHeaders: [], " +
+  // " filters: [] " +
+  // "}]));");
+  //
+  // }
 
-	/**
-	 * Loads a webpage without any explicit wait.
-	 * 
-	 * @param url
-	 * @return
-	 */
-	public String loadUrl(String url) {
-		driver.get(url);
+  // public void addCookie(String url) {
+  // try {
+  // URI uri = new URI(url);
+  // Cookie c = new Cookie("x-a", "xulambis", "." + uri.getHost(), "/", null);
+  // driver.manage().addCookie(c);
+  // } catch (Exception e) {
+  // Logging.printLogError(logger, session, "Cookie could not be set.");
+  // Logging.printLogError(logger, session, CommonMethods.getStackTraceString(e));
+  // }
+  // }
 
-		return driver.getPageSource();
-	}
+  public WebElement findElementByCssSelector(String selector) {
+    return driver.findElement(By.cssSelector(selector));
+  }
 
-	/**
-	 * Loads a webpage and wait for some time.
-	 * 
-	 * @param url
-	 * @param waitTime
-	 * @return A String containing the page html
-	 */
-	public String loadUrl(String url, int waitTime) {
-		this.driver.get(url);
-		this.waitLoad(waitTime);
+  public List<WebElement> findElementsByCssSelector(String selector) {
+    return driver.findElements(By.cssSelector(selector));
+  }
 
-		return this.driver.getPageSource();
-	}
+  /**
+   * Get the html source of the current page loaded in the webdriver.
+   * 
+   * @return
+   */
+  public String getCurrentPageSource() {
+    return driver.getPageSource();
+  }
 
-	public void waitLoad(int time) {
-		try {
-			Thread.sleep(time);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+  /**
+   * Loads a webpage without any explicit wait.
+   * 
+   * @param url
+   * @return
+   */
+  public String loadUrl(String url) {
+    driver.get(url);
 
-	public Actions getActionsBuilder() {
-		return new Actions(driver);
-	}
+    return driver.getPageSource();
+  }
 
-	public WebElement executeJavascript(String javascript) {
-		JavascriptExecutor jse = (JavascriptExecutor) driver;
-		return (WebElement)jse.executeScript(javascript);
-	}
+  /**
+   * Loads a webpage and wait for some time.
+   * 
+   * @param url
+   * @param waitTime
+   * @return A String containing the page html
+   */
+  public String loadUrl(String url, int waitTime) {
+    this.driver.get(url);
+    this.waitLoad(waitTime);
 
-	public void clickOnElementViaJavascript(WebElement element) {
-		JavascriptExecutor jse = (JavascriptExecutor) driver;
-		jse.executeScript("arguments[0].click();", element);
-	}
+    return this.driver.getPageSource();
+  }
 
-	/**
-	 * Get the current loaded page on the webdriver instance.
-	 * 
-	 * @return
-	 */
-	public String getCurURL() {
-		return driver.getCurrentUrl();
-	}
+  public void waitLoad(int time) {
+    try {
+      Thread.sleep(time);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
 
-	/**
-	 * Terminate the web driver.
-	 */
-	public void terminate() {
-		driver.close();
-		driver.quit();
-		if (!(session instanceof TestCrawlerSession) && !(session instanceof TestRankingSession)) {
-			Main.server.decrementWebdriverInstances();
-		}
-	}
+  public Actions getActionsBuilder() {
+    return new Actions(driver);
+  }
 
-	/**
-	 * Get a screenshot from a webpage.
-	 * 
-	 * @param url
-	 * @return
-	 */
-	public File takeScreenshot(String url) {
-		driver.get(url);
-		File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		return screenshot;
-	}
+  public WebElement executeJavascript(String javascript) {
+    JavascriptExecutor jse = (JavascriptExecutor) driver;
+    return (WebElement) jse.executeScript(javascript);
+  }
 
-	/**
-	 * Get a screenshot from a webpage and save the file.
-	 * 
-	 * @param url
-	 * @param path the path where the screenshot will be saved
-	 */
-	public void takeScreenshot(String url, String path) {
-		driver.get(url);
-		File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		try {
-			FileUtils.copyFile(screenshot, new File(path));
-		} catch (Exception ex) {
-			Logging.printLogError(logger, session, "Error saving screenshot! [" + ex.getMessage() + "]");
-		}
-	}
+  public void clickOnElementViaJavascript(WebElement element) {
+    JavascriptExecutor jse = (JavascriptExecutor) driver;
+    jse.executeScript("arguments[0].click();", element);
+  }
 
-	/**
-	 * Get a screenshot from the current loaded webpage and save the file.
-	 * 
-	 * @param url
-	 * @param path the path where the screenshot will be saved
-	 */
-	public void takeScreenshotFromCurrentLoadedPage(String path) {
-		Augmenter augmenter = new Augmenter();
-		File screenshot = ((TakesScreenshot)augmenter.augment(driver)).getScreenshotAs(OutputType.FILE);
-		//File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		try {
-			FileUtils.copyFile(screenshot, new File(path));
-		} catch (Exception ex) {
-			System.err.println("Error saving screenshot! [" + ex.getMessage() + "]");
-		}
-	}
+  /**
+   * Get the current loaded page on the webdriver instance.
+   * 
+   * @return
+   */
+  public String getCurURL() {
+    return driver.getCurrentUrl();
+  }
+
+  /**
+   * Terminate the web driver.
+   */
+  public void terminate() {
+    try {
+      driver.close();
+      driver.quit();
+      if (!(session instanceof TestCrawlerSession) && !(session instanceof TestRankingSession)) {
+        Main.server.decrementWebdriverInstances();
+      }
+    } catch (Exception e) {
+      Logging.printLogError(logger, session, CommonMethods.getStackTrace(e));
+    }
+  }
+
+  /**
+   * Get a screenshot from a webpage.
+   * 
+   * @param url
+   * @return
+   */
+  public File takeScreenshot(String url) {
+    driver.get(url);
+    File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+    return screenshot;
+  }
+
+  /**
+   * Get a screenshot from a webpage and save the file.
+   * 
+   * @param url
+   * @param path the path where the screenshot will be saved
+   */
+  public void takeScreenshot(String url, String path) {
+    driver.get(url);
+    File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+    try {
+      FileUtils.copyFile(screenshot, new File(path));
+    } catch (Exception ex) {
+      Logging.printLogError(logger, session, "Error saving screenshot! [" + ex.getMessage() + "]");
+    }
+  }
+
+  /**
+   * Get a screenshot from the current loaded webpage and save the file.
+   * 
+   * @param url
+   * @param path the path where the screenshot will be saved
+   */
+  public void takeScreenshotFromCurrentLoadedPage(String path) {
+    Augmenter augmenter = new Augmenter();
+    File screenshot = ((TakesScreenshot) augmenter.augment(driver)).getScreenshotAs(OutputType.FILE);
+    // File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+    try {
+      FileUtils.copyFile(screenshot, new File(path));
+    } catch (Exception ex) {
+      System.err.println("Error saving screenshot! [" + ex.getMessage() + "]");
+    }
+  }
 
 }
