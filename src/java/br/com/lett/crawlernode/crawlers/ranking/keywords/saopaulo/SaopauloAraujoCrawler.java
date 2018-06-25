1a72a952b7a1cc0f483be8145f98324ff1b0ba7d
Gabriel Dornelas <gabrieldornelas@lett.com.br>
2018-06-25 14:46:05 -0300

    bugfix ranking araujo [deploy:Production]
    Former-commit-id: 485586c402ebee99f56287f45148c36479978adb

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/saopaulo/SaopauloAraujoCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/saopaulo/SaopauloAraujoCrawler.java
@@ -2,118 +2,92 @@ package br.com.lett.crawlernode.crawlers.ranking.keywords.saopaulo;
 
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.util.CommonMethods;
 
 public class SaopauloAraujoCrawler extends CrawlerRankingKeywords {
 
-	public SaopauloAraujoCrawler(Session session) {
-		super(session);
-	}
+  public SaopauloAraujoCrawler(Session session) {
+    super(session);
+  }
 
-	private String crawlInternalId(Element e) {
-		String internalId = null;
+  @Override
+  protected void extractProductsFromCurrentPage() {
+    this.pageSize = 32;
 
-		return internalId;
-	}
+    String key = this.keywordWithoutAccents.replaceAll(" ", "%20");
 
-	private String crawlInternalPid(Element e) {
-		String internalPid = null;
-		Element pid = e.select("input.qd_cpProdId.qd-productId").first();
+    this.log("Página " + this.currentPage);
+    String url = "http://busca.araujo.com.br/busca?q=" + key + "&page=" + this.currentPage;
+    this.log("Link onde são feitos os crawlers: " + url);
 
-		if (pid != null) {
-			internalPid = pid.attr("value");
-		}
+    this.currentDoc = fetchDocument(url);
 
-		return internalPid;
-	}
+    Elements products = this.currentDoc.select(".neemu-products-container .nm-product-item");
 
-	private String crawlProductUrl(Element e) {
-		String urlProduct = null;
-		Element urlElement = e.select("a.qd_cpProductLink").first();
+    if (!products.isEmpty()) {
+      if (this.totalProducts == 0) {
+        setTotalProducts();
+      }
 
-		if (urlElement != null) {
-			urlProduct = urlElement.attr("href");
-		}
+      for (Element e : products) {
+        String internalPid = crawlInternalPid(e);
+        String productUrl = crawlProductUrl(e);
 
-		return urlProduct;
-	}
+        saveDataProduct(null, internalPid, productUrl);
 
-	@Override
-	protected void extractProductsFromCurrentPage() {
-		// número de produtos por página do market
-		this.pageSize = 32;
+        this.log("Position: " + this.position + " - InternalId: " + null + " - InternalPid: " + internalPid + " - Url: " + productUrl);
+        if (this.arrayProducts.size() == productsLimit)
+          break;
+      }
+    } else {
+      this.result = false;
+      this.log("Keyword sem resultado!");
+    }
 
-		String key = this.keywordWithoutAccents.replaceAll(" ", "%20");
-		;
+    this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
 
-		this.log("Página " + this.currentPage);
-		// monta a url com a keyword e a página
-		String url = "http://busca.araujo.com.br/busca?q=" + key + "&page=" + this.currentPage;
-		this.log("Link onde são feitos os crawlers: " + url);
+  }
 
-		// chama função de pegar a url
-		this.currentDoc = fetchDocument(url);
+  @Override
+  protected void setTotalProducts() {
+    Element totalElement = this.currentDoc.select(".neemu-total-products-container").first();
 
-		Elements products = this.currentDoc.select("div.prateleira ul li[layout]");
+    if (totalElement != null) {
+      String total = totalElement.ownText().replaceAll("[^0-9]", "").trim();
 
-		// se obter 1 ou mais links de produtos e essa página tiver resultado
-		// faça:
-		if (products.size() >= 1) {
-			// se o total de busca não foi setado ainda, chama a função para
-			// setar
-			if (this.totalProducts == 0)
-				setTotalProducts();
+      if (!total.isEmpty()) {
+        this.totalProducts = Integer.parseInt(total);
+      }
+    }
 
-			for (Element e : products) {
-				// InternalPid
-				String internalPid = crawlInternalPid(e);
+    this.log("Total da busca: " + this.totalProducts);
+  }
 
-				// InternalId
-				String internalId = crawlInternalId(e);
+  private String crawlInternalPid(Element e) {
+    String internalPid = null;
 
-				// Url do produto
-				String productUrl = crawlProductUrl(e);
+    String text = e.attr("id");
+    if (text.contains("-")) {
+      internalPid = CommonMethods.getLast(text.split("-"));
+    }
 
-				saveDataProduct(internalId, internalPid, productUrl);
+    return internalPid;
+  }
 
-				this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: "
-						+ internalPid + " - Url: " + productUrl);
-				if (this.arrayProducts.size() == productsLimit)
-					break;
-			}
-		} else {
-			this.result = false;
-			this.log("Keyword sem resultado!");
-		}
+  private String crawlProductUrl(Element e) {
+    String productUrl = null;
+    Element urlElement = e.select(".nm-product-name > a").first();
 
-		this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora "
-				+ this.arrayProducts.size() + " produtos crawleados");
+    if (urlElement != null) {
+      productUrl = urlElement.attr("href");
 
-	}
+      if (!productUrl.startsWith("http")) {
+        productUrl = "https:" + productUrl;
+      }
+    }
 
-	@Override
-	protected boolean hasNextPage() {
-		Elements id = this.currentDoc.select("div.prateleira ul li[layout]");
-
-		if (id.size() >= 32)
-			return true;
-		else
-			return false;
-	}
-
-	@Override
-	protected void setTotalProducts() {
-		Element totalElement = this.currentDoc.select("div#neemu-total-products-container span.value").first();
-
-		try {
-			if (totalElement != null)
-				this.totalProducts = Integer.parseInt(totalElement.text());
-		} catch (Exception e) {
-			this.logError(e.getMessage());
-		}
-
-		this.log("Total da busca: " + this.totalProducts);
-	}
+    return productUrl;
+  }
 }
