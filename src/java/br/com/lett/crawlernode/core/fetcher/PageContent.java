711287cb96a506fb4da186ee96a4c07c4490723a
Gabriel Dornelas <gabrieldornelas@lett.com.br>
2018-06-27 17:54:21 -0300

    new crawler la anonima
    Former-commit-id: 5546e21537b3d268624673c2c4b0d7c131c241aa

--- src/java/br/com/lett/crawlernode/core/fetcher/PageContent.java
+++ src/java/br/com/lett/crawlernode/core/fetcher/PageContent.java
@@ -2,175 +2,173 @@ package br.com.lett.crawlernode.core.fetcher;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
-
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.entity.ContentType;
 import org.apache.http.util.EntityUtils;
-
 import br.com.lett.crawlernode.core.parser.HtmlParseData;
 import br.com.lett.crawlernode.core.parser.TextParseData;
 
 /**
- * This class contains the content fetched from a http request and
- * also it's parsed content.
+ * This class contains the content fetched from a http request and also it's parsed content.
  * 
  * @author Samir Leão
  *
  */
 public class PageContent {
-	
-	/**
-	 * The url
-	 */
-	private String url;
 
-	/**
-	 * The content of this page in binary format.
-	 */
-	private byte[] contentData;
+  /**
+   * The url
+   */
+  private String url;
 
-	/**
-	 * The ContentType of this page.
-	 * For example: "text/html; charset=UTF-8"
-	 */
-	private String contentType;
+  /**
+   * The content of this page in binary format.
+   */
+  private byte[] contentData;
 
-	/**
-	 * The encoding of the content.
-	 * For example: "gzip"
-	 */
-	private String contentEncoding;
+  /**
+   * The ContentType of this page. For example: "text/html; charset=UTF-8"
+   */
+  private String contentType;
 
-	/**
-	 * The charset of the content.
-	 * For example: "UTF-8"
-	 */
-	private String contentCharset;
-	
-	/**
-	 * Language of content
-	 */
-	private String language;
-	
-	/**
-	 * Status code
-	 */
-	private int statusCode;
-	
-	/**
-	 * Content parsed as html
-	 */
-	private HtmlParseData htmlParseData;
-	
-	/**
-	 * Content parsed as plain text
-	 */
-	private TextParseData textParseData;
+  /**
+   * The encoding of the content. For example: "gzip"
+   */
+  private String contentEncoding;
 
-	public PageContent(HttpEntity entity) throws IOException {
-		
-		// setting content type
-		setContentType(null);
-		Header type = entity.getContentType();
-		if (type != null) {
-			setContentType(type.getValue());
-		}
-		
-		// setting content encoding
-		setContentEncoding(null);
-		Header encoding = entity.getContentEncoding();
-		if (encoding != null) {
-			setContentEncoding(encoding.getValue());
-		}
-		
-		// setting charset
-		Charset charset = ContentType.getOrDefault(entity).getCharset();
-		if (charset != null) {
-			setContentCharset(charset.displayName());
-		}
-		
-		// setting content data
-		setContentData(EntityUtils.toByteArray(entity));
-		
-	}
+  /**
+   * The charset of the content. For example: "UTF-8"
+   */
+  private String contentCharset;
 
-	public byte[] getContentData() {
-		return contentData;
-	}
+  /**
+   * Language of content
+   */
+  private String language;
 
-	public void setContentData(byte[] contentData) {
-		this.contentData = contentData;
-	}
+  /**
+   * Status code
+   */
+  private int statusCode;
 
+  /**
+   * Content parsed as html
+   */
+  private HtmlParseData htmlParseData;
 
-	public String getContentType() {
-		return contentType;
-	}
+  /**
+   * Content parsed as plain text
+   */
+  private TextParseData textParseData;
+
+  public PageContent(HttpEntity entity) throws IOException {
+
+    // setting content type
+    setContentType(null);
+    Header type = entity.getContentType();
+    if (type != null) {
+      setContentType(type.getValue());
+    }
+
+    // setting content encoding
+    setContentEncoding(null);
+    Header encoding = entity.getContentEncoding();
+    if (encoding != null) {
+      setContentEncoding(encoding.getValue());
+    }
+
+    String contentTypeString = entity.getContentType().getValue();
+
+    if (!contentTypeString.contains("charset=none")) {
+      // setting charset
+      Charset charset = ContentType.getOrDefault(entity).getCharset();
+      if (charset != null) {
+        setContentCharset(charset.displayName());
+      }
+    }
+
+    // setting content data
+    setContentData(EntityUtils.toByteArray(entity));
+
+  }
+
+  public byte[] getContentData() {
+    return contentData;
+  }
+
+  public void setContentData(byte[] contentData) {
+    this.contentData = contentData;
+  }
 
 
-	public void setContentType(String contentType) {
-		this.contentType = contentType;
-	}
+  public String getContentType() {
+    return contentType;
+  }
 
 
-	public String getContentEncoding() {
-		return contentEncoding;
-	}
+  public void setContentType(String contentType) {
+    this.contentType = contentType;
+  }
 
 
-	public void setContentEncoding(String contentEncoding) {
-		this.contentEncoding = contentEncoding;
-	}
+  public String getContentEncoding() {
+    return contentEncoding;
+  }
 
 
-	public String getContentCharset() {
-		return contentCharset;
-	}
+  public void setContentEncoding(String contentEncoding) {
+    this.contentEncoding = contentEncoding;
+  }
 
 
-	public void setContentCharset(String contentCharset) {
-		this.contentCharset = contentCharset;
-	}
+  public String getContentCharset() {
+    return contentCharset;
+  }
 
-	public String getUrl() {
-		return url;
-	}
 
-	public void setUrl(String url) {
-		this.url = url;
-	}
+  public void setContentCharset(String contentCharset) {
+    this.contentCharset = contentCharset;
+  }
 
-	public String getLanguage() {
-		return language;
-	}
+  public String getUrl() {
+    return url;
+  }
 
-	public void setLanguage(String language) {
-		this.language = language;
-	}
+  public void setUrl(String url) {
+    this.url = url;
+  }
 
-	public int getStatusCode() {
-		return statusCode;
-	}
+  public String getLanguage() {
+    return language;
+  }
 
-	public void setStatusCode(int statusCode) {
-		this.statusCode = statusCode;
-	}
+  public void setLanguage(String language) {
+    this.language = language;
+  }
 
-	public TextParseData getTextParseData() {
-		return textParseData;
-	}
+  public int getStatusCode() {
+    return statusCode;
+  }
 
-	public void setTextParseData(TextParseData textParseData) {
-		this.textParseData = textParseData;
-	}
+  public void setStatusCode(int statusCode) {
+    this.statusCode = statusCode;
+  }
 
-	public HtmlParseData getHtmlParseData() {
-		return htmlParseData;
-	}
+  public TextParseData getTextParseData() {
+    return textParseData;
+  }
 
-	public void setHtmlParseData(HtmlParseData htmlParseData) {
-		this.htmlParseData = htmlParseData;
-	}
+  public void setTextParseData(TextParseData textParseData) {
+    this.textParseData = textParseData;
+  }
+
+  public HtmlParseData getHtmlParseData() {
+    return htmlParseData;
+  }
+
+  public void setHtmlParseData(HtmlParseData htmlParseData) {
+    this.htmlParseData = htmlParseData;
+  }
 
 }
