28511f126de61ccd879b834b2b5a017cc28bd2cb
samirleao <samirleao@192.168.0.26>
2018-06-29 17:14:32 -0300

    - Crawler Carrefour Argentna (in-progress)
    Former-commit-id: d3ab3d31e496f26dbe15b3fa43e2b2528be35d64

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/argentina/ArgentinaCarrefourCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/argentina/ArgentinaCarrefourCrawler.java
@@ -3,11 +3,12 @@ package br.com.lett.crawlernode.crawlers.corecontent.argentina;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang3.tuple.Pair;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.remote.JsonException;
 
 import br.com.lett.crawlernode.core.models.CategoryCollection;
 import br.com.lett.crawlernode.core.models.Product;
@@ -41,12 +42,13 @@ public class ArgentinaCarrefourCrawler extends Crawler {
 
 		if (isProductPage(doc)) {
 			Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
-			
-			JSONObject dataLayer = parseDataLayer(doc);
-			
-			System.out.println(dataLayer);
 
-			String internalId = crawlInternalId(doc);
+			JSONObject dataLayer = parseDataLayer(doc);
+			JSONObject dataBanking = parseDataBanking(doc);
+
+			System.out.println(dataBanking);
+
+			String internalId = crawlInternalId(dataLayer);
 			String internalPid = crawlInternalPid(session.getOriginalURL());
 			String name = crawlName(doc);
 			Float price = crawlPrice(doc);
@@ -58,6 +60,17 @@ public class ArgentinaCarrefourCrawler extends Crawler {
 			String description = crawlDescription(doc);
 			Integer stock = null;
 			Marketplace marketplace = crawlMarketplace();
+
+			//				System.out.println("InternalId: " + internalId);
+			//				System.out.println("InternalPid: " + internalPid);
+			//				System.out.println("Name: " + name);
+			//				System.out.println("Available: " +   available);
+			//				System.out.println("Price: " + price);
+			//				System.out.println("Categories: " + categories.getCategory(0) + " > " + categories.getCategory(1) + " > " + categories.getCategory(2));
+			//				System.out.println("Primary Image: " + primaryImage);
+			//				System.out.println("Secondary Images: " + secondaryImages);
+			//				System.out.println("Marketplace: " + marketplace);
+			//				System.out.println("Description: " + description);
 
 			// Creating the product
 			Product product = ProductBuilder.create()
@@ -78,6 +91,8 @@ public class ArgentinaCarrefourCrawler extends Crawler {
 					.setMarketplace(marketplace)
 					.build();
 
+
+
 			products.add(product);
 
 		} else {
@@ -93,42 +108,79 @@ public class ArgentinaCarrefourCrawler extends Crawler {
 	}
 
 	private String crawlDescription(Document doc) {
-		// TODO Auto-generated method stub
-		return null;
+		StringBuilder description = new StringBuilder();
+		Elements descriptionElements = doc.select("div.product-collateral #collateral-tabs dd.tab-container");
+		for (Element e : descriptionElements) {
+			description.append(e.text().trim());
+		}
+		return description.toString();
 	}
 
 	private String crawlSecondaryImages(Document doc) {
-		// TODO Auto-generated method stub
-		return null;
+		Elements elements = doc.select("div.product-image-gallery .gallery-image[data-zoom-image]");
+		JSONArray images = new JSONArray();
+		if (elements.size() > 1) {
+			for (int i = 1; i < elements.size(); i++) {
+				String imageUrl = elements.get(i).attr("data-zoom-image");
+				images.put(imageUrl);
+			}
+		}
+		return images.toString();
 	}
 
 	private String crawlPrimaryImage(Document doc) {
-		// TODO Auto-generated method stub
+		Elements elements = doc.select("div.product-image-gallery .gallery-image[data-zoom-image]");
+		if (!elements.isEmpty()) {
+			return elements.get(0).attr("data-zoom-image");
+		}
 		return null;
 	}
 
 	private CategoryCollection crawlCategories(Document doc) {
-		// TODO Auto-generated method stub
-		return null;
+		return new CategoryCollection();
 	}
 
 	private boolean crawlAvailability(Document doc) {
-		// TODO Auto-generated method stub
-		return false;
+		if (doc.select("div.add-to-cart-buttons p.out-of-stock").first() != null) {
+			return false;
+		}
+		return true;
 	}
 
 	private Prices crawlPrices(Float price, Document doc) {
-		// TODO Auto-generated method stub
-		return null;
+		Prices prices = new Prices();
+		return prices;
 	}
 
 	private Float crawlPrice(Document doc) {
-		// TODO Auto-generated method stub
-		return null;
+		Float price = null;
+		Element priceElement = doc.select("div.price-info div.price-box .special-price span.price").first();
+		if (priceElement != null) {
+			try {
+				price = Float.parseFloat(priceElement.text().replaceAll("[^0-9,]+", "").replaceAll("\\.", "").replaceAll(",", ".").trim());
+			} catch (NumberFormatException numberFormatException) {
+				Logging.printLogError(logger, session, "Error parsing price String to float.");
+				numberFormatException.printStackTrace();
+			}
+		} else {
+			priceElement = doc.select("div.price-info span.regular-price span[itemprop=price]").first();
+			if (priceElement != null) {
+				String priceString = priceElement.text().replaceAll("[^0-9,]+", "").replaceAll("\\.", "").replaceAll(",", ".").trim();
+				try {
+					price = Float.parseFloat(priceString);
+				} catch (NumberFormatException numberFormatException) {
+					Logging.printLogError(logger, session, "Error parsing price String to float.");
+					numberFormatException.printStackTrace();
+				}
+			}
+		}
+		return price;
 	}
 
 	private String crawlName(Document doc) {
-		// TODO Auto-generated method stub
+		if (doc.select("div.product-name h1").first() != null) {
+			return doc.select("div.product-name h1").first().text().trim();
+		}
 		return null;
 	}
 
@@ -144,15 +196,17 @@ public class ArgentinaCarrefourCrawler extends Crawler {
 		return false;
 	}
 
-	private String crawlInternalId(Document document) {
-		String internalId = null;
-
-		Element internalIdElement = document.select(".span_codigoplu").first();
-		if (internalIdElement != null) {
-			internalId = internalIdElement.text().replaceAll("[^0-9]", "");
+	private String crawlInternalId(JSONObject dataLayer) {
+		if (dataLayer.has("productos")) {
+			JSONArray products = dataLayer.getJSONArray("productos");
+			if (products.length() > 0) {
+				JSONObject product = products.getJSONObject(0);
+				if (product.has("id")) {
+					return product.getString("id");
+				}
+			}
 		}
-
-		return internalId;
+		return null;
 	}
 
 	/*
@@ -168,15 +222,35 @@ public class ArgentinaCarrefourCrawler extends Crawler {
 		              ],
 		"event": "productdetailview"
 	});
-	*/
+	 */
 	private JSONObject parseDataLayer(Document document) {
-		Elements scripts = document.select("script");
 		try {
 			return CrawlerUtils.selectJsonFromHtml(document, "script", "dataLayer.push(", ");");
 		} catch (Exception e) {
 			Logging.printLogWarn(logger, session, Util.getStackTraceString(e));
 			return new JSONObject();
 		}
+	}
+
+	private JSONObject parseDataBanking(Document document) {
+		Elements scripts = document.select("script");
+		JSONObject dataBanking = new JSONObject();
+
+		for (Element e : scripts) {
+			String dataLayer = e.outerHtml().trim();
+
+			if (dataLayer.contains("var dataBankingJson = {")) {
+				int x = dataLayer.indexOf("= {") + 2;
+				int y = dataLayer.indexOf("};", x);				
+				try {
+					dataBanking = new JSONObject(dataLayer.substring(x, y+1));
+				} catch (JsonException jsonException) {
+					Logging.printLogError(logger, session, "Error parsing dataBanking json object [" + jsonException.getMessage() + "]");
+				}
+			}
+		}
+
+		return dataBanking;
 	}
 
 }
