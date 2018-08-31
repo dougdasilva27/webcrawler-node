d721c589878702c4d6b83938c15b051ce5e5f369
victor <victor@localhost.localdomain>
2018-08-31 11:30:39 -0300

    - Fixing FastShop Crawlers sku variation
    Former-commit-id: 6299043520ea7fe08a43878baba81219bf7da3ad

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilFastshopNewCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilFastshopNewCrawler.java
@@ -26,40 +26,45 @@ import models.Util;
 import models.prices.Prices;
 
 public class BrasilFastshopNewCrawler {
-
+  
   private Logger logger;
   private Session session;
   private List<Cookie> cookies;
-
+  
   public BrasilFastshopNewCrawler(Session session, Logger logger2, List<Cookie> cookies) {
     this.session = session;
     this.logger = logger2;
     this.cookies = cookies;
   }
-
+  
   private static final String SELLER_NAME_LOWER = "fastshop";
-
+  
   public List<Product> crawlProductsNewWay() {
     List<Product> products = new ArrayList<>();
     String internalPid = BrasilFastshopCrawlerUtils.crawlPartnerId(session);
     JSONObject productAPIJSON = BrasilFastshopCrawlerUtils.crawlApiJSON(internalPid, session, cookies);
-
+    
     if (productAPIJSON.length() > 0) {
       Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
-
+      
       CategoryCollection categories = new CategoryCollection(); // was not found categories in this market
       StringBuilder description = crawlDescription(internalPid);
       Integer stock = null;
-
+      
       // sku data in json
       JSONArray arraySkus = productAPIJSON != null && productAPIJSON.has("voltage") ? productAPIJSON.getJSONArray("voltage") : new JSONArray();
-
+      
       for (int i = 0; i < arraySkus.length(); i++) {
         JSONObject variationJson = arraySkus.getJSONObject(i);
-
+        
         String internalId = crawlInternalId(variationJson);
-        JSONObject skuAPIJSON =
-            BrasilFastshopCrawlerUtils.crawlApiJSON(variationJson.has("partNumber") ? variationJson.getString("partNumber") : null, session, cookies);
+        JSONObject skuAPIJSON = productAPIJSON;
+        
+        if (arraySkus.length() > 1) { // In case the array only has 1 sku.
+          skuAPIJSON = BrasilFastshopCrawlerUtils.crawlApiJSON(variationJson.has("partNumber") ? variationJson.getString("partNumber") : null,
+              session, cookies);
+        }
+        
         String primaryImage = crawlPrimaryImage(skuAPIJSON);
         String name = crawlName(skuAPIJSON, variationJson);
         String secondaryImages = crawlSecondaryImages(skuAPIJSON);
@@ -69,89 +74,89 @@ public class BrasilFastshopNewCrawler {
         boolean available = marketplaceMap.containsKey(SELLER_NAME_LOWER);
         Prices prices = available ? marketplaceMap.get(SELLER_NAME_LOWER) : new Prices();
         Float price = crawlMainPagePrice(prices);
-
+        
         // Creating the product
         Product product = ProductBuilder.create().setUrl(session.getOriginalURL()).setInternalId(internalId).setInternalPid(internalPid).setName(name)
             .setPrice(price).setPrices(prices).setAvailable(available).setCategory1(categories.getCategory(0)).setCategory2(categories.getCategory(1))
             .setCategory3(categories.getCategory(2)).setPrimaryImage(primaryImage).setSecondaryImages(secondaryImages)
             .setDescription(description.toString()).setStock(stock).setMarketplace(marketplace).build();
-
+        
         products.add(product);
       }
-
+      
     } else {
       Logging.printLogDebug(logger, session, "Not a product page" + this.session.getOriginalURL());
     }
-
+    
     return products;
   }
-
+  
   /*******************
    * General methods *
    *******************/
-
+  
   private String crawlInternalId(JSONObject json) {
     String internalId = null;
-
+    
     if (json.has("catEntry")) {
       internalId = json.getString("catEntry");
     }
-
+    
     return internalId;
   }
-
+  
   private String crawlName(JSONObject apiJson, JSONObject skuJson) {
     StringBuilder name = new StringBuilder();
-
+    
     if (apiJson.has("shortDescription")) {
       name.append(apiJson.get("shortDescription"));
-
+      
       if (apiJson.has("parentPartNumber")) {
         name.append(" - " + apiJson.get("parentPartNumber"));
       }
-
+      
       if (skuJson.has("name")) {
         String variationName = skuJson.getString("name");
-
+        
         if (!name.toString().contains(variationName)) {
           name.append(" " + variationName);
         }
       }
     }
-
+    
     return name.toString();
   }
-
+  
   public Float crawlMainPagePrice(Prices prices) {
     Float price = null;
-
+    
     if (!prices.isEmpty() && prices.getCardPaymentOptions(Card.VISA.toString()).containsKey(1)) {
       Double priceDouble = prices.getCardPaymentOptions(Card.VISA.toString()).get(1);
       price = priceDouble.floatValue();
     }
-
+    
     return price;
   }
-
+  
   public Marketplace assembleMarketplaceFromMap(Map<String, Prices> marketplaceMap) {
     Marketplace marketplace = new Marketplace();
-
+    
     for (Entry<String, Prices> seller : marketplaceMap.entrySet()) {
       String sellerName = seller.getKey();
       if (!sellerName.equalsIgnoreCase(SELLER_NAME_LOWER)) {
         Prices prices = seller.getValue();
-
+        
         JSONObject sellerJSON = new JSONObject();
         sellerJSON.put("name", sellerName);
         sellerJSON.put("prices", prices.toJSON());
-
+        
         if (prices.getCardPaymentOptions(Card.VISA.toString()).containsKey(1)) {
           Double price = prices.getCardPaymentOptions(Card.VISA.toString()).get(1);
           Float priceFloat = MathUtils.normalizeTwoDecimalPlaces(price.floatValue());
-
+          
           sellerJSON.put("price", priceFloat);
         }
-
+        
         try {
           Seller s = new Seller(sellerJSON);
           marketplace.add(s);
@@ -160,32 +165,30 @@ public class BrasilFastshopNewCrawler {
         }
       }
     }
-
+    
     return marketplace;
   }
-
+  
   private Map<String, Prices> crawlMarketplace(JSONObject apiSku, String internalId) {
     Map<String, Prices> marketplace = new HashMap<>();
-
+    
     boolean available = crawlAvailability(apiSku);
-
+    
     if (available) {
       JSONObject jsonPrices = BrasilFastshopCrawlerUtils.fetchPrices(internalId, true, session, logger);
-
+      
       Prices prices = crawlPrices(jsonPrices, apiSku);
-
+      
       if (apiSku.has("marketPlace") && apiSku.getBoolean("marketPlace") && apiSku.has("marketPlaceText")) {
         marketplace.put(apiSku.getString("marketPlaceText").toLowerCase(), prices);
       } else {
         marketplace.put(SELLER_NAME_LOWER, prices);
       }
     }
-
+    
     return marketplace;
   }
-
-
-
+  
   /**
    * Price "de"
    * 
@@ -194,119 +197,119 @@ public class BrasilFastshopNewCrawler {
    */
   private Double crawlPriceFrom(JSONObject jsonSku) {
     Double priceFrom = null;
-
+    
     if (jsonSku.has("priceTag")) {
       String price = jsonSku.get("priceTag").toString().replaceAll("[^0-9.]", "");
-
+      
       if (!price.isEmpty()) {
         priceFrom = Double.parseDouble(price);
       }
     }
-
+    
     return priceFrom;
   }
-
+  
   private boolean crawlAvailability(JSONObject json) {
     if (json.has("buyable")) {
       return json.getBoolean("buyable");
     }
     return false;
   }
-
+  
   private String crawlPrimaryImage(JSONObject json) {
     String primaryImage = null;
-
+    
     if (json.has("images")) {
       JSONArray images = json.getJSONArray("images");
-
+      
       if (images.length() > 0) {
         JSONObject imageJson = images.getJSONObject(0);
-
+        
         if (imageJson.has("path")) {
           primaryImage = imageJson.getString("path");
-
+          
           if (!primaryImage.startsWith("http")) {
             primaryImage = "https://prdresources1-a.akamaihd.net/wcsstore/" + primaryImage;
           }
         }
       }
     }
-
+    
     return primaryImage;
   }
-
+  
   private String crawlSecondaryImages(JSONObject json) {
     String secondaryImages = null;
     JSONArray secondaryImagesArray = new JSONArray();
-
+    
     if (json.has("images")) {
       JSONArray images = json.getJSONArray("images");
-
+      
       for (int i = 1; i < images.length(); i++) {
         JSONObject imageJson = images.getJSONObject(i);
-
+        
         if (imageJson.has("path")) {
           String image = imageJson.getString("path");
-
+          
           if (!image.startsWith("http")) {
             image = "https://prdresources1-a.akamaihd.net/wcsstore/" + image;
           }
-
+          
           secondaryImagesArray.put(image);
         }
       }
     }
-
+    
     if (secondaryImagesArray.length() > 0) {
       secondaryImages = secondaryImagesArray.toString();
     }
-
+    
     return secondaryImages;
   }
-
+  
   private StringBuilder crawlDescription(String partnerId) {
     StringBuilder description = new StringBuilder();
-
+    
     String url = "https://www.fastshop.com.br/webapp/wcs/stores/servlet/SpotsContentView?type=content&hotsite=fastshop&catalogId=11052"
         + "&langId=-6&storeId=10151&emsName=SC_" + partnerId + "_Conteudo";
     Document doc = DataFetcher.fetchDocument(DataFetcher.GET_REQUEST, session, url, null, cookies);
-
+    
     String urlDESC = "https://www.fastshop.com.br/wcs/resources/v1/spots/ProductDetail_" + partnerId;
     Document docDesc = DataFetcher.fetchDocument(DataFetcher.GET_REQUEST, session, urlDESC, null, cookies);
-
+    
     if (!docDesc.toString().contains("errorCode")) {
       description.append(docDesc);
     }
-
+    
     Element iframe = doc.select("iframe").first();
-
+    
     if (iframe != null) {
       description.append(DataFetcher.fetchDocument(DataFetcher.GET_REQUEST, session, iframe.attr("src"), null, cookies));
     }
-
+    
     return description;
   }
-
+  
   private Prices crawlPrices(JSONObject jsonPrices, JSONObject jsonSku) {
     Prices prices = new Prices();
-
+    
     prices.setPriceFrom(crawlPriceFrom(jsonSku));
-
+    
     Map<Integer, Float> installmentPriceMap = new HashMap<>();
-
+    
     if (jsonSku.has("tags")) {
       JSONArray tags = jsonSku.getJSONArray("tags");
-
+      
       for (Object json : tags) {
         JSONObject tag = (JSONObject) json;
-
+        
         if (tag.has("tag")) {
           String tagBoleto = tag.getString("tag");
-
+          
           if (tagBoleto.contains("_boleto")) {
             String[] tokens = tagBoleto.split("/");
             String priceBoleto = tokens[tokens.length - 1].split("_")[0].replaceAll("[^0-9]", "");
-
+            
             if (!priceBoleto.isEmpty()) {
               prices.setBankTicketPrice(Float.parseFloat(priceBoleto));
             }
@@ -314,34 +317,34 @@ public class BrasilFastshopNewCrawler {
         }
       }
     }
-
+    
     if (jsonPrices.has("priceData")) {
       JSONObject priceData = jsonPrices.getJSONObject("priceData");
-
+      
       if (priceData.has("offerPrice")) {
         Float offerPrice = MathUtils.parseFloat(priceData.getString("offerPrice"));
-
+        
         // Preço de boleto e 1 vez no cartão são iguais.
         installmentPriceMap.put(1, offerPrice);
-
+        
         if (prices.getBankTicketPrice() == null) {
           prices.setBankTicketPrice(offerPrice);
         }
       }
-
+      
       if (priceData.has("installmentPrice")) {
         String text = priceData.getString("installmentPrice").toLowerCase();
-
+        
         if (text.contains("x")) {
           int x = text.indexOf('x');
-
+          
           Integer installment = Integer.parseInt(text.substring(0, x));
           Float value = MathUtils.parseFloat(text.substring(x));
-
+          
           installmentPriceMap.put(installment, value);
         }
       }
-
+      
       prices.insertCardInstallment(Card.VISA.toString(), installmentPriceMap);
       prices.insertCardInstallment(Card.MASTERCARD.toString(), installmentPriceMap);
       prices.insertCardInstallment(Card.AMEX.toString(), installmentPriceMap);
@@ -349,7 +352,7 @@ public class BrasilFastshopNewCrawler {
       prices.insertCardInstallment(Card.HIPERCARD.toString(), installmentPriceMap);
       prices.insertCardInstallment(Card.ELO.toString(), installmentPriceMap);
     }
-
+    
     return prices;
   }
 }
