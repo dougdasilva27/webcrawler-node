e7ae08ecfc54a0059e7197bd1ba30c46256e6700
gabriel <gabrieldornelas@lett.digital>
2018-08-31 15:24:02 -0300

    Bugfix last commit
    Former-commit-id: 743d72880fe0004199504021caba3c6ca6019859

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilClimarioCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilClimarioCrawler.java
@@ -1,6 +1,7 @@
 package br.com.lett.crawlernode.crawlers.corecontent.brasil;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -58,120 +59,120 @@ import models.prices.Prices;
  ************************************************************************************************************************************************************************************/
 
 public class BrasilClimarioCrawler extends Crawler {
-  
+
   private static final String HOME_PAGE = "http://www.climario.com.br/";
-  private static final String MAIN_SELLER_NAME_LOWER = "climario";
-  
+  private static final List<String> MAIN_SELLER_NAME_LOWER_LIST = Arrays.asList("climario.com.br", "climario");
+
   public BrasilClimarioCrawler(Session session) {
     super(session);
   }
-  
+
   @Override
   public boolean shouldVisit() {
     String href = this.session.getOriginalURL().toLowerCase();
     return !FILTERS.matcher(href).matches() && (href.startsWith(HOME_PAGE));
   }
-  
-  
+
+
   @Override
   public List<Product> extractInformation(Document doc) throws Exception {
     super.extractInformation(doc);
     List<Product> products = new ArrayList<>();
-    
+
     if (isProductPage(doc)) {
       Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
-      
+
       JSONObject skuJson = CrawlerUtils.crawlSkuJsonVTEX(doc, session);
-      
+
       String internalPid = crawlInternalPid(skuJson);
       CategoryCollection categories = crawlCategories(doc);
       String description = crawlDescription(doc);
-      
+
       // sku data in json
       JSONArray arraySkus = skuJson != null && skuJson.has("skus") ? skuJson.getJSONArray("skus") : new JSONArray();
-      
+
       for (int i = 0; i < arraySkus.length(); i++) {
         JSONObject jsonSku = arraySkus.getJSONObject(i);
-        
+
         String internalId = crawlInternalId(jsonSku);
         String name = crawlName(jsonSku, skuJson);
         Map<String, Float> marketplaceMap = crawlMarketplace(jsonSku);
         Marketplace marketplace = assembleMarketplaceFromMap(marketplaceMap, internalId, jsonSku, doc);
-        boolean available = marketplaceMap.containsKey(MAIN_SELLER_NAME_LOWER);
         Float price = crawlMainPagePrice(marketplaceMap);
-        
+        boolean available = price != null;
+
         JSONObject jsonProduct = crawlApi(internalId);
         String primaryImage = crawlPrimaryImage(jsonProduct);
         String secondaryImages = crawlSecondaryImages(jsonProduct);
         Prices prices = crawlPrices(internalId, price, jsonSku, doc);
         Integer stock = crawlStock(jsonProduct);
-        
+
         // Creating the product
         Product product = ProductBuilder.create().setUrl(session.getOriginalURL()).setInternalId(internalId).setInternalPid(internalPid).setName(name)
             .setPrice(price).setPrices(prices).setAvailable(available).setCategory1(categories.getCategory(0)).setCategory2(categories.getCategory(1))
             .setCategory3(categories.getCategory(2)).setPrimaryImage(primaryImage).setSecondaryImages(secondaryImages).setDescription(description)
             .setStock(stock).setMarketplace(marketplace).build();
-        
+
         products.add(product);
       }
-      
+
     } else {
       Logging.printLogDebug(logger, session, "Not a product page" + this.session.getOriginalURL());
     }
-    
+
     return products;
   }
-  
+
   /*******************************
    * Product page identification *
    *******************************/
-  
+
   private boolean isProductPage(Document document) {
     return document.select(".row-product-info").first() != null;
   }
-  
+
   /*******************
    * General methods *
    *******************/
-  
+
   private String crawlInternalId(JSONObject json) {
     String internalId = null;
-    
+
     if (json.has("sku")) {
       internalId = Integer.toString(json.getInt("sku")).trim();
     }
-    
+
     return internalId;
   }
-  
+
   private String crawlInternalPid(JSONObject skuJson) {
     String internalPid = null;
-    
+
     if (skuJson.has("productId")) {
       internalPid = skuJson.get("productId").toString();
     }
-    
+
     return internalPid;
   }
-  
+
   private String crawlName(JSONObject jsonSku, JSONObject skuJson) {
     String name = null;
-    
+
     String nameVariation = jsonSku.getString("skuname");
-    
+
     if (skuJson.has("name")) {
       name = skuJson.getString("name");
-      
+
       if (name.length() > nameVariation.length()) {
         name += " " + nameVariation;
       } else {
         name = nameVariation;
       }
     }
-    
+
     return name;
   }
-  
+
   /**
    * Price "de"
    * 
@@ -180,76 +181,79 @@ public class BrasilClimarioCrawler extends Crawler {
    */
   private Double crawlPriceFrom(JSONObject jsonSku) {
     Double priceFrom = null;
-    
+
     if (jsonSku.has("listPriceFormated")) {
       Float price = MathUtils.parseFloat(jsonSku.get("listPriceFormated").toString());
       priceFrom = MathUtils.normalizeTwoDecimalPlaces(price.doubleValue());
     }
-    
+
     return priceFrom;
   }
-  
+
   private Float crawlMainPagePrice(Map<String, Float> marketplace) {
     Float price = null;
-    
-    if (marketplace.containsKey(MAIN_SELLER_NAME_LOWER)) {
-      price = marketplace.get(MAIN_SELLER_NAME_LOWER);
+
+    for (String mainSellerNameLower : MAIN_SELLER_NAME_LOWER_LIST) {
+      if (marketplace.containsKey(mainSellerNameLower)) {
+        price = marketplace.get(mainSellerNameLower);
+        break;
+      }
     }
-    
+
     return price;
   }
-  
+
   private String crawlPrimaryImage(JSONObject json) {
     String primaryImage = null;
-    
+
     if (json.has("Images")) {
       JSONArray jsonArrayImages = json.getJSONArray("Images");
-      
+
       for (int i = 0; i < jsonArrayImages.length(); i++) {
         JSONArray arrayImage = jsonArrayImages.getJSONArray(i);
         JSONObject jsonImage = arrayImage.getJSONObject(0);
-        
+
         if (jsonImage.has("IsMain") && jsonImage.getBoolean("IsMain") && jsonImage.has("Path")) {
           primaryImage = changeImageSizeOnURL(jsonImage.getString("Path"));
           break;
         }
       }
     }
-    
+
     return primaryImage;
   }
-  
+
   private String crawlSecondaryImages(JSONObject apiInfo) {
     String secondaryImages = null;
     JSONArray secondaryImagesArray = new JSONArray();
-    
+
     if (apiInfo.has("Images")) {
       JSONArray jsonArrayImages = apiInfo.getJSONArray("Images");
-      
+
       for (int i = 0; i < jsonArrayImages.length(); i++) {
         JSONArray arrayImage = jsonArrayImages.getJSONArray(i);
         JSONObject jsonImage = arrayImage.getJSONObject(0);
-        
+
         // jump primary image
         if (jsonImage.has("IsMain") && jsonImage.getBoolean("IsMain")) {
           continue;
         }
-        
+
         if (jsonImage.has("Path")) {
           String urlImage = changeImageSizeOnURL(jsonImage.getString("Path"));
           secondaryImagesArray.put(urlImage);
         }
-        
+
       }
     }
-    
+
     if (secondaryImagesArray.length() > 0) {
       secondaryImages = secondaryImagesArray.toString();
     }
-    
+
     return secondaryImages;
   }
-  
+
   /**
    * Get the image url and change it size
    * 
@@ -259,42 +263,40 @@ public class BrasilClimarioCrawler extends Crawler {
   private String changeImageSizeOnURL(String url) {
     String[] tokens = url.trim().split("/");
     String dimensionImage = tokens[tokens.length - 2]; // to get dimension image and the image id
-    
+
     String[] tokens2 = dimensionImage.split("-"); // to get the image-id
     String dimensionImageFinal = tokens2[0] + "-1000-1000";
-    
+
     return url.replace(dimensionImage, dimensionImageFinal); // The image size is changed
   }
-  
+
   private Map<String, Float> crawlMarketplace(JSONObject json) {
     Map<String, Float> marketplace = new HashMap<>();
-    
+
     if (json.has("seller")) {
       String nameSeller = json.getString("seller").toLowerCase().trim();
-      
+
       if (json.has("bestPriceFormated") && json.has("available") && json.getBoolean("available")) {
         Float price = MathUtils.parseFloat(json.getString("bestPriceFormated"));
-        if (nameSeller != MAIN_SELLER_NAME_LOWER) {
-          marketplace.put(nameSeller, price);
-        }
+        marketplace.put(nameSeller, price);
       }
     }
-    
+
     return marketplace;
   }
-  
+
   private Marketplace assembleMarketplaceFromMap(Map<String, Float> marketplaceMap, String internalId, JSONObject jsonSku, Document doc) {
     Marketplace marketplace = new Marketplace();
-    
+
     for (String seller : marketplaceMap.keySet()) {
-      if (!seller.equalsIgnoreCase(MAIN_SELLER_NAME_LOWER)) {
+      if (!MAIN_SELLER_NAME_LOWER_LIST.contains(seller)) {
         Float price = marketplaceMap.get(seller);
-        
+
         JSONObject sellerJSON = new JSONObject();
         sellerJSON.put("name", seller);
         sellerJSON.put("price", price);
         sellerJSON.put("prices", crawlPrices(internalId, price, jsonSku, doc).toJSON());
-        
+
         try {
           Seller s = new Seller(sellerJSON);
           marketplace.add(s);
@@ -303,42 +305,42 @@ public class BrasilClimarioCrawler extends Crawler {
         }
       }
     }
-    
+
     return marketplace;
   }
-  
+
   private CategoryCollection crawlCategories(Document document) {
     CategoryCollection categories = new CategoryCollection();
     Elements elementCategories = document.select(".bread-crumb li > a");
-    
+
     for (int i = 1; i < elementCategories.size(); i++) { // first item is the home page
       categories.add(elementCategories.get(i).text().trim());
     }
-    
+
     return categories;
   }
-  
+
   private String crawlDescription(Document doc) {
     StringBuilder description = new StringBuilder();
-    
+
     Element shortDescription = doc.select(".productDescription").first();
     if (shortDescription != null) {
       description.append(shortDescription.html());
     }
-    
+
     Element elementInformation = doc.select(".productSpecification").first();
     if (elementInformation != null) {
       description.append(elementInformation.html());
     }
-    
+
     Element caracteristicas = doc.select("#caracteristicas").first();
     if (caracteristicas != null) {
       description.append(caracteristicas.html());
     }
-    
+
     return description.toString();
   }
-  
+
   /**
    * To crawl this prices is accessed a api Is removed all accents for crawl price 1x like this: Visa
    * à vista R$ 1.790,00
@@ -349,83 +351,83 @@ public class BrasilClimarioCrawler extends Crawler {
    */
   private Prices crawlPrices(String internalId, Float price, JSONObject jsonSku, Document docPrincipal) {
     Prices prices = new Prices();
-    
+
     if (price != null) {
       String url = "https://www.climario.com.br/productotherpaymentsystems/" + internalId;
       Document doc = DataFetcher.fetchDocument(DataFetcher.GET_REQUEST, session, url, null, cookies);
-      
+
       prices.setPriceFrom(crawlPriceFrom(jsonSku));
-      
+
       Element discountElement = docPrincipal.select("p[class^=\"flag a-vista-\"]").first();
       Integer discount = 0;
       if (discountElement != null) {
         String text = discountElement.ownText().replaceAll("[^0-9]", "").trim();
-        
+
         if (!text.isEmpty()) {
           discount = Integer.parseInt(text);
         }
       }
-      
+
       Element bank = doc.select("#ltlPrecoWrapper em").first();
       if (bank != null) {
         Float boleto = MathUtils.parseFloat(bank.text());
-        
+
         if (discount > 0) {
           boleto = MathUtils.normalizeTwoDecimalPlaces(boleto - (boleto * (discount / 100f)));
         }
-        
+
         prices.setBankTicketPrice(boleto);
       } else {
         Float boleto = price;
-        
+
         if (discount > 0) {
           boleto = MathUtils.normalizeTwoDecimalPlaces(boleto - (boleto * (discount / 100f)));
         }
-        
+
         prices.setBankTicketPrice(boleto);
-        
+
       }
-      
+
       Elements cardsElements = doc.select("#ddlCartao option");
-      
+
       if (!cardsElements.isEmpty()) {
         for (Element e : cardsElements) {
           String text = e.text().toLowerCase();
-          
+
           if (text.contains("visa")) {
             Map<Integer, Float> installmentPriceMap = getInstallmentsForCard(doc, e.attr("value"));
             prices.insertCardInstallment(Card.VISA.toString(), installmentPriceMap);
-            
+
           } else if (text.contains("mastercard")) {
             Map<Integer, Float> installmentPriceMap = getInstallmentsForCard(doc, e.attr("value"));
             prices.insertCardInstallment(Card.MASTERCARD.toString(), installmentPriceMap);
-            
+
           } else if (text.contains("diners")) {
             Map<Integer, Float> installmentPriceMap = getInstallmentsForCard(doc, e.attr("value"));
             prices.insertCardInstallment(Card.DINERS.toString(), installmentPriceMap);
-            
+
           } else if (text.contains("american") || text.contains("amex")) {
             Map<Integer, Float> installmentPriceMap = getInstallmentsForCard(doc, e.attr("value"));
             prices.insertCardInstallment(Card.AMEX.toString(), installmentPriceMap);
-            
+
           } else if (text.contains("hipercard") || text.contains("amex")) {
             Map<Integer, Float> installmentPriceMap = getInstallmentsForCard(doc, e.attr("value"));
             prices.insertCardInstallment(Card.HIPERCARD.toString(), installmentPriceMap);
-            
+
           } else if (text.contains("credicard")) {
             Map<Integer, Float> installmentPriceMap = getInstallmentsForCard(doc, e.attr("value"));
             prices.insertCardInstallment(Card.CREDICARD.toString(), installmentPriceMap);
-            
+
           } else if (text.contains("elo")) {
             Map<Integer, Float> installmentPriceMap = getInstallmentsForCard(doc, e.attr("value"));
             prices.insertCardInstallment(Card.ELO.toString(), installmentPriceMap);
-            
+
           }
         }
       } else {
         Map<Integer, Float> installmentPriceMap = new HashMap<>();
         installmentPriceMap.put(1, price);
-        
+
         prices.insertCardInstallment(Card.VISA.toString(), installmentPriceMap);
         prices.insertCardInstallment(Card.MASTERCARD.toString(), installmentPriceMap);
         prices.insertCardInstallment(Card.DINERS.toString(), installmentPriceMap);
@@ -434,64 +436,64 @@ public class BrasilClimarioCrawler extends Crawler {
         prices.insertCardInstallment(Card.ELO.toString(), installmentPriceMap);
       }
     }
-    
+
     return prices;
   }
-  
+
   private Map<Integer, Float> getInstallmentsForCard(Document doc, String idCard) {
     Map<Integer, Float> mapInstallments = new HashMap<>();
-    
+
     Elements installmentsCard = doc.select(".tbl-payment-system#tbl" + idCard + " tr");
     for (Element i : installmentsCard) {
       Element installmentElement = i.select("td.parcelas").first();
-      
+
       if (installmentElement != null) {
         String textInstallment = installmentElement.text().toLowerCase();
         Integer installment;
-        
+
         if (textInstallment.contains("vista")) {
           installment = 1;
         } else {
           installment = Integer.parseInt(textInstallment.replaceAll("[^0-9]", "").trim());
         }
-        
+
         Element valueElement = i.select("td:not(.parcelas)").first();
-        
+
         if (valueElement != null) {
           Float value = Float.parseFloat(valueElement.text().replaceAll("[^0-9,]+", "").replaceAll("\\.", "").replaceAll(",", ".").trim());
-          
+
           mapInstallments.put(installment, value);
         }
       }
     }
-    
+
     return mapInstallments;
   }
-  
+
   private JSONObject crawlApi(String internalId) {
     String url = "https://www.climario.com.br/produto/sku/" + internalId;
-    
+
     JSONArray jsonArray = DataFetcher.fetchJSONArray(DataFetcher.GET_REQUEST, session, url, null, cookies);
-    
+
     if (jsonArray.length() > 0) {
       return jsonArray.getJSONObject(0);
     }
-    
+
     return new JSONObject();
   }
-  
-  
+
+
   private Integer crawlStock(JSONObject jsonProduct) {
     Integer stock = null;
-    
+
     if (jsonProduct.has("SkuSellersInformation")) {
       JSONObject sku = jsonProduct.getJSONArray("SkuSellersInformation").getJSONObject(0);
-      
+
       if (sku.has("AvailableQuantity")) {
         stock = sku.getInt("AvailableQuantity");
       }
     }
-    
+
     return stock;
   }
 }
