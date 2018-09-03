bc43e48fd4f527e37e4dac3ff2f0a352e22709fb
victor <victor@localhost.localdomain>
2018-09-03 18:34:04 -0300

    - new Vtex description function implemented
    Former-commit-id: 737dcc1823fdab33fad321940e36b5a91ed5845f

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilEpocacosmeticosCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilEpocacosmeticosCrawler.java
@@ -2,16 +2,20 @@ package br.com.lett.crawlernode.crawlers.corecontent.brasil;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import br.com.lett.crawlernode.core.models.CategoryCollection;
 import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.models.ProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.Crawler;
 import br.com.lett.crawlernode.crawlers.corecontent.extractionutils.VTEXCrawlersUtils;
 import br.com.lett.crawlernode.util.CrawlerUtils;
 import br.com.lett.crawlernode.util.Logging;
+import models.Marketplace;
+import models.prices.Prices;
 
 public class BrasilEpocacosmeticosCrawler extends Crawler {
   public BrasilEpocacosmeticosCrawler(Session session) {
@@ -42,43 +46,39 @@ public class BrasilEpocacosmeticosCrawler extends Crawler {
       
       String internalPid = vtexUtil.crawlInternalPid(skuJson);
       
-      
       CategoryCollection categories = CrawlerUtils.crawlCategories(doc, ".bread-crumb li > a");
-      System.out.println(categories);
       
-      System.out.println(skuJson.toString());
       
-      // String description = crawlDescription(doc);
-      //
-      // // sku data in json
-      // JSONArray arraySkus = skuJson != null && skuJson.has("skus") ? skuJson.getJSONArray("skus") : new
-      // JSONArray();
-      //
-      // for (int i = 0; i < arraySkus.length(); i++) {
-      // JSONObject jsonSku = arraySkus.getJSONObject(i);
-      //
-      // String internalId = vtexUtil.crawlInternalId(jsonSku);
-      // JSONObject apiJSON = vtexUtil.crawlApi(internalId);
-      // String name = vtexUtil.crawlName(jsonSku, skuJson);
-      // Map<String, Prices> marketplaceMap = vtexUtil.crawlMarketplace(apiJSON, internalId);
-      // Marketplace marketplace = vtexUtil.assembleMarketplaceFromMap(marketplaceMap);
-      // boolean available = marketplaceMap.containsKey(MAIN_SELLER_NAME_LOWER);
-      // String primaryImage = vtexUtil.crawlPrimaryImage(apiJSON);
-      // String secondaryImages = vtexUtil.crawlSecondaryImages(apiJSON);
-      // Prices prices = marketplaceMap.containsKey(MAIN_SELLER_NAME_LOWER) ?
-      // marketplaceMap.get(MAIN_SELLER_NAME_LOWER) : new Prices();
-      // Float price = vtexUtil.crawlMainPagePrice(prices);
-      // Integer stock = vtexUtil.crawlStock(apiJSON);
-      //
-      // // Creating the product
-      // Product product =
-      // ProductBuilder.create().setUrl(session.getOriginalURL()).setInternalId(internalId).setInternalPid(internalPid).setName(name)
-      // .setPrice(price).setPrices(prices).setAvailable(available).setCategory1(categories.getCategory(0)).setCategory2(categories.getCategory(1))
-      // .setCategory3(categories.getCategory(2)).setPrimaryImage(primaryImage).setSecondaryImages(secondaryImages).setDescription(description)
-      // .setStock(stock).setMarketplace(marketplace).build();
-      //
-      // products.add(product);
-      // }
+      // sku data in json
+      JSONArray arraySkus = skuJson != null && skuJson.has("skus") ? skuJson.getJSONArray("skus") : new JSONArray();
+      
+      for (int i = 0; i < arraySkus.length(); i++) {
+        JSONObject jsonSku = arraySkus.getJSONObject(i);
+        
+        String internalId = vtexUtil.crawlInternalId(jsonSku);
+        
+        JSONArray descriptionArray = vtexUtil.crawlDescriptionAPI(internalId, "skuId");
+        String description = crawlDescription(descriptionArray);
+        
+        JSONObject apiJSON = vtexUtil.crawlApi(internalId);
+        String name = vtexUtil.crawlName(jsonSku, skuJson);
+        Map<String, Prices> marketplaceMap = vtexUtil.crawlMarketplace(apiJSON, internalId);
+        Marketplace marketplace = vtexUtil.assembleMarketplaceFromMap(marketplaceMap);
+        boolean available = marketplaceMap.containsKey(MAIN_SELLER_NAME_LOWER);
+        String primaryImage = vtexUtil.crawlPrimaryImage(apiJSON);
+        String secondaryImages = vtexUtil.crawlSecondaryImages(apiJSON);
+        Prices prices = marketplaceMap.containsKey(MAIN_SELLER_NAME_LOWER) ? marketplaceMap.get(MAIN_SELLER_NAME_LOWER) : new Prices();
+        Float price = vtexUtil.crawlMainPagePrice(prices);
+        Integer stock = vtexUtil.crawlStock(apiJSON);
+        
+        // Creating the product
+        Product product = ProductBuilder.create().setUrl(session.getOriginalURL()).setInternalId(internalId).setInternalPid(internalPid).setName(name)
+            .setPrice(price).setPrices(prices).setAvailable(available).setCategory1(categories.getCategory(0)).setCategory2(categories.getCategory(1))
+            .setCategory3(categories.getCategory(2)).setPrimaryImage(primaryImage).setSecondaryImages(secondaryImages).setDescription(description)
+            .setStock(stock).setMarketplace(marketplace).build();
+        
+        products.add(product);
+      }
       
     } else
     
@@ -94,22 +94,14 @@ public class BrasilEpocacosmeticosCrawler extends Crawler {
     return document.selectFirst(".productName") != null;
   }
   
-  private String crawlDescription(Document document) {
-    String description = "";
+  private String crawlDescription(JSONArray document) {
+    StringBuilder description = new StringBuilder();
     
-    Element shortDesc = document.select(".productDescriptionShort").first();
     
-    if (shortDesc != null) {
-      description = description + shortDesc.html();
-    }
     
-    Element descElement = document.select("#detalhes-do-produto").first();
+    System.out.println(document);
     
-    if (descElement != null) {
-      description = description + descElement.html();
-    }
-    
-    return description;
+    return description.toString();
   }
   
 }
