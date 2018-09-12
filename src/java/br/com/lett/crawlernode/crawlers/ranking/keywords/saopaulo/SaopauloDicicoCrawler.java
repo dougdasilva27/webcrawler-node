e70fccc58c5d7aa13832ddd70779b09a046b3806
victor <victor@localhost.localdomain>
2018-09-12 18:40:53 -0300

    - Dicico Ranking crawler implemented
    Former-commit-id: f6015ca805fbc82fe877773336e76393e61b5506

---
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/saopaulo/SaopauloDicicoCrawler.java
@@ -0,0 +1,96 @@
+package br.com.lett.crawlernode.crawlers.ranking.keywords.saopaulo;
+
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.util.MathUtils;
+
+public class SaopauloDicicoCrawler extends CrawlerRankingKeywords {
+  
+  public SaopauloDicicoCrawler(Session session) {
+    super(session);
+  }
+  
+  int pageCounter = 0;
+  
+  
+  @Override
+  protected void extractProductsFromCurrentPage() {
+    this.pageSize = 20;
+    
+    String key = this.keywordWithoutAccents.replaceAll(" ", "%20");
+    this.log("Página " + this.currentPage);
+    
+    
+    String url = "http://www.dicico.com.br/dicico-br/search/?No=" + this.pageCounter + "&Nrpp=" + this.pageSize + "&Ntt=" + key;
+    this.log("Link onde são feitos os crawlers: " + url);
+    
+    this.currentDoc = fetchDocument(url);
+    
+    Elements products = this.currentDoc.select("div.info-box");
+    
+    if (!products.isEmpty()) {
+      if (this.totalProducts == 0) {
+        setTotalProducts();
+      }
+      for (Element e : products) {
+        
+        String internalPid = crawlInternalPid(e);
+        String productUrl = crawlProductUrl(e);
+        
+        saveDataProduct(null, internalPid, productUrl);
+        
+        this.log("Position: " + this.position + " - InternalId: " + null + " - InternalPid: " + internalPid + " - Url: " + productUrl);
+        if (this.arrayProducts.size() == productsLimit)
+          break;
+      }
+    } else {
+      this.result = false;
+      this.log("Keyword sem resultado!");
+    }
+    
+    this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
+    this.pageCounter += this.pageSize;
+  }
+  
+  @Override
+  protected void setTotalProducts() {
+    Element totalElement = this.currentDoc.selectFirst(".active > a > span.pl-5");
+    
+    if (totalElement != null) {
+      String total = totalElement.ownText().replaceAll("[^0-9]", "").trim();
+      
+      if (!total.isEmpty()) {
+        this.totalProducts = Integer.parseInt(total);
+      }
+    }
+    
+    this.log("Total da busca: " + this.totalProducts);
+  }
+  
+  private String crawlInternalPid(Element e) {
+    String internalPid = null;
+    
+    Element text = e.selectFirst(".sku");
+    if (text.ownText().contains(": ")) {
+      internalPid = MathUtils.parseInt((text.ownText())).toString();
+    }
+    return internalPid;
+  }
+  
+  private String crawlProductUrl(Element e) {
+    String productUrl = null;
+    Element urlElement = e.select(".jq-name > a").first();
+    
+    if (urlElement != null) {
+      productUrl = urlElement.attr("href");
+      
+      if (!productUrl.startsWith("http")) {
+        productUrl = "https://www.dicico.com.br" + productUrl;
+      }
+    }
+    return productUrl;
+  }
+  
+}
