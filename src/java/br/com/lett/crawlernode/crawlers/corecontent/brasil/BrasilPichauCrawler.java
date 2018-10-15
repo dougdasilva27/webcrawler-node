2124fed40e700247f86eb191cf630d71fcb14400
gabriel <gabrieldornelas@lett.digital>
2018-10-15 15:09:37 -0300

    finalizei crawler pichau [DQ-231]
    Former-commit-id: 4002656d81180d673d9b2b1e249eff37d3126e31

---
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilPichauCrawler.java
@@ -0,0 +1,121 @@
+package br.com.lett.crawlernode.crawlers.corecontent.brasil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import br.com.lett.crawlernode.core.models.Card;
+import br.com.lett.crawlernode.core.models.CategoryCollection;
+import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.models.ProductBuilder;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.Crawler;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.Logging;
+import br.com.lett.crawlernode.util.Pair;
+import models.Marketplace;
+import models.prices.Prices;
+
+/**
+ * Date: 15/10/2018
+ * 
+ * @author Gabriel Dornelas
+ *
+ */
+public class BrasilPichauCrawler extends Crawler {
+
+  private static final String HOME_PAGE = "https://www.pichau.com.br/";
+
+  public BrasilPichauCrawler(Session session) {
+    super(session);
+  }
+
+  @Override
+  public boolean shouldVisit() {
+    String href = session.getOriginalURL().toLowerCase();
+    return !FILTERS.matcher(href).matches() && (href.startsWith(HOME_PAGE));
+  }
+
+  @Override
+  public List<Product> extractInformation(Document doc) throws Exception {
+    super.extractInformation(doc);
+    List<Product> products = new ArrayList<>();
+
+    if (isProductPage(doc)) {
+      Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
+
+      String internalId = crawlInternalId(doc);
+      String internalPid = CrawlerUtils.scrapStringSimpleInfo(doc, ".codigo-produto", true);
+      String name = CrawlerUtils.scrapStringSimpleInfo(doc, ".product-details h2", true);
+      Float price = CrawlerUtils.scrapSimplePriceFloat(doc, "#product-price-" + internalId, true);
+      Prices prices = crawlPrices(doc, price);
+      boolean available = !doc.select(".product-payment .buy").isEmpty();
+      CategoryCollection categories = CrawlerUtils.crawlCategories(doc, ".breadcrumbs li:not(.home):not(.product) a");
+      String primaryImage = CrawlerUtils.scrapSimplePrimaryImage(doc, ".product-images a", Arrays.asList("href"), "https:", "www.pichau.com.br");
+      String secondaryImages =
+          CrawlerUtils.scrapSimpleSecondaryImages(doc, ".product-images a", Arrays.asList("href"), "https:", "www.pichau.com.br", primaryImage);
+      String description = CrawlerUtils.scrapSimpleDescription(doc, Arrays.asList(".product-attributes", ".product-description"));
+
+      // Creating the product
+      Product product = ProductBuilder.create().setUrl(session.getOriginalURL()).setInternalId(internalId).setInternalPid(internalPid).setName(name)
+          .setPrice(price).setPrices(prices).setAvailable(available).setCategory1(categories.getCategory(0)).setCategory2(categories.getCategory(1))
+          .setCategory3(categories.getCategory(2)).setPrimaryImage(primaryImage).setSecondaryImages(secondaryImages).setDescription(description)
+          .setMarketplace(new Marketplace()).build();
+
+      products.add(product);
+
+    } else {
+      Logging.printLogDebug(logger, session, "Not a product page" + this.session.getOriginalURL());
+    }
+
+    return products;
+
+  }
+
+  private boolean isProductPage(Document doc) {
+    return !doc.select(".product-details").isEmpty();
+  }
+
+  private static String crawlInternalId(Document doc) {
+    String internalId = null;
+
+    Element infoElement = doc.selectFirst("input[name=product]");
+    if (infoElement != null) {
+      internalId = infoElement.val();
+    }
+
+    return internalId;
+  }
+
+  /**
+   * @param doc
+   * @param price
+   * @return
+   */
+  private Prices crawlPrices(Document doc, Float price) {
+    Prices prices = new Prices();
+
+    Map<Integer, Float> installmentPriceMap = new TreeMap<>();
+    installmentPriceMap.put(1, price);
+
+    Float bankTicket = CrawlerUtils.scrapSimplePriceFloat(doc, ".product-payment .boleto .valor", true);
+    if (bankTicket != null) {
+      prices.setBankTicketPrice(bankTicket);
+    } else {
+      prices.setBankTicketPrice(price);
+    }
+
+    Pair<Integer, Float> pair = CrawlerUtils.crawlSimpleInstallment(".product-payment .pricex", doc, true, "x");
+    if (!pair.isAnyValueNull()) {
+      installmentPriceMap.put(pair.getFirst(), pair.getSecond());
+    }
+
+    prices.insertCardInstallment(Card.MASTERCARD.toString(), installmentPriceMap);
+
+    return prices;
+  }
+
+}
