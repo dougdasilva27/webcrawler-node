b5a729fda609103f7484f83777bf943d44df98d8
gabriel <gabrieldornelas@lett.digital>
2018-10-23 16:06:25 -0300

    finalizei ranking da paris [DQ-221]
    Former-commit-id: c90b8ea8109e22d3610452011f3a7c9495702474

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/chile/ChileParisCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/chile/ChileParisCrawler.java
@@ -7,6 +7,7 @@ import org.json.JSONObject;
 import br.com.lett.crawlernode.core.fetcher.methods.POSTFetcher;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.util.CommonMethods;
 import br.com.lett.crawlernode.util.CrawlerUtils;
 
 public class ChileParisCrawler extends CrawlerRankingKeywords {
@@ -20,6 +21,7 @@ public class ChileParisCrawler extends CrawlerRankingKeywords {
     this.pageSize = 30;
 
     this.log("Página " + this.currentPage);
+
     JSONObject search = crawlSearchApi();
 
     if (search.has("hits") && search.getJSONArray("hits").length() > 0) {
@@ -56,11 +58,6 @@ public class ChileParisCrawler extends CrawlerRankingKeywords {
 
   }
 
-  @Override
-  protected boolean hasNextPage() {
-    return true;
-  }
-
   protected void setTotalProducts(JSONObject search) {
     if (search.has("total") && search.get("total") instanceof Integer) {
       this.totalProducts = search.getInt("total");
@@ -95,8 +92,7 @@ public class ChileParisCrawler extends CrawlerRankingKeywords {
 
     Map<String, String> headers = new HashMap<>();
     headers.put("Content-Type", "application/json");
-    headers.put("authority", "www.paris.cl");
-    headers.put("Referer", "https://www.paris.cl/store/search");
+    headers.put("Content-Encoding", "");
 
     String payload;
 
@@ -116,23 +112,41 @@ public class ChileParisCrawler extends CrawlerRankingKeywords {
           + "{\"field\":\"delivery.keyword\",\"size\":1000,\"order\":{\"_key\":\"asc\"}}},\"cat_1.raw\":{\"terms\":{\"field\":\"cat_1.raw\","
           + "\"size\":250,\"order\":{\"_key\":\"asc\"}}}}}";
     } else {
-      headers.put("Referer", "https://www.paris.cl/store/search?query=smartphone+blanco&sort=_score-desc&cur_pos=28");
-      payload =
-          "{\"query\":{\"function_score\":{\"query\":{\"bool\":{\"must\":[{\"bool\":{\"should\":[{\"function_score\":{\"query\":{\"multi_match\":{\"query\":\""
-              + this.location
-              + "\",\"fields\":[\"name^1000\",\"brand\",\"cat_3.stop\"],\"type\":\"best_fields\",\"operator\":\"and\"}},\"field_value_factor\":{\"field\":\"boost\",\"factor\":6}}},{\"multi_match\":{\"query\":\""
-              + this.location
-              + "\",\"fields\":[\"name^8\",\"cat_3.stop\"],\"type\":\"best_fields\",\"operator\":\"or\"}},{\"span_first\":{\"match\":{\"span_term\":{\"name.dym\":\""
-              + this.location
-              + "\"}},\"end\":1,\"boost\":2000}}],\"minimum_should_match\":\"1\"}}]}},\"boost_mode\":\"sum\",\"field_value_factor\":{\"field\":\"boost\",\"factor\":0}}},\"size\":30,\"from\":30,\"sort\":[{\"_score\":{\"order\":\"desc\"}}],\"aggs\":{\"specs\":{\"nested\":{\"path\":\"specs\"},\"aggs\":{\"key\":{\"terms\":{\"field\":\"specs.key\"},\"aggs\":{\"value\":{\"terms\":{\"field\":\"specs.value\",\"size\":100,\"order\":{\"_key\":\"asc\"}}}}}}},\"brands\":{\"terms\":{\"field\":\"brand.keyword\",\"size\":1000,\"order\":{\"_key\":\"asc\"}}},\"price\":{\"range\":{\"field\":\"price\",\"ranges\":[{\"to\":5000},{\"from\":5001,\"to\":10000},{\"from\":10001,\"to\":20000},{\"from\":20001,\"to\":50000},{\"from\":50001,\"to\":100000},{\"from\":100001,\"to\":500000},{\"from\":500001,\"to\":1000000},{\"from\":1000001}]}},\"despacho\":{\"terms\":{\"field\":\"delivery.keyword\",\"size\":1000,\"order\":{\"_key\":\"asc\"}}}}";
+      payload = "{\"query\":{\"function_score\":{\"query\":{\"bool\":{\"must\":[{\"bool\":{\"should\":[{\"function_score\":"
+          + "{\"query\":{\"multi_match\":{\"query\":\"" + this.location + "\",\"fields\":[\"name^1000\",\"brand\",\"cat_3.stop\"],"
+          + "\"type\":\"best_fields\",\"operator\":\"and\"}},\"field_value_factor\":{\"field\":\"boost\",\"factor\":6}}},{\"multi_match\":"
+          + "{\"query\":\"" + this.location + "\",\"fields\":[\"name^8\",\"cat_3.stop\"],\"type\":\"best_fields\",\"operator\":\"or\"}},"
+          + "{\"span_first\":{\"match\":{\"span_term\":{\"name.dym\":\"" + this.location
+          + "\"}},\"end\":1,\"boost\":2000}}],\"minimum_should_match\":"
+          + "\"1\"}}]}},\"boost_mode\":\"sum\",\"field_value_factor\":{\"field\":\"boost\",\"factor\":0}}},\"size\":30,\"from\":"
+          + this.arrayProducts.size() + ",\"sort\":"
+          + "[{\"_score\":{\"order\":\"desc\"}}],\"aggs\":{\"specs\":{\"nested\":{\"path\":\"specs\"},\"aggs\":{\"key\":{\"terms\":{\"field\":"
+          + "\"specs.key\"},\"aggs\":{\"value\":{\"terms\":{\"field\":\"specs.value\",\"size\":100,\"order\":{\"_key\":\"asc\"}}}}}}},\"brands\":"
+          + "{\"terms\":{\"field\":\"brand.keyword\",\"size\":1000,\"order\":{\"_key\":\"asc\"}}},\"price\":{\"range\":{\"field\":\"price\","
+          + "\"ranges\":[{\"to\":5000},{\"from\":5001,\"to\":10000},{\"from\":10001,\"to\":20000},{\"from\":20001,\"to\":50000},{\"from\":50001,"
+          + "\"to\":100000},{\"from\":100001,\"to\":500000},{\"from\":500001,\"to\":1000000},{\"from\":1000001}]}},\"despacho\":{\"terms\":"
+          + "{\"field\":\"delivery.keyword\",\"size\":1000,\"order\":{\"_key\":\"asc\"}}}}}";
     }
 
-    String content = POSTFetcher.fetchPagePOSTWithHeaders(url, session, payload, cookies, 1, headers,
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36", null);
-    JSONObject json = CrawlerUtils.stringToJson(content);
+    JSONObject payloaFetcher = POSTFetcher.fetcherPayloadBuilder(url, "POST", true, payload, headers, null, null);
+    JSONObject fetcherReponse = new JSONObject();
+    try {
+      fetcherReponse = POSTFetcher.requestWithFetcher(session, payloaFetcher, false);
+    } catch (Exception e) {
+      this.logError(CommonMethods.getStackTrace(e));
+    }
 
-    if (json.has("hits")) {
-      searchApi = json.getJSONObject("hits");
+    if (fetcherReponse.has("response") && fetcherReponse.has("request_status_code") && fetcherReponse.getInt("request_status_code") >= 200
+        && fetcherReponse.getInt("request_status_code") < 400) {
+      JSONObject responseJson = fetcherReponse.getJSONObject("response");
+
+      if (responseJson.has("body")) {
+        JSONObject json = CrawlerUtils.stringToJson(responseJson.get("body").toString());
+
+        if (json.has("hits")) {
+          searchApi = json.getJSONObject("hits");
+        }
+      }
     }
 
     return searchApi;
