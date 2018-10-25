fc42beb9d667f8fd4cbc889e1f220cde41f4a6f2
gabriel <gabrieldornelas@lett.digital>
2018-10-25 16:08:35 -0300

    new crawler ranking epoca [DQ-278]
    Former-commit-id: ba9da4e7eddd10cf12e909b85fd9a05505d66d08

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilEpocacosmeticosCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilEpocacosmeticosCrawler.java
@@ -1,127 +1,106 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
 
-import java.util.ArrayList;
-import java.util.List;
-import org.apache.http.cookie.Cookie;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import java.util.HashMap;
+import java.util.Map;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import br.com.lett.crawlernode.core.fetcher.DataFetcher;
+import br.com.lett.crawlernode.core.fetcher.methods.POSTFetcher;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
-import br.com.lett.crawlernode.util.CommonMethods;
+import br.com.lett.crawlernode.util.CrawlerUtils;
 
 public class BrasilEpocacosmeticosCrawler extends CrawlerRankingKeywords {
-  
+
   public BrasilEpocacosmeticosCrawler(Session session) {
     super(session);
   }
-  
-  private List<Cookie> cookies = new ArrayList<>();
-  
+
   @Override
-  protected void extractProductsFromCurrentPage() {
+  public void extractProductsFromCurrentPage() {
     this.pageSize = 24;
+
     this.log("Página " + this.currentPage);
-    
-    String url = "https://busca.epocacosmeticos.com.br/busca?q=" + this.keywordEncoded + "&page=" + this.currentPage;
-    this.log("Link onde são feitos os crawlers: " + url);
-    
-    this.currentDoc = fetchDocument(url, cookies);
-    
-    Elements products = this.currentDoc.select(".nm-product-item");
-    
-    if (!products.isEmpty()) {
-      
+
+    JSONObject search = crawlSearchApi();
+
+    if (search.has("docs") && search.getJSONArray("docs").length() > 0) {
+      JSONArray products = search.getJSONArray("docs");
+
       if (this.totalProducts == 0) {
-        setTotalProducts();
+        setTotalProducts(search);
       }
-      
-      for (Element e : products) {
-        // InternalPid
-        String internalPid = crawlInternalPid(e);
-        
-        // InternalId
-        String internalId = crawlInternalId(e);
-        
-        // Url do produto
-        String productUrl = crawlProductUrl(e);
-        
-        saveDataProduct(internalId, internalPid, productUrl);
-        
-        this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + internalPid + " - Url: " + productUrl);
+
+      for (int i = 0; i < products.length(); i++) {
+        JSONObject product = products.getJSONObject(i);
+
+        String productUrl = crawlProductUrl(product);
+        String internalPid = crawlInternalPid(product);
+
+        saveDataProduct(null, internalPid, productUrl);
+
+        this.log("Position: " + this.position + " - InternalId: " + null + " - InternalPid: " + internalPid + " - Url: " + productUrl);
+
         if (this.arrayProducts.size() == productsLimit) {
           break;
         }
-        
       }
-      
     } else {
       this.result = false;
       this.log("Keyword sem resultado!");
     }
-    
+
     this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
+
   }
-  
-  @Override
-  protected boolean hasNextPage() {
-    if (this.arrayProducts.size() < this.totalProducts) {
-      return true;
-    }
-    
-    return false;
-  }
-  
-  @Override
-  protected void setTotalProducts() {
-    
-    Element totalProducts = this.currentDoc.selectFirst(".neemu-total-products-container");
-    
-    if (totalProducts != null) {
-      String text = totalProducts.text().replaceAll("[^0-9]", "").trim();
-      
-      if (!text.isEmpty()) {
-        this.totalProducts = Integer.parseInt(text);
-      }
-      
+
+  protected void setTotalProducts(JSONObject search) {
+    if (search.has("numFound") && search.get("numFound") instanceof Integer) {
+      this.totalProducts = search.getInt("numFound");
       this.log("Total da busca: " + this.totalProducts);
     }
   }
-  
-  private String crawlInternalId(Element e) {
-    return null;
-  }
-  
-  private String crawlInternalPid(Element e) {
+
+  private String crawlInternalPid(JSONObject product) {
     String internalPid = null;
-    
-    Element pid = e.selectFirst(".nm-product-item");
-    
-    if (pid != null) {
-      
-      String text = pid.attr("id");
-      
-      if (text.contains("-")) {
-        internalPid = CommonMethods.getLast(text.split("-")).trim();
-      }
+
+    if (product.has("id")) {
+      internalPid = product.get("id").toString();
     }
+
     return internalPid;
   }
-  
-  
-  private String crawlProductUrl(Element e) {
-    String productUrl = null;
-    Element eUrl = e.select(".nm-product-name a").first();
-    
-    if (eUrl != null) {
-      productUrl = eUrl.attr("href").trim();
-      
-      if (!productUrl.startsWith("http")) {
-        productUrl = "https:" + productUrl;
+
+  private String crawlProductUrl(JSONObject product) {
+    String urlProduct = null;
+
+    if (product.has("linkId")) {
+      urlProduct = product.getString("linkId");
+    }
+
+    return urlProduct;
+  }
+
+  private JSONObject crawlSearchApi() {
+    JSONObject searchApi = new JSONObject();
+    String url = "https://recs.richrelevance.com/rrserver/api/find/v1/c85912f892c73e30?lang=pt" + "&query=" + this.keywordEncoded
+        + "&log=true&userId=&placement=search_page.find" + "&start=" + this.arrayProducts.size() + "&rows=24";
+    this.log("Link onde são feitos os crawlers: " + url);
+
+    Map<String, String> headers = new HashMap<>();
+    headers.put("Content-Type", "application/json");
+
+    JSONObject json =
+        CrawlerUtils.stringToJson(POSTFetcher.requestUsingFetcher(url, cookies, headers, null, DataFetcher.GET_REQUEST, session, false));
+
+    if (json.has("placements")) {
+      JSONArray placements = json.getJSONArray("placements");
+
+      if (placements.length() > 0) {
+        searchApi = placements.getJSONObject(0);
       }
     }
-    
-    return productUrl;
+
+    return searchApi;
   }
-  
-  
 }
