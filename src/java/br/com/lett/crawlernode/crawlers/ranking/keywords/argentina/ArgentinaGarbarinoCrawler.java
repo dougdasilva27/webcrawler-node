bd355a1aa64ed4aad95b6d3d956bc9a62228ebf1
gabriel <gabrieldornelas@lett.digital>
2018-11-09 17:58:59 -0200

    bugfix ranking [DQ-345]
    Former-commit-id: a80ba5dfc5ad3958c5ca5c823c486bc18e2d31d2

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/argentina/ArgentinaGarbarinoCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/argentina/ArgentinaGarbarinoCrawler.java
@@ -7,91 +7,92 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.util.CrawlerUtils;
 
 public class ArgentinaGarbarinoCrawler extends CrawlerRankingKeywords {
-  
+
   public ArgentinaGarbarinoCrawler(Session session) {
     super(session);
   }
-  
+
   private List<Cookie> cookies = new ArrayList<>();
-  
+
   @Override
   protected void extractProductsFromCurrentPage() {
     // número de produtos por página do market
     this.pageSize = 48;
-    
+
     this.log("Página " + this.currentPage);
-    
+
     String url = "https://www.garbarino.com/q/" + this.keywordWithoutAccents.replace(" ", "%20") + "/srch?page=" + this.currentPage + "&q="
         + this.keywordWithoutAccents.replace(" ", "%20");
     this.log("Link onde são feitos os crawlers: " + url);
-    
+
     this.currentDoc = fetchDocument(url, cookies);
     Elements products = this.currentDoc.select(".itemBox");
-    
+
     if (!products.isEmpty()) {
       if (this.totalProducts == 0) {
         setTotalProducts();
       }
-      
+
       for (Element e : products) {
         String productUrl = crawlProductUrl(e);
         String internalId = crawlInternalId(e);
-        
+
         saveDataProduct(internalId, null, productUrl);
-        
+
         this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + null + " - Url: " + productUrl);
         if (this.arrayProducts.size() == productsLimit) {
           break;
         }
-        
+
       }
     } else {
       this.result = false;
       this.log("Keyword sem resultado!");
     }
-    
+
     this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
   }
-  
+
   @Override
   protected void setTotalProducts() {
     Element totalElement = this.currentDoc.selectFirst(".breadcrumb-item--active > span");
-    
+
     if (totalElement != null) {
       String html = totalElement.text().replaceAll("[^0-9]", "");
-      
+
       if (!html.isEmpty()) {
         this.totalProducts = Integer.parseInt(html);
       }
-      
+
       this.log("Total da busca: " + this.totalProducts);
     }
   }
-  
+
   private String crawlProductUrl(Element e) {
     String productUrl = null;
-    
-    Element urlElement = e.selectFirst("a.itemBox--info[href]");
-    
-    if (!urlElement.text().contains("www.garbarino.com")) {
-      productUrl = ("https://www.garbarino.com/" + urlElement.attr("href")).replace(".com//", ".com/");
+
+    Element urlElement = e.selectFirst(".itemBox--info a");
+
+    if (urlElement != null) {
+      productUrl = CrawlerUtils.sanitizeUrl(urlElement, "href", "https:", "www.garbarino.com");
     }
-    
+
     return productUrl;
   }
-  
+
   private String crawlInternalId(Element e) {
     String internalId = null;
-    
+
     Element idElement = e.selectFirst(".button--action__fav");
-    
+
     if (idElement != null) {
       internalId = idElement.attr("data-id");
     }
-    
+
     return internalId;
   }
-  
+
 }
