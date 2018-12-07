bb2b5851a441ab4b8a9456f85396ff62fafb0e6b
gabriel <gabrieldornelas@lett.digital>
2018-12-07 10:27:17 -0200

    new crawler mexico
    Former-commit-id: a09be8ce7ea0bdfa5aa1071b60433400d665ea1e

---
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/mexico/MexicoAmazonCrawler.java
@@ -0,0 +1,115 @@
+package br.com.lett.crawlernode.crawlers.ranking.keywords.mexico;
+
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.util.CommonMethods;
+
+public class MexicoAmazonCrawler extends CrawlerRankingKeywords {
+
+  public MexicoAmazonCrawler(Session session) {
+    super(session);
+  }
+
+  private static final String HOME_PAGE = "https://www.amazon.com.mx";
+  private String nextPageUrl;
+
+  @Override
+  protected void extractProductsFromCurrentPage() {
+    this.pageSize = 20;
+    this.log("Página " + this.currentPage);
+
+    String url;
+
+    if (this.currentPage == 1) {
+      url = "https://www.amazon.com.mx/s/ref=nb_sb_noss?url=search-alias%3Daps&page=" + this.currentPage + "&keywords=" + this.keywordEncoded;
+    } else {
+      url = this.nextPageUrl;
+    }
+    this.log("Link onde são feitos os crawlers: " + url);
+
+    this.currentDoc = fetchDocument(url, this.cookies);
+    this.nextPageUrl = crawlNextPage();
+
+    Elements products = this.currentDoc.select(".s-result-list .s-result-item");
+    Element result = this.currentDoc.select("#noResultsTitle").first();
+
+    if (!products.isEmpty() && result == null) {
+      if (this.totalProducts == 0) {
+        setTotalProducts();
+      }
+
+      for (Element e : products) {
+
+        String internalPid = crawlInternalPid(e);
+        String internalId = internalPid;
+        String productUrl = crawlProductUrl(internalPid);
+
+        saveDataProduct(internalId, internalPid, productUrl);
+
+        this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + internalPid + " - Url: " + productUrl);
+        if (this.arrayProducts.size() == productsLimit) {
+          break;
+        }
+
+      }
+    } else {
+      this.result = false;
+      this.log("Keyword sem resultado!");
+    }
+
+    this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
+  }
+
+  private String crawlNextPage() {
+    String url = null;
+
+    Element e = this.currentDoc.select(".pagnRA > a").first();
+
+    if (e != null) {
+      url = e.attr("href");
+
+      if (url.startsWith("#")) {
+        url = url.replaceFirst("#", "");
+      }
+
+      if (!url.contains("amazon.com")) {
+        url = HOME_PAGE + url;
+      }
+    }
+
+    return url;
+  }
+
+  @Override
+  protected boolean hasNextPage() {
+    return this.currentDoc.select("#pagnNextString").first() != null;
+  }
+
+  @Override
+  protected void setTotalProducts() {
+    Element total = this.currentDoc.selectFirst("#s-result-count");
+    if (total != null) {
+      String text = total.ownText();
+
+      if (!text.contains("de")) {
+        String totalText = CommonMethods.getLast(text.split("de")).replaceAll("[^0-9]", "");
+
+        if (!totalText.isEmpty()) {
+          this.totalProducts = Integer.parseInt(totalText);
+          this.log("Total da busca: " + this.totalProducts);
+        }
+      }
+    }
+  }
+
+  private String crawlInternalPid(Element e) {
+    return e.attr("data-asin");
+  }
+
+  private String crawlProductUrl(String id) {
+    return HOME_PAGE + "/dp/" + id;
+  }
+
+}
---
+++ src/java/br/com/lett/crawlernode/crawlers/ratingandreviews/mexico/MexicoAmazonRatingReviewCrawler.java
@@ -0,0 +1,115 @@
+package br.com.lett.crawlernode.crawlers.ratingandreviews.mexico;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import br.com.lett.crawlernode.core.models.RatingReviewsCollection;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.RatingReviewCrawler;
+import br.com.lett.crawlernode.util.Logging;
+import models.RatingsReviews;
+
+/**
+ * Date: 22/06/18
+ * 
+ * @author gabriel
+ *
+ */
+public class MexicoAmazonRatingReviewCrawler extends RatingReviewCrawler {
+
+  public MexicoAmazonRatingReviewCrawler(Session session) {
+    super(session);
+  }
+
+  @Override
+  protected RatingReviewsCollection extractRatingAndReviews(Document document) throws Exception {
+    RatingReviewsCollection ratingReviewsCollection = new RatingReviewsCollection();
+
+    if (isProductPage(document)) {
+      Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
+
+      RatingsReviews ratingReviews = new RatingsReviews();
+      ratingReviews.setDate(session.getDate());
+
+      String internalId = crawlInternalId(document);
+
+      Integer totalNumOfEvaluations = getTotalNumOfRatings(document);
+      Double avgRating = getTotalAvgRating(document);
+
+      ratingReviews.setInternalId(internalId);
+      ratingReviews.setTotalRating(totalNumOfEvaluations);
+      ratingReviews.setTotalWrittenReviews(totalNumOfEvaluations);
+      ratingReviews.setAverageOverallRating(avgRating);
+
+      ratingReviewsCollection.addRatingReviews(ratingReviews);
+    } else {
+      Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
+    }
+
+    return ratingReviewsCollection;
+
+  }
+
+  private String crawlInternalId(Document doc) {
+    String internalId = null;
+
+    Element internalIdElement = doc.select("input[name^=ASIN]").first();
+
+    if (internalIdElement != null) {
+      internalId = internalIdElement.val();
+    }
+
+    return internalId;
+  }
+
+  /**
+   * Avg appear in html element
+   * 
+   * @param document
+   * @return
+   */
+  private Double getTotalAvgRating(Document doc) {
+    Double avgRating = 0d;
+    Element reviews = doc.select("#reviewsMedley .arp-rating-out-of-text").first();
+
+    if (reviews != null) {
+      String text = reviews.ownText().trim();
+
+      if (text.contains("de")) {
+        String avgText = text.split("de")[0].replaceAll("[^0-9.]", "").trim();
+
+        if (!avgText.isEmpty()) {
+          avgRating = Double.parseDouble(avgText);
+        }
+      }
+    }
+
+    return avgRating;
+  }
+
+  /**
+   * Number of ratings appear in html element
+   * 
+   * @param doc
+   * @return
+   */
+  private Integer getTotalNumOfRatings(Document doc) {
+    Integer ratingNumber = 0;
+    Element reviews = doc.select("#acrCustomerReviewText").first();
+
+    if (reviews != null) {
+      String text = reviews.ownText().replaceAll("[^0-9]", "").trim();
+
+      if (!text.isEmpty()) {
+        ratingNumber = Integer.parseInt(text);
+      }
+    }
+
+    return ratingNumber;
+  }
+
+
+  private boolean isProductPage(Document doc) {
+    return doc.select("#dp").first() != null;
+  }
+
+}
