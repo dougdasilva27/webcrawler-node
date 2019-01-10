87b546573587e275c1d2acf4bc99b28bfaadb1ab
Joao Pedro <joaopedro@lett.digital>
2019-01-10 10:05:38 -0200

    Fixing internalId from crawler content
    Former-commit-id: 2882be7b0fe6c68d7196f3e92155b9d52f9925f9

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/belohorizonte/BelohorizonteBhvidaCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/belohorizonte/BelohorizonteBhvidaCrawler.java
@@ -26,31 +26,38 @@ public class BelohorizonteBhvidaCrawler extends Crawler {
     super(session);
     // TODO Auto-generated constructor stub
   }
+
   @Override
   public List<Product> extractInformation(Document doc) throws Exception {
     super.extractInformation(doc);
     List<Product> products = new ArrayList<>();
 
     if (isProductPage(doc)) {
-      Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
+      Logging.printLogDebug(logger, session,
+          "Product page identified: " + this.session.getOriginalURL());
 
-      String internalId  = crawlInternalId(doc);
+      String internalId = crawlInternalId(doc);
       String internalPid = crawlInternalPid(doc);
-      boolean available = crawlAvailability(doc);      
-      
+      boolean available = crawlAvailability(doc);
+
       String name = CrawlerUtils.scrapStringSimpleInfo(doc, "#detalhes-mini > ul > li h1", true);
-      Float price = available ? CrawlerUtils.scrapSimplePriceFloat(doc, ".preco strong", true) : null;
-      CategoryCollection categories = CrawlerUtils.crawlCategories(doc, ".produtos a:not(:first-child)");
-      
+      Float price =
+          available ? CrawlerUtils.scrapSimplePriceFloat(doc, ".preco strong", true) : null;
+      CategoryCollection categories =
+          CrawlerUtils.crawlCategories(doc, ".produtos a:not(:first-child)");
+
       Prices prices = crawlPrices(price, doc);
       String description = crawlDescription(doc);
-      String primaryImage = CrawlerUtils.scrapSimplePrimaryImage(doc, "img", Arrays.asList("src"), "https:", "www.bhvida.com/");
+      String primaryImage = CrawlerUtils.scrapSimplePrimaryImage(doc, "img", Arrays.asList("src"),
+          "https:", "www.bhvida.com/");
 
 
-      // Creating the product   
-      Product product = ProductBuilder.create().setUrl(session.getOriginalURL()).setInternalId(internalId).setInternalPid(internalPid).setName(name).setPrice(price)
-          .setPrices(prices).setAvailable(available).setCategory1(categories.getCategory(0)).setCategory2(categories.getCategory(1))
-          .setCategory3(categories.getCategory(2)).setPrimaryImage(primaryImage).setSecondaryImages(null).setDescription(description)
+      // Creating the product
+      Product product = ProductBuilder.create().setUrl(session.getOriginalURL())
+          .setInternalId(internalId).setInternalPid(internalPid).setName(name).setPrice(price)
+          .setPrices(prices).setAvailable(available).setCategory1(categories.getCategory(0))
+          .setCategory2(categories.getCategory(1)).setCategory3(categories.getCategory(2))
+          .setPrimaryImage(primaryImage).setSecondaryImages(null).setDescription(description)
           .setMarketplace(new Marketplace()).build();
 
       products.add(product);
@@ -64,15 +71,15 @@ public class BelohorizonteBhvidaCrawler extends Crawler {
   }
 
   private String crawlDescription(Document doc) {
-    List <String> selectors = new ArrayList<>();
+    List<String> selectors = new ArrayList<>();
     selectors.add("#detalhes-mini ul li p");
     selectors.add("#detalhes-mini ul li  .bloco p");
     selectors.add("#detalhes-mini ul li  .bloco table");
-    
+
     return CrawlerUtils.scrapSimpleDescription(doc, selectors);
   }
-  
-  private boolean isProductPage(Element doc) {    
+
+  private boolean isProductPage(Element doc) {
     return !doc.select(".det_carrinho").isEmpty();
   }
 
@@ -80,30 +87,37 @@ public class BelohorizonteBhvidaCrawler extends Crawler {
     String internalPid = null;
 
     Element searchedId = doc.selectFirst("#detalhes-mini > ul > li:last-child");
-    if(searchedId != null) {
+    if (searchedId != null) {
       internalPid = searchedId.ownText();
     }
 
     return internalPid;
   }
-  
+
   private String crawlInternalId(Element doc) {
     String internalId = null;
 
     Element searchedId = doc.selectFirst("#frmcarrinho #codigo");
-    
-    if(searchedId != null) {
+
+    if (searchedId != null) {
       internalId = searchedId.val().trim();
     } else {
       searchedId = doc.selectFirst(".aviseme button");
-      String input = searchedId.attr("onclick");
-      internalId = input.substring(input.indexOf("'")+1, input.lastIndexOf("'"));
+      if (searchedId != null) {
+        String input = searchedId.attr("onclick");
+
+        if (input.contains("'")) {
+          internalId = input.substring(input.indexOf("'") + 1, input.lastIndexOf("'"));
+
+        }
+
+      }
     }
 
     return internalId;
-  }  
-  
-  private boolean crawlAvailability(Element doc) {    
+  }
+
+  private boolean crawlAvailability(Element doc) {
     return !doc.select("#b_estq").isEmpty();
   }
 
@@ -117,15 +131,15 @@ public class BelohorizonteBhvidaCrawler extends Crawler {
   private Prices crawlPrices(Float price, Element doc) {
     Prices prices = new Prices();
     Element offer = doc.selectFirst(".preco .oferta");
-    
+
     if (price != null) {
       Map<Integer, Float> installmentPriceMapShop = new HashMap<>();
       Map<Integer, Float> installmentPriceMap = new HashMap<>();
-      
+
       installmentPriceMap.put(1, price);
-      
+
       Elements elements = doc.select(".parcelamento ul > li");
-      
+
       for (Element element : elements) {
         Pair<Integer, Float> pair = CrawlerUtils.crawlSimpleInstallment(null, element, true);
 
@@ -133,16 +147,16 @@ public class BelohorizonteBhvidaCrawler extends Crawler {
           installmentPriceMapShop.put(pair.getFirst(), pair.getSecond());
         }
       }
-      
-      if(offer != null) {
+
+      if (offer != null) {
         prices.setPriceFrom(CrawlerUtils.scrapSimplePriceDouble(doc, ".preco .oferta", false));
       }
-      
-      if(crawlAvailability(doc)) {        
+
+      if (crawlAvailability(doc)) {
         prices.setBankTicketPrice(price);
         prices.insertCardInstallment(Card.SHOP_CARD.toString(), installmentPriceMapShop);
       }
-      
+
     }
 
     return prices;
--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/belohorizonte/BelohorizonteBhvidaCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/belohorizonte/BelohorizonteBhvidaCrawler.java
@@ -7,7 +7,7 @@ import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
 import br.com.lett.crawlernode.util.CommonMethods;
 import br.com.lett.crawlernode.util.CrawlerUtils;
 
-public class BelohorizonteBhvidaCrawler  extends CrawlerRankingKeywords {
+public class BelohorizonteBhvidaCrawler extends CrawlerRankingKeywords {
 
   public BelohorizonteBhvidaCrawler(Session session) {
     super(session);
@@ -16,94 +16,100 @@ public class BelohorizonteBhvidaCrawler  extends CrawlerRankingKeywords {
   @Override
   protected void extractProductsFromCurrentPage() {
     this.pageSize = 36;
-    
-    this.log("Página "+ this.currentPage);
-    
-    String url = "https://www.bhvida.com/produto.php?LISTA=procurar&PROCURAR="+ this.keywordEncoded +"&pg=" + this.currentPage;
-    this.log("Link onde são feitos os crawlers: "+url); 
-    
-    this.currentDoc = fetchDocument(url);      
 
-    Elements products =  this.currentDoc.select("#produtos li");
-    if(products.size() >= 1) {          
-      
-      if(this.totalProducts == 0) 
+    this.log("Página " + this.currentPage);
+
+    String url = "https://www.bhvida.com/produto.php?LISTA=procurar&PROCURAR=" + this.keywordEncoded
+        + "&pg=" + this.currentPage;
+    this.log("Link onde são feitos os crawlers: " + url);
+
+    this.currentDoc = fetchDocument(url);
+
+    Elements products = this.currentDoc.select("#produtos li");
+    if (products.size() >= 1) {
+
+      if (this.totalProducts == 0)
         setTotalProducts();
-      
+
       for (Element e : products) {
-        String productUrl  = crawlUrl(e);
+        String productUrl = crawlUrl(e);
         String internalPid = crawlInternalPid(productUrl, e);
         saveDataProduct(null, internalPid, productUrl);
-        this.log("Position: " + this.position + " - InternalId: " + null + " - InternalPid: " + internalPid + " - Url: " + productUrl);
-        if(this.arrayProducts.size() == productsLimit) break;
-        
+        this.log("Position: " + this.position + " - InternalId: " + null + " - InternalPid: "
+            + internalPid + " - Url: " + productUrl);
+        if (this.arrayProducts.size() == productsLimit)
+          break;
+
       }
-      
+
     } else {
-      
+
       this.result = false;
       this.log("Keyword sem resultado!");
     }
-    
+
   }
- 
+
   private String crawlUrl(Element e) {
     String productUrl = null;
     String rel = e.attr("rel");
     Element imgSrc = e.selectFirst(".moldura-p img");
-    
-    if(imgSrc != null && !rel.isEmpty()) {
-      productUrl = CrawlerUtils.completeUrl(rel, "https:", "www.bhvida.com/");    
+
+    if (imgSrc != null && !rel.isEmpty()) {
+      productUrl = CrawlerUtils.completeUrl(rel, "https:", "www.bhvida.com/");
       String newPid = CommonMethods.getLast(imgSrc.attr("src").split("="));
-      String oldPid = getIdFromUrl(productUrl);      
+      String oldPid = getIdFromUrl(productUrl);
       productUrl = productUrl.replace(oldPid, newPid);
     }
-   
+
     return productUrl;
   }
-  
-  
+
+
   private String crawlInternalPid(String url, Element e) {
     String internalPid = null;
     String rel = e.attr("rel");
 
-    if(!rel.isEmpty() && getIdFromUrl(url).contains("sem_imagem")) {
-      
+    if (!rel.isEmpty() && getIdFromUrl(url).contains("sem_imagem")) {
+
       String newUrl = CrawlerUtils.completeUrl(rel, "https:", "www.bhvida.com/");
       internalPid = getIdFromUrl(newUrl);
-      
+
     } else {
-      
+
       internalPid = getIdFromUrl(url);
-      
+
     }
-    
+
     return internalPid;
   }
 
-  private String getIdFromUrl (String url) {
-    String id = null;    
-    
-    if(!url.isEmpty()) {
-      
-      String lastPart = CommonMethods.getLast(url.split("\\?")[0].split("-"));       
-      
-      if(lastPart.contains(".")) {
-        
+  private String getIdFromUrl(String url) {
+    String id = null;
+
+    if (!url.isEmpty()) {
+
+      String lastPart = CommonMethods.getLast(url.split("\\?")[0].split("-"));
+
+      if (lastPart.contains(".")) {
+
         id = lastPart.split("\\.")[0];
-        
+
       } else {
-        
+
         id = lastPart;
-        
-      }      
+
+      }
     }
     return id;
   }
-  
+
   @Override
   protected void setTotalProducts() {
-    this.totalProducts = CrawlerUtils.scrapTotalProductsForRanking(this.currentDoc, "#paginacao .texto strong:first-child", false);
+    this.totalProducts = CrawlerUtils.scrapTotalProductsForRanking(this.currentDoc,
+        "#paginacao .texto strong:first-child", false);
+    this.log("Total da busca: " + this.totalProducts);
+
   }
-  
+
 }
