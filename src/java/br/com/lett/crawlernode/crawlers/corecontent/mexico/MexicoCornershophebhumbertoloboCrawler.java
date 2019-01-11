b346a4e94d8487f682d13f7d1b763d7cbff95c54
Joao Pedro <joaopedro@lett.digital>
2019-01-11 11:06:41 -0200

    Creating crawler corneshop heb mexico humberto lobo
    Former-commit-id: a9ff4bd2ea6c3649babe14391a8b5034f6883787

---
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/mexico/MexicoCornershophebhumbertoloboCrawler.java
@@ -0,0 +1,40 @@
+package br.com.lett.crawlernode.crawlers.corecontent.mexico;
+
+import java.util.List;
+import org.json.JSONObject;
+import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.Crawler;
+import br.com.lett.crawlernode.crawlers.corecontent.extractionutils.CornershopCrawler;
+
+public class MexicoCornershophebhumbertoloboCrawler extends Crawler {
+
+  private static final String HOME_PAGE = "https://web.cornershopapp.com";
+  public static final String STORE_ID = "199";
+
+  private CornershopCrawler cornerCrawler =
+      new CornershopCrawler(session, STORE_ID, logger, cookies);
+
+
+  public MexicoCornershophebhumbertoloboCrawler(Session session) {
+    super(session);
+  }
+
+  @Override
+  public boolean shouldVisit() {
+    String href = this.session.getOriginalURL().toLowerCase();
+    return !FILTERS.matcher(href).matches() && (href.startsWith(HOME_PAGE));
+  }
+
+  @Override
+  protected Object fetch() {
+    return cornerCrawler.fetch();
+  }
+
+  @Override
+  public List<Product> extractInformation(JSONObject jsonSku) throws Exception {
+    super.extractInformation(jsonSku);
+
+    return cornerCrawler.extractInformation(jsonSku);
+  }
+}
