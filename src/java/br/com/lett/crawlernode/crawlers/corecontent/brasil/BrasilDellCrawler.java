074295fa722ae95df6400ae398af9bc0f240e1de
gabriel <gabrieldornelas@lett.digital>
2019-02-27 10:20:04 -0300

    FINISHING crawlers dell
    Former-commit-id: 8dbcfc0ab3cd3e604982c01ef5e4ee34078caab6

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilDellCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilDellCrawler.java
@@ -1,13 +1,15 @@
 package br.com.lett.crawlernode.crawlers.corecontent.brasil;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import br.com.lett.crawlernode.core.fetcher.DataFetcher;
 import br.com.lett.crawlernode.core.models.Card;
 import br.com.lett.crawlernode.core.models.CategoryCollection;
 import br.com.lett.crawlernode.core.models.Product;
@@ -43,32 +45,61 @@ public class BrasilDellCrawler extends Crawler {
 
   @Override
   public List<Product> extractInformation(Document doc) throws Exception {
-    super.extractInformation(doc);
     List<Product> products = new ArrayList<>();
+
+    JSONObject infoJson = CrawlerUtils.selectJsonFromHtml(doc, "script", "Dell.Services.DataModel=", "};", true, false);
+
+    if (infoJson.has("ProductMicroItems")) {
+      JSONArray productsArray = infoJson.getJSONArray("ProductMicroItems");
+      boolean hasVariations = productsArray.length() > 1;
+
+      for (Object o : productsArray) {
+        JSONObject productJson = (JSONObject) o;
+
+        if (productJson.has("Mpn") && productJson.has("Url")) {
+          String internalId = productJson.get("Mpn").toString();
+          String newUrl = CrawlerUtils.completeUrl(productJson.get("Url").toString(), "https", "www.dell.com");
+          Document newDoc = hasVariations ? DataFetcher.fetchDocument(DataFetcher.GET_REQUEST, session, newUrl, null, cookies) : doc;
+
+          products.add(extractProduct(newDoc, internalId, newUrl));
+        }
+      }
+    } else {
+      Logging.printLogDebug(logger, session, "Not a product page: " + this.session.getOriginalURL());
+    }
+
+    return products;
+  }
+
+
+  public Product extractProduct(Document doc, String internalId, String url) throws Exception {
+    super.extractInformation(doc);
+    Product product = new Product();
 
     if (isProductPage(doc)) {
       Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
 
-      String internalId = crawlInternalId(doc);
-      String name = crawlName(doc);
+      String name = CrawlerUtils.scrapStringSimpleInfo(doc, "#sharedPdPageProductTitle", true);
       CategoryCollection categories = CrawlerUtils.crawlCategories(doc, ".breadcrumbs > ol > li:not(:first-child) > a");
-      String primaryImage = crawlPrimaryImage(doc);
-      String secondaryImages = crawlSecondaryImages(doc);
-      String description = crawlDescription(doc);
+      String primaryImage = CrawlerUtils.scrapSimplePrimaryImage(doc, ".slides img, #product-detail-feature-container img",
+          Arrays.asList("data-blzsrc", "data-original", "src"), "https", "i.dell.com");
+      String secondaryImages = CrawlerUtils.scrapSimpleSecondaryImages(doc, ".slides img", Arrays.asList("data-blzsrc", "data-original", "src"),
+          "https", "i.dell.com", primaryImage);
+      String description = CrawlerUtils.scrapSimpleDescription(doc,
+          Arrays.asList("div.xs-text-centered > p", "span.marketing-blurb", "#overview> div.xs-top-offset-medium > div.row.xs-pad-offset-15",
+              "#product-detail-feature-container > div.md-pad-right-30 > div.row.xs-pad-offset-15"));
       Float price = crawlPrice(doc);
       Prices prices = crawlPrices(doc, price);
       boolean available = price != null;
 
-      Product product = ProductBuilder.create().setUrl(session.getOriginalURL()).setInternalId(internalId).setName(name).setPrice(price)
-          .setPrices(prices).setAvailable(available).setCategory1(categories.getCategory(0)).setCategory2(categories.getCategory(1))
-          .setCategory3(categories.getCategory(2)).setPrimaryImage(primaryImage).setSecondaryImages(secondaryImages).setDescription(description)
-          .build();
+      product = ProductBuilder.create().setUrl(url).setInternalId(internalId).setName(name).setPrice(price).setPrices(prices).setAvailable(available)
+          .setCategory1(categories.getCategory(0)).setCategory2(categories.getCategory(1)).setCategory3(categories.getCategory(2))
+          .setPrimaryImage(primaryImage).setSecondaryImages(secondaryImages).setDescription(description).build();
 
-      products.add(product);
     } else {
       Logging.printLogDebug(logger, session, "Not a product page: " + this.session.getOriginalURL());
     }
-    return products;
+    return product;
   }
 
   /**
@@ -79,134 +110,6 @@ public class BrasilDellCrawler extends Crawler {
    */
   private boolean isProductPage(Document doc) {
     return doc.selectFirst("meta[content=productdetails]") != null;
-  }
-
-  /**
-   * Gets the sku pid.
-   * 
-   * @param doc - contais html from the page to be scrapped
-   * @return the extracted pid from the URL.
-   */
-  private String crawlInternalId(Document doc) {
-    String internalId = null;
-
-    Element metaProducts = doc.selectFirst("meta[name=products]");
-    if (metaProducts != null) {
-      internalId = metaProducts.attr("content").replace(";", "");
-    } else {
-      Element metaSnpsku = doc.selectFirst("meta[name=snpsku]");
-
-      if (metaSnpsku != null) {
-        internalId = metaSnpsku.attr("content").replace(";", "");
-      }
-    }
-
-    if (internalId == null || internalId.isEmpty()) {
-      Element metaProductID = doc.selectFirst("meta[name=ProductId]");
-
-      if (metaProductID != null) {
-        internalId = metaProductID.attr("content");
-      }
-    }
-
-    return internalId;
-  }
-
-  /**
-   * Gets the sku Name
-   * 
-   * @param doc - contais html from the page to be scrapped
-   * @return the sku Name
-   */
-  private String crawlName(Document doc) {
-    String name = null;
-    Element nameElement = doc.selectFirst("title");
-
-    if (nameElement != null) {
-      name = nameElement.text();
-    }
-    return name;
-  }
-
-  /**
-   * Get the primary image from the sku.
-   * 
-   * @param doc - the html data to be scrapped
-   * @return - the primary sku image
-   */
-  private String crawlPrimaryImage(Document doc) {
-    String primaryImage = null;
-    Element primaryImageElement = doc.selectFirst(".slides > li > img");
-
-    // In case the sku page its the type that loads data without javascript i search for this element
-    Element imageElement = doc.selectFirst("#features-container .text-centered > img");
-
-    if (primaryImageElement != null) {
-      primaryImage = primaryImageElement.attr("data-blzsrc");
-    } else if (imageElement != null) {
-      primaryImage = imageElement.attr("data-original");
-    }
-
-    if (primaryImage != null && !primaryImage.startsWith("http")) {
-      primaryImage = "https:" + primaryImage;
-    }
-
-    return primaryImage;
-  }
-
-  /**
-   * Get the seconday images from the sku
-   * 
-   * @param doc - html to be scrapped
-   * @return array of images
-   */
-  private String crawlSecondaryImages(Document doc) {
-    String secondaryImages = null;
-    JSONArray secondaryImagesArray = new JSONArray();
-    Elements imagesElements = doc.select(".slides > li" + CrawlerUtils.CSS_SELECTOR_IGNORE_FIRST_CHILD + " > img");
-
-    for (Element e : imagesElements) {
-      String image = e.attr("data-blzsrc");
-
-      if (!image.startsWith("http")) {
-        image = "https:" + image;
-      }
-
-      secondaryImagesArray.put(image);
-    }
-
-    if (secondaryImagesArray.length() > 0) {
-      secondaryImages = secondaryImagesArray.toString();
-    }
-
-    return secondaryImages;
-  }
-
-  /**
-   * Gets the description of the SKU
-   * 
-   * @param doc - html to be scrapped
-   * @return a html formated into a string
-   */
-  private String crawlDescription(Document doc) {
-    StringBuilder description = new StringBuilder();
-
-    Element descHead = doc.selectFirst("div.xs-text-centered > p");
-
-    if (descHead != null) {
-      description.append(descHead.text());
-    }
-
-    Element descType2Head = doc.selectFirst("span.marketing-blurb");
-
-    if (descType2Head != null) {
-      description.append(descType2Head.outerHtml());
-    }
-
-    description.append(doc.select("#overview> div.xs-top-offset-medium > div.row.xs-pad-offset-15").outerHtml().trim());
-    description.append(doc.select("#product-detail-feature-container > div.md-pad-right-30 > div.row.xs-pad-offset-15").outerHtml().trim());
-
-    return description.toString();
   }
 
   /**
--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilDellCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilDellCrawler.java
@@ -8,29 +8,30 @@ import org.json.JSONObject;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
 import br.com.lett.crawlernode.util.CommonMethods;
+import br.com.lett.crawlernode.util.CrawlerUtils;
 
 public class BrasilDellCrawler extends CrawlerRankingKeywords {
-  
+
   public BrasilDellCrawler(Session session) {
     super(session);
   }
-  
+
   @Override
   protected void extractProductsFromCurrentPage() {
     // número de produtos por página do market
     this.pageSize = 8;
-    
+
     this.log("Página " + this.currentPage);
-    
+
     // monta a url com a keyword e a página
     String url = "https://pilot.search.dell.com/queryunderstandingapi/4/br/pt/19/search-v/products?term=" + this.keywordEncoded;
     String payload = createRequestPayload(this.currentPage, pageSize);
-    
+
     Map<String, String> headers = new HashMap<>();
     headers.put("content-type", "application/json");
-    
+
     this.log("Link onde são feitos os crawlers: " + url);
-    
+
     // chama função de pegar o html
     JSONObject products = new JSONObject(fetchPostFetcher(url, payload, headers, null));
     // se obter 1 ou mais links de produtos e essa página tiver resultado faça:
@@ -39,30 +40,26 @@ public class BrasilDellCrawler extends CrawlerRankingKeywords {
       if (this.totalProducts == 0) {
         setTotalBusca(products);
       }
-      
+
       Map<String, String> productsMap = crawlSkuInformations(products);
-      
+
       if (productsMap.size() >= 1) {
-        
+
         for (Entry<String, String> product : productsMap.entrySet()) {
-          
+
           // InternalPid
           String internalPid = null;
-          
+
           // InternalId
           String internalId = product.getKey();
-          
+
           // Url do produto
-          String productUrl = product.getValue();
-          if (productUrl != null && !productUrl.startsWith("http")) {
-            productUrl = "https:" + productUrl;
-          }
-          
-          
+          String productUrl = CrawlerUtils.completeUrl(product.getValue(), "https", "www.dell.com");
+
           saveDataProduct(internalId, internalPid, productUrl);
-          
+
           this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + internalPid + " - Url: " + productUrl);
-          
+
           if (this.arrayProducts.size() == productsLimit) {
             break;
           }
@@ -72,10 +69,10 @@ public class BrasilDellCrawler extends CrawlerRankingKeywords {
       this.result = false;
       this.log("Keyword sem resultado na página atual!");
     }
-    
+
     this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
   }
-  
+
   @Override
   protected boolean hasNextPage() {
     // se elemeno page obtiver algum resultado
@@ -83,26 +80,26 @@ public class BrasilDellCrawler extends CrawlerRankingKeywords {
       // tem próxima página
       return true;
     }
-    
+
     return false;
   }
-  
+
   protected void setTotalBusca(JSONObject products) {
     if (products.has("AdditionalInformation")) {
       JSONObject info = products.getJSONObject("AdditionalInformation");
-      
+
       if (info.has("ResultCount-Products")) {
         try {
           this.totalProducts = Integer.parseInt(info.getString("ResultCount-Products").trim());
         } catch (Exception e) {
           this.logError(CommonMethods.getStackTraceString(e));
         }
-        
+
         this.log("Total da busca: " + this.totalProducts);
       }
     }
   }
-  
+
   /**
    * Mapa de id e url
    * 
@@ -111,47 +108,47 @@ public class BrasilDellCrawler extends CrawlerRankingKeywords {
    */
   private Map<String, String> crawlSkuInformations(JSONObject products) {
     Map<String, String> productMap = new HashMap<>();
-    
+
     if (products.has("Results")) {
       JSONArray arrayProducts = products.getJSONArray("Results");
-      
+
       for (int i = 0; i < arrayProducts.length(); i++) {
         JSONObject product = arrayProducts.getJSONObject(i);
-        
+
         if (product.has("Data")) {
           JSONObject data = product.getJSONObject("Data");
-          
-          if (data.has("ProductId") && data.has("MoreDetailsLink") && !data.getString("ProductId").trim().isEmpty()) {
+
+          if (data.has("Id") && data.has("MoreDetailsLink") && !data.get("Id").toString().trim().isEmpty()) {
             productMap.put(data.getString("ProductId"), data.getString("MoreDetailsLink"));
           }
         }
       }
     }
-    
+
     return productMap;
   }
-  
+
   private String createRequestPayload(int page, int pageSize) {
     JSONObject jsonPayload = new JSONObject();
-    
+
     jsonPayload.put("IncludeRefiners", true);
     jsonPayload.put("IncludeCategoryTree", false);
-    
+
     JSONObject virtualAssistantData = new JSONObject();
-    
+
     virtualAssistantData.put("TemplateName", "AutoSuggest");
     virtualAssistantData.put("TemplateId", JSONObject.NULL);// The Dell API still reads the null value inside the String.
     virtualAssistantData.put("Purpose", JSONObject.NULL);
     virtualAssistantData.put("Data", new JSONObject());
-    
+
     jsonPayload.put("VirtualAssistantData", virtualAssistantData);
     jsonPayload.put("FiltersUpdatedByUser", false);
     jsonPayload.put("PreviousTerm", this.location);
     jsonPayload.put("Categories", new JSONArray());
-    
+
     JSONObject options = new JSONObject();
     JSONArray resultOptions = new JSONArray();
-    
+
     options.put("withqueryunderstandingenabled", true);
     options.put("UrlReferrer", "https://www.dell.com/pt-br");
     options.put("WithNoTrackingEnabled", false);
@@ -159,26 +156,26 @@ public class BrasilDellCrawler extends CrawlerRankingKeywords {
     options.put("IncludeGraph", false);
     options.put("IncludeSignals", false);
     options.put("IncludeTimings", false);
-    
+
     jsonPayload.put("Options", options);
     jsonPayload.put("Categories", JSONObject.NULL);
-    
+
     JSONObject profile = new JSONObject();
-    
+
     profile.put("Segment", "gen");
     profile.put("CustomerSet", "");
     profile.put("Language", "pt");
     profile.put("Country", "br");
     profile.put("STPShopEnabled", false);
-    
+
     jsonPayload.put("Profile", profile);
     jsonPayload.put("Products", new JSONArray().put(new JSONObject().put("Code", "")));
     jsonPayload.put("PreviousCategories", new JSONArray());
     jsonPayload.put("FiltersUpdatedByUser", false);
     jsonPayload.put("OverrideTerm", false);
-    
+
     jsonPayload.put("PagingOptions", new JSONObject().put("Take", this.pageSize).put("Skip", this.arrayProducts.size()));
-    
+
     return jsonPayload.toString();
   }
 }
