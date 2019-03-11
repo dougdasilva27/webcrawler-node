9633d68bfc4841b3a2bf7a6a525078a644c98e98
gabriel <gabrieldornelas@lett.digital>
2019-03-11 14:34:19 -0300

    fix drgariaminas brasil [DQ-774]
    Former-commit-id: d117a24405ac4e27db09c20a8b82dcd3dccc37f2

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilDrogarianetCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilDrogarianetCrawler.java
@@ -1,83 +1,12 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
 
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import br.com.lett.crawlernode.core.session.Session;
-import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
-import br.com.lett.crawlernode.util.CommonMethods;
-import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.crawlers.ranking.keywords.extractionutils.DrogariaMinasbrasilNetCrawler;
 
-public class BrasilDrogarianetCrawler extends CrawlerRankingKeywords {
+public class BrasilDrogarianetCrawler extends DrogariaMinasbrasilNetCrawler {
 
   public BrasilDrogarianetCrawler(Session session) {
     super(session);
-  }
-
-  @Override
-  protected void extractProductsFromCurrentPage() {
-    this.pageSize = 16;
-    this.log("Página " + this.currentPage);
-
-    String url = "https://www.drogarianet.com.br/" + this.keywordEncoded + "?p=" + this.currentPage;
-    this.log("Link onde são feitos os crawlers: " + url);
-
-    this.currentDoc = fetchDocument(url);
-
-    Elements products = this.currentDoc.select(".products .product");
-
-    if (!products.isEmpty()) {
-      if (totalProducts == 0) {
-        setTotalProducts();
-      }
-
-      for (Element e : products) {
-        String internalPid = null;
-        String internalId = crawlInternalId(e);
-        String productUrl = CrawlerUtils.scrapUrl(e, ".product-name a", "href", "https", "www.drogarianet.com.br");
-
-        saveDataProduct(internalId, internalPid, productUrl);
-
-        this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + internalPid + " - Url: " + productUrl);
-        if (this.arrayProducts.size() == productsLimit) {
-          break;
-        }
-
-      }
-    } else {
-      this.result = false;
-      this.log("Keyword sem resultado!");
-    }
-
-    this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
-  }
-
-  @Override
-  protected boolean hasNextPage() {
-    if (this.arrayProducts.size() < this.totalProducts) {
-      return true;
-    }
-
-    return false;
-  }
-
-  @Override
-  protected void setTotalProducts() {
-    this.totalProducts = CrawlerUtils.scrapIntegerFromHtml(currentDoc, ".toolbar-count span", true, 0);
-    this.log("Total products: " + this.totalProducts);
-  }
-
-  private String crawlInternalId(Element e) {
-    String internalId = null;
-    Element idElement = e.selectFirst(".buy-form");
-
-    if (idElement != null) {
-      String url = idElement.attr("action");
-
-      if (url.contains("product/")) {
-        internalId = CommonMethods.getLast(url.split("product/")).split("/")[0].trim();
-      }
-    }
-
-    return internalId;
+    super.host = "www.drogarianet.com.br";
   }
 }
--- src/java/br/com/lett/crawlernode/crawlers/ratingandreviews/brasil/BrasilDrogarianetRatingReviewCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ratingandreviews/brasil/BrasilDrogarianetRatingReviewCrawler.java
@@ -1,13 +1,7 @@
 package br.com.lett.crawlernode.crawlers.ratingandreviews.brasil;
 
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import br.com.lett.crawlernode.core.models.RatingReviewsCollection;
 import br.com.lett.crawlernode.core.session.Session;
-import br.com.lett.crawlernode.core.task.impl.RatingReviewCrawler;
-import br.com.lett.crawlernode.util.CrawlerUtils;
-import br.com.lett.crawlernode.util.Logging;
-import models.RatingsReviews;
+import br.com.lett.crawlernode.crawlers.ratingandreviews.extractionutils.DrogariaMinasbrasilNetRatingReviewCrawler;
 
 /**
  * Date: 28/08/17
@@ -15,59 +9,9 @@ import models.RatingsReviews;
  * @author gabriel
  *
  */
-public class BrasilDrogarianetRatingReviewCrawler extends RatingReviewCrawler {
+public class BrasilDrogarianetRatingReviewCrawler extends DrogariaMinasbrasilNetRatingReviewCrawler {
 
   public BrasilDrogarianetRatingReviewCrawler(Session session) {
     super(session);
   }
-
-  @Override
-  protected RatingReviewsCollection extractRatingAndReviews(Document document) throws Exception {
-    RatingReviewsCollection ratingReviewsCollection = new RatingReviewsCollection();
-
-    if (isProductPage(document)) {
-      Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
-
-      RatingsReviews ratingReviews = new RatingsReviews();
-      ratingReviews.setDate(session.getDate());
-
-      String internalId = CrawlerUtils.scrapStringSimpleInfoByAttribute(document, "input[name=product]", "value");
-
-      Integer totalNumOfEvaluations = CrawlerUtils.scrapIntegerFromHtml(document, ".reviews-summary .reviews-count", true, 0);
-      Double avgRating = getTotalAvgRating(document);
-
-      ratingReviews.setInternalId(internalId);
-      ratingReviews.setTotalRating(totalNumOfEvaluations);
-      ratingReviews.setTotalWrittenReviews(totalNumOfEvaluations);
-      ratingReviews.setAverageOverallRating(avgRating);
-
-      ratingReviewsCollection.addRatingReviews(ratingReviews);
-    }
-
-    return ratingReviewsCollection;
-
-  }
-
-  /**
-   * Avg appear in html element
-   * 
-   * @param document
-   * @return
-   */
-  private Double getTotalAvgRating(Document doc) {
-    Double avgRating = 0d;
-    Element rating = doc.selectFirst("meta[itemprop=ratingValue]");
-
-    if (rating != null) {
-      avgRating = Double.parseDouble(rating.attr("content"));
-    }
-
-    return avgRating;
-  }
-
-
-  private boolean isProductPage(Document doc) {
-    return doc.selectFirst(".product-page") != null;
-  }
-
 }
