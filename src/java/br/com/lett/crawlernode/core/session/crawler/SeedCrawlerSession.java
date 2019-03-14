b6ef28bad0d533292e9a0f5f0203cc338b66bc07
gabriel <gabrieldornelas@lett.digital>
2019-03-14 15:55:27 -0300

    fixing crawler seed
    Former-commit-id: 54a1fd705936d20d345687b02712af4145ddfcac

--- src/java/br/com/lett/crawlernode/core/session/crawler/SeedCrawlerSession.java
+++ src/java/br/com/lett/crawlernode/core/session/crawler/SeedCrawlerSession.java
@@ -1,13 +1,26 @@
 package br.com.lett.crawlernode.core.session.crawler;
 
 import br.com.lett.crawlernode.core.models.Markets;
+import br.com.lett.crawlernode.core.server.request.CrawlerSeedRequest;
 import br.com.lett.crawlernode.core.server.request.Request;
 import br.com.lett.crawlernode.core.session.Session;
 
 public class SeedCrawlerSession extends Session {
-	
-	public SeedCrawlerSession(Request request, String queueName, Markets markets) {
-		super(request, queueName, markets);
-	}
+
+  private String taskId;
+
+  public SeedCrawlerSession(Request request, String queueName, Markets markets) {
+    super(request, queueName, markets);
+
+    setTaskId(((CrawlerSeedRequest) request).getTaskId());
+  }
+
+  public String getTaskId() {
+    return taskId;
+  }
+
+  public void setTaskId(String taskId) {
+    this.taskId = taskId;
+  }
 
 }
