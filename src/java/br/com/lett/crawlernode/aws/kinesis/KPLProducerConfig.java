37e3b0c624c6f085ee1d1a6f8996514fae58e2b0
doomhammer <doomhammer@192.168.0.26>
2019-03-27 13:10:09 -0300

    - Aumentando o tempo de buffer do kinesis de 20 para 30 segundos
    Former-commit-id: 83fce4f1835488cbdcc85392a4ca62872dc6a32c

--- src/java/br/com/lett/crawlernode/aws/kinesis/KPLProducerConfig.java
+++ src/java/br/com/lett/crawlernode/aws/kinesis/KPLProducerConfig.java
@@ -5,7 +5,7 @@ public class KPLProducerConfig {
 	public static final long KPL_MAX_CONNECTIONS = 1;
 	public static final long KPL_REQUEST_TIMEOUT = 120000;
 	public static final long RECORD_TTL = 300000;
-	public static final long RECORD_MAX_BUFFERED_TIME = 20000;
+	public static final long RECORD_MAX_BUFFERED_TIME = 30000; // 30 seconds
 	
 	public static final String REGION = "us-east-1";
 	public static final String STREAM_NAME = "sku-core-crawler-kinesis-stream";
