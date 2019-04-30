2f13eacace36f7947a44f9e6ded92f1c43626ef6
gabriel <gabrieldornelas@lett.digital>
2019-04-30 11:57:13 -0300

    adding prices on sellerv2
    Former-commit-id: c32971335aadd30596f2f6a0f0ef1ff7632bb6d9

--- src/java/br/com/lett/crawlernode/core/models/SellerV2.java
+++ src/java/br/com/lett/crawlernode/core/models/SellerV2.java
@@ -2,15 +2,17 @@ package br.com.lett.crawlernode.core.models;
 
 import org.json.JSONObject;
 import br.com.lett.crawlernode.exceptions.BuyBoxSellerException;
+import models.prices.Prices;
 
 public class SellerV2 {
 
   private String id;
   private String name;
   private Float price;
+  private Prices prices;
   private Integer position;
 
-  public SellerV2(String id, String name, Float price, Integer position) throws BuyBoxSellerException {
+  public SellerV2(String id, String name, Float price, Prices prices, Integer position) throws BuyBoxSellerException {
     if (id != null) {
       if (!id.isEmpty()) {
         this.id = id;
@@ -41,6 +43,16 @@ public class SellerV2 {
       throw new BuyBoxSellerException("PRICE");
     }
 
+    if (prices != null) {
+      if (!prices.isEmpty()) {
+        this.prices = prices;
+      } else {
+        throw new BuyBoxSellerException("PRICES", prices.toJSON());
+      }
+    } else {
+      throw new BuyBoxSellerException("PRICES");
+    }
+
     if (position != null) {
       this.position = position;
     } else {
@@ -60,6 +72,10 @@ public class SellerV2 {
     return price;
   }
 
+  public Prices getPrices() {
+    return prices;
+  }
+
   public int getPosition() {
     return position;
   }
@@ -70,6 +86,7 @@ public class SellerV2 {
     sellerJSON.put("id", this.id);
     sellerJSON.put("name", this.id);
     sellerJSON.put("price", this.price);
+    sellerJSON.put("prices", this.prices.toJSON());
     sellerJSON.put("position", this.position);
 
     return sellerJSON;
