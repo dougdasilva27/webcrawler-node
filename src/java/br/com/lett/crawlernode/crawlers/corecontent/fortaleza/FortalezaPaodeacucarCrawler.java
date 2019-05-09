7d21efda8c7a35dca5e567ceb983e1f37bcc07c0
gabriel <gabrieldornelas@lett.digital>
2019-05-09 15:56:56 -0300

    new crawler fortaleza paodeacucar
    Former-commit-id: daafc0e258db7823cb1733551223bd72affcf51c

---
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/fortaleza/FortalezaPaodeacucarCrawler.java
@@ -0,0 +1,44 @@
+package br.com.lett.crawlernode.crawlers.corecontent.fortaleza;
+
+import java.util.List;
+import org.apache.http.impl.cookie.BasicClientCookie;
+import org.jsoup.nodes.Document;
+import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.Crawler;
+import br.com.lett.crawlernode.crawlers.corecontent.extractionutils.GPACrawler;
+
+public class FortalezaPaodeacucarCrawler extends Crawler {
+
+  private static final String HOME_PAGE = "https://www.paodeacucar.com";
+  private static final String HOME_PAGE_HTTP = "http://www.paodeacucar.com";
+
+  public FortalezaPaodeacucarCrawler(Session session) {
+    super(session);
+  }
+
+  @Override
+  public boolean shouldVisit() {
+    String href = this.session.getOriginalURL().toLowerCase();
+    return !FILTERS.matcher(href).matches() && href.startsWith(HOME_PAGE);
+  }
+
+  @Override
+  public void handleCookiesBeforeFetch() {
+    BasicClientCookie cookie = new BasicClientCookie("ep.selected_store", GPACrawler.FORTALEZA_STORE_ID);
+    cookie.setDomain(".paodeacucar.com.br");
+    cookie.setPath("/");
+    this.cookies.add(cookie);
+  }
+
+  @Override
+  protected Object fetch() {
+    return new Document("");
+  }
+
+  @Override
+  public List<Product> extractInformation(Document doc) throws Exception {
+    super.extractInformation(doc);
+    return new GPACrawler(logger, session, HOME_PAGE, HOME_PAGE_HTTP, GPACrawler.FORTALEZA_STORE_ID, cookies, "pa", dataFetcher).extractInformation();
+  }
+}
