fb447460651f93f19b950046df69349581ca42c3
Joao Pedro <joaopedro@lett.digital>
2019-05-28 16:20:47 -0300

    Creating crawler ranking keywords mercado libre heinz
    Former-commit-id: 27b51772c751dc0e242fce26f45f7cb1fe82673a

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilMercadolivrecotyCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilMercadolivrecotyCrawler.java
@@ -1,12 +1,14 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
 
 import br.com.lett.crawlernode.core.session.Session;
-import br.com.lett.crawlernode.crawlers.ranking.keywords.extractionutils.BrasilMercadolivreCrawler;
+import br.com.lett.crawlernode.crawlers.ranking.keywords.extractionutils.MercadolivreCrawler;
 
-public class BrasilMercadolivrecotyCrawler extends BrasilMercadolivreCrawler {
+public class BrasilMercadolivrecotyCrawler extends MercadolivreCrawler {
 
   public BrasilMercadolivrecotyCrawler(Session session) {
     super(session);
     super.setStoreName("coty");
+    super.setNextUrlHost("lista.mercadolivre.com.br");
+    super.setProductUrlHost("produto.mercadolivre.com.br");
   }
 }
--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilMercadolivremelittaCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilMercadolivremelittaCrawler.java
@@ -1,12 +1,16 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
 
 import br.com.lett.crawlernode.core.session.Session;
-import br.com.lett.crawlernode.crawlers.ranking.keywords.extractionutils.BrasilMercadolivreCrawler;
+import br.com.lett.crawlernode.crawlers.ranking.keywords.extractionutils.MercadolivreCrawler;
 
-public class BrasilMercadolivremelittaCrawler extends BrasilMercadolivreCrawler {
+public class BrasilMercadolivremelittaCrawler extends MercadolivreCrawler {
 
   public BrasilMercadolivremelittaCrawler(Session session) {
     super(session);
     super.setStoreName("melitta");
+    super.setNextUrlHost("lista.mercadolivre.com.br");
+    super.setProductUrlHost("produto.mercadolivre.com.br");
+
+
   }
 }
