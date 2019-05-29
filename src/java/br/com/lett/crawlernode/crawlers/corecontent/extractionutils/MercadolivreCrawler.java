0d6404a03ebf0d5fd91fba3e2a4ceb5c69def0b3
Joao Pedro <joaopedro@lett.digital>
2019-05-29 16:22:29 -0300

    fix
    Former-commit-id: 4d43f688805893b7cb485527183fd31413c77ba9

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/extractionutils/MercadolivreCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/extractionutils/MercadolivreCrawler.java
@@ -20,7 +20,6 @@ import br.com.lett.crawlernode.core.models.Product;
 import br.com.lett.crawlernode.core.models.ProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.Crawler;
-import br.com.lett.crawlernode.test.Test;
 import br.com.lett.crawlernode.util.CommonMethods;
 import br.com.lett.crawlernode.util.CrawlerUtils;
 import br.com.lett.crawlernode.util.Logging;
@@ -66,7 +65,6 @@ public class MercadolivreCrawler extends Crawler {
   public List<Product> extractInformation(Document doc) throws Exception {
     super.extractInformation(doc);
     List<Product> products = new ArrayList<>();
-    doc = Jsoup.parse(CommonMethods.readFile(Test.pathWrite + "melitta.html"));
 
     if (isProductPage(doc)) {
       Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
