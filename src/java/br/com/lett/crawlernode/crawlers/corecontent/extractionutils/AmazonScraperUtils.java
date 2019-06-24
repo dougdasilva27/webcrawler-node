61aee27dc27d50ebe8a4dc2dfb9eb7f6e34a1a10
gabriel <gabrieldornelas@lett.digital>
2019-06-24 14:26:10 -0300

    Those amazon crawlers are using fetcher with moving average now. Now we have a util class too.
    Former-commit-id: 3051fa5d1c726adcb7bfe728331ba452329dec43

---
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/extractionutils/AmazonScraperUtils.java
@@ -0,0 +1,69 @@
+package br.com.lett.crawlernode.crawlers.corecontent.extractionutils;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.http.cookie.Cookie;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import br.com.lett.crawlernode.core.fetcher.methods.ApacheDataFetcher;
+import br.com.lett.crawlernode.core.fetcher.methods.DataFetcher;
+import br.com.lett.crawlernode.core.fetcher.methods.FetcherDataFetcher;
+import br.com.lett.crawlernode.core.fetcher.models.Request;
+import br.com.lett.crawlernode.core.fetcher.models.Request.RequestBuilder;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.util.Logging;
+
+public class AmazonScraperUtils {
+
+  private Logger logger;
+  private Session session;
+
+  public AmazonScraperUtils(Logger logger, Session session) {
+    this.logger = logger;
+    this.session = session;
+  }
+
+  public Document fetchProductPage(List<Cookie> cookies, DataFetcher dataFetcher) {
+    Document doc = Jsoup.parse(fetchPage(session.getOriginalURL(), new HashMap<>(), cookies, session, dataFetcher));
+
+    if (doc.selectFirst("#captchacharacters") != null) {
+      Logging.printLogWarn(logger, session, "Trying to fetch page again, because this site returned a captcha.");
+      doc = Jsoup.parse(fetchPage(session.getOriginalURL(), new HashMap<>(), cookies, session, dataFetcher));
+    }
+
+    return doc;
+  }
+
+  /**
+   * Fetch html from amazon
+   * 
+   * @param url
+   * @param headers
+   * @param cookies
+   * @param session
+   * @param dataFetcher
+   * @return
+   */
+  public String fetchPage(String url, Map<String, String> headers, List<Cookie> cookies, Session session, DataFetcher dataFetcher) {
+    String content;
+
+    if (dataFetcher instanceof FetcherDataFetcher) {
+      headers.put("Accept-Encoding", "no");
+
+      Request request = RequestBuilder.create().setUrl(url).setCookies(cookies).setHeaders(headers).mustSendContentEncoding(false).build();
+      content = dataFetcher.get(session, request).getBody();
+
+      if (content == null || content.isEmpty()) {
+        Request requestApache = RequestBuilder.create().setUrl(url).setCookies(cookies).build();
+        content = new ApacheDataFetcher().get(session, requestApache).getBody();
+      }
+    } else {
+      Request requestApache = RequestBuilder.create().setUrl(url).setHeaders(headers).setCookies(cookies).build();
+      content = dataFetcher.get(session, requestApache).getBody();
+    }
+
+    return content;
+  }
+}
