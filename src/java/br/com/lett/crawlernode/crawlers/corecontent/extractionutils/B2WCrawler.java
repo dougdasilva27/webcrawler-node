f956e39ac90ab79501287566a26489941c1f26ce
gabriel <gabrieldornelas@lett.digital>
2019-07-04 18:09:22 -0300

    fix access americanas
    Former-commit-id: b1415c23069e44ea89101cd63f142e357ddec93c

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/extractionutils/B2WCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/extractionutils/B2WCrawler.java
@@ -71,7 +71,7 @@ public class B2WCrawler extends Crawler {
 
   public String fetchPage(String url, Session session) {
     Request request = RequestBuilder.create().setUrl(url).setCookies(this.cookies).setHeaders(this.headers).mustSendContentEncoding(false)
-        .setSendUserAgent(false).setFetcheroptions(FetcherOptionsBuilder.create().mustUseMovingAverage(false).build())
+        .setFetcheroptions(FetcherOptionsBuilder.create().mustUseMovingAverage(false).build())
         .setProxyservice(Arrays.asList(ProxyCollection.STORM_RESIDENTIAL_EU, ProxyCollection.BUY)).build();
 
     String content = this.dataFetcher.get(session, request).getBody();
