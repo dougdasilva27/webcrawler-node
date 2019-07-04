abdcf07bfb266c538d63537ff014d23b1426fb07
gabriel <gabrieldornelas@lett.digital>
2019-07-04 11:00:56 -0300

    fixing internal Id and Url scrap for drogarianovaesperanca
    Former-commit-id: 4a96a27f7dcae4064d2b4662dfa1f5d0730be72a

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilDrogarianovaesperancaCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilDrogarianovaesperancaCrawler.java
@@ -2,115 +2,87 @@ package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
 
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.util.CommonMethods;
+import br.com.lett.crawlernode.util.CrawlerUtils;
 
-public class BrasilDrogarianovaesperancaCrawler extends CrawlerRankingKeywords{
+public class BrasilDrogarianovaesperancaCrawler extends CrawlerRankingKeywords {
 
-	public BrasilDrogarianovaesperancaCrawler(Session session) {
-		super(session);
-	}
+  public BrasilDrogarianovaesperancaCrawler(Session session) {
+    super(session);
+  }
 
-	@Override
-	protected void extractProductsFromCurrentPage() {
-		//número de produtos por página do market
-		this.pageSize = 12;
-	
-		this.log("Página "+ this.currentPage);
-		
-		//monta a url com a keyword e a página
-		String url = "https://www.drogarianovaesperanca.com.br/busca/?Pagina="+ this.currentPage +"&ob=&q="+ this.keywordEncoded +"&G=&ppg=36";
-		this.log("Link onde são feitos os crawlers: " + url);	
-		
-		//chama função de pegar o html
-		this.currentDoc = fetchDocument(url);
+  private static final int PAGE_SIZE = 12;
 
-		Elements products =  this.currentDoc.select(".produto-lista");
+  @Override
+  protected void extractProductsFromCurrentPage() {
+    this.pageSize = PAGE_SIZE;
+    this.log("Página " + this.currentPage);
 
-		if(totalProducts == 0) {
-			setTotalProducts();
-		}
-		
-		//se obter 1 ou mais links de produtos e essa página tiver resultado faça:
-		if(products.size() >= 1 && this.totalProducts > 0) {
-			for(Element e : products) {
-				// InternalPid
-				String internalPid = null;
-				
-				// Url do produto
-				String productUrl = crawlProductUrl(e);
-				
-				// InternalId
-				String internalId = crawlInternalId(productUrl);
-				
-				saveDataProduct(internalId, internalPid, productUrl);
-				
-				this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + internalPid + " - Url: " + productUrl);
-				if(this.arrayProducts.size() == productsLimit) {
-					break;
-				}
-				
-			}
-		} else {
-			this.result = false;
-			this.log("Keyword sem resultado!");
-		}
-		
-		this.log("Finalizando Crawler de produtos da página "+this.currentPage+" - até agora "+this.arrayProducts.size()+" produtos crawleados");
-	}
+    String url = "https://www.drogarianovaesperanca.com.br/busca/?Pagina=" + this.currentPage + "&q=" +
+        CommonMethods.encondeStringURLToISO8859(this.location, logger, session) + "&ppg=36";
+    this.log("Link onde são feitos os crawlers: " + url);
 
-	@Override
-	protected boolean hasNextPage() {
-		if(this.arrayProducts.size() < this.totalProducts) {
-			return true;
-		}
-			
-		return false;
-	}
-	
-	@Override
-	protected void setTotalProducts() {
-		Element total = this.currentDoc.select(".resultados-busca span").first();
-		
-		if(total != null) {
-			String totalText = total.ownText().replaceAll("[^0-9]", "").trim();
-			
-			if(!totalText.isEmpty()) {
-				this.totalProducts = Integer.parseInt(totalText);
-			}
-		}
-		
-		this.log("Total products: " + this.totalProducts);
-	}
-	
-	private String crawlInternalId(String url){
-		String internalId = null;
-		
-		if(url != null) {
-			String[] tokens = url.split("-");
-			String text = tokens[tokens.length-1].replaceAll("[^0-9]", "").trim();
-			
-			if(!text.isEmpty()) {
-				internalId = text;
-			}
-		}
-		
-		return internalId;
-	}
-	
-	private String crawlProductUrl(Element e){
-		String productUrl = null;
-		Element url = e.select("> a").first();
-		
-		if(url != null) {
-			productUrl = url.attr("href");
-			
-			if(!productUrl.startsWith("https://www.drogarianovaesperanca.com.br")) {
-				productUrl = "https://www.drogarianovaesperanca.com.br" + productUrl;
-			}
-		}
-		
-		return productUrl;
-	}
+    this.currentDoc = fetchDocument(url);
+    Elements products = this.currentDoc.select(".produto-lista");
+
+    // In this scraper we need scrap products total first
+    // because we need this information for verify if the site returns products for the keyword we send
+    // or returns suggestions (in this case we don't have total Products),
+    // we don't scrap suggestions products
+    if (totalProducts == 0) {
+      setTotalProducts();
+    }
+
+    if (!products.isEmpty() && this.totalProducts > 0) {
+      for (Element e : products) {
+        String productUrl = CrawlerUtils.scrapUrl(e, ".produto-descricao-lista a", "href", "https", "www.drogarianovaesperanca.com.br");
+        String internalId = crawlInternalId(productUrl);
+
+        saveDataProduct(internalId, null, productUrl);
+
+        this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + null + " - Url: " + productUrl);
+        if (this.arrayProducts.size() == productsLimit) {
+          break;
+        }
+      }
+    } else {
+      this.result = false;
+      this.log("Keyword sem resultado!");
+    }
+
+    this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
+  }
+
+  @Override
+  protected void setTotalProducts() {
+    this.totalProducts = CrawlerUtils.scrapIntegerFromHtml(currentDoc, ".resultados-busca span", true, 0);
+    this.log("Total: " + this.totalProducts);
+  }
+
+  /**
+   * For scrap internal Id we need the url, is the only "safe" place
+   * 
+   * Ex: Internal Id will be found in the end of url
+   * 
+   * Url:
+   * https://www.drogarianovaesperanca.com.br/naturais/fitoterapicos/comprar-prevelip-com-60-capsulas-16970/
+   * 
+   * InternalId: 16970
+   * 
+   * @param url
+   * @return
+   */
+  private String crawlInternalId(String url) {
+    String internalId = null;
+
+    String lastStringUrl = CommonMethods.getLast(url.split("\\?")[0].split("-"));
+
+    if (lastStringUrl.contains("/")) {
+      internalId = lastStringUrl.split("/")[0];
+    }
+
+    return internalId;
+  }
 }
