eaed9532e4a1755807a842c31a66f9e6b4fec9c4
Joao Pedro <joaopedro@lett.digital>
2019-07-16 14:13:35 -0300

    Creating crawler rating eletrum
    Former-commit-id: 3a5d57678b2b98187fd4a398f346022b9dc37835

---
+++ src/java/br/com/lett/crawlernode/crawlers/ratingandreviews/brasil/BrasilEletrumRatingReviewCrawler.java
@@ -0,0 +1,86 @@
+package br.com.lett.crawlernode.crawlers.ratingandreviews.brasil;
+
+import java.util.List;
+import org.json.JSONObject;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import br.com.lett.crawlernode.core.models.RatingReviewsCollection;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.RatingReviewCrawler;
+import br.com.lett.crawlernode.crawlers.corecontent.extractionutils.VTEXCrawlersUtils;
+import br.com.lett.crawlernode.crawlers.ratingandreviews.extractionutils.YourreviewsRatingCrawler;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+import models.AdvancedRatingReview;
+import models.RatingsReviews;
+
+public class BrasilEletrumRatingReviewCrawler extends RatingReviewCrawler {
+
+  public BrasilEletrumRatingReviewCrawler(Session session) {
+    super(session);
+  }
+
+  @Override
+  protected RatingReviewsCollection extractRatingAndReviews(Document document) throws Exception {
+    RatingReviewsCollection ratingReviewsCollection = new RatingReviewsCollection();
+
+    if (isProductPage(document)) {
+      RatingsReviews ratingReviews = new RatingsReviews();
+      ratingReviews.setDate(session.getDate());
+
+      JSONObject skuJson = CrawlerUtils.crawlSkuJsonVTEX(document, session);
+      YourreviewsRatingCrawler yourReviews =
+          new YourreviewsRatingCrawler(session, cookies, logger, "8ea7baa3-231d-4049-873e-ad5afd085ca4", this.dataFetcher);
+
+      if (skuJson.has("productId")) {
+        String internalPid = Integer.toString(skuJson.getInt("productId"));
+
+        Document docRating = yourReviews.crawlPageRatingsFromYourViews(internalPid, "8ea7baa3-231d-4049-873e-ad5afd085ca4", this.dataFetcher);
+        Integer totalNumOfEvaluations = getTotalNumOfRatingsFromYourViews(docRating);
+        Double avgRating = getTotalAvgRatingFromYourViews(docRating);
+        AdvancedRatingReview advancedRatingReview = yourReviews.getTotalStarsFromEachValue(internalPid);
+
+        ratingReviews.setAdvancedRatingReview(advancedRatingReview);
+        ratingReviews.setTotalRating(totalNumOfEvaluations);
+        ratingReviews.setAverageOverallRating(avgRating);
+        ratingReviews.setTotalWrittenReviews(totalNumOfEvaluations);
+
+        List<String> idList = VTEXCrawlersUtils.crawlIdList(skuJson);
+        for (String internalId : idList) {
+          RatingsReviews clonedRatingReviews = ratingReviews.clone();
+          clonedRatingReviews.setInternalId(internalId);
+          ratingReviewsCollection.addRatingReviews(clonedRatingReviews);
+        }
+      }
+    }
+
+    return ratingReviewsCollection;
+  }
+
+  private Double getTotalAvgRatingFromYourViews(Document docRating) {
+    Double avgRating = 0d;
+    Element rating = docRating.selectFirst("meta[itemprop=ratingValue]");
+
+    if (rating != null) {
+      avgRating = Double.parseDouble(rating.attr("content"));
+    }
+
+    return avgRating;
+  }
+
+  private Integer getTotalNumOfRatingsFromYourViews(Document docRating) {
+    Integer totalRating = 0;
+    Element totalRatingElement = docRating.selectFirst("meta[itemprop=ratingCount]");
+
+    if (totalRatingElement != null) {
+      totalRating = Integer.parseInt(totalRatingElement.attr("content"));
+    }
+
+    return totalRating;
+  }
+
+  private boolean isProductPage(Document document) {
+    return document.selectFirst(".bf-product__spot") != null;
+  }
+
+
+}
