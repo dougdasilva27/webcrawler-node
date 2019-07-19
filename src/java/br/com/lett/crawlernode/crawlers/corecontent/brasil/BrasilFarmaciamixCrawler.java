932eee08911393c2fe7aaacedf25918f9026ae18
Joao Pedro <joaopedro@lett.digital>
2019-07-19 15:40:47 -0300

    Fixing secondary image
    Former-commit-id: cc6d822eeb35138d79e0df8abf1ce4256985708c

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilFarmaciamixCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilFarmaciamixCrawler.java
@@ -1,10 +1,10 @@
 package br.com.lett.crawlernode.crawlers.corecontent.brasil;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import org.json.JSONArray;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -17,6 +17,7 @@ import br.com.lett.crawlernode.core.models.Product;
 import br.com.lett.crawlernode.core.models.ProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.Crawler;
+import br.com.lett.crawlernode.util.CrawlerUtils;
 import br.com.lett.crawlernode.util.Logging;
 import br.com.lett.crawlernode.util.MathUtils;
 import models.Marketplace;
@@ -58,7 +59,8 @@ public class BrasilFarmaciamixCrawler extends Crawler {
       Prices prices = crawlPrices(price, doc);
       CategoryCollection categories = crawlCategories(doc);
       String primaryImage = crawlPrimaryImage(doc);
-      String secondaryImages = crawlSecondaryImages(doc);
+      String secondaryImages = CrawlerUtils.scrapSimpleSecondaryImages(doc, "#gal1 a:not(:first-child)", Arrays.asList("data-image"), "https",
+          "www.farmaciamix.com.br", primaryImage);
       String description = crawlDescription(doc);
       Integer stock = null;
       Marketplace marketplace = crawlMarketplace();
@@ -144,23 +146,6 @@ public class BrasilFarmaciamixCrawler extends Crawler {
     }
 
     return primaryImage;
-  }
-
-  /**
-   * Quando este crawler foi feito não achei imagens secundarias
-   * 
-   * @param doc
-   * @return
-   */
-  private String crawlSecondaryImages(Document doc) {
-    String secondaryImages = null;
-    JSONArray secondaryImagesArray = new JSONArray();
-
-    if (secondaryImagesArray.length() > 0) {
-      secondaryImages = secondaryImagesArray.toString();
-    }
-
-    return secondaryImages;
   }
 
   /**
