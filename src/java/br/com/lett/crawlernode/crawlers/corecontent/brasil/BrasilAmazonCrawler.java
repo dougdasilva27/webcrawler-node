7f226c3e3aa7d1ac6b41781234c68c80d6d1faf4
gabriel <gabrieldornelas@lett.digital>
2019-08-08 15:10:09 -0300

    amazon using infatica
    Former-commit-id: 53e57ef34dc8a71b7eb1382398e6684bcd697493

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilAmazonCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilAmazonCrawler.java
@@ -13,6 +13,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import br.com.lett.crawlernode.core.fetcher.FetchMode;
 import br.com.lett.crawlernode.core.models.Card;
 import br.com.lett.crawlernode.core.models.CategoryCollection;
 import br.com.lett.crawlernode.core.models.Product;
@@ -41,6 +42,7 @@ public class BrasilAmazonCrawler extends Crawler {
 
   public BrasilAmazonCrawler(Session session) {
     super(session);
+    super.config.setFetcher(FetchMode.FETCHER);
   }
 
   @Override
