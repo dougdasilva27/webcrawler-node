63d1f7ed72b98f34897b302e9eb5fff965b327a5
gabriel <gabrieldornelas@lett.digital>
2019-09-04 17:24:08 -0300

    new crawler supernosso
    Former-commit-id: 81d521a30b8fbc9e68ec122fe9d06f37971b2299

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/belohorizonte/BelohorizonteSupernossoCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/belohorizonte/BelohorizonteSupernossoCrawler.java
@@ -1,123 +1,62 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.belohorizonte;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
 import br.com.lett.crawlernode.util.CommonMethods;
+import br.com.lett.crawlernode.util.CrawlerUtils;
 
-public class BelohorizonteSupernossoCrawler extends CrawlerRankingKeywords{
+public class BelohorizonteSupernossoCrawler extends CrawlerRankingKeywords {
 
-	public BelohorizonteSupernossoCrawler(Session session) {
-		super(session);
-	}
+  public BelohorizonteSupernossoCrawler(Session session) {
+    super(session);
+  }
 
-	private static final String HOME_PAGE = "https://www.supernossoemcasa.com.br/e-commerce/";
-	
-	@Override
-	protected void extractProductsFromCurrentPage() {
-		this.pageSize = 60;
-	
-		this.log("Página "+ this.currentPage);
-		
-		JSONObject productsInfo = crawlProductInfo();
-		JSONArray products = productsInfo.has("elements") ? productsInfo.getJSONArray("elements") : new JSONArray();
-		
-		if(products.length() > 0) {
-			if(totalProducts == 0) {
-				this.totalProducts = productsInfo.has("elementsCount") ? productsInfo.getInt("elementsCount") : 0;
-				this.log("Total: " + this.totalProducts);
-			}
-			
-			for(int i = 0; i < products.length(); i++) {
-				JSONObject product = products.getJSONObject(i);
-				
-				String internalPid = crawlInternalPid(product);
-				String internalId = crawlInternalId(product);
-				String productUrl = crawlProductUrl(product, internalPid);
-				
-				saveDataProduct(internalId, internalPid, productUrl);
-				
-				this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + internalPid + " - Url: " + productUrl);
-				
-				if(this.arrayProducts.size() == productsLimit) {
-					break;
-				}
-			}
-		} else {
-			this.result = false;
-			this.log("Keyword sem resultado!");
-		}
-		
-		this.log("Finalizando Crawler de produtos da página "+this.currentPage+" - até agora "+this.arrayProducts.size()+" produtos crawleados");
-	}
+  @Override
+  protected void extractProductsFromCurrentPage() {
+    this.pageSize = 12;
 
-	@Override
-	protected boolean hasNextPage() {
-		if(this.arrayProducts.size() < this.totalProducts) {
-			return true;
-		}
-			
-		return false;
-	}	
-	
-	private String crawlInternalId(JSONObject product) {
-		String internalId = null;
-		
-		if(product.has("sku")) {
-			internalId = product.getString("sku");
-		}
-		
-		return internalId;
-	}
-	
-	private String crawlInternalPid(JSONObject product) {
-		String internalPid = null;
-		
-		if(product.has("id")) {
-			internalPid = product.getString("id");
-		}
-		
-		return internalPid;
-	}
-	
-	private String crawlProductUrl(JSONObject product, String id){
-		String productUrl = null;
-		
-		if(product.has("name")) {
-			productUrl = HOME_PAGE + "p/" + id + "/" + 
-					CommonMethods.removeAccents(product.getString("name")).replace(" ", "-")
-						.toLowerCase().replaceAll("[^a-z0-9]+", "-");
-		}
-		
-		return productUrl;
-	}
-	
-	private JSONObject crawlProductInfo() {
-		JSONObject products = new JSONObject();
-		String payload = "{\"pageNumber\": " + (this.currentPage-1) + ", \"pageSize\": 60, \"query\": \"" + this.location+ "\","
-				+ " \"filters\": [], \"order\": \"relevancia\"}";
-		
-		Map<String, String> headers = new HashMap<>();
-		headers.put("Content-Type", "application/json");
-		headers.put("referer", "https://www.supernossoemcasa.com.br/e-commerce/search?query=" + this.keywordEncoded);
-		headers.put("accept", "application/json");
+    this.log("Página " + this.currentPage);
+    String url = "https://www.supernossoemcasa.com.br/" + this.keywordWithoutAccents.replaceAll(" ", "%20") + "?PageNumber=" + this.currentPage;
+    this.log("Link onde são feitos os crawlers: " + url);
 
-		String page = fetchPostFetcher("https://www.supernossoemcasa.com.br/e-commerce/api/products/autocomplete", payload, headers, null);
-		
-		if(page != null && page.startsWith("{") && page.endsWith("}")) {
-			try {
-				products = new JSONObject(page);
-			} catch (JSONException e) {
-				logError(CommonMethods.getStackTrace(e));
-			}
-		}
-		
-		return products;
-	}
+    this.currentDoc = fetchDocument(url);
+
+    Elements products = this.currentDoc.select(".vitrine.resultItemsWrapper ul > li[layout]");
+    Elements productsIds = this.currentDoc.select(".vitrine.resultItemsWrapper ul > li[id]");
+
+    if (!products.isEmpty()) {
+      if (this.totalProducts == 0) {
+        setTotalProducts();
+      }
+
+      int index = 0;
+
+      for (Element e : products) {
+        String internalPid = CommonMethods.getLast(productsIds.get(index).id().split("_"));
+        String productUrl = CrawlerUtils.scrapUrl(e, ".product-name a", "href", "https", "www.supernossoemcasa.com.br");
+
+        saveDataProduct(null, internalPid, productUrl);
+
+        this.log("Position: " + this.position + " - InternalId: " + null + " - InternalPid: " + internalPid + " - Url: " + productUrl);
+
+        if (this.arrayProducts.size() == productsLimit) {
+          break;
+        }
+        index++;
+      }
+    } else {
+      this.result = false;
+      this.log("Keyword sem resultado!");
+    }
+
+    this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
+  }
+
+  @Override
+  protected void setTotalProducts() {
+    this.totalProducts = CrawlerUtils.scrapIntegerFromHtml(this.currentDoc, "span.resultado-busca-numero span.value", true, 0);
+    this.log("Total da busca: " + this.totalProducts);
+  }
 }
