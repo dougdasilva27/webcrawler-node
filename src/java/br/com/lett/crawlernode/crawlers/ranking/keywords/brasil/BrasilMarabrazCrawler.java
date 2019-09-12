a3a1ff7638292fcc0eab5dbb3abc9560f32d8038
Joao Pedro <joaopedro@lett.digital>
2019-09-12 16:46:51 -0300

    creating ranking marabraz
    Former-commit-id: 20c9c96c82b8b64fa87ed942c7883d338fefc45d

---
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilMarabrazCrawler.java
@@ -0,0 +1,78 @@
+package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
+
+import java.util.Arrays;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.MathUtils;
+
+public class BrasilMarabrazCrawler extends CrawlerRankingKeywords {
+
+  public BrasilMarabrazCrawler(Session session) {
+    super(session);
+  }
+
+  @Override
+  protected void extractProductsFromCurrentPage() {
+    this.pageSize = 10;
+    this.log("Página " + this.currentPage);
+
+    String url = "https://busca.marabraz.com.br/busca?q=" + this.keywordEncoded;
+
+    this.log("Link onde são feitos os crawlers: " + url);
+    this.currentDoc = fetchDocument(url);
+    Elements products = this.currentDoc.select(".neemu-products-container .nm-product-item");
+
+    if (!products.isEmpty()) {
+
+      if (this.totalProducts == 0) {
+        setTotalProducts();
+      }
+
+      for (Element e : products) {
+        String internalId = CrawlerUtils.scrapStringSimpleInfoByAttribute(e, null, "data-sku");
+        String internalPid = CrawlerUtils.scrapStringSimpleInfoByAttribute(e, null, "data-id");
+
+        String productUrl = scrapProductUrl(e);
+
+        saveDataProduct(internalId, internalPid, productUrl);
+
+        this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + internalPid + " - Url: " + productUrl);
+
+        if (this.arrayProducts.size() == productsLimit)
+          break;
+
+      }
+    } else {
+      this.result = false;
+      this.log("Keyword sem resultado!");
+    }
+
+    this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora "
+        + this.arrayProducts.size() + " produtos crawleados");
+  }
+
+  @Override
+  protected void setTotalProducts() {
+    Element totalElement = this.currentDoc.selectFirst(".neemu-total-products-container  strong");
+
+    if (totalElement != null) {
+      this.totalProducts = MathUtils.parseInt(totalElement.text());
+    }
+
+  }
+
+  private String scrapProductUrl(Element e) {
+    String productUrl = null;
+    Element urlElement = e.selectFirst(".nm-product-name a");
+
+    if (urlElement != null) {
+      productUrl = CrawlerUtils.sanitizeUrl(urlElement, Arrays.asList("href"), "https:", "https://www.marabraz.com.br");
+    }
+
+    return productUrl;
+  }
+
+}
