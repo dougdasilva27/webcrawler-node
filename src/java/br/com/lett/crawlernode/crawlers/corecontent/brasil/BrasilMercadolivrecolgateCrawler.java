86db40da76e9635b2f55cdafa2d104e6d073f718
gabriel <gabrieldornelas@lett.digital>
2019-09-13 16:43:27 -0300

    creating mercadolivre colgate crawler
    Former-commit-id: 8235d257bf2ca591007879fb3be48d398c72ff06

---
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilMercadolivrecolgateCrawler.java
@@ -0,0 +1,23 @@
+package br.com.lett.crawlernode.crawlers.corecontent.brasil;
+
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.crawlers.corecontent.extractionutils.MercadolivreCrawler;
+
+/**
+ * Date: 08/10/2018
+ * 
+ * @author Gabriel Dornelas
+ *
+ */
+public class BrasilMercadolivrecolgateCrawler extends MercadolivreCrawler {
+
+  private static final String HOME_PAGE = "https://loja.mercadolivre.com.br/colgate";
+  private static final String MAIN_SELLER_NAME_LOWER = "colgate";
+
+  public BrasilMercadolivrecolgateCrawler(Session session) {
+    super(session);
+    super.setHomePage(HOME_PAGE);
+    super.setMainSellerNameLower(MAIN_SELLER_NAME_LOWER);
+    super.setSeparator(',');
+  }
+}
