cc85078fe7f4d1c4b66315e160d27beda5b08278
Joao Pedro <joaopedro@lett.digital>
2019-09-13 16:46:55 -0300

    Creating ranking crawler
    Former-commit-id: c45556dce156252d61797c53b0846bd93c6d67c8

---
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilCasatemaCrawler.java
@@ -0,0 +1,84 @@
+package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
+
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+
+public class BrasilCasatemaCrawler extends CrawlerRankingKeywords {
+
+  public BrasilCasatemaCrawler(Session session) {
+    super(session);
+  }
+
+  @Override
+  protected void extractProductsFromCurrentPage() {
+    this.pageSize = 10;
+    this.log("Página " + this.currentPage);
+
+    String url = "https://www.casatema.com.br/" + this.keywordEncoded + "#" + this.currentPage;
+
+    this.log("Link onde são feitos os crawlers: " + url);
+    this.currentDoc = fetchDocument(url);
+    Elements products = this.currentDoc.select(".prateleira ul li[layout]");
+
+    if (!products.isEmpty()) {
+
+      if (this.totalProducts == 0) {
+        setTotalProducts();
+      }
+
+      for (Element e : products) {
+        String internalPid = crawlInternalPid(e);
+        String productUrl = crawlProductUrl(e);
+
+        saveDataProduct(null, internalPid, productUrl);
+
+        this.log("Position: " + this.position + " - InternalId: " + null + " - InternalPid: "
+            + internalPid + " - Url: " + productUrl);
+        if (this.arrayProducts.size() == productsLimit)
+          break;
+
+      }
+    } else {
+      this.result = false;
+      this.log("Keyword sem resultado!");
+    }
+
+    this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora "
+        + this.arrayProducts.size() + " produtos crawleados");
+
+  }
+
+
+  private String crawlInternalPid(Element e) {
+    Element inputElement = e.selectFirst("div[data-product-id]");
+    String internalPid = null;
+
+    if (inputElement != null) {
+      internalPid = inputElement.attr("data-product-id").trim();
+    }
+
+    return internalPid;
+  }
+
+  private String crawlProductUrl(Element e) {
+    Element ancorElement = e.selectFirst(".shelf-qd-v1-name a");
+    String url = null;
+
+    if (ancorElement != null) {
+      url = ancorElement.attr("href").trim();
+    }
+
+    return url;
+  }
+
+  @Override
+  protected void setTotalProducts() {
+    this.totalProducts = CrawlerUtils.scrapIntegerFromHtml(this.currentDoc, ".searchResultsTime .resultado-busca-numero span[class=\"value\"]",
+        false, 0);
+
+    this.log("Total de produtos: " + this.totalProducts);
+  }
+}
