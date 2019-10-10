9a1c9b648a49077a400d2b1205fc854537c43b96
Arthur Floresta <arthurrezende@lett.digital>
2019-10-10 18:55:26 -0300

    Created Amaro's Bichos (Brasil) keywords crawler (incomplete)
    Former-commit-id: c8a410a412ac9fceb151351cce7cba24bc2b6af4

---
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilAmarosbichosCrawler.java
@@ -0,0 +1,93 @@
+package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
+
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+
+public class BrasilAmarosbichosCrawler extends CrawlerRankingKeywords {
+  
+  private static final String HOME_PAGE = "www.petshopamarosbichos.com.br";
+
+  public BrasilAmarosbichosCrawler(Session session) {
+    super(session);
+  }
+  
+  // TODO: passar de pagina. A pesquisa não tem total de produtos e o crawler não segue as paginas
+
+  @Override
+  protected void extractProductsFromCurrentPage() {
+    this.pageSize = 15;
+    this.log("Página " + this.currentPage);
+
+    String url = "https://www.petshopamarosbichos.com.br/busca?page=" 
+        + this.currentPage + "&q=" 
+        + this.keywordEncoded;
+    
+    this.log("Link onde são feitos os crawlers: " + url);
+    this.currentDoc = fetchDocument(url);
+    Elements products = this.currentDoc.select(".result-list > div");
+
+    if (!products.isEmpty()) {
+      /*if (this.totalProducts == 0) {
+        setTotalProducts();
+      }*/
+
+      for (Element e : products) {
+        String internalPid = scrapInternalPid(e, ".description-product .product-name > a");
+        String productUrl = CrawlerUtils.scrapUrl(e, ".description-product .product-name > a", "href", "http:", HOME_PAGE);
+
+        saveDataProduct(null, internalPid, productUrl);
+
+        this.log(
+            "Position: " + this.position + 
+            " - InternalId: " + null +
+            " - InternalPid: " + internalPid + 
+            " - Url: " + productUrl);
+        
+        if (this.arrayProducts.size() == productsLimit)
+          break;
+      }
+      
+    } else {
+      this.result = false;
+      this.log("Keyword sem resultado!");
+    }
+
+    this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora "
+        + this.arrayProducts.size() + " produtos crawleados");
+
+  }
+
+
+  private String scrapInternalPid(Element e, String selector) {
+    String internalPid = null;
+    Element urlElement = e.selectFirst(selector);
+    
+    if(urlElement != null && urlElement.hasAttr("href")) {
+      String url = urlElement.attr("href");
+      
+      String[] div1 = url.split("/");
+      
+      if(div1.length > 0) {
+        String[] div2 = div1[div1.length-1].split("-");
+        internalPid = div2[0];
+      }
+    }
+
+    return internalPid;
+  }
+
+  /*@Override
+  protected void setTotalProducts() {
+    this.totalProducts = CrawlerUtils.scrapIntegerFromHtml(
+        this.currentDoc, 
+        ".page-title-inner .flex-col p.woocommerce-result-count", 
+        "de", 
+        "resultados", 
+        false, true, 0);
+
+    this.log("Total de produtos: " + this.totalProducts);
+  }*/
+}
