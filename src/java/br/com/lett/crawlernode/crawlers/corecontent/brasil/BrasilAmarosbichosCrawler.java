c12ca7e84de7f1f75c0bb94ec2d7a72088e41caf
Arthur Floresta <arthurrezende@lett.digital>
2019-10-10 17:51:10 -0300

    Created Amaro's Bichos (Brasil) core content scrapper
    Former-commit-id: 1f12e560e4e4a96b88b4e933f5138223a2541836

---
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilAmarosbichosCrawler.java
@@ -0,0 +1,113 @@
+package br.com.lett.crawlernode.crawlers.corecontent.brasil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import org.jsoup.nodes.Document;
+import br.com.lett.crawlernode.core.models.Card;
+import br.com.lett.crawlernode.core.models.CategoryCollection;
+import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.models.ProductBuilder;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.Crawler;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.Logging;
+import br.com.lett.crawlernode.util.Pair;
+import models.Marketplace;
+import models.Offers;
+import models.prices.Prices;
+
+public class BrasilAmarosbichosCrawler extends Crawler {
+  
+  private static final String HOME_PAGE = "www.petshopamarosbichos.com.br";
+  
+  public BrasilAmarosbichosCrawler(Session session) {
+    super(session);
+  }
+
+  @Override
+  public List<Product> extractInformation(Document doc) throws Exception {
+    super.extractInformation(doc);
+    List<Product> products = new ArrayList<>();
+    
+    if (isProductPage(doc)) {
+      Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
+      
+      String internalId = CrawlerUtils.scrapStringSimpleInfo(doc, ".cod-sku [itemprop=\"sku\"]", true);
+      String internalPid = CrawlerUtils.scrapStringSimpleInfoByAttribute(doc, ".product-container[data-product-id]", "data-product-id");
+      String name = CrawlerUtils.scrapStringSimpleInfo(doc, ".container .title-page-product", true);
+      Float price = CrawlerUtils.scrapFloatPriceFromHtml(doc, ".product .product-sku-information meta[itemprop=\"price\"]", "content", false, ',', session);
+      Prices prices = scrapPrices(doc, price);
+      CategoryCollection categories = CrawlerUtils.crawlCategories(doc, ".breadcrump-container > ul > li span[itemprop=\"title\"]", true);
+      String primaryImage = CrawlerUtils.scrapSimplePrimaryImage(doc, ".product-sku-image .image-highlight a.main-product img", 
+          Arrays.asList("src"), "https:", HOME_PAGE);
+      String secondaryImages = CrawlerUtils.scrapSimpleSecondaryImages(doc, ".product-sku-image .image-highlight a.main-product img", 
+          Arrays.asList("src"), "https:", HOME_PAGE, primaryImage);
+      String description = CrawlerUtils.scrapSimpleDescription(doc, Arrays.asList(".descriptions"));
+      Integer stock = null;
+      boolean available = doc.selectFirst(".buybox .compra-wrapper") != null;
+      Marketplace marketplace = null;
+      Offers offers = null;
+          
+      // Creating the product
+      Product product = ProductBuilder.create()
+          .setUrl(session.getOriginalURL())
+          .setInternalId(internalId)
+          .setInternalPid(internalPid)
+          .setName(name)
+          .setPrice(price)
+          .setPrices(prices)
+          .setAvailable(available)
+          .setCategory1(categories.getCategory(0))
+          .setCategory2(categories.getCategory(1))
+          .setCategory3(categories.getCategory(2))
+          .setPrimaryImage(primaryImage)
+          .setSecondaryImages(secondaryImages)
+          .setDescription(description)
+          .setStock(stock)
+          .setMarketplace(marketplace)
+          .setOffers(offers)
+          .build();
+      
+      products.add(product);
+      
+    } else {
+      Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
+    }
+    
+    return products;
+  }
+  
+  private boolean isProductPage(Document doc) {
+    return doc.selectFirst("#frontend-product .product-container") != null;
+  }
+  
+  private Prices scrapPrices(Document doc, Float price) {
+    Prices prices = new Prices();
+    
+    if(price != null) {
+      
+      prices.setBankTicketPrice(
+          CrawlerUtils.scrapFloatPriceFromHtml(doc, ".product .product-sku-information [itemprop=\"offers\"] .cash-payment", null, false, ',', session));
+      
+      Map<Integer, Float> installmentPriceMap = new TreeMap<>();
+      installmentPriceMap.put(1, price);
+      
+      Pair<Integer, Float> installment = CrawlerUtils.crawlSimpleInstallment(".parcel-price .cash-payment span", doc, true, "x de", "", true);
+      
+      if (!installment.isAnyValueNull()) {
+        installmentPriceMap.put(installment.getFirst(), installment.getSecond());
+      }
+      
+      prices.insertCardInstallment(Card.VISA.toString(), installmentPriceMap);
+      prices.insertCardInstallment(Card.MASTERCARD.toString(), installmentPriceMap);
+      prices.insertCardInstallment(Card.ELO.toString(), installmentPriceMap);
+      prices.insertCardInstallment(Card.DINERS.toString(), installmentPriceMap);
+      prices.insertCardInstallment(Card.AMEX.toString(), installmentPriceMap);      
+    }
+    
+    return prices;
+  }
+}
