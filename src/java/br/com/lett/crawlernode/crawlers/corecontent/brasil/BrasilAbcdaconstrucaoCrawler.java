49a579c07a9c536284eb318dfea11748af135f10
gabriel <gabrieldornelas@lett.digital>
2019-10-23 11:33:48 -0300

    new crawler abcdaconstrucao
    Former-commit-id: 30749793c6fa2ec1d5fd866abfb7c51026556beb

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilAbcdaconstrucaoCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilAbcdaconstrucaoCrawler.java
@@ -1,29 +1,27 @@
 package br.com.lett.crawlernode.crawlers.corecontent.brasil;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import br.com.lett.crawlernode.core.fetcher.models.Request;
 import br.com.lett.crawlernode.core.fetcher.models.Request.RequestBuilder;
-import br.com.lett.crawlernode.core.models.Card;
+import br.com.lett.crawlernode.core.models.CategoryCollection;
 import br.com.lett.crawlernode.core.models.Product;
 import br.com.lett.crawlernode.core.models.ProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.Crawler;
 import br.com.lett.crawlernode.util.CommonMethods;
+import br.com.lett.crawlernode.util.CrawlerUtils;
 import br.com.lett.crawlernode.util.Logging;
-import br.com.lett.crawlernode.util.MathUtils;
 import models.Marketplace;
 import models.prices.Prices;
 
 /**
- * Date: 10/09/2018
+ * Date: 23/10/2019
  * 
  * @author Gabriel Dornelas
  *
@@ -38,26 +36,21 @@ public class BrasilAbcdaconstrucaoCrawler extends Crawler {
 
   @Override
   protected Object fetch() {
-    Document doc = new Document("");
-    String originalUrl = session.getOriginalURL();
-    String payload = "c=produtos";
+    String id = CommonMethods.getLast(session.getOriginalURL().split("\\?")[0].split("/"));
+    String payload = "c=detalhe-produto&id=" + id;
 
     Map<String, String> headers = new HashMap<>();
     headers.put("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-    String urlToFetch = null;
+    String urlToFetch = "https://www.abcdaconstrucao.com.br/ajax/produtos.ajax.php";
 
-    if (originalUrl.contains("ofertas.php")) {
-      urlToFetch = "https://www.abcdaconstrucao.com.br/ajax/index.ajax.php";
-    } else if (originalUrl.contains("outlet.php")) {
-      urlToFetch = "https://www.abcdaconstrucao.com.br/ajax/outlet.ajax.php";
-    }
+    Request request = RequestBuilder.create()
+        .setUrl(urlToFetch)
+        .setCookies(cookies)
+        .setHeaders(headers)
+        .setPayload(payload)
+        .build();
 
-    if (urlToFetch != null) {
-      Request request = RequestBuilder.create().setUrl(urlToFetch).setCookies(cookies).setHeaders(headers).setPayload(payload).build();
-      doc = Jsoup.parse(this.dataFetcher.post(session, request).getBody());
-    }
-
-    return doc;
+    return Jsoup.parse(this.dataFetcher.post(session, request).getBody());
   }
 
   @Override
@@ -74,161 +67,54 @@ public class BrasilAbcdaconstrucaoCrawler extends Crawler {
     if (isProductPage(doc)) {
       Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
 
-      Elements productsElements = doc.select("a.btn2-oferta");
+      String internalId = CrawlerUtils.scrapStringSimpleInfoByAttribute(doc, "a.desejo[id]", "id");
+      String internalPid = scrapInternalPid(doc);
+      String name = CrawlerUtils.scrapStringSimpleInfo(doc, "h1.title-product", true);
+      CategoryCollection categories = CrawlerUtils.crawlCategories(doc, ".breadcrumbAbc a:not(:first-child)");
+      String primaryImage = CrawlerUtils.scrapSimplePrimaryImage(doc, "#galeriaThumb > a", Arrays.asList("data-zoom-image", "data-image"), "https:",
+          "vendas.digitalabc.com.br");
+      String secondaryImages = CrawlerUtils.scrapSimpleSecondaryImages(doc, "#galeriaThumb > a", Arrays.asList("data-zoom-image", "data-image"),
+          "https:", "vendas.digitalabc.com.br", primaryImage);
+      String description = CrawlerUtils.scrapElementsDescription(doc, Arrays.asList("body > div > h2.subtitle, body > div > [style]"));
 
-      for (Element e : productsElements) {
-        String internalId = crawlInternalId(e);
-        String internalPid = crawlInternalPid(e);
-        String name = crawlName(e);
-        Float price = crawlPrice(e);
-        Prices prices = crawlPrices(price, e);
-        String primaryImage = crawlPrimaryImage(e);
-        String description = crawlDescription(e);
+      // Creating the product
+      Product product = ProductBuilder.create()
+          .setUrl(session.getOriginalURL())
+          .setInternalId(internalId)
+          .setInternalPid(internalPid)
+          .setName(name)
+          .setPrices(new Prices())
+          .setAvailable(false)
+          .setCategory1(categories.getCategory(0))
+          .setCategory2(categories.getCategory(1))
+          .setCategory3(categories.getCategory(2))
+          .setPrimaryImage(primaryImage)
+          .setSecondaryImages(secondaryImages)
+          .setDescription(description)
+          .setMarketplace(new Marketplace())
+          .build();
 
-        // Creating the product
-        Product product = ProductBuilder.create().setUrl(session.getOriginalURL()).setInternalId(internalId).setInternalPid(internalPid).setName(name)
-            .setPrice(price).setPrices(prices).setAvailable(true).setPrimaryImage(primaryImage).setDescription(description)
-            .setMarketplace(new Marketplace()).build();
-
-        products.add(product);
-      }
+      products.add(product);
 
     } else {
       Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
     }
 
     return products;
-
   }
 
   private boolean isProductPage(Document doc) {
-    return !doc.select("a.btn2-oferta").isEmpty();
+    return doc.selectFirst("a.desejo[id]") != null;
   }
 
-  private String crawlInternalPid(Element e) {
+  private String scrapInternalPid(Document doc) {
     String internalPid = null;
 
-    if (e.hasAttr("data-cod")) {
-      internalPid = e.attr("data-cod");
-    } else if (e.hasAttr("rel")) {
-      internalPid = e.attr("rel");
+    String brand = CrawlerUtils.scrapStringSimpleInfo(doc, ".box-white-product > p", true);
+    if (brand != null) {
+      internalPid = CommonMethods.getLast(brand.split(":")).trim();
     }
 
     return internalPid;
   }
-
-  private String crawlInternalId(Element e) {
-    String internalId = null;
-
-    Element internalIdElement = e.selectFirst(".rodape-produto");
-    if (internalIdElement != null) {
-      String text = internalIdElement.ownText().toLowerCase().trim();
-
-      if (text.contains("cod.")) {
-        String value = CommonMethods.getLast(text.split("cod.")).trim();
-
-        if (value.contains(" ")) {
-          internalId = value.split(" ")[0];
-        } else {
-          internalId = value;
-        }
-      }
-    }
-
-    return internalId;
-  }
-
-  private String crawlName(Element e) {
-    String name = null;
-    Element nameElement = e.selectFirst(".produtocontent [itemprop=name]");
-
-    if (nameElement != null) {
-      name = nameElement.ownText().trim();
-    }
-
-    return name;
-  }
-
-  private Float crawlPrice(Element e) {
-    Float price = null;
-
-    String priceText = null;
-    Element salePriceElement = e.selectFirst(".produtocontent .preco [itemprop=price]");
-
-    if (salePriceElement != null) {
-      priceText = salePriceElement.ownText();
-
-      // In this site some products appear like this: R$ 54,90
-      // and others appear like this: R$ 54.90
-      if (priceText.contains(",")) {
-        price = MathUtils.parseFloatWithComma(priceText);
-      } else {
-        price = Float.parseFloat(priceText.replaceAll("[^0-9.]", ""));
-      }
-    }
-
-    return price;
-  }
-
-  private String crawlPrimaryImage(Element e) {
-    String primaryImage = null;
-    Element elementPrimaryImage = e.selectFirst(".produtoimagem .img-produto img");
-
-    if (elementPrimaryImage != null) {
-      primaryImage = elementPrimaryImage.attr("src").trim();
-
-      if (!primaryImage.startsWith("http")) {
-        primaryImage = HOME_PAGE + primaryImage;
-      }
-    }
-
-    return primaryImage;
-  }
-
-  private String crawlDescription(Element e) {
-    StringBuilder description = new StringBuilder();
-
-    Element desc = e.selectFirst(".rodape-produto");
-
-    if (desc != null) {
-      description.append(desc.html());
-    }
-
-
-    return description.toString();
-  }
-
-  /**
-   * In the time when this crawler was made, this market hasn't installments informations
-   * 
-   * @param element
-   * @param price
-   * @return
-   */
-  private Prices crawlPrices(Float price, Element element) {
-    Prices prices = new Prices();
-
-    if (price != null) {
-      Map<Integer, Float> installmentPriceMap = new TreeMap<>();
-      installmentPriceMap.put(1, price);
-
-      Element priceFrom = element.selectFirst(".produtocontent .txt-preco1");
-      if (priceFrom != null) {
-        String priceText = priceFrom.ownText();
-
-        if (priceText.contains(",")) {
-          prices.setPriceFrom(MathUtils.parseDoubleWithComma(priceText));
-        } else {
-          prices.setPriceFrom(Double.parseDouble(priceText.replaceAll("[^0-9.]", "")));
-        }
-      }
-
-      prices.setBankTicketPrice(price);
-
-      prices.insertCardInstallment(Card.VISA.toString(), installmentPriceMap);
-    }
-
-    return prices;
-  }
-
 }
