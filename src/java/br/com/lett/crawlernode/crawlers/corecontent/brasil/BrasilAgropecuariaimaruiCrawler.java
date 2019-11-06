22e463d8d999ccbfb8681096f83393f2da391258
paula <paula@lett>
2019-11-06 17:51:37 -0200

    Fix price of product
    Former-commit-id: d4f7fa28a0b19dfa65ff39d0252fe70e3ce85f4b

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilAgropecuariaimaruiCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilAgropecuariaimaruiCrawler.java
@@ -54,7 +54,7 @@ public class BrasilAgropecuariaimaruiCrawler extends Crawler {
       String internalId = CrawlerUtils.scrapStringSimpleInfoByAttribute(doc, "input#comment_post_ID", "value");
       String internalPid = skuJson != null && skuJson.has("sku") ? skuJson.get("sku").toString() : null;
       String name = skuJson != null && skuJson.has("name") ? skuJson.get("name").toString() : null;
-      Float price = CrawlerUtils.scrapFloatPriceFromHtml(doc, ".price-wrapper .product-page-price :not(:first-child) .woocommerce-Price-amount", null, true, ',', session);
+      Float price = crawlPrice(doc);
       Prices prices = scrapPrices(doc, price);
       CategoryCollection categories = CrawlerUtils.crawlCategories(doc, ".product-info .woocommerce-breadcrumb a[href]", true);
       String primaryImage = CrawlerUtils.scrapSimplePrimaryImage(doc, ".woocommerce-product-gallery figure img", Arrays.asList("src"), "https:", HOME_PAGE);
@@ -111,6 +111,14 @@ public class BrasilAgropecuariaimaruiCrawler extends Crawler {
     }
     
     return products;
+  }
+  
+  private float crawlPrice(Document doc) {
+	 Float price = CrawlerUtils.scrapFloatPriceFromHtml(doc, ".price-wrapper .product-page-price :not(:first-child) .woocommerce-Price-amount", null, true, ',', session);
+	 if(price == null) {
+		 price = CrawlerUtils.scrapFloatPriceFromHtml(doc, ".price-wrapper .product-page-price .woocommerce-Price-amount", null, true, ',', session);
+	 }
+	 return price;
   }
   
   private boolean isProductPage(Document doc) {
