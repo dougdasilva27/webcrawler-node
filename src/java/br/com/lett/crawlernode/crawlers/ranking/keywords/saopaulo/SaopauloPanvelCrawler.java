25f5803d9d7cd45ad67487983cf9af20fad7f48d
Gabriel Dornelas <gabrieldornelas@lett.digital>
2019-11-19 14:48:06 -0200

    bugfix panvel selector
    Former-commit-id: a125a4921495749c605f516226d04a67646636b5

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/saopaulo/SaopauloPanvelCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/saopaulo/SaopauloPanvelCrawler.java
@@ -10,76 +10,76 @@ import br.com.lett.crawlernode.util.CommonMethods;
 
 public class SaopauloPanvelCrawler extends CrawlerRankingKeywords {
 
-  public SaopauloPanvelCrawler(Session session) {
-    super(session);
-  }
+   public SaopauloPanvelCrawler(Session session) {
+      super(session);
+   }
 
-  @Override
-  protected void extractProductsFromCurrentPage() {
-    // número de produtos por página do market
-    this.pageSize = 15;
-    this.log("Página " + this.currentPage);
+   @Override
+   protected void extractProductsFromCurrentPage() {
+      // número de produtos por página do market
+      this.pageSize = 15;
+      this.log("Página " + this.currentPage);
 
-    // monta a url com a keyword e a página
-    String url = "https://www.panvel.com/panvel/buscarProduto.do?paginaAtual=" + this.currentPage
-        + "&termoPesquisa=" + this.keywordWithoutAccents.replace(" ", "+");
-    this.log("Link onde são feitos os crawlers: " + url);
+      // monta a url com a keyword e a página
+      String url = "https://www.panvel.com/panvel/buscarProduto.do?paginaAtual=" + this.currentPage
+            + "&termoPesquisa=" + this.keywordWithoutAccents.replace(" ", "+");
+      this.log("Link onde são feitos os crawlers: " + url);
 
-    this.currentDoc = crawlProductsInfo(url);
-    Elements products = this.currentDoc.select(".box-produto > a");
+      this.currentDoc = crawlProductsInfo(url);
+      Elements products = this.currentDoc.select(".box-produto a");
 
-    if (!products.isEmpty()) {
-      for (Element e : products) {
-        String urlProduct = crawlProductUrl(e);
-        String internalId = crawlInternalId(urlProduct);
+      if (!products.isEmpty()) {
+         for (Element e : products) {
+            String urlProduct = crawlProductUrl(e);
+            String internalId = crawlInternalId(urlProduct);
 
-        saveDataProduct(internalId, null, urlProduct);
+            saveDataProduct(internalId, null, urlProduct);
 
-        this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: "
-            + null + " - Url: " + urlProduct);
-        if (this.arrayProducts.size() == productsLimit)
-          break;
+            this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: "
+                  + null + " - Url: " + urlProduct);
+            if (this.arrayProducts.size() == productsLimit)
+               break;
+         }
+      } else {
+         this.result = false;
+         this.log("Keyword sem resultado!");
       }
-    } else {
-      this.result = false;
-      this.log("Keyword sem resultado!");
-    }
 
-    this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora "
-        + this.arrayProducts.size() + " produtos crawleados");
-  }
+      this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora "
+            + this.arrayProducts.size() + " produtos crawleados");
+   }
 
-  @Override
-  protected boolean hasNextPage() {
-    Element item = this.currentDoc.select(".pagination__item a").last();
-    return item != null && !item.hasClass("pagination__arrow--disabled");
-  }
+   @Override
+   protected boolean hasNextPage() {
+      Element item = this.currentDoc.select(".pagination__item a").last();
+      return item != null && !item.hasClass("pagination__arrow--disabled");
+   }
 
-  private String crawlInternalId(String url) {
-    return CommonMethods.getLast(url.split("-"));
-  }
+   private String crawlInternalId(String url) {
+      return CommonMethods.getLast(url.split("-"));
+   }
 
-  private String crawlProductUrl(Element e) {
-    String productUrl = e.attr("href");
+   private String crawlProductUrl(Element e) {
+      String productUrl = e.attr("href");
 
-    if (!productUrl.startsWith("https://www.panvel.com")) {
-      productUrl = "https://www.panvel.com" + productUrl;
-    }
+      if (!productUrl.startsWith("https://www.panvel.com")) {
+         productUrl = "https://www.panvel.com" + productUrl;
+      }
 
-    return productUrl;
-  }
+      return productUrl;
+   }
 
-  private Document crawlProductsInfo(String url) {
-    String response = fetchPostFetcher(url, null, null, null);
+   private Document crawlProductsInfo(String url) {
+      String response = fetchPostFetcher(url, null, null, null);
 
-    if (response != null && !response.isEmpty()) {
+      if (response != null && !response.isEmpty()) {
 
-      return Jsoup.parse(response);
+         return Jsoup.parse(response);
 
-    } else {
+      } else {
 
-      return fetchDocument(url);
+         return fetchDocument(url);
 
-    }
-  }
+      }
+   }
 }
