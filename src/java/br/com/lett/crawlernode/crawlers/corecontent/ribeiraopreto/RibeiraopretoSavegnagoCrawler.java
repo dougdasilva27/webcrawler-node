a40a010e9b38437039b01e00b1df58b49544af49
marcos <marcos@marcos-OptiPlex-7040>
2019-12-05 14:46:27 -0200

    Created queryString
    Former-commit-id: f38a02e5e3c9ea3afc4ddb7853ea222de62d5427

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/ribeiraopreto/RibeiraopretoSavegnagoCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/ribeiraopreto/RibeiraopretoSavegnagoCrawler.java
@@ -26,123 +26,123 @@ import models.prices.Prices;
 
 public class RibeiraopretoSavegnagoCrawler extends Crawler {
 
-  /*
-   * Ribeirão Preto - 1 Sertãozinho - 6 Jardinópolis - 11 Jaboticabal - 7 Franca - 3 Barretos - 10
-   * Bebedouro - 9 Monte Alto - 12 Araraquara - 4 São carlos - 5 Matão - 8
-   */
+   /*
+    * Ribeirão Preto - 1 Sertãozinho - 6 Jardinópolis - 11 Jaboticabal - 7 Franca - 3 Barretos - 10
+    * Bebedouro - 9 Monte Alto - 12 Araraquara - 4 São carlos - 5 Matão - 8
+    */
 
-  private static final String HOME_PAGE = "https://www.savegnago.com.br/";
-  private static final String MAIN_SELLER_NAME_LOWER = "savegnago supermercados";
+   private static final String HOME_PAGE = "https://www.savegnago.com.br/";
+   private static final String MAIN_SELLER_NAME_LOWER = "savegnago supermercados";
 
-  public RibeiraopretoSavegnagoCrawler(Session session) {
-    super(session);
-  }
+   public RibeiraopretoSavegnagoCrawler(Session session) {
+      super(session);
+   }
 
-  @Override
-  public boolean shouldVisit() {
-    String href = this.session.getOriginalURL().toLowerCase();
-    return !FILTERS.matcher(href).matches() && (href.startsWith(HOME_PAGE));
-  }
+   @Override
+   public boolean shouldVisit() {
+      String href = this.session.getOriginalURL().toLowerCase();
+      return !FILTERS.matcher(href).matches() && (href.startsWith(HOME_PAGE));
+   }
 
-  @Override
-  public String handleURLBeforeFetch(String curURL) {
+   @Override
+   public String handleURLBeforeFetch(String curURL) {
 
-    if (curURL.endsWith("/p")) {
-      try {
-        String url = curURL;
-        List<NameValuePair> paramsOriginal = URLEncodedUtils.parse(new URI(url), "UTF-8");
-        List<NameValuePair> paramsNew = new ArrayList<>();
+      if (curURL.endsWith("/p")) {
+         try {
+            String url = curURL;
+            List<NameValuePair> paramsOriginal = URLEncodedUtils.parse(new URI(url), "UTF-8");
+            List<NameValuePair> paramsNew = new ArrayList<>();
 
-        for (NameValuePair param : paramsOriginal) {
-          if (!param.getName().equals("sc")) {
-            paramsNew.add(param);
-          }
-        }
+            for (NameValuePair param : paramsOriginal) {
+               if (!param.getName().equals("sc")) {
+                  paramsNew.add(param);
+               }
+            }
 
-        paramsNew.add(new BasicNameValuePair("sc", "2"));
-        URIBuilder builder = new URIBuilder(curURL.split("\\?")[0]);
+            paramsNew.add(new BasicNameValuePair("sc", "2"));
+            URIBuilder builder = new URIBuilder(curURL.split("\\?")[0]);
 
-        builder.clearParameters();
-        builder.setParameters(paramsNew);
+            builder.clearParameters();
+            builder.setParameters(paramsNew);
 
-        return builder.build().toString();
+            return builder.build().toString();
 
-      } catch (URISyntaxException e) {
-        return curURL;
-      }
-    }
-
-    return curURL;
-
-  }
-
-  @Override
-  public List<Product> extractInformation(Document doc) throws Exception {
-    super.extractInformation(doc);
-    List<Product> products = new ArrayList<>();
-
-    if (isProductPage(doc)) {
-      VTEXCrawlersUtils vtexUtil = new VTEXCrawlersUtils(session, MAIN_SELLER_NAME_LOWER, HOME_PAGE, cookies, dataFetcher);
-      vtexUtil.setHasBankTicket(false);
-
-      JSONObject skuJson = CrawlerUtils.crawlSkuJsonVTEX(doc, session);
-      String internalPid = vtexUtil.crawlInternalPid(skuJson);
-      CategoryCollection categories = CrawlerUtils.crawlCategories(doc, ".bread-crumb > ul li a");
-
-      // sku data in json
-      JSONArray arraySkus = skuJson != null && skuJson.has("skus") ? skuJson.getJSONArray("skus") : new JSONArray();
-
-      // ean data in json
-      JSONArray arrayEans = CrawlerUtils.scrapEanFromVTEX(doc);
-
-      for (int i = 0; i < arraySkus.length(); i++) {
-        JSONObject jsonSku = arraySkus.getJSONObject(i);
-
-        String internalId = vtexUtil.crawlInternalId(jsonSku);
-        JSONObject apiJSON = vtexUtil.crawlApi(internalId);
-        String name = vtexUtil.crawlName(jsonSku, skuJson, " ");
-        String description = scrapDescription(doc, internalId);
-        Map<String, Prices> marketplaceMap = vtexUtil.crawlMarketplace(apiJSON, internalId, false);
-        Marketplace marketplace = vtexUtil.assembleMarketplaceFromMap(marketplaceMap);
-        boolean available = marketplaceMap.containsKey(MAIN_SELLER_NAME_LOWER);
-        String primaryImage = vtexUtil.crawlPrimaryImage(apiJSON);
-        String secondaryImages = vtexUtil.crawlSecondaryImages(apiJSON);
-        Prices prices = marketplaceMap.containsKey(MAIN_SELLER_NAME_LOWER) ? marketplaceMap.get(MAIN_SELLER_NAME_LOWER) : new Prices();
-        Float price = vtexUtil.crawlMainPagePrice(prices);
-        Integer stock = vtexUtil.crawlStock(apiJSON);
-        String ean = i < arrayEans.length() ? arrayEans.getString(i) : null;
-
-        List<String> eans = new ArrayList<>();
-        eans.add(ean);
-
-        // Creating the product
-        Product product = ProductBuilder.create().setUrl(session.getOriginalURL()).setInternalId(internalId).setInternalPid(internalPid).setName(name)
-            .setPrice(price).setPrices(prices).setAvailable(available).setCategory1(categories.getCategory(0)).setCategory2(categories.getCategory(1))
-            .setCategory3(categories.getCategory(2)).setPrimaryImage(primaryImage).setSecondaryImages(secondaryImages).setDescription(description)
-            .setStock(stock).setMarketplace(marketplace).setEans(eans).build();
-
-        products.add(product);
+         } catch (URISyntaxException e) {
+            return curURL;
+         }
       }
 
-    } else {
-      Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
-    }
+      return curURL;
 
-    return products;
-  }
+   }
+
+   @Override
+   public List<Product> extractInformation(Document doc) throws Exception {
+      super.extractInformation(doc);
+      List<Product> products = new ArrayList<>();
+
+      if (isProductPage(doc)) {
+         VTEXCrawlersUtils vtexUtil = new VTEXCrawlersUtils(session, MAIN_SELLER_NAME_LOWER, HOME_PAGE, cookies, dataFetcher);
+         vtexUtil.setHasBankTicket(false);
+
+         JSONObject skuJson = CrawlerUtils.crawlSkuJsonVTEX(doc, session);
+         String internalPid = vtexUtil.crawlInternalPid(skuJson);
+         CategoryCollection categories = CrawlerUtils.crawlCategories(doc, ".bread-crumb > ul li a");
+
+         // sku data in json
+         JSONArray arraySkus = skuJson != null && skuJson.has("skus") ? skuJson.getJSONArray("skus") : new JSONArray();
+
+         // ean data in json
+         JSONArray arrayEans = CrawlerUtils.scrapEanFromVTEX(doc);
+
+         for (int i = 0; i < arraySkus.length(); i++) {
+            JSONObject jsonSku = arraySkus.getJSONObject(i);
+
+            String internalId = vtexUtil.crawlInternalId(jsonSku);
+            JSONObject apiJSON = vtexUtil.crawlApi(internalId, "sc=2");
+            String name = vtexUtil.crawlName(jsonSku, skuJson, " ");
+            String description = scrapDescription(doc, internalId);
+            Map<String, Prices> marketplaceMap = vtexUtil.crawlMarketplace(apiJSON, internalId, false);
+            Marketplace marketplace = vtexUtil.assembleMarketplaceFromMap(marketplaceMap);
+            boolean available = marketplaceMap.containsKey(MAIN_SELLER_NAME_LOWER);
+            String primaryImage = vtexUtil.crawlPrimaryImage(apiJSON);
+            String secondaryImages = vtexUtil.crawlSecondaryImages(apiJSON);
+            Prices prices = marketplaceMap.containsKey(MAIN_SELLER_NAME_LOWER) ? marketplaceMap.get(MAIN_SELLER_NAME_LOWER) : new Prices();
+            Float price = vtexUtil.crawlMainPagePrice(prices);
+            Integer stock = vtexUtil.crawlStock(apiJSON);
+            String ean = i < arrayEans.length() ? arrayEans.getString(i) : null;
+
+            List<String> eans = new ArrayList<>();
+            eans.add(ean);
+
+            // Creating the product
+            Product product = ProductBuilder.create().setUrl(session.getOriginalURL()).setInternalId(internalId).setInternalPid(internalPid).setName(name)
+                  .setPrice(price).setPrices(prices).setAvailable(available).setCategory1(categories.getCategory(0)).setCategory2(categories.getCategory(1))
+                  .setCategory3(categories.getCategory(2)).setPrimaryImage(primaryImage).setSecondaryImages(secondaryImages).setDescription(description)
+                  .setStock(stock).setMarketplace(marketplace).setEans(eans).build();
+
+            products.add(product);
+         }
+
+      } else {
+         Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
+      }
+
+      return products;
+   }
 
 
-  private boolean isProductPage(Document document) {
-    return document.selectFirst(".productName") != null;
-  }
+   private boolean isProductPage(Document document) {
+      return document.selectFirst(".productName") != null;
+   }
 
-  private String scrapDescription(Document doc, String internalId) {
-    StringBuilder description = new StringBuilder();
+   private String scrapDescription(Document doc, String internalId) {
+      StringBuilder description = new StringBuilder();
 
-    description.append(CrawlerUtils.scrapSimpleDescription(doc, Arrays.asList(".productDescription")));
-    description.append(CrawlerUtils.scrapLettHtml(internalId, session, session.getMarket().getNumber()));
+      description.append(CrawlerUtils.scrapSimpleDescription(doc, Arrays.asList(".productDescription")));
+      description.append(CrawlerUtils.scrapLettHtml(internalId, session, session.getMarket().getNumber()));
 
-    return description.toString();
-  }
+      return description.toString();
+   }
 
 }
