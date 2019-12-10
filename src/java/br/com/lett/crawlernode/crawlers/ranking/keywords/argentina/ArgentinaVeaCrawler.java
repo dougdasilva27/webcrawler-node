e2754742e9d8a0fa732b830420cd9d616b8d3380
Gabriel Dornelas <gabrieldornelas@lett.digital>
2019-12-10 13:40:32 -0200

    bugfix ranking argentina vea disco
    Former-commit-id: f14c5dd8673ca8351bd65b5260ef08d3766d91e6

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/argentina/ArgentinaVeaCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/argentina/ArgentinaVeaCrawler.java
@@ -6,6 +6,7 @@ import org.apache.http.HttpHeaders;
 import org.apache.http.impl.cookie.BasicClientCookie;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import br.com.lett.crawlernode.core.fetcher.FetchMode;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
 import br.com.lett.crawlernode.util.CommonMethods;
@@ -14,141 +15,142 @@ import br.com.lett.crawlernode.util.Logging;
 
 public class ArgentinaVeaCrawler extends CrawlerRankingKeywords {
 
-  public ArgentinaVeaCrawler(Session session) {
-    super(session);
-  }
+   public ArgentinaVeaCrawler(Session session) {
+      super(session);
+      super.fetchMode = FetchMode.FETCHER;
+   }
 
-  private static final String HOME_PAGE = "https://www.veadigital.com.ar/";
+   private static final String HOME_PAGE = "https://www.veadigital.com.ar/";
 
-  @Override
-  protected void processBeforeFetch() {
-    BasicClientCookie cookie = new BasicClientCookie("noLocalizar", "true");
-    cookie.setDomain("www.veadigital.com.ar");
-    cookie.setPath("/");
-    this.cookies.add(cookie);
+   @Override
+   protected void processBeforeFetch() {
+      BasicClientCookie cookie = new BasicClientCookie("noLocalizar", "true");
+      cookie.setDomain("www.veadigital.com.ar");
+      cookie.setPath("/");
+      this.cookies.add(cookie);
 
-    Logging.printLogDebug(logger, session, "Adding cookies...");
-    this.cookies.addAll(CrawlerUtils.fetchCookiesFromAPage(HOME_PAGE + "Comprar/Home.aspx", null, "www.veadigital.com.ar", "/", cookies, session, new HashMap<>(), dataFetcher));
-  }
+      Logging.printLogDebug(logger, session, "Adding cookies...");
+      this.cookies.addAll(CrawlerUtils.fetchCookiesFromAPage(HOME_PAGE + "Comprar/Home.aspx", null, "www.veadigital.com.ar", "/", cookies, session, new HashMap<>(), dataFetcher));
+   }
 
-  @Override
-  protected void extractProductsFromCurrentPage() {
-    this.log("Página " + this.currentPage);
+   @Override
+   protected void extractProductsFromCurrentPage() {
+      this.log("Página " + this.currentPage);
 
-    JSONObject jsonSearch = crawlProductsApi(CommonMethods.encondeStringURLToISO8859(this.location, logger, session));
-    JSONArray products = new JSONArray();
+      JSONObject jsonSearch = crawlProductsApi(CommonMethods.encondeStringURLToISO8859(this.location, logger, session));
+      JSONArray products = new JSONArray();
 
-    if (jsonSearch.has("ResultadosBusquedaLevex")) {
-      products = jsonSearch.getJSONArray("ResultadosBusquedaLevex");
-    }
-
-    // se obter 1 ou mais links de produtos e essa página tiver resultado faça:
-    if (products.length() >= 1) {
-      // se o total de busca não foi setado ainda, chama a função para setar
-      if (this.totalProducts == 0) {
-        this.totalProducts = products.length();
+      if (jsonSearch.has("ResultadosBusquedaLevex")) {
+         products = jsonSearch.getJSONArray("ResultadosBusquedaLevex");
       }
 
-      for (int i = 0; i < products.length(); i++) {
-        JSONObject product = products.getJSONObject(i);
+      // se obter 1 ou mais links de produtos e essa página tiver resultado faça:
+      if (products.length() >= 1) {
+         // se o total de busca não foi setado ainda, chama a função para setar
+         if (this.totalProducts == 0) {
+            this.totalProducts = products.length();
+         }
 
-        // InternalPid
-        String internalPid = crawlInternalPid(product);
+         for (int i = 0; i < products.length(); i++) {
+            JSONObject product = products.getJSONObject(i);
 
-        // InternalId
-        String internalId = crawlInternalId(product);
+            // InternalPid
+            String internalPid = crawlInternalPid(product);
 
-        // Url do produto
-        String urlProduct = crawlProductUrl(product);
+            // InternalId
+            String internalId = crawlInternalId(product);
 
-        saveDataProduct(internalId, internalPid, urlProduct);
+            // Url do produto
+            String urlProduct = crawlProductUrl(product);
 
-        this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + internalPid + " - Url: " + urlProduct);
-        if (this.arrayProducts.size() == productsLimit) {
-          break;
-        }
+            saveDataProduct(internalId, internalPid, urlProduct);
 
+            this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + internalPid + " - Url: " + urlProduct);
+            if (this.arrayProducts.size() == productsLimit) {
+               break;
+            }
+
+         }
+      } else {
+         this.result = false;
+         this.log("Keyword sem resultado!");
       }
-    } else {
-      this.result = false;
-      this.log("Keyword sem resultado!");
-    }
 
-    // número de produtos por página do market
-    this.pageSize = this.totalProducts;
+      // número de produtos por página do market
+      this.pageSize = this.totalProducts;
 
-    this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
-  }
+      this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
+   }
 
-  @Override
-  protected boolean hasNextPage() {
-    // Nesse market não existe proximas páginas
-    return false;
-  }
+   @Override
+   protected boolean hasNextPage() {
+      // Nesse market não existe proximas páginas
+      return false;
+   }
 
-  private String crawlInternalId(JSONObject product) {
-    String internalId = null;
+   private String crawlInternalId(JSONObject product) {
+      String internalId = null;
 
-    if (product.has("IdArticulo") && !product.isNull("IdArticulo")) {
-      internalId = product.getString("IdArticulo");
-    }
+      if (product.has("IdArticulo") && !product.isNull("IdArticulo")) {
+         internalId = product.getString("IdArticulo");
+      }
 
-    return internalId;
-  }
+      return internalId;
+   }
 
-  private String crawlInternalPid(JSONObject product) {
-    String internalPid = null;
+   private String crawlInternalPid(JSONObject product) {
+      String internalPid = null;
 
-    return internalPid;
-  }
+      return internalPid;
+   }
 
-  private String crawlProductUrl(JSONObject product) {
-    String productUrl = null;
+   private String crawlProductUrl(JSONObject product) {
+      String productUrl = null;
 
-    if (product.has("DescripcionArticulo") && !product.isNull("DescripcionArticulo")) {
-      String name = product.getString("DescripcionArticulo");
+      if (product.has("DescripcionArticulo") && !product.isNull("DescripcionArticulo")) {
+         String name = product.getString("DescripcionArticulo");
 
-      productUrl = "https://www.veadigital.com.ar/Comprar/Home.aspx?#_atCategory=false&_atGrilla=true&_query=" + CommonMethods.encondeStringURLToISO8859(name, logger, session);
-    }
+         productUrl = "https://www.veadigital.com.ar/Comprar/Home.aspx?#_atCategory=false&_atGrilla=true&_query=" + CommonMethods.encondeStringURLToISO8859(name, logger, session);
+      }
 
-    return productUrl;
-  }
+      return productUrl;
+   }
 
-  /**
-   * Crawl api of search when probably has only one product
-   * 
-   * @param url
-   * @return
-   */
-  private JSONObject crawlProductsApi(String keyword) {
-    JSONObject json = new JSONObject();
+   /**
+    * Crawl api of search when probably has only one product
+    * 
+    * @param url
+    * @return
+    */
+   private JSONObject crawlProductsApi(String keyword) {
+      JSONObject json = new JSONObject();
 
-    Map<String, String> headers = new HashMap<>();
-    headers.put(HttpHeaders.CONTENT_TYPE, "application/json; charset=UTF-8");
+      Map<String, String> headers = new HashMap<>();
+      headers.put(HttpHeaders.CONTENT_TYPE, "application/json; charset=UTF-8");
 
-    String urlSearch = "https://www.veadigital.com.ar/Comprar/HomeService.aspx/ObtenerArticulosPorDescripcionMarcaFamiliaLevex";
-    String payload = "{IdMenu:\"\",textoBusqueda:\"" + keyword + "\"," + " producto:\"\", marca:\"\", pager:\"\", ordenamiento:0, precioDesde:\"\", precioHasta:\"\"}";
+      String urlSearch = "https://www.veadigital.com.ar/Comprar/HomeService.aspx/ObtenerArticulosPorDescripcionMarcaFamiliaLevex";
+      String payload = "{IdMenu:\"\",textoBusqueda:\"" + keyword + "\"," + " producto:\"\", marca:\"\", pager:\"\", ordenamiento:0, precioDesde:\"\", precioHasta:\"\"}";
 
-    this.log("Payload: " + payload);
-    this.log("Cookies: " + this.cookies);
+      this.log("Payload: " + payload);
+      this.log("Cookies: " + this.cookies);
 
-    String jsonString = fetchStringPOST(urlSearch, payload, headers, this.cookies);
+      String jsonString = fetchStringPOST(urlSearch, payload, headers, this.cookies);
 
-    if (jsonString != null && jsonString.startsWith("{")) {
-      json = parseJsonLevex(new JSONObject(jsonString));
-    }
+      if (jsonString != null && jsonString.startsWith("{")) {
+         json = parseJsonLevex(new JSONObject(jsonString));
+      }
 
-    return json;
-  }
+      return json;
+   }
 
-  private JSONObject parseJsonLevex(JSONObject json) {
-    JSONObject jsonD = new JSONObject();
+   private JSONObject parseJsonLevex(JSONObject json) {
+      JSONObject jsonD = new JSONObject();
 
-    if (json.has("d")) {
-      String dParser = JSONObject.stringToValue(json.getString("d")).toString();
-      jsonD = new JSONObject(dParser);
-    }
+      if (json.has("d")) {
+         String dParser = JSONObject.stringToValue(json.getString("d")).toString();
+         jsonD = new JSONObject(dParser);
+      }
 
-    return jsonD;
-  }
+      return jsonD;
+   }
 }
