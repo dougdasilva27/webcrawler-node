724687af645ebab126d3d9ffbcec54cc160ab6c1
Gabriel Dornelas <gabrieldornelas@lett.digital>
2019-12-12 19:14:35 -0200

    bugfix martins
    Former-commit-id: bb1ec18d67032c5521d98de766998f90c8e18c9f

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilMartinsmondelezCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilMartinsmondelezCrawler.java
@@ -17,7 +17,6 @@ import br.com.lett.crawlernode.core.models.Product;
 import br.com.lett.crawlernode.core.models.ProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.Crawler;
-import br.com.lett.crawlernode.test.Test;
 import br.com.lett.crawlernode.util.CommonMethods;
 import br.com.lett.crawlernode.util.CrawlerUtils;
 import br.com.lett.crawlernode.util.Logging;
@@ -26,119 +25,117 @@ import models.prices.Prices;
 
 public class BrasilMartinsmondelezCrawler extends Crawler {
 
-  private static final String HOME_PAGE = "https://www.martinsatacado.com.br";
+   private static final String HOME_PAGE = "https://www.martinsatacado.com.br";
 
-  public BrasilMartinsmondelezCrawler(Session session) {
-    super(session);
-    super.config.setFetcher(FetchMode.FETCHER);
-  }
+   public BrasilMartinsmondelezCrawler(Session session) {
+      super(session);
+      super.config.setFetcher(FetchMode.FETCHER);
+   }
 
-  @Override
-  public boolean shouldVisit() {
-    String href = session.getOriginalURL().toLowerCase();
-    return !FILTERS.matcher(href).matches() && (href.startsWith(HOME_PAGE));
-  }
+   @Override
+   public boolean shouldVisit() {
+      String href = session.getOriginalURL().toLowerCase();
+      return !FILTERS.matcher(href).matches() && (href.startsWith(HOME_PAGE));
+   }
 
-  private static final String EMAIL_LOGIN = "erika.rosa@mdlz.com";
-  private static final String PASSWORD = "monica08";
+   private static final String EMAIL_LOGIN = "erika.rosa@mdlz.com";
+   private static final String PASSWORD = "monica08";
 
 
-  @Override
-  protected Object fetch() {
-     try {
-    this.webdriver = DynamicDataFetcher.fetchPageWebdriver("https://www.martinsatacado.com.br/login", session);
-    this.webdriver.waitLoad(10000);
+   @Override
+   protected Object fetch() {
+      try {
+         this.webdriver = DynamicDataFetcher.fetchPageWebdriver("https://www.martinsatacado.com.br/login", session);
+         this.webdriver.waitLoad(10000);
 
-    WebElement email = this.webdriver.driver.findElement(By.cssSelector("#js_username_login"));
-    email.sendKeys(EMAIL_LOGIN);
-    this.webdriver.waitLoad(2000);
+         WebElement email = this.webdriver.driver.findElement(By.cssSelector("#js_username_login"));
+         email.sendKeys(EMAIL_LOGIN);
+         this.webdriver.waitLoad(2000);
 
-    WebElement cnpj = this.webdriver.driver.findElement(By.cssSelector("#jsSelectCNPJ"));
-    this.webdriver.clickOnElementViaJavascript(cnpj);
-    this.webdriver.waitLoad(2000);
+         WebElement cnpj = this.webdriver.driver.findElement(By.cssSelector("#jsSelectCNPJ"));
+         this.webdriver.clickOnElementViaJavascript(cnpj);
+         this.webdriver.waitLoad(2000);
 
-    WebElement pass = this.webdriver.driver.findElement(By.cssSelector("#j_password[required]"));
-    pass.sendKeys(PASSWORD);
-    this.webdriver.waitLoad(2000);
+         WebElement pass = this.webdriver.driver.findElement(By.cssSelector("#j_password[required]"));
+         pass.sendKeys(PASSWORD);
+         this.webdriver.waitLoad(2000);
 
-    WebElement login = this.webdriver.driver.findElement(By.cssSelector(".sectionButtons .btn-primary"));
-    this.webdriver.clickOnElementViaJavascript(login);
-    this.webdriver.waitLoad(2000);
+         WebElement login = this.webdriver.driver.findElement(By.cssSelector(".sectionButtons .btn-primary"));
+         this.webdriver.clickOnElementViaJavascript(login);
+         this.webdriver.waitLoad(6000);
 
-    this.webdriver.loadUrl(session.getOriginalURL());
-    this.webdriver.waitLoad(10000);
+         this.webdriver.loadUrl(session.getOriginalURL());
+         this.webdriver.waitLoad(6000);
 
-    return Jsoup.parse(this.webdriver.getCurrentPageSource());
-     } catch (Exception e) {
-        Logging.printLogDebug(logger, session, CommonMethods.getStackTrace(e));
-        return super.fetch();
-     }
-  }
+         return Jsoup.parse(this.webdriver.getCurrentPageSource());
+      } catch (Exception e) {
+         Logging.printLogDebug(logger, session, CommonMethods.getStackTrace(e));
+         return super.fetch();
+      }
+   }
 
-  @Override
-  public List<Product> extractInformation(Document doc) throws Exception {
-    super.extractInformation(doc);
-    List<Product> products = new ArrayList<>();
+   @Override
+   public List<Product> extractInformation(Document doc) throws Exception {
+      super.extractInformation(doc);
+      List<Product> products = new ArrayList<>();
 
-    CommonMethods.saveDataToAFile(doc, Test.pathWrite + "MARTINS.html");
-    
-    if (isProductPage(doc)) {
-      Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
+      if (isProductPage(doc)) {
+         Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
 
-      String internalId = CommonMethods.getLast(CrawlerUtils.scrapStringSimpleInfoByAttribute(doc, "input#id", "value").split("_"));
-      String name = CrawlerUtils.scrapStringSimpleInfo(doc, ".qdDetails .title", true);
-      Float price = CrawlerUtils.scrapFloatPriceFromHtml(doc, ".qdValue .value", null, true, ',', session);
-      Prices prices = scrapPrices(price);
-      CategoryCollection categories = CrawlerUtils.crawlCategories(doc, ".breadcrumb li:not(:first-child) > a", true);
-      String primaryImage = CrawlerUtils.scrapSimplePrimaryImage(doc, ".imagePrincipal img", Arrays.asList("src"), "https", "imgprd.martins.com.br");
-      String secondaryImages =
-          CrawlerUtils.scrapSimpleSecondaryImages(doc, ".galeryImages img", Arrays.asList("src"), "https", "imgprd.martins.com.br", primaryImage);
-      String description =
-          CrawlerUtils.scrapSimpleDescription(doc, Arrays.asList(".qdDetails .cods", ".details", "#especfication", ".body #details"));
-      List<String> eans = Arrays.asList(CrawlerUtils.scrapStringSimpleInfo(doc, ".cods .col-2 p", true));
+         String internalId = CommonMethods.getLast(CrawlerUtils.scrapStringSimpleInfoByAttribute(doc, "input#id", "value").split("_"));
+         String name = CrawlerUtils.scrapStringSimpleInfo(doc, ".qdDetails .title", true);
+         Float price = CrawlerUtils.scrapFloatPriceFromHtml(doc, ".qdValue .value", null, true, ',', session);
+         Prices prices = scrapPrices(price);
+         CategoryCollection categories = CrawlerUtils.crawlCategories(doc, ".breadcrumb li:not(:first-child) > a", true);
+         String primaryImage = CrawlerUtils.scrapSimplePrimaryImage(doc, ".imagePrincipal img", Arrays.asList("src"), "https", "imgprd.martins.com.br");
+         String secondaryImages =
+               CrawlerUtils.scrapSimpleSecondaryImages(doc, ".galeryImages img", Arrays.asList("src"), "https", "imgprd.martins.com.br", primaryImage);
+         String description =
+               CrawlerUtils.scrapSimpleDescription(doc, Arrays.asList(".qdDetails .cods", ".details", "#especfication", ".body #details"));
+         List<String> eans = Arrays.asList(CrawlerUtils.scrapStringSimpleInfo(doc, ".cods .col-2 p", true));
 
-      // Creating the product
-      Product product = ProductBuilder.create()
-          .setUrl(session.getOriginalURL())
-          .setInternalId(internalId)
-          .setName(name)
-          .setPrice(price)
-          .setPrices(prices)
-          .setAvailable(price != null)
-          .setCategory1(categories.getCategory(0))
-          .setCategory2(categories.getCategory(1))
-          .setCategory3(categories.getCategory(2))
-          .setPrimaryImage(primaryImage)
-          .setSecondaryImages(secondaryImages)
-          .setDescription(description)
-          .setMarketplace(new Marketplace())
-          .setEans(eans).build();
+         // Creating the product
+         Product product = ProductBuilder.create()
+               .setUrl(session.getOriginalURL())
+               .setInternalId(internalId)
+               .setName(name)
+               .setPrice(price)
+               .setPrices(prices)
+               .setAvailable(price != null)
+               .setCategory1(categories.getCategory(0))
+               .setCategory2(categories.getCategory(1))
+               .setCategory3(categories.getCategory(2))
+               .setPrimaryImage(primaryImage)
+               .setSecondaryImages(secondaryImages)
+               .setDescription(description)
+               .setMarketplace(new Marketplace())
+               .setEans(eans).build();
 
-      products.add(product);
+         products.add(product);
 
-    } else {
-      Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
-    }
+      } else {
+         Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
+      }
 
-    return products;
-  }
+      return products;
+   }
 
-  private boolean isProductPage(Document doc) {
-    return !doc.select("input#id").isEmpty();
-  }
+   private boolean isProductPage(Document doc) {
+      return !doc.select("input#id").isEmpty();
+   }
 
-  private Prices scrapPrices(Float price) {
-    Prices prices = new Prices();
+   private Prices scrapPrices(Float price) {
+      Prices prices = new Prices();
 
-    if (price != null) {
-      Map<Integer, Float> installment = new HashMap<>();
-      installment.put(1, price);
+      if (price != null) {
+         Map<Integer, Float> installment = new HashMap<>();
+         installment.put(1, price);
 
-      prices.setBankTicketPrice(price);
-      prices.insertCardInstallment(Card.VISA.toString(), installment);
-      prices.insertCardInstallment(Card.MASTERCARD.toString(), installment);
-    }
+         prices.setBankTicketPrice(price);
+         prices.insertCardInstallment(Card.VISA.toString(), installment);
+         prices.insertCardInstallment(Card.MASTERCARD.toString(), installment);
+      }
 
-    return prices;
-  }
+      return prices;
+   }
 }
