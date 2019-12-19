0d29428aa157617f1013dcab5a0190ed0a51b6a0
marcos <marcos@marcos-OptiPlex-7040>
2019-12-19 20:00:33 -0300

    BrasilBelatintas - Corecontent BrasilBelatintas - Keywords
    Former-commit-id: 0078920a09fb389669e6891807f0dfb5ffcc62b8

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilBelatintasCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilBelatintasCrawler.java
@@ -14,10 +14,10 @@ import br.com.lett.crawlernode.core.models.Product;
 import br.com.lett.crawlernode.core.models.ProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.Crawler;
+import br.com.lett.crawlernode.crawlers.ratingandreviews.extractionutils.TrustvoxRatingCrawler;
 import br.com.lett.crawlernode.util.CrawlerUtils;
 import br.com.lett.crawlernode.util.Logging;
 import br.com.lett.crawlernode.util.Pair;
-import models.AdvancedRatingReview;
 import models.Marketplace;
 import models.RatingsReviews;
 import models.prices.Prices;
@@ -28,31 +28,24 @@ public class BrasilBelatintasCrawler extends Crawler {
       super.config.setMustSendRatingToKinesis(true);
    }
 
-
-
    @Override
    public List<Product> extractInformation(Document doc) throws Exception {
       super.extractInformation(doc);
       List<Product> products = new ArrayList<>();
       if (isProductPage(doc)) {
          Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
-
-         String internalId = CrawlerUtils.scrapStringSimpleInfo(doc, ".produtoInfo-title .fbits-sku", false).replaceAll("[^0-9]", "").trim();
-         String internalPid = CrawlerUtils.scrapStringSimpleInfoByAttribute(doc, ".produto-comprar a", "id").replaceAll("[^0-9]", "").trim();
+         String internalPid = CrawlerUtils.scrapStringSimpleInfo(doc, ".produtoInfo-title .fbits-sku", false).replaceAll("[^0-9]", "").trim();
+         String internalId = CrawlerUtils.scrapStringSimpleInfoByAttribute(doc, ".produto-comprar a", "id").replaceAll("[^0-9]", "").trim();
          String name = CrawlerUtils.scrapStringSimpleInfo(doc, ".produtoInfo-title h1", false);
          Float price = CrawlerUtils.scrapFloatPriceFromHtml(doc, "#fbits-forma-pagamento #divFormaPagamento .precoPor", null, false, ',', session);
-         Float BankTicket = CrawlerUtils.scrapFloatPriceFromHtml(doc, "#fbits-forma-pagamento .precoVista .fbits-boleto-preco", null, false, ',', session);
-         Prices prices = crawlPrices(doc, price, BankTicket);
+         Prices prices = crawlPrices(doc, price);
          boolean available = !doc.select("meta[property*=\"product:availability\"]").isEmpty();
          CategoryCollection categories = CrawlerUtils.crawlCategories(doc, "#fbits-breadcrumb li", true);
          String primaryImage = CrawlerUtils.scrapSimplePrimaryImage(doc, ".fbits-componente-imagem #zoomImagemProduto", Arrays.asList("src"), "https",
                "www.supermercadodospets.com.br");
-         // String secondaryImages = CrawlerUtils.scrapSimpleSecondaryImages(doc,".product-essential
-         // .product-img-box img:not(:last-child)",
-         // Arrays.asList("src"), "https", "www.supermercadodospets.com.br", primaryImage);
-
+         String secondaryImages = CrawlerUtils.scrapSimpleSecondaryImages(doc, ".jcarousel #galeria li:not(:first-child) a img", Arrays.asList("src"), "https", "www.belatintas.com.br", primaryImage);
          String description = CrawlerUtils.scrapElementsDescription(doc, Arrays.asList(".infoProd .paddingbox"));
-         RatingsReviews ratingsReviews = scrapRatingReviews(doc);
+         RatingsReviews ratingsReviews = scrapRating(internalId, doc);
          // Creating the product
          Product product = ProductBuilder.create()
                .setUrl(session.getOriginalURL())
@@ -66,7 +59,7 @@ public class BrasilBelatintasCrawler extends Crawler {
                .setCategory2(categories.getCategory(1))
                .setCategory3(categories.getCategory(2))
                .setPrimaryImage(primaryImage)
-               // .setSecondaryImages(secondaryImages)
+               .setSecondaryImages(secondaryImages)
                .setDescription(description)
                .setMarketplace(new Marketplace())
                .setRatingReviews(ratingsReviews)
@@ -79,29 +72,25 @@ public class BrasilBelatintasCrawler extends Crawler {
       }
 
       return products;
-
    }
 
-
-   private Prices crawlPrices(Document doc, Float price, Float BankTicket) {
+   private Prices crawlPrices(Document doc, Float price) {
       Prices prices = new Prices();
 
       if (price != null) {
          Map<Integer, Float> installments = new HashMap<>();
          installments.put(1, price);
-         prices.setBankTicketPrice(BankTicket);
+         prices.setBankTicketPrice(CrawlerUtils.scrapFloatPriceFromHtml(doc, "#fbits-forma-pagamento .precoVista .fbits-boleto-preco", null, false, ',', session));
          prices.setPriceFrom(CrawlerUtils.scrapDoublePriceFromHtml(doc, "#fbits-forma-pagamento #divFormaPagamento .precoPor", null, true, ',', session));
 
-         // Integer pricess = CrawlerUtils.scrapSimpleInteger(doc, ".fbits-forma-pagamento .precoParcela
-         // .fbits-quantidadeParcelas", false);
-         // Float valorParcela = price / pricess;
-         // System.err.println(valorParcela);
+         Elements intallmentsElements = doc.select(".details-content p");
+         for (Element e : intallmentsElements) {
+            Pair<Integer, Float> pair = CrawlerUtils.crawlSimpleInstallment(null, e, false);
 
-         Pair<Integer, Float> pair = CrawlerUtils.crawlSimpleInstallment(".fbits-forma-pagamento .precoParcela .fbits-quantidadeParcelas", doc, true);
-         if (!pair.isAnyValueNull()) {
-            installments.put(pair.getFirst(), pair.getSecond());
+            if (!pair.isAnyValueNull()) {
+               installments.put(pair.getFirst(), pair.getSecond());
+            }
          }
-
 
          prices.insertCardInstallment(Card.VISA.toString(), installments);
          prices.insertCardInstallment(Card.MASTERCARD.toString(), installments);
@@ -114,85 +103,13 @@ public class BrasilBelatintasCrawler extends Crawler {
       return prices;
    }
 
-
-   private RatingsReviews scrapRatingReviews(Document doc) {
-      RatingsReviews ratingReviews = new RatingsReviews();
-      ratingReviews.setDate(session.getDate());
-
-      Integer totalComments = CrawlerUtils.scrapIntegerFromHtml(doc, ".product-essential .ratings .rating-links a", false, 0);
-      Double avgRating = scrapAvgRating(doc);
-      AdvancedRatingReview advancedRatingReview = scrapAdvancedRatingReview(doc);
-
-      ratingReviews.setTotalRating(totalComments);
-      ratingReviews.setTotalWrittenReviews(totalComments);
-      ratingReviews.setAverageOverallRating(avgRating);
-      ratingReviews.setAdvancedRatingReview(advancedRatingReview);
-
-      return ratingReviews;
-   }
-
-
-   private Double scrapAvgRating(Document doc) {
-      Double avg = 0d;
-
-      Double percentage = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".row .product-shop [itemprop=ratingValue]", null, false, ',', session);
-      if (percentage != null) {
-         avg = (percentage / 100) * 5;
-      }
-
-      return avg;
-   }
-
-   private AdvancedRatingReview scrapAdvancedRatingReview(Document doc) {
-      Integer star1 = 0;
-      Integer star2 = 0;
-      Integer star3 = 0;
-      Integer star4 = 0;
-      Integer star5 = 0;
-
-      Elements reviews = doc.select("#tab_review_tabbed_contents #product-customer-reviews .review-area .ratings-list .rating-item .rating-box .rating");
-
-      for (Element review : reviews) {
-         if (review != null && review.hasAttr("style")) {
-            // On a html this value will be like this: 100%
-            String percentageString = review.attr("style").replaceAll("[^0-9]+", ""); // "100" or ""
-
-            Integer val = !percentageString.isEmpty() ? Integer.parseInt(percentageString) : 0;
-
-            switch (val) {
-               case 20:
-                  star1 += 1;
-                  break;
-               case 40:
-                  star2 += 1;
-                  break;
-               case 60:
-                  star3 += 1;
-                  break;
-               case 80:
-                  star4 += 1;
-                  break;
-               case 100:
-                  star5 += 1;
-                  break;
-               default:
-                  break;
-            }
-         }
-      }
-
-      return new AdvancedRatingReview.Builder()
-            .totalStar1(star1)
-            .totalStar2(star2)
-            .totalStar3(star3)
-            .totalStar4(star4)
-            .totalStar5(star5)
-            .build();
+   private RatingsReviews scrapRating(String internalId, Document doc) {
+      TrustvoxRatingCrawler trustVox = new TrustvoxRatingCrawler(session, "78142", logger);
+      return trustVox.extractRatingAndReviews(internalId, doc, dataFetcher);
    }
 
    private boolean isProductPage(Document doc) {
       return !doc.select(".content.produto").isEmpty();
    }
-
 
 }
---
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilBelatintasCrawler.java
@@ -0,0 +1,78 @@
+package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
+
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.util.CommonMethods;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+
+public class BrasilBelatintasCrawler extends CrawlerRankingKeywords {
+
+
+   public BrasilBelatintasCrawler(Session session) {
+      super(session);
+   }
+
+   private static final String HOST = "www.belatintas.com.br";
+
+   @Override
+   protected void extractProductsFromCurrentPage() {
+
+      this.pageSize = 24;
+      this.log("Página " + this.currentPage);
+
+      String url = "https://" + HOST + "/busca?busca=" + this.keywordEncoded + "&pagina=" + this.currentPage;
+
+      this.log("Link onde são feitos os crawlers: " + url);
+      this.currentDoc = fetchDocument(url);
+      Elements products = this.currentDoc.select(".spot");
+
+      if (!products.isEmpty()) {
+         for (Element e : products) {
+
+
+            String internalId = scrapId(e);
+            String productUrl = CrawlerUtils.scrapUrl(e, ".spotContent > a", "href", "https", HOST);
+
+            saveDataProduct(null, internalId, productUrl);
+
+            this.log(
+                  "Position: " + this.position +
+                        " - InternalPid: " + internalId +
+                        " - Url: " + productUrl
+            );
+
+            if (this.arrayProducts.size() == productsLimit)
+               break;
+         }
+
+      } else {
+         this.result = false;
+         this.log("Keyword sem resultado!");
+      }
+
+      this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
+
+
+   }
+
+
+   private String scrapId(Element e) {
+      String pid = null;
+
+      String att = CrawlerUtils.scrapStringSimpleInfoByAttribute(e, ".spotContent a", "href");
+      if (att != null) {
+         pid = CommonMethods.getLast(att.split("-"));
+      }
+
+      return pid;
+   }
+
+   @Override
+   protected boolean hasNextPage() {
+      return !this.currentDoc.select("div #btnVerMais").isEmpty();
+   }
+
+
+}
