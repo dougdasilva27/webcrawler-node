6995a53a8255b74a8f7e38f3edcd20b6440304c2
Gabriel Dornelas <gabrieldornelas@lett.digital>
2019-12-27 16:53:31 -0300

    bugfix ranking carrefour [CSI-2173]
    Former-commit-id: cfc3172833e2c7b85e007954843ecba97fb470c6

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilCarrefourCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilCarrefourCrawler.java
@@ -1,13 +1,11 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import br.com.lett.crawlernode.core.fetcher.ProxyCollection;
-import br.com.lett.crawlernode.core.fetcher.methods.ApacheDataFetcher;
+import br.com.lett.crawlernode.core.fetcher.FetchMode;
 import br.com.lett.crawlernode.core.fetcher.models.Request;
 import br.com.lett.crawlernode.core.fetcher.models.Request.RequestBuilder;
 import br.com.lett.crawlernode.core.session.Session;
@@ -17,8 +15,11 @@ import br.com.lett.crawlernode.util.CrawlerUtils;
 
 public class BrasilCarrefourCrawler extends CrawlerRankingKeywords {
 
+   private static final String HOME_PAGE = "https://www.carrefour.com.br/";
+
    public BrasilCarrefourCrawler(Session session) {
       super(session);
+      super.fetchMode = FetchMode.APACHE;
    }
 
    @Override
@@ -132,32 +133,22 @@ public class BrasilCarrefourCrawler extends CrawlerRankingKeywords {
       return CrawlerUtils.sanitizeUrl(e, "href", "https:", "www.carrefour.com.br");
    }
 
-
    protected String fetchPage(String url) {
       Map<String, String> headers = new HashMap<>();
-      headers.put("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+      headers.put("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
       headers.put("accept-language", "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7,es;q=0.6");
-      headers.put("upgrade-insecure-requests", "1");
-      headers.put("referer", "https://www.carrefour.com.br/");
+      headers.put("sec-fetch-user", "?1");
+      headers.put("sec-fetch-site", "none");
+      headers.put("sec-fetch-mode", "navigate");
+      headers.put("referer", this.currentPage == 1 ? HOME_PAGE
+            : "https://www.carrefour.com.br/busca/?termo=" + this.keywordEncoded + "&foodzipzone=na&page=" + (this.currentPage - 1));
 
       Request request = RequestBuilder.create()
             .setUrl(url)
             .setCookies(cookies)
             .setHeaders(headers)
-            .setProxyservice(Arrays.asList(
-                  ProxyCollection.INFATICA_RESIDENTIAL_BR,
-                  ProxyCollection.STORM_RESIDENTIAL_EU,
-                  ProxyCollection.STORM_RESIDENTIAL_US,
-                  ProxyCollection.BONANZA
-            ))
             .build();
 
-
-      String response = this.dataFetcher.get(session, request).getBody();
-      if (response == null || !response.isEmpty()) {
-         response = new ApacheDataFetcher().get(session, request).getBody();
-      }
-
-      return response;
+      return this.dataFetcher.get(session, request).getBody();
    }
 }
