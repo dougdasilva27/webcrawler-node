1a34b9dbc27626d0f6867c9c221f173d3e30235a
Gabriel Dornelas <gabrieldornelas@lett.digital>
2020-01-03 11:11:18 -0300

    bugfix ranking carrefour [CSI-2173]
    Former-commit-id: 7655cc2896615e2c1112ba2a0d054575cdaed7be

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilCarrefourCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilCarrefourCrawler.java
@@ -10,7 +10,6 @@ import br.com.lett.crawlernode.core.fetcher.models.Request;
 import br.com.lett.crawlernode.core.fetcher.models.Request.RequestBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
-import br.com.lett.crawlernode.util.CommonMethods;
 import br.com.lett.crawlernode.util.CrawlerUtils;
 
 public class BrasilCarrefourCrawler extends CrawlerRankingKeywords {
@@ -24,36 +23,28 @@ public class BrasilCarrefourCrawler extends CrawlerRankingKeywords {
 
    @Override
    protected void extractProductsFromCurrentPage() {
-      // número de produtos por página do market
       this.pageSize = 20;
-
       this.log("Página " + this.currentPage);
 
-      // monta a url com a keyword e a página
-      String url = "https://www.carrefour.com.br/busca/?termo=" + this.keywordEncoded + "&foodzipzone=na&page=" + this.currentPage;
+      String url = "https://busca.carrefour.com.br/busca?q=ninho" + this.keywordEncoded + "&page=" + this.currentPage;
       this.log("Link onde são feitos os crawlers: " + url);
 
-      // chama função de pegar o html
       this.currentDoc = Jsoup.parse(fetchPage(url));
 
-      Elements products = this.currentDoc.select("li.product .prd-info a[title]");
+      Elements products = this.currentDoc.select("li.nm-product-item[data-id]");
 
-      // se obter 1 ou mais links de produtos e essa página tiver resultado faça:
       if (!products.isEmpty()) {
-         // se o total de busca não foi setado ainda, chama a função para setar
          if (this.totalProducts == 0) {
             setTotalProductsCarrefour();
          }
 
          for (Element e : products) {
+            String productUrl = CrawlerUtils.scrapUrl(e, ".nm-product-name a", "href", "https", "www.carrefour.com.br");
+            String internalId = e.attr("data-id");
 
-            String productUrl = crawlProductUrl(e);
-            String internalPid = crawlInternalPid(e);
-            String internalId = crawlInternalId(productUrl);
+            saveDataProduct(internalId, null, productUrl);
 
-            saveDataProduct(internalId, internalPid, productUrl);
-
-            this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + internalPid + " - Url: " + productUrl);
+            this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + null + " - Url: " + productUrl);
             if (this.arrayProducts.size() == productsLimit) {
                break;
             }
@@ -67,70 +58,9 @@ public class BrasilCarrefourCrawler extends CrawlerRankingKeywords {
       this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
    }
 
-   @Override
-   protected boolean hasNextPage() {
-      Element finalPage = this.currentDoc.select("#loadNextPage").first();
-
-      // if href on id loadNextPage is nor Empty there are more pages.
-      if (finalPage != null) {
-         String href = finalPage.attr("href").trim();
-
-         if (!href.isEmpty()) {
-            return true;
-         }
-      }
-
-      return false;
-   }
-
    protected void setTotalProductsCarrefour() {
-      Element totalElement = this.currentDoc.select(".result-count strong span").first();
-
-      if (totalElement != null) {
-         try {
-            this.totalProducts = Integer.parseInt(totalElement.text().replaceAll("[^0-9]", "").trim());
-         } catch (Exception e) {
-            this.logError(CommonMethods.getStackTrace(e));
-         }
-
-         this.log("Total da busca: " + this.totalProducts);
-      }
-   }
-
-   private String crawlInternalPid(Element e) {
-      String internalId = null;
-
-      Element id = e.select("input[name=productCodePost]").first();
-
-      if (id != null) {
-         internalId = id.val();
-      }
-
-      return internalId;
-   }
-
-   private String crawlInternalId(String url) {
-      String internalPid = null;
-
-      if (url.contains("?")) {
-         url = url.split("?")[0];
-      }
-
-      if (url.contains("/p/")) {
-         String[] tokens = url.split("/p/");
-
-         if (tokens.length > 1 && tokens[1].contains("/")) {
-            internalPid = tokens[1].split("/")[0];
-         } else if (tokens.length > 1) {
-            internalPid = tokens[1];
-         }
-      }
-
-      return internalPid;
-   }
-
-   private String crawlProductUrl(Element e) {
-      return CrawlerUtils.sanitizeUrl(e, "href", "https:", "www.carrefour.com.br");
+      this.totalProducts = CrawlerUtils.scrapIntegerFromHtml(this.currentDoc, ".neemu-total-products-container", true, 0);
+      this.log("Total: " + this.totalProducts);
    }
 
    protected String fetchPage(String url) {
