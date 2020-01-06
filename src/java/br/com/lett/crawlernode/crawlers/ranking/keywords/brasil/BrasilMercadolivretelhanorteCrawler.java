8859baa99e7284e13a552138876617e5938e7e09
marcos <marcos@marcos-OptiPlex-7040>
2020-01-06 17:50:53 -0300

    Telhanorte Crawler
    Corecontent
    Keywords
    
    Former-commit-id: 2fc5d25fc4c3d7d73ac98fab25ddded71f27167d

---
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilMercadolivretelhanorteCrawler.java
@@ -0,0 +1,17 @@
+package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
+
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.crawlers.ranking.keywords.extractionutils.MercadolivreCrawler;
+
+public class BrasilMercadolivretelhanorteCrawler extends MercadolivreCrawler {
+
+   private final String URL = "https://lista.mercadolivre.com.br/" + this.keywordWithoutAccents.replace(" ", "-") + "_Loja_telhanorte#D[A:"
+         + this.keywordWithoutAccents.replace(" ", "+") + ",telhanorte]";
+
+   public BrasilMercadolivretelhanorteCrawler(Session session) {
+      super(session);
+      super.setUrl(URL);
+      super.setProductUrlHost("produto.mercadolivre.com.br");
+      super.setNextUrlHost("lista.mercadolivre.com.br");
+   }
+}
