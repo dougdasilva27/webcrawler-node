82363b6e72cc472953b0553ba2350134a0d352d9
Gabriel Dornelas <gabrieldornelas@lett.digital>
2020-01-13 11:39:37 -0300

    trying to use fetcher on rappi exito col
    Former-commit-id: 67a67adb46aaa413602ade81c0cd121a4ce91108

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/colombia/ColombiaRappiexitobogotaCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/colombia/ColombiaRappiexitobogotaCrawler.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.jsoup.nodes.Document;
+import br.com.lett.crawlernode.core.fetcher.FetchMode;
 import br.com.lett.crawlernode.core.fetcher.models.Request;
 import br.com.lett.crawlernode.core.fetcher.models.Request.RequestBuilder;
 import br.com.lett.crawlernode.core.models.Card;
@@ -25,252 +26,253 @@ import models.prices.Prices;
 
 public class ColombiaRappiexitobogotaCrawler extends Crawler {
 
-  private static final String HOME_PAGE = "https://www.rappi.com.co/";
-  public static final String PRODUCTS_API_URL = "https://services.grability.rappi.com/api/search-client/search/v2/products";
-  public static final List<String> STORES = Arrays.asList("6660303");
+   private static final String HOME_PAGE = "https://www.rappi.com.co/";
+   public static final String PRODUCTS_API_URL = "https://services.grability.rappi.com/api/search-client/search/v2/products";
+   public static final List<String> STORES = Arrays.asList("6660303");
 
-  public ColombiaRappiexitobogotaCrawler(Session session) {
-    super(session);
-  }
+   public ColombiaRappiexitobogotaCrawler(Session session) {
+      super(session);
+      super.config.setFetcher(FetchMode.FETCHER);
+   }
 
-  @Override
-  public boolean shouldVisit() {
-    String href = this.session.getOriginalURL().toLowerCase();
-    return !FILTERS.matcher(href).matches() && (href.startsWith(HOME_PAGE));
-  }
+   @Override
+   public boolean shouldVisit() {
+      String href = this.session.getOriginalURL().toLowerCase();
+      return !FILTERS.matcher(href).matches() && (href.startsWith(HOME_PAGE));
+   }
 
-  @Override
-  protected Object fetch() {
-    return new Document("");
-  }
+   @Override
+   protected Object fetch() {
+      return new Document("");
+   }
 
-  @Override
-  public List<Product> extractInformation(Document doc) throws Exception {
-    super.extractInformation(doc);
-    List<Product> products = new ArrayList<>();
+   @Override
+   public List<Product> extractInformation(Document doc) throws Exception {
+      super.extractInformation(doc);
+      List<Product> products = new ArrayList<>();
 
-    String productUrl = session.getOriginalURL();
-    JSONObject jsonSku = crawlProductInformatioFromApi(productUrl);
+      String productUrl = session.getOriginalURL();
+      JSONObject jsonSku = crawlProductInformatioFromApi(productUrl);
 
-    if (isProductPage(jsonSku)) {
-      Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
+      if (isProductPage(jsonSku)) {
+         Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
 
-      String internalId = crawlInternalId(jsonSku);
-      String internalPid = crawlInternalPid(jsonSku);
-      CategoryCollection categories = crawlCategories(jsonSku);
-      String description = crawlDescription(jsonSku);
-      boolean available = crawlAvailability(jsonSku);
-      Float price = available ? CrawlerUtils.getFloatValueFromJSON(jsonSku, "price") : null;
-      Double priceFrom = available ? CrawlerUtils.getDoubleValueFromJSON(jsonSku, "real_price") : null;
-      String primaryImage = crawlPrimaryImage(jsonSku);
-      String name = crawlName(jsonSku);
-      Prices prices = crawlPrices(price, priceFrom);
+         String internalId = crawlInternalId(jsonSku);
+         String internalPid = crawlInternalPid(jsonSku);
+         CategoryCollection categories = crawlCategories(jsonSku);
+         String description = crawlDescription(jsonSku);
+         boolean available = crawlAvailability(jsonSku);
+         Float price = available ? CrawlerUtils.getFloatValueFromJSON(jsonSku, "price") : null;
+         Double priceFrom = available ? CrawlerUtils.getDoubleValueFromJSON(jsonSku, "real_price") : null;
+         String primaryImage = crawlPrimaryImage(jsonSku);
+         String name = crawlName(jsonSku);
+         Prices prices = crawlPrices(price, priceFrom);
 
-      // Creating the product
-      Product product = ProductBuilder.create()
-          .setUrl(productUrl)
-          .setInternalId(internalId)
-          .setInternalPid(internalPid)
-          .setName(name)
-          .setPrice(price)
-          .setPrices(prices)
-          .setAvailable(available)
-          .setCategory1(categories.getCategory(0))
-          .setCategory2(categories.getCategory(1))
-          .setCategory3(categories.getCategory(2))
-          .setPrimaryImage(primaryImage)
-          .setDescription(description)
-          .setMarketplace(new Marketplace()).build();
+         // Creating the product
+         Product product = ProductBuilder.create()
+               .setUrl(productUrl)
+               .setInternalId(internalId)
+               .setInternalPid(internalPid)
+               .setName(name)
+               .setPrice(price)
+               .setPrices(prices)
+               .setAvailable(available)
+               .setCategory1(categories.getCategory(0))
+               .setCategory2(categories.getCategory(1))
+               .setCategory3(categories.getCategory(2))
+               .setPrimaryImage(primaryImage)
+               .setDescription(description)
+               .setMarketplace(new Marketplace()).build();
 
-      products.add(product);
-
-
-    } else {
-      Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
-    }
-
-    return products;
-  }
-
-  /*******************************
-   * Product page identification *
-   *******************************/
-
-  private boolean isProductPage(JSONObject jsonSku) {
-    return jsonSku.has("id");
-  }
-
-  /*******************
-   * General methods *
-   *******************/
-
-  private String crawlInternalId(JSONObject json) {
-    String internalId = null;
-
-    if (json.has("id")) {
-      internalId = json.get("id").toString();
-    }
-
-    return internalId;
-  }
+         products.add(product);
 
 
-  private String crawlInternalPid(JSONObject json) {
-    String internalPid = null;
-
-    if (json.has("product_id")) {
-      internalPid = json.getString("product_id");
-    }
-
-    return internalPid;
-  }
-
-  private String crawlName(JSONObject json) {
-    String name = null;
-
-    if (json.has("name")) {
-      name = json.getString("name");
-    }
-
-    return name;
-  }
-
-  private boolean crawlAvailability(JSONObject json) {
-    return json.has("is_available") && json.getBoolean("is_available");
-  }
-
-  private String crawlPrimaryImage(JSONObject json) {
-    String primaryImage = null;
-
-    if (json.has("image")) {
-      primaryImage = "https://images.rappi.com/products/" + json.get("image");
-    }
-
-    return primaryImage;
-  }
-
-  private CategoryCollection crawlCategories(JSONObject json) {
-    CategoryCollection categories = new CategoryCollection();
-
-    if (json.has("categories")) {
-      JSONArray shelfList = json.getJSONArray("categories");
-
-      for (Object o : shelfList) {
-        JSONObject cat = (JSONObject) o;
-
-        if (cat.has("category_name")) {
-          categories.add(cat.getString("category_name"));
-        }
-      }
-    }
-
-    return categories;
-  }
-
-
-  private String crawlDescription(JSONObject json) {
-    StringBuilder description = new StringBuilder();
-
-    if (json.has("description") && json.get("description") instanceof String) {
-      String desc = json.getString("description");
-
-      // Because of this link: https://www.rappi.com.co/search?store_type=hiper&query=900187
-      if (desc.replace(" ", "").contains("-PLU")) {
-        String descFinal = desc.replace(CommonMethods.getLast(desc.split("-")), "").trim();
-        description.append(descFinal.substring(0, descFinal.length() - 2).trim());
       } else {
-        description.append(desc);
+         Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
       }
-    }
 
+      return products;
+   }
 
-    return description.toString();
-  }
+   /*******************************
+    * Product page identification *
+    *******************************/
 
-  /**
-   * In this site has no information of installments
-   * 
-   * @param price
-   * @return
-   */
-  private Prices crawlPrices(Float price, Double priceFrom) {
-    Prices p = new Prices();
+   private boolean isProductPage(JSONObject jsonSku) {
+      return jsonSku.has("id");
+   }
 
-    if (price != null) {
-      Map<Integer, Float> installmentPriceMap = new HashMap<>();
-      installmentPriceMap.put(1, price);
+   /*******************
+    * General methods *
+    *******************/
 
-      p.setPriceFrom(priceFrom);
-      p.setBankTicketPrice(price);
-      
-      List<Card> cards = Arrays.asList(Card.VISA, Card.MASTERCARD, Card.DINERS, Card.AMEX, Card.ELO, Card.SHOP_CARD);
-      for(Card card : cards) {
-        p.insertCardInstallment(card.toString(), installmentPriceMap);
+   private String crawlInternalId(JSONObject json) {
+      String internalId = null;
+
+      if (json.has("id")) {
+         internalId = json.get("id").toString();
       }
-    }
 
-    return p;
-  }
+      return internalId;
+   }
 
 
-  /**
-   * - Get the json of api, this api has all info of product - Spected url like this
-   * https://www.rappi.com.br/search?store_type=market&query=2089952206
-   * 
-   * @return
-   */
-  private JSONObject crawlProductInformatioFromApi(String productUrl) {
-    JSONObject productsInfo = new JSONObject();
+   private String crawlInternalPid(JSONObject json) {
+      String internalPid = null;
 
-    String storeType = "hiper";
-    String productId = null;
-
-    if (productUrl.contains("?")) {
-      String[] parameters = (productUrl.split("\\?")[1]).split("&");
-
-      for (String parameter : parameters) {
-        if (parameter.contains("store_type=")) {
-          storeType = parameter.split("=")[1];
-
-        } else if (parameter.contains("query=")) {
-          productId = parameter.split("=")[1];
-        }
+      if (json.has("product_id")) {
+         internalPid = json.getString("product_id");
       }
-    } else if (productUrl.contains("_")) {
-      productId = CommonMethods.getLast(productUrl.split("_"));
-    }
 
-    if (productId != null && storeType != null) {
+      return internalPid;
+   }
 
-      String payload = "{\"query\":\"" + productId + "\",\"stores\":" + STORES.toString() + ",\"store_type\":\"" + storeType + "\",\"page\":1"
-          + ",\"size\":40,\"options\":{},\"helpers\":{\"home_type\":\"by_categories\",\"store_type_group\":\"market\",\"type\":\"by_categories\"}}";
+   private String crawlName(JSONObject json) {
+      String name = null;
 
-      Map<String, String> headers = new HashMap<>();
-      headers.put("Content-Type", "application/json");
+      if (json.has("name")) {
+         name = json.getString("name");
+      }
 
-      Request request = RequestBuilder.create().setUrl(PRODUCTS_API_URL + "?page=1").setCookies(cookies).setHeaders(headers).setPayload(payload)
-          .mustSendContentEncoding(false).build();
-      String page = this.dataFetcher.post(session, request).getBody().trim();
-      
-      page = Normalizer.normalize(page, Normalizer.Form.NFD);
+      return name;
+   }
 
-      if (page.startsWith("{") && page.endsWith("}")) {
-        try {
-          JSONObject apiResponse = new JSONObject(page);
+   private boolean crawlAvailability(JSONObject json) {
+      return json.has("is_available") && json.getBoolean("is_available");
+   }
 
-          if (apiResponse.has("hits") && apiResponse.get("hits") instanceof JSONArray) {
-            JSONArray hits = apiResponse.getJSONArray("hits");
+   private String crawlPrimaryImage(JSONObject json) {
+      String primaryImage = null;
 
-            if (hits.length() > 0) {
-              productsInfo = hits.getJSONObject(0);
+      if (json.has("image")) {
+         primaryImage = "https://images.rappi.com/products/" + json.get("image");
+      }
+
+      return primaryImage;
+   }
+
+   private CategoryCollection crawlCategories(JSONObject json) {
+      CategoryCollection categories = new CategoryCollection();
+
+      if (json.has("categories")) {
+         JSONArray shelfList = json.getJSONArray("categories");
+
+         for (Object o : shelfList) {
+            JSONObject cat = (JSONObject) o;
+
+            if (cat.has("category_name")) {
+               categories.add(cat.getString("category_name"));
             }
-          }
-        } catch (Exception e) {
-          Logging.printLogWarn(logger, session, CommonMethods.getStackTrace(e));
-        }
+         }
       }
-    }
 
-    return productsInfo;
-  }
+      return categories;
+   }
+
+
+   private String crawlDescription(JSONObject json) {
+      StringBuilder description = new StringBuilder();
+
+      if (json.has("description") && json.get("description") instanceof String) {
+         String desc = json.getString("description");
+
+         // Because of this link: https://www.rappi.com.co/search?store_type=hiper&query=900187
+         if (desc.replace(" ", "").contains("-PLU")) {
+            String descFinal = desc.replace(CommonMethods.getLast(desc.split("-")), "").trim();
+            description.append(descFinal.substring(0, descFinal.length() - 2).trim());
+         } else {
+            description.append(desc);
+         }
+      }
+
+
+      return description.toString();
+   }
+
+   /**
+    * In this site has no information of installments
+    * 
+    * @param price
+    * @return
+    */
+   private Prices crawlPrices(Float price, Double priceFrom) {
+      Prices p = new Prices();
+
+      if (price != null) {
+         Map<Integer, Float> installmentPriceMap = new HashMap<>();
+         installmentPriceMap.put(1, price);
+
+         p.setPriceFrom(priceFrom);
+         p.setBankTicketPrice(price);
+
+         List<Card> cards = Arrays.asList(Card.VISA, Card.MASTERCARD, Card.DINERS, Card.AMEX, Card.ELO, Card.SHOP_CARD);
+         for (Card card : cards) {
+            p.insertCardInstallment(card.toString(), installmentPriceMap);
+         }
+      }
+
+      return p;
+   }
+
+
+   /**
+    * - Get the json of api, this api has all info of product - Spected url like this
+    * https://www.rappi.com.br/search?store_type=market&query=2089952206
+    * 
+    * @return
+    */
+   private JSONObject crawlProductInformatioFromApi(String productUrl) {
+      JSONObject productsInfo = new JSONObject();
+
+      String storeType = "hiper";
+      String productId = null;
+
+      if (productUrl.contains("?")) {
+         String[] parameters = (productUrl.split("\\?")[1]).split("&");
+
+         for (String parameter : parameters) {
+            if (parameter.contains("store_type=")) {
+               storeType = parameter.split("=")[1];
+
+            } else if (parameter.contains("query=")) {
+               productId = parameter.split("=")[1];
+            }
+         }
+      } else if (productUrl.contains("_")) {
+         productId = CommonMethods.getLast(productUrl.split("_"));
+      }
+
+      if (productId != null && storeType != null) {
+
+         String payload = "{\"query\":\"" + productId + "\",\"stores\":" + STORES.toString() + ",\"store_type\":\"" + storeType + "\",\"page\":1"
+               + ",\"size\":40,\"options\":{},\"helpers\":{\"home_type\":\"by_categories\",\"store_type_group\":\"market\",\"type\":\"by_categories\"}}";
+
+         Map<String, String> headers = new HashMap<>();
+         headers.put("Content-Type", "application/json");
+
+         Request request = RequestBuilder.create().setUrl(PRODUCTS_API_URL + "?page=1").setCookies(cookies).setHeaders(headers).setPayload(payload)
+               .mustSendContentEncoding(false).build();
+         String page = this.dataFetcher.post(session, request).getBody().trim();
+
+         page = Normalizer.normalize(page, Normalizer.Form.NFD);
+
+         if (page.startsWith("{") && page.endsWith("}")) {
+            try {
+               JSONObject apiResponse = new JSONObject(page);
+
+               if (apiResponse.has("hits") && apiResponse.get("hits") instanceof JSONArray) {
+                  JSONArray hits = apiResponse.getJSONArray("hits");
+
+                  if (hits.length() > 0) {
+                     productsInfo = hits.getJSONObject(0);
+                  }
+               }
+            } catch (Exception e) {
+               Logging.printLogWarn(logger, session, CommonMethods.getStackTrace(e));
+            }
+         }
+      }
+
+      return productsInfo;
+   }
 }
