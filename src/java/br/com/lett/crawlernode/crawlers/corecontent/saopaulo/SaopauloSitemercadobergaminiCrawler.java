d90c686d440eacbe76452a66286e6c6509aa7914
Charles Fonseca <charlesjrfonseca@gmail.com>
2020-02-05 16:24:59 -0300

    Site mercado consertado, assim como suas subclasses, cada loja/seller possui um identificador, no HTML não possui, portanto é necessário inserir para cada loja.

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/saopaulo/SaopauloSitemercadobergaminiCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/saopaulo/SaopauloSitemercadobergaminiCrawler.java
@@ -3,8 +3,10 @@ package br.com.lett.crawlernode.crawlers.corecontent.saopaulo;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.crawlers.corecontent.extractionutils.BrasilSitemercadoCrawler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * 
  * @author gabriel date: 2019-09-24
  */
 public class SaopauloSitemercadobergaminiCrawler extends BrasilSitemercadoCrawler {
@@ -14,9 +16,12 @@ public class SaopauloSitemercadobergaminiCrawler extends BrasilSitemercadoCrawle
   }
 
   public static final String HOME_PAGE =
-      "https://www.sitemercado.com.br/bergamini/sao-paulo-hiper-vila-constanca-avenida-luis-stamatis/";
+          "https://www.sitemercado.com.br/bergamini/sao-paulo-hiper-vila-constanca-avenida-luis-stamatis/";
   public static final String LOAD_PAYLOAD =
-      "{\"lojaUrl\":\"sao-paulo-hiper-vila-constanca-avenida-luis-stamatis\",\"redeUrl\":\"bergamini\"}";
+          "{\"lojaUrl\":\"sao-paulo-hiper-vila-constanca-avenida-luis-stamatis\",\"redeUrl\":\"bergamini\"}";
+
+  public static final int IDLOJA = 471;
+  public static final int IDREDE = 414;
 
   @Override
   protected String getHomePage() {
@@ -28,4 +33,11 @@ public class SaopauloSitemercadobergaminiCrawler extends BrasilSitemercadoCrawle
     return LOAD_PAYLOAD;
   }
 
+  @Override
+  protected Map<String, Integer> getLojaInfo() {
+    Map<String, Integer> lojaInfo = new HashMap<>();
+    lojaInfo.put("IdLoja", IDLOJA);
+    lojaInfo.put("IdRede", IDREDE);
+    return lojaInfo;
+  }
 }
