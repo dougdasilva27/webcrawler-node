75c1cabcb1233afaa7f5ca834b82e43193617e54
marcos <marcos@marcos-OptiPlex-7040>
2020-02-07 13:14:06 -0300

    Carrefour bug

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilCarrefourCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilCarrefourCrawler.java
@@ -150,11 +150,9 @@ public class BrasilCarrefourCrawler extends Crawler {
          }
 
          Marketplace marketplace = assembleMarketplaceFromMap(marketplaceMap, marketplaceMap.size() > 1 ? null : priceFrom);
-
-         boolean available = crawlAvailability(doc);
+         boolean available = marketplaceMap.containsKey(SELLER_NAME_LOWER);
          Prices prices = available ? marketplaceMap.get(SELLER_NAME_LOWER) : new Prices();
-         Float price = CrawlerUtils.scrapFloatPriceFromHtml(doc, ".priceBig", null, false, ',', session);
-
+         Float price = available ? CrawlerUtils.scrapFloatPriceFromHtml(doc, ".priceBig", null, false, ',', session) : null;
          RatingsReviews rating = crawlRatingReviews(doc);
 
          Product product = ProductBuilder.create()
@@ -239,10 +237,6 @@ public class BrasilCarrefourCrawler extends Crawler {
       }
 
       return name;
-   }
-
-   private boolean crawlAvailability(Document doc) {
-      return doc.selectFirst("#buyProductButtonBottom") != null;
    }
 
    // private Integer scrapStock(String internalId, String internalPid, Document doc) {
