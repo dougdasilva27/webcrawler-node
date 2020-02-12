99244949d6508b6b27ea0f79067f5578fe7b2956
Charles Fonseca <charlesjrfonseca@gmail.com>
2020-02-12 15:11:30 -0300

    Rating adicionado ao The Beauty Box.

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilThebeautyboxCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilThebeautyboxCrawler.java
@@ -1,24 +1,24 @@
 package br.com.lett.crawlernode.crawlers.corecontent.brasil;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import br.com.lett.crawlernode.core.models.Card;
 import br.com.lett.crawlernode.core.models.CategoryCollection;
 import br.com.lett.crawlernode.core.models.Product;
 import br.com.lett.crawlernode.core.models.ProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.Crawler;
+import br.com.lett.crawlernode.crawlers.corecontent.extractionutils.YourreviewsRatingCrawler;
 import br.com.lett.crawlernode.util.CrawlerUtils;
 import br.com.lett.crawlernode.util.Logging;
+import models.AdvancedRatingReview;
 import models.Marketplace;
+import models.RatingsReviews;
 import models.prices.Prices;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.util.*;
 
 public class BrasilThebeautyboxCrawler extends Crawler {
 
@@ -49,6 +49,7 @@ public class BrasilThebeautyboxCrawler extends Crawler {
       String internalPid = productJson.has("productId") && !productJson.isNull("productId") ? productJson.get("productId").toString() : null;
       CategoryCollection categories = CrawlerUtils.crawlCategories(doc, "a[class^=vtex-breadcrumb]:not([href=\"/\"])");
       String description = scrapDescription(productJson);
+      RatingsReviews ratingsReviews = scrapRating(internalPid);
 
       JSONArray items = productJson.has("items") && !productJson.isNull("items") ? productJson.getJSONArray("items") : new JSONArray();
 
@@ -68,27 +69,30 @@ public class BrasilThebeautyboxCrawler extends Crawler {
 
         Prices prices = CrawlerUtils.getPrices(marketplaceMap, mainSellers);
         Float price = CrawlerUtils.extractPriceFromPrices(prices, Card.VISA);
-
+        ratingsReviews.setInternalId(internalId);
+        ratingsReviews.setDate(session.getDate());
+        ratingsReviews.setUrl(session.getOriginalURL());
         List<String> eans = jsonSku.has("ean") ? Arrays.asList(jsonSku.get("ean").toString()) : null;
 
         // Creating the product
         Product product = ProductBuilder.create()
-            .setUrl(session.getOriginalURL())
-            .setInternalId(internalId)
-            .setInternalPid(internalPid)
-            .setName(name)
-            .setPrice(price)
-            .setPrices(prices)
-            .setAvailable(available)
-            .setCategory1(categories.getCategory(0))
-            .setCategory2(categories.getCategory(1))
-            .setCategory3(categories.getCategory(2))
-            .setPrimaryImage(primaryImage)
-            .setSecondaryImages(secondaryImages)
-            .setDescription(description)
-            .setMarketplace(marketplace)
-            .setEans(eans)
-            .build();
+                .setUrl(session.getOriginalURL())
+                .setInternalId(internalId)
+                .setInternalPid(internalPid)
+                .setName(name)
+                .setRatingReviews(ratingsReviews.clone())
+                .setPrice(price)
+                .setPrices(prices)
+                .setAvailable(available)
+                .setCategory1(categories.getCategory(0))
+                .setCategory2(categories.getCategory(1))
+                .setCategory3(categories.getCategory(2))
+                .setPrimaryImage(primaryImage)
+                .setSecondaryImages(secondaryImages)
+                .setDescription(description)
+                .setMarketplace(marketplace)
+                .setEans(eans)
+                .build();
 
         products.add(product);
       }
@@ -118,7 +122,6 @@ public class BrasilThebeautyboxCrawler extends Crawler {
    * 
    * We need to build this key with url path
    * 
-   * @param doc
    * @param url
    * @return
    */
@@ -296,5 +299,33 @@ public class BrasilThebeautyboxCrawler extends Crawler {
     }
 
     return prices;
+  }
+
+  private RatingsReviews scrapRating(String internalPid) {
+    RatingsReviews ratingReviews = new RatingsReviews();
+    YourreviewsRatingCrawler yourReviews = new YourreviewsRatingCrawler(session, cookies, logger, "31087896-d490-4f03-b64b-5948e3fb52e5", dataFetcher);
+
+    Document docRating = yourReviews.crawlPageRatingsFromYourViews(internalPid, "31087896-d490-4f03-b64b-5948e3fb52e5", this.dataFetcher);
+    Integer totalNumOfEvaluations = CrawlerUtils.scrapIntegerFromHtml(docRating, "strong[itemprop=ratingCount]", false, 0);
+    Double avgRating = getTotalAvgRatingFromYourViews(docRating);
+    AdvancedRatingReview advancedRatingReview = yourReviews.getTotalStarsFromEachValue(internalPid);
+
+    ratingReviews.setAdvancedRatingReview(advancedRatingReview);
+    ratingReviews.setTotalRating(totalNumOfEvaluations);
+    ratingReviews.setAverageOverallRating(avgRating);
+    ratingReviews.setTotalWrittenReviews(totalNumOfEvaluations);
+
+    return ratingReviews;
+  }
+
+  private Double getTotalAvgRatingFromYourViews(Document docRating) {
+    Double avgRating = 0D;
+    Double ratingOnHtml = CrawlerUtils.scrapDoublePriceFromHtml(docRating, "meta[itemprop=ratingValue]", "content", false, '.', session);
+
+    if (ratingOnHtml != null) {
+      avgRating = ratingOnHtml;
+    }
+
+    return avgRating;
   }
 }
