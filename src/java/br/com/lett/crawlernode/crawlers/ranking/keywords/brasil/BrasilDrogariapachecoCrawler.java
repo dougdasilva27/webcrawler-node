9418d602d6b84d29cb4d58dfc10f8b92c0fced86
marcos <marcos@marcos-OptiPlex-7040>
2020-02-12 14:38:51 -0300

    DrogariapachecoCrawler
    The website haves changed. Now we have to acess an Api to get all the
    info thet we need.

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilDrogariapachecoCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilDrogariapachecoCrawler.java
@@ -1,91 +1,88 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
 
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import java.util.HashMap;
+import java.util.Map;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import br.com.lett.crawlernode.core.fetcher.models.Request;
+import br.com.lett.crawlernode.core.fetcher.models.Request.RequestBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.util.JSONUtils;
 
 public class BrasilDrogariapachecoCrawler extends CrawlerRankingKeywords {
 
-  public BrasilDrogariapachecoCrawler(Session session) {
-    super(session);
-  }
+   public BrasilDrogariapachecoCrawler(Session session) {
+      super(session);
+   }
+
+   private JSONArray extractJsonFromApi() {
+
+      String url = "https://www.drogariaspacheco.com.br/api/catalog_system/pub/products/search/?ft=" + this.keywordWithoutAccents + "&_from=0&_to=8&O=OrderByReleaseDateDESC";
+
+      Map<String, String> headers = new HashMap<>();
+      headers.put("user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36");
+      headers.put("Accept", "*/*");
+
+      Request request = RequestBuilder.create().setUrl(url).setCookies(cookies).build();
+      String body = this.dataFetcher.get(session, request).getBody();
+
+      JSONArray stringToJson = JSONUtils.stringToJsonArray(body);
+
+      return stringToJson;
+
+   }
+
+   @Override
+   protected void extractProductsFromCurrentPage() {
+      this.pageSize = 18;
+
+      this.log("Página" + this.currentPage);
+
+      JSONArray products = extractJsonFromApi();
+
+      if (products.length() > 0) {
+         for (int i = 0; i < products.length(); i++) {
+            JSONObject product = products.getJSONObject(i);
+
+            String productUrl = crawlProductUrl(product);
+            String internalPid = crawlInternalPid(product);
+
+            saveDataProduct(null, internalPid, productUrl);
+
+            this.log("Position: " + this.position + " - InternalId: " + null + " - InternalPid: " + internalPid + " - Url: " + productUrl);
 
 
-  @Override
-  protected void extractProductsFromCurrentPage() {
-    // número de produtos por página do market
-    this.pageSize = 15;
-
-    this.log("Página " + this.currentPage);
-    String url = "https://www.drogariaspacheco.com.br/" + this.keywordWithoutAccents.replaceAll(" ", "%20") + "?PS=50&PageNumber=" + this.currentPage;
-    this.log("Link onde são feitos os crawlers: " + url);
-
-    this.currentDoc = fetchDocument(url);
-
-    Elements products = this.currentDoc.select(".vitrine.resultItemsWrapper ul > li[layout]");
-
-    if (!products.isEmpty()) {
-      if (this.totalProducts == 0) {
-        setTotalProducts();
+            if (this.arrayProducts.size() == productsLimit) {
+               break;
+            }
+         }
+      } else {
+         this.result = false;
+         this.log("Keyword sem resultado!");
       }
 
-      for (Element e : products) {
-        String internalPid = crawlInternalPid(e);
-        String productUrl = crawlProductUrl(e);
+      this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
 
-        saveDataProduct(null, internalPid, productUrl);
+   }
 
-        this.log("Position: " + this.position + " - InternalId: " + null + " - InternalPid: " + internalPid + " - Url: " + productUrl);
+   private String crawlInternalPid(JSONObject product) {
+      String internalPid = null;
 
-        if (this.arrayProducts.size() == productsLimit) {
-          break;
-        }
+      if (product.has("productId")) {
+         internalPid = product.getString("productId");
       }
-    } else {
-      this.result = false;
-      this.log("Keyword sem resultado!");
-    }
 
-    this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
+      return internalPid;
+   }
 
-  }
+   private String crawlProductUrl(JSONObject product) {
+      String urlProduct = null;
 
-  @Override
-  protected void setTotalProducts() {
-    Element totalElement = this.currentDoc.select("span.resultado-busca-numero span.value").first();
-
-    if (totalElement != null) {
-      String text = totalElement.ownText().replaceAll("[^0-9]", "").trim();
-
-      if (!text.isEmpty()) {
-        this.totalProducts = Integer.parseInt(text);
+      if (product.has("link")) {
+         urlProduct = product.getString("link");
       }
-    }
 
-    this.log("Total da busca: " + this.totalProducts);
-  }
-
-  private String crawlInternalPid(Element e) {
-    String internalPid = null;
-
-    Element ids = e.select(".idSku").first();
-
-    if (ids != null) {
-      internalPid = ids.text().trim();
-    }
-
-    return internalPid;
-  }
-
-  private String crawlProductUrl(Element e) {
-    String urlProduct = null;
-    Element urlElement = e.selectFirst("a.productPrateleira");
-
-    if (urlElement != null) {
-      urlProduct = urlElement.attr("href");
-    }
-
-    return urlProduct;
-  }
+      return urlProduct;
+   }
 }
