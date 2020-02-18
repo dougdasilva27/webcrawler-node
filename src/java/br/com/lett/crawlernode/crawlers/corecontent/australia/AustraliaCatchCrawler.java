95fb003c089ec72090c5ac3b109210d476841413
Charles Fonseca <charlesjrfonseca@gmail.com>
2020-02-18 11:04:20 -0300

    Rafatorações Catch.

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/australia/AustraliaCatchCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/australia/AustraliaCatchCrawler.java
@@ -39,10 +39,9 @@ public class AustraliaCatchCrawler extends Crawler {
             boolean available = crawlAvailability(doc);
 
             String name = CrawlerUtils.scrapStringSimpleInfo(doc, ".product-container.container.grid-row h1", false);
-            String internalId = crawlInternalId(doc);
-
             Float price = CrawlerUtils.scrapFloatPriceFromHtml(doc, ".top-info__price .product--price", "content", false, '.', session);
             List<String> categories = scrapCategories(doc);
+
             RatingsReviews ratingsReviews = scrapRating(doc, internalPid);
             Prices prices = crawlPrices(price, doc);
             String description = crawlDescription(doc);
@@ -52,8 +51,10 @@ public class AustraliaCatchCrawler extends Crawler {
             images.remove(0);
 
             Elements variation = doc.select(".attribute-dropdown__option");
-            Map<String, String> skus = scrapSkuVariations(variation, internalId);
+            Map<String, String> skus = scrapSkuVariations(variation, internalPid);
+
             for (Map.Entry<String, String> sku : skus.entrySet()) {
+
                 RatingsReviews reviewsClone = ratingsReviews.clone();
                 reviewsClone.setInternalId(sku.getKey());
                 reviewsClone.setDate(session.getDate());
@@ -72,18 +73,28 @@ public class AustraliaCatchCrawler extends Crawler {
                         .setDescription(description)
                         .build());
             }
+
         } else {
             Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
         }
-
         return products;
-
     }
 
     private RatingsReviews scrapRating(Document doc, String internalPid) {
         RatingsReviews ratingsReviews = new RatingsReviews();
         Double avgRating = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".hidden > span[itemprop=ratingValue]", null, false, '.', session);
         Integer totalReview = CrawlerUtils.scrapIntegerFromHtml(doc, ".hidden > span[itemprop=reviewCount]", false, 0);
+
+        AdvancedRatingReview advancedRatingReview = scrapAdvancedRatingReview(internalPid);
+
+        ratingsReviews.setAdvancedRatingReview(advancedRatingReview);
+        ratingsReviews.setTotalRating(totalReview);
+        ratingsReviews.setAverageOverallRating(avgRating);
+        ratingsReviews.setTotalWrittenReviews(totalReview);
+        return ratingsReviews;
+    }
+
+    private AdvancedRatingReview scrapAdvancedRatingReview(String internalPid) {
         Map<String, String> headers = new HashMap<>();
         headers.put(HttpHeaders.USER_AGENT, "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.100 Safari/537.36");
         String homePage = "https://www.catch.com.au/";
@@ -92,11 +103,14 @@ public class AustraliaCatchCrawler extends Crawler {
                 .setCookies(cookies)
                 .setHeaders(headers)
                 .build();
+
         String response = dataFetcher.get(session, request).getBody();
         Document documentRating = Jsoup.parse(response);
         Elements starsElem = documentRating.select("meta[itemprop=ratingValue]");
+
         AdvancedRatingReview advancedRatingReview = new AdvancedRatingReview();
         int star1 = 0, star2 = 0, star3 = 0, star4 = 0, star5 = 0;
+
         for (Element element : starsElem) {
             Integer star = MathUtils.parseInt(element.attr("content"));
             switch (star) {
@@ -122,40 +136,37 @@ public class AustraliaCatchCrawler extends Crawler {
         advancedRatingReview.setTotalStar3(star3);
         advancedRatingReview.setTotalStar4(star4);
         advancedRatingReview.setTotalStar5(star5);
-        ratingsReviews.setAdvancedRatingReview(advancedRatingReview);
-        ratingsReviews.setTotalRating(totalReview);
-        ratingsReviews.setAverageOverallRating(avgRating);
-        ratingsReviews.setTotalWrittenReviews(totalReview);
-        return ratingsReviews;
+        return advancedRatingReview;
     }
 
     private Map<String, String> scrapSkuVariations(Elements doc, String internalId) {
         Map<String, String> skus = new HashMap<>();
         skus.put(internalId, "");
-        for (Element element : doc.select(".attribute-dropdown__option")) {
-            Element sufixoElem = element.selectFirst(".attribute-name-label");
-            if (sufixoElem != null)
-                if (sufixoElem.text() != null && element.select(".attribute-description").isEmpty()) {
-                    skus.put(CrawlerUtils.scrapStringSimpleInfoByAttribute(element, ".notify-me.notify-me-btn", "data-product-sku-id"),
-                            sufixoElem.text());
-                }
 
+        for (Element element : doc.select(".attribute-dropdown__option")) {
+
+            Element sufixoElem = element.selectFirst(".attribute-name-label");
+            if (sufixoElem != null && sufixoElem.text() != null && element.select(".attribute-description").isEmpty())
+                skus.put(CrawlerUtils.scrapStringSimpleInfoByAttribute(element, ".notify-me.notify-me-btn", "data-product-sku-id"),
+                        sufixoElem.text());
         }
         return skus;
     }
 
     private List<String> scrapCategories(Document doc) {
         List<String> images = new LinkedList<>();
-        for (Element elem : doc.select(".breadcrumb > span > a"))
+        for (Element elem : doc.select(".breadcrumb > span > a")) {
             images.add(elem.ownText());
+        }
         images.removeIf("Home"::equals);
         return images;
     }
 
     private JSONArray scrapImages(Document doc) {
         JSONArray imagesArray = new JSONArray();
-        for (Element elem : doc.select(".product-thumbnails__link"))
+        for (Element elem : doc.select(".product-thumbnails__link")) {
             imagesArray.put(elem.attr("data-large-img"));
+        }
         return imagesArray;
     }
 
@@ -171,26 +182,13 @@ public class AustraliaCatchCrawler extends Crawler {
         return CrawlerUtils.scrapStringSimpleInfoByAttribute(doc, ".padded-content.buy-box.bordered-content", "data-product-id");
     }
 
-    private String crawlInternalId(Element doc) {
-        String internalId = null;
-        Elements scripts = doc.select("script");
-        for (Element elem : scripts) {
-            String script = elem.toString().replace(" ", "");
-            if (script.contains("sku_id")) {
-                String skuIdSubString = script.substring(script.indexOf("sku_id"));
-                internalId = Objects.requireNonNull(MathUtils.parseInt(skuIdSubString.substring(0, skuIdSubString.indexOf(",")))).toString();
-            }
-        }
-
-        return internalId;
-    }
-
     private boolean crawlAvailability(Element doc) {
         return !doc.select(".btn.primary.upper.large.js-buy-now-btn").isEmpty();
     }
 
     private Prices crawlPrices(Float price, Element doc) {
         Prices prices = new Prices();
+
         prices.setPriceFrom(CrawlerUtils.scrapDoublePriceFromHtml(doc, ".price--strike", null, false, '.', session));
         prices.setBankTicketPrice(price);
         return prices;
