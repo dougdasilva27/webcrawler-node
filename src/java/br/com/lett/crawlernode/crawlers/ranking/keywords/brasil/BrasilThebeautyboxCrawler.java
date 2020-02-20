adece6739a37b680fcc8b87aa45d35efba8856c6
Gabriel Dornelas <gabrieldornelas@lett.digital>
2020-02-20 18:22:27 -0300

    bugfix beautybox

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilThebeautyboxCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilThebeautyboxCrawler.java
@@ -8,6 +8,10 @@ import java.util.Map;
 import org.apache.http.HttpHeaders;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import br.com.lett.crawlernode.core.fetcher.FetchMode;
 import br.com.lett.crawlernode.core.fetcher.models.Request;
 import br.com.lett.crawlernode.core.fetcher.models.Request.RequestBuilder;
@@ -19,167 +23,182 @@ import br.com.lett.crawlernode.util.Logging;
 
 public class BrasilThebeautyboxCrawler extends CrawlerRankingKeywords {
 
-  public BrasilThebeautyboxCrawler(Session session) {
-    super(session);
-    super.fetchMode = FetchMode.APACHE;
-  }
+   public BrasilThebeautyboxCrawler(Session session) {
+      super(session);
+      super.fetchMode = FetchMode.APACHE;
+   }
 
-  private String keySHA256;
-  private static final Integer API_VERSION = 1;
-  private static final String SENDER = "tbb.main-theme@0.x";
-  private static final String PROVIDER = "tbb.chaordic-graphql@0.x";
+   private String keySHA256;
+   private static final Integer API_VERSION = 1;
+   private static final String SENDER = "vtex.store-resources@0.x";
+   private static final String PROVIDER = "vtex.search-graphql@0.x";
 
-  @Override
-  protected void extractProductsFromCurrentPage() {
-    this.log("Página " + this.currentPage);
-    this.pageSize = 9;
+   @Override
+   protected void extractProductsFromCurrentPage() {
+      this.log("Página " + this.currentPage);
+      this.pageSize = 9;
 
-    if (this.currentPage == 1) {
-      this.keySHA256 = fetchSHA256Key();
-    }
-
-    JSONObject searchApi = fetchSearchApi();
-    JSONArray products = searchApi.has("products") ? searchApi.getJSONArray("products") : new JSONArray();
-
-    if (products.length() > 0) {
-
-      if (this.totalProducts == 0) {
-        setTotalProducts(searchApi);
+      if (this.currentPage == 1) {
+         this.keySHA256 = fetchSHA256Key();
       }
 
-      for (Object object : products) {
-        JSONObject product = (JSONObject) object;
-        String productUrl = product.has("url") && !product.isNull("url") ? CrawlerUtils.completeUrl(product.get("url").toString(), "https",
-            "www.beautybox.com.br") : null;
-        String internalPid = product.has("id") && !product.isNull("id") ? product.get("id").toString() : null;
+      JSONObject searchApi = fetchSearchApi();
+      JSONArray products = searchApi.has("products") ? searchApi.getJSONArray("products") : new JSONArray();
 
-        saveDataProduct(null, internalPid, productUrl);
+      if (products.length() > 0) {
 
-        this.log("Position: " + this.position + " - InternalId: " + null + " - InternalPid: " + internalPid + " - Url: " + productUrl);
+         if (this.totalProducts == 0) {
+            setTotalProducts(searchApi);
+         }
 
-        if (this.arrayProducts.size() == productsLimit) {
-          break;
-        }
+         for (Object object : products) {
+            JSONObject product = (JSONObject) object;
+            String productUrl = product.has("linkText") && !product.isNull("linkText") ? "https://www.beautybox.com.br/" + (product.get("linkText").toString()) + "/p" : null;
+            String internalPid = product.has("productId") && !product.isNull("productId") ? product.get("productId").toString() : null;
+
+            saveDataProduct(null, internalPid, productUrl);
+
+            this.log("Position: " + this.position + " - InternalId: " + null + " - InternalPid: " + internalPid + " - Url: " + productUrl);
+
+            if (this.arrayProducts.size() == productsLimit) {
+               break;
+            }
+         }
+
+      } else {
+         this.result = false;
+         this.log("Keyword sem resultado!");
       }
 
-    } else {
-      this.result = false;
-      this.log("Keyword sem resultado!");
-    }
+      this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
+   }
 
-    this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
-  }
+   private void setTotalProducts(JSONObject data) {
+      this.totalProducts = CrawlerUtils.getIntegerValueFromJSON(data, "recordsFiltered", 0);
+      this.log("Total da busca: " + this.totalProducts);
+   }
 
-  private void setTotalProducts(JSONObject data) {
-    this.totalProducts = CrawlerUtils.getIntegerValueFromJSON(data, "size", 0);
-    this.log("Total da busca: " + this.totalProducts);
-  }
+   /**
+    * This function request a api with a JSON encoded on BASE64
+    * 
+    * This json has informations like: pageSize, keyword and substantive {@link fetchSubstantive}
+    * 
+    * @return
+    */
+   private JSONObject fetchSearchApi() {
+      JSONObject searchApi = new JSONObject();
 
-  /**
-   * This function request a api with a JSON encoded on BASE64
-   * 
-   * This json has informations like: pageSize, keyword and substantive {@link fetchSubstantive}
-   * 
-   * @return
-   */
-  private JSONObject fetchSearchApi() {
-    JSONObject searchApi = new JSONObject();
+      StringBuilder url = new StringBuilder();
+      url.append("https://www.beautybox.com.br/_v/segment/graphql/v1?");
+      url.append("workspace=master");
+      url.append("&maxAge=short");
+      url.append("&appsEtag=remove");
+      url.append("&domain=store");
+      url.append("&locale=pt-BR");
+      url.append("&operationName=productSearchV2");
 
-    StringBuilder url = new StringBuilder();
-    url.append("https://www.beautybox.com.br/_v/segment/graphql/v1?");
-    url.append("workspace=wfrc");
-    url.append("&maxAge=short");
-    url.append("&appsEtag=remove");
-    url.append("&domain=store");
-    url.append("&locale=pt-BR");
-    url.append("&operationName=getSearchProducts");
+      JSONObject extensions = new JSONObject();
+      JSONObject persistedQuery = new JSONObject();
 
-    JSONObject extensions = new JSONObject();
-    JSONObject persistedQuery = new JSONObject();
+      persistedQuery.put("version", API_VERSION);
+      persistedQuery.put("sha256Hash", this.keySHA256);
+      persistedQuery.put("sender", SENDER);
+      persistedQuery.put("provider", PROVIDER);
+      extensions.put("variables", createVariablesBase64());
+      extensions.put("persistedQuery", persistedQuery);
 
-    persistedQuery.put("version", API_VERSION);
-    persistedQuery.put("sha256Hash", this.keySHA256);
-    persistedQuery.put("sender", SENDER);
-    persistedQuery.put("provider", PROVIDER);
-    extensions.put("variables", createVariablesBase64());
-    extensions.put("persistedQuery", persistedQuery);
-
-    StringBuilder payload = new StringBuilder();
-    try {
-      payload.append("&variables=" + URLEncoder.encode("{}", "UTF-8"));
-      payload.append("&extensions=" + URLEncoder.encode(extensions.toString(), "UTF-8"));
-    } catch (UnsupportedEncodingException e) {
-      Logging.printLogError(logger, session, CommonMethods.getStackTrace(e));
-    }
-
-    url.append(payload.toString());
-    this.log("Link onde são feitos os crawlers: " + url);
-
-    Map<String, String> headers = new HashMap<>();
-    headers.put(HttpHeaders.CONTENT_TYPE, "application/json");
-
-    Request request = RequestBuilder.create()
-        .setUrl(url.toString())
-        .setCookies(cookies)
-        .setPayload(payload.toString())
-        .build();
-
-    JSONObject response = CrawlerUtils.stringToJson(this.dataFetcher.get(session, request).getBody());
-
-    if (response.has("data") && !response.isNull("data")) {
-      JSONObject data = response.getJSONObject("data");
-
-      if (data.has("searchProducts") && !data.isNull("searchProducts")) {
-        searchApi = data.getJSONObject("searchProducts");
+      StringBuilder payload = new StringBuilder();
+      try {
+         payload.append("&variables=" + URLEncoder.encode("{}", "UTF-8"));
+         payload.append("&extensions=" + URLEncoder.encode(extensions.toString(), "UTF-8"));
+      } catch (UnsupportedEncodingException e) {
+         Logging.printLogError(logger, session, CommonMethods.getStackTrace(e));
       }
-    }
 
-    return searchApi;
-  }
+      url.append(payload.toString());
+      this.log("Link onde são feitos os crawlers: " + url);
 
-  private String createVariablesBase64() {
-    JSONObject search = new JSONObject();
+      Map<String, String> headers = new HashMap<>();
+      headers.put(HttpHeaders.CONTENT_TYPE, "application/json");
 
-    search.put("terms", this.keywordWithoutAccents.replace(" ", "-"));
-    search.put("page", this.currentPage);
-    search.put("sortBy", "ascPrice");
-    search.put("resultsPerPage", 23);
+      Request request = RequestBuilder.create()
+            .setUrl(url.toString())
+            .setCookies(cookies)
+            .setPayload(payload.toString())
+            .build();
 
-    return Base64.getEncoder().encodeToString(search.toString().getBytes());
-  }
+      JSONObject response = CrawlerUtils.stringToJson(this.dataFetcher.get(session, request).getBody());
 
-  /**
-   * This function accesses the search url and extracts a hash that will be required to access the
-   * search api.
-   * 
-   * This hash is inside a key in json STATE. Ex:
-   * 
-   * "$ROOT_QUERY.productSearch({\"from\":0,\"hideUnavailableItems\":true,\"map\":\"ft\",\"orderBy\":\"OrderByTopSaleDESC\",\"query\":\"ACONDICIONADOR\",\"to\":19}) @runtimeMeta({\"hash\":\"0be25eb259af62c2a39f305122908321d46d3710243c4d4ec301bf158554fa71\"})"
-   * 
-   * Hash: 1d1ad37219ceb86fc281aa774971bbe1fe7656730e0a2ac50ba63ed63e45a2a3
-   * 
-   * @return
-   */
-  private String fetchSHA256Key() {
-    String hash = "6d98ffeb45b2706036dedc2189973929924433d158b5af169beafa14d3f2898c";
-    String url = "https://tbb.vteximg.com.br/_v/public/assets/v1/published/bundle/public/react/asset.min.js?"
-        + "v=0&files=tbb.main-theme@0.16.0,40,4,6,10,12,15,28,32,Header,20,ChaordicShelfPage,Trends,ChaordicSugestionsPage"
-        + "&files=vtex.store@2.69.0,common,StoreWrapper,DefaultChallenge,SearchWrapper,SearchContext"
-        + "&files=vtex.store-components@3.79.0,common,Animation,Container&workspace=wfrc";
+      if (response.has("data") && !response.isNull("data")) {
+         JSONObject data = response.getJSONObject("data");
 
-    Request request = RequestBuilder.create().setUrl(url).setCookies(cookies).mustSendContentEncoding(false).build();
-    String response = this.dataFetcher.get(session, request).getBody().replace(" ", "");
+         if (data.has("productSearch") && !data.isNull("productSearch")) {
+            searchApi = data.getJSONObject("productSearch");
+         }
+      }
 
-    String searchProducts = CrawlerUtils.extractSpecificStringFromScript(response, "querygetSearchProducts(", false, "',", false);
+      return searchApi;
+   }
 
-    String firstIndexString = "@runtimeMeta(hash:";
-    if (searchProducts.contains(firstIndexString) && searchProducts.contains(")")) {
-      int x = searchProducts.indexOf(firstIndexString) + firstIndexString.length();
-      int y = searchProducts.indexOf(')', x);
+   private String createVariablesBase64() {
+      JSONObject search = new JSONObject();
 
-      hash = searchProducts.substring(x, y).replace("\"", "");
-    }
+      search.put("withFacets", false);
+      search.put("hideUnavailableItems", false);
+      search.put("skusFilter", "ALL_AVAILABLE");
+      search.put("simulationBehavior", "default");
+      search.put("query", this.keywordWithoutAccents);
+      search.put("map", "c");
+      search.put("orderBy", "OrderByTopSaleDESC");
+      search.put("from", this.arrayProducts.size());
+      search.put("to", this.arrayProducts.size() + this.pageSize);
+      search.put("facetsBehavior", "Static");
 
-    return hash;
-  }
+      return Base64.getEncoder().encodeToString(search.toString().getBytes());
+   }
+
+   private String fetchSHA256Key() {
+      // When sha256Hash is not found, this key below works (on 03/02/2020)
+      String hash = "e7089d34509aca4eeee981c1cc619cbc13f09e6a9d4c2abbd02183a8f0d9fd92";
+      // When script with hash is not found, we use this url
+      String url =
+            "https://tbb.vtexassets.com/_v/public/assets/v1/published/bundle/public/react/asset.min.js?v=1"
+                  +
+                  "&files=vtex.css-handles@0.4.1,useCssHandles,applyModifiers&files=vtex.react-portal@0.3.0,common,Overlay"
+                  + "&files=vtex.format-currency@0.1.3,common,FormattedCurrency,formatCurrency"
+                  +
+                  "&files=vtex.store-resources@0.47.0,common,0,1,Mutations,OrderFormContext,7,4,8,6,9,3,2,5,Queries&workspace=master";
+
+      String homePage = "https://www.exito.com/";
+
+      Request requestHome =
+            RequestBuilder.create().setUrl(homePage).setCookies(cookies).mustSendContentEncoding(false).build();
+      Document doc = Jsoup.parse(this.dataFetcher.get(session, requestHome).getBody());
+
+      Elements scripts = doc.select("body > script[crossorigin]");
+      for (Element e : scripts) {
+         String scriptUrl = CrawlerUtils.scrapUrl(e, null, "src", "https", "tbb.vtexassets.com");
+         if (scriptUrl.contains("vtex.store-resources@")) {
+            url = scriptUrl;
+            break;
+         }
+      }
+
+      Request request =
+            RequestBuilder.create().setUrl(url).setCookies(cookies).mustSendContentEncoding(false).build();
+      String response = this.dataFetcher.get(session, request).getBody().replace(" ", "");
+
+      String searchProducts = CrawlerUtils.extractSpecificStringFromScript(response, "productSearch(",
+            false, "',", false);
+      String firstIndexString = "@runtimeMeta(hash:";
+      if (searchProducts.contains(firstIndexString) && searchProducts.contains(")")) {
+         int x = searchProducts.indexOf(firstIndexString) + firstIndexString.length();
+         int y = searchProducts.indexOf(')', x);
+
+         hash = searchProducts.substring(x, y).replace("\"", "");
+
+      }
+
+      return hash;
+   }
 }
