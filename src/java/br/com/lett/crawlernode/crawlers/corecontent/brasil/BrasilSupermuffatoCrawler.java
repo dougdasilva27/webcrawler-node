f220b8e9c03ffebc6e3deb13fd47423300bdab79
marcos <marcos@Mr-M>
2020-03-18 15:50:43 -0300

    BrasilSupermuffato
    I changed HOME_PAGE and MAIN_SELLER_NAME_LOWER

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilSupermuffatoCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilSupermuffatoCrawler.java
@@ -26,134 +26,136 @@ import models.prices.Prices;
 
 public class BrasilSupermuffatoCrawler extends Crawler {
 
-  private static final String HOME_PAGE = "https://www.supermuffato.com.br/";
-  private static final String MAIN_SELLER_NAME_LOWER = "super muffato eletro";
+   private static final String HOME_PAGE = "https://delivery.supermuffato.com.br/";
+   private static final String MAIN_SELLER_NAME_LOWER = "super muffato delivery";
 
-  public BrasilSupermuffatoCrawler(Session session) {
-    super(session);
-    super.config.setMustSendRatingToKinesis(true);
-  }
+   public BrasilSupermuffatoCrawler(Session session) {
+      super(session);
+      super.config.setMustSendRatingToKinesis(true);
+   }
 
-  @Override
-  public boolean shouldVisit() {
-    String href = this.session.getOriginalURL().toLowerCase();
-    return !FILTERS.matcher(href).matches() && (href.startsWith(HOME_PAGE));
-  }
+   @Override
+   public boolean shouldVisit() {
+      String href = this.session.getOriginalURL().toLowerCase();
+      return !FILTERS.matcher(href).matches() && (href.startsWith(HOME_PAGE));
+   }
 
 
-  @Override
-  public List<Product> extractInformation(Document doc) throws Exception {
-    super.extractInformation(doc);
-    List<Product> products = new ArrayList<>();
+   @Override
+   public List<Product> extractInformation(Document doc) throws Exception {
+      super.extractInformation(doc);
+      List<Product> products = new ArrayList<>();
 
-    if (isProductPage(doc)) {
-      VTEXCrawlersUtils vtexUtil = new VTEXCrawlersUtils(session, MAIN_SELLER_NAME_LOWER, HOME_PAGE, cookies, dataFetcher);
+      if (isProductPage(doc)) {
+         VTEXCrawlersUtils vtexUtil = new VTEXCrawlersUtils(session, MAIN_SELLER_NAME_LOWER, HOME_PAGE, cookies, dataFetcher);
+
+         JSONObject skuJson = CrawlerUtils.crawlSkuJsonVTEX(doc, session);
+
+         String internalPid = vtexUtil.crawlInternalPid(skuJson);
+         CategoryCollection categories = CrawlerUtils.crawlCategories(doc, ".bread-crumb > ul li a");
+         String description = CrawlerUtils.scrapSimpleDescription(doc, Arrays.asList("#prd-description", "#prd-specifications"));
+
+         // sku data in json
+         JSONArray arraySkus = skuJson != null && skuJson.has("skus") ? skuJson.getJSONArray("skus") : new JSONArray();
+
+         // ean data in json
+         JSONArray arrayEans = CrawlerUtils.scrapEanFromVTEX(doc);
+
+         for (int i = 0; i < arraySkus.length(); i++) {
+            JSONObject jsonSku = arraySkus.getJSONObject(i);
+
+            String internalId = vtexUtil.crawlInternalId(jsonSku);
+            JSONObject apiJSON = vtexUtil.crawlApi(internalId);
+            String name = vtexUtil.crawlName(jsonSku, skuJson, " ");
+            Map<String, Prices> marketplaceMap = vtexUtil.crawlMarketplace(apiJSON, internalId, true);
+            Marketplace marketplace = CrawlerUtils.assembleMarketplaceFromMap(marketplaceMap, Arrays.asList(MAIN_SELLER_NAME_LOWER), Card.VISA, session);
+            boolean available = marketplaceMap.containsKey(MAIN_SELLER_NAME_LOWER);
+            String primaryImage = vtexUtil.crawlPrimaryImage(apiJSON);
+            String secondaryImages = vtexUtil.crawlSecondaryImages(apiJSON);
+            Prices prices = marketplaceMap.containsKey(MAIN_SELLER_NAME_LOWER) ? marketplaceMap.get(MAIN_SELLER_NAME_LOWER) : new Prices();
+            Float price = vtexUtil.crawlMainPagePrice(prices);
+            Integer stock = vtexUtil.crawlStock(apiJSON);
+            String ean = i < arrayEans.length() ? arrayEans.getString(i) : null;
+            RatingsReviews ratingReviews = extractRatingAndReviews(doc);
+
+            List<String> eans = new ArrayList<>();
+            eans.add(ean);
+
+            // Creating the product
+            Product product = ProductBuilder.create().setUrl(session.getOriginalURL()).setInternalId(internalId).setInternalPid(internalPid).setName(name)
+                  .setPrice(price).setPrices(prices).setAvailable(available).setCategory1(categories.getCategory(0)).setCategory2(categories.getCategory(1))
+                  .setCategory3(categories.getCategory(2)).setPrimaryImage(primaryImage).setSecondaryImages(secondaryImages).setDescription(description)
+                  .setStock(stock).setMarketplace(marketplace).setEans(eans).setRatingReviews(ratingReviews).build();
+
+            products.add(product);
+         }
+
+      } else {
+         Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
+      }
+
+      return products;
+   }
+
+   private boolean isProductPage(Document document) {
+      return document.selectFirst(".productName") != null;
+   }
+
+
+
+   protected RatingsReviews extractRatingAndReviews(Document doc) {
+      RatingsReviews ratingReviews = new RatingsReviews();
+      ratingReviews.setDate(session.getDate());
 
       JSONObject skuJson = CrawlerUtils.crawlSkuJsonVTEX(doc, session);
 
-      String internalPid = vtexUtil.crawlInternalPid(skuJson);
-      CategoryCollection categories = CrawlerUtils.crawlCategories(doc, ".bread-crumb > ul li a");
-      String description = CrawlerUtils.scrapSimpleDescription(doc, Arrays.asList("#prd-description", "#prd-specifications"));
+      if (skuJson.has("productId")) {
+         Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
 
-      // sku data in json
-      JSONArray arraySkus = skuJson != null && skuJson.has("skus") ? skuJson.getJSONArray("skus") : new JSONArray();
+         List<String> idList = VTEXCrawlersUtils.crawlIdList(skuJson);
+         for (String internalId : idList) {
 
-      // ean data in json
-      JSONArray arrayEans = CrawlerUtils.scrapEanFromVTEX(doc);
+            JSONObject rating = crawlProductInformatioApi(internalId);
 
-      for (int i = 0; i < arraySkus.length(); i++) {
-        JSONObject jsonSku = arraySkus.getJSONObject(i);
+            Integer totalReviews = CrawlerUtils.getIntegerValueFromJSON(rating, "count", 0);
 
-        String internalId = vtexUtil.crawlInternalId(jsonSku);
-        JSONObject apiJSON = vtexUtil.crawlApi(internalId);
-        String name = vtexUtil.crawlName(jsonSku, skuJson, " ");
-        Map<String, Prices> marketplaceMap = vtexUtil.crawlMarketplace(apiJSON, internalId, true);
-        Marketplace marketplace = CrawlerUtils.assembleMarketplaceFromMap(marketplaceMap, Arrays.asList(MAIN_SELLER_NAME_LOWER), Card.VISA, session);
-        boolean available = marketplaceMap.containsKey(MAIN_SELLER_NAME_LOWER);
-        String primaryImage = vtexUtil.crawlPrimaryImage(apiJSON);
-        String secondaryImages = vtexUtil.crawlSecondaryImages(apiJSON);
-        Prices prices = marketplaceMap.containsKey(MAIN_SELLER_NAME_LOWER) ? marketplaceMap.get(MAIN_SELLER_NAME_LOWER) : new Prices();
-        Float price = vtexUtil.crawlMainPagePrice(prices);
-        Integer stock = vtexUtil.crawlStock(apiJSON);
-        String ean = i < arrayEans.length() ? arrayEans.getString(i) : null;
-        RatingsReviews ratingReviews = extractRatingAndReviews(doc);
+            Double avgRating = CrawlerUtils.getDoubleValueFromJSON(rating, "rate", true, false);
 
-        List<String> eans = new ArrayList<>();
-        eans.add(ean);
+            ratingReviews.setTotalRating(totalReviews);
+            ratingReviews.setTotalWrittenReviews(totalReviews);
+            ratingReviews.setAverageOverallRating(avgRating == null ? 0d : avgRating);
+            ratingReviews.setInternalId(internalId);
+         }
 
-        // Creating the product
-        Product product = ProductBuilder.create().setUrl(session.getOriginalURL()).setInternalId(internalId).setInternalPid(internalPid).setName(name)
-            .setPrice(price).setPrices(prices).setAvailable(available).setCategory1(categories.getCategory(0)).setCategory2(categories.getCategory(1))
-            .setCategory3(categories.getCategory(2)).setPrimaryImage(primaryImage).setSecondaryImages(secondaryImages).setDescription(description)
-            .setStock(stock).setMarketplace(marketplace).setEans(eans).setRatingReviews(ratingReviews).build();
-
-        products.add(product);
       }
 
-    } else {
-      Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
-    }
+      return ratingReviews;
 
-    return products;
-  }
+   }
 
-  private boolean isProductPage(Document document) {
-    return document.selectFirst(".productName") != null;
-  }
+   private JSONObject crawlProductInformatioApi(String internalId) {
+      JSONObject ratingJson = new JSONObject();
 
+      String apiUrl = "https://awsapis3.netreviews.eu/product";
+      String payload =
+            "{\"query\":\"average\",\"products\":[\"" + internalId + "\"],\"idWebsite\":\"4f870cb3-d6ef-5664-2950-de136d5b471e\",\"plateforme\":\"br\"}";
+      Map<String, String> headers = new HashMap<>();
+      headers.put("Content-Type", "application/json; charset=UTF-8");
 
+      Request request =
+            RequestBuilder.create().setUrl(apiUrl).setCookies(cookies).setHeaders(headers).setPayload(payload).mustSendContentEncoding(false).build();
+      JSONObject response = CrawlerUtils.stringToJson(new FetcherDataFetcher().post(session, request).getBody());
 
-  protected RatingsReviews extractRatingAndReviews(Document doc) {
-    RatingsReviews ratingReviews = new RatingsReviews();
-    ratingReviews.setDate(session.getDate());
+      if (response.has(internalId)) {
+         JSONArray rate = response.getJSONArray(internalId);
 
-    JSONObject skuJson = CrawlerUtils.crawlSkuJsonVTEX(doc, session);
-
-    if (skuJson.has("productId")) {
-      Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
-
-      List<String> idList = VTEXCrawlersUtils.crawlIdList(skuJson);
-      for (String internalId : idList) {
-
-        JSONObject rating = crawlProductInformatioApi(internalId);
-
-        Integer totalReviews = CrawlerUtils.getIntegerValueFromJSON(rating, "count", 0);
-
-        Double avgRating = CrawlerUtils.getDoubleValueFromJSON(rating, "rate", true, false);
-
-        ratingReviews.setTotalRating(totalReviews);
-        ratingReviews.setTotalWrittenReviews(totalReviews);
-        ratingReviews.setAverageOverallRating(avgRating == null ? 0d : avgRating);
-        ratingReviews.setInternalId(internalId);
+         if (rate.length() > 0) {
+            ratingJson = rate.getJSONObject(0);
+         }
       }
 
-    }
+      return ratingJson;
+   }
 
-    return ratingReviews;
 
-  }
-
-  private JSONObject crawlProductInformatioApi(String internalId) {
-    JSONObject ratingJson = new JSONObject();
-
-    String apiUrl = "https://awsapis3.netreviews.eu/product";
-    String payload =
-        "{\"query\":\"average\",\"products\":[\"" + internalId + "\"],\"idWebsite\":\"4f870cb3-d6ef-5664-2950-de136d5b471e\",\"plateforme\":\"br\"}";
-    Map<String, String> headers = new HashMap<>();
-    headers.put("Content-Type", "application/json; charset=UTF-8");
-
-    Request request =
-        RequestBuilder.create().setUrl(apiUrl).setCookies(cookies).setHeaders(headers).setPayload(payload).mustSendContentEncoding(false).build();
-    JSONObject response = CrawlerUtils.stringToJson(new FetcherDataFetcher().post(session, request).getBody());
-
-    if (response.has(internalId)) {
-      JSONArray rate = response.getJSONArray(internalId);
-
-      if (rate.length() > 0) {
-        ratingJson = rate.getJSONObject(0);
-      }
-    }
-
-    return ratingJson;
-  }
 }
