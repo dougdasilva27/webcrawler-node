96a4c55aa1090467f10c2ce1e7a21852a88e18ce
Vitor de Andrade <vitorandrade@lett.digital>
2020-03-27 22:41:57 -0300

    Adds remaining crawlers for sitemercado

---
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/saltodepirapora/SaltodepiraporaSitemercadosaoroqueCrawler.java
@@ -0,0 +1,47 @@
+package br.com.lett.crawlernode.crawlers.corecontent.saltodepirapora;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.json.JSONObject;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.crawlers.corecontent.extractionutils.BrasilSitemercadoCrawler;
+import br.com.lett.crawlernode.util.CommonMethods;
+
+/**
+ * @author gabriel date: 2019-09-24
+ */
+public class SaltodepiraporaSitemercadosaoroqueCrawler extends BrasilSitemercadoCrawler {
+
+   public SaltodepiraporaSitemercadosaoroqueCrawler(Session session) {
+      super(session);
+   }
+
+   public static final String HOME_PAGE = "https://www.sitemercado.com.br/saoroque/salto-de-pirapora-salto-de-pirapora-centro-r-antonio-rodrigues-simoes";
+
+   public static final int IDLOJA = 2379;
+   public static final int IDREDE = 271;
+
+   @Override
+   protected String getHomePage() {
+      return HOME_PAGE;
+   }
+
+   @Override
+   protected String getLoadPayload() {
+      JSONObject payload = new JSONObject();
+      String[] split = HOME_PAGE.split("/");
+
+      payload.put("lojaUrl", CommonMethods.getLast(split));
+      payload.put("redeUrl", split[split.length - 2]);
+
+      return payload.toString();
+   }
+
+   @Override
+   protected Map<String, Integer> getLojaInfo() {
+      Map<String, Integer> lojaInfo = new HashMap<>();
+      lojaInfo.put("IdLoja", IDLOJA);
+      lojaInfo.put("IdRede", IDREDE);
+      return lojaInfo;
+   }
+}
