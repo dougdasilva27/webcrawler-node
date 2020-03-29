f0d7ee47c37c794172de65b8f17a5fbce578ebf3
Vitor de Andrade <vitorandrade@lett.digital>
2020-03-28 23:43:56 -0300

    Fixes João Pessoa IOB and Manaira Crawlers

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/joaopessoa/JoaopessoaSitemercadosupermercadoiobCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/joaopessoa/JoaopessoaSitemercadosupermercadoiobCrawler.java
@@ -18,8 +18,8 @@ public class JoaopessoaSitemercadosupermercadoiobCrawler extends BrasilSitemerca
 
    public static final String HOME_PAGE = "https://www.sitemercado.com.br/supermercadoiob/joao-pessoa-manaira-manaira-rua-major-ciraulo";
 
-   public static final int IDLOJA = 1208;
-   public static final int IDREDE = 1108;
+   public static final int IDLOJA = 927;
+   public static final int IDREDE = 848;
 
    @Override
    protected String getHomePage() {
