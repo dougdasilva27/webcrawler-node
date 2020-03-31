ff55375b53e81b96a027e51cf63dfa5ea07ae593
Arthur Floresta <15871221+arthurfrez@users.noreply.github.com>
2020-03-31 17:12:57 -0300

    Done

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilAgropecuariaimaruiCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilAgropecuariaimaruiCrawler.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import com.google.common.collect.Sets;
 import br.com.lett.crawlernode.core.models.Card;
 import br.com.lett.crawlernode.core.models.CategoryCollection;
 import br.com.lett.crawlernode.core.models.Product;
@@ -18,13 +20,27 @@ import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.Crawler;
 import br.com.lett.crawlernode.util.CrawlerUtils;
 import br.com.lett.crawlernode.util.Logging;
+import br.com.lett.crawlernode.util.MathUtils;
 import br.com.lett.crawlernode.util.Pair;
-import models.Marketplace;
+import exceptions.MalformedPricingException;
+import exceptions.OfferException;
+import models.Offer.OfferBuilder;
+import models.Offers;
 import models.prices.Prices;
+import models.pricing.BankSlip.BankSlipBuilder;
+import models.pricing.CreditCards;
+import models.pricing.Installment.InstallmentBuilder;
+import models.pricing.Installments;
+import models.pricing.Pricing;
+import models.pricing.Pricing.PricingBuilder;
 
 public class BrasilAgropecuariaimaruiCrawler extends Crawler {
 
    private static final String HOME_PAGE = "https://agropecuariaimarui.com.br/";
+   private final String MAIN_SELLER_NAME = "Agropecuária Imaruí";
+   private Set<String> cards = Sets.newHashSet(Card.MASTERCARD.toString(), 
+         Card.VISA.toString(), Card.ELO.toString(), Card.AMEX.toString(), Card.DINERS.toString(),
+         Card.DISCOVER.toString(), Card.AURA.toString(), Card.UNKNOWN_CARD.toString(), Card.HIPERCARD.toString());
 
    public BrasilAgropecuariaimaruiCrawler(Session session) {
       super(session);
@@ -54,14 +70,11 @@ public class BrasilAgropecuariaimaruiCrawler extends Crawler {
          String internalId = CrawlerUtils.scrapStringSimpleInfoByAttribute(doc, "input#comment_post_ID", "value");
          String internalPid = skuJson != null && skuJson.has("sku") ? skuJson.get("sku").toString() : null;
          String name = skuJson != null && skuJson.has("name") ? skuJson.get("name").toString() : null;
-         Float price = crawlPrice(doc);
-         Prices prices = scrapPrices(doc, price);
          CategoryCollection categories = CrawlerUtils.crawlCategories(doc, ".product-info .woocommerce-breadcrumb a[href]", true);
          String primaryImage = CrawlerUtils.scrapSimplePrimaryImage(doc, ".woocommerce-product-gallery figure img", Arrays.asList("src"), "https:", HOME_PAGE);
          String secondaryImages = null;
          String description = CrawlerUtils.scrapElementsDescription(doc, Arrays.asList(".product-short-description p", ".product-footer .tab-panels #tab-description"));
-         boolean available = !doc.select(".product-info .stock.in-stock").isEmpty();
-         Marketplace marketplace = null;
+         Offers offers = scrapOffers(doc);
 
          // Creating the product
          Product product = ProductBuilder.create()
@@ -69,16 +82,13 @@ public class BrasilAgropecuariaimaruiCrawler extends Crawler {
                .setInternalId(internalId)
                .setInternalPid(internalPid)
                .setName(name)
-               .setPrice(price)
-               .setPrices(prices)
-               .setAvailable(available)
                .setCategory1(categories.getCategory(0))
                .setCategory2(categories.getCategory(1))
                .setCategory3(categories.getCategory(2))
                .setPrimaryImage(primaryImage)
                .setSecondaryImages(secondaryImages)
                .setDescription(description)
-               .setMarketplace(marketplace)
+               .setOffers(offers)
                .build();
 
          Element variationElement = doc.selectFirst(".product-container .product-info form.variations_form");
@@ -110,54 +120,64 @@ public class BrasilAgropecuariaimaruiCrawler extends Crawler {
 
       return products;
    }
+   
+   private Offers scrapOffers(Document doc) throws OfferException, MalformedPricingException {
+      Offers offers = new Offers();
+      Pricing pricing = scrapPricing(doc);
 
-   private float crawlPrice(Document doc) {
-      Float price = CrawlerUtils.scrapFloatPriceFromHtml(doc, ".price-wrapper .product-page-price :not(:first-child) .woocommerce-Price-amount", null, true, ',', session);
-      if (price == null) {
-         price = CrawlerUtils.scrapFloatPriceFromHtml(doc, ".price-wrapper .product-page-price .woocommerce-Price-amount", null, true, ',', session);
+      offers.add(OfferBuilder.create()
+            .setUseSlugNameAsInternalSellerId(true)
+            .setSellerFullName(MAIN_SELLER_NAME)
+            .setSellersPagePosition(1)
+            .setIsBuybox(false)
+            .setIsMainRetailer(true)
+            .setPricing(pricing)
+            .build());
+
+      return offers;
+   }
+   
+   private Pricing scrapPricing(Document doc) throws MalformedPricingException {
+      Double spotlightPrice = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".price-wrapper .product-page-price :not(:first-child) .woocommerce-Price-amount", null, true, ',', session);
+      if (spotlightPrice == null) {
+         spotlightPrice = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".price-wrapper .product-page-price .woocommerce-Price-amount", null, true, ',', session);
       }
+      Double priceFrom = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".price-wrapper .price del .amount", null, true, ',', session);
+      CreditCards creditCards = scrapCreditCards(doc, spotlightPrice);
+      
+      return PricingBuilder.create()
+            .setSpotlightPrice(spotlightPrice)
+            .setPriceFrom(priceFrom)
+            .setCreditCards(creditCards)
+            .setBankSlip(BankSlipBuilder.create().setFinalPrice(spotlightPrice).setOnPageDiscount(0d).build())
+            .build();
+   }
 
-      return price;
+   private CreditCards scrapCreditCards(Document doc, Double spotlightPrice) throws MalformedPricingException {
+      CreditCards creditCards = new CreditCards();
+
+      Installments installments = new Installments();
+      installments.add(InstallmentBuilder.create()
+            .setInstallmentNumber(1)
+            .setInstallmentPrice(spotlightPrice)
+            .build());
+
+      for (Element e : doc.select(".product-info .wc-simulador-parcelas-payment-options li")) {
+         Pair<Integer, Float> installment = CrawlerUtils.crawlSimpleInstallment(null, e, false, "x de", "juros", true, ',');
+
+         if (!installment.isAnyValueNull()) {
+            installments.add(InstallmentBuilder.create()
+                  .setInstallmentNumber(installment.getFirst())
+                  .setInstallmentPrice(MathUtils.normalizeTwoDecimalPlaces(installment.getSecond().doubleValue()))
+                  .build());
+         }
+      }
+      
+      return creditCards;
    }
 
    private boolean isProductPage(Document doc) {
       return doc.selectFirst(".product-container .product-main") != null;
-   }
-
-   private Prices scrapPrices(Document doc, Float price) {
-      Prices prices = new Prices();
-
-      if (price != null) {
-         prices.setBankTicketPrice(CrawlerUtils.scrapDoublePriceFromHtml(doc, ".wc-simulador-parcelas-offer .woocommerce-Price-amount", null, true, ',', session));
-
-         Double priceFrom = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".price-wrapper .price del .amount", null, true, ',', session);
-         if (priceFrom != null) {
-            prices.setPriceFrom(priceFrom);
-         }
-
-         Map<Integer, Float> installmentPriceMap = new TreeMap<>();
-         installmentPriceMap.put(1, price);
-
-         for (Element e : doc.select(".product-info .wc-simulador-parcelas-payment-options li")) {
-            Pair<Integer, Float> installment = CrawlerUtils.crawlSimpleInstallment(null, e, false, "x de", "juros", true);
-
-            if (!installment.isAnyValueNull()) {
-               installmentPriceMap.put(installment.getFirst(), installment.getSecond());
-            }
-         }
-
-         prices.insertCardInstallment(Card.MASTERCARD.toString(), installmentPriceMap);
-         prices.insertCardInstallment(Card.VISA.toString(), installmentPriceMap);
-         prices.insertCardInstallment(Card.ELO.toString(), installmentPriceMap);
-         prices.insertCardInstallment(Card.AMEX.toString(), installmentPriceMap);
-         prices.insertCardInstallment(Card.DINERS.toString(), installmentPriceMap);
-         prices.insertCardInstallment(Card.DISCOVER.toString(), installmentPriceMap);
-         prices.insertCardInstallment(Card.AURA.toString(), installmentPriceMap);
-         prices.insertCardInstallment(Card.UNKNOWN_CARD.toString(), installmentPriceMap);
-         prices.insertCardInstallment(Card.HIPERCARD.toString(), installmentPriceMap);
-      }
-
-      return prices;
    }
 
    private String scrapVariationName(JSONObject json, String name) {
