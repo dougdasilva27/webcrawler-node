b8f97dcff87c099ec3496814d0554c5db706050b
Gabriel Dornelas <gabrieldornelas@lett.digital>
2020-04-01 11:05:23 -0300

    removing test commit

--- src/java/br/com/lett/crawlernode/test/Test.java
+++ src/java/br/com/lett/crawlernode/test/Test.java
@@ -103,7 +103,7 @@ public class Test {
          Session session;
 
          if (testType.equals(KEYWORDS_TEST)) {
-            session = SessionFactory.createTestRankingKeywordsSession("jugo ", market);
+            session = SessionFactory.createTestRankingKeywordsSession("comfort ", market);
          } else if (testType.equals(CATEGORIES_TEST)) {
             session = SessionFactory.createTestRankingCategoriesSession(
                   "https://produto.mercadolivre.com.br/MLB-1225997122-capsula-de-cafe-espresso-melitta-staccato-10-unidades-_JM",
@@ -111,7 +111,7 @@ public class Test {
          } else {
             session = SessionFactory
                   .createTestSession(
-                        "https://www.amazon.com.br/Carga-Gillette-Mach3-Turbo-Unidades/dp/B00G4UBIZQ/ref=sr_1_116",
+                        "https://www.gpsfarma.com/cuidado-personal-1/afeitada/maquina-de-afeitar-prestobarba3-sense-care-x-4-un.html",
                         market);
          }
 
