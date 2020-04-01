5bb344371856e9da88ab40c1672d8c47c1243fc0
Gabriel Dornelas <gabrieldornelas@lett.digital>
2020-04-01 11:06:19 -0300

    test

--- src/java/br/com/lett/crawlernode/test/Test.java
+++ src/java/br/com/lett/crawlernode/test/Test.java
@@ -103,7 +103,7 @@ public class Test {
          Session session;
 
          if (testType.equals(KEYWORDS_TEST)) {
-            session = SessionFactory.createTestRankingKeywordsSession("comfort ", market);
+            session = SessionFactory.createTestRankingKeywordsSession("jugo ", market);
          } else if (testType.equals(CATEGORIES_TEST)) {
             session = SessionFactory.createTestRankingCategoriesSession(
                   "https://produto.mercadolivre.com.br/MLB-1225997122-capsula-de-cafe-espresso-melitta-staccato-10-unidades-_JM",
@@ -111,7 +111,7 @@ public class Test {
          } else {
             session = SessionFactory
                   .createTestSession(
-                        "https://www.gpsfarma.com/cuidado-personal-1/afeitada/maquina-de-afeitar-prestobarba3-sense-care-x-4-un.html",
+                        "https://www.carrefour.com.br/Kit-de-Fraldas-Huggies-Hiper-Supreme-Care-XXG-156-Unidades/p/B2-3-502952",
                         market);
          }
 
