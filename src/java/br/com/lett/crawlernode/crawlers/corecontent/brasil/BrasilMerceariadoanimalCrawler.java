f291f208ea192d88a227c4224d6be1c6cdfa35e9
Arthur Floresta <15871221+arthurfrez@users.noreply.github.com>
2020-04-02 17:26:14 -0300

    Did stuff

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilMerceariadoanimalCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilMerceariadoanimalCrawler.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import com.google.common.collect.Sets;
 import br.com.lett.crawlernode.core.fetcher.models.Request;
 import br.com.lett.crawlernode.core.fetcher.models.Request.RequestBuilder;
 import br.com.lett.crawlernode.core.models.Card;
@@ -21,19 +24,36 @@ import br.com.lett.crawlernode.core.task.impl.Crawler;
 import br.com.lett.crawlernode.util.CrawlerUtils;
 import br.com.lett.crawlernode.util.JSONUtils;
 import br.com.lett.crawlernode.util.Logging;
+import br.com.lett.crawlernode.util.MathUtils;
 import br.com.lett.crawlernode.util.Pair;
+import exceptions.MalformedPricingException;
+import exceptions.OfferException;
 import models.Marketplace;
+import models.Offer;
+import models.Offer.OfferBuilder;
+import models.Offers;
 import models.RatingsReviews;
+import models.Seller;
 import models.prices.Prices;
+import models.pricing.BankSlip.BankSlipBuilder;
+import models.pricing.CreditCard.CreditCardBuilder;
+import models.pricing.CreditCards;
+import models.pricing.Installment.InstallmentBuilder;
+import models.pricing.Installments;
+import models.pricing.Pricing;
+import models.pricing.Pricing.PricingBuilder;
 
 public class BrasilMerceariadoanimalCrawler extends Crawler {
-  
+   
+  private static final String HOME_PAGE = "https://www.merceariadoanimal.com.br/";
+  private static final String MAIN_SELLER_NAME = "Mercearia do Animal";
+  private Set<String> cards = Sets.newHashSet(Card.VISA.toString(), Card.MASTERCARD.toString(), 
+       Card.AMEX.toString(), Card.DINERS.toString());
+
   public BrasilMerceariadoanimalCrawler(Session session) {
     super(session);
     super.config.setMustSendRatingToKinesis(true);
   }
-
-  private static final String HOME_PAGE = "https://www.merceariadoanimal.com.br/";
 
   @Override
   public List<Product> extractInformation(Document doc) throws Exception {
@@ -47,18 +67,13 @@ public class BrasilMerceariadoanimalCrawler extends Crawler {
       String internalPid = CrawlerUtils.scrapStringSimpleInfoByAttribute(doc, "#productDetailsAddToCartForm > input[name=\"product_id\"]", "value");
       String name = CrawlerUtils.scrapStringSimpleInfo(doc, ".ProductMain [itemprop=\"name\"]", true) + " - " +
           CrawlerUtils.scrapStringSimpleInfo(doc, ".ProductMain .brand > a", true);
-      Float price = CrawlerUtils.scrapFloatPriceFromHtml(doc, ".PriceRow [itemprop=\"price\"]", "content", false, '.', session);
-      Prices prices = scrapPrices(doc, price);
       CategoryCollection categories = CrawlerUtils.crawlCategories(doc, ".Breadcrumb > ul > li[itemprop]", true);
       String primaryImage = CrawlerUtils.scrapSimplePrimaryImage(doc, ".ProductThumbImage > a", Arrays.asList("href"), "https:", HOME_PAGE);
       String secondaryImages = scrapSecondaryImages(doc, primaryImage);
       String description = CrawlerUtils.scrapElementsDescription(doc, Arrays.asList(
           "#product-tabs ul> li:not(#tab-reviews)", "#product-tabs .tab-content > div:not(#reviews)"));
-      Integer stock = null;
-      boolean available = doc.selectFirst("#hidden_aviseme") == null;
-      Marketplace marketplace = null;
       RatingsReviews ratingReviews = scrapRatingsReviews(doc);
-      
+      Offers offers = scrapOffers(doc);
 
       // Creating the product
       Product product = ProductBuilder.create()
@@ -66,25 +81,24 @@ public class BrasilMerceariadoanimalCrawler extends Crawler {
           .setInternalId(internalId)
           .setInternalPid(internalPid)
           .setName(name)
-          .setPrice(price)
-          .setPrices(prices)
-          .setAvailable(available)
           .setCategory1(categories.getCategory(0))
           .setCategory2(categories.getCategory(1))
           .setCategory3(categories.getCategory(2))
           .setPrimaryImage(primaryImage)
           .setSecondaryImages(secondaryImages)
           .setDescription(description)
-          .setStock(stock)
-          .setMarketplace(marketplace)
           .setRatingReviews(ratingReviews)
+          .setOffers(offers)
           .build();
       
       Elements selectVariations = doc.select(".ProductOptionList .VariationSelect option:not([value=\"\"])");
-      Elements radioVariations = doc.select(".ProductOptionList .Value > ul > li > label");
+      Elements radioVariations = doc.select(".ProductOptionList .Value > ul > li > label > input");
       
-      if(!selectVariations.isEmpty()) {
-        for(Element variation : selectVariations) {
+      ArrayList<Element> allVariations = selectVariations;
+      allVariations.addAll(radioVariations);
+      
+      if(!allVariations.isEmpty()) {
+        for(Element variation : allVariations) {
           JSONObject json = getVariationJSON(variation.attr("value"), internalPid);
           
           Product clone = product.clone();
@@ -93,41 +107,42 @@ public class BrasilMerceariadoanimalCrawler extends Crawler {
             clone.setInternalId(json.get("combinationid").toString());
           }
           
+          Offers variationOffers = scrapVariationOffers(json);
           clone.setName(product.getName() + ", " + variation.text().replace("(Indisponível)", "").trim());
+          clone.setOffers(variationOffers);
           
-          clone.setPrice(CrawlerUtils.getFloatValueFromJSON(json, "price", false, true));
-          clone.setPrices(scrapVariationPrices(prices, json, clone.getPrice()));
-         
+          // TODO: remove this when bug is fixed - this is an almost copy paste from ProductBuilder.build()
+          // bug happens because this code only occur at ProductBuilder.build()
+          // when you clone a product and set a new offers, the other fields that are supposed
+          // to change stays the same.
+          if (variationOffers != null) {
+             for (Offer offer : variationOffers.getOffersList()) {
+                if (offer.getPricing() != null) {
+                   if (offer.getIsMainRetailer()) {
+                      Pricing pricing = offer.getPricing();
+                      clone.setAvailable(true);
+                      clone.setPrices(new Prices(pricing));
+                      clone.setPrice(pricing.getSpotlightPrice().floatValue());
+                   } else {
+                      Marketplace variationMkp = clone.getMarketplace();
+                      
+                      if (variationMkp == null) {
+                         variationMkp = new Marketplace();
+                      }
+
+                      variationMkp.add(new Seller(offer));
+                      clone.setMarketplace(variationMkp);
+                   }
+                }
+             }
+          }
+          // ----------- REMOVE ABOVE -----------
+          
           if(json.has("instock") && json.get("instock") instanceof Boolean) {
             clone.setAvailable(json.getBoolean("instock"));
           }
           
           products.add(clone);
-        }
-      } else if(!radioVariations.isEmpty()) {        
-        for(Element variation : radioVariations) {
-          Element input = variation.selectFirst("input");
-          
-          if(input != null) {
-            JSONObject json = getVariationJSON(input.attr("value"), internalPid);
-            
-            Product clone = product.clone();
-            
-            if(json.has("combinationid") && !json.isNull("combinationid")) {
-              clone.setInternalId(json.get("combinationid").toString());
-            }
-            
-            clone.setName(product.getName() + ", " + variation.text().replace("(Indisponível)", "").trim());
-            
-            clone.setPrice(CrawlerUtils.getFloatValueFromJSON(json, "price", false, true));
-            clone.setPrices(scrapVariationPrices(prices, json, clone.getPrice()));
-           
-            if(json.has("instock") && json.get("instock") instanceof Boolean) {
-              clone.setAvailable(json.getBoolean("instock"));
-            }
-            
-            products.add(clone);
-          }
         }
       } else {
         products.add(product);
@@ -136,6 +151,146 @@ public class BrasilMerceariadoanimalCrawler extends Crawler {
       Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
     }
     return products;
+  }
+  
+  private Offers scrapVariationOffers(JSONObject json) throws OfferException, MalformedPricingException {
+     Offers offers = new Offers();
+     Pricing pricing = scrapVariationPricing(json);
+
+     if(pricing != null) {
+       offers.add(OfferBuilder.create()
+             .setUseSlugNameAsInternalSellerId(true)
+             .setSellerFullName(MAIN_SELLER_NAME)
+             .setSellersPagePosition(1)
+             .setIsBuybox(false)
+             .setIsMainRetailer(true)
+             .setPricing(pricing)
+             .build());
+     }
+     
+     return offers;
+  }
+  
+  private Pricing scrapVariationPricing(JSONObject json) throws MalformedPricingException {
+     Double spotlightPrice = CrawlerUtils.getDoubleValueFromJSON(json, "price", false, true);
+
+     if(spotlightPrice != null) {
+       Double bankSlipPrice = null;
+       Double bankSlipDiscount = null;
+       CreditCards creditCards = scrapVariationCreditCards(json, spotlightPrice);
+       
+       if(json.has("checkdescmsg") && json.get("checkdescmsg") instanceof String) {
+          Document slipDoc = Jsoup.parse(json.getString("checkdescmsg"));
+          bankSlipPrice = CrawlerUtils.scrapDoublePriceFromHtml(slipDoc, "strong", null, true, ',', session);
+          bankSlipDiscount = CrawlerUtils.scrapDoublePriceFromHtml(slipDoc, "span", null, true, ',', session)/100;
+       }
+
+       return PricingBuilder.create()
+             .setSpotlightPrice(spotlightPrice)
+             .setCreditCards(creditCards)
+             .setBankSlip(BankSlipBuilder.create().setFinalPrice(bankSlipPrice).setOnPageDiscount(bankSlipDiscount).build())
+             .build();
+     }
+
+     return null;
+  }
+  
+  private CreditCards scrapVariationCreditCards(JSONObject json, Double spotlightPrice) throws MalformedPricingException {
+     CreditCards creditCards = new CreditCards();
+
+     Installments installments = new Installments();
+     installments.add(InstallmentBuilder.create()
+           .setInstallmentNumber(1)
+           .setInstallmentPrice(spotlightPrice)
+           .build());
+
+     if(json.has("parcmsg") && json.get("parcmsg") instanceof String) {
+        String installmentText = json.getString("parcmsg");
+        
+        Pair<Integer, Float> pair = CrawlerUtils.crawlSimpleInstallmentFromString(installmentText, "x", "juros", true);
+        if (!pair.isAnyValueNull()) {
+           installments.add(InstallmentBuilder.create()
+                .setInstallmentNumber(pair.getFirst())
+                .setInstallmentPrice(MathUtils.normalizeTwoDecimalPlaces(pair.getSecond().doubleValue()))
+                .build());
+        }
+     }
+     
+     for (String brand : cards) {
+        creditCards.add(CreditCardBuilder.create()
+              .setBrand(brand)
+              .setIsShopCard(false)
+              .setInstallments(installments)
+              .build());
+     }
+     
+     return creditCards;
+  }
+  
+  private Offers scrapOffers(Document doc) throws OfferException, MalformedPricingException {
+     Offers offers = new Offers();
+     Pricing pricing = scrapPricing(doc);
+
+     if(pricing != null) {
+       offers.add(OfferBuilder.create()
+             .setUseSlugNameAsInternalSellerId(true)
+             .setSellerFullName(MAIN_SELLER_NAME)
+             .setSellersPagePosition(1)
+             .setIsBuybox(false)
+             .setIsMainRetailer(true)
+             .setPricing(pricing)
+             .build());
+     }
+     
+     return offers;
+  }
+  
+  private Pricing scrapPricing(Document doc) throws MalformedPricingException {
+     Double spotlightPrice = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".PriceRow [itemprop=\"price\"]", "content", false, '.', session);
+
+     if(spotlightPrice != null) {
+       Double priceFrom = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".product .promotion", null, true, ',', session);
+       Double bankSlipPrice = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".MsgBoleto strong", null, true, ',', session);
+       Double bankSlipDiscount = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".MsgBoleto span", null, true, ',', session)/100;
+       CreditCards creditCards = scrapCreditCards(doc, spotlightPrice);
+
+       return PricingBuilder.create()
+             .setSpotlightPrice(spotlightPrice)
+             .setPriceFrom(priceFrom)
+             .setCreditCards(creditCards)
+             .setBankSlip(BankSlipBuilder.create().setFinalPrice(bankSlipPrice).setOnPageDiscount(bankSlipDiscount).build())
+             .build();
+     }
+
+     return null;
+  }
+  
+  private CreditCards scrapCreditCards(Document doc, Double spotlightPrice) throws MalformedPricingException {
+     CreditCards creditCards = new CreditCards();
+
+     Installments installments = new Installments();
+     installments.add(InstallmentBuilder.create()
+           .setInstallmentNumber(1)
+           .setInstallmentPrice(spotlightPrice)
+           .build());
+
+     Pair<Integer, Float> pair = CrawlerUtils.crawlSimpleInstallment(".MsgParcelamento", doc, true, "x de", "juros", true, ',');
+     if (!pair.isAnyValueNull()) {
+        installments.add(InstallmentBuilder.create()
+              .setInstallmentNumber(pair.getFirst())
+              .setInstallmentPrice(MathUtils.normalizeTwoDecimalPlaces(pair.getSecond().doubleValue()))
+              .build());
+     }
+     
+     for (String brand : cards) {
+        creditCards.add(CreditCardBuilder.create()
+              .setBrand(brand)
+              .setIsShopCard(false)
+              .setInstallments(installments)
+              .build());
+     }
+     
+     return creditCards;
   }
 
   private boolean isProductPage(Document doc) {
@@ -167,29 +322,6 @@ public class BrasilMerceariadoanimalCrawler extends Crawler {
     }
     
     return secondaryImages;
-  }
-  
-  private Prices scrapPrices(Document doc, Float price) {
-    Prices prices = new Prices();
-    
-    if(price != null) {
-      prices.setBankTicketPrice(CrawlerUtils.scrapFloatPriceFromHtml(doc, ".MsgBoleto strong", null, true, ',', session));
-      
-      Map<Integer, Float> installmentPriceMap = new TreeMap<>();
-      installmentPriceMap.put(1, price);
-      
-      Pair<Integer, Float> installment = CrawlerUtils.crawlSimpleInstallment(".MsgParcelamento", doc, true, "x de", "juros", true);
-      if (!installment.isAnyValueNull()) {
-        installmentPriceMap.put(installment.getFirst(), installment.getSecond());
-      }
-      
-      prices.insertCardInstallment(Card.VISA.toString(), installmentPriceMap);
-      prices.insertCardInstallment(Card.MASTERCARD.toString(), installmentPriceMap);
-      prices.insertCardInstallment(Card.AMEX.toString(), installmentPriceMap);
-      prices.insertCardInstallment(Card.DINERS.toString(), installmentPriceMap);
-    }
-    
-    return prices;
   }
   
   private RatingsReviews scrapRatingsReviews(Document doc) {
