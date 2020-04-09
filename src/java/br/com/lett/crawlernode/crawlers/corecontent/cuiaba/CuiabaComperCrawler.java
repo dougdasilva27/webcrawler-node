23752d0a89409678fe30ed719a77476269a1a4a5
Gabriel Dornelas <gabrieldornelas@lett.digital>
2020-04-09 16:47:19 -0300

    new comper crawlers

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/cuiaba/CuiabaComperCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/cuiaba/CuiabaComperCrawler.java
@@ -1,15 +1,15 @@
 
-package br.com.lett.crawlernode.crawlers.corecontent.campogrande;
+package br.com.lett.crawlernode.crawlers.corecontent.cuiaba;
 
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.crawlers.corecontent.extractionutils.ComperCrawler;
 
-public class CampograndeComperCrawler extends ComperCrawler {
+public class CuiabaComperCrawler extends ComperCrawler {
 
-   private static final String STORE_ID = "6602";
-   private static final String MULTI_STORE_ID = "02010714110000101010000010001000";
+   private static final String STORE_ID = "6637";
+   private static final String MULTI_STORE_ID = "05050714110000101010100010100010";
 
-   public CampograndeComperCrawler(Session session) {
+   public CuiabaComperCrawler(Session session) {
       super(session);
    }
 
