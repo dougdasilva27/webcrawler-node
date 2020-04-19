330634163c36d2334da85f4d40b2093b166bdf47
Fabricio Massula <fabriciomassula@lett.digital>
2020-04-18 23:12:10 -0300

    Crawlers do Carrefoiur para os 13 centros de distribuição do brasil

---
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/saopaulo/SaopauloCarrefourbrooklinCrawler.java
@@ -0,0 +1,39 @@
+package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
+
+import java.util.HashMap;
+import java.util.Map;
+import br.com.lett.crawlernode.core.fetcher.FetchMode;
+import br.com.lett.crawlernode.core.fetcher.models.Request;
+import br.com.lett.crawlernode.core.fetcher.models.Request.RequestBuilder;
+import br.com.lett.crawlernode.core.session.Session;
+
+/**
+ * 18/04/2020
+ * 
+ * @author Fabrício
+ *
+ */
+public class SaopauloCarrefourbrooklinCrawler extends BrasilCarrefourCrawler {
+
+   public SaopauloCarrefourbrooklinCrawler(Session session) {
+      super(session);
+      
+      super.fetchMode = FetchMode.FETCHER;
+   }
+
+   @Override
+   protected String fetchPage(String url) {
+      Map<String, String> headers = new HashMap<>();
+      headers.put("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+      headers.put("accept-language", "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7,es;q=0.6");
+      headers.put("sec-fetch-user", "?1");
+      headers.put("sec-fetch-site", "none");
+      headers.put("sec-fetch-mode", "navigate");
+      headers.put("referer", "https://www.carrefour.com.br/");
+      headers.put("cookie", "statusCepConsultation=true; cepConsultation=04702-000; sideStoreOn=true; "
+            + "selectedPointOfServices=BRA124%2CBRA084%2CBRA007%2CBRA001%2CBRA300%2CBRA302%2CBRA303%2CBRA306%2CBRA307%2CBRA500%2CBRA313%2CBRA315%2Cbra317%2CBRA311%2CBRA312%2CBRADS;");
+
+      Request request = RequestBuilder.create().setUrl(url).setHeaders(headers).build();
+      return this.dataFetcher.get(session, request).getBody();
+   }
+}
---
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/saopaulo/SaopauloCarrefourtieteCrawler.java
@@ -0,0 +1,38 @@
+package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
+
+import java.util.HashMap;
+import java.util.Map;
+import br.com.lett.crawlernode.core.fetcher.FetchMode;
+import br.com.lett.crawlernode.core.fetcher.models.Request;
+import br.com.lett.crawlernode.core.fetcher.models.Request.RequestBuilder;
+import br.com.lett.crawlernode.core.session.Session;
+
+/**
+ * 18/04/2020
+ * 
+ * @author Fabrício
+ *
+ */
+public class SaopauloCarrefourtieteCrawler extends BrasilCarrefourCrawler {
+
+   public SaopauloCarrefourtieteCrawler(Session session) {
+      super(session);
+      super.fetchMode = FetchMode.FETCHER;
+   }
+
+   @Override
+   protected String fetchPage(String url) {
+      Map<String, String> headers = new HashMap<>();
+      headers.put("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+      headers.put("accept-language", "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7,es;q=0.6");
+      headers.put("sec-fetch-user", "?1");
+      headers.put("sec-fetch-site", "none");
+      headers.put("sec-fetch-mode", "navigate");
+      headers.put("referer", "https://www.carrefour.com.br/");
+      headers.put("cookie", "statusCepConsultation=true; cepConsultation=02063-000; sideStoreOn=true; "
+            + "selectedPointOfServices=BRA056%2CBRA011%2CBRA012%2CBRA008%2CBRA304%2CBRA316%2CBRA318%2CBRA309%2CBRA008;");
+
+      Request request = RequestBuilder.create().setUrl(url).setHeaders(headers).build();
+      return this.dataFetcher.get(session, request).getBody();
+   }
+}
