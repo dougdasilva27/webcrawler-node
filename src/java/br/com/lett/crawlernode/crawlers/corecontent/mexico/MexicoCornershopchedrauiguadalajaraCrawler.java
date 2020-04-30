393b8ed85cc8bc48e6402e33448b6f53b319a63f
Gabriel Dornelas <gabrieldornelas@lett.digital>
2020-04-30 09:41:58 -0300

    missing crawlers of cornershop

---
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/mexico/MexicoCornershopchedrauiguadalajaraCrawler.java
@@ -0,0 +1,18 @@
+package br.com.lett.crawlernode.crawlers.corecontent.mexico;
+
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.crawlers.corecontent.extractionutils.CornershopCrawler;
+
+public class MexicoCornershopchedrauiguadalajaraCrawler extends CornershopCrawler {
+
+   public MexicoCornershopchedrauiguadalajaraCrawler(Session session) {
+      super(session);
+   }
+
+   public static final String STORE_ID = "165";
+
+   @Override
+   protected String getStoreId() {
+      return STORE_ID;
+   }
+}
