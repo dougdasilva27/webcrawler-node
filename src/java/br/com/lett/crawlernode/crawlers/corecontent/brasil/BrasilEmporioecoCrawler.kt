c9487ce2dcd3a812b8955e2f21a8c280e62ab307
Charles Fonseca <charlesjrfonseca@gmail.com>
2020-05-06 14:27:36 -0300

    ident to Java-like (2 space), elvis null check

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilEmporioecoCrawler.kt
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilEmporioecoCrawler.kt
@@ -14,95 +14,104 @@ import org.json.JSONObject
 import org.jsoup.nodes.Document
 
 class BrasilEmporioecoCrawler(session: Session?) : Crawler(session) {
-    override fun extractInformation(document: Document): MutableList<Product> {
-        val products = mutableListOf<Product>()
+  override fun extractInformation(document: Document): MutableList<Product> {
+    val products = mutableListOf<Product>()
 
-        if (document.selectFirst(".row.la-single-product-page") == null) {
-            Logging.printLogDebug(logger, session, "Not a product page: ${session.originalURL}")
-            return products
-        }
-
-        val builder = ProductBuilder.create()
-                .setUrl(session.originalURL)
-
-        val json = CrawlerUtils.stringToJson(
-                document.selectFirst("script[type='application/ld+json']").html())
-
-        json?.let {
-            builder.apply {
-                setName(it.optString("name"))
-                setInternalId(it.optString("sku"))
-                setInternalPid(it.optString("sku"))
-                setDescription(document.selectFirst("#tab-description").html())
-                setCategories(document.select(".la-breadcrumb-item")
-                        ?.toCategories(arrayOf(0, 1), arrayOf("/")))
-                setPrimaryImage(it.optString("image"))
-                setSecondaryImages(document.select(".woocommerce-product-gallery__wrapper a")
-                        ?.toSecondaryImagesBy(ignoreIndex = arrayOf(0)))
-            }
-
-            for (jsonOffer in json.optJSONArray("offers")) {
-                if (jsonOffer is JSONObject) {
-                    val productBuilder = builder
-                            .setOffers(scrapOffers(document, jsonOffer))
-
-                    if (document.selectFirst(".variations_form.cart") != null) {
-                        val variations = scrapVariations(document, productBuilder)
-
-                        products.addAll(variations)
-                    } else {
-                        products.add(productBuilder
-                                .build())
-                    }
-                }
-            }
-        }
-        return products
+    if (document.selectFirst(".row.la-single-product-page") == null) {
+      Logging.printLogDebug(logger, session, "Not a product page: ${session.originalURL}")
+      return products
     }
 
-    private fun scrapVariations(document: Document, productBuilder: ProductBuilder): Collection<Product> {
-        val products = mutableListOf<Product>()
+    val builder = ProductBuilder.create()
+      .setUrl(session.originalURL)
 
-        val jsonArray = document.selectFirst(".variations_form.cart").attr("data-product_variations").toJsonArray()
+    val json = CrawlerUtils.stringToJson(
+      document.selectFirst("script[type='application/ld+json']")?.html()
+    )
 
-        val name = document.selectFirst(".product_title.entry-title").text()
-        document.select("li.variable-item").forEachIndexed { index, element ->
-            val jsonObject = jsonArray.optJSONObject(index)
+    json?.let {
+      builder.apply {
+        setName(it.optString("name"))
+        setInternalId(it.optString("sku"))
+        setInternalPid(it.optString("sku"))
+        setDescription(document.selectFirst("#tab-description")?.html())
+        setCategories(
+          document.select(".la-breadcrumb-item")
+            ?.toCategories(arrayOf(0, 1), arrayOf("/"))
+        )
+        setPrimaryImage(it.optString("image"))
+        setSecondaryImages(
+          document.select(".woocommerce-product-gallery__wrapper a")
+            ?.toSecondaryImagesBy(ignoreIndex = arrayOf(0))
+        )
+      }
 
-            val product = productBuilder
-                    .setInternalId(jsonObject.optString("variation_id"))
-                    .setName("$name ${element.selectFirst("span").text()}")
-                    .build()
+      for (jsonOffer in json.optJSONArray("offers")) {
+        if (jsonOffer is JSONObject) {
+          val productBuilder = builder.setOffers(scrapOffers(document, jsonOffer))
 
-            products.add(product)
+          if (document.selectFirst(".variations_form.cart") != null) {
+            val variations = scrapVariations(document, productBuilder)
+
+            products.addAll(variations)
+          } else {
+            products.add(productBuilder.build())
+          }
         }
+      }
+    }
+    return products
+  }
 
-        return products
+  private fun scrapVariations(document: Document, productBuilder: ProductBuilder): Collection<Product> {
+    val products = mutableListOf<Product>()
+
+    val jsonArray = document.selectFirst(".variations_form.cart")?.attr("data-product_variations")?.toJsonArray()
+
+    val name = document.selectFirst(".product_title.entry-title")?.text()
+    document.select("li.variable-item").forEachIndexed { index, element ->
+      val jsonObject = jsonArray?.optJSONObject(index)
+
+      val product = productBuilder
+        .setInternalId(jsonObject?.optString("variation_id"))
+        .setName("$name ${element.selectFirst("span")?.text()}")
+        .build()
+
+      products.add(product)
     }
 
-    private fun scrapOffers(doc: Document, json: JSONObject): Offers {
-        val offers = Offers()
+    return products
+  }
 
-        val price = json.optDouble("price")
-        val priceFrom = doc.selectFirst(".price .woocommerce-Price-amount.amount").toDoubleComma()
+  private fun scrapOffers(doc: Document, json: JSONObject): Offers {
+    val offers = Offers()
 
-        val creditCards = listOf(MASTERCARD, VISA, ELO, AMEX, HIPER, HIPERCARD).toCreditCards(price)
+    val price = json.optDouble("price")
+    val priceFrom = doc.selectFirst(".price .woocommerce-Price-amount.amount")?.toDoubleComma()
 
-        offers.add(OfferBuilder.create()
-                .setIsBuybox(false)
-                .setPricing(PricingBuilder.create()
-                        .setSpotlightPrice(price)
-                        .setPriceFrom(if ((priceFrom != 0.0) and (priceFrom != price)) priceFrom else null)
-                        .setBankSlip(BankSlipBuilder.create()
-                                .setFinalPrice(price)
-                                .build())
-                        .setCreditCards(creditCards)
-                        .build())
-                .setSellerFullName(json.optJSONObject("seller")?.optString("name", null))
-                .setIsMainRetailer(true)
-                .setUseSlugNameAsInternalSellerId(true)
-                .build())
+    val creditCards = listOf(MASTERCARD, VISA, ELO, AMEX, HIPER, HIPERCARD).toCreditCards(price)
 
-        return offers
-    }
+    offers.add(
+      OfferBuilder.create()
+        .setIsBuybox(false)
+        .setPricing(
+          PricingBuilder.create()
+            .setSpotlightPrice(price)
+            .setPriceFrom(if ((priceFrom != 0.0) and (priceFrom != price)) priceFrom else null)
+            .setBankSlip(
+              BankSlipBuilder.create()
+                .setFinalPrice(price)
+                .build()
+            )
+            .setCreditCards(creditCards)
+            .build()
+        )
+        .setSellerFullName(json.optJSONObject("seller")?.optString("name", null))
+        .setIsMainRetailer(true)
+        .setUseSlugNameAsInternalSellerId(true)
+        .build()
+    )
+
+    return offers
+  }
 }
--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilEmporioecoCrawler.kt
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilEmporioecoCrawler.kt
@@ -7,24 +7,24 @@ import br.com.lett.crawlernode.util.toDoc
 
 class BrasilEmporioecoCrawler(session: Session) : CrawlerRankingKeywords(session) {
 
-    init {
-        pageSize = 12
-    }
+  init {
+    pageSize = 12
+  }
 
-    override fun extractProductsFromCurrentPage() {
-        val request = RequestBuilder.create().setUrl("https://lojaemporioeco.com.br/page/$currentPage/?s=$keywordEncoded&post_type=product")
-                .build()
-        currentDoc = dataFetcher.get(session, request).body.toDoc()
+  override fun extractProductsFromCurrentPage() {
+    val request = RequestBuilder.create().setUrl("https://lojaemporioeco.com.br/page/$currentPage/?s=$keywordEncoded&post_type=product")
+      .build()
+    currentDoc = dataFetcher.get(session, request).body?.toDoc()
 
-        currentDoc.select(".product_item--thumbnail .wrap-addto > a").forEach {
-            val internalPid = it.attr("data-product_sku")
-            val url = it.attr("href")
-            saveDataProduct(null, internalPid, url)
-            log("Position: $position - InternalPid: $internalPid - Url: $url")
-        }
+    currentDoc.select(".product_item--thumbnail .wrap-addto > a").forEach {
+      val internalPid = it.attr("data-product_sku")
+      val url = it.attr("href")
+      saveDataProduct(null, internalPid, url)
+      log("Position: $position - InternalPid: $internalPid - Url: $url")
     }
+  }
 
-    override fun hasNextPage(): Boolean {
-        return currentDoc.selectFirst(".next.page-numbers") != null
-    }
+  override fun hasNextPage(): Boolean {
+    return currentDoc.selectFirst(".next.page-numbers") != null
+  }
 }
