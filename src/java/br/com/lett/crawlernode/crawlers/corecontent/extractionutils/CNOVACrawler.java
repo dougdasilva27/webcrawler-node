eac4b27aebcc559bee2e2f55560f11b2b4bbd5af
Gabriel Dornelas <gabrieldornelas@lett.digital>
2020-05-21 16:16:11 -0300

    using apache on cnova

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/extractionutils/CNOVACrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/extractionutils/CNOVACrawler.java
@@ -116,7 +116,7 @@ public abstract class CNOVACrawler extends Crawler {
 
    public CNOVACrawler(Session session) {
       super(session);
-      super.config.setFetcher(FetchMode.FETCHER);
+      super.config.setFetcher(FetchMode.APACHE);
       super.config.setMustSendRatingToKinesis(true);
    }
 
