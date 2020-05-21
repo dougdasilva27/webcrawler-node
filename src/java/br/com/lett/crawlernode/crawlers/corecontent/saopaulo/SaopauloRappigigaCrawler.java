8dc9ce9af275261a81b63d2dd968ff7c6141acdc
Charles Fonseca <charlesjrfonseca@gmail.com>
2020-05-21 13:16:07 -0300

    rappi giga

---
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/saopaulo/SaopauloRappigigaCrawler.java
@@ -0,0 +1,23 @@
+package br.com.lett.crawlernode.crawlers.corecontent.saopaulo;
+
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.crawlers.corecontent.extractionutils.BrasilRappiCrawler;
+
+public class SaopauloRappigigaCrawler extends BrasilRappiCrawler {
+  private static final String STORE_TYPE = "giga";
+  private static final String LOCATION = "lat=-23.5078641&lng=-46.68407819999999";
+
+  public SaopauloRappigigaCrawler(Session session) {
+    super(session);
+  }
+
+  @Override
+  protected String setStoreType() {
+    return STORE_TYPE;
+  }
+
+  @Override
+  protected String setLocationParameters() {
+    return LOCATION;
+  }
+}
