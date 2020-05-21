62b9237749b5ffcf11c481a7ca2dd50b9b96b5e9
Gabriel Dornelas <gabrieldornelas@lett.digital>
2020-05-21 16:17:35 -0300

    extra with fetcher

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/saopaulo/SaopauloExtramarketplaceCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/saopaulo/SaopauloExtramarketplaceCrawler.java
@@ -1,18 +1,20 @@
 package br.com.lett.crawlernode.crawlers.corecontent.saopaulo;
 
+import br.com.lett.crawlernode.core.fetcher.FetchMode;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.crawlers.corecontent.extractionutils.CNOVACrawler;
 
 public class SaopauloExtramarketplaceCrawler extends CNOVACrawler {
 
-  private static final String MAIN_SELLER_NAME_LOWER = "extra";
-  private static final String MAIN_SELLER_NAME_LOWER_2 = "extra.com.br";
-  private static final String HOST = "www.extra.com.br";
+   private static final String MAIN_SELLER_NAME_LOWER = "extra";
+   private static final String MAIN_SELLER_NAME_LOWER_2 = "extra.com.br";
+   private static final String HOST = "www.extra.com.br";
 
-  public SaopauloExtramarketplaceCrawler(Session session) {
-    super(session);
-    super.mainSellerNameLower = MAIN_SELLER_NAME_LOWER;
-    super.mainSellerNameLower2 = MAIN_SELLER_NAME_LOWER_2;
-    super.marketHost = HOST;
-  }
+   public SaopauloExtramarketplaceCrawler(Session session) {
+      super(session);
+      super.config.setFetcher(FetchMode.FETCHER);
+      super.mainSellerNameLower = MAIN_SELLER_NAME_LOWER;
+      super.mainSellerNameLower2 = MAIN_SELLER_NAME_LOWER_2;
+      super.marketHost = HOST;
+   }
 }
