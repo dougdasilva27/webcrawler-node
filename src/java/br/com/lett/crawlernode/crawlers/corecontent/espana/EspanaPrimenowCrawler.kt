a5f48a3b414cb216ae9b0e5c00e95c6c19cc1d3e
Charles Fonseca <charlesjrfonseca@gmail.com>
2020-06-18 11:45:09 -0300

    Prime now espana fixes

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/espana/EspanaPrimenowCrawler.kt
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/espana/EspanaPrimenowCrawler.kt
@@ -1,6 +1,8 @@
 package br.com.lett.crawlernode.crawlers.corecontent.espana
 
 import br.com.lett.crawlernode.core.fetcher.FetchMode
+import br.com.lett.crawlernode.core.fetcher.FetchUtilities
+import br.com.lett.crawlernode.core.fetcher.ProxyCollection
 import br.com.lett.crawlernode.core.fetcher.models.Request.RequestBuilder
 import br.com.lett.crawlernode.core.fetcher.models.Response
 import br.com.lett.crawlernode.core.models.Product
@@ -11,49 +13,42 @@ import br.com.lett.crawlernode.util.*
 import models.Offer.OfferBuilder
 import models.Offers
 import models.pricing.Pricing
-import org.json.JSONArray
-import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
-import br.com.lett.crawlernode.core.fetcher.FetchUtilities
 import org.apache.http.HttpHeaders
-import br.com.lett.crawlernode.core.fetcher.ProxyCollection
-import br.com.lett.crawlernode.core.fetcher.models.FetcherOptions.FetcherOptionsBuilder
-import java.util.Arrays
-import java.util.HashMap
+import org.json.JSONArray
+import org.jsoup.nodes.Document
 
 class EspanaPrimenowCrawler(session: Session) : Crawler(session) {
 
   private val cep = "28010"
 
-	private val USER_AGENT = FetchUtilities.randUserAgent();
-	
+  private val userAgent = FetchUtilities.randUserAgent();
+
   init {
-    super.config.setFetcher(FetchMode.FETCHER);
+    super.config.fetcher = FetchMode.FETCHER;
   }
 
   override fun handleCookiesBeforeFetch() {
-	  val headers = HashMap<String, String>()
-	  headers.put(HttpHeaders.USER_AGENT, USER_AGENT);
-//	  val headers: Map<String,String> = mapOf(HttpHeaders.USER_AGENT to USER_AGENT);
-	  
-    val requestOn: (String, Boolean) -> Response = { url: String, bodyIsrequired: Boolean ->
+    val headers = mutableMapOf(HttpHeaders.USER_AGENT to userAgent);
+
+    val requestOn: (String, Boolean) -> Response = { url, bodyIsrequired ->
       val resp = dataFetcher.get(
         session, RequestBuilder.create()
           .setUrl("https://primenow.amazon.es/$url")
           .setCookies(cookies)
-				  .setHeaders(headers)
-				  .setBodyIsRequired(bodyIsrequired)
-				  .setProxyservice(
-                     listOf(
-                           ProxyCollection.INFATICA_RESIDENTIAL_BR,
-                           ProxyCollection.STORM_RESIDENTIAL_EU,
-                           ProxyCollection.STORM_RESIDENTIAL_US))
-               .setFetcheroptions(FetcherOptionsBuilder.create().setForbiddenCssSelector("#captchacharacters").build())
+          .setHeaders(headers)
+          .setBodyIsRequired(bodyIsrequired)
+          .setProxyservice(
+            listOf(
+              ProxyCollection.INFATICA_RESIDENTIAL_BR,
+              ProxyCollection.STORM_RESIDENTIAL_EU,
+              ProxyCollection.STORM_RESIDENTIAL_US
+            )
+          )
           .build()
       )
       if (resp.cookies.size > 0) {
-         cookies = resp.cookies
-	 	  }
+        cookies = resp.cookies
+      }
       resp
     }
 
@@ -65,12 +60,6 @@ class EspanaPrimenowCrawler(session: Session) : Crawler(session) {
     requestOn("cart/initiatePostalCodeUpdate?newPostalCode=$cep&allCartItemsSwappableUrl=%2Fhome&noCartUpdateRequiredUrl=%2Fhome&someCartItemsUnswappableUrl=%2Fhome&offer-swapping-token=$token", false)
   }
 
-  override fun fetch(): Any {
-    val request = RequestBuilder.create().setCookies(cookies).setUrl(session.originalURL).build()
-    val response = dataFetcher[session, request]
-    return Jsoup.parse(response.body)
-  }
-
   override fun extractInformation(document: Document): MutableList<Product> {
     val products = mutableListOf<Product>()
 
@@ -78,11 +67,11 @@ class EspanaPrimenowCrawler(session: Session) : Crawler(session) {
 
       val name = document.selectFirst("#productTitle")?.text()?.trim()
       val description = document.selectFirst("#productDescription_feature_div")?.html()
-      val primaryImg = document.selectFirst("#landingImage")?.attr("src")
+      val primaryImg = document.selectFirst("#landingImage")?.attr("data-old-hires")
       val secondaryImgs = JSONArray()
       document.select(".a-spacing-small.item img")?.eachAttr("src", arrayOf(0))
         ?.forEach { img -> secondaryImgs.put(img.replaceFirst("38,50", "640,480")) }
-      val internalId = session.originalURL.substringAfter("dp/")
+      val internalId = session.originalURL.substringAfter("dp/").substringBefore("?")
 
       val offers = scrapOffers(document)
 
@@ -116,7 +105,7 @@ class EspanaPrimenowCrawler(session: Session) : Crawler(session) {
       .setPricing(pricing)
       .setSellerFullName(document.selectFirst("#merchant-info")?.text()?.trim()?.substringAfter("por "))
       .setIsBuybox(false)
-			.setIsMainRetailer(true)
+      .setIsMainRetailer(true)
       .build()
 
     offers.add(offer)
