d8c411fe8bef5a10045ab909c6ff2f0207efc5a8
Charles Fonseca <charlesjrfonseca@gmail.com>
2020-07-02 15:04:57 -0300

    Paraiso Saudavel first commit

---
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/saopaulo/SaopauloParaisosaudavelCrawler.kt
@@ -0,0 +1,12 @@
+package br.com.lett.crawlernode.crawlers.ranking.keywords.saopaulo
+
+import br.com.lett.crawlernode.core.session.Session
+import br.com.lett.crawlernode.crawlers.ranking.keywords.extractionutils.TrayCommerceCrawler
+
+
+class SaopauloParaisosaudavelCrawler(session: Session) : TrayCommerceCrawler(session) {
+
+  override fun setStoreId(): String = "738124"
+
+  override fun setHomePage(): String = "https://www.paraisosaudavel.com.br"
+}
