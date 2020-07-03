72c6525059498a7fe015852477353897e48515ec
Charles Fonseca <charlesjrfonseca@gmail.com>
2020-07-03 18:00:39 -0300

    DSL updates

--- src/java/br/com/lett/crawlernode/util/Dsl.kt
+++ src/java/br/com/lett/crawlernode/util/Dsl.kt
@@ -7,12 +7,15 @@ import models.Offers
 import models.RatingsReviews
 import models.pricing.Pricing
 
+fun buildProduct(builder: ProductBuilderDsl.() -> Unit): Product {
+  return ProductBuilderDsl().invoke(builder)
+}
+
 @DslMarker
 annotation class ProductDsl
 
 @ProductDsl
 class ProductBuilderDsl {
-
   lateinit var url: String
   lateinit var internalId: String
   var internalPid: String? = null
@@ -23,73 +26,75 @@ class ProductBuilderDsl {
   var description: String? = null
   var stock: Int? = null
   var eans: MutableList<String>? = null
-  var offers: Offers? = null
+  private var offers = Offers()
   var ratingReviews: RatingsReviews? = null
   private val productBuilder = ProductBuilder()
 
-
-  inline fun buildOffer(builder: OfferBuilderDsl.() -> Unit) {
-
+  operator fun invoke(initializer: ProductBuilderDsl.() -> Unit): Product {
+    initializer()
+    return productBuilder
+      .setUrl(url)
+      .setInternalId(internalId)
+      .setInternalPid(internalPid)
+      .setName(name)
+      .setCategories(categories)
+      .setPrimaryImage(primaryImage)
+      .setSecondaryImages(secondaryImages)
+      .setDescription(description)
+      .setStock(stock)
+      .setEans(eans)
+      .setOffers(offers)
+      .setRatingReviews(ratingReviews)
+      .build()
   }
 
-  operator fun invoke(): Product = productBuilder
-    .setUrl(url)
-    .setInternalId(internalId)
-    .setInternalPid(internalPid)
-    .setName(name)
-    .setCategories(categories)
-    .setPrimaryImage(primaryImage)
-    .setSecondaryImages(secondaryImages)
-    .setDescription(description)
-    .setStock(stock)
-    .setEans(eans)
-    .setOffers(offers)
-    .setRatingReviews(ratingReviews)
-    .build()
+  fun offer(block: OfferBuilderDsl.() -> Unit) {
+    val offerBuilder = OfferBuilderDsl()
+    offers.add(offerBuilder.build(block))
+  }
 }
 
-@ProductDsl
 class OfferBuilderDsl {
   private val offerBuilder = Offer.OfferBuilder()
+
+  private var buybox: Boolean? = false
+  private var mainRetailer: Boolean = false
+  private var slugNameAsInternalSellerId = false
   var sellerFullName: String? = null
   var internalSellerId: String? = null
   var mainPagePosition: Int? = null
   var sellersPagePosition: Int? = null
   var sellerType: String? = null
-  var isBuybox: Boolean? = null
   var sales: List<String>? = null
   var pricing: Pricing? = null
-  var isMainRetailer: Boolean? = null
-  var useSlugNameAsInternalSellerId = false
 
-
-  operator fun invoke(): Offer = offerBuilder.setPricing(pricing)
-    .setIsBuybox(isBuybox)
-    .setSellerFullName(sellerFullName)
-    .setSellersPagePosition(sellersPagePosition)
-    .setUseSlugNameAsInternalSellerId(useSlugNameAsInternalSellerId)
-    .setIsMainRetailer(isMainRetailer)
-    .setSales(sales)
-    .setSellerType(sellerType)
-    .build()
-
-}
-
-inline fun buildProduct(builder: ProductBuilderDsl.() -> Unit): Product {
-  val productBuilder = ProductBuilderDsl()
-
-  productBuilder.builder()
-  return productBuilder.invoke()
-}
-
-fun test() {
-  String.let { }
-  val p = buildProduct {
-    primaryImage = ""
-
-    buildOffer{
-
+  val useSlugNameAsInternalSellerId: Boolean
+    get() {
+      slugNameAsInternalSellerId = true
+      return true
+    }
+  val isBuybox: Boolean
+    get() {
+      buybox = true
+      return true
+    }
+  val isMainRetailer: Boolean
+    get() {
+      mainRetailer = true
+      return true
     }
 
+  internal fun build(initializer: OfferBuilderDsl.() -> Unit): Offer {
+    initializer()
+
+    return offerBuilder.setPricing(pricing)
+      .setIsBuybox(buybox)
+      .setSellerFullName(sellerFullName)
+      .setSellersPagePosition(sellersPagePosition)
+      .setUseSlugNameAsInternalSellerId(slugNameAsInternalSellerId)
+      .setIsMainRetailer(mainRetailer)
+      .setSales(sales)
+      .setSellerType(sellerType)
+      .build()
   }
 }
