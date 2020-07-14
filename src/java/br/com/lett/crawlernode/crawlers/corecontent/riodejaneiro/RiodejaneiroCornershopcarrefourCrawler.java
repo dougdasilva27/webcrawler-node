9d11ec9e39fa5185ffdb4a84f4885422aad93e87
Gabriel Dornelas <gabrieldornelas@lett.digital>
2020-07-14 09:27:01 -0300

    cornershop carrefour

---
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/riodejaneiro/RiodejaneiroCornershopcarrefourCrawler.java
@@ -0,0 +1,18 @@
+package br.com.lett.crawlernode.crawlers.corecontent.riodejaneiro;
+
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.crawlers.corecontent.extractionutils.CornershopCrawler;
+
+public class RiodejaneiroCornershopcarrefourCrawler extends CornershopCrawler {
+
+   public RiodejaneiroCornershopcarrefourCrawler(Session session) {
+      super(session);
+   }
+
+   public static final String STORE_ID = "7484";
+
+   @Override
+   protected String getStoreId() {
+      return STORE_ID;
+   }
+}
