44d8e06e15a44fa4c914c557319329c77ba5ed75
marcosmourabro <marcosmourabro@gmail.com>
2020-07-15 15:24:46 -0300

    Ifood Big Pacaembu
    Ifood extract Utils
    Corecontent

---
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/saopaulo/SaopauloIfoodbigpacaembuCrawler.java
@@ -0,0 +1,30 @@
+package br.com.lett.crawlernode.crawlers.corecontent.saopaulo;
+
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.crawlers.corecontent.extractionutils.BrasilIfood;
+
+public class SaopauloIfoodbigpacaembuCrawler extends BrasilIfood {
+
+  public static final String region = "sao-paulo-sp";
+  public static final String store_name = "big-pacaembu---express-bom-retiro";
+  public static final String seller_full_name = "Big Pacaembu";
+
+  public SaopauloIfoodbigpacaembuCrawler(Session session) {
+    super(session);
+  }
+
+  @Override protected String getRegion() {
+    return region;
+  }
+
+  @Override protected String getStore_name() {
+    return store_name;
+  }
+
+  @Override protected String getSellerFullName() {
+    return seller_full_name;
+  }
+
+
+}
+
