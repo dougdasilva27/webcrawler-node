25ffbef0b0ac873b4ee02de99011f88232123302
Igor Viana <igorviana@lett.digital>
2020-07-17 18:08:19 -0300

    Attempt to fix bug for keyword incrivel

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/ribeiraopreto/RibeiraopretoSavegnagoCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/ribeiraopreto/RibeiraopretoSavegnagoCrawler.java
@@ -1,128 +1,83 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.ribeiraopreto;
 
+import br.com.lett.crawlernode.core.fetcher.models.Request;
+import br.com.lett.crawlernode.core.fetcher.models.Response;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
-import br.com.lett.crawlernode.util.CommonMethods;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RibeiraopretoSavegnagoCrawler extends CrawlerRankingKeywords {
 
-  public RibeiraopretoSavegnagoCrawler(Session session) {
-    super(session);
-  }
+    public RibeiraopretoSavegnagoCrawler(Session session) {
+        super(session);
+    }
 
-  /**
-   * Código das cidades: Ribeirão Preto - 2 Sertãozinho - 6 Jardianópolis - 11 Jaboticabal - 7
-   * Franca - 3 Barretos - 10 Bebedouro - 9 Monte Alto - 12 Araraquara - 4 São carlos - 5 Matão - 8
-   */
-  // private static final int cityCode = ControllerKeywords.codeCity;
-  private static final int cityCode = 2;
+    /**
+     * Código das cidades: Ribeirão Preto - 2 Sertãozinho - 6 Jardianópolis - 11 Jaboticabal - 7
+     * Franca - 3 Barretos - 10 Bebedouro - 9 Monte Alto - 12 Araraquara - 4 São carlos - 5 Matão - 8
+     */
+    // private static final int cityCode = ControllerKeywords.codeCity;
+    private static final int cityCode = 2;
+    private static final String API_KEY = "savegnago";
+    private static final int SALES_CHANNEL = 1;
 
-  @Override
-  protected void extractProductsFromCurrentPage() {
-    // número de produtos por página do market
-    this.pageSize = 20;
+    @Override
+    protected void extractProductsFromCurrentPage() {
+        this.pageSize = 32;
+        this.log("Página " + this.currentPage);
 
-    this.log("Página " + this.currentPage);
+        JSONObject json = loadJson();
+        JSONArray products = json.optJSONArray("products");
 
-    // monta a url com a keyword e a página
-    String url = "http://busca.savegnago.com.br/busca?q=" + this.keywordEncoded + "&sc=" + cityCode
-        + "&page=" + this.currentPage;
-    this.log("Link onde são feitos os crawlers: " + url);
+        if (products != null && !products.isEmpty()) {
+            if (totalProducts == 0) {
+                setTotalProducts(json);
+            }
+            for (Object productObj : products) {
+                JSONObject product = (JSONObject) productObj;
+                String internalId = product.optString("id");
+                String internalPid = internalId;
+                String productUrl = product.optString("url").replace("//", "");
+                saveDataProduct(internalId, internalPid, productUrl);
 
-    // chama função de pegar a url
-    this.currentDoc = fetchDocument(url);
-
-    Elements products = this.currentDoc.select("li.nm-product-item");
-
-    // se obter 1 ou mais links de produtos e essa página tiver resultado faça:
-    if (products.size() >= 1) {
-      // se o total de busca não foi setado ainda, chama a função para setar
-      if (this.totalProducts == 0) {
-        setTotalProducts();
-      }
-
-      for (Element e : products) {
-        // InternalPid
-        String internalPid = crawlInternalPid(e);
-
-        // InternalId
-        String internalId = crawlInternalId(e);
-
-        // Url do produto
-        String productUrl = crawlProductUrl(e);
-
-        saveDataProduct(internalId, internalPid, productUrl);
-
-        this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: "
-            + internalPid + " - Url: " + productUrl);
-        if (this.arrayProducts.size() == productsLimit) {
-          break;
+                log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + internalPid + " - Url: " + productUrl);
+                if (arrayProducts.size() == productsLimit) {
+                    break;
+                }
+            }
+        } else {
+            result = false;
+            log("Keyword sem resultado!");
         }
-
-      }
-    } else {
-      this.result = false;
-      this.log("Keyword sem resultado!");
+        log("Finalizando Crawler de produtos da página $currentPage até agora ${arrayProducts.size} produtos crawleados");
     }
 
-    this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora "
-        + this.arrayProducts.size() + " produtos crawleados");
-  }
-
-  @Override
-  protected boolean hasNextPage() {
-    return arrayProducts.size() < this.totalProducts;
-  }
-
-  @Override
-  protected void setTotalProducts() {
-    Element totalElement = this.currentDoc.select("span.nm-total-products").first();
-
-    try {
-      if (totalElement != null) {
-        this.totalProducts = Integer.parseInt(totalElement.text());
-      }
-    } catch (Exception e) {
-      this.logError(CommonMethods.getStackTrace(e));
+    private void setTotalProducts(JSONObject json) {
+        this.totalProducts = json.optInt("size", 0);
+        this.log("Total de produtos: " + this.totalProducts);
     }
 
-    this.log("Total da busca: " + this.totalProducts);
-  }
+    private JSONObject loadJson() {
+        String url = "https://api.linximpulse.com/engage/search/v3/search/?salesChannel=" + SALES_CHANNEL + "&terms=" + this.keywordEncoded + "&resultsPerPage=32&page=" + this.currentPage + "&apiKey=" + API_KEY;
 
-  private String crawlInternalId(Element e) {
-    return null;
-  }
+        this.log("Link onde são feitos os crawlers: " + url);
 
-  private String crawlInternalPid(Element e) {
-    return e.attr("data-sku");
-  }
+        Map<String, String> headers = new HashMap<>();
+        headers.put("origin", "https://www.savegnago.com.br");
+        headers.put("content-type:", "application/json");
 
-  private String crawlProductUrl(Element e) {
-    String productUrl = null;
-    Element urlElement = e.select("h2 a").first();
-
-    if (urlElement != null) {
-      productUrl = urlElement.attr("href");
-
-      if (!productUrl.startsWith("http")) {
-        productUrl = "https:" + productUrl;
-      }
-
-      if (!productUrl.contains("sc=") && productUrl.contains("?")) {
-        productUrl += "&sc=" + cityCode;
-      } else if (!productUrl.contains("sc=")) {
-        productUrl += "?sc=" + cityCode;
-      } else {
-        int x = productUrl.lastIndexOf("sc=") + 3;
-        String cityCodeDefault = productUrl.substring(x).split("")[0];
-
-        productUrl = productUrl.replace("sc=" + cityCodeDefault, "sc=" + cityCode);
-      }
+        Request request = Request.RequestBuilder.create().setUrl(url)
+                .setCookies(cookies)
+                .setHeaders(headers)
+                .mustSendContentEncoding(false)
+                .build();
+        Response response = this.dataFetcher.get(session, request);
+        return CrawlerUtils.stringToJson(response.getBody());
     }
-
-    return productUrl;
-  }
 
 }
