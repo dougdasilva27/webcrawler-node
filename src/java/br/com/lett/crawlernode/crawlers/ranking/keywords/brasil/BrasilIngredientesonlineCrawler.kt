2e1ada50f147f3dd3c413017711db2880ec69582
Layunne <fellypelayunne@gmail.com>
2020-07-23 15:29:06 -0300

    complete ranking

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilIngredientesonlineCrawler.kt
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilIngredientesonlineCrawler.kt
@@ -11,11 +11,32 @@ import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords
  */
 class BrasilIngredientesonlineCrawler(session: Session) : CrawlerRankingKeywords(session) {
 
+    init {
+        pageSize = 18
+    }
+
     override fun extractProductsFromCurrentPage() {
+
         currentDoc = fetchDocument(
                 "https://www.ingredientesonline.com.br/catalogsearch/result/" +
                         "?q=$keywordEncoded" +
                         "&p=$currentPage"
         )
+
+        val items = currentDoc.select(".item .suporte")
+
+        for (it in items) {
+
+            val productUrl = it.selectFirst(".product-image-wrapper a").attr("href")
+            val internalId = it.selectFirst(".bt-add button").attr("data-id")
+
+            saveDataProduct(internalId, null, productUrl)
+            log(">>> ✅ productId: $internalId")
+        }
+    }
+
+    override fun hasNextPage(): Boolean {
+
+        return currentDoc.select(".pager li .i-next").isNotEmpty()
     }
 }
