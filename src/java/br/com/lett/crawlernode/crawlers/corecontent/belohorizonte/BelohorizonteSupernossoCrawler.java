ae84ff265a6217853bbced7fbef7e4c513cec3ce
Igor Viana <igorviana@lett.digital>
2020-07-29 10:15:01 -0300

    Removing comments

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/belohorizonte/BelohorizonteSupernossoCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/belohorizonte/BelohorizonteSupernossoCrawler.java
@@ -49,97 +49,9 @@ public class BelohorizonteSupernossoCrawler extends VTEXOldScraper {
       return MAIN_SELLER_NAME_LOWER;
    }
 
-   /*@Override
-  public boolean shouldVisit() {
-    String href = this.session.getOriginalURL().toLowerCase();
-    return !FILTERS.matcher(href).matches() && (href.startsWith(HOME_PAGE));
-  }
-
-  @Override
-  public List<Product> extractInformation(Document doc) throws Exception {
-    super.extractInformation(doc);
-    List<Product> products = new ArrayList<>();
-
-    if (isProductPage(doc, session.getOriginalURL())) {
-      VTEXCrawlersUtils vtexUtil = new VTEXCrawlersUtils(session, MAIN_SELLER_NAME_LOWER, HOME_PAGE, cookies, dataFetcher);
-
-      JSONObject skuJson = CrawlerUtils.crawlSkuJsonVTEX(doc, session);
-
-      String internalPid = vtexUtil.crawlInternalPid(skuJson);
-      CategoryCollection categories = CrawlerUtils.crawlCategories(doc, ".bread-crumb > ul li a");
-      String description = CrawlerUtils.scrapSimpleDescription(doc, Arrays.asList(".productDescription", ".productSpecification"));
-
-      // sku data in json
-      JSONArray arraySkus = skuJson != null && skuJson.has("skus") ? skuJson.getJSONArray("skus") : new JSONArray();
-
-      // ean data in json
-      JSONArray arrayEans = CrawlerUtils.scrapEanFromVTEX(doc);
-
-      for (int i = 0; i < arraySkus.length(); i++) {
-        JSONObject jsonSku = arraySkus.getJSONObject(i);
-
-        String internalId = vtexUtil.crawlInternalId(jsonSku);
-        JSONObject apiJSON = vtexUtil.crawlApi(internalId);
-        String name = vtexUtil.crawlName(jsonSku, skuJson, " ");
-        Map<String, Prices> marketplaceMap = vtexUtil.crawlMarketplace(apiJSON, internalId, true);
-        List<String> sellersPacheco = CrawlerUtils.getMainSellers(marketplaceMap, Arrays.asList(MAIN_SELLER_NAME_LOWER));
-        Marketplace marketplace = CrawlerUtils.assembleMarketplaceFromMap(marketplaceMap, sellersPacheco, Arrays.asList(Card.VISA), session);
-        boolean available = CrawlerUtils.getAvailabilityFromMarketplaceMap(marketplaceMap, sellersPacheco);
-        String primaryImage = vtexUtil.crawlPrimaryImage(apiJSON);
-        String secondaryImages = vtexUtil.crawlSecondaryImages(apiJSON);
-        Prices prices = CrawlerUtils.getPrices(marketplaceMap, sellersPacheco);
-        Float price = CrawlerUtils.extractPriceFromPrices(prices, Card.VISA);
-        Integer stock = vtexUtil.crawlStock(apiJSON);
-        String ean = i < arrayEans.length() ? arrayEans.getString(i) : null;
-        RatingsReviews ratingReviews = crawlRating(doc, internalId);
-
-        List<String> eans = new ArrayList<>();
-        eans.add(ean);
-
-        // Creating the product
-        Product product = ProductBuilder.create()
-            .setUrl(session.getOriginalURL())
-            .setInternalId(internalId)
-            .setInternalPid(internalPid)
-            .setName(name)
-            .setPrice(price)
-            .setPrices(prices)
-            .setRatingReviews(ratingReviews)
-            .setAvailable(available)
-            .setCategory1(categories.getCategory(0))
-            .setCategory2(categories.getCategory(1))
-            .setCategory3(categories.getCategory(2))
-            .setPrimaryImage(primaryImage)
-            .setSecondaryImages(secondaryImages)
-            .setDescription(description)
-            .setStock(stock)
-            .setMarketplace(marketplace)
-            .setEans(eans)
-            .build();
-
-        products.add(product);
-      }
-
-    } else {
-      Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
-    }
-
-    return products;
-  }*/
-
    @Override
    protected RatingsReviews scrapRating(String internalId, String internalPid, Document doc, JSONObject jsonSku) {
       return new VtexRatingCrawler(session, HOME_PAGE, logger, cookies)
          .extractRatingAndReviews(internalId, doc, dataFetcher);
    }
-
-   /*private RatingsReviews crawlRating(Document document, String id) {
-    return new VtexRatingCrawler(session, HOME_PAGE, logger, cookies)
-        .extractRatingAndReviews(id, document, dataFetcher);
-  }
-
-
-  private boolean isProductPage(Document document, String url) {
-    return document.selectFirst(".productName") != null && url.startsWith(HOME_PAGE);
-  }*/
 }
