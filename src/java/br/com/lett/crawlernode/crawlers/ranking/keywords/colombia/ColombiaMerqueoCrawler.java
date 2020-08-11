a8c95b7e156294d1820ef9e7a7de59c1debef6ec
marcosmourabro <marcosmourabro@gmail.com>
2020-08-11 09:57:35 -0300

    Colombia Merqueo
    The website's api has been updated and some code changes were necessary.

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/colombia/ColombiaMerqueoCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/colombia/ColombiaMerqueoCrawler.java
@@ -11,100 +11,103 @@ import br.com.lett.crawlernode.util.CrawlerUtils;
 
 public class ColombiaMerqueoCrawler extends CrawlerRankingKeywords {
 
-  public ColombiaMerqueoCrawler(Session session) {
-    super(session);
-  }
+   public ColombiaMerqueoCrawler(Session session) {
+      super(session);
+   }
 
-  @Override
-  protected void extractProductsFromCurrentPage() {
-    this.log("Página " + this.currentPage);
+   @Override
+   protected void extractProductsFromCurrentPage() {
+      this.log("Página " + this.currentPage);
 
-    String url = "https://merqueo.com/api/2.0/stores/63/search?q=" + this.keywordEncoded + "&per_page=15&page=" + this.currentPage;
-    this.log("Link onde são feitos os crawlers: " + url);
+      String url = "https://merqueo.com/api/3.1/stores/63/search?q=+" + this.keywordEncoded + "&page=" + this.currentPage + "&per_page=50";
 
-    JSONObject apiJson = fetchApiProducts(url);
-    JSONArray productsArray = new JSONArray();
+      this.log("Link onde são feitos os crawlers: " + url);
 
-    if (apiJson.has("data") && !apiJson.isNull("data")) {
-      productsArray = apiJson.getJSONArray("data");
-    }
+      JSONObject apiJson = fetchApiProducts(url);
+      JSONArray productsArray = new JSONArray();
 
-    if (productsArray.length() > 0) {
-      if (this.totalProducts == 0) {
-        this.totalProducts = productsArray.length();
+      if (apiJson.has("included") && !apiJson.isNull("included")) {
+         productsArray = apiJson.getJSONArray("included");
+
       }
 
-      for (Object object : productsArray) {
-        JSONObject data = (JSONObject) object;
+      if (productsArray.length() > 0) {
+         if (this.totalProducts == 0) {
+            this.totalProducts = productsArray.length();
+         }
 
-        String internalId = data.has("id") ? data.get("id").toString() : null;
-        String productUrl = assembleProductUrl(data);
-        saveDataProduct(internalId, null, productUrl);
+         for (Object object : productsArray) {
+            JSONObject included = (JSONObject) object;
 
-        this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + null + " - Url: " + productUrl);
-        if (this.arrayProducts.size() == productsLimit) {
-          break;
-        }
+            if (included.optString("type").equals("slug")) {
+
+               String internalId = included.has("id") ? included.get("id").toString() : null;
+               String productUrl = assembleProductUrl(included);
+               saveDataProduct(internalId, null, productUrl);
+
+               this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + null + " - Url: " + productUrl);
+               if (this.arrayProducts.size() == productsLimit) {
+                  break;
+               }
+            }
+         }
+
+      } else {
+         this.result = false;
+         this.log("Keyword sem resultado!");
       }
 
-    } else {
-      this.result = false;
-      this.log("Keyword sem resultado!");
-    }
+      this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
+   }
 
-    this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
-  }
+   /*
+    * Url exemple:
+    * https://merqueo.com/bogota/despensa/condimentos-especias-y-adobos/canela-su-despensa-20-gr
+    */
+   private String assembleProductUrl(JSONObject included) {
+      String productUrl = "";
 
-  /*
-   * Url exemple:
-   * https://merqueo.com/bogota/despensa/condimentos-especias-y-adobos/canela-su-despensa-20-gr
-   */
-  private String assembleProductUrl(JSONObject data) {
-    String productUrl = "";
 
-    if (data.has("slugs") && !data.isNull("slugs")) {
-      JSONObject slugs = data.getJSONObject("slugs");
+      if (included.has("attributes") && !included.isNull("attributes")) {
+         JSONObject attributes = included.getJSONObject("attributes");
 
-      if (slugs.has("data") && !slugs.isNull("data")) {
-        JSONObject dataSlugs = slugs.getJSONObject("data");
+         boolean hasFields = attributes.has("city")
+            && attributes.has("department")
+            && attributes.has("shelf")
+            && attributes.has("product");
 
-        boolean hasFields = dataSlugs.has("city")
-            && dataSlugs.has("department")
-            && dataSlugs.has("shelf")
-            && dataSlugs.has("product");
+         boolean isFieldsNull =
+            !attributes.isNull("city")
+               && !attributes.isNull("department")
+               && !attributes.isNull("shelf")
+               && !attributes.isNull("product");
 
-        boolean isFieldsNull =
-            !dataSlugs.isNull("city")
-                && !dataSlugs.isNull("department")
-                && !dataSlugs.isNull("shelf")
-                && !dataSlugs.isNull("product");
-
-        if (hasFields && isFieldsNull) {
-          productUrl = productUrl
-              .concat("https://merqueo.com/")
-              .concat(dataSlugs.getString("city"))
-              .concat("/")
-              .concat(dataSlugs.getString("department"))
-              .concat("/")
-              .concat(dataSlugs.getString("shelf"))
-              .concat("/")
-              .concat(dataSlugs.getString("product"));
-        }
+         if (hasFields && isFieldsNull) {
+            productUrl = productUrl
+               .concat("https://merqueo.com/")
+               .concat(attributes.getString("city"))
+               .concat("/")
+               .concat(attributes.getString("department"))
+               .concat("/")
+               .concat(attributes.getString("shelf"))
+               .concat("/")
+               .concat(attributes.getString("product"));
+         }
       }
-    }
 
-    return productUrl;
-  }
 
-  private JSONObject fetchApiProducts(String url) {
+      return productUrl;
+   }
 
-    Request request = RequestBuilder
-        .create()
-        .setUrl(url)
-        .mustSendContentEncoding(false)
-        .build();
+   private JSONObject fetchApiProducts(String url) {
 
-    return CrawlerUtils.stringToJson(new FetcherDataFetcher().get(session, request).getBody());
-  }
+      Request request = RequestBuilder
+         .create()
+         .setUrl(url)
+         .mustSendContentEncoding(false)
+         .build();
+
+      return CrawlerUtils.stringToJson(new FetcherDataFetcher().get(session, request).getBody());
+   }
 
 }
