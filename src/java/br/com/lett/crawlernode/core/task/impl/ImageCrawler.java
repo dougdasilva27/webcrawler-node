5353a8d1e30da4c7281fa47a82a0f45d7acc360b
Gabriel Dornelas <gabrieldornelas@lett.digital>
2020-08-19 18:04:32 -0300

    bugfix image muffato

--- src/java/br/com/lett/crawlernode/core/task/impl/ImageCrawler.java
+++ src/java/br/com/lett/crawlernode/core/task/impl/ImageCrawler.java
@@ -317,7 +317,7 @@ public class ImageCrawler extends Task {
          }
       } else if (marketId == 307 || marketId == 27 || marketId == 836) {
          request.setSendContentEncoding(false);
-      } else if (marketId == 976) {
+      } else if (marketId == 976 || marketId == 65) {
          headers.put(HttpHeaders.USER_AGENT, "Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25");
       }
 
