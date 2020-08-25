ecabd7ce450032a5211ecf2fe9083e4f341ed725
Gabriel Dornelas <gabrieldornelas@lett.digital>
2020-08-25 15:53:43 -0300

    bugfix mambo void product

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/saopaulo/SaopauloMamboCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/saopaulo/SaopauloMamboCrawler.java
@@ -1,7 +1,15 @@
 package br.com.lett.crawlernode.crawlers.corecontent.saopaulo;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.jsoup.nodes.Document;
 import br.com.lett.crawlernode.core.fetcher.models.Request;
 import br.com.lett.crawlernode.core.fetcher.models.Request.RequestBuilder;
+import br.com.lett.crawlernode.core.fetcher.models.Response;
 import br.com.lett.crawlernode.core.models.Card;
 import br.com.lett.crawlernode.core.models.CategoryCollection;
 import br.com.lett.crawlernode.core.models.Product;
@@ -9,18 +17,14 @@ import br.com.lett.crawlernode.core.models.ProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.Crawler;
 import br.com.lett.crawlernode.crawlers.corecontent.extractionutils.TrustvoxRatingCrawler;
-import br.com.lett.crawlernode.util.*;
+import br.com.lett.crawlernode.util.CommonMethods;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.JSONUtils;
+import br.com.lett.crawlernode.util.Logging;
+import br.com.lett.crawlernode.util.MathUtils;
 import models.Marketplace;
 import models.RatingsReviews;
 import models.prices.Prices;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.jsoup.nodes.Document;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 /**
  * Date: 30/08/17
@@ -47,19 +51,25 @@ public class SaopauloMamboCrawler extends Crawler {
    @Override
    protected JSONObject fetch() {
       JSONObject api = new JSONObject();
+      Request requestPage = RequestBuilder.create().setUrl(session.getOriginalURL()).setCookies(cookies).build();
+      Response pageResponse = this.dataFetcher.get(session, requestPage);
 
-      String[] tokens = session.getOriginalURL().split("\\?")[0].split("/");
-      String id = CommonMethods.getLast(tokens);
-      String pathName = tokens[tokens.length - 2];
+      if (pageResponse.getLastStatusCode() == 200) {
+         String[] tokens = session.getOriginalURL().split("\\?")[0].split("/");
+         String id = CommonMethods.getLast(tokens);
+         String pathName = tokens[tokens.length - 2];
 
-      String apiUrl =
-            "https://www.mambo.com.br/ccstoreui/v1/pages/" + pathName + "/p/" + id + "?dataOnly=false&cacheableDataOnly=true&productTypesRequired=true";
+         String apiUrl =
+               "https://www.mambo.com.br/ccstoreui/v1/pages/" + pathName + "/p/" + id + "?dataOnly=false&cacheableDataOnly=true&productTypesRequired=true";
 
-      Request request = RequestBuilder.create().setUrl(apiUrl).setCookies(cookies).build();
-      JSONObject response = CrawlerUtils.stringToJson(this.dataFetcher.get(session, request).getBody());
+         Request request = RequestBuilder.create().setUrl(apiUrl).setCookies(cookies).build();
+         JSONObject response = CrawlerUtils.stringToJson(this.dataFetcher.get(session, request).getBody());
 
-      if (response.has("data")) {
-         api = response.getJSONObject("data");
+         if (response.has("data")) {
+            api = response.optJSONObject("data");
+         }
+      } else {
+         Logging.printLogDebug(logger, session, "404 Page: " + this.session.getOriginalURL());
       }
 
       return api;
