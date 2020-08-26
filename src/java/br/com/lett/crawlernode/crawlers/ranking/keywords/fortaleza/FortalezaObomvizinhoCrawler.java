59909ecb20d91e1eca83f360078b53edb66d5325
marcosmourabro <marcosmourabro@gmail.com>
2020-08-26 15:58:05 -0300

    Fortaleza Centerbox & OBomVizinho
    Now the token has to be set manually :(

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/fortaleza/FortalezaObomvizinhoCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/fortaleza/FortalezaObomvizinhoCrawler.java
@@ -2,6 +2,7 @@ package br.com.lett.crawlernode.crawlers.ranking.keywords.fortaleza;
 
 import java.util.HashMap;
 import java.util.Map;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 import br.com.lett.crawlernode.core.fetcher.models.Request;
@@ -20,41 +21,12 @@ public class FortalezaObomvizinhoCrawler extends CrawlerRankingKeywords {
    private static final String URL_PRODUCT_PAGE = "https://loja.obomvizinho.com.br?id=";
    private static final String CEP = "60840-285";
 
-   private String getToken() {
-      String token = null;
 
-      String apiAdress = "https://loja.obomvizinho.com.br/static/js/main.72333aad.chunk.js";
-
-      Request request = RequestBuilder.create().setUrl(apiAdress).setCookies(cookies).build();
-      String content = this.dataFetcher.get(session, request).getBody();
-
-      if (content != null) {
-
-         Integer indexOf = content.indexOf("Auth-Token\":\"");
-
-         if (indexOf != null) {
-            Integer lastIndexOf = content.lastIndexOf("\"},$");
-            if (lastIndexOf != null) {
-               String buildJson = "{\"" + content.substring(indexOf, lastIndexOf) + "\"}";
-
-               JSONObject jsonToken = CrawlerUtils.stringToJson(buildJson);
-
-               token = jsonToken.optString("Auth-Token");
-
-            }
-         }
-      }
-
-      return token;
-   }
-
-   protected Object fetch() {
+   protected JSONObject fetch() {
       JSONObject api = new JSONObject();
 
-      String token = getToken();
-
       Map<String, String> headers = new HashMap<>();
-      headers.put("Auth-Token", token);
+      headers.put("Auth-Token", "RUsycjRnU1BLTndkblIyTnF1T3FvMGlnUDJKVWx4Nk95eC9IL0RaMU80dz0tLVl3dlBqUjJnK1p2amdheW9WRVlWM0E9PQ");
       headers.put("Connection", "keep-alive");
 
       String url = "https://www.merconnect.com.br/api/v4/markets?cep=" + CEP + "&market_codename=pinheiro";
@@ -127,7 +99,7 @@ public class FortalezaObomvizinhoCrawler extends CrawlerRankingKeywords {
                   saveDataProduct(internalId, internalPid, productUrl);
 
                   this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " +
-                        internalPid + " - Url: " + productUrl);
+                     internalPid + " - Url: " + productUrl);
                }
             }
 
@@ -135,8 +107,7 @@ public class FortalezaObomvizinhoCrawler extends CrawlerRankingKeywords {
             this.result = false;
             this.log("Keyword sem resultado!");
          }
-         this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
       }
+      this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
    }
-
 }
