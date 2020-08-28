bd7a49c5473bb8afb2326ce6ecd304c982e958b0
Gabriel Dornelas <gabrieldornelas@lett.digital>
2020-08-28 10:31:11 -0300

    bug of millenium

--- src/java/br/com/lett/crawlernode/main/Main.java
+++ src/java/br/com/lett/crawlernode/main/Main.java
@@ -1,15 +1,15 @@
 package br.com.lett.crawlernode.main;
 
+import java.io.File;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import br.com.lett.crawlernode.aws.kinesis.KPLProducer;
 import br.com.lett.crawlernode.aws.sqs.QueueHandler;
 import br.com.lett.crawlernode.core.server.Server;
 import br.com.lett.crawlernode.core.task.Resources;
 import br.com.lett.crawlernode.database.Persistence;
+import br.com.lett.crawlernode.util.CommonMethods;
 import br.com.lett.crawlernode.util.Logging;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.File;
 
 
 /**
@@ -37,41 +37,46 @@ import java.io.File;
 
 public class Main {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+   private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
-  public static QueueHandler queueHandler;
-  public static Resources globalResources;
-  public static Server server;
+   public static QueueHandler queueHandler;
+   public static Resources globalResources;
+   public static Server server;
 
-  public static void main(String[] args) {
-    Logging.printLogInfo(LOGGER, "Starting webcrawler-node...");
+   public static void main(String[] args) {
+      Logging.printLogInfo(LOGGER, "Starting webcrawler-node...");
 
-    // Setting global configuraions
-    GlobalConfigurations.setConfigurations();
+      // Setting global configuraions
+      GlobalConfigurations.setConfigurations();
 
-    // Create Kinesis KPL child process
-    KPLProducer.getInstance();
+      // Create Kinesis KPL child process
+      KPLProducer.getInstance();
 
-    // Check resources
-    Logging.printLogInfo(LOGGER, "Checking files...");
-    checkFiles();
+      // Check resources
+      Logging.printLogInfo(LOGGER, "Checking files...");
+      checkFiles();
 
-    // Initialize temporary folder for images download
-    Persistence.initializeImagesDirectories(GlobalConfigurations.markets);
+      // Initialize temporary folder for images download
+      Persistence.initializeImagesDirectories(GlobalConfigurations.markets);
 
-    // Create a queue handler that will contain an Amazon SQS instance
-    queueHandler = new QueueHandler();
+      // Create a queue handler that will contain an Amazon SQS instance
+      queueHandler = new QueueHandler();
 
-    // Create the server
-    server = new Server();
-  }
+      try {
+         // Create the server
+         server = new Server();
+      } catch (Exception e) {
+         Logging.printLogError(LOGGER, "Error of millenium: " + CommonMethods.getStackTrace(e));
+         System.exit(1);
+      }
+   }
 
-  private static void checkFiles() {
-    File phantom = new File(GlobalConfigurations.executionParameters.getPhantomjsPath());
-    if (!phantom.exists() && !phantom.isDirectory()) {
-      Logging.printLogError(LOGGER, "Phantom webdriver binary not found.");
-      System.exit(1);
-    }
-  }
+   private static void checkFiles() {
+      File phantom = new File(GlobalConfigurations.executionParameters.getPhantomjsPath());
+      if (!phantom.exists() && !phantom.isDirectory()) {
+         Logging.printLogError(LOGGER, "Phantom webdriver binary not found.");
+         System.exit(1);
+      }
+   }
 
 }
