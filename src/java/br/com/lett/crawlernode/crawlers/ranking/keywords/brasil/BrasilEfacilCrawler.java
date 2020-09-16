2c1ff9619e4102efeb2ac2f45d81ba35d5ade9c4
Samuel Melo <samuelmelo00@hotmail.com>
2020-09-16 12:16:24 -0300

    New Crawler - Brasil Efacil

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilEfacilCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilEfacilCrawler.java
@@ -2,8 +2,12 @@ package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
 
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.util.CrawlerUtils;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 public class BrasilEfacilCrawler extends CrawlerRankingKeywords {
 
@@ -14,7 +18,7 @@ public class BrasilEfacilCrawler extends CrawlerRankingKeywords {
   @Override
   protected void extractProductsFromCurrentPage() {
     // número de produtos por página do market
-    this.pageSize = 12;
+    this.pageSize = 48;
 
     this.log("Página " + this.currentPage);
 
@@ -44,9 +48,14 @@ public class BrasilEfacilCrawler extends CrawlerRankingKeywords {
         String internalId = crawlInternalId(e);
 
         // Url do produto
-        String productUrl = crawlProductUrl(e);
+         String productUrl = null;
+         try {
+            productUrl = crawlProductUrl(e);
+         } catch (UnsupportedEncodingException unsupportedEncodingException) {
+            unsupportedEncodingException.printStackTrace();
+         }
 
-        saveDataProduct(internalId, internalPid, productUrl);
+         saveDataProduct(internalId, internalPid, productUrl);
 
         this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: "
             + internalPid + " - Url: " + productUrl);
@@ -73,48 +82,51 @@ public class BrasilEfacilCrawler extends CrawlerRankingKeywords {
 
   @Override
   protected void setTotalProducts() {
-    Element totalElement = this.currentDoc.select(".neemu-total-products-container").first();
 
-    if (totalElement != null) {
-      String text = totalElement.text().replaceAll("[^0-9]", "").trim();
+     String totalElement = CrawlerUtils.scrapStringSimpleInfo(this.currentDoc, ".neemu-total-products-container", true);
+     int totalProducts = 0;
+     if(totalElement != null){
+        String totalStr = totalElement.replaceAll("[^0-9]", "").trim();
+        totalProducts = !totalStr.isEmpty() ? Integer.parseInt(totalStr) : 0;
+     }
 
-      if (!text.isEmpty()) {
-        this.totalProducts = Integer.parseInt(text);
-      }
+     this.totalProducts = totalProducts;
+     this.log("Total da busca: " + this.totalProducts);
 
-      this.log("Total da busca: " + this.totalProducts);
-    }
   }
 
   private String crawlInternalId(Element e) {
-    return null;
+
+     return CrawlerUtils.scrapStringSimpleInfoByAttribute(e, null, "catentry");
+
   }
 
   private String crawlInternalPid(Element e) {
     String internalPid = null;
-    Element pid = e.select(".nm-box-product").first();
 
-    if (pid != null) {
-      String[] tokens = pid.attr("class").split("block_product_");
-      internalPid = tokens[tokens.length - 1].trim().split(" ")[0].trim();
+    String comparedProducts = CrawlerUtils.scrapStringSimpleInfoByAttribute(e, ".compare label input", "onclick");
+    String[] formattedProducts = comparedProducts != null ? comparedProducts.split(",") : null;
+
+    if(formattedProducts != null){
+       internalPid = formattedProducts.length > 0 ? formattedProducts[formattedProducts.length -1]: null;
+       internalPid = internalPid != null ? internalPid.replaceAll("[^0-9]", "").trim() : null;
     }
+
 
     return internalPid;
   }
 
-  private String crawlProductUrl(Element e) {
-    String productUrl = null;
-    Element eUrl = e.select(".nm-product-name a").first();
+  private String crawlProductUrl(Element e) throws UnsupportedEncodingException {
 
-    if (eUrl != null) {
-      productUrl = eUrl.attr("href").trim();
+     String productUrl = e.select(".nm-product-img-container img").attr("alt");
+     String[] separatedUrl = productUrl.split("/");
+     String lastPart = null;
+     if(separatedUrl.length > 0){
+        lastPart = separatedUrl[separatedUrl.length - 1];
+     }
+     String encodedPart = lastPart != null ? URLEncoder.encode(lastPart,"utf-8") : null;
+     return "https://www.efacil.com.br/loja/produto/" + encodedPart;
 
-      if (!productUrl.startsWith("http")) {
-        productUrl = "https:" + productUrl;
-      }
-    }
-
-    return productUrl;
   }
 
 }
