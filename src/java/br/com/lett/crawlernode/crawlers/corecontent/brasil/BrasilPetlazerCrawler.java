7ada1c631d5e48ac5baf6f09995d7429fc8afb6d
Layunne <fellypelayunne@gmail.com>
2020-09-17 10:12:05 -0300

    petlaser: fix internalId on unavailable products

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilPetlazerCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilPetlazerCrawler.java
@@ -1,14 +1,5 @@
 package br.com.lett.crawlernode.crawlers.corecontent.brasil;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import com.google.common.collect.Sets;
 import br.com.lett.crawlernode.core.models.Card;
 import br.com.lett.crawlernode.core.models.CategoryCollection;
 import br.com.lett.crawlernode.core.models.Product;
@@ -18,11 +9,12 @@ import br.com.lett.crawlernode.core.task.impl.Crawler;
 import br.com.lett.crawlernode.util.CrawlerUtils;
 import br.com.lett.crawlernode.util.Logging;
 import br.com.lett.crawlernode.util.MathUtils;
+import br.com.lett.crawlernode.util.Pair;
+import com.google.common.collect.Sets;
 import exceptions.MalformedPricingException;
 import exceptions.OfferException;
 import models.Offer.OfferBuilder;
 import models.Offers;
-import models.prices.Prices;
 import models.pricing.BankSlip;
 import models.pricing.BankSlip.BankSlipBuilder;
 import models.pricing.CreditCard.CreditCardBuilder;
@@ -31,14 +23,21 @@ import models.pricing.Installment.InstallmentBuilder;
 import models.pricing.Installments;
 import models.pricing.Pricing;
 import models.pricing.Pricing.PricingBuilder;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 public class BrasilPetlazerCrawler extends Crawler {
 
    private static final String HOME_PAGE = "petlazer.com.br";
    private static final String SELLER_FULL_NAME = "Pet Lazer";
    protected Set<String> cards = Sets.newHashSet(Card.VISA.toString(), Card.MASTERCARD.toString(),
-         Card.AURA.toString(), Card.DINERS.toString(), Card.HIPER.toString(), Card.AMEX.toString());
+      Card.AURA.toString(), Card.DINERS.toString(), Card.HIPER.toString(), Card.AMEX.toString());
 
    public BrasilPetlazerCrawler(Session session) {
       super(session);
@@ -58,23 +57,23 @@ public class BrasilPetlazerCrawler extends Crawler {
          String primaryImage = CrawlerUtils.scrapSimplePrimaryImage(doc, "#produto .img-responsive", Arrays.asList("src"), "https", HOME_PAGE);
          String secondaryImages = null;
          String description = CrawlerUtils.scrapElementsDescription(doc, Arrays.asList(".descricao_div_conteudo:not(col-lg-3)"));
-         boolean available = doc.selectFirst(".bt_carrinho") != null;
-         Offers offers = available ? scrapOffers(doc) : new Offers();
+
+         Offers offers = scrapOffers(doc);
 
          // Creating the product
          Product product = ProductBuilder.create()
-               .setUrl(session.getOriginalURL())
-               .setInternalId(internalId)
-               .setInternalPid(internalId)
-               .setName(name)
-               .setCategory1(categories.getCategory(0))
-               .setCategory2(categories.getCategory(1))
-               .setCategory3(categories.getCategory(2))
-               .setPrimaryImage(primaryImage)
-               .setSecondaryImages(secondaryImages)
-               .setDescription(description)
-               .setOffers(offers)
-               .build();
+            .setUrl(session.getOriginalURL())
+            .setInternalId(internalId)
+            .setInternalPid(internalId)
+            .setName(name)
+            .setCategory1(categories.getCategory(0))
+            .setCategory2(categories.getCategory(1))
+            .setCategory3(categories.getCategory(2))
+            .setPrimaryImage(primaryImage)
+            .setSecondaryImages(secondaryImages)
+            .setDescription(description)
+            .setOffers(offers)
+            .build();
 
 
          products.add(product);
@@ -90,102 +89,156 @@ public class BrasilPetlazerCrawler extends Crawler {
       return doc.selectFirst("#produto") != null;
    }
 
+   private List<String> scrapSales(Document doc) {
+      List<String> sales = new ArrayList<>();
+
+      String saleStr = CrawlerUtils.scrapStringSimpleInfo(doc, "#produto div div[style] center b[style]", false);
+
+      if (saleStr != null && !saleStr.isEmpty()) {
+         sales.add(saleStr);
+      }
+
+      return sales;
+   }
+
 
    private Offers scrapOffers(Document doc) throws MalformedPricingException, OfferException {
       Offers offers = new Offers();
-      List<String> sales = new ArrayList<>();
+
+      boolean available = doc.selectFirst(".bt_carrinho") != null;
+
+      if (!available) {
+         return offers;
+      }
+
       Pricing pricing = scrapPricing(doc);
+      List<String> sales = scrapSales(doc);
+
 
       offers.add(OfferBuilder.create()
-            .setUseSlugNameAsInternalSellerId(true)
-            .setSellerFullName(SELLER_FULL_NAME)
-            .setMainPagePosition(1)
-            .setIsBuybox(false)
-            .setIsMainRetailer(true)
-            .setPricing(pricing)
-            .setSales(sales)
-            .build());
+         .setUseSlugNameAsInternalSellerId(true)
+         .setSellerFullName(SELLER_FULL_NAME)
+         .setMainPagePosition(1)
+         .setIsBuybox(false)
+         .setIsMainRetailer(true)
+         .setPricing(pricing)
+         .setSales(sales)
+         .build());
 
       return offers;
    }
 
    private Pricing scrapPricing(Document doc) throws MalformedPricingException {
-      Double spotlightPrice = CrawlerUtils.scrapDoublePriceFromHtml(doc, "div[id=produto] div font[size] b", null, false, '.', session);
+      Double spotlightPrice = CrawlerUtils.scrapDoublePriceFromHtml(doc, "div[id=produto] div font[size] b", null, false, ',', session);
       CreditCards creditCards = scrapCreditCards(doc, spotlightPrice);
       BankSlip bankSlip = BankSlipBuilder.create()
-            .setFinalPrice(spotlightPrice)
-            .build();
+         .setFinalPrice(spotlightPrice)
+         .build();
 
       return PricingBuilder.create()
-            .setPriceFrom(null)
-            .setSpotlightPrice(spotlightPrice)
-            .setCreditCards(creditCards)
-            .setBankSlip(bankSlip)
-            .build();
-
-
+         .setPriceFrom(null)
+         .setSpotlightPrice(spotlightPrice)
+         .setCreditCards(creditCards)
+         .setBankSlip(bankSlip)
+         .build();
    }
 
    private CreditCards scrapCreditCards(Document doc, Double spotlightPrice) throws MalformedPricingException {
       CreditCards creditCards = new CreditCards();
 
-      Installments installments = scrapInstallments(doc);
-      if (installments.getInstallments().isEmpty()) {
-         installments.add(InstallmentBuilder.create()
-               .setInstallmentNumber(1)
-               .setInstallmentPrice(spotlightPrice)
-               .build());
-      }
+      Installments installments = scrapInstallments(doc, spotlightPrice);
 
       for (String card : cards) {
          creditCards.add(CreditCardBuilder.create()
-               .setBrand(card)
-               .setInstallments(installments)
-               .setIsShopCard(false)
-               .build());
+            .setBrand(card)
+            .setInstallments(installments)
+            .setIsShopCard(false)
+            .build());
       }
 
       return creditCards;
    }
 
-   private Installments scrapInstallments(Document doc) throws MalformedPricingException {
+   private Installments scrapInstallments(Document doc, Double spotlightPrice) throws MalformedPricingException {
       Installments installments = new Installments();
 
       Elements elements = doc.select("div[id=produto] div font b");
 
-      if (elements.size() > 1 && elements.get(1) != null) {
+      Element installmentEl = null;
 
-         Element installmentsCard = elements.get(1);
+      if (elements.size() == 2 && elements.get(1) != null) {
+         installmentEl = elements.get(1);
+      } else if (elements.size() == 3 && elements.get(2) != null ) {
+         installmentEl = elements.get(2);
+      }
 
-         String installmentString = installmentsCard.text().split("x")[0];
-         Integer installment = !installmentString.isEmpty() ? MathUtils.parseInt(installmentString) : null;
+      if (installmentEl != null) {
+         Pair<Integer, Float> installmentPair = CrawlerUtils.crawlSimpleInstallment(null, installmentEl, true, "x de");
 
-         String valueString = installmentsCard.text();
-         Integer rs = !valueString.isEmpty() && valueString.contains("R$") ? valueString.indexOf("R$") : null;
-         Double value = rs != null ? MathUtils.parseDoubleWithComma(valueString.substring(rs)) : null;
+         if (!installmentPair.isAnyValueNull()) {
+
+            if (installmentPair.getFirst() > 1) {
+               installments.add(InstallmentBuilder.create()
+                  .setInstallmentNumber(1)
+                  .setInstallmentPrice(spotlightPrice)
+                  .build());
+            }
 
 
-         installments.add(InstallmentBuilder.create()
-               .setInstallmentNumber(installment)
-               .setInstallmentPrice(value)
+            installments.add(InstallmentBuilder.create()
+               .setInstallmentNumber(installmentPair.getFirst())
+               .setInstallmentPrice(MathUtils.normalizeTwoDecimalPlaces(installmentPair.getSecond().doubleValue()))
                .build());
+         }
+      } else {
+         installments.add(InstallmentBuilder.create()
+            .setInstallmentNumber(1)
+            .setInstallmentPrice(spotlightPrice)
+            .build());
       }
 
       return installments;
    }
 
-   private String scrapInternalId(Document doc) {
-      String primaryImage = CrawlerUtils.scrapSimplePrimaryImage(doc, "#produto .img-responsive", Arrays.asList("src"), "https", HOME_PAGE);
+   private String scrapInternalId (Document doc) {
 
-      if (primaryImage != null) {
-         String[] split = primaryImage.split("/");
+      String internalId = null;
 
-         if (split.length > 0) {
-            return split[split.length-1].replace(".jpg", "");
+      Element button = doc.selectFirst("#produto button");
+
+      if (button == null) {
+         return internalId;
+      }
+
+      if (button.hasAttr("onclick")) {
+         String att = button.attr("onclick");
+
+         if (!att.isEmpty()) {
+
+            String search = "carrinho/";
+
+            int firstIndex = att.lastIndexOf(search);
+            int lastIndex = att.lastIndexOf("/");
+
+            if (firstIndex > 0 && lastIndex > firstIndex + search.length()) {
+               internalId = att.substring(firstIndex + search.length(), lastIndex);
+            }
+         }
+      } else if (button.hasAttr("href")) {
+         String att = button.attr("href");
+
+         if (!att.isEmpty()) {
+            String search = "produto=";
+
+            int firstIndex = att.lastIndexOf(search);
+            int lastIndex = att.indexOf("&", firstIndex+search.length());
+
+            if (firstIndex > 0 && lastIndex > firstIndex + search.length()) {
+               internalId = att.substring(firstIndex + search.length(), lastIndex);
+            }
          }
       }
 
-
-      return null;
+      return internalId;
    }
 }
