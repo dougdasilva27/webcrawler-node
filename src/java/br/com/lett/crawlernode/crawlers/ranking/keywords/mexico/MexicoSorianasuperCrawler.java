767d5928053037ae2f43d3ae1593ea1247bebd78
marcosmourabro <marcosmourabro@gmail.com>
2020-09-17 13:52:31 -0300

    add scrapURL add comment

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/mexico/MexicoSorianasuperCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/mexico/MexicoSorianasuperCrawler.java
@@ -30,6 +30,7 @@ public class MexicoSorianasuperCrawler extends CrawlerRankingKeywords {
 
    @Override protected Document fetchDocument(String url) {
       Map<String, String> headers = new HashMap<>();
+      //When this crawler was made I couldn't find a better way to set these cookies. This is a palliative solution that should be changed in the near future.
       headers.put("cookie", "ASP.NET_SessionId=jlg2o22z13aawvuupormhlwz; NumTipServProduccion=NumTipServ=1; NumTdaProduccion=NumTda=51; NombreTiendaProduccion=NombreTienda=Las Palmas; ak_bmsc=4B1A9D3922FA768C410FCBE2D8058F7FBBBD0C66093E00000653635F90894447~plU2z70pKcWwGdm+L04fL9QLVPYbTkwVrJYNhOn+2To0yxUGAuN5GON6+m47KR37IZy45g6eQUvkjoikpSAhuleL9pOadprKJpxrbYQfQe6kzdPXExNfERAZtnd/cztaJjqb/VRZ70CSkPcXA6qBeMfWMjTmffyHYGEe6ScBdCtPjc1fQdHyzaD1M6NBOEq3ElbYgf/rStsF7oDvClOpZyVXUUa+jMuX2gLb6E4xlna2o=; _ga=GA1.3.1591322.1600344843; _gid=GA1.3.1641728810.1600344843; _fbp=fb.1.1600344844919.1605184225; _hjTLDTest=1; _hjid=6e343037-4ee8-48c7-9e49-a5abf1093868; _hjIncludedInPageviewSample=1; _hjAbsoluteSessionInProgress=1; isPushNotificationClient=false; LinkToStoreUrl=?p=13365&Txt_Bsq_Descripcion=Galleta&Marca=&Linea=&Paginacion=2&nuor=0; TS01f618f3=015c364ca06ba3c49675ac1ae5f693f54c11b9e3cffa88d6e321851a8f12994fd973c75f711d05ecad3ab355e73912fbb906aff8152652fe2bfb25bd71fae25e5fba39cda80f03017c707b408ff9445264028b9822f23385c218fb12a7107f6111f40710d5dd2dc06176eed889451df0190b575c30180d83c62daca52b025493a56f212d9e; bm_sv=DE56AD8BF013B90939026FFFF79D8F1B~fZWDV2ik8K6ftTGGQwGLjavj1i8DfYV+uX9MasTzkqpZEHDnoGimmKuNKTFWdOde2CDznCgJBcCrK6BKoVLV/S4/W2uZ0EWQsdCHEdOUKJ9pJVHrijQdzfZJBGuOb8yT0ySSpSO0/T0lo9lLD1aeWVn5US4j7Rql0zggry5J5zw=");
 
       Request request = RequestBuilder.create()
@@ -67,7 +68,7 @@ public class MexicoSorianasuperCrawler extends CrawlerRankingKeywords {
       for (Element e : products) {
 
         String internalId = crawlInternalId(e);
-        String productUrl = PROTOCOL +DOMAIN + CrawlerUtils.scrapStringSimpleInfoByAttribute(e, "a[href]:first-child", "href" );
+        String productUrl = CrawlerUtils.scrapUrl(e, "a[href]:first-child", "href",PROTOCOL ,DOMAIN);
 
         saveDataProduct(internalId, null, productUrl);
 
