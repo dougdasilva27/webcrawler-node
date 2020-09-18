afa22f924723c5557b83aef5b8359c3ffec19a5c
Gabriel Dornelas <gabrieldornelas@lett.digital>
2020-09-18 16:06:31 -0300

    files

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/saopaulo/SaopauloGeracaopetvilatoninhoCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/saopaulo/SaopauloGeracaopetvilatoninhoCrawler.java
@@ -5,10 +5,11 @@ import br.com.lett.crawlernode.crawlers.corecontent.extractionutils.GeracaopetCr
 
 public class SaopauloGeracaopetvilatoninhoCrawler extends GeracaopetCrawler {
 
-  private static final String CEP = "15081-500";
+   private static final String CEP = "15081-500";
+   private static final String STORE_ID = "5e435f05187f5e455067c17b";
 
-  public SaopauloGeracaopetvilatoninhoCrawler(Session session) {
-    super(session, CEP);
-  }
+   public SaopauloGeracaopetvilatoninhoCrawler(Session session) {
+      super(session, CEP, STORE_ID);
+   }
 
 }
