532ce35f25fbd5d0f646b2a5ec52b16c9b37d184
Samuel Melo <samuelmelo00@hotmail.com>
2020-10-08 16:06:32 -0300

    site mercado crawlers - andradina, garça, treslagoas (Big Mart)

---
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/andradina/AndradinaSitemercadobigmartCrawler.java
@@ -0,0 +1,29 @@
+package br.com.lett.crawlernode.crawlers.ranking.keywords.andradina;
+
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.crawlers.ranking.keywords.extractionutils.BrasilSitemercadoCrawler;
+import br.com.lett.crawlernode.util.CommonMethods;
+import org.json.JSONObject;
+
+public class AndradinaSitemercadobigmartCrawler extends BrasilSitemercadoCrawler {
+
+   public static final String HOME_PAGE = "https://www.sitemercado.com.br/bigmartsupermercados/andradina-loja-andradina-stella-maris-av-guanabara";
+
+   public AndradinaSitemercadobigmartCrawler(Session session) {
+      super(session);
+   }
+
+   @Override
+   protected String getHomePage() { return HOME_PAGE; }
+
+   @Override
+   protected String getLoadPayload() {
+      JSONObject payload = new JSONObject();
+      String[] split = HOME_PAGE.split("/");
+
+      payload.put("lojaUrl", CommonMethods.getLast(split));
+      payload.put("redeUrl", split[split.length - 2]);
+
+      return payload.toString();
+   }
+}
