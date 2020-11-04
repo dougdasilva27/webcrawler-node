318b42ba2902e4dc8231863b19563acce89ff89e
Marcos Moura <marcosmourabro@gmail.com>
2020-11-04 14:49:09 -0300

    multiar
    bugfix

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilMultiarCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilMultiarCrawler.java
@@ -2,93 +2,80 @@ package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
 
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
-import br.com.lett.crawlernode.util.CommonMethods;
+import br.com.lett.crawlernode.util.CrawlerUtils;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class BrasilMultiarCrawler extends CrawlerRankingKeywords {
 
-  public BrasilMultiarCrawler(Session session) {
-    super(session);
-  }
+   public BrasilMultiarCrawler(Session session) {
+      super(session);
+   }
 
-  private boolean isCategory;
+   private boolean isCategory;
 
-  @Override
-  protected void extractProductsFromCurrentPage() {
-    // número de produtos por página do market
-    this.pageSize = 24;
+   @Override
+   protected void extractProductsFromCurrentPage() {
+      // número de produtos por página do market
+      this.pageSize = 24;
 
-    this.log("Página " + this.currentPage);
+      this.log("Página " + this.currentPage);
 
-    String key = this.keywordWithoutAccents.replaceAll(" ", "%20");
+      String key = this.keywordWithoutAccents.replaceAll(" ", "%20");
 
-    // monta a url com a keyword e a página
-    String url = "http://www.multiar.com.br/" + key;
+      // monta a url com a keyword e a página
+      String url = "http://www.multiar.com.br/" + key;
 
-    if (this.currentPage > 1) {
-      if (!this.isCategory) {
-        url = "http://www.multiar.com.br/buscapagina?ft=" + key + "&PS=24" + "&sl=379a0b55-f6f6-4127-8d29-0be97c93ba9a&cc=0&sm=0&PageNumber="
-            + this.currentPage;
+      if (this.currentPage > 1) {
+         if (!this.isCategory) {
+            url = "http://www.multiar.com.br/buscapagina?ft=" + key + "&PS=24" + "&sl=379a0b55-f6f6-4127-8d29-0be97c93ba9a&cc=0&sm=0&PageNumber="
+               + this.currentPage;
+         }
       }
-    }
 
-    this.log("Link onde são feitos os crawlers: " + url);
+      this.log("Link onde são feitos os crawlers: " + url);
 
-    // chama função de pegar a url
-    this.currentDoc = fetchDocument(url);
+      // chama função de pegar a url
+      this.currentDoc = fetchDocument(url);
 
-    Element elementIsCategory = this.currentDoc.select("div.bread-crumb > ul > li.last").first();
+      Element elementIsCategory = this.currentDoc.select("div.bread-crumb > ul > li.last").first();
 
-    if (this.currentPage == 1) {
-      this.isCategory = elementIsCategory != null;
-    }
-
-    Elements products = this.currentDoc.select("div.prateleira.vitrine > ul > li");
-
-    // se obter 1 ou mais links de produtos e essa página tiver resultado faça:
-    if (!products.isEmpty()) {
-      for (int i = 0; i < products.size(); i++) {
-        if (products.get(i).hasAttr("layout")) {
-          String[] tokens = products.get(i + 1).attr("id").split("_");
-          String internalPid = CommonMethods.getLast(tokens);
-          String internalId = null;
-
-          // monta a url
-          Element eUrl = products.get(i).select("> a[title]").first();
-          String productUrl = eUrl.attr("href");
-
-          saveDataProduct(internalId, internalPid, productUrl);
-
-          this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + internalPid + " - Url: " + productUrl);
-          if (this.arrayProducts.size() == productsLimit)
-            break;
-        }
+      if (this.currentPage == 1) {
+         this.isCategory = elementIsCategory != null;
       }
-    } else {
-      setTotalProducts();
-      this.result = false;
-      this.log("Keyword sem resultado!");
-    }
 
-    this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
-    if (!(hasNextPage()))
-      setTotalProducts();
-  }
+      Elements products = this.currentDoc.select("div.prateleira.vitrine > ul > li[layout]");
 
-  @Override
-  protected boolean hasNextPage() {
-    Elements ids = this.currentDoc.select("div.prateleira.vitrine > ul > li[layout]");
+      // se obter 1 ou mais links de produtos e essa página tiver resultado faça:
+      if (!products.isEmpty()) {
+         for (Element e: products) {
 
-    if (this.currentPage > 1) {
-      return ids.size() >= 24;
-    } else {
-      if (this.isCategory) {
-        return false;
+            String internalPid = CrawlerUtils.scrapStringSimpleInfoByAttribute(e, " .spot", "data-id");
+            String productUrl = CrawlerUtils.scrapUrl(e, ".spot .spot-img", "href", "https:", "www.leveros.com.br");
+
+            saveDataProduct(null, internalPid, productUrl);
+
+            this.log("Position: " + this.position + " - InternalId: " + null + " - InternalPid: " + internalPid + " - Url: " + productUrl);
+            if (this.arrayProducts.size() == productsLimit)
+               break;
+
+         }
       } else {
-        return ids.size() >= 24;
+         setTotalProducts();
+         this.result = false;
+         this.log("Keyword sem resultado!");
       }
-    }
-  }
+
+      this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
+      if (!(hasNextPage()))
+         setTotalProducts();
+   }
+
+   @Override
+   protected void setTotalProducts() {
+      this.totalProducts = CrawlerUtils.scrapIntegerFromHtml(this.currentDoc, ".resultado-busca-numero span[class=\"value\"]", null, null, false, false, 0);
+      this.log("Total: " + this.totalProducts);
+
+   }
 
 }
