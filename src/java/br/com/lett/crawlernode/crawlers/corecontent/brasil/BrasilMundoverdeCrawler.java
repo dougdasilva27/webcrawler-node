bd7f7d9145c97b4667e8e43b3dc4a35d0a1d610c
Marcos Moura <marcosmourabro@gmail.com>
2020-11-05 13:43:06 -0300

    Mundo verde
    bugfix core and Ranking

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilMundoverdeCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilMundoverdeCrawler.java
@@ -1,9 +1,10 @@
 package br.com.lett.crawlernode.crawlers.corecontent.brasil;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+
+import br.com.lett.crawlernode.crawlers.corecontent.extractionutils.VTEXNewScraper;
+import br.com.lett.crawlernode.crawlers.corecontent.extractionutils.VTEXOldScraper;
+import org.json.JSONObject;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -31,226 +32,32 @@ import models.pricing.Installments;
 import models.pricing.Pricing;
 import models.pricing.Pricing.PricingBuilder;
 
-public class BrasilMundoverdeCrawler extends Crawler {
+public class BrasilMundoverdeCrawler extends VTEXNewScraper {
 
-   private final String HOME_PAGE = "https://www.mundoverde.com.br/";
-   private static final String SELLER_FULL_NAME = "Mundo Verde";
-   protected Set<String> cards = Sets.newHashSet(Card.VISA.toString(), Card.MASTERCARD.toString(),
-         Card.AURA.toString(), Card.DINERS.toString(), Card.HIPER.toString(), Card.AMEX.toString());
+   private static final String HOME_PAGE = "https://www.mundoverde.com.br/";
+   private static final List<String> MAIN_SELLERS = Collections.singletonList("NATUE COMERCIO E IMPORTACAO DE COSMETICOS E PRODUTOS ALIMENTÍCIOS EM GERAL S/A");
+
+   @Override
+   protected String scrapInternalpidThirdWay(JSONObject runtimeJSON) {
+      return super.scrapInternalpidThirdWay(runtimeJSON);
+   }
 
    public BrasilMundoverdeCrawler(Session session) {
       super(session);
    }
 
    @Override
-   public boolean shouldVisit() {
-      String href = session.getOriginalURL().toLowerCase();
-      return !FILTERS.matcher(href).matches() && (href.startsWith(HOME_PAGE));
+   protected String getHomePage() {
+      return HOME_PAGE;
    }
 
    @Override
-   public List<Product> extractInformation(Document doc) throws Exception {
-      super.extractInformation(doc);
-      List<Product> products = new ArrayList<Product>();
-
-      if (isProductPage(doc)) {
-         Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
-
-         String internalId = CrawlerUtils.scrapStringSimpleInfoByAttribute(doc, ".addToCart input", "value");
-         String internalPid = internalId;
-         String name = CrawlerUtils.scrapStringSimpleInfo(doc, ".productDetail--info .productDetail--headline", false);
-         boolean available = doc.selectFirst(".priceContainer .productDetail--buy") != null;
-         CategoryCollection categories = CrawlerUtils.crawlCategories(doc, ".breadcrumbs a span");
-         String primaryImage = CrawlerUtils.scrapSimplePrimaryImage(doc, "#productDetail--image figure img", Arrays.asList("src"), "https://", "www.mundoverde.com.br/");
-         String secondaryImages = CrawlerUtils.scrapSimpleSecondaryImages(doc, "#productDetail--elevateZoom--gallery a img", Arrays.asList("src"), "https://", "www.mundoverde.com.br/", primaryImage);
-         String description = CrawlerUtils.scrapElementsDescription(doc, Arrays.asList(".wrap--section .productDetail--description", ".productDetail--ingredients_table"));
-         Integer stock = null;
-         Offers offers = available ? scrapOffers(doc) : new Offers();
-         RatingsReviews ratingReviews = scrapRatingReviews(doc);
-
-         // Creating the product
-         Product product = ProductBuilder.create()
-               .setUrl(session.getOriginalURL())
-               .setInternalId(internalId)
-               .setInternalPid(internalPid)
-               .setName(name)
-               .setCategory1(categories.getCategory(0))
-               .setCategory2(categories.getCategory(1))
-               .setCategory3(categories.getCategory(2))
-               .setPrimaryImage(primaryImage)
-               .setSecondaryImages(secondaryImages)
-               .setDescription(description)
-               .setStock(stock)
-               .setOffers(offers)
-               .setRatingReviews(ratingReviews)
-               .build();
-
-         products.add(product);
-
-      } else {
-         Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
-      }
-
-      return products;
+   protected List<String> getMainSellersNames() {
+      return MAIN_SELLERS;
    }
 
-   private boolean isProductPage(Document doc) {
-      return doc.selectFirst(".productDetail--topContainer") != null;
+   @Override
+   protected RatingsReviews scrapRating(String internalId, String internalPid, Document doc, JSONObject jsonSku) {
+      return null;
    }
-
-   private RatingsReviews scrapRatingReviews(Document doc) {
-      RatingsReviews ratingReviews = new RatingsReviews();
-      ratingReviews.setDate(session.getDate());
-
-      Integer totalNumOfEvaluations = getTotalNumOfRatings(doc);
-      Double avgRating = getTotalAvgRating(doc);
-      AdvancedRatingReview advancedRatingReview = scrapAdvancedRatingReview(doc);
-
-      ratingReviews.setTotalRating(totalNumOfEvaluations);
-      ratingReviews.setAverageOverallRating(avgRating);
-      ratingReviews.setTotalWrittenReviews(totalNumOfEvaluations);
-      ratingReviews.setAdvancedRatingReview(advancedRatingReview);
-      return ratingReviews;
-   }
-
-
-   private Double getTotalAvgRating(Document doc) {
-
-      Double avgRating = 0.0;
-      avgRating = CrawlerUtils.extractRatingAverageFromAdvancedRatingReview(scrapAdvancedRatingReview(doc));
-      return avgRating;
-   }
-
-   private AdvancedRatingReview scrapAdvancedRatingReview(Document doc) {
-      Integer star1 = 0;
-      Integer star2 = 0;
-      Integer star3 = 0;
-      Integer star4 = 0;
-      Integer star5 = 0;
-
-      Elements reviews = doc.select(".js-reviewList .customerReviews .productDetail--testemonials--rating");
-
-      for (Element review : reviews) {
-
-         Elements elementStarNumber = review.select(".star-full");
-
-         if (elementStarNumber != null) {
-
-            Integer StarNumber = elementStarNumber.size();
-
-            switch (StarNumber) {
-               case 5:
-                  star5 += 1;
-                  break;
-               case 4:
-                  star4 += 1;
-                  break;
-               case 3:
-                  star3 += 1;
-                  break;
-               case 2:
-                  star2 += 1;
-                  break;
-               case 1:
-                  star1 += 1;
-                  break;
-               default:
-                  break;
-            }
-         }
-      }
-
-      return new AdvancedRatingReview.Builder()
-            .totalStar1(star1)
-            .totalStar2(star2)
-            .totalStar3(star3)
-            .totalStar4(star4)
-            .totalStar5(star5)
-            .build();
-   }
-
-   private Integer getTotalNumOfRatings(Document doc) {
-      Integer totalRating = null;
-      Elements totalRatingElement = doc.select(".customerReviews");
-
-      if (totalRatingElement != null) {
-         totalRating = totalRatingElement.size();
-      }
-
-      return totalRating;
-   }
-
-
-
-   private Offers scrapOffers(Document doc) throws MalformedPricingException, OfferException {
-      Offers offers = new Offers();
-      Pricing pricing = scrapPricing(doc);
-      List<String> sales = scrapSales(doc);
-
-      offers.add(OfferBuilder.create()
-            .setUseSlugNameAsInternalSellerId(true)
-            .setSellerFullName(SELLER_FULL_NAME)
-            .setMainPagePosition(1)
-            .setIsBuybox(false)
-            .setIsMainRetailer(true)
-            .setPricing(pricing)
-            .setSales(sales)
-            .build());
-
-      return offers;
-   }
-
-   private List<String> scrapSales(Document doc) {
-      List<String> sales = new ArrayList<>();
-
-
-      Element salesOneElement = doc.selectFirst(".image--badges .discount");
-      String firstSales = salesOneElement != null ? salesOneElement.ownText() : null;
-
-      if (firstSales != null && !firstSales.isEmpty()) {
-         sales.add(firstSales);
-      }
-
-      return sales;
-
-   }
-
-   private Pricing scrapPricing(Document doc) throws MalformedPricingException {
-      Double priceFrom = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".priceContainer .price--value", null, false, ',', session);
-      Double spotlightPrice = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".priceBox .price .price--value", null, false, ',', session);
-      CreditCards creditCards = scrapCreditCards(doc, spotlightPrice);
-      BankSlip bankSlip = BankSlipBuilder.create()
-            .setFinalPrice(spotlightPrice)
-            .build();
-
-      return PricingBuilder.create()
-            .setPriceFrom(priceFrom)
-            .setSpotlightPrice(spotlightPrice)
-            .setCreditCards(creditCards)
-            .setBankSlip(bankSlip)
-            .build();
-   }
-
-   private CreditCards scrapCreditCards(Document doc, Double spotlightPrice) throws MalformedPricingException {
-      CreditCards creditCards = new CreditCards();
-
-      Installments installments = new Installments();
-      if (installments.getInstallments().isEmpty()) {
-         installments.add(InstallmentBuilder.create()
-               .setInstallmentNumber(1)
-               .setInstallmentPrice(spotlightPrice)
-               .build());
-      }
-
-      for (String card : cards) {
-         creditCards.add(CreditCardBuilder.create()
-               .setBrand(card)
-               .setInstallments(installments)
-               .setIsShopCard(false)
-               .build());
-      }
-
-      return creditCards;
-   }
-
 }
--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilMundoverdeCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilMundoverdeCrawler.java
@@ -1,10 +1,10 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
 
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
 import br.com.lett.crawlernode.util.CrawlerUtils;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class BrasilMundoverdeCrawler extends CrawlerRankingKeywords {
 
@@ -15,42 +15,49 @@ public class BrasilMundoverdeCrawler extends CrawlerRankingKeywords {
    @Override
    protected void extractProductsFromCurrentPage() {
 
-      this.pageSize = 60;
+      this.pageSize = 12;
 
       this.log("Página " + this.currentPage);
 
-      String url = "https://www.mundoverde.com.br/catalog/?re=0&q=" + this.keywordWithoutAccents + "&page=" + this.currentPage;
+      String url = "https://www.mundoverde.com.br/" + this.keywordEncoded + "?page=" + this.currentPage;
       this.log("Link onde são feitos os crawlers: " + url);
 
       this.currentDoc = fetchDocument(url);
 
-      Elements products = this.currentDoc.select(".products .productList--item");
+      Elements products = this.currentDoc.select(".vtex-product-summary-2-x-container");
 
-      if (products.size() >= 1) {
-         for (Element e : products) {
-
-            String internalPid = null;
-            String internalId = e.attr("data-sku");
-            String urlProduct = CrawlerUtils.completeUrl(CrawlerUtils.scrapStringSimpleInfoByAttribute(e, "a", "href"), "https://", "www.mundoverde.com.br");
-
-            saveDataProduct(internalId, internalPid, urlProduct);
-
-            this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + internalPid + " - Url: " + urlProduct);
-            if (this.arrayProducts.size() == productsLimit) break;
-
+      if (!products.isEmpty()) {
+         if (this.totalProducts == 0) {
+            setTotalProducts();
          }
-      } else {
-         this.result = false;
-         this.log("Keyword sem resultado!");
+
+         if (products.size() >= 1) {
+            for (Element e : products) {
+
+               String urlProduct = CrawlerUtils.scrapUrl(e, "a", "href", "https://", "www.mundoverde.com.br");
+
+               saveDataProduct(null, null, urlProduct);
+
+               this.log("Position: " + this.position + " - InternalId: " + null + " - InternalPid: " + null + " - Url: " + urlProduct);
+               if (this.arrayProducts.size() == productsLimit) break;
+
+            }
+         } else {
+            this.result = false;
+            this.log("Keyword sem resultado!");
+         }
+
+         this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
+
       }
-
-      this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
-
    }
 
    @Override
-   protected boolean hasNextPage() {
-      return this.currentDoc.selectFirst(".productList--pagination a[rel=\"next\"]") != null;
+   protected void setTotalProducts() {
+      this.totalProducts = CrawlerUtils.scrapIntegerFromHtml(currentDoc, ".vtex-search-result-3-x-totalProducts--layout span", null, null, false, false, 0);
+      System.err.println(this.totalProducts);
+      this.log("Total de produtos: " + this.totalProducts);
    }
-
 }
+
+
