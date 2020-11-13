a72bc6b07a7eb005fc6bd3647e78dcc845984914
Marcos Moura <marcosmourabro@gmail.com>
2020-11-13 14:39:36 -0300

    ranking bug fix

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilLebesCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilLebesCrawler.java
@@ -2,117 +2,77 @@ package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
 
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.util.CrawlerUtils;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class BrasilLebesCrawler extends CrawlerRankingKeywords {
 
-  public BrasilLebesCrawler(Session session) {
-    super(session);
-  }
+   public BrasilLebesCrawler(Session session) {
+      super(session);
+   }
 
-  @Override
-  protected void extractProductsFromCurrentPage() {
-    this.log("Página " + this.currentPage);
+   @Override
+   protected void extractProductsFromCurrentPage() {
+      this.log("Página " + this.currentPage);
 
-    // número de produtos por página do market
-    this.pageSize = 24;
+      // número de produtos por página do market
+      this.pageSize = 16;
+      String keyword = this.keywordWithoutAccents.replace(" ", "%20");
 
-    String keyword = this.keywordWithoutAccents.replaceAll(" ", "%20");
+      // monta a url com a keyword e a página
+      String url = "https://www.lebes.com.br/" + keyword + "?PageNumber=" + this.currentPage;
 
-    // monta a url com a keyword e a página
-    String url = "http://www.lebes.com.br/" + keyword + "?PageNumber=" + this.currentPage;
-    this.log("Link onde são feitos os crawlers: " + url);
+      this.log("Link onde são feitos os crawlers: " + url);
 
-    // chama função de pegar a url
-    this.currentDoc = fetchDocument(url);
+      // chama função de pegar a url
+      this.currentDoc = fetchDocument(url);
 
-    Elements products = this.currentDoc.select("div.shelf--products:not([id]) ul > li[layout]");
+      Elements products = this.currentDoc.select(".prateleira.principal ul li .data");
 
-    // se obter 1 ou mais links de produtos e essa página tiver resultado faça:
-    if (products.size() >= 1) {
-      // se o total de busca não foi setado ainda, chama a função para setar
-      if (this.totalProducts == 0) {
-        setTotalProducts();
+      // se obter 1 ou mais links de produtos e essa página tiver resultado faça:
+      if (products.size() >= 1) {
+         // se o total de busca não foi setado ainda, chama a função para setar
+         if (this.totalProducts == 0) {
+            setTotalProducts();
+         }
+
+         for (Element e : products) {
+
+            String internalPid = CrawlerUtils.scrapStringSimpleInfo(e, ".id-produto", false);
+            String urlProduct = CrawlerUtils.scrapUrl(e,".productImage", "href", "https:", "https://www.lebes.com.br/");
+
+            saveDataProduct(null, internalPid, urlProduct);
+
+            this.log("Position: " + this.position + " - InternalId: " + null
+               + " - InternalPid: " + internalPid + " - Url: " + urlProduct);
+
+            if (this.arrayProducts.size() == productsLimit) {
+               break;
+            }
+         }
+      } else {
+         this.result = false;
+         this.log("Keyword sem resultado!");
       }
 
-      for (Element e : products) {
-        // InternalPid
-        String internalPid = crawlInternalPid(e);
+      this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora "
+         + this.arrayProducts.size() + " produtos crawleados");
+   }
 
-        if (internalPid != null) {
-          // InternalId
-          String internalId = crawlInternalId(e);
 
-          // Url do produto
-          String urlProduct = crawlProductUrl(e);
+   @Override
+   protected void setTotalProducts() {
+      Element totalElement = this.currentDoc.select("span.resultado-busca-numero span.value").first();
 
-          saveDataProduct(internalId, internalPid, urlProduct);
-
-          this.log("Position: " + this.position + " - InternalId: " + internalId
-              + " - InternalPid: " + internalPid + " - Url: " + urlProduct);
-
-          if (this.arrayProducts.size() == productsLimit) {
-            break;
-          }
-        }
-
+      try {
+         if (totalElement != null) {
+            this.totalProducts = Integer.parseInt(totalElement.text());
+         }
+      } catch (Exception e) {
+         this.logError(e.getMessage());
       }
-    } else {
-      this.result = false;
-      this.log("Keyword sem resultado!");
-    }
 
-    this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora "
-        + this.arrayProducts.size() + " produtos crawleados");
-  }
-
-  @Override
-  protected boolean hasNextPage() {
-
-    return arrayProducts.size() < this.totalProducts;
-  }
-
-  @Override
-  protected void setTotalProducts() {
-    Element totalElement = this.currentDoc.select("span.resultado-busca-numero span.value").first();
-
-    try {
-      if (totalElement != null) {
-        this.totalProducts = Integer.parseInt(totalElement.text());
-      }
-    } catch (Exception e) {
-      this.logError(e.getMessage());
-    }
-
-    this.log("Total da busca: " + this.totalProducts);
-  }
-
-  private String crawlInternalId(Element e) {
-    String internalId = null;
-
-    return internalId;
-  }
-
-  private String crawlInternalPid(Element e) {
-    String internalPid = null;
-    Element pidElement = e.select(".vtex-cpProdId").first();
-
-    if (pidElement != null) {
-      internalPid = pidElement.attr("value");
-    }
-
-    return internalPid;
-  }
-
-  private String crawlProductUrl(Element e) {
-    String urlProduct = null;
-    Element urlElement = e.select(".vtex-cpUri").first();
-
-    if (urlElement != null) {
-      urlProduct = urlElement.val();
-    }
-
-    return urlProduct;
-  }
+      this.log("Total da busca: " + this.totalProducts);
+   }
 }
