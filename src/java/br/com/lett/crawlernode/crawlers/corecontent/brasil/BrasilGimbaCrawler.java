305c8860d9152db1efcb3369ad00811059bf50fc
matheus bussolotti <matheusbussolotti@gmail.com>
2020-11-16 16:30:31 -0300

    erro descricao

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilGimbaCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilGimbaCrawler.java
@@ -1,5 +1,9 @@
 package br.com.lett.crawlernode.crawlers.corecontent.brasil;
 
+import br.com.lett.crawlernode.core.fetcher.DynamicDataFetcher;
+import br.com.lett.crawlernode.core.fetcher.FetchMode;
+import br.com.lett.crawlernode.core.fetcher.models.Request;
+import br.com.lett.crawlernode.core.fetcher.models.Response;
 import br.com.lett.crawlernode.core.models.Card;
 import br.com.lett.crawlernode.core.models.CategoryCollection;
 import br.com.lett.crawlernode.core.models.Product;
@@ -10,18 +14,23 @@ import br.com.lett.crawlernode.util.CrawlerUtils;
 import br.com.lett.crawlernode.util.Logging;
 import br.com.lett.crawlernode.util.MathUtils;
 import com.google.common.collect.Sets;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
 import exceptions.MalformedPricingException;
 import exceptions.OfferException;
 import models.Offer;
 import models.Offers;
 import models.pricing.*;
+import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.cookie.BasicClientCookie;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class BrasilGimbaCrawler extends Crawler {
 
@@ -29,6 +38,7 @@ public class BrasilGimbaCrawler extends Crawler {
 
    public BrasilGimbaCrawler(Session session) {
       super(session);
+      super.config.setFetcher(FetchMode.JAVANET);
    }
 
    @Override
@@ -38,6 +48,27 @@ public class BrasilGimbaCrawler extends Crawler {
    }
 
    @Override
+   protected Object fetch() {
+      String html = "";
+      if (config.getFetcher() == FetchMode.WEBDRIVER) {
+         webdriver = DynamicDataFetcher.fetchPageWebdriver(session.getOriginalURL(), session);
+
+         if (webdriver != null) {
+            html = webdriver.getCurrentPageSource();
+         }
+      } else {
+         Request request = Request.RequestBuilder.create().setCookies(cookies).setUrl(session.getOriginalURL()).build();
+         Response response = dataFetcher.get(session, request);
+
+         cookies.addAll(response.getCookies());
+         html = response.getBody();
+      }
+
+      return Jsoup.parse(html);
+   }
+
+
+   @Override
    public List<Product> extractInformation(Document doc) throws Exception {
       super.extractInformation(doc);
       List<Product> products = new ArrayList<>();
@@ -45,14 +76,19 @@ public class BrasilGimbaCrawler extends Crawler {
       if (doc.selectFirst("#produtoCadastro") != null) {
          Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
 
+         Offers offers = new Offers();
          String internalId = CrawlerUtils.scrapStringSimpleInfo(doc, "#codigoProduto p", true);
          String name = CrawlerUtils.scrapStringSimpleInfo(doc, ".produto-nome", false);
          boolean available = doc.selectFirst("#botao-comprar .btn-add-new") != null;
          CategoryCollection categories = CrawlerUtils.crawlCategories(doc, "#breadCrumb div a");
          String primaryImage = CrawlerUtils.scrapSimplePrimaryImage(doc, ".main-thumb a", Arrays.asList("href"), "https:", "www.gimba.com.br");
          String secondaryImages = CrawlerUtils.scrapSimpleSecondaryImages(doc, "#thumb a", Arrays.asList("href"), "https:", "www.gimba.com.br", primaryImage);
-         Offers offers = scrapOffer(doc);
-         String description = CrawlerUtils.scrapStringSimpleInfo(doc, "#detalhe-produto-novo .fonte-descricao-prod dd p b", false);
+         if(available) {
+            offers = scrapOffer(doc);
+         }
+//         String description = CrawlerUtils.scrapStringSimpleInfo(doc, "#detalhe-produto-novo .fonte-descricao-prod dd p b", false);
+         String description = scrapDescriptionHard(doc);
+
          // Creating the product
          Product product = ProductBuilder.create()
             .setUrl(session.getOriginalURL())
@@ -76,6 +112,49 @@ public class BrasilGimbaCrawler extends Crawler {
       return products;
    }
 
+   private String scrapDescriptionHard(Document doc){
+      String urlpid = session.getOriginalURL();
+      String pid = urlpid.substring(urlpid.indexOf("PID=")+4);
+      String verificationToken = doc.selectFirst("[name=__RequestVerificationToken]").attr("value");
+      String url = "https://www.gimba.com.br/produtos/JsonRetornaProdutoDetalhe?id="+pid+"&kit=false";
+//      String payload ="__RequestVerificationToken=%20KumdF5wJhwqhWwj5iEPmCgowlhXBzJw8VROLBu3oOAFD--0DBclzSOVwXp7nmOGc6cO0DT29-MJQEouxleQPXkQgXVY1";
+      String payload ="__RequestVerificationToken="+verificationToken;
+      String cookietoken = null;
+      for (Cookie cookie :cookies) {
+         if(cookie.getName().equals("__RequestVerificationToken")){
+            cookietoken=cookie.getValue();
+         }
+      }
+      String requestCookieValue ="__RequestVerificationToken="+cookietoken+";";
+      Map<String,String> headres = new HashMap<>();
+      headres.put("cookie",requestCookieValue); // __cfduid=df74acc0656409981cd5a6e4e04f12f5a1605298735; .ASPXANONYMOUS=liD39ovw1gEkAAAAZmE1NzIyNDktOWI1My00Y2E5LTgxMjYtMWRlYzgxZGNiNDg01aMqC0bPXAwlY7j8ByT7s6tUDtE1; ASP.NET_SessionId=0bddw0vlpcd0owdu2euozo1k; PROMOTOR_GIMBA=");
+      headres.put("user-agent","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36");
+      headres.put("Content-Type","application/x-www-form-urlencoded");
+      headres.put("Host","www.gimba.com.br");
+
+      Request request = Request.RequestBuilder
+         .create()
+         .setUrl(url)
+         .setHeaders(headres)
+         .setPayload(payload)
+         .build();
+
+      String responce = dataFetcher.post(session,request).getBody();
+      responce = responce.replaceAll("\"","");
+//      responce = StringEscapeUtils.unescapeJava(responce);
+      String responce2 = responce.replaceAll("\u003c","<").replaceAll("\u003e",">");
+      System.out.println(responce2);
+      Document document = Jsoup.parseBodyFragment(responce);
+      Elements elements = document.getAllElements();
+//      elements.forEach(element -> System.out.println(element.text()));
+      String result =  CrawlerUtils.scrapStringSimpleInfo(document, ".fonte-descricao-prod dd p b", false);
+
+      return result;
+   }
+
+
+
+
 
    private Offers scrapOffer(Document doc) throws OfferException, MalformedPricingException {
       Offers offers = new Offers();
@@ -95,7 +174,6 @@ public class BrasilGimbaCrawler extends Crawler {
       return offers;
 
    }
-
 
    private Pricing scrapPricing(Document doc) throws MalformedPricingException {
       Double priceFrom = CrawlerUtils.scrapDoublePriceFromHtml(doc, "#valores-dados #valores-dados-preco-de", null, false, ',', session);
