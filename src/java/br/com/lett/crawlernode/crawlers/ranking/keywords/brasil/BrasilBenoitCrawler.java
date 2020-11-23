f1dea75273dd0fc2228864bf69408dd2f1560bb5
Samuel Melo <samuelmelo00@hotmail.com>
2020-11-23 10:38:23 -0300

    Bug fix for share of search N/A benoit.

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilBenoitCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilBenoitCrawler.java
@@ -1,5 +1,8 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
 
+import br.com.lett.crawlernode.core.fetcher.models.Request;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import br.com.lett.crawlernode.core.session.Session;
@@ -13,31 +16,44 @@ public class BrasilBenoitCrawler extends CrawlerRankingKeywords {
   }
 
   @Override
+  protected Document fetchDocument(String url){
+
+     Request request = Request.RequestBuilder.create().setUrl(url).build();
+
+     return Jsoup.parse(this.dataFetcher.get(session,request).getBody());
+  }
+
+  @Override
   protected void extractProductsFromCurrentPage() {
     this.pageSize = 40;
+    Elements products;
+    String url = "https://www.benoit.com.br/pesquisa.partial?pg=" + this.currentPage + "&o=mais-relevantes&t=" + this.keywordEncoded;
+
+    if(this.currentPage == 1){
+
+       url = "https://www.benoit.com.br/pesquisa" + "?t=" + this.keywordEncoded;
+    }
 
     this.log("Página " + this.currentPage);
-
-    String url = "https://www.benoit.com.br/pesquisa.partial?pg=" + this.currentPage + "&o=mais-relevantes&t=" + this.keywordEncoded;
 
     this.log("Link onde são feitos os crawlers: " + url);
     this.currentDoc = fetchDocument(url);
 
-    Elements products = this.currentDoc.select("#wd6 ul li");
+    if(this.currentPage == 1){
+       products = this.currentDoc.select(".wd-browsing-grid-list ul li");
+       setTotalProducts();
+    } else{
+       products = this.currentDoc.select(".wd-content ul li");
+    }
 
     if (!products.isEmpty()) {
-
-      if (this.totalProducts == 0) {
-        setTotalProducts();
-      }
 
       for (Element e : products) {
 
         String internalPid = CrawlerUtils.scrapStringSimpleInfoByAttribute(e, "div[data-pid]", "data-pid");
-        String productUrl = CrawlerUtils.scrapUrl(e, "h3[class=\"name\"] a", "href", "https:", "www.benoit.com.br");
+        String productUrl = CrawlerUtils.scrapUrl(e, "div[data-pid] a", "href", "https:", "www.benoit.com.br");
 
         saveDataProduct(null, internalPid, productUrl);
-
 
         this.log("Position: " + this.position + " - InternalId: " + null + " - InternalPid: " + internalPid + " - Url: " + productUrl);
         if (this.arrayProducts.size() == productsLimit)
