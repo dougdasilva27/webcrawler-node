6457584198fc84defa08f223e0b49fcaa5087b08
matheus bussolotti <matheusbussolotti@gmail.com>
2020-12-11 12:11:48 -0300

    change IdLoja

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/votuporanga/VotuporangaSitemercadoservetodossantacruzCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/votuporanga/VotuporangaSitemercadoservetodossantacruzCrawler.java
@@ -1,25 +1,24 @@
 package br.com.lett.crawlernode.crawlers.corecontent.votuporanga;
 
-import java.util.HashMap;
-import java.util.Map;
-import org.json.JSONObject;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.crawlers.extractionutils.core.BrasilSitemercadoCrawler;
 import br.com.lett.crawlernode.util.CommonMethods;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author gabriel date: 2019-09-24
  */
 public class VotuporangaSitemercadoservetodossantacruzCrawler extends BrasilSitemercadoCrawler {
 
+   public static final String HOME_PAGE = "https://www.sitemercado.com.br/santacruz/votuporanga-loja-tocantins-vila-muniz-rua-amazonas";
+   public static final int IDLOJA = 4673;
+   public static final int IDREDE = 2;
    public VotuporangaSitemercadoservetodossantacruzCrawler(Session session) {
       super(session);
    }
-
-   public static final String HOME_PAGE = "https://www.sitemercado.com.br/santacruz/votuporanga-loja-tocantins-vila-muniz-rua-amazonas";
-
-   public static final int IDLOJA = 20;
-   public static final int IDREDE = 2;
 
    @Override
    protected String getHomePage() {
