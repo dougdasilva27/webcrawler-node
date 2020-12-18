3bfcaf5a2cf498d8dc144b09897e37055aa46732
Gabriel Dornelas <gabrieldornelas@lett.digital>
2020-12-18 16:41:21 -0300

    CU-2q1abv - Share of search N/A na Beleza na Web - L'oreal

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilBelezanawebCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilBelezanawebCrawler.java
@@ -1,87 +1,72 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
 
-import br.com.lett.crawlernode.core.fetcher.methods.ApacheDataFetcher;
-import br.com.lett.crawlernode.core.fetcher.methods.FetcherDataFetcher;
-import br.com.lett.crawlernode.core.fetcher.methods.JavanetDataFetcher;
-import br.com.lett.crawlernode.core.fetcher.models.Request;
+import org.json.JSONObject;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
 import br.com.lett.crawlernode.util.CrawlerUtils;
-import java.util.Collections;
-import org.json.JSONObject;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 public class BrasilBelezanawebCrawler extends CrawlerRankingKeywords {
 
-  public BrasilBelezanawebCrawler(Session session) {
-    super(session);
-  }
+   public BrasilBelezanawebCrawler(Session session) {
+      super(session);
+   }
 
-  @Override
-  protected void extractProductsFromCurrentPage() {
-    this.pageSize = 36;
+   @Override
+   protected void extractProductsFromCurrentPage() {
+      this.pageSize = 36;
 
-    this.log("Página " + this.currentPage);
+      this.log("Página " + this.currentPage);
 
-    String url = "https://www.belezanaweb.com.br/busca?q=" + this.keywordEncoded + "&pagina="
-        + this.currentPage;
-    this.log("Link onde são feitos os crawlers: " + url);
+      String url = "https://www.belezanaweb.com.br/busca?q=" + this.keywordEncoded + "&pagina="
+            + this.currentPage;
+      this.log("Link onde são feitos os crawlers: " + url);
 
-    this.currentDoc = fetchDocument(url);
+      this.currentDoc = fetchDocument(url);
 
-    Elements products = this.currentDoc.select(".showcase-item");
+      Elements products = this.currentDoc.select(".showcase-item");
 
-    if (!products.isEmpty()) {
-      if (this.totalProducts == 0) {
-        setTotalProducts();
+      if (!products.isEmpty()) {
+         if (this.totalProducts == 0) {
+            setTotalProducts();
+         }
+         for (Element e : products) {
+
+            JSONObject json = CrawlerUtils.stringToJson(e.attr("data-event"));
+            String internalId = json.optString("sku", null);
+            String productUrl = CrawlerUtils.scrapStringSimpleInfoByAttribute(e, "a.showcase-item-title", "href");
+
+            saveDataProduct(internalId, null, productUrl);
+
+            this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: "
+                  + null + " - Url: " + productUrl);
+            if (this.arrayProducts.size() == productsLimit) {
+               break;
+            }
+
+         }
+      } else {
+         this.result = false;
+         this.log("Keyword sem resultado!");
       }
-      for (Element e : products) {
 
-        JSONObject json = CrawlerUtils.stringToJson(e.attr("data-event"));
-        String internalId = json.optString("sku", null);
-        String productUrl = CrawlerUtils.scrapStringSimpleInfoByAttribute(e, "a.showcase-item-title", "href");
+      this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora "
+            + this.arrayProducts.size() + " produtos crawleados");
+   }
 
-        saveDataProduct(internalId, null, productUrl);
+   @Override
+   protected void setTotalProducts() {
+      Element totalElement = this.currentDoc.selectFirst(".pagination-total strong");
 
-        this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: "
-            + null + " - Url: " + productUrl);
-        if (this.arrayProducts.size() == productsLimit) {
-          break;
-        }
+      if (totalElement != null) {
+         String total = totalElement.ownText().replaceAll("[^0-9]", "").trim();
 
+         if (!total.isEmpty()) {
+            this.totalProducts = Integer.parseInt(total);
+         }
       }
-    } else {
-      this.result = false;
-      this.log("Keyword sem resultado!");
-    }
 
-    this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora "
-        + this.arrayProducts.size() + " produtos crawleados");
-  }
-
-  @Override
-  protected void setTotalProducts() {
-    Element totalElement = this.currentDoc.selectFirst(".pagination-total strong");
-
-    if (totalElement != null) {
-      String total = totalElement.ownText().replaceAll("[^0-9]", "").trim();
-
-      if (!total.isEmpty()) {
-        this.totalProducts = Integer.parseInt(total);
-      }
-    }
-
-    this.log("Total da busca: " + this.totalProducts);
-  }
-
-  @Override
-   protected Document fetchDocument(String url){
-
-     Request request = Request.RequestBuilder.create().setUrl(url).build();
-
-     return Jsoup.parse(new JavanetDataFetcher().get(session, request).getBody());
-  }
+      this.log("Total da busca: " + this.totalProducts);
+   }
 }
