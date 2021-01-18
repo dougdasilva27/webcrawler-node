22872bebf3271dfec9f6e13600c1d90aa75a041d
Thainá <thainaj.aguiar@gmail.com>
2021-01-18 14:32:07 -0300

    adding the brand to the product name and removing the need to capture pid for request

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/chile/ChileJumbolascondesCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/chile/ChileJumbolascondesCrawler.java
@@ -2,6 +2,8 @@ package br.com.lett.crawlernode.crawlers.corecontent.chile;
 
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.crawlers.extractionutils.core.ChileJumboCrawler;
+import org.json.JSONObject;
+import org.jsoup.nodes.Document;
 
 public class ChileJumbolascondesCrawler extends ChileJumboCrawler {
    public static final String CODE_LOCATE = "11";
@@ -13,5 +15,16 @@ public class ChileJumbolascondesCrawler extends ChileJumboCrawler {
    @Override
    protected String getCodeLocate() {
       return CODE_LOCATE;
+   }
+
+   @Override
+   protected String scrapName(Document doc, JSONObject productJson, JSONObject jsonSku) {
+      String name = super.scrapName(doc, productJson, jsonSku);
+      String brand = productJson.has("brand") ? productJson.get("brand").toString(): null;
+      String nameWithBrand = null;
+     if (name != null && brand != null){
+         nameWithBrand = name + " - " + brand;
+     }
+      return nameWithBrand;
    }
 }
