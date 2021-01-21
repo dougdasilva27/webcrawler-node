e57f824d7c6904a817cc95422a1482abc0ba7f82
Layunne <fellypelayunne@gmail.com>
2021-01-21 12:11:17 -0300

    jumbo-chile: refactor ranking

--- src/java/br/com/lett/crawlernode/crawlers/extractionutils/ranking/ChileJumboCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/extractionutils/ranking/ChileJumboCrawler.java
@@ -1,17 +1,25 @@
 package br.com.lett.crawlernode.crawlers.extractionutils.ranking;
 
+import br.com.lett.crawlernode.core.fetcher.ProxyCollection;
 import br.com.lett.crawlernode.core.fetcher.methods.ApacheDataFetcher;
+import br.com.lett.crawlernode.core.fetcher.methods.FetcherDataFetcher;
+import br.com.lett.crawlernode.core.fetcher.methods.JsoupDataFetcher;
 import br.com.lett.crawlernode.core.fetcher.models.Request;
 import br.com.lett.crawlernode.core.fetcher.models.Request.RequestBuilder;
+import br.com.lett.crawlernode.core.fetcher.models.Response;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
 import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.JSONUtils;
 import br.com.lett.crawlernode.util.Logging;
 import org.apache.http.impl.cookie.BasicClientCookie;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.net.URLEncoder;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 public class ChileJumboCrawler extends CrawlerRankingKeywords {
@@ -28,28 +36,20 @@ public class ChileJumboCrawler extends CrawlerRankingKeywords {
    protected void extractProductsFromCurrentPage() {
       this.log("Página " + this.currentPage);
 
-      this.pageSize = 12;
+      this.pageSize = 40;
 
-      String url = "https://" + HOST + "/busca/?ft=" + keywordWithoutAccents.replace(" ", "%20") +
-         "&page=" + currentPage;
-      String apiUrl = "https://api.smdigital.cl:8443/v0/cl/jumbo/vtex/front/prod/proxy/api/v2/products/search/busca?ft=" +
-         keywordWithoutAccents.replace(" ", "%20") +
-         "&page=" + currentPage +
-         "&sc=" + storeCode;
+      JSONObject bodyJson = fetchProducts();
 
-      Map<String, String> headers = new HashMap<>();
-      headers.put("Referer", url);
-      headers.put("x-api-key", API_KEY);
+      if (this.currentPage == 1) {
+         this.totalProducts = bodyJson.optInt("recordsFiltered");
+      }
 
-      Request request = RequestBuilder.create().setUrl(apiUrl).setHeaders(headers).setCookies(cookies).build();
-      String res = new ApacheDataFetcher().get(session, request).getBody();
+      JSONArray products = JSONUtils.getJSONArrayValue(bodyJson, "products");
 
-      JSONArray products = CrawlerUtils.stringToJsonArray(res);
-
-      if (products.length() > 0) {
-         for (Object e : products) {
-            JSONObject product = (JSONObject) e;
-            String internalPid = product.has("productId") && !product.isNull("productId") ? product.get("productId").toString() : null;
+      if (!products.isEmpty()) {
+         for (Object o : products) {
+            JSONObject product = (JSONObject) o;
+            String internalPid = product.optString("productReference", null);
             String productUrl = crawlProductUrl(product);
 
             saveDataProduct(null, internalPid, productUrl);
@@ -66,29 +66,43 @@ public class ChileJumboCrawler extends CrawlerRankingKeywords {
       this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
    }
 
+   private JSONObject fetchProducts() {
+      String url = "https://apijumboweb.smdigital.cl/catalog/api/v1/search/" + keywordWithoutAccents.toLowerCase().replace(" ", "%20") + "?page=" + this.currentPage;
+
+      Map<String, String> headers = new HashMap<>();
+      headers.put("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36");
+      headers.put("x-api-key", API_KEY);
+      headers.put("content-type", "application/json");
+      headers.put("accept", "*/*");
+      headers.put("origin", "https://www.jumbo.cl");
+      headers.put("referer", "https://www.jumbo.cl/");
+
+      String payload = "{\"selectedFacets\":[{\"key\":\"trade-policy\",\"value\":\""+getStoreCode()+"\"}]}";
+
+      Request request = RequestBuilder.create()
+         .setUrl(url)
+         .setHeaders(headers)
+         .setPayload(payload)
+         .setProxyservice(
+            Arrays.asList(
+               ProxyCollection.BUY_HAPROXY,
+               ProxyCollection.LUMINATI_SERVER_BR_HAPROXY,
+               ProxyCollection.INFATICA_RESIDENTIAL_BR_HAPROXY
+               )
+         )
+         .build();
+
+      Response response = new JsoupDataFetcher().post(session, request);
+
+      return CrawlerUtils.stringToJson(response.getBody());
+   }
+
    protected String getStoreCode() {
       return storeCode;
    }
 
-   @Override
-   protected void processBeforeFetch() {
-      super.processBeforeFetch();
-
-      Logging.printLogDebug(logger, session, "Adding cookie...");
-
-      BasicClientCookie cookie = new BasicClientCookie("VTEXSC", "sc=" + storeCode);
-      cookie.setDomain("." + ChileJumboCrawler.HOST);
-      cookie.setPath("/");
-      this.cookies.add(cookie);
-   }
-
-   @Override
-   protected boolean hasNextPage() {
-      return !this.arrayProducts.isEmpty() && ((this.arrayProducts.size() / this.currentPage) >= this.pageSize);
-   }
-
    private String crawlProductUrl(JSONObject product) {
-      String productUrl = product.has("linkText") && !product.isNull("linkText") ? product.get("linkText").toString() : null;
+      String productUrl = product.optString("linkText", null);
 
       if (productUrl != null) {
          productUrl += productUrl.endsWith("/p") ? "" : "/p";
