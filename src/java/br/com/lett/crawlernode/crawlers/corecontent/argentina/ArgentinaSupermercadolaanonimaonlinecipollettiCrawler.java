1cca1236357491dd56ffe86cf6f593da5d0e9be0
matheus bussolotti <matheusbussolotti@gmail.com>
2021-01-26 14:38:00 -0300

    new crawler

---
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/argentina/ArgentinaSupermercadolaanonimaonlinecipollettiCrawler.java
@@ -0,0 +1,166 @@
+package br.com.lett.crawlernode.crawlers.corecontent.argentina;
+
+import br.com.lett.crawlernode.core.models.CategoryCollection;
+import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.models.ProductBuilder;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.Crawler;
+import br.com.lett.crawlernode.util.CommonMethods;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.Logging;
+import exceptions.MalformedPricingException;
+import exceptions.OfferException;
+import models.AdvancedRatingReview;
+import models.Offer;
+import models.Offers;
+import models.RatingsReviews;
+import models.pricing.CreditCards;
+import models.pricing.Pricing;
+import org.apache.http.impl.cookie.BasicClientCookie;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArgentinaSupermercadolaanonimaonlinecipollettiCrawler extends Crawler {
+   private static final String HOME_PAGE = "https://supermercado.laanonimaonline.com/";
+   private static final String SELLER_FULL_NAME = "La anonima";
+
+
+   public ArgentinaSupermercadolaanonimaonlinecipollettiCrawler(Session session) {
+      super(session);
+      super.config.setMustSendRatingToKinesis(true);
+   }
+
+   @Override
+   public void handleCookiesBeforeFetch() {
+
+      BasicClientCookie cookie = new BasicClientCookie("laanonimasucursalnombre", "CIPOLLETTI");
+      cookie.setDomain("www.laanonimaonline.com");
+      cookie.setPath("/");
+      this.cookies.add(cookie);
+
+
+      BasicClientCookie cookie2 = new BasicClientCookie("laanonimasucursal", "22");
+      cookie2.setDomain("www.laanonimaonline.com");
+      cookie2.setPath("/");
+      this.cookies.add(cookie2);
+   }
+
+   @Override
+   public List<Product> extractInformation(Document doc) throws Exception {
+      super.extractInformation(doc);
+      List<Product> products = new ArrayList<>();
+
+      if (isProductPage(doc)) {
+         Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
+
+         String internalId = CrawlerUtils.scrapStringSimpleInfoByAttribute(doc, "#id_item", "value");
+         String internalPid = crawlInternalPid(doc);
+         String name = CrawlerUtils.scrapStringSimpleInfo(doc, ".titulo_producto.principal", true);
+         boolean available = crawlAvailability(doc);
+         Offers offers = available ? crawlOffers(doc) : new Offers();
+         CategoryCollection categories = crawlCategories(doc);
+         String primaryImage = crawlPrimaryImage(doc);
+         List<String> secondaryImages = crawlSecondaryImages(doc, primaryImage);
+
+         // Creating the product
+         Product product = ProductBuilder.create()
+            .setUrl(session.getOriginalURL())
+            .setInternalId(internalId)
+            .setInternalPid(internalPid)
+            .setName(name)
+            .setOffers(offers)
+            .setCategory1(categories.getCategory(0))
+            .setCategory2(categories.getCategory(1))
+            .setCategory3(categories.getCategory(2))
+            .setPrimaryImage(primaryImage)
+            .setSecondaryImages(secondaryImages)
+            .build();
+
+         products.add(product);
+
+      } else {
+         Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
+      }
+
+      return products;
+
+   }
+
+   private Offers crawlOffers(Document doc) throws OfferException, MalformedPricingException {
+      Offers offers = new Offers();
+      Pricing pricing = crawPricing(doc);
+
+      offers.add(Offer.OfferBuilder.create()
+         .setUseSlugNameAsInternalSellerId(true)
+         .setSellerFullName(SELLER_FULL_NAME)
+         .setMainPagePosition(1)
+         .setIsBuybox(false)
+         .setIsMainRetailer(true)
+         .setPricing(pricing)
+         .build());
+
+      return offers;
+   }
+
+   private Pricing crawPricing(Document doc) throws MalformedPricingException {
+      Double spotlightPrice = crawlPrice(doc);
+
+
+      CreditCards creditCards = new CreditCards();
+
+      return Pricing.PricingBuilder.create()
+         .setSpotlightPrice(spotlightPrice)
+         .setCreditCards(creditCards)
+         .build();
+   }
+
+   private boolean isProductPage(Document doc) {
+      return doc.selectFirst("#id_item") != null;
+   }
+
+
+   private String crawlInternalPid(Document doc) {
+      String internalPid = CrawlerUtils.scrapStringSimpleInfoByAttribute(doc, "div[id*=prod_]", "id");
+      return CommonMethods.getLast(internalPid.split("prod_"));
+   }
+
+
+   private Double crawlPrice(Document doc) {
+      return CrawlerUtils.scrapDoublePriceFromHtml(doc, ".precio.destacado", null, false, ',', session);
+   }
+
+   private boolean crawlAvailability(Document doc) {
+      String style = CrawlerUtils.scrapStringSimpleInfoByAttribute(doc, "div[id*=btnagregarcarritosinstock]", "style");
+      return style != null && style.contains("display:none");
+   }
+
+
+   private String crawlPrimaryImage(Document doc) {
+      return CrawlerUtils.scrapSimplePrimaryImage(doc, "#img_producto img", Arrays.asList("src"), "https:", "d1on8qs0xdu5jz.cloudfront.net");
+   }
+
+   private List<String> crawlSecondaryImages(Document doc, String primaryImage) {
+      return CrawlerUtils.scrapSecondaryImages(doc, "#galeria_img div a", Arrays.asList("href"), "https:", "d1on8qs0xdu5jz.cloudfront.net", primaryImage);
+   }
+
+   private CategoryCollection crawlCategories(Document doc) {
+      CategoryCollection categories = new CategoryCollection();
+
+      Elements elementCategories = doc.select(".barra_navegacion a.link_navegacion");
+      for (Element e : elementCategories) {
+         categories.add(e.text().trim());
+      }
+
+      return categories;
+   }
+
+
+
+
+
+}
---
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/argentina/ArgentinaSupermercadolaanonimaonlinecipollettiCrawler.java
@@ -0,0 +1,104 @@
+package br.com.lett.crawlernode.crawlers.ranking.keywords.argentina;
+
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.util.CommonMethods;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.cookie.BasicClientCookie;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArgentinaSupermercadolaanonimaonlinecipollettiCrawler extends CrawlerRankingKeywords {
+
+   private List<Cookie> cookies = new ArrayList<>();
+
+   public ArgentinaSupermercadolaanonimaonlinecipollettiCrawler(Session session) {
+      super(session);
+   }
+
+   @Override
+   protected void processBeforeFetch() {
+      // Criando cookie da cidade CABA
+      BasicClientCookie cookie = new BasicClientCookie("laanonimasucursalnombre", "CIPOLLETTI");
+      cookie.setDomain("www.laanonimaonline.com");
+      cookie.setPath("/");
+      this.cookies.add(cookie);
+
+      // Criando cookie da regiao sao nicolas
+      BasicClientCookie cookie2 = new BasicClientCookie("laanonimasucursal", "22");
+      cookie2.setDomain("www.laanonimaonline.com");
+      cookie2.setPath("/");
+      this.cookies.add(cookie2);
+   }
+
+   @Override
+   protected void extractProductsFromCurrentPage() {
+      // número de produtos por página do market
+      this.pageSize = 20;
+
+      this.log("Página " + this.currentPage);
+
+      String url = "http://supermercado.laanonimaonline.com/buscar?pag=" + this.currentPage + "&clave=" + this.keywordWithoutAccents.replace(" ", "%20");
+      this.log("Link onde são feitos os crawlers: " + url);
+
+      this.currentDoc = fetchDocument(url, cookies);
+      Elements products = this.currentDoc.select(".caja1.producto > div");
+
+      if (!products.isEmpty()) {
+
+         for (Element e : products) {
+            String productUrl = crawlProductUrl(e);
+            String internalId = crawlInternalId(productUrl);
+
+            saveDataProduct(internalId, null, productUrl);
+
+            this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + null + " - Url: " + productUrl);
+            if (this.arrayProducts.size() == productsLimit) {
+               break;
+            }
+
+         }
+      } else {
+         this.result = false;
+         this.log("Keyword sem resultado!");
+      }
+
+      this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
+   }
+
+   @Override
+   protected boolean hasNextPage() {
+      return true;
+   }
+
+   private String crawlInternalId(String url) {
+      String internalId = null;
+
+      if (url.contains("art_")) {
+         internalId = CommonMethods.getLast(url.split("art_"));
+
+         if (internalId.contains("?")) {
+            internalId = internalId.split("\\?")[0];
+         }
+
+         if (internalId.contains("/")) {
+            internalId = internalId.split("\\/")[0];
+         }
+      }
+
+      return internalId;
+   }
+
+   private String crawlProductUrl(Element e) {
+
+      String url = CrawlerUtils.scrapStringSimpleInfoByAttribute(e,".titulo_puntos a","href");
+      String productUrl = CrawlerUtils.completeUrl(url,"https:","www.supermercado.laanonimaonline.com");
+
+      return productUrl;
+   }
+}
