d4df4984b6e2f000c7595e420000a1126baf9ca6
Layunne <fellypelayunne@gmail.com>
2021-01-29 15:53:57 -0300

    Test2 cleanup

--- src/java/br/com/lett/crawlernode/test/Test2.kt
+++ src/java/br/com/lett/crawlernode/test/Test2.kt
@@ -1,20 +1,5 @@
 package br.com.lett.crawlernode.test
 
-import br.com.lett.crawlernode.core.fetcher.ProxyCollection
-import br.com.lett.crawlernode.core.models.Market
-import br.com.lett.crawlernode.core.models.Markets
-import br.com.lett.crawlernode.core.session.Session
-import br.com.lett.crawlernode.core.session.SessionFactory
-import br.com.lett.crawlernode.core.session.SessionFactory.createTestSession
-import br.com.lett.crawlernode.core.task.base.TaskFactory
-import br.com.lett.crawlernode.database.DatabaseCredentialsSetter
-import br.com.lett.crawlernode.database.DatabaseDataFetcher
-import br.com.lett.crawlernode.database.DatabaseManager
-import br.com.lett.crawlernode.main.ExecutionParameters
-import br.com.lett.crawlernode.main.GlobalConfigurations
-import br.com.lett.crawlernode.processor.ResultManager
-import br.com.lett.crawlernode.test.Test.*
-
 enum class TestType {
    KEYWORDS, INSIGHTS
 }
