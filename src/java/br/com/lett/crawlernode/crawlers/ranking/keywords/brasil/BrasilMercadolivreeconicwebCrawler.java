e8c517b39e41a72cb3c30e94aa86a20b2bad57c8
Thainá <thainaj.aguiar@gmail.com>
2021-02-01 18:00:14 -0300

    bugfix/CU-390ch8_50-de-falso-void-na-Mercado-livre-tudo-de-bicho---Bayer

---
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilMercadolivreeconicwebCrawler.java
@@ -0,0 +1,17 @@
+package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
+
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.crawlers.extractionutils.ranking.MercadolivreCrawler;
+
+public class BrasilMercadolivreeconicwebCrawler extends MercadolivreCrawler {
+
+   private final String URL = "https://lista.mercadolivre.com.br/" + this.keywordWithoutAccents.replace(" ", "-") + "_eshop_e-conic+web#D[A:"
+      + this.keywordWithoutAccents.replace(" ", "+") + ",on]";
+
+   public BrasilMercadolivreeconicwebCrawler(Session session) {
+      super(session);
+      super.setUrl(URL);
+      super.setProductUrlHost("produto.mercadolivre.com.br");
+      super.setNextUrlHost("lista.mercadolivre.com.br");
+   }
+}
