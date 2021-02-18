f09ce1f0f58961de5277e463ec35c3679a9a0cd6
Marcos Moura <marcosmourabro@gmail.com>
2021-02-18 11:47:07 -0300

    open farma
    variation

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/argentina/ArgentinaOpenfarmaCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/argentina/ArgentinaOpenfarmaCrawler.java
@@ -25,13 +25,15 @@ import models.pricing.Installment.InstallmentBuilder;
 import models.pricing.Installments;
 import models.pricing.Pricing;
 import models.pricing.Pricing.PricingBuilder;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class ArgentinaOpenfarmaCrawler extends Crawler {
 
    private final String HOME_PAGE = "https://www.openfarma.com.ar/";
    private static final String SELLER_FULL_NAME = "Open farma";
    protected Set<String> cards = Sets.newHashSet(Card.VISA.toString(), Card.MASTERCARD.toString(),
-         Card.AURA.toString(), Card.DINERS.toString(), Card.HIPER.toString(), Card.AMEX.toString());
+      Card.AURA.toString(), Card.DINERS.toString(), Card.HIPER.toString(), Card.AMEX.toString());
 
 
    public ArgentinaOpenfarmaCrawler(Session session) {
@@ -53,33 +55,60 @@ public class ArgentinaOpenfarmaCrawler extends Crawler {
       if (isProductPage(doc)) {
          Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
 
-         String internalId = CrawlerUtils.scrapStringSimpleInfoByAttribute(doc, ".wrapper", "data-product-id");
-         String internalPid = internalId;
+         String internalPid = CrawlerUtils.scrapStringSimpleInfoByAttribute(doc, ".wrapper", "data-product-id");
          String name = CrawlerUtils.scrapStringSimpleInfo(doc, ".row .product-name", false);
-         String primaryImage = CrawlerUtils.scrapSimplePrimaryImage(doc, "#zoom .item img",
-               Arrays.asList("src"), "https://", HOME_PAGE);
-         String secondaryImages = CrawlerUtils.scrapSimpleSecondaryImages(doc, "#zoom .slider-for .item:not(.default-image) img",
-               Arrays.asList("src"), "https://", HOME_PAGE, primaryImage);
          String description = CrawlerUtils.scrapStringSimpleInfo(doc, ".row article", false);
-         Integer stock = null;
-         boolean availableToBuy = doc.selectFirst(".row .btn-submit") != null;
-         Offers offers = availableToBuy ? offers(doc) : new Offers();
 
-         // Creating the product
-         Product product = ProductBuilder.create()
+         if(checkIfHasVariation(doc)) {
+            Elements variatons = doc.select(".product-variations-container .btn-group.mg-lg-b");
+
+            for (Element e : variatons) {
+
+               String internalId = CrawlerUtils.scrapStringSimpleInfoByAttribute(e, "a", "data-variant-id");
+               String primaryImage = CrawlerUtils.scrapSimplePrimaryImage(doc, "#zoom .item img",
+                  Arrays.asList("src"), "https://", HOME_PAGE);
+               String secondaryImages = CrawlerUtils.scrapSimpleSecondaryImages(doc, "#zoom .slider-for .item:not(.default-image) img",
+                  Arrays.asList("src"), "https://", HOME_PAGE, primaryImage);
+               boolean availableToBuy = doc.selectFirst(".row .btn-submit") != null;
+               Offers offers = availableToBuy ? offers(doc, e) : new Offers();
+
+               // Creating the product
+               Product product = ProductBuilder.create()
+                  .setUrl(session.getOriginalURL())
+                  .setInternalId(internalId)
+                  .setInternalPid(internalPid)
+                  .setName(name)
+                  .setPrimaryImage(primaryImage)
+                  .setSecondaryImages(secondaryImages)
+                  .setDescription(description)
+                  .setOffers(offers)
+                  .build();
+
+               products.add(product);
+            }
+         } else {
+
+            String primaryImage = CrawlerUtils.scrapSimplePrimaryImage(doc, "#zoom .item img",
+               Arrays.asList("src"), "https://", HOME_PAGE);
+            String secondaryImages = CrawlerUtils.scrapSimpleSecondaryImages(doc, "#zoom .slider-for .item:not(.default-image) img",
+               Arrays.asList("src"), "https://", HOME_PAGE, primaryImage);
+            boolean availableToBuy = doc.selectFirst(".row .btn-submit") != null;
+            Offers offers = availableToBuy ? offers(doc, null) : new Offers();
+
+            // Creating the product
+            Product product = ProductBuilder.create()
                .setUrl(session.getOriginalURL())
-               .setInternalId(internalId)
+               .setInternalId(internalPid)
                .setInternalPid(internalPid)
                .setName(name)
                .setPrimaryImage(primaryImage)
                .setSecondaryImages(secondaryImages)
                .setDescription(description)
-               .setStock(stock)
                .setOffers(offers)
                .build();
 
-         products.add(product);
-
+            products.add(product);
+         }
       } else {
          Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
       }
@@ -92,22 +121,24 @@ public class ArgentinaOpenfarmaCrawler extends Crawler {
       return doc.select("#product").first() != null;
    }
 
-
-   private Offers offers(Document doc) throws MalformedPricingException, OfferException {
+   private boolean checkIfHasVariation(Document doc){
+      return doc.selectFirst(".product-variations-container") != null;
+   }
+   private Offers offers(Document doc, Element variation) throws MalformedPricingException, OfferException {
       Offers offers = new Offers();
-      Pricing pricing = scrapPricing(doc);
+      Pricing pricing = scrapPricing(doc, variation);
       List<String> sales = scrapSales(doc);
 
 
       offers.add(OfferBuilder.create()
-            .setUseSlugNameAsInternalSellerId(true)
-            .setSellerFullName(SELLER_FULL_NAME)
-            .setMainPagePosition(1)
-            .setIsBuybox(false)
-            .setIsMainRetailer(true)
-            .setPricing(pricing)
-            .setSales(sales)
-            .build());
+         .setUseSlugNameAsInternalSellerId(true)
+         .setSellerFullName(SELLER_FULL_NAME)
+         .setMainPagePosition(1)
+         .setIsBuybox(false)
+         .setIsMainRetailer(true)
+         .setPricing(pricing)
+         .setSales(sales)
+         .build());
 
       return offers;
 
@@ -121,26 +152,39 @@ public class ArgentinaOpenfarmaCrawler extends Crawler {
 
       if (salesString != null && !salesString.isEmpty()) {
          sales.add(salesString + "% OFF"); // On HTML this info appears like this: has-promo promo-20-off so a have to make this little
-                                           // adjustment to show sales like client see
+         // adjustment to show sales like client see
       }
 
       return sales;
    }
 
-   private Pricing scrapPricing(Document doc) throws MalformedPricingException {
-      Double priceFrom = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".prices .promo", null, false, ',', session);
-      Double spotlightPrice = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".prices .regular", null, false, ',', session);
+   private Pricing scrapPricing(Document doc, Element variation) throws MalformedPricingException {
+      Double priceFrom;
+      Double spotlightPrice;
+
+      if (variation != null){
+
+         priceFrom = CrawlerUtils.scrapDoublePriceFromHtml(variation, "a", "data-display-price", false, '.', session);
+         spotlightPrice = CrawlerUtils.scrapDoublePriceFromHtml(variation, "a", "data-price", false, '.', session);
+
+      } else {
+
+         priceFrom = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".prices .promo", null, false, ',', session);
+         spotlightPrice = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".prices .regular", null, false, ',', session);
+
+      }
+
       CreditCards creditCards = scrapCreditCards(doc, spotlightPrice);
       BankSlip bankslip = BankSlipBuilder.create()
-            .setFinalPrice(spotlightPrice)
-            .build();
+         .setFinalPrice(spotlightPrice)
+         .build();
 
       return PricingBuilder.create()
-            .setPriceFrom(priceFrom)
-            .setSpotlightPrice(spotlightPrice)
-            .setBankSlip(bankslip)
-            .setCreditCards(creditCards)
-            .build();
+         .setPriceFrom(priceFrom)
+         .setSpotlightPrice(spotlightPrice)
+         .setBankSlip(bankslip)
+         .setCreditCards(creditCards)
+         .build();
 
    }
 
@@ -150,17 +194,17 @@ public class ArgentinaOpenfarmaCrawler extends Crawler {
       Installments installments = scrapInstallment(spotlightPrice);
       if (installments.getInstallments().isEmpty()) {
          installments.add(InstallmentBuilder.create()
-               .setInstallmentNumber(1)
-               .setInstallmentPrice(spotlightPrice)
-               .build());
+            .setInstallmentNumber(1)
+            .setInstallmentPrice(spotlightPrice)
+            .build());
       }
 
       for (String card : cards) {
          creditCards.add(CreditCardBuilder.create()
-               .setBrand(card)
-               .setInstallments(installments)
-               .setIsShopCard(false)
-               .build());
+            .setBrand(card)
+            .setInstallments(installments)
+            .setIsShopCard(false)
+            .build());
       }
 
       return creditCards;
@@ -171,9 +215,9 @@ public class ArgentinaOpenfarmaCrawler extends Crawler {
       Installments installments = new Installments();
 
       installments.add(InstallmentBuilder.create()
-            .setInstallmentNumber(1)
-            .setInstallmentPrice(spotlightPrice)
-            .build());
+         .setInstallmentNumber(1)
+         .setInstallmentPrice(spotlightPrice)
+         .build());
 
       return installments;
    }
