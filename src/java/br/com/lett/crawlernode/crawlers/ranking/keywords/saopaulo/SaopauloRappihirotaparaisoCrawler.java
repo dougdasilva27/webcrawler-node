ac270b4d4f13d9036205aa204969ac5efa973e43
Thainá <thainaj.aguiar@gmail.com>
2021-02-26 16:57:58 -0300

    changing to new rappi

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/saopaulo/SaopauloRappihirotaparaisoCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/saopaulo/SaopauloRappihirotaparaisoCrawler.java
@@ -1,18 +1,31 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.saopaulo;
 
 import br.com.lett.crawlernode.core.session.Session;
-import br.com.lett.crawlernode.crawlers.extractionutils.ranking.RappiCrawlerOld;
-import java.util.Arrays;
+import br.com.lett.crawlernode.crawlers.extractionutils.ranking.BrasilRappiCrawlerRanking;
 
 /**
  * Date: 19/10/20
  *
  * @author Fellype Layunne
  */
-public class SaopauloRappihirotaparaisoCrawler extends RappiCrawlerOld {
+public class SaopauloRappihirotaparaisoCrawler extends BrasilRappiCrawlerRanking {
 
    public SaopauloRappihirotaparaisoCrawler(Session session) {
-      super(session, Arrays.asList("900021834"));
+      super(session);
    }
+
+   public static final String STORE_ID = "900021834";
+   public static final String STORE_TYPE = "hiper";
+
+   @Override
+   protected String getStoreId() {
+      return STORE_ID;
+   }
+
+   @Override
+   protected String getStoreType() {
+      return STORE_TYPE;
+   }
+
 
 }
--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/saopaulo/SaopauloRappimambopinheirosCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/saopaulo/SaopauloRappimambopinheirosCrawler.java
@@ -1,18 +1,31 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.saopaulo;
 
 import br.com.lett.crawlernode.core.session.Session;
-import br.com.lett.crawlernode.crawlers.extractionutils.ranking.RappiCrawlerOld;
-import java.util.Arrays;
+import br.com.lett.crawlernode.crawlers.extractionutils.ranking.BrasilRappiCrawlerRanking;
 
 /**
  * Date: 19/10/20
  *
  * @author Fellype Layunne
  */
-public class SaopauloRappimambopinheirosCrawler extends RappiCrawlerOld {
+public class SaopauloRappimambopinheirosCrawler extends BrasilRappiCrawlerRanking {
 
    public SaopauloRappimambopinheirosCrawler(Session session) {
-      super(session, Arrays.asList("900020814"));
+      super(session);
    }
+
+   public static final String STORE_ID = "900020814";
+   public static final String STORE_TYPE = "hiper";
+
+   @Override
+   protected String getStoreId() {
+      return STORE_ID;
+   }
+
+   @Override
+   protected String getStoreType() {
+      return STORE_TYPE;
+   }
+
 
 }
