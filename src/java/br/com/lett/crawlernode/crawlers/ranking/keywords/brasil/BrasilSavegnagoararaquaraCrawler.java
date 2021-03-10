41d6f17f084bf3f29e728baf0ab5fd1ef5c96a2d
matheus <matheusbussolotti@gmail.com>
2021-03-10 17:13:28 -0300

    finish crawler

---
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilSavegnagoararaquaraCrawler.java
@@ -0,0 +1,13 @@
+package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
+
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.crawlers.extractionutils.core.SavegnagoCrawler;
+import br.com.lett.crawlernode.crawlers.extractionutils.ranking.SavegnagoRanking;
+
+public class BrasilSavegnagoararaquaraCrawler extends SavegnagoRanking {
+   public BrasilSavegnagoararaquaraCrawler(Session session) {
+      super(session);
+   }
+
+
+}
