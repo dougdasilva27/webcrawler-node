6baa9b35f0a4436a474904fbcfe7520d6543a618
Charles Fonseca <charlesjrfonseca@gmail.com>
2021-03-19 16:51:20 -0300

    refactoring, remove deprecated lib

--- src/java/br/com/lett/crawlernode/database/DiscordMessages.java
+++ src/java/br/com/lett/crawlernode/database/DiscordMessages.java
@@ -1,30 +1,33 @@
 package br.com.lett.crawlernode.database;
 
+import club.minnced.discord.webhook.WebhookClient;
+import club.minnced.discord.webhook.send.WebhookMessageBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.mrpowergamerbr.temmiewebhook.DiscordMessage;
-import com.mrpowergamerbr.temmiewebhook.TemmieWebhook;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.util.CommonMethods;
 import br.com.lett.crawlernode.util.Logging;
 
 /**
- * Classe onde são reportados os erros para o canal no slack
+ * Send price change to Discord
  */
-
 public class DiscordMessages {
 
-   private static Logger logger = LoggerFactory.getLogger(DiscordMessages.class);
+   private static final WebhookClient DISCORD_CLIENT = WebhookClient.withUrl("https://discord.com/api/webhooks/780417668830330922/U7ns_RyD1qhwpeysi46hNhbdLQP9fdB7aEftAkBZdrHxR4JIxsQoKfbO3tBI4LxU53RL");
+
+   private DiscordMessages() {
+   }
+
+   private static final Logger logger = LoggerFactory.getLogger(DiscordMessages.class);
 
    public static void reportPriceChanges(Session session, String msg, String author, String avatar) {
       try {
-         TemmieWebhook temmie = new TemmieWebhook("https://discord.com/api/webhooks/780417668830330922/U7ns_RyD1qhwpeysi46hNhbdLQP9fdB7aEftAkBZdrHxR4JIxsQoKfbO3tBI4LxU53RL");
-         DiscordMessage dm = new DiscordMessage();
-         dm.setUsername(author);
-         dm.setAvatarUrl(avatar);
-         dm.setContent(msg);
-         temmie.sendMessage(dm);
-         Logging.printLogDebug(logger, session, "Send one message to Discord.");
+
+         WebhookMessageBuilder builder = new WebhookMessageBuilder();
+         builder.setUsername(author);
+         builder.setAvatarUrl(avatar);
+         builder.setContent(msg);
+         DISCORD_CLIENT.send(builder.build());
       } catch (Exception ex) {
          Logging.printLogWarn(logger, session, CommonMethods.getStackTraceString(ex));
       }
