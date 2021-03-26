353987e7af8044abc1d52ad951482a3e6baf6f9c
matheus <matheusbussolotti@gmail.com>
2021-03-26 11:36:58 -0300

    clean code

--- src/java/br/com/lett/crawlernode/test/NewTest.java
+++ src/java/br/com/lett/crawlernode/test/NewTest.java
@@ -1,22 +1,12 @@
 package br.com.lett.crawlernode.test;
 
-import br.com.lett.crawlernode.core.models.Market;
-import br.com.lett.crawlernode.core.session.Session;
-import br.com.lett.crawlernode.core.task.base.Task;
-import br.com.lett.crawlernode.core.task.base.TaskFactory;
-import br.com.lett.crawlernode.database.DatabaseDataFetcher;
-import br.com.lett.crawlernode.main.GlobalConfigurations;
 import io.github.cdimascio.dotenv.Dotenv;
 
-import java.util.Arrays;
 import java.util.Collections;
 
 public class NewTest {
 
-   private static final String CORE_TEST = "CORE";
-   private static final String KEYWORDS_TEST = "KEYWORDS";
    private static final String DISCOVERY_TEST = "DISCOVERY";
-
 
    public static void main(String[] args) {
 
@@ -26,8 +16,6 @@ public class NewTest {
          .directory("src/java/br/com/lett/crawlernode/test")
          .filename("test.env")
          .load();
-
-      DatabaseDataFetcher fetcher = new DatabaseDataFetcher(GlobalConfigurations.dbManager);
 
       String testType = dotenv.get("TEST_TYPE");
       String marketId = dotenv.get("MARKET_ID");
