eefe1b8d906f23e66e270b15788cf6cfc85d31e3
matheus <matheusbussolotti@gmail.com>
2021-03-29 11:37:38 -0300

    Rappi fix id and change to new unification

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/colombia/ColombiaRappiexitobogotaCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/colombia/ColombiaRappiexitobogotaCrawler.java
@@ -4,12 +4,12 @@ import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.crawlers.extractionutils.core.ColombiaRappiCrawler;
 
 public class ColombiaRappiexitobogotaCrawler extends ColombiaRappiCrawler {
-
-   public static final String CEP = "Calle 134 #9-51 Bogota";
+   
    public static final String STORE_ID = "6660081";
 
    public ColombiaRappiexitobogotaCrawler(Session session) {
       super(session);
+      newUnification =true;
    }
 
    @Override
