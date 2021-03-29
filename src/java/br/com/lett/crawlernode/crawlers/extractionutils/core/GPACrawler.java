44ec411d261efc788fce7f66fdf9d1fb23d406fb
CostaLucasIV <lucascmm3@gmail.com>
2021-03-29 15:59:53 -0300

    json pricing capture fixed

--- src/java/br/com/lett/crawlernode/crawlers/extractionutils/core/GPACrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/extractionutils/core/GPACrawler.java
@@ -11,6 +11,7 @@ import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.Crawler;
 import br.com.lett.crawlernode.util.*;
 import com.google.common.collect.Sets;
+import com.mongodb.util.JSON;
 import exceptions.MalformedPricingException;
 import exceptions.OfferException;
 import models.AdvancedRatingReview;
@@ -149,7 +150,7 @@ public class GPACrawler extends Crawler {
          String secondaryImages = crawlSecondaryImages(jsonSku, primaryImage);
 
          String redirectedToURL = session.getRedirectedToURL(productUrl);
-         if (internalPid != null && redirectedToURL!= null && !redirectedToURL.isEmpty()) {
+         if (internalPid != null && redirectedToURL != null && !redirectedToURL.isEmpty()) {
             productUrl = redirectedToURL;
          }
 
@@ -699,11 +700,13 @@ public class GPACrawler extends Crawler {
       Offers offers = new Offers();
       if (data != null) {
          Pricing pricing = scrapPricing(data);
+         String sales = CrawlerUtils.calculateSales(pricing);
 
          if (pricing != null) {
             offers.add(Offer.OfferBuilder.create()
                .setUseSlugNameAsInternalSellerId(true)
                .setSellerFullName(MAIN_SELLER_NAME)
+               .setSales(Collections.singletonList(sales))
                .setSellersPagePosition(1)
                .setIsBuybox(false)
                .setIsMainRetailer(true)
@@ -715,11 +718,26 @@ public class GPACrawler extends Crawler {
    }
 
    private Pricing scrapPricing(JSONObject data) throws MalformedPricingException {
-      Double spotlightPrice = data.optDouble("currentPrice");
-      Double priceFrom = null;
-      if (data.has("priceFrom")) {
-         priceFrom = data.optDouble("priceFrom");
+      Double spotlightPrice = 0.0;
+      Double priceFrom = 0.0;
+
+      if (data.has("productPromotions")) {
+         JSONArray promotions = data.optJSONArray("productPromotions");
+         for (Object e : promotions) {
+            if(e instanceof JSONObject && ((JSONObject) e).optInt("ruleId") == 51241) {
+               spotlightPrice = ((JSONObject) e).optDouble("unitPrice");
+               priceFrom = data.optDouble("currentPrice");
+            }
+         }
+      } else {
+         spotlightPrice = data.optDouble("currentPrice");
+         priceFrom = null;
+
+         if (data.has("priceFrom")) {
+            priceFrom = data.optDouble("priceFrom");
+         }
       }
+
       CreditCards creditCards = scrapCreditCards(spotlightPrice);
 
       return PricingBuilder.create()
