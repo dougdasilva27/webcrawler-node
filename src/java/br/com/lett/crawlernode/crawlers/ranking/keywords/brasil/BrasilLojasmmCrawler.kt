fa82ca12824facc5d1b9049508a00b58a916188b
Charles Fonseca <charlesjrfonseca@gmail.com>
2021-03-31 15:17:30 -0300

    imports

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilLojasmmCrawler.kt
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilLojasmmCrawler.kt
@@ -1,5 +1,6 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil
 
+import br.com.lett.crawlernode.core.fetcher.FetchMode
 import br.com.lett.crawlernode.core.fetcher.models.Request
 import br.com.lett.crawlernode.core.session.Session
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords
