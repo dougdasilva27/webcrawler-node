ceec03cf69bca36242be9a7f5e941e0f62da156a
Charles Fonseca <charlesjrfonseca@gmail.com>
2021-04-13 20:14:23 -0300

    selector is product page change

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/saopaulo/SaopauloTendadriveCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/saopaulo/SaopauloTendadriveCrawler.java
@@ -1,10 +1,5 @@
 package br.com.lett.crawlernode.crawlers.corecontent.saopaulo;
 
-import java.util.ArrayList;
-import java.util.List;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.jsoup.nodes.Document;
 import br.com.lett.crawlernode.core.models.Card;
 import br.com.lett.crawlernode.core.models.Product;
 import br.com.lett.crawlernode.core.models.ProductBuilder;
@@ -24,6 +19,12 @@ import models.pricing.CreditCards;
 import models.pricing.Installment.InstallmentBuilder;
 import models.pricing.Installments;
 import models.pricing.Pricing;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.jsoup.nodes.Document;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Date: 04/09/17
@@ -54,7 +55,7 @@ public class SaopauloTendadriveCrawler extends Crawler {
       super.extractInformation(doc);
       List<Product> products = new ArrayList<>();
 
-      if (doc.selectFirst("#__NEXT_DATA__") != null) {
+      if (doc.selectFirst(".box-product .title") != null) {
 
          JSONObject jsonObject = JSONUtils.stringToJson(doc.selectFirst("#__NEXT_DATA__").data());
          JSONObject skuJson = (JSONObject) jsonObject.optQuery("/props/pageProps/product");
@@ -71,17 +72,17 @@ public class SaopauloTendadriveCrawler extends Crawler {
          RatingsReviews ratingsReviews = scrapRating(internalId, doc);
 
          Product product =
-               ProductBuilder.create()
-                     .setUrl(session.getOriginalURL())
-                     .setOffers(offers)
-                     .setInternalId(internalId)
-                     .setName(name)
-                     .setPrimaryImage(primaryImage)
-                     .setSecondaryImages(secondaryImages)
-                     .setDescription(description)
-                     .setStock(stock)
-                     .setRatingReviews(ratingsReviews)
-                     .build();
+                 ProductBuilder.create()
+                         .setUrl(session.getOriginalURL())
+                         .setOffers(offers)
+                         .setInternalId(internalId)
+                         .setName(name)
+                         .setPrimaryImage(primaryImage)
+                         .setSecondaryImages(secondaryImages)
+                         .setDescription(description)
+                         .setStock(stock)
+                         .setRatingReviews(ratingsReviews)
+                         .build();
 
          products.add(product);
 
@@ -119,37 +120,37 @@ public class SaopauloTendadriveCrawler extends Crawler {
    }
 
    private Offers scrapOffers(JSONObject skuJson)
-         throws MalformedPricingException, OfferException {
+           throws MalformedPricingException, OfferException {
       Offers offers = new Offers();
       Double price = skuJson.optDouble("price");
       CreditCards creditCards = new CreditCards();
 
       Installments installments = new Installments();
       installments.add(InstallmentBuilder.create()
-            .setInstallmentNumber(1)
-            .setInstallmentPrice(price)
-            .build());
+              .setInstallmentNumber(1)
+              .setInstallmentPrice(price)
+              .build());
 
       creditCards.add(CreditCardBuilder.create()
-            .setIsShopCard(false)
-            .setBrand(Card.VISA.toString())
-            .setInstallments(installments)
-            .build());
+              .setIsShopCard(false)
+              .setBrand(Card.VISA.toString())
+              .setInstallments(installments)
+              .build());
 
 //tenda-atacado-sao-paulo---av-guarapiranga
       offers.add(
-            OfferBuilder.create()
-                  .setSellerFullName(SELLER_NAME)
-                  .setIsBuybox(false)
-                  .setPricing(
-                        Pricing.PricingBuilder.create()
-                              .setSpotlightPrice(price)
-                              .setBankSlip(BankSlipBuilder.create().setFinalPrice(price).build())
-                              .setCreditCards(creditCards)
-                              .build())
-                  .setIsMainRetailer(true)
-                  .setUseSlugNameAsInternalSellerId(true)
-                  .build());
+              OfferBuilder.create()
+                      .setSellerFullName(SELLER_NAME)
+                      .setIsBuybox(false)
+                      .setPricing(
+                              Pricing.PricingBuilder.create()
+                                      .setSpotlightPrice(price)
+                                      .setBankSlip(BankSlipBuilder.create().setFinalPrice(price).build())
+                                      .setCreditCards(creditCards)
+                                      .build())
+                      .setIsMainRetailer(true)
+                      .setUseSlugNameAsInternalSellerId(true)
+                      .build());
 
       return offers;
    }
