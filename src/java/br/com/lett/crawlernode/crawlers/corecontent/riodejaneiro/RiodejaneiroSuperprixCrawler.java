2148d3034b492c5b45aefa37a2dfebe32c1d7a58
Charles Fonseca <charlesjrfonseca@gmail.com>
2021-04-23 14:49:31 -0300

    superprix fix

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/riodejaneiro/RiodejaneiroSuperprixCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/riodejaneiro/RiodejaneiroSuperprixCrawler.java
@@ -1,379 +1,63 @@
 package br.com.lett.crawlernode.crawlers.corecontent.riodejaneiro;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.regex.Pattern;
+import java.util.*;
+
+import br.com.lett.crawlernode.crawlers.extractionutils.core.VTEXOldScraper;
+import models.RatingsReviews;
+import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.cookie.BasicClientCookie;
-import org.json.JSONArray;
 import org.json.JSONObject;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.DataNode;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import com.google.common.net.HttpHeaders;
 import br.com.lett.crawlernode.core.fetcher.models.Request;
-import br.com.lett.crawlernode.core.fetcher.models.Request.RequestBuilder;
-import br.com.lett.crawlernode.core.models.Card;
-import br.com.lett.crawlernode.core.models.CategoryCollection;
-import br.com.lett.crawlernode.core.models.Product;
-import br.com.lett.crawlernode.core.models.ProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
-import br.com.lett.crawlernode.core.task.impl.Crawler;
-import br.com.lett.crawlernode.util.CrawlerUtils;
-import br.com.lett.crawlernode.util.JSONUtils;
-import br.com.lett.crawlernode.util.Logging;
-import models.Marketplace;
-import models.prices.Prices;
 
-/**
- * Date: 05/06/2017
- *
- * @author Gabriel Dornelas
- *
- */
-public class RiodejaneiroSuperprixCrawler extends Crawler {
+public class RiodejaneiroSuperprixCrawler extends VTEXOldScraper {
 
-   private static final String HOME_PAGE = "http://www.ipanema.superprix.com.br/";
+   private static final String HOME_PAGE = "https://www.ipanema.superprix.com.br/";
    private static final String CEP = "22041080";
    private static final String CITY = "RIO DE JANEIRO";
-   private static final String VTEXSC = "sc=1";
+   private static final String LOCATION = "4";
+
 
    public RiodejaneiroSuperprixCrawler(Session session) {
       super(session);
    }
 
    @Override
-   public boolean shouldVisit() {
-      String href = this.session.getOriginalURL().toLowerCase();
-      return !FILTERS.matcher(href).matches() && (href.startsWith(HOME_PAGE));
-   }
-
-   public void handleCookiesBeforeFetch() {
-      Map<String, String> cookiesMap = new HashMap<>();
-      cookiesMap.put("VTEXSC", VTEXSC);
-      cookiesMap.put("userCep", CEP);
-      cookiesMap.put("cityValidation", CITY);
-
-      for (Entry<String, String> entry : cookiesMap.entrySet()) {
-         BasicClientCookie cookie = new BasicClientCookie(entry.getKey(), entry.getValue());
-         cookie.setDomain(".www.superprix.com.br");
-         cookie.setPath("/");
-         this.cookies.add(cookie);
-      }
-   }
-
-   @Override
    protected Object fetch() {
-      Map<String, String> headers = new HashMap<>();
-      headers.put(HttpHeaders.REFERER, "https://www.ipanema.superprix.com.br/?redirect&cep=22041080");
+//      List<Cookie> cookiesFetc = Arrays.asList(new BasicClientCookie("VTEXSC", "sc=" + LOCATION),
+//         new BasicClientCookie("userCep", CEP),
+//         new BasicClientCookie("cityValidation", CITY));
 
-      Request request = RequestBuilder.create().setUrl(session.getOriginalURL()).setCookies(cookies).setHeaders(headers).build();
-      return Jsoup.parse(this.dataFetcher.get(session, request).getBody());
+      cookies.addAll(Arrays.asList(new BasicClientCookie("VTEXSC", "sc=" + LOCATION),
+         new BasicClientCookie("userCep", CEP),
+         new BasicClientCookie("cityValidation", CITY)));
+
+      Request request = Request.RequestBuilder.create().setUrl(session.getOriginalURL()).setCookies(cookies).build();
+      String response = dataFetcher.get(session, request).getBody();
+
+      return Jsoup.parse(response);
    }
 
    @Override
-   public List<Product> extractInformation(Document doc) throws Exception {
-      super.extractInformation(doc);
-      List<Product> products = new ArrayList<>();
-
-      if (isProductPage(doc)) {
-         Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
-
-         // Pid
-         String internalPid = crawlInternalPid(doc);
-
-         // Categories
-         CategoryCollection categories = crawlCategories(doc);
-
-         // Description
-         String description = crawlDescription(doc);
-
-         // Primary image
-         String primaryImage = crawlPrimaryImage(doc);
-
-         // Secondary images
-         String secondaryImages = crawlSecondaryImages(doc);
-
-         // sku data in json
-         JSONArray arraySkus = crawlSkuJsonArray(doc);
-
-         // ean data in html
-         JSONArray arrayEan = CrawlerUtils.scrapEanFromVTEX(doc);
-
-
-         for (int i = 0; i < arraySkus.length(); i++) {
-            JSONObject jsonSku = arraySkus.getJSONObject(i);
-
-            // Availability
-            boolean available = crawlAvailability(jsonSku);
-
-            // InternalId
-            String internalId = crawlInternalId(jsonSku);
-
-            // Price
-            Float price = crawlMainPagePrice(jsonSku, available);
-
-            // Name
-            String name = crawlName(doc, jsonSku);
-
-            // Prices
-            Prices prices = crawlPrices(price, jsonSku);
-
-            // Stock
-            Integer stock = crawlStock(internalId, available);
-
-            // Ean
-            String ean = i < arrayEan.length() ? arrayEan.getString(i) : null;
-
-            List<String> eans = new ArrayList<>();
-            eans.add(ean);
-
-            // Creating the product
-            Product product = ProductBuilder.create().setUrl(session.getOriginalURL()).setInternalId(internalId).setInternalPid(internalPid).setName(name)
-                  .setPrice(price).setPrices(prices).setAvailable(available).setCategory1(categories.getCategory(0)).setCategory2(categories.getCategory(1))
-                  .setCategory3(categories.getCategory(2)).setPrimaryImage(primaryImage).setSecondaryImages(secondaryImages).setDescription(description)
-                  .setStock(stock).setMarketplace(new Marketplace()).setEans(eans).build();
-
-            products.add(product);
-         }
-
-      } else {
-         Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
-      }
-
-      return products;
+   protected String getHomePage() {
+      return HOME_PAGE;
    }
 
-   /*******************************
-    * Product page identification *
-    *******************************/
-
-   private boolean isProductPage(Document document) {
-      return document.select(".productName").first() != null;
+   @Override
+   protected List<String> getMainSellersNames() {
+      return Collections.singletonList("superprixipanema");
    }
 
-   /*******************
-    * General methods *
-    *******************/
-
-   private String crawlInternalId(JSONObject json) {
-      String internalId = null;
-
-      if (json.has("sku")) {
-         internalId = Integer.toString(json.getInt("sku")).trim();
-      }
-
-      return internalId;
-   }
-
-
-   private String crawlInternalPid(Document document) {
-      String internalPid = null;
-      Element internalPidElement = document.select("#___rc-p-id").first();
-
-      if (internalPidElement != null) {
-         internalPid = internalPidElement.attr("value").trim();
-      }
-
-      return internalPid;
-   }
-
-   private String crawlName(Document document, JSONObject jsonSku) {
-      String name = null;
-      Element nameElement = document.select(".productName").first();
-
-      String nameVariation = jsonSku.getString("skuname");
-
-      if (nameElement != null) {
-         name = nameElement.text().trim();
-
-         if (name.length() > nameVariation.length()) {
-            name += " " + nameVariation;
-         } else {
-            name = nameVariation;
-         }
-      }
-
-      return name;
-   }
-
-   private Float crawlMainPagePrice(JSONObject json, boolean available) {
-      Float price = null;
-
-      if (json.has("bestPriceFormated") && available) {
-         price = Float.parseFloat(json.getString("bestPriceFormated").replaceAll("[^0-9,]+", "").replaceAll("\\.", "").replaceAll(",", "."));
-      }
-
-      return price;
-   }
-
-   private boolean crawlAvailability(JSONObject json) {
-      if (json.has("available")) {
-         return json.getBoolean("available");
-      }
-      return false;
-   }
-
-   private String crawlPrimaryImage(Document doc) {
-      String primaryImage = null;
-
-      Element image = doc.select("#botaoZoom").first();
-
-      if (image != null) {
-         primaryImage = image.attr("zoom").trim();
-
-         if (primaryImage == null || primaryImage.isEmpty()) {
-            primaryImage = image.attr("rel").trim();
-         }
-      }
-
-      return primaryImage;
-   }
-
-   private String crawlSecondaryImages(Document doc) {
-      String secondaryImages = null;
-      JSONArray secondaryImagesArray = new JSONArray();
-
-      Elements imageThumbs = doc.select("#botaoZoom");
-
-      for (int i = 1; i < imageThumbs.size(); i++) { // starts with index 1, because the first image
-                                                     // is the primary image
-         String url = imageThumbs.get(i).attr("zoom");
-
-         if (url == null || url.isEmpty()) {
-            url = imageThumbs.get(i).attr("rel");
-         }
-
-         if (url != null && !url.isEmpty()) {
-            secondaryImagesArray.put(url);
-         }
-      }
-
-      if (secondaryImagesArray.length() > 0) {
-         secondaryImages = secondaryImagesArray.toString();
-      }
-
-      return secondaryImages;
-   }
-
-   private CategoryCollection crawlCategories(Document document) {
-      CategoryCollection categories = new CategoryCollection();
-      Elements elementCategories = document.select(".bread-crumb > ul li a");
-
-      for (int i = 1; i < elementCategories.size(); i++) { // starting from index 1, because the first
-                                                           // is the market name
-         categories.add(elementCategories.get(i).text().trim());
-      }
-
-      return categories;
-   }
-
-
-   private String crawlDescription(Document doc) {
-      StringBuilder str = new StringBuilder();
-
-      Element desc = doc.select(".prod-info .prod-descricao").first();
-
-      if (desc != null) {
-         desc.select("h4.Conteudo-da-Pagina-de-Produto").remove();
-         str.append(desc.html());
-      }
-
-      Elements desc2 = doc.select("#caracteristicas table");
-
-      for (Element e : desc2) {
-         e.select("h4.Conteudo-da-Pagina-de-Produto").remove();
-
-         if (!e.select(".Tabela-Nutricional").isEmpty()) {
-            str.append(e.html());
-            break;
-         }
-      }
-
-      return str.toString();
-   }
-
-   /**
-    * There is no bank slip payment method Has no informations of installments
-    * 
-    * @param internalId
-    * @param price
-    * @return
-    */
-   private Prices crawlPrices(Float price, JSONObject jsonSku) {
-      Prices prices = new Prices();
-
-      if (price != null) {
-         prices.setPriceFrom(JSONUtils.getDoubleValueFromJSON(jsonSku, "ListPrice", true));
-
-         Map<Integer, Float> mapInstallments = new HashMap<>();
-         mapInstallments.put(1, price);
-
-         prices.insertCardInstallment(Card.VISA.toString(), mapInstallments);
-         prices.insertCardInstallment(Card.MASTERCARD.toString(), mapInstallments);
-         prices.insertCardInstallment(Card.HIPERCARD.toString(), mapInstallments);
-         prices.insertCardInstallment(Card.DINERS.toString(), mapInstallments);
-         prices.insertCardInstallment(Card.AMEX.toString(), mapInstallments);
-      }
-
-      return prices;
-   }
-
-
-   private Integer crawlStock(String internalId, boolean available) {
-      if (available) {
-         String url = HOME_PAGE + "produto/sku/" + internalId;
-
-         Request request = RequestBuilder.create().setUrl(url).setCookies(cookies).build();
-         JSONArray jsonArray = CrawlerUtils.stringToJsonArray(this.dataFetcher.get(session, request).getBody());
-
-         if (jsonArray.length() > 0) {
-            JSONObject skuInfo = jsonArray.getJSONObject(0);
-
-            if (skuInfo.has("SkuSellersInformation")) {
-               JSONObject sku = skuInfo.getJSONArray("SkuSellersInformation").getJSONObject(0);
-
-               if (sku.has("AvailableQuantity")) {
-                  return sku.getInt("AvailableQuantity");
-               }
-            }
-         }
-      }
-
+   @Override
+   protected RatingsReviews scrapRating(String internalId, String internalPid, Document doc, JSONObject jsonSku) {
       return null;
    }
 
-   /**
-    * Get the script having a json with the availability information
-    * 
-    * @return
-    */
-   private JSONArray crawlSkuJsonArray(Document document) {
-      Elements scriptTags = document.getElementsByTag("script");
-      JSONObject skuJson = null;
-      JSONArray skuJsonArray = null;
-
-      for (Element tag : scriptTags) {
-         for (DataNode node : tag.dataNodes()) {
-            if (tag.html().trim().startsWith("var skuJson_0 = ")) {
-               skuJson = new JSONObject(node.getWholeData().split(Pattern.quote("var skuJson_0 = "))[1]
-                     + node.getWholeData().split(Pattern.quote("var skuJson_0 = "))[1].split(Pattern.quote("}]};"))[0]);
-            }
-         }
-      }
-
-      if (skuJson != null && skuJson.has("skus")) {
-         skuJsonArray = skuJson.getJSONArray("skus");
-      }
-
-      if (skuJsonArray == null) {
-         skuJsonArray = new JSONArray();
-      }
-
-      return skuJsonArray;
+   @Override
+   protected JSONObject crawlProductApi(String internalPid, String parameters) {
+      return super.crawlProductApi(internalPid, null);
    }
+
 }
