87f9d48599b523557569bcf05aab2a1891618406
CostaLucasIV <lucascmm3@gmail.com>
2021-04-27 18:08:11 -0300

    new crawler vila nova (login nestle) + refactor

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilVilanovanestleCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilVilanovanestleCrawler.java
@@ -3,9 +3,9 @@ package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.crawlers.extractionutils.ranking.BrasilVilanova;
 
-public class BrasilVilanovamondelezCrawler extends BrasilVilanova {
+public class BrasilVilanovanestleCrawler extends BrasilVilanova {
 
-  public BrasilVilanovamondelezCrawler(Session session) {
-    super(session);
-  }
+   public BrasilVilanovanestleCrawler(Session session) {
+      super(session);
+   }
 }
