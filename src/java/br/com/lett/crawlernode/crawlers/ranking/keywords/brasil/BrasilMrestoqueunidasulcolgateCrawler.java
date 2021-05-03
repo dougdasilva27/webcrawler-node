80bc58d2bacd855aaedb26456be6f3cd2f3da3c3
Thainá <thainaj.aguiar@gmail.com>
2021-05-03 13:48:36 -0300

    new crawler mr estoque - colgate

---
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilMrestoqueunidasulcolgateCrawler.java
@@ -0,0 +1,11 @@
+package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
+
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.crawlers.extractionutils.ranking.MrestoqueunidasulCrawler;
+
+public class BrasilMrestoqueunidasulcolgateCrawler extends MrestoqueunidasulCrawler {
+
+   public BrasilMrestoqueunidasulcolgateCrawler(Session session) {
+      super(session);
+   }
+}
