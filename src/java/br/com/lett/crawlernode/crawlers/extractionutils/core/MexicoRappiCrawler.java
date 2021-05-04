cd1309c6397441964cb7c8d6249ca9f4784da7f5
Thainá <thainaj.aguiar@gmail.com>
2021-05-04 10:19:33 -0300

    fixing url of mexico rappi

--- src/java/br/com/lett/crawlernode/crawlers/extractionutils/core/MexicoRappiCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/extractionutils/core/MexicoRappiCrawler.java
@@ -1,16 +1,18 @@
 package br.com.lett.crawlernode.crawlers.extractionutils.core;
 
-import java.text.Normalizer;
-import java.util.HashMap;
-import java.util.Map;
-
 import br.com.lett.crawlernode.core.fetcher.FetchMode;
-import org.json.JSONObject;
-import br.com.lett.crawlernode.core.fetcher.models.Request;
-import br.com.lett.crawlernode.core.fetcher.models.Request.RequestBuilder;
+import br.com.lett.crawlernode.core.models.CategoryCollection;
+import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.models.ProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.util.CommonMethods;
+import br.com.lett.crawlernode.util.JSONUtils;
 import br.com.lett.crawlernode.util.Logging;
+import models.Offers;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class MexicoRappiCrawler extends RappiCrawler {
 
@@ -36,6 +38,51 @@ public abstract class MexicoRappiCrawler extends RappiCrawler {
       String href = this.session.getOriginalURL().toLowerCase();
       return !FILTERS.matcher(href).matches() && (href.startsWith(HOME_PAGE));
    }
+
+   @Override
+   public List<Product> extractInformation(JSONObject jsonSku) throws Exception {
+
+      List<Product> products = new ArrayList<>();
+
+      JSONObject productJson = JSONUtils.getJSONValue(jsonSku, "product");
+
+      if (isProductPage(productJson)) {
+         Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
+
+
+         String internalPid = crawlInternalPid(productJson);
+         String internalId = newUnification ? internalPid : crawlInternalId(productJson);
+         String description = crawlDescription(productJson);
+         String primaryImage = crawlPrimaryImage(jsonSku);
+         List<String> secondaryImages = crawlSecondaryImages(jsonSku, primaryImage);
+         CategoryCollection categories = crawlCategories(productJson);
+         String name = crawlName(productJson);
+         List<String> eans = scrapEan(productJson);
+         boolean available = crawlAvailability(productJson);
+         Offers offers = available ? scrapOffers(productJson) : new Offers();
+
+         // Creating the product
+         Product product = ProductBuilder.create()
+            .setUrl(session.getOriginalURL().replace("product", "producto"))
+            .setInternalId(internalId)
+            .setInternalPid(internalPid)
+            .setName(name)
+            .setPrimaryImage(primaryImage)
+            .setSecondaryImages(secondaryImages)
+            .setCategories(categories)
+            .setDescription(description)
+            .setEans(eans)
+            .setOffers(offers)
+            .build();
+
+         products.add(product);
+      } else {
+         Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
+      }
+
+      return products;
+   }
+
 
    @Override
    protected String crawlDescription(JSONObject json) {
