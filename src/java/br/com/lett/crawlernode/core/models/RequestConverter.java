f84a828a56aa91fda158d762061b2827101fae8d
matheus <matheusbussolotti@gmail.com>
2021-05-10 15:21:38 -0300

    Set values final on request parse

--- src/java/br/com/lett/crawlernode/core/models/RequestConverter.java
+++ src/java/br/com/lett/crawlernode/core/models/RequestConverter.java
@@ -15,11 +15,30 @@ import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.security.PrivateKey;
 import java.util.stream.Collectors;
 
 public class RequestConverter {
 
    protected static final Logger logger = LoggerFactory.getLogger(RequestConverter.class);
+
+   private static final String BODY_QUEUE = "queue";
+   private static final String BODY_MSG_ID = "msgId";
+   private static final String BODY_CLASS_NAME = "className";
+   private static final String BODY_OPTIONS = "options";
+   private static final String BODY_INTERNAL_ID = "internalId";
+   private static final String BODY_SUPPLIER_ID = "supplierId";
+   private static final String BODY_MARKET = "market";
+   private static final String BODY_PROCESSED_ID = "processedId" ;
+   private static final String BODY_PARAMETERS = "parameters";
+   private static final String BODY_SEED_TASK_ID ="taskId" ;
+   private static final String BODY_RANKING_SCREENSHOT = "screenshot";
+   private static final String MARKET_ID = "marketId";
+   private static final String MARKET_NAME = "name";
+   private static final String MARKET_FULL_NAME = "fullName";
+   private static final String MARKET_CODE = "code";
+   private static final String MARKET_REGEX= "regex";
+   private static final String MARKET_USE_BROWSER= "use_browser";
 
    private RequestConverter() {
    }
@@ -48,36 +67,36 @@ public class RequestConverter {
       }
 
       request.setRequestMethod(req.getMethod());
-      request.setQueueName(body.optString("queue"));
-      request.setMessageId(body.optString("msgId"));
-      request.setClassName(body.optString("className"));
+      request.setQueueName(body.optString(BODY_QUEUE));
+      request.setMessageId(body.optString(BODY_MSG_ID));
+      request.setClassName(body.optString(BODY_CLASS_NAME));
 
-      String options = body.optString("options");
+      String options = body.optString(BODY_OPTIONS);
       if(options!=null && !options.isEmpty()) {
          request.setOptions(JSONUtils.stringToJson(options));
       }
 
-      request.setInternalId(body.optString("internalId"));
+      request.setInternalId(body.optString(BODY_INTERNAL_ID));
 
-      String supplierIdString = body.optString("supplierId");
+      String supplierIdString = body.optString(BODY_SUPPLIER_ID);
 
       if (supplierIdString != null && !supplierIdString.isEmpty()) {
             request.setSupplierId(Long.parseLong(supplierIdString.trim()));
       }
 
 
-      JSONObject marketObj = body.getJSONObject("market");
+      JSONObject marketObj = body.getJSONObject(BODY_MARKET);
       if (marketObj != null) {
          request.setMarket(createMarket(marketObj));
       }
 
 
-      String processedIdString = body.optString("processedId");
+      String processedIdString = body.optString(BODY_PROCESSED_ID);
       if (processedIdString != null && !processedIdString.isEmpty()) {
          request.setProcessedId(Long.parseLong(processedIdString));
       }
 
-      String parameters = body.optString("parameters");
+      String parameters = body.optString(BODY_PARAMETERS);
 
       request.setParameter(parameters);
       request.setScraperType(scraperType);
@@ -85,13 +104,13 @@ public class RequestConverter {
 
 
       if (request instanceof CrawlerSeedRequest) {
-         ((CrawlerSeedRequest) request).setTaskId(body.optString("taskId"));
+         ((CrawlerSeedRequest) request).setTaskId(body.optString(BODY_SEED_TASK_ID));
       }
 
       if (request instanceof CrawlerRankingKeywordsRequest) {
          ((CrawlerRankingKeywordsRequest) request).setLocation(parameters);
 
-         ((CrawlerRankingKeywordsRequest) request).setTakeAScreenshot(body.optBoolean("screenshot",false));
+         ((CrawlerRankingKeywordsRequest) request).setTakeAScreenshot(body.optBoolean(BODY_RANKING_SCREENSHOT,false));
 
       }
 
@@ -99,7 +118,7 @@ public class RequestConverter {
    }
 
    private static Market createMarket(JSONObject marketObj) {
-      return new Market(marketObj.optInt("marketId"), marketObj.optString("name"), marketObj.optString("fullName"), marketObj.optString("code"),marketObj.optString("regex"),marketObj.optBoolean("use_browser"));
+      return new Market(marketObj.optInt(MARKET_ID), marketObj.optString(MARKET_NAME), marketObj.optString(MARKET_FULL_NAME), marketObj.optString(MARKET_CODE),marketObj.optString(MARKET_REGEX),marketObj.optBoolean(MARKET_USE_BROWSER));
    }
 
    private static String getRequestBody(HttpServletRequest req) {
