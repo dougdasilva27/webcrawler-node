6bec1aa63e6190a45543347ee3f23019a994d96f
marcosmoura <marcosmourabro@gmail.com>
2021-05-12 17:09:01 -0300

    test

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,10 @@ $(aws ecr get-login --no-include-email)
 export ENVIROMENT_DEPLOY=$1
 export ECR_IMAGE_URL=868884350453.dkr.ecr.us-east-1.amazonaws.com/team-data-capture/webcrawler:$1
 
+
+if [ "$ENVIROMENT_DEPLOY" == "prod-high" ]; then "this.containerDefinitions[0].memoryReservation"="1700"
+fi
+
 npx json -I -f Dockerrun.aws.json -e "this.containerDefinitions[0].image"="'$ECR_IMAGE_URL'"
 
 docker-compose -f build.yml run --rm maven
