ba25a2dd5233f29c16425b84b211bcdc2e8ccaa8
marcosmoura <marcosmourabro@gmail.com>
2021-05-18 17:44:15 -0300

    remove imports

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilHavanCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilHavanCrawler.java
@@ -12,23 +12,18 @@ import br.com.lett.crawlernode.util.Logging;
 import com.google.common.collect.Sets;
 import exceptions.MalformedPricingException;
 import exceptions.OfferException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
 import models.AdvancedRatingReview;
 import models.Offer;
 import models.Offers;
 import models.RatingsReviews;
-import models.pricing.BankSlip;
-import models.pricing.CreditCard;
-import models.pricing.CreditCards;
-import models.pricing.Installment;
-import models.pricing.Installments;
-import models.pricing.Pricing;
+import models.pricing.*;
 import org.json.JSONArray;
 import org.jsoup.nodes.Document;
-import software.amazon.awssdk.services.glue.model.Crawl;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 
 public class BrasilHavanCrawler extends Crawler {
