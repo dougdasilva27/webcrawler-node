74e7a890d49737af7edb0c55f383062990f50768
CostaLucasIV <lucascmm3@gmail.com>
2021-05-18 15:53:19 -0300

    productInfo null check and internalId empty string check

--- src/java/br/com/lett/crawlernode/crawlers/extractionutils/core/Vipcommerce.java
+++ src/java/br/com/lett/crawlernode/crawlers/extractionutils/core/Vipcommerce.java
@@ -34,7 +34,7 @@ public abstract class Vipcommerce extends Crawler {
    private final String DOMAIN = getDomain();
    private final String LOCATE_CODE = getLocateCode();
    protected Set<String> cards = Sets.newHashSet(Card.VISA.toString(), Card.MASTERCARD.toString(),
-           Card.AURA.toString(), Card.DINERS.toString(), Card.HIPER.toString(), Card.AMEX.toString());
+      Card.AURA.toString(), Card.DINERS.toString(), Card.HIPER.toString(), Card.AMEX.toString());
 
    public Vipcommerce(Session session) {
       super(session);
@@ -67,9 +67,9 @@ public abstract class Vipcommerce extends Crawler {
       headers.put("origin", "https://www." + DOMAIN + "");
 
       JSONObject payload = new JSONObject()
-              .put("domain", DOMAIN)
-              .put("username", "loja")
-              .put("key", "df072f85df9bf7dd71b6811c34bdbaa4f219d98775b56cff9dfa5f8ca1bf8469");
+         .put("domain", DOMAIN)
+         .put("username", "loja")
+         .put("key", "df072f85df9bf7dd71b6811c34bdbaa4f219d98775b56cff9dfa5f8ca1bf8469");
 
       Request request = Request.RequestBuilder.create().setUrl(url).setHeaders(headers).setPayload(payload.toString()).setCookies(cookies).build();
       JSONObject tokenJson = CrawlerUtils.stringToJson(this.dataFetcher.post(session, request).getBody());
@@ -106,33 +106,34 @@ public abstract class Vipcommerce extends Crawler {
          String token = getToken();
          JSONObject jsonData = crawlApi(token);
          JSONObject productInfo = JSONUtils.getJSONValue(jsonData, "produto");
-         JSONObject OffersInfo = JSONUtils.getJSONValue(productInfo, "oferta");
-         String internalId = productInfo.optString("produto_id");
 
-         if (internalId == null) {
-            return products;
+         if (productInfo != null && !productInfo.isEmpty()) {
+            JSONObject offersInfo = JSONUtils.getJSONValue(productInfo, "oferta");
+            String internalId = productInfo.optString("produto_id");
+
+            if (internalId == null || internalId.equals("")) {
+               return products;
+            }
+
+            String internalPid = JSONUtils.getStringValue(productInfo, "id");
+            String name = JSONUtils.getStringValue(productInfo, "descricao");
+            String primaryImage = CrawlerUtils.completeUrl(JSONUtils.getStringValue(productInfo, "imagem"), " https://", "s3.amazonaws.com/produtos.vipcommerce.com.br/250x250");
+            boolean availeble = productInfo.optBoolean("disponivel");
+            Integer stock = JSONUtils.getIntegerValueFromJSON(productInfo, "quantidade_maxima", null);
+            Offers offers = availeble ? scrapOffers(offersInfo, productInfo) : new Offers();
+
+            Product product = ProductBuilder.create()
+               .setUrl(session.getOriginalURL())
+               .setInternalId(internalId)
+               .setInternalPid(internalPid)
+               .setName(name)
+               .setPrimaryImage(primaryImage)
+               .setStock(stock)
+               .setOffers(offers)
+               .build();
+
+            products.add(product);
          }
-
-         String internalPid = JSONUtils.getStringValue(productInfo, "id");
-         String name = JSONUtils.getStringValue(productInfo, "descricao");
-         String primaryImage = CrawlerUtils.completeUrl(JSONUtils.getStringValue(productInfo, "imagem"), " https://", "s3.amazonaws.com/produtos.vipcommerce.com.br/250x250");
-         boolean availeble = productInfo.optBoolean("disponivel");
-         Integer stock = JSONUtils.getIntegerValueFromJSON(productInfo, "quantidade_maxima", null);
-         Offers offers = availeble ? scrapOffers(OffersInfo, productInfo) : new Offers();
-
-         Product product = ProductBuilder.create()
-                 .setUrl(session.getOriginalURL())
-                 .setInternalId(internalId)
-                 .setInternalPid(internalPid)
-                 .setName(name)
-                 .setPrimaryImage(primaryImage)
-                 .setStock(stock)
-                 .setOffers(offers)
-                 .build();
-
-         products.add(product);
-
-
       } else {
          Logging.printLogDebug(logger, session, "Not a product page.");
       }
@@ -149,14 +150,14 @@ public abstract class Vipcommerce extends Crawler {
       List<String> sales = new ArrayList<>();
 
       offers.add(Offer.OfferBuilder.create()
-              .setUseSlugNameAsInternalSellerId(true)
-              .setSellerFullName(SELLER_FULL_NAME)
-              .setMainPagePosition(1)
-              .setIsBuybox(false)
-              .setIsMainRetailer(true)
-              .setPricing(pricing)
-              .setSales(sales)
-              .build());
+         .setUseSlugNameAsInternalSellerId(true)
+         .setSellerFullName(SELLER_FULL_NAME)
+         .setMainPagePosition(1)
+         .setIsBuybox(false)
+         .setIsMainRetailer(true)
+         .setPricing(pricing)
+         .setSales(sales)
+         .build());
 
       return offers;
    }
@@ -177,11 +178,11 @@ public abstract class Vipcommerce extends Crawler {
       CreditCards creditCards = scrapCreditCards(spotlightPrice);
 
       return Pricing.PricingBuilder.create()
-              .setPriceFrom(priceFrom)
-              .setSpotlightPrice(spotlightPrice)
-              .setBankSlip(bankSlip)
-              .setCreditCards(creditCards)
-              .build();
+         .setPriceFrom(priceFrom)
+         .setSpotlightPrice(spotlightPrice)
+         .setBankSlip(bankSlip)
+         .setCreditCards(creditCards)
+         .build();
    }
 
    private CreditCards scrapCreditCards(Double spotlightPrice) throws MalformedPricingException {
@@ -190,17 +191,17 @@ public abstract class Vipcommerce extends Crawler {
       Installments installments = new Installments();
       if (installments.getInstallments().isEmpty()) {
          installments.add(Installment.InstallmentBuilder.create()
-                 .setInstallmentNumber(1)
-                 .setInstallmentPrice(spotlightPrice)
-                 .build());
+            .setInstallmentNumber(1)
+            .setInstallmentPrice(spotlightPrice)
+            .build());
       }
 
       for (String card : cards) {
          creditCards.add(CreditCard.CreditCardBuilder.create()
-                 .setBrand(card)
-                 .setInstallments(installments)
-                 .setIsShopCard(false)
-                 .build());
+            .setBrand(card)
+            .setInstallments(installments)
+            .setIsShopCard(false)
+            .build());
       }
 
       return creditCards;
