b5458007eea5df4375cd25b91b32c15ad2f68e2d
CostaLucasIV <lucascmm3@gmail.com>
2021-05-21 11:37:39 -0300

    hiper queiroz first commit

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilHiperqueirozCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilHiperqueirozCrawler.java
@@ -1,150 +1,32 @@
 package br.com.lett.crawlernode.crawlers.corecontent.brasil;
 
-import br.com.lett.crawlernode.core.fetcher.FetchMode;
-import br.com.lett.crawlernode.core.fetcher.ProxyCollection;
-import br.com.lett.crawlernode.core.fetcher.methods.DataFetcher;
-import br.com.lett.crawlernode.core.fetcher.methods.FetcherDataFetcher;
-import br.com.lett.crawlernode.core.fetcher.models.FetcherOptions;
-import br.com.lett.crawlernode.core.fetcher.models.Request;
-import br.com.lett.crawlernode.core.fetcher.models.Response;
-import br.com.lett.crawlernode.core.models.*;
 import br.com.lett.crawlernode.core.session.Session;
-import br.com.lett.crawlernode.core.task.impl.Crawler;
-import br.com.lett.crawlernode.util.CrawlerUtils;
-import br.com.lett.crawlernode.util.Logging;
-import com.google.common.collect.Sets;
-import models.Offer;
-import models.Offers;
-import models.RatingsReviews;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.jsoup.nodes.Document;
+import br.com.lett.crawlernode.crawlers.extractionutils.core.BrasilMerconnectCrawler;
 
 import java.util.*;
 
-public class BrasilHiperqueirozCrawler extends Crawler {
+public class BrasilHiperqueirozCrawler extends BrasilMerconnectCrawler {
 
    private static final String CLIENT_ID = "dcdbcf6fdb36412bf96d4b1b4ca8275de57c2076cb9b88e27dc7901e8752cdff";
    private static final String CLIENT_SECRET = "27c92c098d3f4b91b8cb1a0d98138b43668c89d677b70bed397e6a5e0971257c";
-
-   protected Set<String> cards = Sets.newHashSet(Card.DINERS.toString(), Card.VISA.toString(), Card.MASTERCARD.toString(), Card.ELO.toString());
+   private static final String STORE_ID = "167";
 
    public BrasilHiperqueirozCrawler(Session session) {
       super(session);
-      config.setFetcher(FetchMode.JSOUP);
    }
 
    @Override
-   protected JSONObject fetch() {
-      JSONObject json = new JSONObject();
-
-      String[] splittedUrl = session.getOriginalURL().split("/");
-
-      if (splittedUrl.length > 0) {
-         String lastIndex = splittedUrl[splittedUrl.length - 1];
-         String url = "https://www.merconnect.com.br/mapp/v2/markets/167/items/" + lastIndex.substring(0, lastIndex.indexOf("?"));
-
-         Map<String, String> headers = new HashMap<>();
-         headers.put("Accept-Encoding", "gzip, deflate, br");
-         headers.put("Content-Type", "application/json;charset=UTF-8");
-         headers.put("Connection", "keep-alive");
-         headers.put("Authorization", "Bearer " + fetchApiToken(headers));
-
-         Request request = Request.RequestBuilder.create()
-            .setUrl(url)
-            .setHeaders(headers)
-            .build();
-
-         Response response = dataFetcher.get(session, request);
-
-         json = CrawlerUtils.stringToJson(response.getBody());
-      }
-
-      return json;
-   }
-
-   protected String fetchApiToken(Map<String, String> headers) {
-      String apiToken = "";
-
-      JSONObject payload = new JSONObject();
-      payload.put("client_id", CLIENT_ID);
-      payload.put("client_secret", CLIENT_SECRET);
-      payload.put("grant_type", "client_credentials");
-
-      Request request = Request.RequestBuilder.create()
-         .setUrl("https://www.merconnect.com.br/oauth/token")
-         .setHeaders(headers)
-         .setPayload(payload.toString())
-         .build();
-
-      JSONObject json = CrawlerUtils.stringToJson(dataFetcher.post(session, request).getBody());
-
-      if (json != null && !json.isEmpty()) {
-         apiToken = json.optString("access_token");
-      }
-
-      return apiToken;
+   protected String getClientId() {
+      return CLIENT_ID;
    }
 
    @Override
-   public List<Product> extractInformation(JSONObject json) throws Exception {
-      super.extractInformation(json);
-      List<Product> products = new ArrayList<>();
-
-      if (json.has("item")) {
-         Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
-
-//         String internalId = crawlInternalId(doc);
-//         String internalPid = internalId;
-//         String name = crawlName(doc);
-//         CategoryCollection categories = crawlCategories(doc);
-//
-//         JSONArray images = this.amazonScraperUtils.scrapImagesJSONArray(doc);
-//         String primaryImage = this.amazonScraperUtils.scrapPrimaryImage(images, doc, IMAGES_PROTOCOL, IMAGES_HOST);
-//         String secondaryImages = this.amazonScraperUtils.scrapSecondaryImages(images, IMAGES_PROTOCOL, IMAGES_HOST);
-//
-//         String description = crawlDescription(doc);
-//         Integer stock = null;
-//
-//         Offer mainPageOffer = scrapMainPageOffer(doc);
-//         List<Document> docOffers = fetchDocumentsOffers(doc, internalId);
-//         Offers offers = scrapOffers(doc, docOffers, mainPageOffer);
-//
-//         String ean = crawlEan(doc);
-//
-//         RatingReviewsCollection ratingReviewsCollection = new RatingReviewsCollection();
-//         ratingReviewsCollection.addRatingReviews(crawlRating(doc, internalId));
-//         RatingsReviews ratingReviews = ratingReviewsCollection.getRatingReviews(internalId);
-
-//         List<String> eans = new ArrayList<>();
-//         eans.add(ean);
-
-         // Creating the product
-         Product product = ProductBuilder.create()
-            .setUrl(session.getOriginalURL())
-//            .setInternalId(internalId)
-//            .setInternalPid(internalPid)
-//            .setName(name)
-//            .setCategory1(categories.getCategory(0))
-//            .setCategory2(categories.getCategory(1))
-//            .setCategory3(categories.getCategory(2))
-//            .setPrimaryImage(primaryImage)
-//            .setSecondaryImages(secondaryImages)
-//            .setDescription(description)
-//            .setStock(stock)
-//            .setEans(eans)
-//            .setRatingReviews(ratingReviews)
-//            .setOffers(offers)
-            .build();
-
-         products.add(product);
-
-      } else {
-         Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
-      }
-
-      return products;
+   protected String getClientSecret() {
+      return CLIENT_SECRET;
    }
 
-
+   @Override
+   protected String getStoreId() {
+      return STORE_ID;
+   }
 }
---
+++ src/java/br/com/lett/crawlernode/crawlers/extractionutils/core/BrasilMerconnectCrawler.java
@@ -0,0 +1,224 @@
+package br.com.lett.crawlernode.crawlers.extractionutils.core;
+
+import br.com.lett.crawlernode.core.fetcher.FetchMode;
+import br.com.lett.crawlernode.core.fetcher.models.Request;
+import br.com.lett.crawlernode.core.fetcher.models.Response;
+import br.com.lett.crawlernode.core.models.Card;
+import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.models.ProductBuilder;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.Crawler;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.Logging;
+import com.google.common.collect.Sets;
+import exceptions.MalformedPricingException;
+import exceptions.OfferException;
+import models.Offer;
+import models.Offers;
+import models.pricing.*;
+import org.json.JSONObject;
+
+import java.util.*;
+
+public abstract class BrasilMerconnectCrawler extends Crawler {
+
+   public BrasilMerconnectCrawler(Session session) {
+      super(session);
+
+      config.setFetcher(FetchMode.JSOUP);
+   }
+
+   protected abstract String getStoreId();
+
+   protected abstract String getClientId();
+
+   protected abstract String getClientSecret();
+
+   @Override
+   protected JSONObject fetch() {
+      JSONObject json = new JSONObject();
+
+      String[] splittedUrl = session.getOriginalURL().split("/");
+
+      if (splittedUrl.length > 0) {
+         String lastIndex = splittedUrl[splittedUrl.length - 1];
+         String url = "https://www.merconnect.com.br/mapp/v2/markets/" + getStoreId() + "/items/" + lastIndex.substring(0, lastIndex.indexOf("?"));
+
+         Map<String, String> headers = new HashMap<>();
+         headers.put("Accept-Encoding", "gzip, deflate, br");
+         headers.put("Content-Type", "application/json;charset=UTF-8");
+         headers.put("Connection", "keep-alive");
+         headers.put("Authorization", "Bearer " + fetchApiToken(headers));
+
+         Request request = Request.RequestBuilder.create()
+            .setUrl(url)
+            .setHeaders(headers)
+            .build();
+
+         Response response = dataFetcher.get(session, request);
+
+         json = CrawlerUtils.stringToJson(response.getBody());
+      }
+
+      return json;
+   }
+
+   protected String fetchApiToken(Map<String, String> headers) {
+      String apiToken = "";
+
+      JSONObject payload = new JSONObject();
+      payload.put("client_id", getClientId());
+      payload.put("client_secret", getClientSecret());
+      payload.put("grant_type", "client_credentials");
+
+      Request request = Request.RequestBuilder.create()
+         .setUrl("https://www.merconnect.com.br/oauth/token")
+         .setHeaders(headers)
+         .setPayload(payload.toString())
+         .build();
+
+      JSONObject json = CrawlerUtils.stringToJson(dataFetcher.post(session, request).getBody());
+
+      if (json != null && !json.isEmpty()) {
+         apiToken = json.optString("access_token");
+      }
+
+      return apiToken;
+   }
+
+   @Override
+   public List<Product> extractInformation(JSONObject json) throws Exception {
+      super.extractInformation(json);
+      List<Product> products = new ArrayList<>();
+
+      if (json.has("item")) {
+         Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
+
+         String internalId = scrapInternalId(json);
+         String internalPid = internalId;
+         String name = scrapProductName(json);
+         String primaryImage = scrapPrimaryImage(json);
+
+         String ean = scrapEan(json);
+         List<String> eans = new ArrayList<>();
+         eans.add(ean);
+
+         Integer stock = scrapStock(json);
+         Offers offers = stock > 0 ? scrapOffers(json) : new Offers();
+
+         // Creating the product
+         Product product = ProductBuilder.create()
+            .setUrl(session.getOriginalURL())
+            .setInternalId(internalId)
+            .setInternalPid(internalPid)
+            .setName(name)
+            .setPrimaryImage(primaryImage)
+            .setStock(stock)
+            .setEans(eans)
+            .setOffers(offers)
+            .build();
+
+         products.add(product);
+
+      } else {
+         Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
+      }
+
+      return products;
+   }
+
+   protected String scrapInternalId(JSONObject json) {
+      return json.optString("id");
+   }
+
+   protected String scrapProductName(JSONObject json) {
+      return json.optString("description");
+   }
+
+   protected String scrapPrimaryImage(JSONObject json) {
+      return json.optString("image");
+   }
+
+   protected Integer scrapStock(JSONObject json) {
+      Integer stock = 0;
+      String stockStr = json.optString("stock");
+
+      if (Objects.nonNull(stockStr) && !stockStr.equals("")) {
+         stock = Integer.parseInt(stockStr);
+      }
+
+      return stock;
+   }
+
+   protected String scrapEan(JSONObject json) {
+      return json.optString("bar_code");
+   }
+
+   protected Offers scrapOffers(JSONObject json) throws MalformedPricingException, OfferException {
+      Offers offers = new Offers();
+
+      Pricing pricing = scrapPricing(json);
+      List<String> sales = new ArrayList<>();
+      sales.add(CrawlerUtils.calculateSales(pricing));
+
+      Offer offer = new Offer.OfferBuilder().setSellerFullName("Rappi")
+         .setInternalSellerId(json.optString("market_id", null))
+         .setMainPagePosition(1)
+         .setIsBuybox(false)
+         .setPricing(pricing)
+         .setIsMainRetailer(true)
+         .setSales(sales)
+         .build();
+      offers.add(offer);
+
+      return offers;
+   }
+
+   public static Pricing scrapPricing(JSONObject jsonSku) throws MalformedPricingException {
+      Double priceFrom = jsonSku.optDouble("original_price", 0D);
+      Double spotlightPrice = jsonSku.optDouble("price", 0D);
+
+      if (spotlightPrice == 0D || spotlightPrice.equals(priceFrom)) {
+         spotlightPrice = priceFrom;
+         priceFrom = null;
+      }
+      CreditCards creditCards = scrapCreditCards(spotlightPrice);
+
+      return Pricing.PricingBuilder.create()
+         .setSpotlightPrice(spotlightPrice)
+         .setPriceFrom(priceFrom)
+         .setCreditCards(creditCards)
+         .build();
+   }
+
+   public static CreditCards scrapCreditCards(Double spotlightPrice) throws MalformedPricingException {
+      CreditCards creditCards = new CreditCards();
+      Installments installments = new Installments();
+
+      installments.add(Installment.InstallmentBuilder.create()
+         .setInstallmentNumber(1)
+         .setInstallmentPrice(spotlightPrice)
+         .build());
+
+      Set<Card> cards = Sets.newHashSet(
+         Card.VISA,
+         Card.MASTERCARD,
+         Card.DINERS,
+         Card.AMEX,
+         Card.ELO,
+         Card.SHOP_CARD
+      );
+
+      for (Card card : cards) {
+         creditCards.add(CreditCard.CreditCardBuilder.create()
+            .setBrand(card.toString())
+            .setInstallments(installments)
+            .setIsShopCard(false)
+            .build());
+      }
+
+      return creditCards;
+   }
+
+
+}
