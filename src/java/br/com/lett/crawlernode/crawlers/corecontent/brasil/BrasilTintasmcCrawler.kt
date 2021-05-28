075c09db3acab2e76dbe0ac144015e24934abfa9
Charles Fonseca <charlesjrfonseca@gmail.com>
2021-05-28 10:54:01 -0300

    ranking tintasmc

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilTintasmcCrawler.kt
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilTintasmcCrawler.kt
@@ -3,25 +3,28 @@ package br.com.lett.crawlernode.crawlers.corecontent.brasil
 import br.com.lett.crawlernode.core.session.Session
 import br.com.lett.crawlernode.crawlers.extractionutils.core.Tintasmc
 
-class BrasilTintasmcCrawler constructor(session: Session) : Tintasmc(session) {
-    override fun setJsonLocation(): String {
-        return """
-           {
-             "lat": -23.5514188,
-             "lng": -46.7211741,
-             "full_address": "Av. das Nações Unidas - Alto de Pinheiros, São Paulo - SP, 05466, Brasil",
-             "address": {
-               "address": "Avenida das Nações Unidas",
-               "neighborhood": "Alto de Pinheiros",
-               "city": "São Paulo",
-               "state": "SP",
-               "_state": "São Paulo",
-               "country": "BR",
-               "_country": "Brasil",
-               "postal_code": "05466"
-             },
-             "place_id": "EktBdi4gZGFzIE5hw6fDtWVzIFVuaWRhcyAtIEFsdG8gZGUgUGluaGVpcm9zLCBTw6NvIFBhdWxvIC0gU1AsIDA1NDY2LCBCcmF6aWwiLiosChQKEgm_RA79OlbOlBHcB5b-x2OnrhIUChIJ1aA4ijBWzpQRBXaLKuLHye8"
-           }
-        """.trimIndent()
-    }
+class BrasilTintasmcCrawler(session: Session) : Tintasmc(session) {
+
+   override fun setJsonLocation(): String = locationJson
+
+   companion object {
+      val locationJson = """
+              {
+                "lat": -23.5514188,
+                "lng": -46.7211741,
+                "full_address": "Av. das Nações Unidas - Alto de Pinheiros, São Paulo - SP, 05466, Brasil",
+                "address": {
+                  "address": "Avenida das Nações Unidas",
+                  "neighborhood": "Alto de Pinheiros",
+                  "city": "São Paulo",
+                  "state": "SP",
+                  "_state": "São Paulo",
+                  "country": "BR",
+                  "_country": "Brasil",
+                  "postal_code": "05466"
+                },
+                "place_id": "EktBdi4gZGFzIE5hw6fDtWVzIFVuaWRhcyAtIEFsdG8gZGUgUGluaGVpcm9zLCBTw6NvIFBhdWxvIC0gU1AsIDA1NDY2LCBCcmF6aWwiLiosChQKEgm_RA79OlbOlBHcB5b-x2OnrhIUChIJ1aA4ijBWzpQRBXaLKuLHye8"
+              }
+           """.trimIndent()
+   }
 }
--- src/java/br/com/lett/crawlernode/crawlers/corecontent/saopaulo/SaopauloTintasmcviladasaudeCrawler.kt
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/saopaulo/SaopauloTintasmcviladasaudeCrawler.kt
@@ -5,24 +5,25 @@ import br.com.lett.crawlernode.crawlers.extractionutils.core.Tintasmc
 
 class SaopauloTintasmcviladasaudeCrawler(session: Session) : Tintasmc(session) {
 
-   override fun setJsonLocation(): String {
-      //language=JSON
-      return """
-         {
-           "lat": -23.618162,
-           "lng": -46.6278979,
-           "full_address": "Av. Prof. Abraão de Morais - Vila da Saúde, São Paulo - SP, Brasil",
-           "address": {
-             "address": "Avenida Professor Abraão de Morais",
-             "neighborhood": "Vila da Saúde",
-             "city": "São Paulo",
-             "state": "SP",
-             "_state": "São Paulo",
-             "country": "BR",
-             "_country": "Brasil"
-           },
-           "place_id": "EkVBdi4gUHJvZi4gQWJyYcOjbyBkZSBNb3JhaXMgLSBWaWxhIGRhIFNhw7pkZSwgU8OjbyBQYXVsbyAtIFNQLCBCcmF6aWwiLiosChQKEgln_Q4LAVvOlBER2R-K_eNztRIUChIJKccVtlNazpQRPtrD0ItR-z4"
-         }
-      """.trimIndent()
+   override fun setJsonLocation(): String = locationJson
+
+   companion object {
+      val locationJson = """
+            {
+              "lat": -23.618162,
+              "lng": -46.6278979,
+              "full_address": "Av. Prof. Abraão de Morais - Vila da Saúde, São Paulo - SP, Brasil",
+              "address": {
+                "address": "Avenida Professor Abraão de Morais",
+                "neighborhood": "Vila da Saúde",
+                "city": "São Paulo",
+                "state": "SP",
+                "_state": "São Paulo",
+                "country": "BR",
+                "_country": "Brasil"
+              },
+              "place_id": "EkVBdi4gUHJvZi4gQWJyYcOjbyBkZSBNb3JhaXMgLSBWaWxhIGRhIFNhw7pkZSwgU8OjbyBQYXVsbyAtIFNQLCBCcmF6aWwiLiosChQKEgln_Q4LAVvOlBER2R-K_eNztRIUChIJKccVtlNazpQRPtrD0ItR-z4"
+            }
+         """.trimIndent()
    }
 }
