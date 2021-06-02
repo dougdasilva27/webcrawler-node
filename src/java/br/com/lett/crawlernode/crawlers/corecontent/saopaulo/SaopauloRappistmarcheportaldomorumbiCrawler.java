1b8bc079ea266ae9def88def2bda795194b7e47a
bussolotti <matheusbussolotti@gmail.com>
2021-06-02 15:46:14 -0300

    rappi unification

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/saopaulo/SaopauloRappistmarcheportaldomorumbiCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/saopaulo/SaopauloRappistmarcheportaldomorumbiCrawler.java
@@ -5,12 +5,11 @@ import br.com.lett.crawlernode.crawlers.extractionutils.core.BrasilRappiCrawler;
 
 public class SaopauloRappistmarcheportaldomorumbiCrawler extends BrasilRappiCrawler {
 
-   private static final String STORE_TYPE = "st_marche";
-   private static final String LOCATION = "lng=-46.7360631&lat=-23.6163906";
    public static final String STORE_ID = "900033773";
 
    public SaopauloRappistmarcheportaldomorumbiCrawler(Session session) {
       super(session);
+      newUnification = true;
    }
 
    @Override
--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/saopaulo/SaopauloRappistmarcheportaldomorumbiCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/saopaulo/SaopauloRappistmarcheportaldomorumbiCrawler.java
@@ -1,13 +1,21 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.saopaulo;
 
-import java.util.Arrays;
 import br.com.lett.crawlernode.core.session.Session;
-import br.com.lett.crawlernode.crawlers.extractionutils.ranking.RappiCrawlerOld;
+import br.com.lett.crawlernode.crawlers.extractionutils.core.BrasilRappiCrawler;
+import br.com.lett.crawlernode.crawlers.extractionutils.core.RappiCrawler;
 
-public class SaopauloRappistmarcheportaldomorumbiCrawler extends RappiCrawlerOld {
+import java.util.Arrays;
+
+public class SaopauloRappistmarcheportaldomorumbiCrawler extends BrasilRappiCrawler {
 
    public SaopauloRappistmarcheportaldomorumbiCrawler(Session session) {
-      super(session, Arrays.asList("900033773"));
+      super(session);
+      newUnification = true;
+   }
+
+   @Override
+   protected String getStoreId() {
+      return "900033773";
    }
 
 }
