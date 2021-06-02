9207cc6bd89086ecacbfa6bb38d5fd931a246a74
CostaLucasIV <lucascmm3@gmail.com>
2021-06-02 15:05:34 -0300

    vila nova refactor to login with webdriver

--- src/java/br/com/lett/crawlernode/crawlers/extractionutils/ranking/BrasilVilanova.java
+++ src/java/br/com/lett/crawlernode/crawlers/extractionutils/ranking/BrasilVilanova.java
@@ -9,63 +9,61 @@ import org.jsoup.select.Elements;
 
 public abstract class BrasilVilanova extends CrawlerRankingKeywords {
 
-  public BrasilVilanova(Session session) {
-    super(session);
-    super.fetchMode = FetchMode.FETCHER;
-  }
+   public BrasilVilanova(Session session) {
+      super(session);
+      super.fetchMode = FetchMode.FETCHER;
+   }
 
-  @Override
-  protected void extractProductsFromCurrentPage() {
-    this.log("Página " + this.currentPage);
+   @Override
+   protected void extractProductsFromCurrentPage() {
+      this.log("Página " + this.currentPage);
 
-    this.pageSize = 24;
-    String url = "https://www.vilanova.com.br/Busca/Resultado/?p="
-            + this.currentPage
-            + "&loja=&q="
-            + this.keywordEncoded
-            + "&ordenacao=6&limit=24";
+      this.pageSize = 24;
+      String url = "https://www.vilanova.com.br/Busca/Resultado/?p="
+         + this.currentPage
+         + "&loja=&q="
+         + this.keywordEncoded
+         + "&ordenacao=6&limit=24";
 
-    this.log("Link onde são feitos os crawlers: " + url);
-    this.currentDoc = fetchDocument(url);
+      this.log("Link onde são feitos os crawlers: " + url);
+      this.currentDoc = fetchDocument(url);
 
-    Elements products = this.currentDoc.select(".shelf-content-items .box-produto");
+      Elements products = this.currentDoc.select(".shelf-content-items div.shelf-item");
 
-    if (!products.isEmpty()) {
-      if (this.totalProducts == 0) {
-        setTotalProducts();
+      if (!products.isEmpty()) {
+         if (this.totalProducts == 0) {
+            setTotalProducts();
+         }
+
+         for (Element e : products) {
+            String internalPid = CrawlerUtils.scrapStringSimpleInfoByAttribute(e, "button.btn-primary", "data-codigoproduto");
+            String productUrl = CrawlerUtils.scrapStringSimpleInfoByAttribute(e, "p.product-name > a", "href");
+
+            saveDataProduct(null, internalPid, productUrl);
+
+            this.log("Position: " + this.position + " - InternalId: " + null + " - InternalPid: " + internalPid
+               + " - Url: " + productUrl);
+            if (this.arrayProducts.size() == productsLimit) {
+               break;
+            }
+         }
+      } else {
+         this.result = false;
+         this.log("Keyword sem resultado!");
       }
 
-      for (Element e : products) {
-        Element internalIdElement = e.selectFirst(".text-center a");
-        String internalPid = internalIdElement.attr("data-codigoproduto");
-        String productUrl =
-            CrawlerUtils.scrapUrl(e, ".img-name a", "href", "https", "www.vilanova.com.br");
-
-        saveDataProduct(null, internalPid, productUrl);
-
-        this.log("Position: " + this.position + " - InternalId: " + null + " - InternalPid: " + internalPid
-                + " - Url: " + productUrl);
-        if (this.arrayProducts.size() == productsLimit) {
-          break;
-        }
-      }
-    } else {
-      this.result = false;
-      this.log("Keyword sem resultado!");
-    }
-
-    this.log(
-        "Finalizando Crawler de produtos da página "
+      this.log(
+         "Finalizando Crawler de produtos da página "
             + this.currentPage
             + " - até agora "
             + this.arrayProducts.size()
             + " produtos crawleados");
-  }
+   }
 
-  @Override
-  protected void setTotalProducts() {
-    this.totalProducts =
-        CrawlerUtils.scrapIntegerFromHtml(this.currentDoc, ".qtd-produtos", true, 0);
-    this.log("Total da busca: " + this.totalProducts);
-  }
+   @Override
+   protected void setTotalProducts() {
+      this.totalProducts =
+         CrawlerUtils.scrapIntegerFromHtml(this.currentDoc, ".qtd-produtos", true, 0);
+      this.log("Total da busca: " + this.totalProducts);
+   }
 }
