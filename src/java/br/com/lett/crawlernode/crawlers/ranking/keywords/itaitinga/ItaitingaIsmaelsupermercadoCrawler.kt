b5aa112c824a4ce863fd8efb81e2ac36f98a9f5e
Charles Fonseca <charlesjrfonseca@gmail.com>
2021-06-07 09:30:44 -0300

    ismael supermercado change store id

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/itaitinga/ItaitingaIsmaelsupermercadoCrawler.kt
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/itaitinga/ItaitingaIsmaelsupermercadoCrawler.kt
@@ -5,7 +5,7 @@ import br.com.lett.crawlernode.crawlers.extractionutils.ranking.MerconnectRankin
 
 class ItaitingaIsmaelsupermercadoCrawler(session: Session) : MerconnectRanking(session) {
 
-   override fun getStoreId() = "71"
+   override fun getStoreId() = "94"
    override fun getClientId() = "dcdbcf6fdb36412bf96d4b1b4ca8275de57c2076cb9b88e27dc7901e8752cdff"
    override fun getClientSecret() = "27c92c098d3f4b91b8cb1a0d98138b43668c89d677b70bed397e6a5e0971257c"
    override fun getHomePage()= "https://www.ismaelsupermercados.com.br/"
