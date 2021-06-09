0f0247da6baace56c1f373dea66250bd147f81a9
Charles Fonseca <charlesjrfonseca@gmail.com>
2021-06-09 17:39:02 -0300

    vtex impl

---
+++ src/java/br/com/lett/crawlernode/crawlers/extractionutils/core/VTEXNewImpl.kt
@@ -0,0 +1,28 @@
+package br.com.lett.crawlernode.crawlers.extractionutils.core
+
+import br.com.lett.crawlernode.core.session.Session
+import models.RatingsReviews
+import org.apache.http.impl.cookie.BasicClientCookie
+import org.json.JSONObject
+import org.jsoup.nodes.Document
+
+class VTEXNewImpl(session: Session) : VTEXNewScraper(session) {
+
+   override fun handleCookiesBeforeFetch() {
+      session.options?.optJSONObject("cookies")?.toMap()
+         ?.forEach { (key: String?, value: Any) -> cookies.add(BasicClientCookie(key, value.toString())) }
+   }
+
+   override fun getHomePage(): String {
+      return session.options.optString("homePage")
+   }
+
+   override fun getMainSellersNames(): List<String> {
+      return session.options?.optJSONArray("sellers")?.toList()
+         ?.map { obj: Any -> obj.toString() } ?: listOf()
+   }
+
+   override fun scrapRating(internalId: String, internalPid: String, doc: Document, jsonSku: JSONObject): RatingsReviews? {
+      return null
+   }
+}
