8a264bac91eca0226b10efa67c078885d12b5a06
Charles Fonseca <charlesjrfonseca@gmail.com>
2021-06-15 16:54:34 -0300

    fix token

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/saopaulo/SaopauloCarrefourbutantaavenidadeputadojacobCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/saopaulo/SaopauloCarrefourbutantaavenidadeputadojacobCrawler.java
@@ -5,8 +5,9 @@ import br.com.lett.crawlernode.crawlers.extractionutils.ranking.CarrefourCrawler
 
 public class SaopauloCarrefourbutantaavenidadeputadojacobCrawler extends CarrefourCrawler {
 
-   private static final String HOME_PAGE = br.com.lett.crawlernode.crawlers.corecontent.saopaulo.SaopauloCarrefourbutantaavenidadeputadojacobCrawler.HOME_PAGE;
-   public static final String CEP = br.com.lett.crawlernode.crawlers.corecontent.saopaulo.SaopauloCarrefourbutantaavenidadeputadojacobCrawler.CEP;
+   public static final String HOME_PAGE = "https://mercado.carrefour.com.br/";
+   public static final String CEP = "05512-390";
+   public static final String LOCATION_TOKEN = "eyJjYW1wYWlnbnMiOm51bGwsImNoYW5uZWwiOiIyIiwicHJpY2VUYWJsZXMiOm51bGwsInJlZ2lvbklkIjpudWxsLCJ1dG1fY2FtcGFpZ24iOm51bGwsInV0bV9zb3VyY2UiOm51bGwsInV0bWlfY2FtcGFpZ24iOm51bGwsImN1cnJlbmN5Q29kZSI6IkJSTCIsImN1cnJlbmN5U3ltYm9sIjoiUiQiLCJjb3VudHJ5Q29kZSI6IkJSQSIsImN1bHR1cmVJbmZvIjoicHQtQlIiLCJjaGFubmVsUHJpdmFjeSI6InB1YmxpYyJ9";
 
    public SaopauloCarrefourbutantaavenidadeputadojacobCrawler(Session session) {
       super(session);
@@ -19,7 +20,7 @@ public class SaopauloCarrefourbutantaavenidadeputadojacobCrawler extends Carrefo
 
    @Override
    protected String getLocation() {
-      return br.com.lett.crawlernode.crawlers.corecontent.saopaulo.SaopauloCarrefourbutantaavenidadeputadojacobCrawler.LOCATION_TOKEN;
+      return LOCATION_TOKEN;
    }
 
    @Override
