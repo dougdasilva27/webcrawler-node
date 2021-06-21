187ffbb2229c034b4746198910615aed9f283bc3
bussolotti <matheusbussolotti@gmail.com>
2021-06-21 10:41:00 -0300

    rappi Unification

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/saopaulo/SaopauloRappistmarchehigienopolisCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/saopaulo/SaopauloRappistmarchehigienopolisCrawler.java
@@ -7,10 +7,11 @@ public class SaopauloRappistmarchehigienopolisCrawler extends BrasilRappiCrawler
 
    private static final String STORE_TYPE = "st_marche";
    private static final String LOCATION = "lng=-46.6588414&lat=-23.5422447";
-   public static final String STORE_ID = "900020365";
+   public static final String STORE_ID = "900130015";
 
    public SaopauloRappistmarchehigienopolisCrawler(Session session) {
       super(session);
+      newUnification = true;
    }
 
    @Override
