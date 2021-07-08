c5ea61a11512aa1ad7acb7dfb9da4a3b72158a97
marcosmoura <marcosmourabro@gmail.com>
2021-07-08 12:07:35 -0300

    elmache

---
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/panama/PanamaElmachetazoCrawler.java
@@ -0,0 +1,140 @@
+package br.com.lett.crawlernode.crawlers.corecontent.panama;
+
+import br.com.lett.crawlernode.core.models.Card;
+import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.models.ProductBuilder;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.Crawler;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.Logging;
+import com.google.common.collect.Sets;
+import exceptions.MalformedPricingException;
+import exceptions.OfferException;
+import models.Offer;
+import models.Offers;
+import models.pricing.*;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+public class PanamaElmachetazoCrawler extends Crawler {
+
+   private static final String SELLER_FULL_NAME = "El Machetazo";
+   protected Set<String> cards = Sets.newHashSet(Card.VISA.toString(), Card.MASTERCARD.toString(),
+      Card.AURA.toString(), Card.DINERS.toString(), Card.HIPER.toString(), Card.AMEX.toString());
+
+
+   protected PanamaElmachetazoCrawler(Session session) {super(session);}
+
+   @Override
+   public List<Product> extractInformation(Document doc) throws Exception {
+      super.extractInformation(doc);
+      List<Product> products = new ArrayList<>();
+
+      if (isProductPage(doc)) {
+         Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
+
+         String internalId = CrawlerUtils.scrapStringSimpleInfo(doc, ".product-info-price div[data-product-id]", false);
+         String name = CrawlerUtils.scrapStringSimpleInfo(doc, "..page-title .base", false);
+         String primaryImage = CrawlerUtils.scrapSimplePrimaryImage(doc, ".fotorama__stage__frame.fotorama__active  img", Arrays.asList("src"),"http", "elmachetazo.com");
+         // Quando esse scraper foi feito o site não possuia imagem secundaria.
+         boolean availableToBuy = doc.selectFirst(".stock.available") != null;
+         Offers offers = availableToBuy ? scrapOffer(doc, internalId) : new Offers();
+
+         // Creating the product
+         Product product = ProductBuilder.create()
+            .setUrl(session.getOriginalURL())
+            .setInternalId(internalId)
+            .setInternalPid(internalId)
+            .setName(name)
+            .setPrimaryImage(primaryImage)
+            .setOffers(offers)
+            .build();
+
+         products.add(product);
+
+      } else {
+         Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
+      }
+
+      return products;
+
+   }
+
+   private boolean isProductPage(Document doc) {
+      return doc.selectFirst(".columns .product-info-main") != null;
+   }
+
+   private Offers scrapOffer(Document doc, String internalId) throws OfferException, MalformedPricingException {
+      Offers offers = new Offers();
+      Pricing pricing = scrapPricing(doc);
+      List<String> sales = scrapSales(doc);
+
+      offers.add(Offer.OfferBuilder.create()
+         .setUseSlugNameAsInternalSellerId(true)
+         .setSellerFullName(SELLER_FULL_NAME)
+         .setMainPagePosition(1)
+         .setIsBuybox(false)
+         .setIsMainRetailer(true)
+         .setPricing(pricing)
+         .setSales(sales)
+         .build());
+
+      return offers;
+
+   }
+
+   private List<String> scrapSales(Document doc) {
+      List<String> sales = new ArrayList<>();
+
+      Element salesOneElement = doc.selectFirst(".price-box .discount_text");
+      String firstSales = salesOneElement != null ? salesOneElement.text() : null;
+
+      if (firstSales != null && !firstSales.isEmpty()) {
+         sales.add(firstSales);
+      }
+
+      return sales;
+   }
+
+   private Pricing scrapPricing(Document doc) throws MalformedPricingException {
+      Double spotlightPrice = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".price-container.price-final_price span[data-price-type=\"finalPrice\"] .price", null,false, '.',session);
+      Double priceFrom = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".price-container.price-final_price span[data-price-type=\"oldPrice\"] .price", null,false, '.',session);
+
+      CreditCards creditCards = scrapCreditCards(spotlightPrice);
+
+      return Pricing.PricingBuilder.create()
+         .setPriceFrom(priceFrom)
+         .setSpotlightPrice(spotlightPrice)
+         .setCreditCards(creditCards)
+         .build();
+   }
+
+
+   private CreditCards scrapCreditCards(Double spotlightPrice) throws MalformedPricingException {
+      CreditCards creditCards = new CreditCards();
+
+      Installments installments = new Installments();
+      if (installments.getInstallments().isEmpty()) {
+         installments.add(Installment.InstallmentBuilder.create()
+            .setInstallmentNumber(1)
+            .setInstallmentPrice(spotlightPrice)
+            .build());
+      }
+
+      for (String card : cards) {
+         creditCards.add(CreditCard.CreditCardBuilder.create()
+            .setBrand(card)
+            .setInstallments(installments)
+            .setIsShopCard(false)
+            .build());
+      }
+
+      return creditCards;
+   }
+
+}
