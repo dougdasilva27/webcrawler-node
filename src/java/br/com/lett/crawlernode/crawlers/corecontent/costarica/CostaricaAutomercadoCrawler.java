4dcb2a4200da24b691ee2bc8f3f6072dc99d2d39
Thainá <thainaj.aguiar@gmail.com>
2021-07-09 19:31:03 -0300

    add core to automercado

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/costarica/CostaricaAutomercadoCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/costarica/CostaricaAutomercadoCrawler.java
@@ -1,15 +1,160 @@
 package br.com.lett.crawlernode.crawlers.corecontent.costarica;
 
+import br.com.lett.crawlernode.core.fetcher.FetchMode;
+import br.com.lett.crawlernode.core.fetcher.models.Request;
+import br.com.lett.crawlernode.core.models.Card;
+import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.models.ProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.Crawler;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.JSONUtils;
+import br.com.lett.crawlernode.util.Logging;
+import com.google.common.collect.Sets;
+import exceptions.MalformedPricingException;
+import exceptions.OfferException;
+import models.Offer;
+import models.Offers;
+import models.pricing.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CostaricaAutomercadoCrawler extends Crawler {
 
+   protected Set<String> cards = Sets.newHashSet(Card.ELO.toString(), Card.VISA.toString(), Card.MASTERCARD.toString(), Card.AMEX.toString(), Card.HIPERCARD.toString());
 
+   private static final String SELLER_NAME = "automercado";
 
-   protected CostaricaAutomercadoCrawler(Session session) {
+   public CostaricaAutomercadoCrawler(Session session) {
       super(session);
+      config.setFetcher(FetchMode.FETCHER);
    }
 
+   @Override
+   protected JSONObject fetch() {
+      String internalPid = getProductPid();
+      String API = "https://www.automercado.cr/algoliaSearch";
+
+      String payload = "{\"facetFilters\":[[\"productNumber:" + internalPid + "\"],[\"storeid:03\"]]}";
+
+      Map<String, String> headers = new HashMap<>();
+      headers.put("Content-Type", "application/json;charset=UTF-8");
+      Request request = Request.RequestBuilder.create()
+         .setUrl(API)
+         .setPayload(payload)
+         .setHeaders(headers)
+         .mustSendContentEncoding(false)
+         .build();
+      String content = this.dataFetcher
+         .post(session, request)
+         .getBody();
+
+      JSONArray jsonArray = CrawlerUtils.stringToJsonArray(content);
+
+
+      return jsonArray != null ? (JSONObject) jsonArray.get(0) : null;
+   }
+
+
+   private String getProductPid() {
+      String url = null;
+      Pattern pattern = Pattern.compile("\\/([0-9]*)\\?");
+      Matcher matcher = pattern.matcher(session.getOriginalURL());
+      if (matcher.find()) {
+         url = matcher.group(1);
+      }
+      return url;
+   }
+
+   @Override
+   public List<Product> extractInformation(JSONObject json) throws Exception {
+      super.extractInformation(json);
+      List<Product> products = new ArrayList<>();
+
+      if (!json.isEmpty()) {
+         Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
+
+         String internalId = json.optString("productID");
+         String internalPid = getProductPid();
+         String name = json.optString("ecomDescription");
+         String primaryImage = json.optString("imageUrl");
+         String description = json.optString("ecomDescription");
+         boolean available = json.optBoolean("productAvailable");
+         Offers offers = available ? scrapOffers(json) : new Offers();
+
+         Product product = ProductBuilder.create()
+            .setUrl(session.getOriginalURL())
+            .setInternalId(internalId)
+            .setInternalPid(internalPid)
+            .setName(name)
+            .setPrimaryImage(primaryImage)
+            .setDescription(description)
+            .setOffers(offers)
+            .build();
+
+         products.add(product);
+
+      } else {
+         Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
+      }
+
+      return products;
+   }
+
+   private Offers scrapOffers(JSONObject productInfo) throws OfferException, MalformedPricingException {
+      Offers offers = new Offers();
+      Pricing pricing = scrapPricing(productInfo);
+
+      offers.add(Offer.OfferBuilder.create()
+         .setUseSlugNameAsInternalSellerId(true)
+         .setSellerFullName(SELLER_NAME)
+         .setIsBuybox(false)
+         .setIsMainRetailer(true)
+         .setPricing(pricing)
+         .build());
+
+      return offers;
+
+   }
+
+
+   private Pricing scrapPricing(JSONObject productInfo) throws MalformedPricingException {
+
+      Double spotlightPrice = JSONUtils.getDoubleValueFromJSON(productInfo, "uomPrice", true);
+      CreditCards creditCards = scrapCreditCards(spotlightPrice);
+      BankSlip bankSlip = BankSlip.BankSlipBuilder.create()
+         .setFinalPrice(spotlightPrice)
+         .build();
+
+      return Pricing.PricingBuilder.create()
+         .setSpotlightPrice(spotlightPrice)
+         .setCreditCards(creditCards)
+         .setBankSlip(bankSlip)
+         .build();
+   }
+
+   private CreditCards scrapCreditCards(Double spotlightPrice) throws MalformedPricingException {
+      CreditCards creditCards = new CreditCards();
+
+      Installments installments = new Installments();
+      installments.add(Installment.InstallmentBuilder.create()
+         .setInstallmentNumber(1)
+         .setInstallmentPrice(spotlightPrice)
+         .build());
+
+      for (String brand : cards) {
+         creditCards.add(CreditCard.CreditCardBuilder.create()
+            .setBrand(brand)
+            .setIsShopCard(false)
+            .setInstallments(installments)
+            .build());
+      }
+
+      return creditCards;
+   }
 
 }
--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/costarica/CostaricaAutomercadoCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/costarica/CostaricaAutomercadoCrawler.java
@@ -1,5 +1,6 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.costarica;
 
+import br.com.lett.crawlernode.core.fetcher.FetchMode;
 import br.com.lett.crawlernode.core.fetcher.models.Request;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
@@ -9,14 +10,16 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CostaricaAutomercadoCrawler extends CrawlerRankingKeywords {
 
 
-   private int numberPageResult = 0;
-
    public CostaricaAutomercadoCrawler(Session session) {
       super(session);
+      super.fetchMode = FetchMode.FETCHER;
+
    }
 
    @Override
@@ -26,38 +29,60 @@ public class CostaricaAutomercadoCrawler extends CrawlerRankingKeywords {
 
       JSONObject apiJson = fetchProducts();
 
-      if(apiJson!=null && !apiJson.isEmpty()) {
-         JSONObject result = JSONUtils.getValueRecursive(apiJson,"results.0",JSONObject.class);
-         numberPageResult = result.optInt("nbPages",0);
+
+      if (apiJson != null && !apiJson.isEmpty()) {
+         JSONObject result = JSONUtils.getValueRecursive(apiJson, "results.0", JSONObject.class);
+         if (this.totalProducts == 0) {
+            this.totalProducts = result.optInt("nbHits", 0);
+         }
          JSONArray products = result.optJSONArray("hits");
          for (Object o : products) {
             JSONObject product = (JSONObject) o;
 
             String internalId = product.optString("productID");
-            String internalPId = product.optString("productNumber");
-            String url = "https://www.automercado.cr/shop/"+internalPId +"?objectID="+internalId;
+            String internalPid = product.optString("productNumber");
+            String url = getUrl(internalId, internalPid);
 
-            saveDataProduct(internalId,internalPId,url);
+            saveDataProduct(internalId, internalPid, url);
+            this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + internalPid + " - Url: " + url);
 
          }
 
 
+      } else {
+         log("keyword sem resultado");
       }
 
 
    }
 
+   private String getUrl(String internalId, String internalPid) {
+      String url = null;
+      if (internalId != null && internalPid != null) {
+         url = "https://www.automercado.cr/shop/" + internalPid + "?objectID=" + internalId;
+
+      }
+      return url;
+
+   }
+
    private JSONObject fetchProducts() {
 
-      String payload ="{ \"requests\": [ { \"indexName\": \"Product_Catalogue\", \"params\": \"query="+this.keywordEncoded+"page="+this.currentPage+"\" } ] }";
+      String payload = "{\"requests\":[{\"indexName\":\"Product_Catalogue\",\"params\":\"query=" + this.keywordWithoutAccents + "&getRankingInfo=true&clickAnalytics=true&filters=storeid%3A%2203%22&highlightPreTag=__ais-highlight__&highlightPostTag=__%2Fais-highlight__&maxValuesPerFacet=50&page=" + (this.currentPage - 1) + "&facets=%5B%22catecom%22%2C%22parentProductid2%22%2C%22parentProductid%22%2C%22marca%22%5D&tagFilters=\"}]}";
 
-
-      Request request  = Request.RequestBuilder.create()
+      Map<String, String> headers = new HashMap<>();
+      headers.put("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36");
+      headers.put("content-type", "application/x-www-form-urlencoded");
+      headers.put("Accept", "*/*");
+      Request request = Request.RequestBuilder.create()
          .setUrl("https://fu5xfx7knl-dsn.algolia.net/1/indexes/*/queries?x-algolia-api-key=113941a18a90ae0f17d602acd16f91b2&x-algolia-application-id=FU5XFX7KNL")
          .setPayload(payload)
+         .setHeaders(headers)
+         .mustSendContentEncoding(false)
          .build();
+      String content = this.dataFetcher.post(session, request).getBody();
 
-      return CrawlerUtils.stringToJson(dataFetcher.post(session,request).getBody());
+      return CrawlerUtils.stringToJson(content);
 
    }
 }
