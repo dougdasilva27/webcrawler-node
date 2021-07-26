2e7754d066a1dbeea670d1bb141a36e926197d4d
marcosmoura <marcosmourabro@gmail.com>
2021-07-26 12:05:59 -0300

    Fix Ranking

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/saopaulo/SaopauloDrogasilCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/saopaulo/SaopauloDrogasilCrawler.java
@@ -1,16 +1,14 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.saopaulo;
 
-import java.util.HashMap;
-import java.util.Map;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import br.com.lett.crawlernode.core.fetcher.methods.FetcherDataFetcher;
 import br.com.lett.crawlernode.core.fetcher.models.Request;
 import br.com.lett.crawlernode.core.fetcher.models.Request.RequestBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
 import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.JSONUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.jsoup.nodes.Element;
 
 public class SaopauloDrogasilCrawler extends CrawlerRankingKeywords {
 
@@ -20,50 +18,40 @@ public class SaopauloDrogasilCrawler extends CrawlerRankingKeywords {
 
    @Override
    protected void extractProductsFromCurrentPage() {
-      this.pageSize = 12;
-
-      this.log("Página " + this.currentPage);
-      String keyword = this.keywordWithoutAccents.replace(" ", "%20");
+      this.pageSize = 24;
 
       this.log("Página " + this.currentPage);
 
       // monta a url com a keyword e a página
-      String url = "https://busca.drogasil.com.br/search?w=" + keyword + "&cnt=36&srt="
-            + this.arrayProducts.size();
-
-      Map<String, String> headers = new HashMap<>();
-      headers.put("upgrade-insecure-requests", "1");
-      headers.put("accept", "text/html");
-      headers.put("accept-language", "pt-BR");
+      String url = "https://api-gateway-prod.drogasil.com.br/search/v1/store/DROGASIL/product/search/live?term=" + this.keywordEncoded + "&limit="
+         + this.pageSize + "&offset=" + this.arrayProducts.size();
 
       this.log("Link onde são feitos os crawlers: " + url);
 
-      Request request = RequestBuilder.create().setUrl(url).setCookies(cookies).setHeaders(headers).build();
-      String response = new FetcherDataFetcher().get(session, request).getBody();
+      Request request = RequestBuilder.create().setUrl(url).setCookies(cookies).build();
+      String response = this.dataFetcher.get(session,request).getBody();
 
-      if (response != null && !response.isEmpty()) {
-         this.currentDoc = Jsoup.parse(response);
-      } else {
-         this.currentDoc = fetchDocument(url);
-      }
-
-
-      Elements products = this.currentDoc.select(".item div.container:not(.min-limit)");
+      JSONObject gridInfo = CrawlerUtils.stringToJson(response);
+      JSONArray products = JSONUtils.getValueRecursive(gridInfo, "results.products", JSONArray.class);
 
       if (!products.isEmpty()) {
          if (this.totalProducts == 0) {
-            setTotalProducts();
+            setTotalProducts(gridInfo);
          }
 
-         for (Element e : products) {
-            String internalId = crawlInternalId(e);
-            String productUrl = crawlProductUrl(e);
+         for (Object o : products) {
 
-            saveDataProduct(internalId, null, productUrl);
+            if (o instanceof JSONObject) {
+               JSONObject productInfo = (JSONObject) o;
+               String internalId = productInfo.optString("sku");
+               String productUrl = productInfo.optString("urlKey");
 
-            this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + null + " - Url: " + productUrl);
-            if (this.arrayProducts.size() == productsLimit) {
-               break;
+               saveDataProduct(internalId, null, productUrl);
+
+               this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + null + " - Url: " + productUrl);
+               if (this.arrayProducts.size() == productsLimit) {
+                  break;
+               }
             }
          }
       } else {
@@ -74,49 +62,8 @@ public class SaopauloDrogasilCrawler extends CrawlerRankingKeywords {
       this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
    }
 
-   @Override
-   protected void setTotalProducts() {
-      Element totalElement = this.currentDoc.selectFirst("p.amount");
-
-      if (totalElement != null && this.currentDoc.selectFirst("#pantene") == null) {
-         String token = totalElement.text().replaceAll("[^0-9]", "").trim();
-
-         if (!token.isEmpty()) {
-            this.totalProducts = Integer.parseInt(token);
-         }
-
-         this.log("Total da busca: " + this.totalProducts);
-      }
+   protected void setTotalProducts(JSONObject gridinfo) {
+      this.totalProducts = JSONUtils.getValueRecursive(gridinfo, "metadata.totalCount", Integer.class);
+      this.log("Total da busca: " + this.totalProducts);
    }
-
-   private String crawlInternalId(Element e) {
-      String internalId = null;
-      Element id = e.selectFirst(".trustvox-shelf-container[data-trustvox-product-code]");
-
-      if (id != null) {
-         internalId = id.attr("data-trustvox-product-code");
-      }
-
-      return internalId;
-   }
-
-   private String crawlProductUrl(Element e) {
-      String urlProduct = null;
-      Element urlElement = e.selectFirst(".product-name.sli_title a");
-
-      if (urlElement != null) {
-         urlProduct = urlElement.attr("title");
-      } else {
-         urlElement = e.selectFirst(".product-name a");
-
-         if (urlElement != null) {
-
-            urlProduct = CrawlerUtils.completeUrl(urlElement.attr("href"), "https://", "www.drogasil.com.br");
-
-         }
-      }
-
-      return urlProduct;
-   }
-
 }
