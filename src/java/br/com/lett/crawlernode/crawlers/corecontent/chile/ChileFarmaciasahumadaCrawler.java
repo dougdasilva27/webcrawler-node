cdd2f681e6d145db1e39e69b83f3574551ae0c9d
marcosmoura <marcosmourabro@gmail.com>
2021-07-27 11:31:48 -0300

    Novo crawler

---
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/chile/ChileFarmaciasahumadaCrawler.java
@@ -0,0 +1,172 @@
+package br.com.lett.crawlernode.crawlers.corecontent.chile;
+
+import br.com.lett.crawlernode.core.models.Card;
+import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.models.ProductBuilder;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.Crawler;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.Logging;
+import br.com.lett.crawlernode.util.MathUtils;
+import com.google.common.collect.Sets;
+import exceptions.MalformedPricingException;
+import exceptions.OfferException;
+import models.Offer;
+import models.Offers;
+import models.pricing.*;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+public class ChileFarmaciasahumadaCrawler extends Crawler {
+
+   private static final String SELLER_FULL_NAME = "farmaciasahumada";
+   protected Set<String> cards = Sets.newHashSet(Card.VISA.toString(), Card.MASTERCARD.toString(),
+      Card.AURA.toString(), Card.DINERS.toString(), Card.HIPER.toString(), Card.AMEX.toString());
+
+   public ChileFarmaciasahumadaCrawler(Session session) {
+      super(session);
+   }
+
+   @Override
+   public List<Product> extractInformation(Document doc) throws Exception {
+      List<Product> products = new ArrayList<>();
+
+      if (isProductPage(doc)) {
+         Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
+
+         String internalId = CrawlerUtils.scrapStringSimpleInfoByAttribute(doc, ".product-info-price > div", "data-product-id");
+         String internalPid = internalId;
+         String name = CrawlerUtils.scrapStringSimpleInfo(doc, ".page-title-wrapper.product span", false);
+         String primaryImage = CrawlerUtils.scrapSimplePrimaryImage(doc,".product.media .gallery-placeholder img", Arrays.asList("src"),"https", "cd-ahumada-premium.azureedge.net/catalog/product/cache/");
+         boolean available = doc.selectFirst(".actions #product-addtocart-button") != null;
+         Offers offers = available ? scrapOffer(doc) : null;
+
+         Elements description = doc.select(".data.table.additional-attributes tbody");
+
+         Product product = ProductBuilder.create()
+            .setUrl(session.getOriginalURL())
+            .setInternalId(internalId)
+            .setInternalPid(internalPid)
+            .setName(name)
+            .setOffers(offers)
+            .setPrimaryImage(primaryImage)
+            .setDescription(description.toString())
+            .build();
+
+         products.add(product);
+      } else {
+         Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
+      }
+
+      return products;
+   }
+
+   private boolean isProductPage(Document doc){
+      return doc.selectFirst(".product-info-main") != null;
+   }
+
+
+   private Offers scrapOffer(Document doc) throws OfferException, MalformedPricingException {
+      Offers offers = new Offers();
+      Pricing pricing = scrapPricing(doc);
+      List<String> sales = scrapSales(doc);
+
+      offers.add(Offer.OfferBuilder.create()
+         .setUseSlugNameAsInternalSellerId(true)
+         .setSellerFullName(SELLER_FULL_NAME)
+         .setMainPagePosition(1)
+         .setIsBuybox(false)
+         .setIsMainRetailer(true)
+         .setPricing(pricing)
+         .setSales(sales)
+         .build());
+
+      return offers;
+
+   }
+
+   private List<String> scrapSales(Document doc) {
+      List<String> sales = new ArrayList<>();
+
+      Element salesOneElement = doc.selectFirst(".promo-container .promo_label .prom_fa");
+      String firstSales = salesOneElement != null ? salesOneElement.text() : null;
+
+      if (firstSales != null && !firstSales.isEmpty()) {
+         sales.add(firstSales);
+      }
+
+      return sales;
+   }
+
+   private Pricing scrapPricing(Document doc) throws MalformedPricingException {
+      Double spotlightPrice = CrawlerUtils.scrapDoublePriceFromHtml(doc,".product-info-main .special-price .price-container.price-final_price .price", null,false,'.',session);
+      Double priceFrom = CrawlerUtils.scrapDoublePriceFromHtml(doc,".product-info-main .old-price .price-container .price", null,false,'.',session);
+
+      if(spotlightPrice == null) {
+         spotlightPrice = CrawlerUtils.scrapDoublePriceFromHtml(doc,".product-info-main .price-container.price-final_price .price", null,false,'.',session);
+      }
+
+
+      CreditCards creditCards = scrapCreditCards(spotlightPrice);
+
+      return Pricing.PricingBuilder.create()
+         .setPriceFrom(priceFrom)
+         .setSpotlightPrice(spotlightPrice)
+         .setCreditCards(creditCards)
+         .build();
+   }
+
+
+   private CreditCards scrapCreditCards(Double spotlightPrice) throws MalformedPricingException {
+      CreditCards creditCards = new CreditCards();
+
+      Installments installments = new Installments();
+      if (installments.getInstallments().isEmpty()) {
+         installments.add(Installment.InstallmentBuilder.create()
+            .setInstallmentNumber(1)
+            .setInstallmentPrice(spotlightPrice)
+            .build());
+      }
+
+      for (String card : cards) {
+         creditCards.add(CreditCard.CreditCardBuilder.create()
+            .setBrand(card)
+            .setInstallments(installments)
+            .setIsShopCard(false)
+            .build());
+      }
+
+      return creditCards;
+   }
+
+   public Installments scrapInstallments(Document doc) throws MalformedPricingException {
+      Installments installments = new Installments();
+
+      Element installmentsCard = doc.selectFirst(".info_productPrice .product_discount_pay span");
+
+      if (installmentsCard != null) {
+         String installmentString = installmentsCard.text().replaceAll("[^0-9]", "").trim();
+         Integer installment = !installmentString.isEmpty() ? Integer.parseInt(installmentString) : null;
+         Element valueElement = doc.selectFirst(".info_productPrice .product_discount_pay strong");
+
+         if (valueElement != null && installment != null) {
+            Double value = MathUtils.parseDoubleWithComma(valueElement.text());
+
+            installments.add(Installment.InstallmentBuilder.create()
+               .setInstallmentNumber(installment)
+               .setInstallmentPrice(value)
+               .build());
+         }
+      }
+
+      return installments;
+   }
+
+
+}
