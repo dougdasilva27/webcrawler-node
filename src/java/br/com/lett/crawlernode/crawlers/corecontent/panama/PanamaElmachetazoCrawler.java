0929d13296cc460a3fb715c954841a23aca44d8b
Thainá <thainaj.aguiar@gmail.com>
2021-07-28 16:58:20 -0300

    removing imports

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/panama/PanamaElmachetazoCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/panama/PanamaElmachetazoCrawler.java
@@ -1,10 +1,7 @@
 package br.com.lett.crawlernode.crawlers.corecontent.panama;
 
-import br.com.lett.crawlernode.core.fetcher.FetchMode;
 import br.com.lett.crawlernode.core.fetcher.ProxyCollection;
-import br.com.lett.crawlernode.core.fetcher.methods.FetcherDataFetcher;
 import br.com.lett.crawlernode.core.fetcher.models.Request;
-import br.com.lett.crawlernode.core.fetcher.models.Response;
 import br.com.lett.crawlernode.core.models.Card;
 import br.com.lett.crawlernode.core.models.Product;
 import br.com.lett.crawlernode.core.models.ProductBuilder;
@@ -38,8 +35,8 @@ public class PanamaElmachetazoCrawler extends Crawler {
    protected Object fetch() {
 
       Map<String, String> headers = new HashMap<>();
-      headers.put("user-agent","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36");
-      headers.put("cookie","cf_clearance=27767dd1d813c1c782def2851ff86fece5ecccc5-1627501885-0-150;"); //Thu Jul 28 2022 17:51:25 GMT-0300 (Brasilia Standard Time)
+      headers.put("user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36");
+      headers.put("cookie", "cf_clearance=27767dd1d813c1c782def2851ff86fece5ecccc5-1627501885-0-150;"); //Thu Jul 28 2022 17:51:25 GMT-0300 (Brasilia Standard Time)
 
       Request request = Request.RequestBuilder.create().setUrl(session.getOriginalURL()).setHeaders(headers).setProxyservice(
          Arrays.asList(
