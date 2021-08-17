f53865f0d0504cab3298969e51285a5fbbf721b2
bussolotti <matheusbussolotti@gmail.com>
2021-08-17 14:52:36 -0300

    fix wong

--- src/java/br/com/lett/crawlernode/crawlers/extractionutils/ranking/PeruRappiCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/extractionutils/ranking/PeruRappiCrawler.java
@@ -1,16 +1,23 @@
 package br.com.lett.crawlernode.crawlers.extractionutils.ranking;
 
+import br.com.lett.crawlernode.core.fetcher.models.Request;
+import br.com.lett.crawlernode.core.fetcher.models.Response;
 import br.com.lett.crawlernode.core.session.Session;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PeruRappiCrawler extends RappiCrawlerRanking {
 
    public PeruRappiCrawler(Session session) {
       super(session);
-      PRODUCT_BASE_URL = "https://www."+getProductDomain()+"/produto/";
+      PRODUCT_BASE_URL = "https://www." + getProductDomain() + "/produto/";
    }
 
-   public static final String API_DOMAIN = "rappi.com.pe";
+   public static final String API_DOMAIN = "rappi.pe";
    public static final String PRODUCT_DOMAIN = "rappi.com.pe";
+   public final String storeName = session.getOptions().optString("storeName");
 
    @Override
    protected String getApiDomain() {
@@ -21,4 +28,6 @@ public class PeruRappiCrawler extends RappiCrawlerRanking {
    protected String getProductDomain() {
       return PRODUCT_DOMAIN;
    }
+
+
 }
