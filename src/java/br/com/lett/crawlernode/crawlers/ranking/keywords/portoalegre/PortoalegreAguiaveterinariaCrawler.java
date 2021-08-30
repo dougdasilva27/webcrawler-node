55ad156bde5ef8a9969902e984be289065f0f0e6
Thainá <thainaj.aguiar@gmail.com>
2021-08-30 11:50:59 -0300

    refactoring aguia veterinaria

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/portoalegre/PortoalegreAguiaveterinariaCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/portoalegre/PortoalegreAguiaveterinariaCrawler.java
@@ -1,62 +1,64 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.portoalegre;
 
-import java.util.Arrays;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.util.CommonMethods;
 import br.com.lett.crawlernode.util.CrawlerUtils;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.Collections;
 
 public class PortoalegreAguiaveterinariaCrawler extends CrawlerRankingKeywords {
 
-  public PortoalegreAguiaveterinariaCrawler(Session session) {
-    super(session);
-  }
+   public PortoalegreAguiaveterinariaCrawler(Session session) {
+      super(session);
+   }
 
-  @Override
-  protected void extractProductsFromCurrentPage() {
-    this.pageSize = 24;
-    this.log("Página " + this.currentPage);
+   @Override
+   protected void extractProductsFromCurrentPage() {
+      this.pageSize = 24;
+      this.log("Página " + this.currentPage);
 
-    String url = "https://www.aguiaveterinaria.com.br/busca/page:" + this.currentPage + "?item=" + this.keywordEncoded;
+      String url = "https://www.aguiaveterinaria.com.br/busca?busca=" + this.keywordEncoded + "&pagina=" + this.currentPage;
 
-    this.log("Link onde são feitos os crawlers: " + url);
-    this.currentDoc = fetchDocument(url);
-    Elements products = this.currentDoc.select(".product-list-item");
+      this.log("Link onde são feitos os crawlers: " + url);
+      this.currentDoc = fetchDocument(url);
+      Elements products = this.currentDoc.select(".row .fbits-item-lista-spot");
 
-    if (!products.isEmpty()) {
+      if (!products.isEmpty()) {
 
-      if (this.totalProducts == 0) {
-        setTotalProducts();
+         if (this.totalProducts == 0) {
+            setTotalProducts();
+         }
+
+         for (Element e : products) {
+            String productUrl = CrawlerUtils.scrapUrl(e, ".spotContent a ", Collections.singletonList("href"), "https", "www.aguiaveterinaria.com.br");
+            String internalPid = CommonMethods.getLast(productUrl.split("-"));
+            saveDataProduct(null, internalPid, productUrl);
+
+            this.log(
+               "Position: " + this.position +
+                  " - InternalId: " + null +
+                  " - InternalPid: " + internalPid +
+                  " - Url: " + productUrl);
+
+            if (this.arrayProducts.size() == productsLimit)
+               break;
+         }
+
+      } else {
+         this.result = false;
+         this.log("Keyword sem resultado!");
       }
 
-      for (Element e : products) {
-        String productUrl = CrawlerUtils.scrapUrl(e, ".title > a", Arrays.asList("href"), "https", "www.aguiaveterinaria.com.br");
-        String internalId = CrawlerUtils.scrapStringSimpleInfoByAttribute(e, ".md-button[data-grade-id]", "data-grade-id");
+      this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora "
+         + this.arrayProducts.size() + " produtos crawleados");
+   }
 
-        saveDataProduct(internalId, null, productUrl);
-
-        this.log(
-            "Position: " + this.position +
-                " - InternalId: " + internalId +
-                " - InternalPid: " + null +
-                " - Url: " + productUrl);
-
-        if (this.arrayProducts.size() == productsLimit)
-          break;
-      }
-
-    } else {
-      this.result = false;
-      this.log("Keyword sem resultado!");
-    }
-
-    this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora "
-        + this.arrayProducts.size() + " produtos crawleados");
-  }
-
-  @Override
-  protected boolean hasNextPage() {
-    return !this.currentDoc.select(".pagination-buttons .next").isEmpty();
-  }
+   @Override
+   protected boolean hasNextPage() {
+      String nextPage = CrawlerUtils.scrapStringSimpleInfo(this.currentDoc, ".fbits-paginacao li:last-child a", true);
+      return nextPage != null && nextPage.equals(">");
+   }
 }
