3ad72e76554b244f17ad536a3280fdc09516b600
bussolotti <matheusbussolotti@gmail.com>
2021-08-31 16:36:44 -0300

    fix shoptime

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/saopaulo/SaopauloShoptimeCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/saopaulo/SaopauloShoptimeCrawler.java
@@ -2,15 +2,45 @@ package br.com.lett.crawlernode.crawlers.ranking.keywords.saopaulo;
 
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.crawlers.extractionutils.ranking.B2WCrawlerRanking;
+import br.com.lett.crawlernode.crawlers.extractionutils.ranking.B2WScriptPageCrawlerRanking;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
-public class SaopauloShoptimeCrawler extends B2WCrawlerRanking {
+import java.util.HashMap;
+import java.util.Map;
+
+public class SaopauloShoptimeCrawler extends B2WScriptPageCrawlerRanking {
+
+   private static final String HOME_PAGE = "https://www.shoptime.com.br/";
 
   public SaopauloShoptimeCrawler(Session session) {
     super(session);
   }
+   @Override
+   protected String getHomePage() {
+      return HOME_PAGE;
+   }
 
-  @Override
-  protected String getStoreName() {
-    return "shoptime";
-  }
+   @Override
+   protected Document fetchPage() {
+      String keyword = this.keywordWithoutAccents.replace(" ", "-");
+
+      String url = homePage + "busca/" + keyword + "?limit=24&offset=" + (this.currentPage - 1) * pageSize;
+      this.log("Link onde são feitos os crawlers: " + url);
+
+      Map<String,String> headers = new HashMap<>();
+
+      headers.put("authority", "www.americanas.com.br");
+      headers.put("sec-ch-ua", " \" Not A;Brand\";v=\"99\", \"Chromium\";v=\"90\", \"Google Chrome\";v=\"90\"");
+      headers.put("sec-ch-ua-mobile", "?0");
+      headers.put("upgrade-insecure-requests", "1");
+      headers.put("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.9");
+      headers.put("sec-fetch-site", "none");
+      headers.put("sec-fetch-mode", "navigate");
+      headers.put("sec-fetch-user", "?1");
+      headers.put("sec-fetch-dest", "document");
+      headers.put("accept-language", "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7,es;q=0.6");
+
+      return Jsoup.parse(br.com.lett.crawlernode.crawlers.corecontent.saopaulo.SaopauloAmericanasCrawler.fetchPage(url,this.dataFetcher,this.cookies,headers,session));
+   }
 }
