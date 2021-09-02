2c141514fc6fbd1b87f355f670ecfdd1aa00b41a
Thainá <thainaj.aguiar@gmail.com>
2021-09-02 14:51:38 -0300

    bugfix/carrefour_home_page

--- src/java/br/com/lett/crawlernode/crawlers/extractionutils/core/ArgentinaCarrefoursuper.java
+++ src/java/br/com/lett/crawlernode/crawlers/extractionutils/core/ArgentinaCarrefoursuper.java
@@ -28,8 +28,50 @@ public abstract class ArgentinaCarrefoursuper extends CarrefourCrawler {
    }
 
    @Override
+   protected String fetchPage(String url) {
+
+      Map<String, String> headers = new HashMap<>();
+
+      String token = getLocationToken();
+
+      String userLocationData = getCep();
+      headers.put("accept", "*/*");
+
+      StringBuilder cookiesBuilder = new StringBuilder();
+      if (token != null) {
+         cookiesBuilder.append("vtex_segment=").append(token).append(";");
+      }
+      if (userLocationData != null) {
+         cookiesBuilder.append("userLocationData=").append(userLocationData).append(";");
+      }
+      headers.put("cookie", cookiesBuilder.toString());
+
+      Request request = RequestBuilder.create()
+         .setUrl(url)
+         .setHeaders(headers)
+         .setSendUserAgent(false)
+         .mustSendContentEncoding(false)
+         .setFetcheroptions(
+            FetcherOptions.FetcherOptionsBuilder.create()
+               .mustUseMovingAverage(false)
+               .mustRetrieveStatistics(true)
+               .build())
+         .setProxyservice(Arrays.asList(
+            ProxyCollection.NETNUT_RESIDENTIAL_AR_HAPROXY,
+            ProxyCollection.NETNUT_RESIDENTIAL_ANY_HAPROXY,
+            ProxyCollection.LUMINATI_SERVER_BR,
+            ProxyCollection.NO_PROXY)
+         )
+         .build();
+
+      Response response = alternativeFetch(request);
+
+      return response.getBody();
+   }
+
+   @Override
    protected String getHomePage() {
-      return "https://carrefour.com.ar/";
+      return "https://www.carrefour.com.ar/";
    }
 
    @Override
