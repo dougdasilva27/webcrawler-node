db55ec9a6563ab5049b6e9894a6c3c3b2d4f4f25
Thainá <thainaj.aguiar@gmail.com>
2021-09-03 00:56:00 -0300

    newcrawler/CU-8z0eu9_Palato---Macei-AL

---
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/maceio/MaceioPalatoCrawler.java
@@ -0,0 +1,147 @@
+package br.com.lett.crawlernode.crawlers.corecontent.maceio;
+
+import br.com.lett.crawlernode.core.models.Card;
+import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.models.ProductBuilder;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.Crawler;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.Logging;
+import com.google.common.collect.Sets;
+import exceptions.MalformedPricingException;
+import exceptions.OfferException;
+import models.Offer;
+import models.Offers;
+import models.pricing.*;
+import org.jsoup.nodes.Document;
+
+import java.util.*;
+
+public class MaceioPalatoCrawler extends Crawler {
+
+   private static final String HOME_PAGE = "https://loja.palato.com.br/";
+   private static final String SELLER_FULL_NAME = "Palato";
+   protected Set<String> cards = Sets.newHashSet(Card.VISA.toString(), Card.MASTERCARD.toString(),
+      Card.AURA.toString(), Card.DINERS.toString(), Card.HIPER.toString(), Card.AMEX.toString());
+
+
+   public MaceioPalatoCrawler(Session session) {
+      super(session);
+   }
+
+   @Override
+   public boolean shouldVisit() {
+      String href = this.session.getOriginalURL().toLowerCase();
+      return !FILTERS.matcher(href).matches() && (href.startsWith(HOME_PAGE));
+   }
+
+   @Override
+   public List<Product> extractInformation(Document doc) throws Exception {
+      super.extractInformation(doc);
+      List<Product> products = new ArrayList<>();
+
+      if (isProductPage(doc)) {
+         Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
+
+         String internalId = CrawlerUtils.scrapStringSimpleInfoByAttribute(doc, ".product-options input[name='id']", "value");
+         String internalPid = CrawlerUtils.scrapStringSimpleInfoByAttribute(doc, ".product-options input[name='sku']", "value");
+         String name = CrawlerUtils.scrapStringSimpleInfo(doc, "h3.font-weight-bold", true);
+         String primaryImage = CrawlerUtils.scrapSimplePrimaryImage(doc, ".sp-wrap img", Collections.singletonList("src"), "https", "loja.palato.com.br/");
+         String description = CrawlerUtils.scrapSimpleDescription(doc, Arrays.asList(".produto-informacoes"));
+         boolean availableToBuy = !doc.select(".col-xl-6 .add-to-cart ").isEmpty();
+         Offers offers = availableToBuy ? scrapOffer(doc) : new Offers();
+
+         // Creating the product
+         Product product = ProductBuilder.create()
+            .setUrl(session.getOriginalURL())
+            .setInternalId(internalId)
+            .setInternalPid(internalPid)
+            .setName(name)
+            .setPrimaryImage(primaryImage)
+            //  .setSecondaryImages(secondaryImages)
+            .setDescription(description)
+            .setOffers(offers)
+            .build();
+
+         products.add(product);
+
+      } else {
+         Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
+      }
+
+      return products;
+
+   }
+
+
+   private boolean isProductPage(Document doc) {
+      return !doc.select(".produto-informacoes").isEmpty();
+   }
+
+   private Offers scrapOffer(Document doc) throws OfferException, MalformedPricingException {
+      Offers offers = new Offers();
+      Pricing pricing = scrapPricing(doc);
+      List<String> sales = scrapSales(pricing);
+
+      offers.add(Offer.OfferBuilder.create()
+         .setUseSlugNameAsInternalSellerId(true)
+         .setSellerFullName(SELLER_FULL_NAME)
+         .setMainPagePosition(1)
+         .setIsBuybox(false)
+         .setIsMainRetailer(true)
+         .setPricing(pricing)
+         .setSales(sales)
+         .build());
+
+      return offers;
+
+   }
+
+   private List<String> scrapSales(Pricing pricing) {
+      List<String> sales = new ArrayList<>();
+
+      String sale = CrawlerUtils.calculateSales(pricing);
+
+      if (sale != null) {
+         sales.add(sale);
+      }
+
+      return sales;
+   }
+
+   private Pricing scrapPricing(Document doc) throws MalformedPricingException {
+      Double spotlightPrice = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".h3.green", null, true, ',', session);
+      Double priceFrom = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".size-2.mb-0.font-weight-normal.sale", null, true, ',', session);
+      CreditCards creditCards = scrapCreditCards(spotlightPrice);
+
+      return Pricing.PricingBuilder.create()
+         .setPriceFrom(priceFrom)
+         .setSpotlightPrice(spotlightPrice)
+         .setCreditCards(creditCards)
+         .build();
+   }
+
+
+   private CreditCards scrapCreditCards(Double spotlightPrice) throws MalformedPricingException {
+      CreditCards creditCards = new CreditCards();
+
+      Installments installments = new Installments();
+      if (installments.getInstallments().isEmpty()) {
+         installments.add(Installment.InstallmentBuilder.create()
+            .setInstallmentNumber(1)
+            .setInstallmentPrice(spotlightPrice)
+            .build());
+      }
+
+      for (String card : cards) {
+         creditCards.add(CreditCard.CreditCardBuilder.create()
+            .setBrand(card)
+            .setInstallments(installments)
+            .setIsShopCard(false)
+            .build());
+      }
+
+      return creditCards;
+   }
+
+}
