5dea00ffd92f606a2db154e9a82c142cc93b6bef
CostaLucasIV <lucascmm3@gmail.com>
2021-09-08 17:29:47 -0300

    refactoring rihappy

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilRihappyCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilRihappyCrawler.java
@@ -1,55 +1,145 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
 
+import br.com.lett.crawlernode.core.fetcher.models.Request;
+import br.com.lett.crawlernode.exceptions.ApiResponseException;
+import br.com.lett.crawlernode.util.CommonMethods;
+import br.com.lett.crawlernode.util.JSONUtils;
+import br.com.lett.crawlernode.util.Logging;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
 import br.com.lett.crawlernode.util.CrawlerUtils;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 public class BrasilRihappyCrawler extends CrawlerRankingKeywords {
 
-  public BrasilRihappyCrawler(Session session) {
-    super(session);
-  }
+   public BrasilRihappyCrawler(Session session) {
+      super(session);
+   }
 
-  @Override
-  protected void extractProductsFromCurrentPage() {
-    this.pageSize = 36;
-    this.log("Página " + this.currentPage);
+   private static final Integer API_VERSION = 1;
+   private static final String SENDER = "vtex.store-resources@0.x";
+   private static final String PROVIDER = "vtex.search-graphql@0.x";
+   private static final String KEY_SHA256 = "e1d5228ca8b18d1f83b777d849747a7f55c9597f670f20c8557d2b0428bf6be7";
 
-    String url = "http://www.rihappy.com.br/" + this.keywordWithoutAccents.replaceAll(" ", "%20") + "?PageNumber=" + this.currentPage + "&PS=50";
-    this.log("Link onde são feitos os crawlers: " + url);
+   protected String createVariablesBase64() {
+      JSONObject search = new JSONObject();
+      search.put("hideUnavailableItems", true);
+      search.put("skusFilter", "ALL_AVAILABLE");
+      search.put("simulationBehavior", "default");
+      search.put("installmentCriteria", "MAX_WITHOUT_INTEREST");
+      search.put("productOriginVtex", false);
+      search.put("map", "ft");
+      search.put("query", this.keywordWithoutAccents.replace(" ", "%20"));
+      search.put("orderBy", "OrderByScoreDESC");
+      search.put("from", arrayProducts.size());
+      search.put("to", arrayProducts.size() + 20);
+      search.put("facetsBehavior", "dynamic");
+      search.put("categoryTreeBehavior", "default");
+      search.put("withFacets", false);
+      search.put("fullText", this.keywordWithoutAccents);
 
-    this.currentDoc = fetchDocument(url);
-    Elements products = this.currentDoc.select("div.vitrine ul > li[layout] > a");
+      return Base64.getEncoder().encodeToString(search.toString().getBytes());
+   }
 
-    if (!products.isEmpty()) {
-      if (this.totalProducts == 0) {
-        setTotalProducts();
+   protected JSONObject fetchSearchApi() {
+      JSONObject searchApi = new JSONObject();
+      StringBuilder url = new StringBuilder();
+      url.append("https://www.rihappy.com.br/_v/segment/graphql/v1?");
+
+      JSONObject extensions = new JSONObject();
+      JSONObject persistedQuery = new JSONObject();
+
+      persistedQuery.put("version", API_VERSION);
+      persistedQuery.put("sha256Hash", KEY_SHA256);
+      persistedQuery.put("sender", SENDER);
+      persistedQuery.put("provider", PROVIDER);
+
+      extensions.put("variables", createVariablesBase64());
+      extensions.put("persistedQuery", persistedQuery);
+
+      StringBuilder payload = new StringBuilder();
+      payload.append("workspace=master");
+      payload.append("&maxAge=short");
+      payload.append("&appsEtag=remove");
+      payload.append("&domain=store");
+      payload.append("&locale=pt-BR");
+      payload.append("&operationName=productSearchV3");
+      payload.append("&variables=" + URLEncoder.encode("{}", StandardCharsets.UTF_8));
+      payload.append("&extensions=" + URLEncoder.encode(extensions.toString(), StandardCharsets.UTF_8));
+      url.append(payload);
+
+      log("Link onde são feitos os crawlers:" + url);
+
+      Request request = Request.RequestBuilder.create()
+         .setUrl(url.toString())
+         .setCookies(cookies)
+         .setPayload(payload.toString())
+         .build();
+
+      JSONObject response = CrawlerUtils.stringToJson(this.dataFetcher.get(session, request).getBody());
+      try {
+         if (!response.has("errors")) {
+            searchApi = JSONUtils.getValueRecursive(response, "data.productSearch", JSONObject.class, new JSONObject());
+         } else {
+            throw new ApiResponseException(response.toString());
+         }
+      } catch (Exception ex) {
+         Logging.printLogError(logger, session, CommonMethods.getStackTrace(ex));
       }
 
-      for (Element e : products) {
-        String internalPid = e.attr("rel");
-        String productUrl = CrawlerUtils.completeUrl(e.attr("href"), "https", "www.rihappy.com.br");
+      return searchApi;
+   }
 
-        saveDataProduct(null, internalPid, productUrl);
 
-        this.log("Position: " + this.position + " - InternalId: " + null + " - InternalPid: " + internalPid + " - Url: " + productUrl);
-        if (this.arrayProducts.size() == productsLimit) {
-          break;
-        }
+   protected void extractProductsFromCurrentPage() {
+      this.log("Página " + this.currentPage);
+      this.pageSize = 20;
+
+      JSONObject searchApi = fetchSearchApi();
+      JSONArray products = JSONUtils.getJSONArrayValue(searchApi, "products");
+
+      if (products.length() > 0) {
+         if (this.totalProducts == 0) {
+            setTotalProducts(searchApi);
+         }
+
+         for (Object object : products) {
+            JSONObject product = (JSONObject) object;
+            String productUrl = "https://www.rihappy.com.br" + product.optString("link");
+            String internalPid = product.optString("productId");
+            String internalId = JSONUtils.getValueRecursive(product, "items.0.itemId", String.class);
+
+            saveDataProduct(internalId, internalPid, productUrl);
+
+            this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + internalPid + " - Url: " + productUrl);
+
+            if (this.arrayProducts.size() == productsLimit) {
+               break;
+            }
+         }
+
+      } else {
+         this.result = false;
+         this.log("Keyword sem resultado!");
       }
-    } else {
-      this.result = false;
-      this.log("Keyword sem resultado!");
-    }
 
-    this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
-  }
+      this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
+   }
 
-  @Override
-  protected void setTotalProducts() {
-    this.totalProducts = CrawlerUtils.scrapIntegerFromHtml(currentDoc, "span.resultado-busca-numero > span.value", null, true, 0);
-    this.log("Total da busca: " + this.totalProducts);
-  }
+   protected void setTotalProducts(JSONObject data) {
+      this.totalProducts = CrawlerUtils.getIntegerValueFromJSON(data, "recordsFiltered", 0);
+      this.log("Total da busca: " + this.totalProducts);
+   }
+
+   @Override
+   protected boolean hasNextPage(){
+      return true;
+   }
 }
