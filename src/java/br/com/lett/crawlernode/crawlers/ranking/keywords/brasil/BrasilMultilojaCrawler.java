5ada5b50228d11cab7771460adf8b3c27e9367a8
Thainá <thainaj.aguiar@gmail.com>
2021-09-08 11:50:41 -0300

    refactoring multiloja

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilMultilojaCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilMultilojaCrawler.java
@@ -1,98 +1,92 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.List;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import br.com.lett.crawlernode.core.fetcher.models.Request;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
-import br.com.lett.crawlernode.util.CommonMethods;
 import br.com.lett.crawlernode.util.CrawlerUtils;
-import br.com.lett.crawlernode.util.Logging;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * 
  * @author gabriel
- *
  */
 public class BrasilMultilojaCrawler extends CrawlerRankingKeywords {
 
-  public BrasilMultilojaCrawler(Session session) {
-    super(session);
-  }
+   public BrasilMultilojaCrawler(Session session) {
+      super(session);
+   }
 
-  @Override
-  protected void extractProductsFromCurrentPage() {
-    if (this.currentPage == 1) {
-      String url = "https://www.multiloja.com.br/" + this.keywordWithoutAccents.replace(" ", "-") + ".mht";
-      scrapProducts(url);
-    } else {
-      List<String> categoriesUrls = scrapCategoriesUrls();
-      for (String url : categoriesUrls) {
-        scrapProducts(url);
+   protected JSONObject fetch(String url) {
+      Map<String, String> headers = new HashMap<>();
+      headers.put("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkV2ZXJ0b24iLCJhZG1pbiI6dHJ1ZSwianRpIjoiN2M0Y2FiOTc0MDcyMTRkODAyNWQ5MzFkNTY4MThlYmI1MDQ5OWFmOWVmMGMyNjM3NGI3ZTRlNWU3ZmRjNDVkYiIsImlhdCI6MTYxNjQ0NTI3OSwiZXhwIjoxNzczNjkzMjc5LCJlbWFpbCI6ImV2ZXJ0b25AcHJlY29kZS5jb20uYnIiLCJlbXByZXNhIjoiUHJlY29kZSIsInNjaGVtYSI6Ik11bHRpbG9qYSIsImlkU2NoZW1hIjo3LCJpZFNlbGxlciI6NCwiaWRVc2VyIjoxfQ==.cpC5OeaTtyTjHzNJ2r+JtKWjQ5Q8rgyZ/uPdNxZm0HY=");
 
-        if (this.arrayProducts.size() == productsLimit) {
-          break;
-        }
+      Request request = Request.RequestBuilder.create()
+         .setUrl(url)
+         .setHeaders(headers)
+         .build();
+      String content = this.dataFetcher
+         .get(session, request)
+         .getBody();
 
-        this.currentPage++;
+      return CrawlerUtils.stringToJson(content);
+
+   }
+
+   @Override
+   protected void extractProductsFromCurrentPage() {
+      this.pageSize = 15;
+      this.log("Página " + this.currentPage);
+
+      String url = "https://www.allfront.com.br/api/busca/" + this.keywordEncoded + "?offset=" + (this.currentPage - 1) * 15;
+
+      this.log("Link onde são feitos os crawlers: " + url);
+      JSONObject searchedJson = fetch(url);
+      JSONArray productsArray = searchedJson.optJSONArray("products");
+      if (productsArray != null && !productsArray.isEmpty()) {
+         if (this.totalProducts == 0) {
+            setTotalProducts(searchedJson);
+         }
+         for (Object obj : productsArray) {
+            if (obj instanceof JSONObject) {
+
+               JSONObject product = (JSONObject) obj;
+
+               String productUrl = getUrl(product);
+               String productPid = product.optString("id");
+
+               saveDataProduct(null, productPid, productUrl);
+
+               this.log("Position: " + this.position + " - InternalId: " + null + " - InternalPid: " + productPid + " - Url: " + productUrl);
+               if (this.arrayProducts.size() == productsLimit)
+                  break;
+
+            }
+         }
+      } else {
+         this.result = false;
+         this.log("Keyword sem resultado!");
       }
-    }
-  }
 
-  private void scrapProducts(String url) {
-    this.log("Página " + this.currentPage);
-    this.currentDoc = fetchDocument(url);
-    Elements products = this.currentDoc.select(".listaprod li[itemprop=\"offers\"]");
+      this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
+   }
 
-    if (!products.isEmpty()) {
-
-      for (Element e : products) {
-        String internalPid = CrawlerUtils.scrapStringSimpleInfoByAttribute(e, "meta[itemprop=productID]", "content");
-        String productUrl = CrawlerUtils.scrapUrl(e, "a[itemprop=url]", "href", "https", "www.multiloja.com.br");
-
-        saveDataProduct(null, internalPid, productUrl);
-
-        this.log("Position: " + this.position + " - InternalId: " + null + " - InternalPid: " + internalPid + " - Url: " + productUrl);
-        if (this.arrayProducts.size() == productsLimit)
-          break;
-
+   private String getUrl(JSONObject product) {
+      String url = null;
+      String link = product.optString("link");
+      if (link != null && !link.isEmpty()) {
+         url = "https://www.estudionetshop.com.br/" + link;
       }
-    } else {
-      this.result = false;
-      this.log("Keyword sem resultado!");
-    }
 
-    this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size()
-        + " produtos crawleados");
-  }
+      return url;
+   }
 
-  @Override
-  protected boolean hasNextPage() {
-    return !this.currentDoc.select(".conteudo .BarraPaginas a:last-child input").isEmpty() ||
-        !this.currentDoc.select("#conteudoProdutosSB > div > .mProdutosEspacoAntPonto a").isEmpty();
-  }
+   protected void setTotalProducts(JSONObject searchedJson) {
+      this.totalProducts = searchedJson.optInt("quantidade");
+      this.log("Total da busca: " + this.totalProducts);
 
-  private List<String> scrapCategoriesUrls() {
-    List<String> urls = new ArrayList<>();
+   }
 
-    Elements categories = this.currentDoc.select("#conteudoProdutosSB > div > .mProdutosEspacoAntPonto");
-    for (Element e : categories) {
-      if (!e.select("a").isEmpty()) {
-        String in = CrawlerUtils.scrapStringSimpleInfoByAttribute(e, "input[name=in]", "value");
-
-        if (in != null) {
-          try {
-            urls.add("https://www.multiloja.com.br/comprar/encontrar.php?des=s&in=" + URLEncoder.encode(in, "UTF-8"));
-          } catch (UnsupportedEncodingException ex) {
-            Logging.printLogError(logger, session, CommonMethods.getStackTrace(ex));
-          }
-        }
-      }
-    }
-
-    return urls;
-  }
 }
