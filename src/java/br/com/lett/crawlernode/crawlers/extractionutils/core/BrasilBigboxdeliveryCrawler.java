d6de298318a49fad36f9445764e50ee1d2f99131
marcos <marcosmourabro@gmail.com>
2021-09-24 14:38:41 -0300

    new crawler

---
+++ src/java/br/com/lett/crawlernode/crawlers/extractionutils/core/BrasilBigboxdeliveryCrawler.java
@@ -0,0 +1,174 @@
+package br.com.lett.crawlernode.crawlers.extractionutils.core;
+
+import br.com.lett.crawlernode.core.fetcher.models.Request;
+import br.com.lett.crawlernode.core.models.Card;
+import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.models.ProductBuilder;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.Crawler;
+import br.com.lett.crawlernode.util.CommonMethods;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.Logging;
+import com.google.common.collect.Sets;
+import exceptions.MalformedPricingException;
+import exceptions.OfferException;
+import models.Offer;
+import models.Offers;
+import models.pricing.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.jsoup.nodes.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class BrasilBigboxdeliveryCrawler extends Crawler {
+
+   private static final String MAIN_SELLER_NAME = "Big-Box-Delivery";
+   protected Set<String> cards = Sets.newHashSet(Card.VISA.toString(), Card.MASTERCARD.toString(),
+      Card.AURA.toString(), Card.DINERS.toString(), Card.HIPER.toString(), Card.AMEX.toString());
+   private String STORE_ID = session.getOptions().optString("store_id");
+
+
+   public BrasilBigboxdeliveryCrawler(Session session) {
+      super(session);
+   }
+
+   @Override
+   public List<Product> extractInformation(Document doc) throws Exception {
+      super.extractInformation(doc);
+      List<Product> products = new ArrayList<>();
+
+      JSONObject productInfo = scrapProductInfoFromAPI();
+
+      if (!productInfo.isEmpty()) {
+         Logging.printLogDebug(logger, session, "Product page identified: " + session.getOriginalURL());
+
+         String internalId = productInfo.optString("id");
+         String internalPid = internalId;
+         String name = productInfo.optString("name");
+         String primaryImage = "https://assets.instabuy.com.br/ib.item.image.big/b-" + scrapPrimaryImage(productInfo.optJSONArray("images"));
+         List<String> secondaryImages = scrapSecondaryImages(productInfo.optJSONArray("images"));
+         boolean availableStock = productInfo.optBoolean("available_stock");
+         Offers offers = availableStock? scrapOffers(productInfo.optJSONArray("prices")): new Offers();
+
+         Product product = ProductBuilder.create()
+            .setUrl(session.getOriginalURL())
+            .setInternalId(internalId)
+            .setInternalPid(internalPid)
+            .setName(name)
+            .setPrimaryImage(primaryImage)
+            .setSecondaryImages(secondaryImages)
+            .setOffers(offers)
+            .build();
+
+         products.add(product);
+
+      } else {
+         Logging.printLogDebug(logger, session, "Not a product page: " + session.getOriginalURL());
+      }
+      return products;
+   }
+
+   private JSONObject scrapProductInfoFromAPI(){
+      JSONObject prodcutInfo = new JSONObject();
+
+      String slug = CommonMethods.getLast(session.getOriginalURL().split("p/"));
+      String url = "https://www.bigboxdelivery.com.br/apiv3/item?slug=" + slug + "&store_id=" + STORE_ID;
+
+      Request request = Request.RequestBuilder.create().setUrl(url).build();
+      String response = this.dataFetcher.get(session,request).getBody();
+      JSONObject json = CrawlerUtils.stringToJson(response);
+      System.err.println(json);
+      JSONArray dataArr = json.optJSONArray("data");
+
+      for(Object o: dataArr){
+         if (o instanceof JSONObject){
+            prodcutInfo = (JSONObject) o;
+         }
+      }
+
+      return prodcutInfo;
+   }
+
+   private String scrapPrimaryImage (JSONArray images){
+      String primaryImage = "";
+
+      if ( images != null){
+         primaryImage = (String) images.get(0);
+      }
+
+      return primaryImage;
+   }
+
+   private List<String> scrapSecondaryImages(JSONArray images){
+      List<String> secondaryImages = new ArrayList<>();
+
+      if ( images != null){
+         for(int i = 0 ; i > images.length(); i++) {
+           images.remove(0);
+         }
+      }
+      return secondaryImages;
+   }
+
+   private Offers scrapOffers(JSONArray prices) throws OfferException, MalformedPricingException {
+      Offers offers = new Offers();
+
+      for(Object o: prices) {
+         JSONObject price = (JSONObject) o;
+         Pricing pricing = scrapPricing(price);
+
+         if (pricing != null) {
+            offers.add(Offer.OfferBuilder.create()
+               .setUseSlugNameAsInternalSellerId(true)
+               .setSellerFullName(MAIN_SELLER_NAME)
+               .setSellersPagePosition(1)
+               .setIsBuybox(false)
+               .setIsMainRetailer(true)
+               .setPricing(pricing)
+               .build());
+         }
+      }
+
+      return offers;
+   }
+
+   private Pricing scrapPricing(JSONObject price) throws MalformedPricingException {
+      Double priceFrom =  price.has("promo_price")?price.optDouble("promo_price"): null;
+      Double spotlightPrice = price.optDouble("price");
+
+      CreditCards creditCards = scrapCreditCards(spotlightPrice);
+
+      return Pricing.PricingBuilder.create()
+         .setSpotlightPrice(spotlightPrice)
+         .setPriceFrom(priceFrom)
+         .setCreditCards(creditCards)
+         .setBankSlip(BankSlip.BankSlipBuilder.create()
+            .setFinalPrice(spotlightPrice)
+            .build())
+         .build();
+   }
+
+   private CreditCards scrapCreditCards(Double spotlightPrice) throws MalformedPricingException {
+      CreditCards creditCards = new CreditCards();
+      Installments installments = new Installments();
+
+      installments.add(Installment.InstallmentBuilder.create()
+         .setInstallmentNumber(1)
+         .setInstallmentPrice(spotlightPrice)
+         .build());
+
+      for (String brand : cards) {
+         creditCards.add(CreditCard.CreditCardBuilder.create()
+            .setBrand(brand)
+            .setIsShopCard(false)
+            .setInstallments(installments)
+            .build());
+      }
+
+      return creditCards;
+   }
+
+}
---
+++ src/java/br/com/lett/crawlernode/crawlers/extractionutils/ranking/BrasilBigboxdeliveryCrawler.java
@@ -0,0 +1,107 @@
+package br.com.lett.crawlernode.crawlers.extractionutils.ranking;
+
+import br.com.lett.crawlernode.core.fetcher.models.Request;
+import br.com.lett.crawlernode.core.models.RankingProductBuilder;
+import br.com.lett.crawlernode.core.models.RankingProducts;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.exceptions.MalformedProductException;
+import br.com.lett.crawlernode.util.CommonMethods;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.MathUtils;
+import org.eclipse.jetty.util.ajax.JSON;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
+public class BrasilBigboxdeliveryCrawler extends CrawlerRankingKeywords {
+
+   private String STORE_ID = session.getOptions().optString("store_id");
+
+
+   public BrasilBigboxdeliveryCrawler(Session session) {
+      super(session);
+   }
+
+   @Override
+   protected void extractProductsFromCurrentPage() throws UnsupportedEncodingException, MalformedProductException {
+      this.log("Página " + this.currentPage);
+
+      this.pageSize = 16;
+
+      JSONArray products = scrapProductInfoFromAPI();
+
+
+      if (!products.isEmpty()) {
+
+         for (Object e : products) {
+
+            JSONObject product = (JSONObject) e;
+
+            String internalPid = product.optString("id");
+            String internalId = internalPid;
+            String productUrl = "https://www.bigboxdelivery.com.br/p/" +  product.optString("slug");
+            String name = product.optString("name");
+            String imageUrl = "https://assets.instabuy.com.br/ib.item.image.big/b-" + scrapImage(product.optJSONArray("images"));
+            int price = (int) product.optDouble("min_price_valid");
+            boolean isAvailable = product.optBoolean("available_stock");
+
+            //New way to send products to save data product
+            RankingProducts productRanking = RankingProductBuilder.create()
+               .setUrl(productUrl)
+               .setInternalId(internalId)
+               .setInternalPid(internalPid)
+               .setName(name)
+               .setPriceInCents(price)
+               .setAvailability(isAvailable)
+               .setImageUrl(imageUrl)
+               .build();
+
+            saveDataProduct(productRanking);
+
+            this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + internalPid + " - Url: " + productUrl);
+            if (this.arrayProducts.size() == productsLimit)
+               break;
+         }
+      } else {
+         this.result = false;
+         this.log("Keyword sem resultado!");
+      }
+
+      this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
+   }
+
+
+
+   private JSONArray scrapProductInfoFromAPI(){
+      JSONArray products = new JSONArray();
+
+
+      String url = "https://www.bigboxdelivery.com.br/apiv3/search?page="+this.currentPage+"&N=18&search="+this.keywordEncoded+"&store_id=5e77b54b231a5bdbf395b06a";
+
+      Request request = Request.RequestBuilder.create().setUrl(url).build();
+      String response = this.dataFetcher.get(session,request).getBody();
+      JSONObject json = CrawlerUtils.stringToJson(response);
+      this.totalProducts = json.optInt("count");
+      products = json.optJSONArray("data");
+
+     return products;
+   }
+
+   private String scrapImage (JSONArray images){
+      String primaryImage = "";
+
+      for (Object o : images) {
+         if (!images.isEmpty()) {
+            primaryImage = (String) o;
+         }
+      }
+      return primaryImage;
+   }
+
+   @Override
+   protected void setTotalProducts() {
+      this.log("Total da busca: " + this.totalProducts);
+   }
+}
