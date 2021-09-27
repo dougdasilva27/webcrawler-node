0529a5a49301d986256eaa3818647cc50620196a
marcos <marcosmourabro@gmail.com>
2021-09-27 16:49:23 -0300

    change something

--- deploy/deploy.dev.sh
+++ deploy/deploy.dev.sh
@@ -8,8 +8,8 @@ export ECR_IMAGE_URL=868884350453.dkr.ecr.us-east-1.amazonaws.com/team-data-capt
 
 npx json -I -f deploy/Dockerrun.aws.dev.json -e "this.containerDefinitions[0].image"="'$ECR_IMAGE_URL'"
 
-docker-compose -f ./deploy/build.yml run --rm maven
-docker-compose -f ./deploy/build.yml build webcrawler
+docker-compose -f deploy/build.yml run --rm maven
+docker-compose -f deploy/build.yml build webcrawler
 
 docker push $ECR_IMAGE_URL
 docker rmi $(docker image ls -q $ECR_IMAGE_URL)
