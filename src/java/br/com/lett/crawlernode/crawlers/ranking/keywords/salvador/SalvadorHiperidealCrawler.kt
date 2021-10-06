8a1ccd8e3bbe9c33adf2e575d31baf86287ac8e3
gabriel <tassargabriel@gmail.com>
2021-10-06 17:43:34 -0300

    putting store location on url

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/salvador/SalvadorHiperidealCrawler.kt
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/salvador/SalvadorHiperidealCrawler.kt
@@ -21,9 +21,11 @@ class SalvadorHiperidealCrawler(session: Session) : CrawlerRankingKeywords(sessi
       currentDoc = fetchDocument("https://www.hiperideal.com.br/buscapagina?ft=$keywordEncoded&PS=16&sl=$token&cc=4&sm=0&PageNumber=$currentPage")
       for (element in currentDoc.select(".product--large")) {
          val sku = element.selectFirst(".skuid")?.text()
-         val url = element.selectFirst(".product__title a")?.attr("href")
+         val url = CrawlerUtils.completeUrl(element.selectFirst(".product__title a")?.attr("href"), "https", "www.hiperideal.com.br") + "?sc=9";
+
          saveDataProduct(sku, null, url)
          log("Internalid $sku - Url $url")
       }
    }
+
 }
