28e95db74600f24f3620c1b90d2729f6023652f0
Carol Evangelista <cdsevangelista@inf.ufpel.edu.br>
2021-10-11 15:07:33 -0300

    Product is already being created, it is necessary to make pageId dynamic

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/mexico/MexicoCoppelCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/mexico/MexicoCoppelCrawler.java
@@ -4,6 +4,7 @@ import br.com.lett.crawlernode.core.fetcher.FetchMode;
 import br.com.lett.crawlernode.core.fetcher.ProxyCollection;
 import br.com.lett.crawlernode.core.fetcher.models.Request;
 import br.com.lett.crawlernode.core.fetcher.models.Response;
+import br.com.lett.crawlernode.core.models.Card;
 import br.com.lett.crawlernode.core.models.CategoryCollection;
 import br.com.lett.crawlernode.core.models.Product;
 import br.com.lett.crawlernode.core.models.ProductBuilder;
@@ -11,10 +12,13 @@ import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.Crawler;
 import br.com.lett.crawlernode.util.CrawlerUtils;
 import br.com.lett.crawlernode.util.Logging;
+import exceptions.MalformedPricingException;
+import exceptions.OfferException;
+import models.Offer;
 import models.Offers;
+import models.pricing.*;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.util.*;
@@ -27,6 +31,8 @@ public class MexicoCoppelCrawler extends Crawler {
       super(session);
       super.config.setFetcher(FetchMode.APACHE);
    }
+
+   private static final List<String> cards = Arrays.asList("Coppel"); //TODO verificar se precisa adicionar na lista de cartões
 
    @Override
    protected Document fetch() {
@@ -62,29 +68,25 @@ public class MexicoCoppelCrawler extends Crawler {
          String description = CrawlerUtils.scrapSimpleDescription(doc, Arrays.asList("#product_longdescription_1599029")); //TODO: remover html tags e tornar pageId dinamico
          String primaryImage = CrawlerUtils.scrapSimplePrimaryImage(doc, "#productMainImage ", Arrays.asList("src"), "https", "");
          List<String> secondaryImages = CrawlerUtils.scrapSecondaryImages(doc,"[class=mz-lens] img", Arrays.asList("src"),"https", "padovani.vteximg.com.br", primaryImage);
-         CategoryCollection categories = getCategories(doc, "[name=keywords]");
-         System.out.println("categories 0 " + categories.getCategory(0));
-         System.out.println("categories 1 " + categories.getCategory(1));
+         CategoryCollection categories = getCategories(doc, "[name=keywords]", "content");
          boolean available = doc.selectFirst("[class=available]") != null;
+         Offers offers = available ? scrapOffers(doc) : new Offers();
 
-//         Offers offers = available ? new Offers() : new Offers();
-
-//         // Creating the product
-//         Product product = ProductBuilder.create()
-//            .setUrl(session.getOriginalURL())
-//            .setInternalId(internalId)
-//            .setInternalPid(internalId)
-//            .setName(name)
-//            .setOffers(offers)
-//            .setCategory1(categories.getCategory(0))
-//            .setCategory2(categories.getCategory(1))
-//            .setCategory3(categories.getCategory(2))
-//            .setPrimaryImage(primaryImage)
-//            .setSecondaryImages(secondaryImages)
-//            .setDescription(description)
-//            .build();
-//
-//         products.add(product);
+         // Creating the product
+         Product product = ProductBuilder.create()
+            .setUrl(session.getOriginalURL())
+            .setInternalId(internalId)
+            .setInternalPid(internalId)
+            .setName(name)
+            .setOffers(offers)
+            .setCategory1(categories.getCategory(0))
+            .setCategory2(categories.getCategory(1))
+            .setCategory3(categories.getCategory(2))
+            .setPrimaryImage(primaryImage)
+            .setSecondaryImages(secondaryImages)
+            .setDescription(description)
+            .build();
+         products.add(product);
       } else {
          Logging.printLogDebug(logger, session, "Not a product page:   " + this.session.getOriginalURL());
       }
@@ -96,19 +98,69 @@ public class MexicoCoppelCrawler extends Crawler {
       return sku;
    }
 
-   public static CategoryCollection getCategories(Document doc, String selector) {
+   public static CategoryCollection getCategories(Document doc, String selector, String attr) {
       CategoryCollection categories = new CategoryCollection();
-      selector = selector.attr("content");
+
       Elements elementCategories = doc.select(selector);
-      String categoriesString = CrawlerUtils.scrapStringSimpleInfoByAttribute(doc, "[name=keywords] ", "content");
-      System.out.println("categoriesString " + categoriesString);
 
-      for (String e : categoriesString) {
-         System.out.println("e " + e);
+      String selectorAttribute = elementCategories.attr(attr);
 
-         categories.add(e.replace(">", "").trim());
+      ArrayList<String> categoryList = new ArrayList<>(Arrays.asList(selectorAttribute.split(",")));
+
+      for (String category: categoryList) {
+         categories.add(category.trim());
+      }
+      return categories;
+   }
+
+   private Offers scrapOffers(Document doc) throws MalformedPricingException, OfferException {
+      Offers offers = new Offers();
+      Pricing pricing = scrapPricing(doc);
+      List<String> sales = Collections.singletonList(CrawlerUtils.calculateSales(pricing));
+
+      offers.add(new Offer.OfferBuilder()
+         .setUseSlugNameAsInternalSellerId(true)
+         .setSellerFullName("coppel")//TODO verificar se full name é slug
+         .setMainPagePosition(1)
+         .setIsBuybox(false)
+         .setIsMainRetailer(true)
+         .setPricing(pricing)
+         .setSales(sales)
+         .build());
+
+      return offers;
+   }
+
+   private Pricing scrapPricing(Document doc) throws MalformedPricingException {
+      Double spotlightPrice = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".unique_price", null, true, ',', session);//TODO formato do preço
+      Double priceFrom = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".old_price", null, true, ',', session);
+
+      CreditCards creditCards = scrapCreditCards(spotlightPrice);
+
+      return Pricing.PricingBuilder.create()
+         .setSpotlightPrice(spotlightPrice)
+         .setPriceFrom(priceFrom)
+         .setCreditCards(creditCards)
+         .build();
+   }
+
+   private CreditCards scrapCreditCards(Double price) throws MalformedPricingException {
+      CreditCards creditCards = new CreditCards();
+      Installments installments = new Installments();
+
+      installments.add(Installment.InstallmentBuilder.create()//TODO verificar parcelas
+         .setInstallmentNumber(1)
+         .setInstallmentPrice(price)
+         .build());
+
+      for (String card : cards) {
+         creditCards.add(CreditCard.CreditCardBuilder.create()
+            .setBrand(card)
+            .setInstallments(installments)
+            .setIsShopCard(false)
+            .build());
       }
 
-      return categories;
+      return creditCards;
    }
 }
