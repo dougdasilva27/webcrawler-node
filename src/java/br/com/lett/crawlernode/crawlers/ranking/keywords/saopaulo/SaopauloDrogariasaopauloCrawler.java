2e466b7593fca1898f300df70e70c68e21cf5036
Carol Evangelista <cdsevangelista@inf.ufpel.edu.br>
2021-10-14 08:36:49 -0300

    New sos ranking for Drogaria São paulo

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/saopaulo/SaopauloDrogariasaopauloCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/saopaulo/SaopauloDrogariasaopauloCrawler.java
@@ -1,6 +1,9 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.saopaulo;
 
 import br.com.lett.crawlernode.core.fetcher.models.Request;
+import br.com.lett.crawlernode.core.models.RankingProduct;
+import br.com.lett.crawlernode.core.models.RankingProductBuilder;
+import br.com.lett.crawlernode.exceptions.MalformedProductException;
 import br.com.lett.crawlernode.util.CommonMethods;
 import br.com.lett.crawlernode.util.CrawlerUtils;
 import br.com.lett.crawlernode.util.JSONUtils;
@@ -18,14 +21,7 @@ public class SaopauloDrogariasaopauloCrawler extends CrawlerRankingKeywords {
       super(session);
    }
 
-   private JSONArray productsJSONArray;
-
-   @Override
-   protected void extractProductsFromCurrentPage() {
-      this.pageSize = 48;
-      this.log("Página " + this.currentPage);
-
-      this.productsJSONArray = new JSONArray();
+   private JSONObject extractJsonFromApi() {
       String urlApi = "https://api.linximpulse.com/engage/search/v3/search?apiKey=drogariasaopaulo&productFormat=complete&resultsPerPage=48&page="+this.currentPage+"&terms="+keywordEncoded;
 
       Map<String,String> headers = new HashMap<>();
@@ -38,42 +34,70 @@ public class SaopauloDrogariasaopauloCrawler extends CrawlerRankingKeywords {
 
       String response = dataFetcher.get(session,request).getBody() ;
 
-      JSONObject jsononj = CrawlerUtils.stringToJson(response);
+      return CrawlerUtils.stringToJson(response);
 
+   }
 
-      productsJSONArray = JSONUtils.getJSONArrayValue(jsononj,"products");
+   @Override
+   protected void extractProductsFromCurrentPage() throws MalformedProductException {
+      this.pageSize = 48;
+      this.log("Página " + this.currentPage);
 
+      JSONObject json = extractJsonFromApi();
 
+      JSONArray products = json.optJSONArray("products");
 
-      if (productsJSONArray.length() > 0) {
-         for (Object o : productsJSONArray) {
-            JSONObject item = (JSONObject) o;
-            String internalPid = item.optString("id") ;
-            String url = JSONUtils.getValueRecursive(item,"skus.0.properties.url",String.class);
+      if (products != null && !products.isEmpty()) {
+         for (Object obj : products) {
+            if (obj instanceof JSONObject) {
+               JSONObject product = (JSONObject) obj;
 
-            String productUrl = CrawlerUtils.completeUrl(url,"https","www.drogariasaopaulo.com.br");
+               String internalPid = product.optString("id") ;
+               String internalId = internalPid;
+               String productName = product.optString("name");
+               String url = JSONUtils.getValueRecursive(product,"skus.0.properties.url", String.class);
+               String productUrl = CrawlerUtils.completeUrl(url,"https","www.drogariasaopaulo.com.br");
+               String productImg = JSONUtils.getValueRecursive(product,"images.1000x1000", String.class);
+               Integer productPrice = getPrice(product.optInt("price"));
+               boolean isAvailable = product.optString("status").equals("AVAILABLE");
 
+               RankingProduct productRanking = RankingProductBuilder.create()
+                  .setUrl(productUrl)
+                  .setInternalId(internalId)
+                  .setInternalPid(internalPid)
+                  .setName(productName)
+                  .setImageUrl(productImg)
+                  .setPriceInCents(productPrice)
+                  .setAvailability(isAvailable)
+                  .build();
 
-            saveDataProduct(null, internalPid, productUrl);
+               saveDataProduct(productRanking);
 
-            this.log("Position: " + this.position + " - InternalId: " + null + " - InternalPid: " + internalPid + " - Url: " + productUrl);
-            if (this.arrayProducts.size() == productsLimit) {
-               break;
+               this.log(
+                  "Position: " + this.position +
+                     " - InternalId: " + internalId +
+                     " - internalPid: " + internalPid +
+                     " - name: " + productName +
+                     " - Url: " + productUrl);
+
+               if (this.arrayProducts.size() == productsLimit) {
+                  break;
+               }
             }
-
          }
       } else {
          this.result = false;
          this.log("Keyword sem resultado!");
       }
-
       this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
+   }
+
+   private int getPrice(int price) {
+      return (price != 0) ? (price * 100) : 0;
    }
 
    @Override
    protected boolean hasNextPage() {
       return true;
    }
-
-
 }
