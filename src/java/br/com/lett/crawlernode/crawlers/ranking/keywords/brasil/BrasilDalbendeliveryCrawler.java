460e7eb8cad1a600d519ded6fc454560f8881479
Thainá <thainaj.aguiar@gmail.com>
2021-10-15 17:35:13 -0300

    fix url - dalben

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilDalbendeliveryCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilDalbendeliveryCrawler.java
@@ -1,21 +1,11 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import br.com.lett.crawlernode.crawlers.extractionutils.ranking.VipcommerceRanking;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import br.com.lett.crawlernode.core.fetcher.models.Request;
-import br.com.lett.crawlernode.core.fetcher.models.Request.RequestBuilder;
 import br.com.lett.crawlernode.core.session.Session;
-import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
-import br.com.lett.crawlernode.util.CrawlerUtils;
-import br.com.lett.crawlernode.util.JSONUtils;
+import br.com.lett.crawlernode.crawlers.extractionutils.ranking.VipcommerceRanking;
 
 public class BrasilDalbendeliveryCrawler extends VipcommerceRanking {
 
-   private static final String DOMAIN = "dalbendelivery.com.br";
+   private static final String DOMAIN = "superdalben.com.br";
 
    public BrasilDalbendeliveryCrawler(Session session) {
       super(session);
