94c08fa82c38019a499a75547e7a0d830265923a
CostaLucasIV <lucascmm3@gmail.com>
2021-10-25 18:56:28 -0300

    pigmento crawler refactor

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/argentina/ArgentinaPigmentoCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/argentina/ArgentinaPigmentoCrawler.java
@@ -1,64 +1,82 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.argentina;
 
+import br.com.lett.crawlernode.core.models.RankingProduct;
+import br.com.lett.crawlernode.core.models.RankingProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.exceptions.MalformedProductException;
 import br.com.lett.crawlernode.util.CrawlerUtils;
+import org.json.JSONObject;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class ArgentinaPigmentoCrawler extends CrawlerRankingKeywords {
 
-  public ArgentinaPigmentoCrawler(Session session) {
-    super(session);
-  }
+   public ArgentinaPigmentoCrawler(Session session) {
+      super(session);
+   }
 
-  @Override
-  protected void extractProductsFromCurrentPage() {
+   @Override
+   protected void extractProductsFromCurrentPage() throws MalformedProductException {
 
-    currentDoc =
-        fetchDocument(
-            "https://perfumeriaspigmento.com.ar/catalogsearch/result/?p="
-                + currentPage
-                + "&q="
-                + keywordEncoded);
+      String url = "https://www.perfumeriaspigmento.com.ar/" + this.keywordEncoded + "?_q=" + this.keywordEncoded + "&map=ft&page=" + this.currentPage;
+      currentDoc = fetchDocument(url);
 
-    if (this.totalProducts == 0) {
-      setTotalProducts();
-    }
+      if (this.totalProducts == 0) {
+         setTotalProducts();
+      }
 
-    currentDoc
-        .select(".item.product.product-item")
-        .forEach(
-            elem -> {
-              String link = elem.selectFirst(".product-img a").attr("href");
+      JSONObject jsonProducts = scrapJson();
 
-              String internalId =
-                  elem.selectFirst(".price-box.price-final_price").attr("data-product-id");
+      Elements products = currentDoc.select("div.vtex-search-result-3-x-galleryItem.vtex-search-result-3-x-galleryItem--normal.vtex-search-result-3-x-galleryItem--grid.pa4");
 
-              saveDataProduct(internalId, null, link);
-              this.log(
-                  "Position: "
-                      + this.position
-                      + " - InternalId: "
-                      + internalId
-                      + " - Url: "
-                      + link);
-            });
-  }
+      for (Element product : products) {
+         String slug = CrawlerUtils.scrapStringSimpleInfoByAttribute(product, "a.vtex-product-summary-2-x-clearLink.h-100.flex.flex-column", "href");
+         String link = "https://www.perfumeriaspigmento.com.ar" + slug;
+         String jsonKey = "Product:" + slug.substring(1, slug.indexOf("/p"));
+         String internalPid = jsonProducts.has(jsonKey) ? jsonProducts.optJSONObject(jsonKey).optString("productId") : null;
 
-  @Override
-  protected void setTotalProducts() {
+         String name = CrawlerUtils.scrapStringSimpleInfo(product, "span.vtex-product-summary-2-x-productBrand.vtex-product-summary-2-x-brandName.t-body", true);
+         String imageUrl = CrawlerUtils.scrapStringSimpleInfoByAttribute(product, "img.vtex-product-summary-2-x-imageNormal.vtex-product-summary-2-x-image", "src");
+         Integer price = CrawlerUtils.scrapPriceInCentsFromHtml(product, "span.vtex-product-price-1-x-currencyInteger.vtex-product-price-1-x-currencyInteger--summary", null, true, ',', session, 0);
+         boolean isAvailable = price != 0;
 
-     Element element = this.currentDoc.select("#toolbar-amount .toolbar-number:nth-child(3)").first();
+         //New way to send products to save data product
+         RankingProduct productRanking = RankingProductBuilder.create()
+            .setUrl(link)
+            .setInternalId(internalPid)
+            .setName(name)
+            .setImageUrl(imageUrl)
+            .setPriceInCents(price)
+            .setAvailability(isAvailable)
+            .build();
 
-     int totalProdutos = 0;
+         saveDataProduct(productRanking);
+      }
+   }
 
-     if (element != null) {
-        totalProdutos = CrawlerUtils.scrapIntegerFromHtml(this.currentDoc, "#toolbar-amount .toolbar-number:nth-child(3)", false, 0);
-     } else{
-        totalProdutos = CrawlerUtils.scrapIntegerFromHtml(this.currentDoc, "#toolbar-amount .toolbar-number", false, 0);
-     }
+   private JSONObject scrapJson() {
+      String result = "";
+      Elements els = currentDoc.select("script");
 
-     this.totalProducts = totalProdutos;
-    this.log("Total da busca: " + this.totalProducts);
-  }
+      for (Element el : els) {
+         String stringEl = el.toString();
+
+         if (stringEl.contains("__STATE__")) {
+            String partialJson = stringEl.substring(stringEl.indexOf("__STATE__"));
+            partialJson = partialJson
+               .replace("__STATE__ = ", "")
+               .replace("</script>", "");
+            result = partialJson.trim();
+            break;
+         }
+      }
+
+      return CrawlerUtils.stringToJson(result);
+   }
+
+   @Override
+   protected boolean hasNextPage() {
+      return true;
+   }
 }
