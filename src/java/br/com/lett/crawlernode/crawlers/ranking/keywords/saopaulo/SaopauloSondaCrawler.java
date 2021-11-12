256a40a449139b1c9a4afa19194eecad3a0d9168
Carol Evangelista <cdsevangelista@inf.ufpel.edu.br>
2021-11-11 22:16:34 -0300

    New sos sonda and fixing indentation

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/saopaulo/SaopauloSondaCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/saopaulo/SaopauloSondaCrawler.java
@@ -1,5 +1,8 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.saopaulo;
 
+import br.com.lett.crawlernode.core.models.RankingProduct;
+import br.com.lett.crawlernode.core.models.RankingProductBuilder;
+import br.com.lett.crawlernode.exceptions.MalformedProductException;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import br.com.lett.crawlernode.core.fetcher.FetchMode;
@@ -8,59 +11,72 @@ import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
 import br.com.lett.crawlernode.util.CommonMethods;
 import br.com.lett.crawlernode.util.CrawlerUtils;
 
+import java.util.Arrays;
+
 public class SaopauloSondaCrawler extends CrawlerRankingKeywords {
 
-  public SaopauloSondaCrawler(Session session) {
-    super(session);
-    super.fetchMode = FetchMode.APACHE;
-  }
+   public SaopauloSondaCrawler(Session session) {
+      super(session);
+      super.fetchMode = FetchMode.APACHE;
+   }
 
-  @Override
-  protected void extractProductsFromCurrentPage() {
-    this.pageSize = 96;
-    this.log("Página " + this.currentPage);
+   @Override
+   protected void extractProductsFromCurrentPage() throws MalformedProductException {
+      this.pageSize = 96;
+      this.log("Página " + this.currentPage);
 
-    String url =
-        "https://www.sondadelivery.com.br/delivery/busca/" + this.keywordWithoutAccents.replace(" ", "%20") + "/" + this.currentPage + "/96/0/";
-    this.log("Link onde são feitos os crawlers: " + url);
+      String url =
+         "https://www.sondadelivery.com.br/delivery/busca/" + this.keywordWithoutAccents.replace(" ", "%20") + "/" + this.currentPage + "/96/0/";
+      this.log("Link onde são feitos os crawlers: " + url);
 
-    this.currentDoc = fetchDocument(url);
+      this.currentDoc = fetchDocument(url);
 
-    Elements products = this.currentDoc.select(".product-list .product:first-child");
+      Elements products = this.currentDoc.select(".product-list .product:first-child");
 
-    if (!products.isEmpty()) {
-      if (this.totalProducts == 0) {
-        setTotalProducts();
+      if (!products.isEmpty()) {
+         if (this.totalProducts == 0) {
+            setTotalProducts();
+         }
+
+         for (Element e : products) {
+
+            String productUrl = CrawlerUtils.scrapUrl(e, ".product--info > a", "href", "https", "www.sondadelivery.com.br");
+            String internalId = productUrl != null ? crawlInternalId(productUrl) : null;
+            String name = CrawlerUtils.scrapStringSimpleInfo(e, ".product--title span", false);
+            String imgUrl = CrawlerUtils.scrapSimplePrimaryImage(e, ".product--thumb img", Arrays.asList("data-srcset"), "https", "");
+            Integer price = CrawlerUtils.scrapPriceInCentsFromHtml(e, ".int span", null, false, ',', session, 0);
+            boolean isAvailable = price != 0;
+
+            RankingProduct productRanking = RankingProductBuilder.create()
+               .setUrl(productUrl)
+               .setInternalId(internalId)
+               .setName(name)
+               .setImageUrl(imgUrl)
+               .setPriceInCents(price)
+               .setAvailability(isAvailable)
+               .build();
+
+            saveDataProduct(productRanking);
+
+            if (this.arrayProducts.size() == productsLimit) {
+               break;
+            }
+         }
+      } else {
+         this.result = false;
+         this.log("Keyword sem resultado!");
       }
 
-      for (Element e : products) {
+      this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
+   }
 
-        String productUrl = CrawlerUtils.scrapUrl(e, ".product--info > a", "href", "https", "www.sondadelivery.com.br");
-        String internalId = productUrl != null ? crawlInternalId(productUrl) : null;
+   @Override
+   protected void setTotalProducts() {
+      this.totalProducts = CrawlerUtils.scrapIntegerFromHtml(currentDoc, ".search-filter--results strong", null, true, 0);
+      this.log("Total da busca: " + this.totalProducts);
+   }
 
-        saveDataProduct(internalId, null, productUrl);
-
-        this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + null + " - Url: " + productUrl);
-
-        if (this.arrayProducts.size() == productsLimit) {
-          break;
-        }
-      }
-    } else {
-      this.result = false;
-      this.log("Keyword sem resultado!");
-    }
-
-    this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
-  }
-
-  @Override
-  protected void setTotalProducts() {
-    this.totalProducts = CrawlerUtils.scrapIntegerFromHtml(currentDoc, ".search-filter--results strong", null, true, 0);
-    this.log("Total da busca: " + this.totalProducts);
-  }
-
-  private String crawlInternalId(String url) {
-    return CommonMethods.getLast(url.split("\\?")[0].split("/"));
-  }
+   private String crawlInternalId(String url) {
+      return CommonMethods.getLast(url.split("\\?")[0].split("/"));
+   }
 }
