0089af5759a87a3bcf5a8d2426824605f01835e5
Thainá <thainaj.aguiar@gmail.com>
2021-11-26 16:53:01 -0300

    fix total product page

--- src/java/br/com/lett/crawlernode/crawlers/extractionutils/ranking/AmericanasmaisCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/extractionutils/ranking/AmericanasmaisCrawler.java
@@ -6,8 +6,12 @@ import br.com.lett.crawlernode.core.fetcher.methods.JsoupDataFetcher;
 import br.com.lett.crawlernode.core.fetcher.models.FetcherOptions;
 import br.com.lett.crawlernode.core.fetcher.models.Request;
 import br.com.lett.crawlernode.core.fetcher.models.Response;
+import br.com.lett.crawlernode.core.models.RankingProduct;
+import br.com.lett.crawlernode.core.models.RankingProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.exceptions.MalformedProductException;
+import br.com.lett.crawlernode.util.CommonMethods;
 import br.com.lett.crawlernode.util.CrawlerUtils;
 import br.com.lett.crawlernode.util.JSONUtils;
 import org.json.JSONException;
@@ -19,7 +23,9 @@ import org.jsoup.select.Elements;
 
 import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 public abstract class AmericanasmaisCrawler extends CrawlerRankingKeywords {
 
@@ -37,13 +43,29 @@ public abstract class AmericanasmaisCrawler extends CrawlerRankingKeywords {
 
    private Document fetchPage() {
 
-      String url = HOME_PAGE + storeId + "/pick-up?ordenacao=relevance&conteudo=" +
+      Map<String, String> headers = new HashMap<>();
+
+      headers.put("authority", "www.americanas.com.br");
+      headers.put("sec-ch-ua", " \" Not A;Brand\";v=\"99\", \"Chromium\";v=\"90\", \"Google Chrome\";v=\"90\"");
+      headers.put("sec-ch-ua-mobile", "?0");
+      headers.put("upgrade-insecure-requests", "1");
+      headers.put("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.9");
+      headers.put("sec-fetch-site", "none");
+      headers.put("sec-fetch-mode", "navigate");
+      headers.put("sec-fetch-user", "?1");
+      headers.put("sec-fetch-dest", "document");
+      headers.put("user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36");
+      headers.put("accept-language", "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7,es;q=0.6");
+
+
+      String url = HOME_PAGE + storeId + "?ordenacao=topSelling&conteudo=" +
          this.keywordEncoded.replace("+", "%20") +
-         "&limite=24&offset=" + (this.currentPage - 1) * pageSize + "&ordenacao=relevance";
+         "&limite=24&offset=" + (this.currentPage - 1) * pageSize;
 
       Request request = Request.RequestBuilder.create()
          .setUrl(url)
          .mustSendContentEncoding(false)
+         .setHeaders(headers)
          .setFetcheroptions(
             FetcherOptions.FetcherOptionsBuilder.create()
                .mustUseMovingAverage(false)
@@ -51,9 +73,10 @@ public abstract class AmericanasmaisCrawler extends CrawlerRankingKeywords {
                .build()
          ).setProxyservice(
             Arrays.asList(
-               ProxyCollection.INFATICA_RESIDENTIAL_BR_HAPROXY,
                ProxyCollection.NETNUT_RESIDENTIAL_ES_HAPROXY,
-               ProxyCollection.NETNUT_RESIDENTIAL_BR_HAPROXY
+               ProxyCollection.NETNUT_RESIDENTIAL_BR_HAPROXY,
+               ProxyCollection.NETNUT_RESIDENTIAL_AR_HAPROXY,
+               ProxyCollection.NETNUT_RESIDENTIAL_CO_HAPROXY
             )
          ).build();
 
@@ -67,7 +90,6 @@ public abstract class AmericanasmaisCrawler extends CrawlerRankingKeywords {
          Integer.toString(statusCode).charAt(0) != '3'
          && statusCode != 404)) {
          request.setProxyServices(Arrays.asList(
-            ProxyCollection.INFATICA_RESIDENTIAL_BR,
             ProxyCollection.BUY,
             ProxyCollection.NETNUT_RESIDENTIAL_BR));
 
@@ -79,7 +101,7 @@ public abstract class AmericanasmaisCrawler extends CrawlerRankingKeywords {
    }
 
    @Override
-   protected void extractProductsFromCurrentPage() throws UnsupportedEncodingException {
+   protected void extractProductsFromCurrentPage() throws UnsupportedEncodingException, MalformedProductException {
       this.pageSize = 24;
       this.log("Página " + this.currentPage);
 
@@ -95,13 +117,25 @@ public abstract class AmericanasmaisCrawler extends CrawlerRankingKeywords {
          }
          for (String internalId : products.toMap().keySet()) {
             String productUrl = HOME_PAGE + storeId + "/ship?ordenacao=relevance&conteudo=" + internalId;
+            JSONObject productInfo = products.optJSONObject(internalId);
+            if (productInfo != null) {
+               String name = productInfo.optString("name");
+               String imgUrl = JSONUtils.getValueRecursive(productInfo, "images.0.large", String.class);
+               Integer price = CommonMethods.doublePriceToIntegerPrice(JSONUtils.getValueRecursive(productInfo, "offers.result.0.salesPrice", Double.class), 0);
+               boolean isAvailable = price != 0;
 
-            saveDataProduct(internalId, null, productUrl);
+               RankingProduct productRanking = RankingProductBuilder.create()
+                  .setUrl(productUrl)
+                  .setInternalId(internalId)
+                  .setInternalPid(internalId)
+                  .setName(name)
+                  .setPriceInCents(price)
+                  .setImageUrl(imgUrl)
+                  .setAvailability(isAvailable)
+                  .build();
 
-            this.log(
-               "Position: " + this.position +
-                  " - InternalId: " + internalId +
-                  " - Url: " + productUrl);
+               saveDataProduct(productRanking);
+            }
          }
       }
 
@@ -129,10 +163,16 @@ public abstract class AmericanasmaisCrawler extends CrawlerRankingKeywords {
 
    private void setTotalProducts(JSONObject json) {
       JSONObject jsonObject = json.optJSONObject("pages");
-      String key = jsonObject.keys().next();
-      JSONObject jsonObjectWithKey = jsonObject.optJSONObject(key);
+      for (Iterator<String> it = jsonObject.keys(); it.hasNext(); ) {
+         String key = it.next();
+         if (!key.equals("type")) {
+            JSONObject jsonObjectWithKey = jsonObject.optJSONObject(key);
+            this.totalProducts = JSONUtils.getValueRecursive(jsonObjectWithKey, "queries.getStoreOffersAcom.result.search.total", Integer.class);
+            ;
+            this.log("Total da busca: " + this.totalProducts);
+            break;
+         }
+      }
 
-      this.totalProducts = JSONUtils.getValueRecursive(jsonObjectWithKey, "queries.getStoreOffersAcom.result.search.total", Integer.class);;
-      this.log("Total da busca: " + this.totalProducts);
    }
 }
