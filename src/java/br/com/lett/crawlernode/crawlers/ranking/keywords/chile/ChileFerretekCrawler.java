75946f58c43eb46bbb5f2f178affc8bb92a272ca
Thiago Frois <zthiagopf@gmail.com>
2021-12-06 18:49:48 -0300

    fix productUrl

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/chile/ChileFerretekCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/chile/ChileFerretekCrawler.java
@@ -21,43 +21,6 @@ import java.util.*;
 public class ChileFerretekCrawler extends CrawlerRankingKeywords {
    public ChileFerretekCrawler(Session session) {
       super(session);
-//      super.fetchMode = FetchMode.FETCHER;
-   }
-
-   @Override
-   protected Document fetchDocument(String url) {
-      this.currentDoc = new Document(url);
-
-      if (this.currentPage == 1) {
-         this.session.setOriginalURL(url);
-      }
-
-
-      Map<String,String> headers = new HashMap<>();
-//      headers.put("Cookies", "frontend_lang=es_CL; session_id=48493f26b2b0b2fc8207742e47ee45c61d158c94");
-//      headers.put("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36");
-//      headers.put("Accept","*/*");
-//      headers.put("Referer", "https://herramientas.cl/");
-//      headers.put("Accept-Encoding", "gzip, deflate, br");
-//      headers.put("Connection", "keep-alive");
-      headers.put("Host", "herramientas.cl");
-
-      Request request = Request.RequestBuilder.create()
-         .setUrl(url)
-         .setHeaders(headers)
-         .setTimeout(60000)
-         .setCookies(this.cookies)
-         .setProxyservice(List.of(ProxyCollection.BUY_HAPROXY, ProxyCollection.BONANZA, ProxyCollection.NETNUT_RESIDENTIAL_BR_HAPROXY))
-         .build();
-
-      Response response = dataFetcher.get(session, request);
-
-      return Jsoup.parse(response.getBody());
-   }
-
-   @Override
-   protected void processBeforeFetch() {
-      this.cookies = CrawlerUtils.fetchCookiesFromAPage( "https://herramientas.cl",null,"www.herramientas.cl", "/", null, session, dataFetcher);
    }
    @Override
    protected void extractProductsFromCurrentPage() throws UnsupportedEncodingException, MalformedProductException {
@@ -74,7 +37,7 @@ public class ChileFerretekCrawler extends CrawlerRankingKeywords {
       if(!products.isEmpty()) {
          for(Element product : products) {
             String internalId = CrawlerUtils.scrapStringSimpleInfoByAttribute(product, "button", "data-product-id");
-            String productUrl = CrawlerUtils.scrapStringSimpleInfoByAttribute(product, ".card-body > a", "href");
+            String productUrl = "https://herramientas.cl" + CrawlerUtils.scrapStringSimpleInfoByAttribute(product, ".card-body > a", "href");
             String name = CrawlerUtils.scrapStringSimpleInfo(product, ".card-body h6", true);
             String imgUrl = CrawlerUtils.scrapSimplePrimaryImage(product, ".card-body img", Collections.singletonList("src"), "https", "herramientas.cl");
             Integer price = CrawlerUtils.scrapIntegerFromHtml(product, ".oe_currency_value", true, 0);
