254a7ad36da123561bd1897ebc8231ba739c8c8c
vagner sampaio <vagnersampaio92@gmail.com>
2021-12-23 14:16:33 -0300

    Bug fix SOS CuritibaSchummancuritiba

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/curitiba/CuritibaSchummancuritibaCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/curitiba/CuritibaSchummancuritibaCrawler.java
@@ -1,6 +1,7 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.curitiba;
 
 import br.com.lett.crawlernode.core.fetcher.models.Request;
+import br.com.lett.crawlernode.core.fetcher.models.Response;
 import br.com.lett.crawlernode.core.models.RankingProduct;
 import br.com.lett.crawlernode.core.models.RankingProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
@@ -11,99 +12,86 @@ import br.com.lett.crawlernode.util.CrawlerUtils;
 import br.com.lett.crawlernode.util.JSONUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CuritibaSchummancuritibaCrawler extends CrawlerRankingKeywords {
-
+   private String HOME_PAGE = "https://www.schumann.com.br/";
+   private Integer pageSize = 12;
    public CuritibaSchummancuritibaCrawler(Session session) {
       super(session);
    }
 
-   protected JSONObject fetch(String url) {
-      Map<String, String> headers = new HashMap<>();
-      headers.put("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6bnVsbCwiYWRtaW4iOnRydWUsImp0aSI6IjU4N2NjMjJjNGYyNjhlMWU5Yjc0YmZlY2NmYTBjN2IyNGZjYTg5MzA3ZDQzYzdjZmVkYTQwYTU4NTZkMWZlNTgiLCJpYXQiOjE2MDE1ODE0NjIsImV4cCI6MTY1MTE3NTA2MiwiZW1haWwiOiJldmVydG9uQHByZWNvZGUuY29tLmJyIiwiZW1wcmVzYSI6bnVsbCwic2NoZW1hIjoiRXN0dWRpb25ldHNob3AiLCJpZFNjaGVtYSI6NTQsImlkU2VsbGVyIjoiNjIiLCJpZFVzZXIiOjF9.v+7JeiaPQVabn+/CeE0RUGQCYaAtCNqo+omeem8Vo2s=");
-
-      Request request = Request.RequestBuilder.create()
-         .setUrl(url)
-         .setHeaders(headers)
-         .build();
-      String content = this.dataFetcher
-         .get(session, request)
-         .getBody();
-
-      return CrawlerUtils.stringToJson(content);
-
-   }
-
    @Override
-   protected void extractProductsFromCurrentPage() throws MalformedProductException {
-      this.pageSize = 15;
-      this.log("Página " + this.currentPage);
+   protected void extractProductsFromCurrentPage() throws UnsupportedEncodingException, MalformedProductException {
 
-      String url = "https://www.allfront.com.br/api/busca/" + this.keywordEncoded + "?offset=" + (this.currentPage - 1) * 15;
-
-      this.log("Link onde são feitos os crawlers: " + url);
-      JSONObject searchedJson = fetch(url);
-      JSONArray productsArray = searchedJson.optJSONArray("products");
-      if (productsArray != null && !productsArray.isEmpty()) {
-         if (this.totalProducts == 0) {
-            setTotalProducts(searchedJson);
+     //String url = HOME_PAGE + "catalogsearch/result/index/?p=" + this.currentPage + "&product_list_limit=" + pageSize + "&q=" + this.keywordEncoded;
+      String url = this.fetchApi();
+      this.currentDoc = fetchDocument(url);
+      Elements products = this.currentDoc.select(".row.mx-0 li");
+      if (!products.isEmpty()) {
+         if (totalProducts == 0) {
+            setTotalProducts();
          }
-         for (Object obj : productsArray) {
-            if (obj instanceof JSONObject) {
+         for (Element e : products) {
+            String productUrl = "https://www.schumann.com.br" + CrawlerUtils.scrapStringSimpleInfoByAttribute(e, ".name a", "href");
+            String internalId = scrapInternalId(e);
+            String name = CrawlerUtils.scrapStringSimpleInfo(e, ".name", false);
+            String imgUrl = CrawlerUtils.scrapSimplePrimaryImage(e, ".lazyload.current-img.no-effect", Arrays.asList("data-src"), "", "");
+            Integer price = CrawlerUtils.scrapPriceInCentsFromHtml(e, ".instant-price", null, true, ',', session, 0);
+            boolean isAvailable = price != 0;
+            RankingProduct productRanking = RankingProductBuilder.create()
+               .setUrl(productUrl)
+               .setInternalId(internalId)
+               .setName(name)
+               .setImageUrl(imgUrl)
+               .setPriceInCents(price)
+               .setAvailability(isAvailable)
+               .build();
 
-               JSONObject product = (JSONObject) obj;
+            saveDataProduct(productRanking);
 
-               String productUrl = getUrl(product);
-               String internalPid = product.optString("id");
-               String name = product.optString("nome");
-               String imageUrl = JSONUtils.getValueRecursive(product, "images.0.images", String.class);
-               int price = CommonMethods.doublePriceToIntegerPrice(product.optDouble("preco"), 0);
-               boolean isAvailable = price != 0;
-
-               //New way to send products to save data product
-               RankingProduct productRanking = RankingProductBuilder.create()
-                  .setUrl(productUrl)
-                  .setInternalId(null)
-                  .setInternalPid(internalPid)
-                  .setName(name)
-                  .setPriceInCents(price)
-                  .setAvailability(isAvailable)
-                  .setImageUrl(imageUrl)
-                  .build();
-
-               saveDataProduct(productRanking);
-
-               if (this.arrayProducts.size() == productsLimit)
-                  break;
-
-            }
          }
       } else {
          this.result = false;
          this.log("Keyword sem resultado!");
       }
-
-      this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
    }
 
-   private String getUrl(JSONObject product) {
-      String url = null;
-      String link = product.optString("link");
-      if (link != null && !link.isEmpty()) {
-         url = "https://www.schumann.com.br/" + link;
-      }
+   private String scrapInternalId(Element e) {
+      return CrawlerUtils.scrapStringSimpleInfoByAttribute(e, ".wd-product-line-medias.wd-widget-js", "data-pid");
+   }
 
-      return url;
+   protected String fetchApi() {
+      Map<String, String> headers = new HashMap<>();
+
+      String url = HOME_PAGE + this.keywordEncoded+"?pg="+ this.currentPage;
+
+      this.log("Link onde são feitos os crawlers: " + url);
+
+      headers.put("origin", HOME_PAGE);
+
+      Request request = Request.RequestBuilder.create()
+         .setUrl(url)
+         .setHeaders(headers)
+         .setCookies(cookies)
+         .setFollowRedirects(true)
+         .build();
+      Response response = this.dataFetcher.get(session, request);
+      return response.getRedirectUrl();
    }
 
 
-   protected void setTotalProducts(JSONObject searchedJson) {
-      this.totalProducts = searchedJson.optInt("quantidade");
-      this.log("Total da busca: " + this.totalProducts);
+   @Override
+   protected void setTotalProducts() {
+      //(this.currentDoc, ".product-count", false)
+      this.totalProducts = CrawlerUtils.scrapIntegerFromHtml(this.currentDoc, ".product-count", false);
 
+      this.log("Total: " + this.totalProducts);
    }
-
 }
