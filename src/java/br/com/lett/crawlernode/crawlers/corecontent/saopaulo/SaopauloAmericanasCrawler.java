2af21ec0dfb75930f87328c0195a83e1f1b4471d
Thainá <thainaj.aguiar@gmail.com>
2021-12-23 16:09:53 -0300

    fix 1p

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/saopaulo/SaopauloAmericanasCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/saopaulo/SaopauloAmericanasCrawler.java
@@ -23,7 +23,6 @@ public class SaopauloAmericanasCrawler extends B2WCrawler {
    private static final String HOME_PAGE = "https://www.americanas.com.br/";
    private static final String URL_PAGE_OFFERS = "https://www.americanas.com.br/parceiros/";
    private static final String MAIN_SELLER_NAME_LOWER = "americanas.com";
-   private static final List<String>  MAIN_SELLER_NAME_LIST= Collections.singletonList("Lojas Americanas");
    private static final String MAIN_SELLER_NAME_LOWER_FROM_HTML = "Americanas";
 
    private static final List<String> UserAgent = Arrays.asList(
@@ -40,11 +39,10 @@ public class SaopauloAmericanasCrawler extends B2WCrawler {
 
    public SaopauloAmericanasCrawler(Session session) {
       super(session);
-      super.subSellers = Arrays.asList("b2w", "lojas americanas", "lojas americanas mg", "lojas americanas rj", "lojas americanas sp", "lojas americanas rs");
+      super.subSellers = Arrays.asList("b2w", "lojas americanas", "lojas americanas mg", "lojas americanas rj", "lojas americanas sp", "lojas americanas rs", "Lojas Americanas");
       super.sellerNameLower = MAIN_SELLER_NAME_LOWER;
       super.sellerNameLowerFromHTML = MAIN_SELLER_NAME_LOWER_FROM_HTML;
       super.homePage = HOME_PAGE;
-      super.subSellers = MAIN_SELLER_NAME_LIST;
       super.urlPageOffers = URL_PAGE_OFFERS;
       super.config.setFetcher(FetchMode.JSOUP);
    }
