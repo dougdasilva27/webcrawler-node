847e5d643703708d4bdf4b48fea6f472824bad25
Thiago Frois <zthiagopf@gmail.com>
2022-01-05 17:21:26 -0300

    [vtex] update vtex ranking to new architecture

--- src/java/br/com/lett/crawlernode/crawlers/extractionutils/ranking/VtexRankingKeywordsNew.java
+++ src/java/br/com/lett/crawlernode/crawlers/extractionutils/ranking/VtexRankingKeywordsNew.java
@@ -2,9 +2,13 @@ package br.com.lett.crawlernode.crawlers.extractionutils.ranking;
 
 import br.com.lett.crawlernode.core.fetcher.models.Request;
 import br.com.lett.crawlernode.core.fetcher.models.Response;
+import br.com.lett.crawlernode.core.models.RankingProduct;
+import br.com.lett.crawlernode.core.models.RankingProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.exceptions.MalformedProductException;
 import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.JSONUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -32,16 +36,70 @@ public abstract class VtexRankingKeywordsNew extends CrawlerRankingKeywords {
          JSONObject product = (JSONObject) object;
          String productUrl = product.optString("link");
          String internalPid = product.optString("productId");
+         String name = product.optString("productName");
 
-         saveDataProduct(null, internalPid, productUrl);
+         JSONObject itemData = (JSONObject) product.optQuery("/items/0");
+         String image = null;
+         int priceInCents = 0;
+         boolean isAvailable = false;
 
-         this.log("Position: " + this.position + " - InternalId: " + null  + " - InternalPid: " + internalPid
-            + " - Url: " + productUrl);
+         if(itemData != null) {
+            JSONArray images = itemData.optJSONArray("images");
+            JSONArray sellers = itemData.optJSONArray("sellers");
+            image = crawlImage(images);
+
+            if(sellers != null && sellers.length() > 0) {
+               JSONObject seller = sellers.optJSONObject(0);
+               JSONObject commertialOffer = seller.optJSONObject("commertialOffer");
+
+               priceInCents = crawlPrice(commertialOffer);
+               isAvailable = commertialOffer.optBoolean("IsAvailable");
+            }
+         }
+
+         try {
+            RankingProduct rankingProduct = RankingProductBuilder.create()
+               .setInternalPid(internalPid)
+               .setUrl(productUrl)
+               .setName(name)
+               .setImageUrl(image)
+               .setPriceInCents(priceInCents)
+               .setAvailability(isAvailable)
+               .build();
+
+            saveDataProduct(rankingProduct);
+         } catch (MalformedProductException e) {
+            this.log(e.getMessage());
+         }
 
          if (this.arrayProducts.size() == productsLimit) {
             break;
          }
       }
+   }
+
+   protected int crawlPrice(JSONObject commertialOffer) {
+      int priceInCents = 0;
+
+         if(commertialOffer != null) {
+            Object price = commertialOffer.opt("Price");
+            if (price instanceof Double) {
+               priceInCents = (int) Math.round((Double) price * 100);
+            } else if (price instanceof Integer) {
+               priceInCents = (int) price * 100;
+            }
+         }
+      return priceInCents;
+   }
+
+   protected String crawlImage(JSONArray images) {
+         if (images != null && images.length() > 0) {
+            JSONObject image = images.optJSONObject(0);
+            if(image != null) {
+               return image.optString("imageUrl");
+            }
+         }
+      return null;
    }
 
    protected JSONArray fetchPage(String url) {
