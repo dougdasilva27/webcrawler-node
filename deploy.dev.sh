2ce9504b72dd14620a5ff1b09361a9a2d5beddc8
marcos <marcosmourabro@gmail.com>
2022-01-07 16:45:57 -0300

    change deploy.dev

--- deploy.dev.sh
+++ deploy.dev.sh
@@ -6,7 +6,8 @@ $(aws ecr get-login --no-include-email)
 export ENVIROMENT_DEPLOY=$1
 export ECR_IMAGE_URL=868884350453.dkr.ecr.us-east-1.amazonaws.com/team-data-capture/webcrawler:$1
 
-npx json -I -f docker-compose.dev.yml -e "image"="'$ECR_IMAGE_URL'"
+#npx json -I -f docker-compose.dev.yml -e "this.services.webscraper."="'$ECR_IMAGE_URL'"
+grep image docker-compose.dev.yml | sed -i 's/""/'"$ECR_IMAGE_URL"'/' docker-compose.dev.yml
 
 docker-compose -f build.yml run --rm maven
 docker-compose -f build.yml build webcrawler
