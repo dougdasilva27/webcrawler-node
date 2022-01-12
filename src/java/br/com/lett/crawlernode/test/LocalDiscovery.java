9d27ae2fbb550bf878697192594dd7069e19ce22
Thiago Frois <zthiagopf@gmail.com>
2022-01-12 16:58:44 -0300

    add verification to compare internalId and internalPid between core and ranking

--- src/java/br/com/lett/crawlernode/test/LocalDiscovery.java
+++ src/java/br/com/lett/crawlernode/test/LocalDiscovery.java
@@ -2,6 +2,7 @@ package br.com.lett.crawlernode.test;
 
 import br.com.lett.crawlernode.core.models.Market;
 import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.models.Ranking;
 import br.com.lett.crawlernode.core.models.RankingProduct;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.session.crawler.TestCrawlerSession;
@@ -9,6 +10,7 @@ import br.com.lett.crawlernode.core.task.base.Task;
 import br.com.lett.crawlernode.core.task.impl.Crawler;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRanking;
 import br.com.lett.crawlernode.util.CommonMethods;
+import br.com.lett.crawlernode.util.ConsoleHackTools;
 import br.com.lett.crawlernode.util.Logging;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -17,12 +19,15 @@ import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class LocalDiscovery {
 
    protected static final Logger logger = LoggerFactory.getLogger(Crawler.class);
    private final JSONArray errors = new JSONArray();
+   public static final String RED_COLOR = "\033[1;31m";
+   public static final String RESET_COLOR = "\033[0m";
 
    public void discovery(Market market, List<String> keywords, Integer maxProducts, Integer corePoolSize) throws InterruptedException {
 
@@ -65,7 +70,16 @@ public class LocalDiscovery {
                } else if (((TestCrawlerSession) session).getProducts() != null && !((TestCrawlerSession) session).getProducts().isEmpty()) {
                   countProducts++;
                   for (Product product : ((TestCrawlerSession) session).getProducts()) {
-                     System.out.println(TestUtils.printProduct(product));
+                     RankingProduct productRanking = products.stream().filter(rankingProduct -> Objects.equals(rankingProduct.getUrl(), product.getUrl())).findAny().orElse(null);
+                     if(productRanking != null){
+                        if(productRanking.getInternalId() != null && !productRanking.getInternalId().equals(product.getInternalId())) {
+                           System.out.println(RED_COLOR + "MISMATCHING internalId: Ranking > " + productRanking.getInternalId() + " - Core > " + product.getInternalId());
+                        }
+                        if(productRanking.getInteranlPid() != null && !productRanking.getInteranlPid().equals(product.getInternalPid())) {
+                           System.out.println(RED_COLOR + "MISMATCHING internalPid: Ranking > " + productRanking.getInteranlPid() + " - Core > " + product.getInternalPid());
+                        }
+                     }
+                     System.out.println(RESET_COLOR + TestUtils.printProduct(product));
                   }
                }else {
                   System.out.println("\t Nenhum produto encontrado");
@@ -81,3 +95,4 @@ public class LocalDiscovery {
    }
 
 }
+
