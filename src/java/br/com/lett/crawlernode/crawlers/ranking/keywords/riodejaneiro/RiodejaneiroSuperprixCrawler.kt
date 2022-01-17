50ebe33320a40fc6a23e442c9ca5035367c1e872
Carol Evangelista <cdsevangelista@inf.ufpel.edu.br>
2022-01-17 08:51:52 -0300

    Updating super prix to new sos

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/riodejaneiro/RiodejaneiroSuperprixCrawler.kt
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/riodejaneiro/RiodejaneiroSuperprixCrawler.kt
@@ -1,9 +1,12 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.riodejaneiro
 
+import br.com.lett.crawlernode.core.models.RankingProductBuilder
 import br.com.lett.crawlernode.core.session.Session
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords
+import br.com.lett.crawlernode.util.CrawlerUtils
 import org.apache.http.impl.cookie.BasicClientCookie
 import org.jsoup.nodes.Element
+import java.util.*
 
 class RiodejaneiroSuperprixCrawler(session: Session) : CrawlerRankingKeywords(session) {
 
@@ -21,12 +24,28 @@ class RiodejaneiroSuperprixCrawler(session: Session) : CrawlerRankingKeywords(se
       val url = home + "buscapagina?ft=${keywordWithoutAccents.replace(" ", "+")}" +
          "&PS=15&sl=$token&cc=15&sm=0&PageNumber=$currentPage"
       val fetchDocument = fetchDocument(url, cookies)
+
       fetchDocument.select(".prateleira li[layout]")?.forEach {
-         val internal = it.selectFirst(".buy-button-normal")?.attr("id")
-         val pid = it.selectFirst(".avaliacao div")?.attr("id")?.split("-")?.last()
+         val internalId = it.selectFirst(".buy-button-normal")?.attr("id")
+         val internalPid = it.selectFirst(".avaliacao div")?.attr("id")?.split("-")?.last()
          val productUrl = it.selectFirst("a")?.attr("href")
-         log("InternalId: $internal - pid: $pid - Url: $productUrl")
-         saveDataProduct(internal, pid, productUrl)
+         val name = CrawlerUtils.scrapStringSimpleInfo(it, ".data > h3 > a", true);
+         val imgUrl = CrawlerUtils.scrapSimplePrimaryImage(it, ".productImage img", Arrays.asList("src"), "https", "")
+         val price = CrawlerUtils.scrapPriceInCentsFromHtml(it, ".newPrice em", null, false, ',', session, 0)
+
+         val isAvailable = price != 0
+
+         val productRanking = RankingProductBuilder.create()
+            .setUrl(productUrl)
+            .setInternalId(internalId)
+            .setInternalPid(internalPid)
+            .setName(name)
+            .setImageUrl(imgUrl)
+            .setPriceInCents(price)
+            .setAvailability(isAvailable)
+            .build()
+
+         saveDataProduct(productRanking)
       }
    }
 
