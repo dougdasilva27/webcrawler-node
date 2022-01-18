2d6f0fa4d86869d07e8b989e319027cc5a56a794
BuSSoLoTTi <matheusbussolotti@gmail.com>
2022-01-18 09:49:50 -0300

    fix ranking big / bom preço

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/saopaulo/SaopauloBigCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/saopaulo/SaopauloBigCrawler.java
@@ -9,19 +9,15 @@ import br.com.lett.crawlernode.util.JSONUtils;
 import br.com.lett.crawlernode.util.Logging;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.Base64;
 
 public class SaopauloBigCrawler extends VTEXRankingKeywords {
-
-   private static final Integer API_VERSION = 1;
-   private static final String SENDER = "vtex.store-resources@0.x";
-   private static final String PROVIDER = "vtex.search-graphql@0.x";
-   private String keySHA256 = "e1d5228ca8b18d1f83b777d849747a7f55c9597f670f20c8557d2b0428bf6be7";
-
-
+   
    public SaopauloBigCrawler(Session session) {
       super(session);
    }
@@ -29,6 +25,22 @@ public class SaopauloBigCrawler extends VTEXRankingKeywords {
    @Override
    protected String getHomePage() {
       return session.getOptions().getString("homePage");
+   }
+
+   protected Document fetchDocument() {
+
+      StringBuilder searchPage = new StringBuilder();
+
+      searchPage.append(getHomePage())
+         .append(this.keywordEncoded)
+         .append("?_q")
+         .append(this.keywordEncoded)
+         .append("&map=ft&page")
+         .append(this.currentPage);
+
+      String apiUrl = searchPage.toString().replace("+", "%20");
+
+      return Jsoup.parse(fetchPage(apiUrl));
    }
 
    @Override
@@ -41,82 +53,4 @@ public class SaopauloBigCrawler extends VTEXRankingKeywords {
       return session.getOptions().getString("vtex_segment");
    }
 
-   @Override
-   protected String createVariablesBase64() {
-      JSONObject search = new JSONObject();
-      search.put("hideUnavailableItems", false);
-      search.put("skusFilter", "ALL");
-      search.put("simulationBehavior", "default");
-      search.put("installmentCriteria", "MAX_WITHOUT_INTEREST");
-      search.put("productOriginVtex", true);
-      search.put("map", "ft");
-      search.put("query", keywordEncoded);
-      search.put("orderBy", "");
-      search.put("from", this.arrayProducts.size());
-      search.put("to", this.arrayProducts.size() + (this.pageSize - 1));
-
-      JSONArray selectedFacets = new JSONArray();
-      JSONObject obj = new JSONObject();
-      obj.put("key", "ft");
-      obj.put("value", this.keywordEncoded);
-
-      selectedFacets.put(obj);
-
-      search.put("selectedFacets", selectedFacets);
-      search.put("fullText", this.location);
-      search.put("operator", JSONObject.NULL);
-      search.put("fuzzy", JSONObject.NULL);
-      search.put("facetsBehavior", "Static");
-      search.put("categoryTreeBehavior", "default");
-      search.put("withFacets", false);
-
-      return Base64.getEncoder().encodeToString(search.toString().getBytes());
-   }
-
-   @Override
-   protected JSONObject fetchSearchApi() {
-      JSONObject searchApi;
-      StringBuilder url = new StringBuilder();
-      url.append(getHomePage() + "_v/segment/graphql/v1?");
-
-      JSONObject extensions = new JSONObject();
-      JSONObject persistedQuery = new JSONObject();
-
-      persistedQuery.put("version", API_VERSION);
-      persistedQuery.put("sha256Hash", this.keySHA256);
-      persistedQuery.put("sender", SENDER);
-      persistedQuery.put("provider", PROVIDER);
-
-      extensions.put("variables", createVariablesBase64());
-      extensions.put("persistedQuery", persistedQuery);
-
-      StringBuilder payload = new StringBuilder();
-      payload.append("workspace=master");
-      payload.append("&maxAge=short");
-      payload.append("&appsEtag=remove");
-      payload.append("&domain=store");
-      payload.append("&locale=pt-BR");
-      payload.append("&operationName=productSearchV3");
-      try {
-         payload.append("&variables=" + URLEncoder.encode("{}", "UTF-8"));
-         payload.append("&extensions=" + URLEncoder.encode(extensions.toString(), "UTF-8"));
-      } catch (UnsupportedEncodingException e) {
-         Logging.printLogError(logger, session, CommonMethods.getStackTrace(e));
-      }
-      url.append(payload.toString());
-
-      log("Link onde são feitos os crawlers:" + url);
-
-      Request request = Request.RequestBuilder.create()
-         .setUrl(url.toString())
-         .setCookies(cookies)
-         .setPayload(payload.toString())
-         .build();
-
-      JSONObject response = CrawlerUtils.stringToJson(this.dataFetcher.get(session, request).getBody());
-
-      searchApi = JSONUtils.getValueRecursive(response, "data.productSearch", JSONObject.class, new JSONObject());
-
-      return searchApi;
-   }
 }
