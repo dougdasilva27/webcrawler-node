c8e8c56e91998f166e83d19b36e9690381e7ab0f
Thainá <thainaj.aguiar@gmail.com>
2022-01-21 09:35:07 -0300

    add new sos - kanui

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilKanuiCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilKanuiCrawler.java
@@ -1,71 +1,93 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
 
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import br.com.lett.crawlernode.core.models.RankingProduct;
+import br.com.lett.crawlernode.core.models.RankingProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.exceptions.MalformedProductException;
+import br.com.lett.crawlernode.util.CommonMethods;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class BrasilKanuiCrawler extends CrawlerRankingKeywords {
 
-  public BrasilKanuiCrawler(Session session) {
-    super(session);
-  }
+   public BrasilKanuiCrawler(Session session) {
+      super(session);
+   }
 
-  @Override
-  protected void extractProductsFromCurrentPage() {
-    // número de produtos por página do market
-    this.pageSize = 48;
+   @Override
+   protected void extractProductsFromCurrentPage() throws MalformedProductException {
+      // número de produtos por página do market
+      this.pageSize = 48;
 
-    this.log("Página " + this.currentPage);
+      this.log("Página " + this.currentPage);
 
-    // monta a url com a keyword e a página
-    String url = "https://www.kanui.com.br/catalog/?q=" + this.keywordEncoded + "&page=" + this.currentPage;
-    this.log("Link onde são feitos os crawlers: " + url);
+      // monta a url com a keyword e a página
+      String url = "https://www.kanui.com.br/catalog/?q=" + this.keywordEncoded + "&page=" + this.currentPage;
+      this.log("Link onde são feitos os crawlers: " + url);
 
-    this.currentDoc = fetchDocument(url);
+      this.currentDoc = fetchDocument(url);
 
-    Elements products = this.currentDoc.select("div.product-box div[id]");
+      Elements products = this.currentDoc.select(".content-products-list.row .product-box");
 
-    if (!products.isEmpty()) {
-      if (this.totalProducts == 0)
-        setTotalProducts();
+      if (!products.isEmpty()) {
+         if (this.totalProducts == 0)
+            setTotalProducts();
 
-      for (Element e : products) {
-        // seta o id da classe pai com o id retirado do elements
-        String internalPid = e.attr("id");
-        String internalId = null;
+         for (Element e : products) {
+            String internalPid = CrawlerUtils.scrapStringSimpleInfoByAttribute(e, ".product-box-image", "id");
+            String productUrl = CrawlerUtils.scrapStringSimpleInfoByAttribute(e, "a", "href");
+            String name = CrawlerUtils.scrapStringSimpleInfo(e, ".product-box-title", true);
+            String imageUrl = CrawlerUtils.scrapStringSimpleInfoByAttribute(e, " .product-box-image a", "data-model-picture");
+            int price = CommonMethods.doublePriceToIntegerPrice(scrapPrice(e), 0);
+            boolean isAvailable = price != 0;
 
-        // monta a url
-        Element urlElement = e.select(" > a").first();
-        String productUrl = urlElement.attr("href");
+            RankingProduct productRanking = RankingProductBuilder.create()
+               .setUrl(productUrl)
+               .setInternalId(null)
+               .setInternalPid(internalPid)
+               .setName(name)
+               .setPriceInCents(price)
+               .setAvailability(isAvailable)
+               .setImageUrl(imageUrl)
+               .build();
 
-        saveDataProduct(internalId, internalPid, productUrl);
+            saveDataProduct(productRanking);
 
-        this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + internalPid + " - Url: " + productUrl);
-        if (this.arrayProducts.size() == productsLimit)
-          break;
+            if (this.arrayProducts.size() == productsLimit)
+               break;
 
+         }
+      } else {
+         this.result = false;
+         this.log("Keyword sem resultado!");
       }
-    } else {
-      this.result = false;
-      this.log("Keyword sem resultado!");
-    }
 
-    this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
+      this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
 
-  }
+   }
 
-  @Override
-  protected void setTotalProducts() {
-    Element totalElement = this.currentDoc.select("div.items-products.select-options-item span").first();
+   private Double scrapPrice(Element e) {
+      Double price = CrawlerUtils.scrapDoublePriceFromHtml(e, ".product-box-price-to", null, true, ',', session);
+      if (price == null) {
+         price = CrawlerUtils.scrapDoublePriceFromHtml(e, ".product-box-price-from", null, true, ',', session);
+      }
+      return price;
 
-    try {
-      if (totalElement != null)
-        this.totalProducts = Integer.parseInt(totalElement.text());
-    } catch (Exception e) {
-      this.logError(e.getMessage());
-    }
+   }
 
-    this.log("Total da busca: " + this.totalProducts);
-  }
+   @Override
+   protected void setTotalProducts() {
+      Element totalElement = this.currentDoc.select("div.items-products.select-options-item span").first();
+
+      try {
+         if (totalElement != null)
+            this.totalProducts = Integer.parseInt(totalElement.text());
+      } catch (Exception e) {
+         this.logError(e.getMessage());
+      }
+
+      this.log("Total da busca: " + this.totalProducts);
+   }
 }
