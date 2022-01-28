8f9262953f9809035cdf307430a9b1543e051d2e
Thiago Frois <zthiagopf@gmail.com>
2022-01-28 17:46:51 -0300

    [padovani] update do linx

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/saopaulo/SaopauloPadovaniCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/saopaulo/SaopauloPadovaniCrawler.java
@@ -3,6 +3,7 @@ package br.com.lett.crawlernode.crawlers.ranking.keywords.saopaulo;
 import br.com.lett.crawlernode.core.fetcher.models.Request;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.crawlers.extractionutils.ranking.LinxImpulseRanking;
 import br.com.lett.crawlernode.util.JSONUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -10,69 +11,23 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-public class SaopauloPadovaniCrawler extends CrawlerRankingKeywords {
+public class SaopauloPadovaniCrawler extends LinxImpulseRanking {
+   private String internalId = "";
 
    public SaopauloPadovaniCrawler(Session session) {
       super(session);
    }
 
    @Override
-   protected void extractProductsFromCurrentPage() {
-      this.pageSize = 12;
-      this.log("Página " + this.currentPage);
+   protected String crawlInternalPid(JSONObject product) {
+      String internalPid = super.crawlInternalPid(product);
+      this.internalId = super.crawlInternalId(product, internalPid);
 
-      JSONObject gridInfo = scrapProductGridFromApi();
-
-      JSONArray products = gridInfo.optJSONArray("products");
-
-      if (!products.isEmpty()) {
-         if (this.totalProducts == 0) {
-            setTotalProducts(gridInfo);
-         }
-
-         for (Object e : products) {
-
-            JSONObject product = (JSONObject) e;
-
-            String internalId = product.optString("id");
-            String urlProduct = "https:" + product.optString("url");
-            saveDataProduct(internalId, null, urlProduct);
-
-            this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + null + " - Url: " + urlProduct);
-            if (this.arrayProducts.size() == productsLimit)
-               break;
-         }
-      } else {
-         this.result = false;
-         this.log("Keyword sem resultado!");
-      }
-
-      this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
-      if (!(hasNextPage()))
-         setTotalProducts();
-
+      return internalId;
    }
 
-
-   private JSONObject scrapProductGridFromApi(){
-      String apiURL = "https://api.linximpulse.com/engage/search/v3/search?apiKey=padovani&page=" + this.currentPage + "&resultsPerPage=30&terms=" + this.keywordEncoded + "&sortBy=relevance";
-
-      this.log("Link onde são feitos os crawlers: " + apiURL);
-
-
-      Map<String,String> headers = new HashMap<>();
-      headers.put("origin","https://www.padovani.com.br");
-
-      Request request = Request.RequestBuilder.create().setUrl(apiURL).setHeaders(headers).build();
-      String resp = this.dataFetcher.get(session,request).getBody();
-
-
-      return JSONUtils.stringToJson(resp);
-   }
-
-
-   protected void setTotalProducts(JSONObject json) {
-      this.totalProducts = json.optInt("size");
-      this.log("Total da busca: " + this.totalProducts);
+   @Override
+   protected String crawlInternalId(JSONObject product, String internalPid) {
+      return this.internalId;
    }
 }
