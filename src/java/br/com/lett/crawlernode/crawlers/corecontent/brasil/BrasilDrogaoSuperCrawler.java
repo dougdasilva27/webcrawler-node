0d64b40fa5298bc05857705c3d0991709b620b90
vagner sampaio <vagnersampaio92@gmail.com>
2022-02-15 09:11:08 -0300

    finalizado ranking

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilDrogaoSuperCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilDrogaoSuperCrawler.java
@@ -1,2 +1,4 @@
-package br.com.lett.crawlernode.crawlers.corecontent.brasil;public class BrasilDrogaoSuperCrawler {
+package br.com.lett.crawlernode.crawlers.corecontent.brasil;
+
+public class BrasilDrogaoSuperCrawler {
 }
