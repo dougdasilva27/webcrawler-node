016b83a6c1588b1cbbe0bfa4594a141232dd9abe
Thiago Frois <62728696+tpfrois@users.noreply.github.com>
2022-02-22 16:27:35 -0300

    [gimba] fix ranking

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilGimbaCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilGimbaCrawler.java
@@ -1,10 +1,16 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
 
+import br.com.lett.crawlernode.core.models.RankingProduct;
+import br.com.lett.crawlernode.core.models.RankingProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.exceptions.MalformedProductException;
+import br.com.lett.crawlernode.util.CommonMethods;
 import br.com.lett.crawlernode.util.CrawlerUtils;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import java.util.Collections;
 
 public class BrasilGimbaCrawler extends CrawlerRankingKeywords {
    public BrasilGimbaCrawler(Session session) {
@@ -12,38 +18,43 @@ public class BrasilGimbaCrawler extends CrawlerRankingKeywords {
    }
 
    @Override
-   protected void extractProductsFromCurrentPage() {
+   protected void extractProductsFromCurrentPage() throws MalformedProductException {
 
       this.pageSize = 21;
 
       this.log("Página " + this.currentPage);
 
-      String url = "https://www.gimba.com.br/?txt-busca="+this.keywordWithoutAccents+"&btn-buscar=";
+      String url = "https://www.gimba.com.br/?txt-busca=" + this.keywordWithoutAccents + "&btn-buscar=Buscar&numeroPagina=" + this.currentPage;
 
       this.log("Link onde são feitos os crawlers: " + url);
 
       this.currentDoc = fetchDocument(url);
 
-      Elements products = this.currentDoc.select(".lst-vitrine.clearfix li[id]");
+      Elements products = this.currentDoc.select(".card-product");
 
-      if (products.size() >= 1) {
-
-         if (this.totalProducts == 0) {
-            setTotalProducts();
-         }
-
+      if (!products.isEmpty()) {
          for (Element e : products) {
+            String internalId = crawlInternalId(e);
+            String name = CrawlerUtils.scrapStringSimpleInfo(e, ".card-product-name", true);
+            String urlProduct = crawlProductUrl(e, internalId, name);
+            String imgUrl = CrawlerUtils.scrapSimplePrimaryImage(e, ".card-product-thumbnail img", Collections.singletonList("src"), "https", "imagens.gimba.com.br");
+            Integer price = CrawlerUtils.scrapPriceInCentsFromHtml(e, ".card-product-offer .text-price", null, false, ',', session, 0);
+            boolean isAvailable = price != 0;
 
-               String internalId = CrawlerUtils.scrapStringSimpleInfoByAttribute(e, "ul li", "data-erp");
-               String urlProduct = CrawlerUtils.scrapUrl(e, "li[data-name] a", "href", "http:", "www.gimba.com.br");
-               saveDataProduct(internalId, null, urlProduct);
+            RankingProduct productRanking = RankingProductBuilder.create()
+               .setUrl(urlProduct)
+               .setInternalId(internalId)
+               .setInternalPid(null)
+               .setImageUrl(imgUrl)
+               .setName(name)
+               .setPriceInCents(price)
+               .setAvailability(isAvailable)
+               .build();
 
-               this.log("Position: " + this.position + " - InternalId: " + internalId
-                  + " - InternalPid: " + null + " - Url: " + urlProduct);
-
-               if (this.arrayProducts.size() == productsLimit) {
-                  break;
-               }
+            saveDataProduct(productRanking);
+            if (this.arrayProducts.size() == productsLimit) {
+               break;
+            }
          }
       } else {
          this.result = false;
@@ -54,19 +65,25 @@ public class BrasilGimbaCrawler extends CrawlerRankingKeywords {
          + this.arrayProducts.size() + " produtos crawleados");
    }
 
+   private String crawlProductUrl(Element e, String internalId, String name) {
+      String url = CrawlerUtils.scrapUrl(e, ".card-product-content > a", "href", "https", "www.gimba.com.br");
+      if(url == null || url.isEmpty()) {
+         String slugifiedName = CommonMethods.toSlug(name);
+         return "https://www.gimba.com.br/" + slugifiedName +"/" + "?PID=" + internalId;
+      }
+      return url;
+   }
+
+   private String crawlInternalId(Element e) {
+      String internalId = e.attr("id");
+      if(!internalId.isEmpty() && internalId != null) {
+         return internalId.replaceAll("[^0-9]", "");
+      }
+      return null;
+   }
 
    @Override
-   protected void setTotalProducts() {
-      Element totalElement = this.currentDoc.selectFirst(".total-buscas .red");
-
-      try {
-         if (totalElement != null) {
-            this.totalProducts = Integer.parseInt(totalElement.text());
-         }
-      } catch (Exception e) {
-         this.logError(e.getMessage());
-      }
-
-      this.log("Total da busca: " + this.totalProducts);
+   protected boolean hasNextPage() {
+      return true;
    }
 }
