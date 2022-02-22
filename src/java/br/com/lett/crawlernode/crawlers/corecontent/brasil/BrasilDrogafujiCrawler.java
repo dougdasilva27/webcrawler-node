2fc1cb7ce93dabd158e44504b58e1c95095107e4
Thales Rodrigues <thalessantos30@gmail.com>
2022-02-22 18:56:35 -0300

    updating droga fuji core and ranking crawler

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilDrogafujiCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilDrogafujiCrawler.java
@@ -6,19 +6,23 @@ import br.com.lett.crawlernode.core.models.Product;
 import br.com.lett.crawlernode.core.models.ProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.Crawler;
+import br.com.lett.crawlernode.util.CrawlerUtils;
 import br.com.lett.crawlernode.util.Logging;
 import br.com.lett.crawlernode.util.MathUtils;
+import com.google.common.collect.Sets;
+import exceptions.MalformedPricingException;
+import exceptions.OfferException;
 import models.Marketplace;
+import models.Offer;
+import models.Offers;
 import models.prices.Prices;
+import models.pricing.*;
 import org.json.JSONArray;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 /**
  * Date: 08/08/2017
@@ -29,6 +33,9 @@ import java.util.TreeMap;
 public class BrasilDrogafujiCrawler extends Crawler {
 
   private final String HOME_PAGE = "https://www.drogafuji.com.br/";
+  private static final String SELLER_FULL_NAME = "droga-fuji-brasil";
+
+  protected Set<String> cards = Sets.newHashSet(Card.ELO.toString(), Card.VISA.toString(), Card.MASTERCARD.toString(), Card.AMEX.toString(), Card.HIPERCARD.toString(), Card.DINERS.toString());
 
   public BrasilDrogafujiCrawler(Session session) {
     super(session);
@@ -48,24 +55,32 @@ public class BrasilDrogafujiCrawler extends Crawler {
     if (isProductPage(doc)) {
       Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
 
-      String internalId = crawlInternalId(doc);
-      String internalPid = crawlInternalPid(doc);
-      String name = crawlName(doc);
-      Float price = crawlPrice(doc, internalId);
-      Prices prices = crawlPrices(price, doc);
-      boolean available = crawlAvailability(doc);
+      String internalId = CrawlerUtils.scrapStringSimpleInfoByAttribute(doc, "input#___rc-p-sku-ids", "value");
+      String internalPid = CrawlerUtils.scrapStringSimpleInfo(doc, ".skuReference", false);
+      String name = CrawlerUtils.scrapStringSimpleInfo(doc, ".productName", false);
+      boolean isAvailable = crawlAvailability(doc);
+      Offers offers = isAvailable ? crawlOffers(doc) : new Offers();
       CategoryCollection categories = crawlCategories(doc);
-      String primaryImage = crawlPrimaryImage(doc);
-      String secondaryImages = crawlSecondaryImages(doc);
-      String description = crawlDescription(doc);
+      String primaryImage = CrawlerUtils.scrapSimplePrimaryImage(doc, "#image-main", Collections.singletonList("src"), "https", "drogafuji.vteximg.com.br");
+      List<String> secondaryImages = crawlSecondaryImages(doc);
+      String description = CrawlerUtils.scrapSimpleDescription(doc, Collections.singletonList(".productDescription .productDescription"));
       Integer stock = null;
       Marketplace marketplace = crawlMarketplace();
 
       // Creating the product
-      Product product = ProductBuilder.create().setUrl(session.getOriginalURL()).setInternalId(internalId).setInternalPid(internalPid).setName(name)
-          .setPrice(price).setPrices(prices).setAvailable(available).setCategory1(categories.getCategory(0)).setCategory2(categories.getCategory(1))
-          .setCategory3(categories.getCategory(2)).setPrimaryImage(primaryImage).setSecondaryImages(secondaryImages).setDescription(description)
-          .setStock(stock).setMarketplace(marketplace).build();
+      Product product = ProductBuilder.create()
+         .setUrl(session.getOriginalURL())
+         .setInternalId(internalId)
+         .setInternalPid(internalPid)
+         .setName(name)
+         .setCategories(categories)
+         .setPrimaryImage(primaryImage)
+         .setSecondaryImages(secondaryImages)
+         .setDescription(description)
+         .setStock(stock)
+         .setOffers(offers)
+         .setMarketplace(marketplace)
+         .build();
 
       products.add(product);
 
@@ -78,41 +93,97 @@ public class BrasilDrogafujiCrawler extends Crawler {
   }
 
   private boolean isProductPage(Document doc) {
-    return doc.select(".product-view").first() != null;
+    return doc.select(".product-details").first() != null;
   }
 
-  private String crawlInternalId(Document doc) {
-    String internalId = null;
+   private Offers crawlOffers(Document doc) throws OfferException, MalformedPricingException {
+      Offers offers = new Offers();
+      Pricing pricing = scrapPricing(doc);
+      List<String> sales = scrapSales(pricing);
 
-    Element internalIdElement = doc.select("input[name=product]").first();
-    if (internalIdElement != null) {
-      internalId = internalIdElement.val();
-    }
+      if(pricing != null){
+         offers.add(Offer.OfferBuilder.create()
+            .setUseSlugNameAsInternalSellerId(true)
+            .setSellerFullName(SELLER_FULL_NAME)
+            .setMainPagePosition(1)
+            .setIsBuybox(false)
+            .setIsMainRetailer(true)
+            .setPricing(pricing)
+            .setSales(sales)
+            .build());
+      }
 
-    return internalId;
-  }
+      return offers;
+   }
 
-  private String crawlInternalPid(Document doc) {
-    String internalPid = null;
-    Element pdi = doc.select("#product-attribute-specs-table td.data").first();
+   private Pricing scrapPricing(Document doc) throws MalformedPricingException {
+      Double[] prices = scrapPrices(doc);
+      Double priceFrom = null;
+      Double spotlightPrice = null;
+      if (prices.length >= 2) {
+         priceFrom = prices[0];
+         spotlightPrice = prices[1];
+      }
 
-    if (pdi != null) {
-      internalPid = pdi.ownText().trim();
-    }
+      if(spotlightPrice != null){
+         CreditCards creditCards = scrapCreditCards(spotlightPrice);
+         BankSlip bankSlip = BankSlip.BankSlipBuilder.create()
+            .setFinalPrice(spotlightPrice)
+            .build();
 
-    return internalPid;
-  }
+         return Pricing.PricingBuilder.create()
+            .setSpotlightPrice(spotlightPrice)
+            .setPriceFrom(priceFrom)
+            .setCreditCards(creditCards)
+            .setBankSlip(bankSlip)
+            .build();
+      }else{
+         return null;
+      }
+   }
 
-  private String crawlName(Document document) {
-    String name = null;
-    Element nameElement = document.select(".product-name h1").first();
+   private Double[] scrapPrices(Document doc){
+      Double spotlightPrice;
+      Double priceFrom;
 
-    if (nameElement != null) {
-      name = nameElement.ownText().trim();
-    }
+      priceFrom = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".skuListPrice", null, false, ',', session);
+      if (priceFrom != null && priceFrom == 0) {
+         priceFrom = null;
+      }
 
-    return name;
-  }
+      spotlightPrice = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".skuBestPrice", null, false, ',', session);
+
+      return new Double[]{priceFrom, spotlightPrice};
+   }
+
+   private CreditCards scrapCreditCards(Double spotlightPrice) throws MalformedPricingException {
+      CreditCards creditCards = new CreditCards();
+      Installments installments = new Installments();
+      installments.add(Installment.InstallmentBuilder.create()
+         .setInstallmentNumber(1)
+         .setInstallmentPrice(spotlightPrice)
+         .build());
+
+
+      for (String card : cards) {
+         creditCards.add(CreditCard.CreditCardBuilder.create()
+            .setBrand(card)
+            .setInstallments(installments)
+            .setIsShopCard(false)
+            .build());
+      }
+
+      return creditCards;
+   }
+
+   private List<String> scrapSales(Pricing pricing) {
+      List<String> sales = new ArrayList<>();
+
+      String saleDiscount = CrawlerUtils.calculateSales(pricing);
+      sales.add(saleDiscount);
+
+      return sales;
+   }
 
   private Float crawlPrice(Document document, String internalId) {
     Float price = null;
@@ -158,18 +229,16 @@ public class BrasilDrogafujiCrawler extends Crawler {
    * @param doc
    * @return
    */
-  private String crawlSecondaryImages(Document doc) {
-    String secondaryImages = null;
-    JSONArray secondaryImagesArray = new JSONArray();
+  private List<String> crawlSecondaryImages(Document doc) {
+    List<String> secondaryImages = new ArrayList<>();
 
-    // Elements images = doc.select(".more-views-list > li:not([class=active]) a");
-    //
-    // for(Element e : images) {
-    // secondaryImagesArray.put(e.attr("href"));
-    // }
+    Elements imagesLi = doc.select(".thumbs img");
+    for (Element imageLi : imagesLi) {
+       secondaryImages.add(imageLi.attr("src"));
+    }
 
-    if (secondaryImagesArray.length() > 0) {
-      secondaryImages = secondaryImagesArray.toString();
+    if (secondaryImages.size() > 0) {
+       secondaryImages.remove(0);
     }
 
     return secondaryImages;
@@ -183,15 +252,12 @@ public class BrasilDrogafujiCrawler extends Crawler {
    */
   private CategoryCollection crawlCategories(Document document) {
     CategoryCollection categories = new CategoryCollection();
-    Elements elementCategories = document.select(".breadcrumbs li[class^=category] a");
+    Elements elementCategories = document.select(".bread-crumb ul > li");
 
-    for (int i = 0; i < elementCategories.size(); i++) {
-      String cat = elementCategories.get(i).ownText().trim();
-
-      if (!cat.isEmpty()) {
-        categories.add(cat);
-      }
-    }
+     for (Element elementCategory : elementCategories) {
+        String category = CrawlerUtils.scrapStringSimpleInfo(elementCategory, "span[itemprop=name]", false);
+        categories.add(category);
+     }
 
     return categories;
   }
@@ -215,7 +281,7 @@ public class BrasilDrogafujiCrawler extends Crawler {
   }
 
   private boolean crawlAvailability(Document doc) {
-    return doc.select("div.add-to-box .btn-cart").first() != null;
+    return doc.select(".skuBestPrice").first() != null;
   }
 
   /**
