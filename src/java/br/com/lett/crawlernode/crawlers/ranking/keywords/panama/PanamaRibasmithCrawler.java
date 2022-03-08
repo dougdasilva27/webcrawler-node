b1c53c36d9fac947e52276114f227f7fd552d49a
Thiago Frois <62728696+tpfrois@users.noreply.github.com>
2022-03-08 09:37:46 -0300

    [riba-smith] update to new sos and fix core pid

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/panama/PanamaRibasmithCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/panama/PanamaRibasmithCrawler.java
@@ -1,10 +1,16 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.panama;
 
 import br.com.lett.crawlernode.core.fetcher.models.Request;
+import br.com.lett.crawlernode.core.models.RankingProduct;
+import br.com.lett.crawlernode.core.models.RankingProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.exceptions.MalformedProductException;
+import br.com.lett.crawlernode.util.CommonMethods;
 import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.JSONUtils;
 import org.json.JSONObject;
+import org.yaml.snakeyaml.util.UriEncoder;
 
 import java.io.UnsupportedEncodingException;
 
@@ -27,42 +33,60 @@ public class PanamaRibasmithCrawler extends CrawlerRankingKeywords {
    }
 
    @Override
-   protected void extractProductsFromCurrentPage() throws UnsupportedEncodingException {
-
+   protected void extractProductsFromCurrentPage() throws UnsupportedEncodingException, MalformedProductException {
       this.pageSize = 20;
 
       JSONObject searchResult = searchJson();
 
       if (searchResult != null && searchResult.has("items")) {
-
-         if(this.totalProducts ==0){
+         if (this.totalProducts == 0) {
             this.totalProducts = searchResult.optInt("total_results");
          }
 
          for (Object object : searchResult.optJSONArray("items")) {
-            JSONObject products = (JSONObject) object;
+            JSONObject product = (JSONObject) object;
 
+            String internalId = product.optString("sku");
+            String internalPid = product.optString("id");
+            String url = scrapUrl(product);
+            String name = product.optString("l");
+            String imgUrl = product.optString("t");
+            Integer price = scrapPrice(product);
+            boolean isAvailable = price != null;
 
+            RankingProduct productRanking = RankingProductBuilder.create()
+               .setUrl(url)
+               .setInternalId(internalId)
+               .setInternalPid(internalPid)
+               .setImageUrl(imgUrl)
+               .setName(name)
+               .setPriceInCents(price)
+               .setAvailability(isAvailable)
+               .build();
 
-            String internalId = products.getString("sku");
-            String internalPId = products.getString("id");
-            String url = products.getString("u");
-
-            saveDataProduct(internalId, internalPId, url);
-
-            this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + internalPId + " - Url: " + url);
-
-
+            saveDataProduct(productRanking);
          }
-
-
       } else {
          log("keyword sem resultado");
       }
    }
 
+   private String scrapUrl(JSONObject product) {
+      String url = product.optString("u");
+      if(!url.isEmpty()) {
+         url = UriEncoder.encode(url);
+      }
+
+      return url;
+   }
+
+   private int scrapPrice(JSONObject product) {
+      String price = product.optString("p");
+      return CommonMethods.stringPriceToIntegerPrice(price, '.', null);
+   }
+
    @Override
    protected boolean hasNextPage() {
-      return !(this.position>=this.totalProducts);
+      return !(this.position >= this.totalProducts);
    }
 }
