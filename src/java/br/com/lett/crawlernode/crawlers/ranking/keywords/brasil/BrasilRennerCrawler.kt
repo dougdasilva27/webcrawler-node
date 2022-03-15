738d1ad10a1c98dcc8d6b17b4923733b468b7e73
Thiago Frois <62728696+tpfrois@users.noreply.github.com>
2022-03-15 09:19:09 -0300

    [renner] update to new sos

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilRennerCrawler.kt
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilRennerCrawler.kt
@@ -1,10 +1,10 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil
 
 import br.com.lett.crawlernode.core.fetcher.models.Request
+import br.com.lett.crawlernode.core.models.RankingProductBuilder
 import br.com.lett.crawlernode.core.session.Session
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords
 import br.com.lett.crawlernode.util.CrawlerUtils
-import br.com.lett.crawlernode.util.toInt
 import org.json.JSONObject
 import java.net.URLDecoder
 
@@ -17,66 +17,89 @@ import java.net.URLDecoder
  */
 class BrasilRennerCrawler(session: Session) : CrawlerRankingKeywords(session) {
 
-    init {
-        pageSize = 40
-    }
+   init {
+      pageSize = 40
+   }
 
-    override fun extractProductsFromCurrentPage() {
+   override fun extractProductsFromCurrentPage() {
 
-        val json = requestApi()
+      val json = requestApi()
 
-        if (this.totalProducts == 0) {
-            setTotalProducts(json)
-        }
+      if (this.totalProducts == 0) {
+         setTotalProducts(json)
+      }
 
-        val elements = json.optJSONArray("docs")
+      val elements = json.optJSONArray("docs")
 
-        if (elements.isEmpty) {
-            result = false
-            return
-        }
+      if (elements.isEmpty) {
+         result = false
+         return
+      }
 
-        for (elem in elements) {
-            val element = (elem as JSONObject)
-            val internalId = element.optJSONArray("sku_list").optString(0)
-            val internalPid = element.optString("parent_product_id")
+      for (elem in elements) {
+         val element = (elem as JSONObject)
+         val internalId = element.optJSONArray("sku_list").optString(0)
+         val internalPid = element.optString("parent_product_id")
 
-            val productUrl = crawlProductUrl(element)
-            saveDataProduct(internalId, internalPid, productUrl)
-            if (arrayProducts.size == productsLimit) {
-                break
-            }
-        }
-    }
+         val productUrl = crawlProductUrl(element)
+         val productName = element.optString("name")
+         val productPrice = scrapPrice(element)
+         val productImage = element.optString("imageId")
+         val available = element.optString("inventory_status", "").equals("INSTOCK", true)
 
-    private fun crawlProductUrl(element: JSONObject): String {
-        return URLDecoder.decode( element.optString("clickUrl").substringAfter("&ct=").replace("&redirect=true", ""), "UTF-8" );
-    }
+         val productRanking = RankingProductBuilder.create()
+            .setUrl(productUrl)
+            .setInternalId(internalId)
+            .setInternalPid(internalPid)
+            .setImageUrl(productImage)
+            .setName(productName)
+            .setPriceInCents(productPrice)
+            .setAvailability(available)
+            .build()
 
-    private fun requestApi() :JSONObject {
-        val url = "https://recs.richrelevance.com/rrserver/api/find/v1/24f07d816ef94d7f" +
-                "?log=true&lang=pt&placement=search_page.find&ssl=true&mm=3%3C90%25" +
-                "&query=$keywordEncoded" +
-                "&rows=$pageSize" +
-                "&start=${(currentPage-1)*pageSize}"
+         saveDataProduct(productRanking)
+         if (arrayProducts.size == productsLimit) {
+            break
+         }
+      }
+   }
 
-        val headers: MutableMap<String, String> = HashMap()
-        headers["Content-Type"] = "application/json"
+   private fun scrapPrice(element: JSONObject): Int? {
+      if(element.has("salePriceCents")) {
+         return element.optInt("salePriceCents")
+      } else {
+         return element.optInt("priceCents")
+      }
+   }
 
-        val request = Request.RequestBuilder.create().setUrl(url).setCookies(cookies).setHeaders(headers).mustSendContentEncoding(false).build()
+   private fun crawlProductUrl(element: JSONObject): String {
+      return URLDecoder.decode(element.optString("clickUrl").substringAfter("&ct=").replace("&redirect=true", ""), "UTF-8");
+   }
 
-        val json = CrawlerUtils.stringToJson(dataFetcher[session, request].body)
+   private fun requestApi(): JSONObject {
+      val url = "https://recs.richrelevance.com/rrserver/api/find/v1/24f07d816ef94d7f" +
+         "?log=true&lang=pt&placement=search_page.find&ssl=true&mm=3%3C90%25" +
+         "&query=$keywordEncoded" +
+         "&rows=$pageSize" +
+         "&start=${(currentPage - 1) * pageSize}"
 
-        return json.optJSONArray("placements").optJSONObject(0)
-    }
+      val headers: MutableMap<String, String> = HashMap()
+      headers["Content-Type"] = "application/json"
 
-    fun setTotalProducts(search: JSONObject) {
+      val request = Request.RequestBuilder.create().setUrl(url).setCookies(cookies).setHeaders(headers).mustSendContentEncoding(false).build()
 
-        val total = search.opt("numFound")
+      val json = CrawlerUtils.stringToJson(dataFetcher[session, request].body)
 
-        if (total is Int) {
-            totalProducts = total
-        }
-    }
+      return json.optJSONArray("placements").optJSONObject(0)
+   }
+
+   fun setTotalProducts(search: JSONObject) {
+
+      val total = search.opt("numFound")
+
+      if (total is Int) {
+         totalProducts = total
+      }
+   }
 }
 
