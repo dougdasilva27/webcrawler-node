5cd60a797b0ab9428383822510fd24aff13c633f
Daniel Santos <danielsantos@lett.digital>
2022-03-15 10:19:01 -0300

    Uptade SOS for ExtraBom

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilExtrabomCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilExtrabomCrawler.java
@@ -3,8 +3,11 @@ package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
 import br.com.lett.crawlernode.core.fetcher.ProxyCollection;
 import br.com.lett.crawlernode.core.fetcher.models.LettProxy;
 import br.com.lett.crawlernode.core.fetcher.models.Request;
+import br.com.lett.crawlernode.core.models.RankingProduct;
+import br.com.lett.crawlernode.core.models.RankingProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.exceptions.MalformedProductException;
 import br.com.lett.crawlernode.util.CrawlerUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -13,6 +16,7 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -41,7 +45,7 @@ public class BrasilExtrabomCrawler extends CrawlerRankingKeywords {
    }
 
    @Override
-   protected void extractProductsFromCurrentPage() {
+   protected void extractProductsFromCurrentPage() throws MalformedProductException {
 
       this.pageSize = 20;
       this.log("Página " + this.currentPage);
@@ -50,16 +54,14 @@ public class BrasilExtrabomCrawler extends CrawlerRankingKeywords {
 
       this.log("Link onde são feitos os crawlers: " + url);
       this.currentDoc = fetchDocument(url);
-      Elements products = this.currentDoc.select(".carousel__detalhes-top");
+      Elements products = this.currentDoc.select(".carousel__detalhe-item ");
 
       if (!products.isEmpty()) {
 
          for (Element e : products) {
             String data = CrawlerUtils.scrapStringSimpleInfoByAttribute(e, "a", "href");
-
             String urlProduct = "";
             String internalId = "";
-
             if (data != null) {
                urlProduct = "https://www.extrabom.com.br/" + data;
                String[] internalIdSplit = data.split("/");
@@ -68,13 +70,21 @@ public class BrasilExtrabomCrawler extends CrawlerRankingKeywords {
                }
             }
 
-            saveDataProduct(internalId, null, urlProduct);
+            String name = crawlName(e);
+            String image = CrawlerUtils.scrapSimplePrimaryImage(e, ".carousel__box-img > table > tbody > tr > td > a > img", Collections.singletonList("src"), "https", "www.extrabom.com.br");
+            Integer priceInCents = CrawlerUtils.scrapPriceInCentsFromHtml(e, ".item-por > strong ", null, false, ',', session, null);
+            boolean available = priceInCents != null;
 
-            this.log(
-               "Position: " + this.position +
-                  " - InternalId: " + internalId +
-                  " - InternalPid: " + null +
-                  " - Url: " + urlProduct);
+            RankingProduct productRanking = RankingProductBuilder.create()
+               .setUrl(urlProduct)
+               .setInternalId(internalId)
+               .setImageUrl(image)
+               .setName(name)
+               .setPriceInCents(priceInCents)
+               .setAvailability(available)
+               .build();
+
+            saveDataProduct(productRanking);
 
             if (this.arrayProducts.size() == productsLimit) {
                break;
@@ -94,6 +104,16 @@ public class BrasilExtrabomCrawler extends CrawlerRankingKeywords {
       Element pagination = this.currentDoc.selectFirst(".pagination-box a:nth-last-child(2) > span");
       String selector = pagination != null ? pagination.text() : null;
       return selector != null && selector.contains("»");
+   }
+
+   private String crawlName(Element e) {
+      String scrapName = CrawlerUtils.scrapStringSimpleInfo(e, ".carousel__inner > a > table > tbody > tr > td > div.name-produto", false);
+      if(scrapName != null){
+         return scrapName;
+      }else {
+         scrapName = CrawlerUtils.scrapStringSimpleInfo(e, ".carousel__box__dados > a > table > tbody > tr > td", false);
+         return scrapName;
+      }
    }
 
 }
