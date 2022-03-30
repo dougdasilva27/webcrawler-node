d77c0e6195b0f9b173a545c9d8ef4dc698a52b5b
Daniel Santos <danielsantos@lett.digital>
2022-03-30 13:57:16 -0300

    Sos Update
    Update the HomeCenter Ranking for the new model of SOS

--- src/java/br/com/lett/crawlernode/crawlers/extractionutils/ranking/HomecenterCrawlerRanking.java
+++ src/java/br/com/lett/crawlernode/crawlers/extractionutils/ranking/HomecenterCrawlerRanking.java
@@ -1,7 +1,10 @@
 package br.com.lett.crawlernode.crawlers.extractionutils.ranking;
 
+import br.com.lett.crawlernode.core.models.RankingProduct;
+import br.com.lett.crawlernode.core.models.RankingProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.exceptions.MalformedProductException;
 import br.com.lett.crawlernode.util.CrawlerUtils;
 import org.apache.http.impl.cookie.BasicClientCookie;
 import org.jsoup.nodes.Element;
@@ -35,7 +38,7 @@ public abstract class HomecenterCrawlerRanking extends CrawlerRankingKeywords {
    }
 
    @Override
-   protected void extractProductsFromCurrentPage() {
+   protected void extractProductsFromCurrentPage() throws MalformedProductException {
       this.pageSize = 28;
       this.log("Página " + this.currentPage);
 
@@ -50,14 +53,42 @@ public abstract class HomecenterCrawlerRanking extends CrawlerRankingKeywords {
             String internalId = CrawlerUtils.scrapStringSimpleInfoByAttribute(e, null, "data-key");
             String productUrl = CrawlerUtils.scrapUrl(e, ".product.ie11-product-container .link-with-wrapper a", Collections.singletonList("href"), "https", HOME_PAGE);
 
-            saveDataProduct(internalId, null, productUrl);
+            String name = CrawlerUtils.scrapStringSimpleInfo(e, ".jsx-411745769 > a > .jsx-411745769.product-title", true);
+            String imageUrl = CrawlerUtils.scrapSimplePrimaryImage(e, ".jsx-411745769.product-image > div > img", Collections.singletonList("data-src"), "https", "https://www.homecenter.com.co");
+            Integer price = null;
+            boolean isAvailable = getAvaliability(e);
+            if (isAvailable) {
+               price = CrawlerUtils.scrapPriceInCentsFromHtml(e, ".phablet-desktop > .product-price-and-logo > .main.gridView > .price > span:first-child", null, true, ',', session, null);
+            }
 
-            this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + null
-               + " - Url: " + productUrl);
+            RankingProduct productRanking = RankingProductBuilder.create()
+               .setUrl(productUrl)
+               .setInternalId(internalId)
+               .setName(name)
+               .setPriceInCents(price)
+               .setAvailability(isAvailable)
+               .setImageUrl(imageUrl)
+               .build();
+
+            saveDataProduct(productRanking);
+
+            if (this.arrayProducts.size() == productsLimit) {
+               break;
+            }
          }
+      } else {
+         this.result = false;
+         this.log("Keyword sem resultado!");
       }
       this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora "
          + this.arrayProducts.size() + " produtos crawleados");
+   }
+
+   private boolean getAvaliability(Element e) {
+      String firstValidation = CrawlerUtils.scrapStringSimpleInfo(e, ".jsx-2799553099.dispatch-info", false);
+      String secondValidation = CrawlerUtils.scrapStringSimpleInfo(e, ".jsx-2799553099.withdrawl-info", false);
+
+      return firstValidation.equals("Disponible para despacho") || secondValidation.equals("Disponible para retiro");
    }
 
    @Override
