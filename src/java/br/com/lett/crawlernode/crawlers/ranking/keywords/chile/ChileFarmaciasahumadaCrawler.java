2875ac9a9affb68958e5901a112049653c9ebe4f
Carol Evangelista <cdsevangelista@inf.ufpel.edu.br>
2022-03-30 23:58:12 -0300

    Updating SOS Farmacias ahumada

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/chile/ChileFarmaciasahumadaCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/chile/ChileFarmaciasahumadaCrawler.java
@@ -1,13 +1,17 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.chile;
 
+import br.com.lett.crawlernode.core.models.RankingProduct;
+import br.com.lett.crawlernode.core.models.RankingProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.exceptions.MalformedProductException;
 import br.com.lett.crawlernode.util.CrawlerUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class ChileFarmaciasahumadaCrawler extends CrawlerRankingKeywords {
@@ -17,7 +21,7 @@ public class ChileFarmaciasahumadaCrawler extends CrawlerRankingKeywords {
    }
 
    @Override
-   protected void extractProductsFromCurrentPage() {
+   protected void extractProductsFromCurrentPage() throws MalformedProductException {
       this.pageSize = 20;
       this.log("Página " + this.currentPage);
 
@@ -37,12 +41,25 @@ public class ChileFarmaciasahumadaCrawler extends CrawlerRankingKeywords {
 
             String internalPid = CrawlerUtils.scrapStringSimpleInfoByAttribute(e,".product-item-info .price-box","data-product-id");
             String internalId = internalPid;
-
             String productUrl = CrawlerUtils.scrapStringSimpleInfoByAttribute(e, ".product-item-info .product-item-photo", "href");
+            String name = CrawlerUtils.scrapStringSimpleInfo(e,".product-item-info .product-item-name a", false);
+            String imgUrl = CrawlerUtils.scrapSimplePrimaryImage(e, ".product-item-info img.product-image-photo", Arrays.asList("src"), "https", "");
+            Integer price = scrapPrice(e);
 
-            saveDataProduct(internalId, internalPid, productUrl);
+            boolean isAvailable = price != 0;
 
-            this.log("Position: " + this.position + " - InternalId: " + internalId + " - InternalPid: " + internalPid + " - Url: " + productUrl);
+            RankingProduct productRanking = RankingProductBuilder.create()
+               .setUrl(productUrl)
+               .setInternalId(internalId)
+               .setInternalPid(internalPid)
+               .setName(name)
+               .setImageUrl(imgUrl)
+               .setPriceInCents(price)
+               .setAvailability(isAvailable)
+               .build();
+
+            saveDataProduct(productRanking);
+
             if (this.arrayProducts.size() == productsLimit) {
                break;
             }
@@ -59,5 +76,15 @@ public class ChileFarmaciasahumadaCrawler extends CrawlerRankingKeywords {
    @Override
    protected boolean hasNextPage() {
       return this.currentDoc.selectFirst(".toolbar.toolbar-products .items.pages-items .action.next") != null;
+   }
+
+   protected Integer scrapPrice(Element e) {
+      Integer spotlightPrice = CrawlerUtils.scrapPriceInCentsFromHtml(e, "[data-price-type=\"finalPrice\"] .price", null, false, ',', session, 0);
+
+      if (spotlightPrice == 0) {
+         spotlightPrice = CrawlerUtils.scrapPriceInCentsFromHtml(e, ".prueba .price", null, false, ',', session, 0);
+      }
+
+      return spotlightPrice;
    }
 }
