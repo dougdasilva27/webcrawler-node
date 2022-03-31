daa12619c31c8710a9984ef8c7b8d026989ae3e9
Carol Evangelista <cdsevangelista@inf.ufpel.edu.br>
2022-03-31 00:34:16 -0300

    Updating arcomix crawler

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/recife/RecifeArcomixCrawler.kt
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/recife/RecifeArcomixCrawler.kt
@@ -2,12 +2,14 @@ package br.com.lett.crawlernode.crawlers.ranking.keywords.recife
 
 import br.com.lett.crawlernode.core.fetcher.FetchMode
 import br.com.lett.crawlernode.core.fetcher.models.Request.RequestBuilder
+import br.com.lett.crawlernode.core.models.RankingProductBuilder
 import br.com.lett.crawlernode.core.session.Session
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords
+import br.com.lett.crawlernode.util.CrawlerUtils
 import br.com.lett.crawlernode.util.JSONUtils
-import org.apache.http.HttpHeaders.CONTENT_TYPE
 import org.json.JSONArray
 import org.json.JSONObject
+import java.util.*
 
 class RecifeArcomixCrawler(session: Session?) : CrawlerRankingKeywords(session) {
 
@@ -18,9 +20,9 @@ class RecifeArcomixCrawler(session: Session?) : CrawlerRankingKeywords(session) 
    private val idArmazem = session!!.options.optString("id_armazem")
 
    override fun extractProductsFromCurrentPage() {
-        pageSize = 30
-       val url = "https://arcomix.com.br/api/busca"
-       val payload = """
+      pageSize = 30
+      val url = "https://arcomix.com.br/api/busca"
+      val payload = """
           {
           "avaliacoes": [],
           "categorias": [],
@@ -36,30 +38,52 @@ class RecifeArcomixCrawler(session: Session?) : CrawlerRankingKeywords(session) 
           }
           """.trimIndent()
 
-       val headers: MutableMap<String, String> = HashMap()
-       headers["Cookie"] = "ls.uid_armazem=$idArmazem"
-       headers["Content-Type"] = "application/json;charset=UTF-8"
+      val headers: MutableMap<String, String> = HashMap()
+      headers["Cookie"] = "ls.uid_armazem=$idArmazem"
+      headers["Content-Type"] = "application/json;charset=UTF-8"
 
-        val request = RequestBuilder.create().setUrl(url).setPayload(payload).setHeaders(headers)
-            .build()
+      val request = RequestBuilder.create().setUrl(url).setPayload(payload).setHeaders(headers)
+         .build()
 
-        val json = dataFetcher.post(session, request).body
+      val json = dataFetcher.post(session, request).body
 
-        val productsJson = JSONUtils.stringToJson(json)?.optJSONArray("Produtos")?: JSONArray()
+      val productsJson = JSONUtils.stringToJson(json)?.optJSONArray("Produtos")?: JSONArray()
 
-        for (productJson in productsJson) {
-            if (productJson is JSONObject) {
-                val internalId = productJson.optString("id_produto",null)
-                val productUrl = "https://arcomix.com.br/produto/${productJson.optString("str_link_produto")}"
-                saveDataProduct(internalId, null, productUrl)
-                log("""Position: $position - InternalId: $internalId - Url: $productUrl""")
-            }
-        }
+      for (productJson in productsJson) {
+         if (productJson is JSONObject) {
+            val internalId = productJson.optString("id_produto",null)
+            val productUrl = "https://arcomix.com.br/produto/${productJson.optString("str_link_produto")}"
+            val name = productJson.optString("str_nom_produto",null)
+            val imgUrl = productJson.optString("str_img_path",null)
+            val price: Int = scrapPrice(productJson)
 
-        log("""Finalizando Crawler de produtos da página $currentPage - até agora ${arrayProducts.size} produtos crawleados""")
-    }
+            val isAvailable = price != 0
 
-    override fun hasNextPage(): Boolean {
-        return (arrayProducts.size % pageSize - currentPage) < 0
-    }
+            val productRanking = RankingProductBuilder.create()
+               .setUrl(productUrl)
+               .setInternalId(internalId)
+               .setName(name)
+               .setImageUrl(imgUrl)
+               .setPriceInCents(price)
+               .setAvailability(isAvailable)
+               .build()
+
+            saveDataProduct(productRanking)
+         }
+      }
+
+      log("""Finalizando Crawler de produtos da página $currentPage - até agora ${arrayProducts.size} produtos crawleados""")
+   }
+
+   override fun hasNextPage(): Boolean {
+      return (arrayProducts.size % pageSize - currentPage) < 0
+   }
+
+   fun scrapPrice(productJson: JSONObject): Int {
+      val priceDouble = productJson.optDouble("mny_vlr_produto_por")
+
+      val priceInCents = priceDouble * 100
+
+      return priceInCents.toInt()
+   }
 }
