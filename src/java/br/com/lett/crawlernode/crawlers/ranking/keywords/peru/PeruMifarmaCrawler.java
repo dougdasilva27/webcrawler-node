afbe66ed29b514249679588c3b104e1ec0d83989
Thainá Jardim <67081271+ThainaJardim@users.noreply.github.com>
2022-04-13 16:48:20 -0300

    fix pagination

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/peru/PeruMifarmaCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/peru/PeruMifarmaCrawler.java
@@ -1,7 +1,6 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.peru;
 
 import br.com.lett.crawlernode.core.fetcher.ProxyCollection;
-import br.com.lett.crawlernode.core.fetcher.methods.DataFetcher;
 import br.com.lett.crawlernode.core.fetcher.methods.JsoupDataFetcher;
 import br.com.lett.crawlernode.core.fetcher.models.Request;
 import br.com.lett.crawlernode.core.models.RankingProduct;
@@ -24,7 +23,7 @@ public class PeruMifarmaCrawler extends CrawlerRankingKeywords {
       super(session);
    }
 
-   protected JSONObject fetchJSONObject(String url, String payload, DataFetcher dataFetcher) {
+   protected JSONObject fetchJSONObject(String url, String payload) {
       Map<String, String> headers = new HashMap<>();
       headers.put("content-type", "application/json");
       headers.put("origin", "https://mifarma.com.pe");
@@ -42,22 +41,23 @@ public class PeruMifarmaCrawler extends CrawlerRankingKeywords {
             ProxyCollection.NETNUT_RESIDENTIAL_AR_HAPROXY,
             ProxyCollection.NETNUT_RESIDENTIAL_CO_HAPROXY)
          )
+         .setSendUserAgent(true)
          .build();
 
-      String json = dataFetcher.post(session, request).getBody();
+      String json = new JsoupDataFetcher().post(session, request).getBody();
       return JSONUtils.stringToJson(json);
    }
 
    @Override
    protected void extractProductsFromCurrentPage() throws MalformedProductException {
-      this.pageSize = 10;
+
       this.log("Página " + this.currentPage);
 
       String payload = getPayload();
 
       String url = "https://5doa19p9r7.execute-api.us-east-1.amazonaws.com/MFPRD/filtered-products";
 
-      JSONObject json = fetchJSONObject(url, payload, new JsoupDataFetcher());
+      JSONObject json = fetchJSONObject(url, payload);
       JSONArray products = json.optJSONArray("rows");
 
       if (products != null && !products.isEmpty()) {
@@ -94,11 +94,6 @@ public class PeruMifarmaCrawler extends CrawlerRankingKeywords {
       this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
    }
 
-   @Override
-   protected boolean hasNextPage() {
-      return true;
-   }
-
    private Integer getPriceIfAvailable(JSONObject product) {
       String status = product.optString("productStatus");
       if ("AVAILABLE".equals(status)) {
@@ -118,13 +113,11 @@ public class PeruMifarmaCrawler extends CrawlerRankingKeywords {
       JSONObject payloadJson = new JSONObject();
       List<String> ids = new ArrayList<>();
 
-      String url = "https://o74e6qkj1f-dsn.algolia.net/1/indexes/" +
-         "products/query?x-algolia-agent=Algolia%20for%20JavaScript%20(3.35.1)%3B%20Browser" +
-         "&x-algolia-application-id=O74E6QKJ1F&x-algolia-api-key=b65e33077a0664869c7f2544d5f1e332";
+      String url = "https://o74e6qkj1f-dsn.algolia.net/1/indexes/products/query?x-algolia-agent=Algolia%20for%20JavaScript%20(3.35.1)%3B%20Browser&x-algolia-application-id=O74E6QKJ1F&x-algolia-api-key=b65e33077a0664869c7f2544d5f1e332";
 
-      String payload = "{\"params\":\"query=" + this.keywordEncoded + "&attributesToRetrieve=%5B%22objectID%22%2C%22name%22%2C%22uri%22%5D&hitsPerPage=10&page=" + (this.currentPage - 1) + "\"}";
+      String payload = "{\"params\":\"query=" + keywordEncoded + "&attributesToRetrieve=%5B%22objectID%22%2C%22name%22%2C%22uri%22%5D&hitsPerPage=" + productsLimit + "\"}";
 
-      JSONObject json = fetchJSONObject(url, payload, new JsoupDataFetcher());
+      JSONObject json = fetchJSONObject(url, payload);
       JSONArray products = json.optJSONArray("hits");
       if (products != null && !products.isEmpty()) {
          for (Object obj : products) {
@@ -134,8 +127,7 @@ public class PeruMifarmaCrawler extends CrawlerRankingKeywords {
                if (internalPid != null && !internalPid.isEmpty()) ids.add(internalPid);
             }
          }
-         System.out.println("Workkk");
-         payloadJson.put("page", this.currentPage - 1);
+         payloadJson.put("page", "0");
          payloadJson.put("rows", products.length());
          payloadJson.put("order", "ASC");
          payloadJson.put("sort", "ranking");
@@ -144,8 +136,7 @@ public class PeruMifarmaCrawler extends CrawlerRankingKeywords {
          return payloadJson.toString();
       }
 
-      return "{\"page\":0,\"rows\":10,\"order\":\"ASC\",\"sort\":\"ranking\",\"productsFilter\": [\"029136\",\"011640\",\"424079\",\"430683\",\"010278\",\"424080\",\"003408\",\"431498\",\"003411\",\"023871\"]}";
-
+      return "";
    }
 
 
