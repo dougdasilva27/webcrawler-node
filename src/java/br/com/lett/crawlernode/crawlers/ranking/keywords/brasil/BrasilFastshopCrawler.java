e57a32913f0cdc17f37d05698ce71d6435a9f90d
Carol Evangelista <cdsevangelista@inf.ufpel.edu.br>
2022-04-13 04:20:45 -0300

    Scrapping ranking from category keywords

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilFastshopCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilFastshopCrawler.java
@@ -2,10 +2,13 @@ package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
 
 import java.util.*;
 
+import br.com.lett.crawlernode.core.fetcher.ProxyCollection;
+import br.com.lett.crawlernode.core.fetcher.methods.JsoupDataFetcher;
+import br.com.lett.crawlernode.core.fetcher.models.Request;
+import br.com.lett.crawlernode.core.fetcher.models.Response;
 import br.com.lett.crawlernode.core.models.RankingProduct;
 import br.com.lett.crawlernode.core.models.RankingProductBuilder;
 import br.com.lett.crawlernode.exceptions.MalformedProductException;
-import br.com.lett.crawlernode.util.CrawlerUtils;
 import br.com.lett.crawlernode.util.JSONUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -22,7 +25,6 @@ public class BrasilFastshopCrawler extends CrawlerRankingKeywords {
 
    @Override
    protected void extractProductsFromCurrentPage() throws MalformedProductException {
-
       this.log("Página " + this.currentPage);
 
       String apiUrl = "https://fastshop-v6.neemu.com/searchapi/v3/search?apiKey=fastshop-v6&secretKey=7V0dpc8ZFxwCRyCROLZ8xA%253D%253D&terms="
@@ -33,42 +35,50 @@ public class BrasilFastshopCrawler extends CrawlerRankingKeywords {
       headers.put("origin", "https://www.fastshop.com.br");
 
       String json = fetchGetFetcher(apiUrl, null, headers, null);
+      JSONObject apiResponse = new JSONObject(json);
 
-      JSONObject api = new JSONObject(json);
-      extractProductFromJSON(api);
+      if (apiResponse.optString("queryType").equals("redirect")) {
+         apiResponse = scrapJsonByCategory(apiResponse);
+      }
+
+      extractProductFromJSON(apiResponse);
+
    }
 
    private void extractProductFromJSON(JSONObject api) throws MalformedProductException {
-      if (api.has("products")) {
+      if (api != null && api.has("products")) {
+         JSONArray products = api.getJSONArray("products");
+
          if (this.totalProducts == 0) {
-            setTotalProductsFromJSON(api);
+            this.totalProducts = products.length();
          }
 
-         JSONArray products = api.getJSONArray("products");
+         JSONArray prices = scrapPricesJson(products);
          for (int i = 0; i < products.length(); i++) {
             JSONObject product = products.getJSONObject(i);
 
             String internalPid = crawlinternalPid(product);
+            if (Objects.equals(internalPid, "111498_PRD")) {
+               System.out.println("s");
+            }
             String productUrl = crawlProductUrlFromJson(product, internalPid);
-            JSONArray internalIds = crawlInternalId(product);
-            String name = product.optString("name");
+            List<String> internalIds = crawlInternalIds(product);
+            String name = crawlName(product);
             String imgUrl = crawlImgUrl(product);
-            Double price = product.optDouble("price");
-            Integer priceInCents = price.intValue() * 100;
-            Boolean isAvailable = product.optString("status").equals("AVAILABLE");
+            Integer price = scrapPriceInCents(prices, internalPid, product);
+            Boolean isAvailable = scrapAvailability(product);
 
-            this.position++;
-
-            for (int j = 0; j < internalIds.length(); j++) {
-               String internalId = internalIds.getString(j);
+            for (int j = 0; j < internalIds.size(); j++) {
+               String internalId = internalIds.get(j);
+               String variationName = isVoltageVariation(name, internalId, product);
 
                RankingProduct productRanking = RankingProductBuilder.create()
                   .setUrl(productUrl)
                   .setInternalId(internalId)
                   .setInternalPid(internalPid)
-                  .setName(name)
+                  .setName(variationName)
                   .setImageUrl(imgUrl)
-                  .setPriceInCents(priceInCents)
+                  .setPriceInCents(price)
                   .setAvailability(isAvailable)
                   .build();
 
@@ -79,40 +89,115 @@ public class BrasilFastshopCrawler extends CrawlerRankingKeywords {
                break;
             }
          }
+      } else {
+         this.result = false;
+         this.log("Keyword sem resultado!");
       }
    }
 
    @Override
    protected boolean hasNextPage() {
-      return this.arrayProducts.size() < this.totalProducts;
+      return true;
    }
 
-   private void setTotalProductsFromJSON(JSONObject api) {
-      if (api.has("size") && api.get("size") instanceof Integer) {
-         this.totalProducts = api.getInt("size");
-         this.log("Total de produtos: " + this.totalProducts);
-      }
+   private JSONObject scrapJsonByCategory(JSONObject product) {
+      String redirectUrl = product.optString("link");
+      String categoryId = redirectUrl.replaceAll("[^0-9]", "");
+
+      Map<String, String> headers = new HashMap<>();
+      headers.put("content-type", "application/json");
+      headers.put("origin", "https://www.fastshop.com.br");
+
+      Request request = Request.RequestBuilder.create().setUrl("https://api.fastshop.com.br/regionalized-catalog/v1/catalog/by-category?category=" +
+            categoryId
+            +"&pageNumber=" + this.currentPage)
+         .setHeaders(headers)
+         .mustSendContentEncoding(false)
+         .build();
+
+      Response response = this.dataFetcher.get(session, request);
+
+      JSONObject apiResponse = new JSONObject(response.getBody());
+
+      return apiResponse;
    }
 
-   private JSONArray crawlInternalId(JSONObject product) {
-      JSONArray internalIds = new JSONArray();
+   private JSONArray scrapPricesJson(JSONArray products) {
+      String internalPids = scrapAllInternalPid(products);
+      JSONArray productsArray = new JSONArray();
 
-      if (product.has("details")) {
-         JSONObject details = product.getJSONObject("details");
+      Map<String, String> headers = new HashMap<>();
+      headers.put("content-type", "application/json");
+      headers.put("origin", "https://www.fastshop.com.br");
 
-         if (details.has("catalogEntryId")) {
-            internalIds = details.getJSONArray("catalogEntryId");
+      Request request = Request.RequestBuilder.create()
+         .setUrl("https://price-management.fastshop.com.br/api/v1/price-promotion/price?store=fastshop&channel=webapp" + internalPids)
+         .setHeaders(headers)
+         .mustSendContentEncoding(false)
+         .build();
+
+      Response response = new JsoupDataFetcher().get(session, request);
+
+      if (response.getBody() != null && response.getBody().startsWith("{")) {
+         JSONObject apiResponse = new JSONObject(response.getBody());
+
+         if (apiResponse.has("result")) {
+            productsArray = JSONUtils.getValueRecursive(apiResponse, "result.0.products", JSONArray.class);
          }
       }
 
-      return internalIds;
+      return productsArray;
+   }
+
+   private String scrapAllInternalPid(JSONArray products) {
+      String internalPids = "";
+
+      if (products != null) {
+         for (Object product : products) {
+            if (product != null) {
+               String pid = JSONUtils.getStringValue((JSONObject) product, "partNumber");
+               if (pid != null) {
+                  internalPids = internalPids.concat("&skus=" + pid);
+               }
+            }
+
+         }
+      }
+
+      return internalPids;
+   }
+
+   private List<String> crawlInternalIds(JSONObject product) {
+      List<String> internalIdList = new ArrayList();
+
+      if (product != null && product.has("details")) {
+         JSONObject details = product.getJSONObject("details");
+
+         if (details.has("catalogEntryId")) {
+            JSONArray internalIds = details.getJSONArray("catalogEntryId");
+
+            for (int j = 0; j < internalIds.length(); j++) {
+               if (internalIds.getString(j) != null) {
+                  internalIdList.add(internalIds.getString(j));
+               }
+            }
+         }
+      } else if (product != null && product.has("productID")) {
+         internalIdList = List.of(product.optString("productID"));
+      }
+
+      return internalIdList;
    }
 
    private String crawlinternalPid(JSONObject product) {
       String pid = null;
 
       if (product.has("id")) {
-         pid = product.get("id").toString();
+         pid = product.optString("id");
+      }
+
+      if (product.has("partNumber")) {
+         pid = product.optString("partNumber");
       }
 
       return pid;
@@ -125,6 +210,11 @@ public class BrasilFastshopCrawler extends CrawlerRankingKeywords {
          productUrl.append("https://www.fastshop.com.br/web/p/d/");
          productUrl.append(pid + "/");
          productUrl.append(CommonMethods.getLast(product.get("url").toString().split("/")));
+      } else if(product != null && product.has("partNumber")) {
+         productUrl.append("https://www.fastshop.com.br/web/p/d/");
+         productUrl.append(pid + "/");
+         String url = convertNameToUrl(product.optString("shortDescription"));
+         productUrl.append(url);
       } else {
          return null;
       }
@@ -135,6 +225,101 @@ public class BrasilFastshopCrawler extends CrawlerRankingKeywords {
    private String crawlImgUrl(JSONObject product) {
       String imgUrlUnformatted = JSONUtils.getValueRecursive(product, "images.default", String.class);
 
-      return "https:" + imgUrlUnformatted;
+      if (imgUrlUnformatted != null) {
+         imgUrlUnformatted = "https:" + imgUrlUnformatted;
+      } else if (product.has("thumbnail")) {
+         imgUrlUnformatted = "https://www.fastshop.com.br" + product.optString("thumbnail", null);
+      }
+
+      return imgUrlUnformatted;
+   }
+
+   private String convertNameToUrl(String name) {
+      if (name != null) {
+         name = name.toLowerCase();
+         name = name.replaceAll("[^a-zA-Z0-9\\s]", "");
+         name = name.replaceAll(" ", "-");
+         return name;
+      }
+      return null;
+   }
+
+   private String crawlName(JSONObject product) {
+      String name = null;
+
+      if (product != null && product.has("name")) {
+         name = product.optString("name", null);
+      } else if (product != null && product.has("shortDescription")) {
+         name = product.optString("shortDescription", null);
+      }
+
+      return name;
+   }
+
+   private Integer scrapPriceInCents(JSONArray prices, String internalPid, JSONObject product) {
+      if (prices == null) {
+         Double price = product.optDouble("price");
+         Integer priceInCents = price.intValue() * 100;
+         return priceInCents;
+      } else {
+         for (Object priceInfo : prices) {
+            String pid = JSONUtils.getStringValue((JSONObject) priceInfo, "product");
+
+            if (Objects.equals(internalPid, pid)) {
+               Double productPrice = JSONUtils.getValueRecursive(priceInfo, "skus.0.promotions.0.value", Double.class);
+               Double priceInCents = null;
+
+               if (productPrice != null) {
+                  priceInCents = productPrice * 100.0;
+               } else {
+                  productPrice = JSONUtils.getValueRecursive(priceInfo, "skus.0.price.offerPrice", Double.class);
+                  if (productPrice != null) {
+                     priceInCents = productPrice * 100.0;
+                  }
+               }
+               if (priceInCents != null) {
+                  return priceInCents.intValue();
+               }
+            }
+         }
+      }
+
+      return null;
+   }
+
+   private Boolean scrapAvailability(JSONObject product) {
+      Boolean available = false;
+
+      if (product.has("status")) {
+         available = product.optString("status").equals("AVAILABLE");
+      } else if (product.has("buyable")) {
+         available = product.optBoolean("buyable");
+      }
+
+      return available;
+   }
+
+   private String isVoltageVariation(String name, String internalId, JSONObject product) {
+      String specsFirstProduct = JSONUtils.getValueRecursive(product, "skus.0.specs.Voltagem.0", String.class);
+      String specsSecondProduct = JSONUtils.getValueRecursive(product, "skus.1.specs.Voltagem.0", String.class);
+
+      if (specsFirstProduct != null && specsSecondProduct != null) {
+         if (!specsFirstProduct.equals(specsSecondProduct)) {
+
+            JSONArray skus = product.getJSONArray("skus");
+
+            for (Object sku : skus) {
+               String catalogId = JSONUtils.getValueRecursive(sku, "specs.catalogEntryId.0", String.class);
+
+               if (Objects.equals(catalogId, internalId)) {
+                  String voltage = JSONUtils.getValueRecursive(sku, "specs.Voltagem.0", String.class);
+
+                  name = name + " - " + voltage;
+               }
+            }
+         }
+      }
+
+      return name;
    }
 }
