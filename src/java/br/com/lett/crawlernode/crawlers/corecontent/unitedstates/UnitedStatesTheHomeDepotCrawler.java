d56a83244de26c031de27777e049453611b449c9
Thiago Frois <62728696+tpfrois@users.noreply.github.com>
2022-04-19 10:34:50 -0300

    [Home Depot] update fetch to use api

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/unitedstates/UnitedStatesTheHomeDepotCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/unitedstates/UnitedStatesTheHomeDepotCrawler.java
@@ -3,10 +3,11 @@ package br.com.lett.crawlernode.crawlers.corecontent.unitedstates;
 import br.com.lett.crawlernode.core.fetcher.DynamicDataFetcher;
 import br.com.lett.crawlernode.core.fetcher.FetchMode;
 import br.com.lett.crawlernode.core.fetcher.ProxyCollection;
-import br.com.lett.crawlernode.core.models.Card;
-import br.com.lett.crawlernode.core.models.CategoryCollection;
-import br.com.lett.crawlernode.core.models.Product;
-import br.com.lett.crawlernode.core.models.ProductBuilder;
+import br.com.lett.crawlernode.core.fetcher.methods.JsoupDataFetcher;
+import br.com.lett.crawlernode.core.fetcher.models.FetcherOptions;
+import br.com.lett.crawlernode.core.fetcher.models.Request;
+import br.com.lett.crawlernode.core.fetcher.models.Response;
+import br.com.lett.crawlernode.core.models.*;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.Crawler;
 import br.com.lett.crawlernode.util.CommonMethods;
@@ -42,7 +43,9 @@ public class UnitedStatesTheHomeDepotCrawler extends Crawler {
 
    public UnitedStatesTheHomeDepotCrawler(Session session) {
       super(session);
-      super.config.setFetcher(FetchMode.APACHE);   }
+      super.config.setFetcher(FetchMode.FETCHER);
+      super.config.setParser(Parser.HTML);
+   }
 
    @Override
    public void handleCookiesBeforeFetch() {
@@ -58,42 +61,64 @@ public class UnitedStatesTheHomeDepotCrawler extends Crawler {
    }
 
    @Override
-   protected Document fetch() {
-      Document doc = null;
+   protected Response fetchResponse() {
+      String url = "https://www.homedepot.com/federation-gateway/graphql?opname=productClientOnlyProduct";
+      String productId = getProductIdFromUrl();
 
-      try {
-         if (this.VARIATION == 1) {
-            webdriver = DynamicDataFetcher.fetchPageWebdriver(session.getOriginalURL(), ProxyCollection.NETNUT_RESIDENTIAL_US_HAPROXY, session, this.cookiesWD, this.HOME_PAGE);
-            webdriver.waitForElement("h1.product-details__title", 120);
+      Map<String, String> headers = new HashMap<>();
+      headers.put("cookie", CommonMethods.cookiesToString(this.cookies));
+      headers.put("authority", "www.homedepot.com");
+      headers.put("referer", session.getOriginalURL());
+      headers.put("origin", HOME_PAGE);
+      headers.put("accept", "*/*");
+      headers.put("content-type", "application/json");
+      headers.put("x-debug", "false");
+      headers.put("x-experience-name", "general-merchandise");
+      headers.put("x-hd-dc", "origin");
 
-            webdriver.waitLoad(70000);
-            this.CURRENT_URL = this.session.getOriginalURL();
-         } else {
-            webdriver.waitForElement(".super-sku__inline-attribute-wrapper", 120);
+      /*
+       * TODO:
+       *  - Receber o storeId pelas options
+       *  - Receber o zipCode pelas options
+       *  - Talvez seja possível modificar o payload para receber somente as informações necessárias
+       * **/
+      String payload = "{\"operationName\":\"productClientOnlyProduct\",\"variables\":{\"skipSpecificationGroup\":false,\"skipSubscribeAndSave\":false,\"skipKPF\":false,\"itemId\":\"" + productId + "\",\"storeId\":\"721\",\"zipCode\":\"37013\"},\"query\":\"query productClientOnlyProduct($storeId: String, $zipCode: String, $itemId: String!, $dataSource: String, $loyaltyMembershipInput: LoyaltyMembershipInput, $skipSpecificationGroup: Boolean = false, $skipSubscribeAndSave: Boolean = false, $skipKPF: Boolean = false) {\\n  product(itemId: $itemId, dataSource: $dataSource, loyaltyMembershipInput: $loyaltyMembershipInput) {\\n    fulfillment(storeId: $storeId, zipCode: $zipCode) {\\n      backordered\\n      fulfillmentOptions {\\n        type\\n        services {\\n          type\\n          locations {\\n            isAnchor\\n            inventory {\\n              isLimitedQuantity\\n              isOutOfStock\\n              isInStock\\n              quantity\\n              isUnavailable\\n              maxAllowedBopisQty\\n              minAllowedBopisQty\\n              __typename\\n            }\\n            type\\n            storeName\\n            locationId\\n            curbsidePickupFlag\\n            isBuyInStoreCheckNearBy\\n            distance\\n            state\\n            storePhone\\n            __typename\\n          }\\n          deliveryTimeline\\n          deliveryDates {\\n            startDate\\n            endDate\\n            __typename\\n          }\\n          deliveryCharge\\n          dynamicEta {\\n            hours\\n            minutes\\n            __typename\\n          }\\n          hasFreeShipping\\n          freeDeliveryThreshold\\n          totalCharge\\n          __typename\\n        }\\n        fulfillable\\n        __typename\\n      }\\n      anchorStoreStatus\\n      anchorStoreStatusType\\n      backorderedShipDate\\n      bossExcludedShipStates\\n      sthExcludedShipState\\n      bossExcludedShipState\\n      excludedShipStates\\n      seasonStatusEligible\\n      onlineStoreStatus\\n      onlineStoreStatusType\\n      inStoreAssemblyEligible\\n      __typename\\n    }\\n    info {\\n      dotComColorEligible\\n      hidePrice\\n      ecoRebate\\n      quantityLimit\\n      sskMin\\n      sskMax\\n      unitOfMeasureCoverage\\n      wasMaxPriceRange\\n      wasMinPriceRange\\n      fiscalYear\\n      productDepartment\\n      classNumber\\n      forProfessionalUseOnly\\n      globalCustomConfigurator {\\n        customButtonText\\n        customDescription\\n        customExperience\\n        customExperienceUrl\\n        customTitle\\n        __typename\\n      }\\n      paintBrand\\n      movingCalculatorEligible\\n      label\\n      prop65Warning\\n      returnable\\n      recommendationFlags {\\n        visualNavigation\\n        reqItems\\n        batItems\\n        packages\\n        __typename\\n      }\\n      replacementOMSID\\n      hasSubscription\\n      minimumOrderQuantity\\n      projectCalculatorEligible\\n      subClassNumber\\n      calculatorType\\n      isLiveGoodsProduct\\n      protectionPlanSku\\n      hasServiceAddOns\\n      consultationType\\n      __typename\\n    }\\n    itemId\\n    dataSources\\n    identifiers {\\n      canonicalUrl\\n      brandName\\n      itemId\\n      modelNumber\\n      productLabel\\n      storeSkuNumber\\n      upcGtin13\\n      specialOrderSku\\n      toolRentalSkuNumber\\n      rentalCategory\\n      rentalSubCategory\\n      upc\\n      productType\\n      isSuperSku\\n      parentId\\n      roomVOEnabled\\n      sampleId\\n      __typename\\n    }\\n    availabilityType {\\n      discontinued\\n      status\\n      type\\n      buyable\\n      __typename\\n    }\\n    details {\\n      description\\n      collection {\\n        url\\n        collectionId\\n        __typename\\n      }\\n      highlights\\n      descriptiveAttributes {\\n        name\\n        value\\n        bulleted\\n        sequence\\n        __typename\\n      }\\n      infoAndGuides {\\n        name\\n        url\\n        __typename\\n      }\\n      installation {\\n        leadGenUrl\\n        __typename\\n      }\\n      __typename\\n    }\\n    media {\\n      images {\\n        url\\n        type\\n        subType\\n        sizes\\n        __typename\\n      }\\n      video {\\n        shortDescription\\n        thumbnail\\n        url\\n        videoStill\\n        link {\\n          text\\n          url\\n          __typename\\n        }\\n        title\\n        type\\n        videoId\\n        longDescription\\n        __typename\\n      }\\n      threeSixty {\\n        id\\n        url\\n        __typename\\n      }\\n      augmentedRealityLink {\\n        usdz\\n        image\\n        __typename\\n      }\\n      richContent {\\n        content\\n        displayMode\\n        richContentSource\\n        __typename\\n      }\\n      __typename\\n    }\\n    pricing(storeId: $storeId) {\\n      promotion {\\n        dates {\\n          end\\n          start\\n          __typename\\n        }\\n        type\\n        description {\\n          shortDesc\\n          longDesc\\n          __typename\\n        }\\n        dollarOff\\n        percentageOff\\n        savingsCenter\\n        savingsCenterPromos\\n        specialBuySavings\\n        specialBuyDollarOff\\n        specialBuyPercentageOff\\n        experienceTag\\n        subExperienceTag\\n        anchorItemList\\n        itemList\\n        reward {\\n          tiers {\\n            minPurchaseAmount\\n            minPurchaseQuantity\\n            rewardPercent\\n            rewardAmountPerOrder\\n            rewardAmountPerItem\\n            rewardFixedPrice\\n            __typename\\n          }\\n          __typename\\n        }\\n        nvalues\\n        brandRefinementId\\n        __typename\\n      }\\n      value\\n      alternatePriceDisplay\\n      alternate {\\n        bulk {\\n          pricePerUnit\\n          thresholdQuantity\\n          value\\n          __typename\\n        }\\n        unit {\\n          caseUnitOfMeasure\\n          unitsOriginalPrice\\n          unitsPerCase\\n          value\\n          __typename\\n        }\\n        __typename\\n      }\\n      original\\n      mapAboveOriginalPrice\\n      message\\n      preferredPriceFlag\\n      specialBuy\\n      unitOfMeasure\\n      __typename\\n    }\\n    reviews {\\n      ratingsReviews {\\n        averageRating\\n        totalReviews\\n        __typename\\n      }\\n      __typename\\n    }\\n    seo {\\n      seoKeywords\\n      seoDescription\\n      __typename\\n    }\\n    specificationGroup @skip(if: $skipSpecificationGroup) {\\n      specifications {\\n        specName\\n        specValue\\n        __typename\\n      }\\n      specTitle\\n      __typename\\n    }\\n    taxonomy {\\n      breadCrumbs {\\n        label\\n        url\\n        browseUrl\\n        creativeIconUrl\\n        deselectUrl\\n        dimensionName\\n        refinementKey\\n        __typename\\n      }\\n      brandLinkUrl\\n      __typename\\n    }\\n    favoriteDetail {\\n      count\\n      __typename\\n    }\\n    sizeAndFitDetail {\\n      attributeGroups {\\n        attributes {\\n          attributeName\\n          dimensions\\n          __typename\\n        }\\n        dimensionLabel\\n        productType\\n        __typename\\n      }\\n      __typename\\n    }\\n    subscription @skip(if: $skipSubscribeAndSave) {\\n      defaultfrequency\\n      discountPercentage\\n      subscriptionEnabled\\n      __typename\\n    }\\n    badges(storeId: $storeId) {\\n      label\\n      color\\n      creativeImageUrl\\n      endDate\\n      message\\n      name\\n      timerDuration\\n      timer {\\n        timeBombThreshold\\n        daysLeftThreshold\\n        dateDisplayThreshold\\n        message\\n        __typename\\n      }\\n      __typename\\n    }\\n    keyProductFeatures @skip(if: $skipKPF) {\\n      keyProductFeaturesItems {\\n        features {\\n          name\\n          refinementId\\n          refinementUrl\\n          value\\n          __typename\\n        }\\n        __typename\\n      }\\n      __typename\\n    }\\n    seoDescription\\n    installServices {\\n      scheduleAMeasure\\n      __typename\\n    }\\n    dataSource\\n    __typename\\n  }\\n}\\n\"}";
 
-            String variationButtonString = CrawlerUtils.scrapStringSimpleInfo(this.currentDoc, ".super-sku__inline-attribute-wrapper div:nth-child(" + this.VARIATION + ") button", false);
-            WebElement variationButton;
+      Request request = Request.RequestBuilder.create()
+         .setUrl(url)
+         .setHeaders(headers)
+         .setCookies(cookies)
+         .setPayload(payload)
+         .setProxyservice(List.of(ProxyCollection.NETNUT_RESIDENTIAL_US_HAPROXY, ProxyCollection.NETNUT_RESIDENTIAL_MX_HAPROXY))
+         .build();
 
-            if (variationButtonString != null) {
-               variationButton = webdriver.driver.findElement(
-                  By.cssSelector(".super-sku__inline-attribute-wrapper div:nth-child(" + this.VARIATION + ") button"));
-            } else {
-               variationButton = webdriver.driver.findElement(
-                  By.cssSelector(".super-sku__inline-attribute-wrapper button:nth-child(" + this.VARIATION + ")"));
+      Response response = this.dataFetcher.post(session, request);
+
+      if (!response.isSuccess()) {
+         response = new JsoupDataFetcher().get(session, request);
+
+         if (!response.isSuccess()) {
+            int tries = 0;
+            while (!response.isSuccess() && tries < 3) {
+               tries++;
+               if (tries % 2 == 0) {
+                  response = new JsoupDataFetcher().post(session, request);
+               } else {
+                  response = this.dataFetcher.post(session, request);
+               }
             }
-
-            webdriver.clickOnElementViaJavascript(variationButton);
-            webdriver.waitLoad(70000);
-
-            this.CURRENT_URL = webdriver.getCurURL();
          }
-         doc = Jsoup.parse(webdriver.getCurrentPageSource());
-
-      } catch (Exception e) {
-         Logging.printLogInfo(logger, session, CommonMethods.getStackTrace(e));
-         webdriver.terminate();
       }
-      return doc;
+
+         return response;
+   }
+
+   private String getProductIdFromUrl() {
+      String productId = CommonMethods.getLast(session.getOriginalURL().split("/"));
+      if (productId.contains("?")) {
+         productId = productId.split("\\?")[0];
+      }
+         return productId;
    }
 
    @Override
@@ -242,7 +267,7 @@ public class UnitedStatesTheHomeDepotCrawler extends Crawler {
 
    private Document fetchVariation(Document doc) {
       if (this.VARIATION != 1) {
-            doc = fetch();
+         doc = null;
       }
       this.VARIATION++;
       return doc;
@@ -265,7 +290,7 @@ public class UnitedStatesTheHomeDepotCrawler extends Crawler {
       return variations;
    }
 
-   JSONObject scrapJsonFromHtml (Document doc) {
+   JSONObject scrapJsonFromHtml(Document doc) {
       JSONObject productJson = CrawlerUtils.selectJsonFromHtml(doc, "script[data-th=\"server\"]", null, null, false, false);
 
       if (productJson == null) {
@@ -346,8 +371,8 @@ public class UnitedStatesTheHomeDepotCrawler extends Crawler {
       Elements breadcrumbs = doc.select("[name=\"breadcrumbs\"] a");
 
       for (Element category : breadcrumbs) {
-         String categoryName = CrawlerUtils.scrapStringSimpleInfo(category,"a", false);
-          if (categoryName != null)  categories.add(categoryName);
+         String categoryName = CrawlerUtils.scrapStringSimpleInfo(category, "a", false);
+         if (categoryName != null) categories.add(categoryName);
       }
 
       return categories;
