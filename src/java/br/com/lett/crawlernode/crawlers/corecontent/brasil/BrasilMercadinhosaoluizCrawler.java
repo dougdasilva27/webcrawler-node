60fa2e03d7c14e34bbe0cd4ba26d4edeb3806f8b
Lua <danielsantos@lett.digital>
2022-04-25 13:21:20 -0300

    New Crawler

---
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilMercadinhosaoluizCrawler.java
@@ -0,0 +1,129 @@
+package br.com.lett.crawlernode.crawlers.corecontent.brasil;
+
+import br.com.lett.crawlernode.core.models.Card;
+import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.models.ProductBuilder;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.Crawler;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.Logging;
+import com.google.common.collect.Sets;
+import exceptions.MalformedPricingException;
+import exceptions.OfferException;
+import models.Offer;
+import models.Offers;
+import models.pricing.*;
+import org.jsoup.nodes.Document;
+import models.pricing.CreditCards;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+public class BrasilMercadinhosaoluizCrawler extends Crawler {
+   private static final Set<String> cards = Sets.newHashSet(Card.VISA.toString(), Card.MASTERCARD.toString(),
+      Card.DINERS.toString(), Card.AMEX.toString(), Card.ELO.toString());
+
+   public BrasilMercadinhosaoluizCrawler(Session session) {
+      super(session);
+   }
+
+   public List<Product> extractInformation(Document doc) throws Exception {
+      super.extractInformation(doc);
+      List<Product> products = new ArrayList<>();
+
+      if (isProductPage(doc)) {
+         Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
+
+         String internalId = CrawlerUtils.scrapStringSimpleInfoByAttribute(doc, "#___rc-p-id", "value");
+         String name = CrawlerUtils.scrapStringSimpleInfo(doc, ".product__head > h2 > .productName", false);
+         String description = CrawlerUtils.scrapStringSimpleInfo(doc, ".infos-product > .shell > .desc_product > .productDescription", false);
+         String primaryImage = CrawlerUtils.scrapSimplePrimaryImage(doc, "#image-main", Arrays.asList("src"), "http://", "www.mercadinhossaoluiz.com.br");
+         boolean available = true;
+         //doc.selectFirst("") != null
+         Offers offers = available ? scrapOffers(doc) : new Offers();
+
+         Product product = ProductBuilder.create()
+            .setUrl(session.getOriginalURL())
+            .setInternalId(internalId)
+            .setInternalPid(internalId)
+            .setName(name)
+            .setPrimaryImage(primaryImage)
+            .setDescription(description)
+            .setOffers(offers)
+            .build();
+
+         products.add(product);
+
+
+      } else {
+         Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
+      }
+      return products;
+   }
+
+   private boolean isProductPage(Document doc) {
+      return doc.selectFirst(".section-product") != null;
+   }
+
+   private Offers scrapOffers(Document doc) throws OfferException, MalformedPricingException {
+      Offers offers = new Offers();
+      Pricing pricing = scrapPricing(doc);
+
+      if (pricing != null) {
+         offers.add(Offer.OfferBuilder.create()
+            .setUseSlugNameAsInternalSellerId(true)
+            .setSellerFullName("Mercadinho São Luiz")
+            .setSellersPagePosition(1)
+            .setIsBuybox(false)
+            .setIsMainRetailer(true)
+            .setPricing(pricing)
+            .build());
+      }
+
+      return offers;
+   }
+
+   private Pricing scrapPricing(Document doc) throws MalformedPricingException {
+      Double priceFrom = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".product__price > div > .descricao-preco > .valor-de > .skuListPrice", null, true, ',', session);
+      Double spotlightPrice = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".product__price > div > .descricao-preco > .valor-por > .skuBestPrice", null, true, ',', session);
+
+      if (spotlightPrice == null){
+         spotlightPrice = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".product__price > div > .descricao-preco > .valor-de > .skuBestPrice", "skuListPrice", false, ',', session);
+      }
+
+      BankSlip bankSlip = CrawlerUtils.setBankSlipOffers(spotlightPrice, null);
+      CreditCards creditCards = scrapCreditCards(spotlightPrice);
+
+      return Pricing.PricingBuilder.create()
+         .setSpotlightPrice(spotlightPrice)
+         .setPriceFrom(priceFrom)
+         .setCreditCards(creditCards)
+         .setBankSlip(bankSlip)
+         .build();
+   }
+   private CreditCards scrapCreditCards(Double spotlightPrice) throws MalformedPricingException {
+      CreditCards creditCards = new CreditCards();
+
+      Installments installments = new Installments();
+      if (installments.getInstallments().isEmpty()) {
+         installments.add(Installment.InstallmentBuilder.create()
+            .setInstallmentNumber(1)
+            .setInstallmentPrice(spotlightPrice)
+            .build());
+      }
+
+      for (String card : cards) {
+         creditCards.add(CreditCard.CreditCardBuilder.create()
+            .setBrand(card)
+            .setInstallments(installments)
+            .setIsShopCard(false)
+            .build());
+      }
+
+      return creditCards;
+
+   }
+}
+
---
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilMercadinhosaoluizCrawler.java
@@ -0,0 +1,72 @@
+package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
+
+import br.com.lett.crawlernode.core.models.RankingProduct;
+import br.com.lett.crawlernode.core.models.RankingProductBuilder;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.exceptions.MalformedProductException;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+
+public class BrasilMercadinhosaoluizCrawler extends CrawlerRankingKeywords {
+   public BrasilMercadinhosaoluizCrawler(Session session) {
+      super(session);
+   }
+
+   @Override
+   protected void extractProductsFromCurrentPage() throws UnsupportedEncodingException, MalformedProductException {
+
+      this.pageSize = 8;
+
+      this.log("Página " + this.currentPage);
+
+      String url = "https://www.mercadinhossaoluiz.com.br/buscapagina?ft=" + this.keywordEncoded + "&PS=8&sl=6c852839-4fab-453f-a4d0-e7fc54f20719&cc=4&sm=0&PageNumber=" + currentPage;
+
+      this.log("Link onde são feitos os crawlers: " + url);
+
+      this.currentDoc = fetchDocument(url);
+
+      Elements products = this.currentDoc.select(".vitrine.prateleira.n4colunas >  ul > li:nth-child(odd)");
+
+      if (!products.isEmpty()) {
+         for (Element e : products) {
+            String internalId = CrawlerUtils.scrapStringSimpleInfoByAttribute(e, ".btn-like.js-btn-like", "id");
+            String productUrl = CrawlerUtils.scrapUrl(e, ".product__content > .product__head > h4 > a", "href", "https:", url);
+            String name = CrawlerUtils.scrapStringSimpleInfo(e, ".product__content > .product__head > h4 > a", true);
+            String imageUrl = CrawlerUtils.scrapSimplePrimaryImage(e, ".product__image > figure > .x-image-default > img", Arrays.asList("src"), "https:", url);
+            Integer price = CrawlerUtils.scrapPriceInCentsFromHtml(e, ".product__content > .product__price > .best-price", null, true, ',', session, null);
+            boolean isAvailable = price != null;
+
+            RankingProduct productRanking = RankingProductBuilder.create()
+               .setUrl(productUrl)
+               .setInternalId(internalId)
+               .setName(name)
+               .setPriceInCents(price)
+               .setAvailability(isAvailable)
+               .setImageUrl(imageUrl)
+               .build();
+
+            saveDataProduct(productRanking);
+
+            if (this.arrayProducts.size() == productsLimit) {
+               break;
+            }
+
+         }
+      } else {
+         this.result = false;
+         this.log("Keyword sem resultado!");
+      }
+
+      this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
+
+   }
+   @Override
+   protected boolean hasNextPage() {
+      return this.currentDoc.selectFirst(".vitrine.prateleira.n4colunas >  ul > li:nth-child(odd)") != null;
+   }
+}
