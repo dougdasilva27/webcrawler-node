6e693c0eeb904ec37c194f4857096b9b25e0de7a
Thainá Jardim <67081271+ThainaJardim@users.noreply.github.com>
2022-04-25 15:53:51 -0300

    add queue map

--- src/java/br/com/lett/crawlernode/aws/sqs/QueueService.java
+++ src/java/br/com/lett/crawlernode/aws/sqs/QueueService.java
@@ -73,6 +73,7 @@ public class QueueService {
       queueURLMap.put(QueueName.PRODUCT_IMAGE_DOWNLOAD_SEED.toString(), QUEUE_URL + QueueName.PRODUCT_IMAGE_DOWNLOAD_SEED.toString());
       queueURLMap.put(QueueName.WEB_SCRAPER_PRODUCT_ZE_DELIVERY.toString(), QUEUE_URL + QueueName.WEB_SCRAPER_PRODUCT_ZE_DELIVERY.toString());
       queueURLMap.put(QueueName.WEB_SCRAPER_PRODUCT_AMAZON_WD.toString(), QUEUE_URL + QueueName.WEB_SCRAPER_PRODUCT_AMAZON_WD.toString());
+      queueURLMap.put(QueueName.WEB_SCRAPER_PRODUCT_DEV.toString(), QUEUE_URL + QueueName.WEB_SCRAPER_PRODUCT_DEV.toString());
 
    }
 
