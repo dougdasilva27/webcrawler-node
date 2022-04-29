6761fc3ea4a4b41a2df768a38e57f4e0358b2ee2
Lua <danielsantos@lett.digital>
2022-04-29 10:31:56 -0300

    Bug correction

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/colombia/ColombiasurtiappbogotaCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/colombia/ColombiasurtiappbogotaCrawler.java
@@ -1,5 +1,9 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.colombia;
 
+import br.com.lett.crawlernode.core.fetcher.DynamicDataFetcher;
+import br.com.lett.crawlernode.core.fetcher.FetchMode;
+import br.com.lett.crawlernode.core.fetcher.ProxyCollection;
+import br.com.lett.crawlernode.core.fetcher.methods.FetcherDataFetcher;
 import br.com.lett.crawlernode.core.fetcher.methods.JsoupDataFetcher;
 import br.com.lett.crawlernode.core.fetcher.models.Request;
 import br.com.lett.crawlernode.core.fetcher.models.Response;
@@ -10,65 +14,39 @@ import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
 import br.com.lett.crawlernode.exceptions.MalformedProductException;
 import br.com.lett.crawlernode.util.CommonMethods;
 import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.Logging;
+import org.apache.http.impl.cookie.BasicClientCookie;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import software.amazon.awssdk.http.Header;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ColombiasurtiappbogotaCrawler extends CrawlerRankingKeywords {
-
-
+   private int login = 0;
+   List<String> proxies = Arrays.asList(ProxyCollection.NETNUT_RESIDENTIAL_CO_HAPROXY, ProxyCollection.NETNUT_RESIDENTIAL_MX_HAPROXY, ProxyCollection.NETNUT_RESIDENTIAL_BR_HAPROXY);
    @Override
    protected void processBeforeFetch() {
-      Request requestCookies = Request.RequestBuilder.create()
-         .setUrl("https://tienda.surtiapp.com.co/Security/Login")
-         .build();
-
-      Response responseCookies = dataFetcher.get(session, requestCookies);
-
-      this.cookies.addAll(responseCookies.getCookies());
-
-      Document document = Jsoup.parse(responseCookies.getBody());
-
-      String requestVerificationToken = CrawlerUtils.scrapStringSimpleInfoByAttribute(document, "input[name=\"__RequestVerificationToken\"]", "value");
-
-      Map<String, String> headers = new HashMap<>();
-
-      headers.put("RequestVerificationToken", requestVerificationToken);
-      headers.put(Header.CONTENT_TYPE, "application/x-www-form-urlencoded");
-      headers.put("Cookie", CommonMethods.cookiesToString(responseCookies.getCookies()));
-
-      String payload = "email=" +
-         session.getOptions().optString("email", "") +
-         "&password=" +
-         session.getOptions().optString("password", "");
-
-
-      Request requestLogin = Request.RequestBuilder.create()
-         .setUrl("https://tienda.surtiapp.com.co/Security/Login?handler=Authenticate")
-         .setHeaders(headers)
-         .setPayload(payload)
-         .build();
-
-      new JsoupDataFetcher().post(session, requestLogin);
-
-
    }
 
    @Override
    protected Document fetchDocument(String url) {
 
-      Map<String, String> headers = new HashMap<>();
-      headers.put("Cookie", CommonMethods.cookiesToString(this.cookies));
       Request request = Request.RequestBuilder.create()
          .setUrl(url)
-         .setHeaders(headers)
+         .setCookies(this.cookies)
+         .setProxyservice(proxies)
          .build();
-      String html = this.dataFetcher.get(session, request).getBody();
+      String html = new JsoupDataFetcher().get(session, request).getBody();
 
       return Jsoup.parse(html);
    }
@@ -76,17 +54,21 @@ public class ColombiasurtiappbogotaCrawler extends CrawlerRankingKeywords {
 
    public ColombiasurtiappbogotaCrawler(Session session) {
       super(session);
+      super.fetchMode = FetchMode.FETCHER;
    }
 
    @Override
    protected void extractProductsFromCurrentPage() throws MalformedProductException {
+      if (login == 0){
+         getCookiesLogin();
+      }
       this.pageSize = 25;
       this.log("Página " + this.currentPage);
 
-      String url = "https://tienda.surtiapp.com.co/Store/SearchResults/" + this.keywordEncoded;
+      String url = "https://tienda.surtiapp.com.co/WithoutLoginB2B/Store/SearchResults/" + this.keywordEncoded;
 
       this.log("Link onde são feitos os crawlers: " + url);
-      this.currentDoc = fetchDocument(url);
+      this.currentDoc = fetchDocument(url, this.cookies);
 
       Elements products = this.currentDoc.select(".product-card.product-id-contaniner");
 
@@ -99,7 +81,6 @@ public class ColombiasurtiappbogotaCrawler extends CrawlerRankingKeywords {
             int price = CrawlerUtils.scrapIntegerFromHtml(e, ".product-card__price", true, 0);
             boolean isAvailable = price != 0;
 
-            //New way to send products to save data product
             RankingProduct productRanking = RankingProductBuilder.create()
                .setUrl(productUrl)
                .setInternalId(internalId)
@@ -125,6 +106,37 @@ public class ColombiasurtiappbogotaCrawler extends CrawlerRankingKeywords {
 
       this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora "
          + this.arrayProducts.size() + " produtos crawleados");
+   }
+
+   public static void waitForElement(WebDriver driver, String cssSelector) {
+      WebDriverWait wait = new WebDriverWait(driver, 20);
+      wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(cssSelector)));
+   }
+
+   public void getCookiesLogin() {
+      Map<String, String> Headers = new HashMap<>();
+      Headers.put("Content-Type","application/x-www-form-urlencoded; charset=UTF-8");
+      Request request = Request.RequestBuilder.create()
+         .setUrl("https://tienda.surtiapp.com.co/WithoutLoginB2B/Store/ProductDetail/d7e53433-89a4-ec11-a99b-00155d30fb1f")
+         .setHeaders(Headers)
+         .mustSendContentEncoding(true)
+         .build();
+      Response responseApi = new JsoupDataFetcher().get(session, request);
+      Document document = Jsoup.parse(responseApi.getBody());
+
+      String verificationToken = CrawlerUtils.scrapStringSimpleInfoByAttribute(document, ".mh__search-bar-wrapper > form > input[type=hidden]", "value");
+      Headers.put("RequestVerificationToken", verificationToken);
+      String payload = "username=1130409480&password=Lett12345.&isMobileLogin=false&RedirectTo=";
+      Request requestLogin = Request.RequestBuilder.create()
+         .setHeaders(Headers)
+         .setCookies(responseApi.getCookies())
+         .setPayload(payload)
+         .mustSendContentEncoding(true)
+         .setUrl("https://tienda.surtiapp.com.co/WithoutLoginB2B/Security/UserAccount?handler=Authenticate")
+         .build();
+      Response responseApiLogin = new JsoupDataFetcher().post(session, requestLogin);
+       this.cookies.addAll(responseApiLogin.getCookies());
+      login ++;
    }
 
 }
