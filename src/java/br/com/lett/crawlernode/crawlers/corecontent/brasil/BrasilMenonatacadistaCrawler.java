e00e82608d88d8dd8e4d89765c55b98fbdc3ddcf
Luiza <lrsofal@sga.pucminas.br>
2022-05-11 13:45:34 -0300

    New Crawler Menon Atacadista

---
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilMenonatacadistaCrawler.java
@@ -0,0 +1,231 @@
+package br.com.lett.crawlernode.crawlers.corecontent.brasil;
+
+import br.com.lett.crawlernode.core.fetcher.ProxyCollection;
+import br.com.lett.crawlernode.core.fetcher.methods.ApacheDataFetcher;
+import br.com.lett.crawlernode.core.fetcher.methods.FetcherDataFetcher;
+import br.com.lett.crawlernode.core.fetcher.models.Request;
+import br.com.lett.crawlernode.core.fetcher.models.Response;
+import br.com.lett.crawlernode.core.models.Card;
+import br.com.lett.crawlernode.core.models.CategoryCollection;
+import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.models.ProductBuilder;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.Crawler;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.Logging;
+import com.google.common.collect.Sets;
+import exceptions.MalformedPricingException;
+import exceptions.OfferException;
+import models.Offer;
+import models.Offers;
+import models.pricing.*;
+import org.apache.http.HttpHeaders;
+import org.apache.http.cookie.Cookie;
+import org.json.JSONArray;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class BrasilMenonatacadistaCrawler extends Crawler {
+
+   private final String HOME_PAGE = "https://www.menonatacadista.com.br/";
+   private static final String SELLER_FULL_NAME = "Menon Atacadista";
+   protected Set<String> cards = Sets.newHashSet(Card.VISA.toString(), Card.MASTERCARD.toString(),
+      Card.ELO.toString(), Card.AMEX.toString(), Card.DINERS.toString(), Card.DISCOVER.toString(),
+      Card.JCB.toString(), Card.AURA.toString(), Card.HIPERCARD.toString());
+
+   public BrasilMenonatacadistaCrawler(Session session) {
+      super(session);
+   }
+
+   private String cookiePHPSESSID = null;
+
+   private void loginMasterAccount() {
+      Map<String, String> headers = new HashMap<>();
+      headers.put("Content-Type", "application/x-www-form-urlencoded");
+      headers.put("authority","www.menonatacadista.com.br");
+      String payloadString = "email=paulo.carvalho%40mdlz.com&password=c9d59";
+
+      Request request = Request.RequestBuilder.create()
+         .setUrl("https://www.menonatacadista.com.br/index.php?route=account/login")
+         .setPayload(payloadString)
+         .setHeaders(headers)
+         .setProxyservice(Arrays.asList(
+            ProxyCollection.BUY_HAPROXY,
+            ProxyCollection.BONANZA,
+            ProxyCollection.LUMINATI_SERVER_BR_HAPROXY,
+            ProxyCollection.NETNUT_RESIDENTIAL_BR
+         ))
+         .build();
+
+      Response response = new FetcherDataFetcher().post(session, request);
+
+      List<Cookie> cookiesResponse = response.getCookies();
+
+      for (Cookie cookieResponse : cookiesResponse) {
+         if (cookieResponse.getName().equalsIgnoreCase("PHPSESSID")) {
+            this.cookiePHPSESSID = cookieResponse.getValue();
+         }
+      }
+   }
+
+   @Override
+   public void handleCookiesBeforeFetch() {
+      loginMasterAccount();
+
+      Map<String, String> headers = new HashMap<>();
+      headers.put("Cookie", "PHPSESSID=" + this.cookiePHPSESSID + ";");
+
+      Request request = Request.RequestBuilder.create()
+         .setUrl("https://www.menonatacadista.com.br/")
+         .setHeaders(headers)
+         .setProxyservice(Arrays.asList(
+            ProxyCollection.BUY_HAPROXY,
+            ProxyCollection.BONANZA,
+            ProxyCollection.LUMINATI_SERVER_BR_HAPROXY,
+            ProxyCollection.NETNUT_RESIDENTIAL_BR
+         ))
+         .build();
+
+      CrawlerUtils.retryRequest(request, session, dataFetcher);
+   }
+
+   @Override
+   protected Response fetchResponse() {
+
+      Map<String, String> headers = new HashMap<>();
+      headers.put("Cookie", "PHPSESSID=" + this.cookiePHPSESSID + ";");
+
+      Request request = Request.RequestBuilder.create()
+         .setUrl("https://www.menonatacadista.com.br/")
+         .setHeaders(headers)
+         .setProxyservice(Arrays.asList(
+            ProxyCollection.BUY_HAPROXY,
+            ProxyCollection.BONANZA,
+            ProxyCollection.LUMINATI_SERVER_BR_HAPROXY,
+            ProxyCollection.NETNUT_RESIDENTIAL_BR
+         ))
+         .setHeaders(headers)
+         .build();
+
+      return new ApacheDataFetcher().get(session, request);
+   }
+
+   public List<Product> extractInformation(Document doc) throws Exception {
+      super.extractInformation(doc);
+      List<Product> products = new ArrayList<>();
+
+      if (isProductPage(doc)) {
+         Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
+
+         String code = CrawlerUtils.scrapStringSimpleInfo(doc, "div.product-detail-info small",true);
+         String internalId = getcodeId(code);
+         String name = CrawlerUtils.scrapStringSimpleInfo(doc, "h1.product-title", true);
+         CategoryCollection categories = CrawlerUtils.crawlCategories(doc, "ul.breadcrumb a", false);
+         JSONArray imagesArray = CrawlerUtils.crawlArrayImagesFromScriptMagento(doc);
+         String primaryImage = CrawlerUtils.scrapSimplePrimaryImage(doc, "a.thumbnail img", Arrays.asList("src"), "https", "www.menonatacadista.com.br");
+         //List<String> secondaryImage = CrawlerUtils.scrapSecondaryImagesMagentoList(imagesArray, primaryImage);
+         String description = CrawlerUtils.scrapSimpleDescription(doc, Arrays.asList("div.text"));
+         boolean availableToBuy = !doc.select("span.product-price .price").isEmpty();
+         Offers offers = availableToBuy ? scrapOffers(doc) : new Offers();
+
+         Product product = ProductBuilder.create()
+            .setUrl(session.getOriginalURL())
+            .setInternalId(internalId)
+            .setInternalPid(internalId)
+            .setName(name)
+            .setCategories(categories)
+            .setPrimaryImage(primaryImage)
+
+            .setDescription(description)
+
+            .build();
+
+         products.add(product);
+
+      } else {
+         Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
+      }
+
+      return products;
+   }
+
+   private boolean isProductPage(Document doc) {
+      return doc.selectFirst("div.product-detail-info") != null;
+   }
+
+   private Offers scrapOffers(Document doc) throws OfferException, MalformedPricingException {
+      Offers offers = new Offers();
+      Pricing pricing = scrapPricing(doc);
+      List<String> sales = scrapSales(pricing);
+
+      offers.add(Offer.OfferBuilder.create()
+         .setUseSlugNameAsInternalSellerId(true)
+         .setSellerFullName(SELLER_FULL_NAME)
+         .setMainPagePosition(1)
+         .setSales(sales)
+         .setIsBuybox(false)
+         .setIsMainRetailer(true)
+         .setPricing(pricing)
+         .build());
+
+      return offers;
+
+   }
+
+   private Pricing scrapPricing(Document doc) throws MalformedPricingException {
+      Double spotlightPrice = CrawlerUtils.scrapDoublePriceFromHtml(doc, "span.product-price .price", null, true, ',', session);
+      Double priceFrom = CrawlerUtils.scrapDoublePriceFromHtml(doc, "span[class*=Information_discounted]", null, true, ',', session);
+      CreditCards creditCards = scrapCreditCards(spotlightPrice);
+
+      return Pricing.PricingBuilder.create()
+         .setSpotlightPrice(spotlightPrice)
+         .setPriceFrom(priceFrom)
+         .setCreditCards(creditCards)
+         .build();
+   }
+
+   private CreditCards scrapCreditCards(Double spotlightPrice) throws MalformedPricingException {
+      CreditCards creditCards = new CreditCards();
+
+      Installments installments = new Installments();
+      installments.add(Installment.InstallmentBuilder.create()
+         .setInstallmentNumber(1)
+         .setInstallmentPrice(spotlightPrice)
+         .build());
+
+
+      for (String card : cards) {
+         creditCards.add(CreditCard.CreditCardBuilder.create()
+            .setBrand(card)
+            .setInstallments(installments)
+            .setIsShopCard(false)
+            .build());
+      }
+
+      return creditCards;
+   }
+
+   private List<String> scrapSales(Pricing pricing) {
+      List<String> sales = new ArrayList<>();
+
+      String saleDiscount = CrawlerUtils.calculateSales(pricing);
+
+      if (saleDiscount != null) {
+         sales.add(saleDiscount);
+      }
+
+      return sales;
+   }
+
+   private String getcodeId(String code) {
+      String regex = "[0-9]+";
+
+      Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+      Matcher matcher = pattern.matcher(code);
+      return String.valueOf(matcher);
+   }
+}
---
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilMenonatacadistaCrawler.java
@@ -0,0 +1,163 @@
+package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
+
+import br.com.lett.crawlernode.core.fetcher.ProxyCollection;
+import br.com.lett.crawlernode.core.fetcher.methods.ApacheDataFetcher;
+import br.com.lett.crawlernode.core.fetcher.methods.FetcherDataFetcher;
+import br.com.lett.crawlernode.core.fetcher.models.Request;
+import br.com.lett.crawlernode.core.fetcher.models.Response;
+import br.com.lett.crawlernode.core.models.RankingProduct;
+import br.com.lett.crawlernode.core.models.RankingProductBuilder;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.exceptions.MalformedProductException;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+import org.apache.http.HttpHeaders;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.cookie.BasicClientCookie;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class BrasilMenonatacadistaCrawler extends CrawlerRankingKeywords {
+   public BrasilMenonatacadistaCrawler(Session session) {
+      super(session);
+   }
+
+   private String cookiePHPSESSID = null;
+
+   private void loginMasterAccount() {
+      Map<String, String> headers = new HashMap<>();
+      headers.put("Content-Type", "application/x-www-form-urlencoded");
+      headers.put("authority","www.menonatacadista.com.br");
+      String payloadString = "email=paulo.carvalho%40mdlz.com&password=c9d59";
+
+      Request request = Request.RequestBuilder.create()
+         .setUrl("https://www.menonatacadista.com.br/index.php?route=account/login")
+         .setPayload(payloadString)
+         .setHeaders(headers)
+         .setProxyservice(Arrays.asList(
+            ProxyCollection.BUY_HAPROXY,
+            ProxyCollection.BONANZA,
+            ProxyCollection.LUMINATI_SERVER_BR_HAPROXY,
+            ProxyCollection.NETNUT_RESIDENTIAL_BR
+         ))
+         .build();
+      Response response = CrawlerUtils.retryRequest(request, session, dataFetcher);
+
+      List<Cookie> cookiesResponse = response.getCookies();
+
+      for (Cookie cookieResponse : cookiesResponse) {
+         if (cookieResponse.getName().equalsIgnoreCase("PHPSESSID")) {
+            this.cookiePHPSESSID = cookieResponse.getValue();
+         }
+      }
+   }
+
+   @Override
+   protected void processBeforeFetch() {
+      loginMasterAccount();
+
+      Map<String, String> headers = new HashMap<>();
+      headers.put("Cookie", "PHPSESSID=" + this.cookiePHPSESSID + ";");
+
+      Request request = Request.RequestBuilder.create()
+         .setUrl("https://www.menonatacadista.com.br/")
+         .setHeaders(headers)
+         .setProxyservice(Arrays.asList(
+            ProxyCollection.BUY_HAPROXY,
+            ProxyCollection.BONANZA,
+            ProxyCollection.LUMINATI_SERVER_BR_HAPROXY,
+            ProxyCollection.NETNUT_RESIDENTIAL_BR
+         ))
+         .build();
+
+      CrawlerUtils.retryRequest(request, session, dataFetcher);
+
+      BasicClientCookie cookie = new BasicClientCookie("PHPSESSID", this.cookiePHPSESSID);
+      this.cookies.add(cookie);
+   }
+
+   @Override
+   protected Document fetchDocument(String url, List<Cookie> cookies) {
+      Request request = Request.RequestBuilder
+         .create()
+         .setCookies(cookies)
+         .setUrl(url)
+         .setProxyservice(Arrays.asList(
+            ProxyCollection.BUY_HAPROXY,
+            ProxyCollection.BONANZA,
+            ProxyCollection.LUMINATI_SERVER_BR_HAPROXY,
+            ProxyCollection.NETNUT_RESIDENTIAL_BR
+         ))
+         .build();
+
+      return Jsoup.parse(new ApacheDataFetcher().get(session, request).getBody());
+   }
+
+
+   @Override
+   protected void extractProductsFromCurrentPage() throws MalformedProductException {
+      this.pageSize = 100;
+      this.log("Página " + this.currentPage);
+      String url = "https://www.menonatacadista.com.br/index.php?route=product/search&search="+ this.keywordEncoded+"&page=" + this.currentPage;
+      this.log("Link onde são feitos os crawlers: " + url);
+      this.currentDoc = fetchDocument(url);
+
+      Elements products = this.currentDoc.select("div.products-list-item");
+      pageSize = products.size();
+
+      if (!products.isEmpty()) {
+         if (this.totalProducts == 0) setTotalProducts();
+         for (Element e : products) {
+            String code = CrawlerUtils.scrapStringSimpleInfo(e, "h4.caption small",true);
+            String internalId = getcodeId(code);
+            String productUrl = CrawlerUtils.scrapStringSimpleInfoByAttribute(e, "h4.caption a", "href");
+            String name = CrawlerUtils.scrapStringSimpleInfo(e, "h4.caption a", true);
+            Integer price = CrawlerUtils.scrapPriceInCentsFromHtml(e, "div.qty-price .price", null, false, ',', session, null);
+            String imageUrl = CrawlerUtils.scrapSimplePrimaryImage(e, "div.image img ", Arrays.asList("src"), "https", "www.menonatacadista.com.br");
+            boolean isAvailable = price != null;
+
+            RankingProduct productRanking = RankingProductBuilder.create()
+               .setUrl(productUrl)
+               .setInternalId(internalId)
+               .setInternalPid(internalId)
+               .setImageUrl(imageUrl)
+               .setName(name)
+               .setPriceInCents(price)
+               .setAvailability(isAvailable)
+               .build();
+
+            saveDataProduct(productRanking);
+
+            if (this.arrayProducts.size() == productsLimit) {
+               break;
+            }
+         }
+      } else {
+         this.result = false;
+         this.log("Keyword sem resultado!");
+      }
+
+      this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
+
+   }
+
+   private String getcodeId(String code) {
+      String regex = "[0-9]+";
+
+      Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+      Matcher matcher = pattern.matcher(code);
+      return String.valueOf(matcher);
+   }
+
+
+}
