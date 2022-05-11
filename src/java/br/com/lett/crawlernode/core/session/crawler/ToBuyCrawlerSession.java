f9b4c9df546b6126a6797f04eb245612f3691dee
Thainá Jardim <67081271+ThainaJardim@users.noreply.github.com>
2022-05-11 16:48:21 -0300

    remove extends

--- src/java/br/com/lett/crawlernode/core/session/crawler/ToBuyCrawlerSession.java
+++ src/java/br/com/lett/crawlernode/core/session/crawler/ToBuyCrawlerSession.java
@@ -1,11 +1,78 @@
 package br.com.lett.crawlernode.core.session.crawler;
 
 import br.com.lett.crawlernode.core.models.Market;
+import br.com.lett.crawlernode.core.models.SkuStatus;
 import br.com.lett.crawlernode.core.server.request.Request;
+import br.com.lett.crawlernode.core.session.Session;
 
-public class ToBuyCrawlerSession extends InsightsCrawlerSession {
+public class ToBuyCrawlerSession  extends Session {
+
+   /**
+    * Processed id associated with the sku being crawled
+    */
+   private Long processedId;
+
+   /**
+    * Internal id associated with the sku being crawled
+    */
+   private String internalId;
+
+   /**
+    * Number of readings to prevent a void status
+    */
+   private int voidAttemptsCounter;
+
+   private SkuStatus productStatus;
+
 
    public ToBuyCrawlerSession(Request request, String queueName, Market market) {
       super(request, queueName, market);
+
+      processedId = request.getProcessedId();
+      internalId = request.getInternalId();
+
+      this.voidAttemptsCounter = 0;
    }
+
+   @Override
+   public Long getProcessedId() {
+      return processedId;
+   }
+
+   public void setProcessedId(Long processedId) {
+      this.processedId = processedId;
+   }
+
+   @Override
+   public void incrementVoidAttemptsCounter() {
+      this.voidAttemptsCounter++;
+   }
+
+   @Override
+   public int getVoidAttempts() {
+      return voidAttemptsCounter;
+   }
+
+   @Override
+   public String getInternalId() {
+      return internalId;
+   }
+
+   public void setInternalId(String internalId) {
+      this.internalId = internalId;
+   }
+
+   public void setVoidAttempts(int voidAttempts) {
+      this.voidAttemptsCounter = voidAttempts;
+   }
+
+   public SkuStatus getProductStatus() {
+      return productStatus;
+   }
+
+   public void setProductStatus(SkuStatus productStatus) {
+      this.productStatus = productStatus;
+   }
+
 }
+
