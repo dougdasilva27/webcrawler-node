5e92aac7821bde5ce3e4bfea245358605d5e8479
Caroline <cdsevangelista@inf.ufpel.edu.br>
2022-05-11 18:15:10 -0300

    Getting compra agora cookies from session

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilCompreAgoraCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilCompreAgoraCrawler.java
@@ -142,7 +142,7 @@ public class BrasilCompreAgoraCrawler extends LinxImpulseRanking {
    protected void fetchPrice(String id) {
       String url = "https://www.compra-agora.com/api/productLookup/" + id;
       Map<String, String> headers = new HashMap<>();
-      headers.put("Cookie", "CPL=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJpbmZyYWNvbW1lcmNlLmNvbS5iciIsInN1YiI6IkluZnJhY29tbWVyY2UiLCJhdWQiOiJjb21wcmEtYWdvcmEuY29tIiwiaWF0IjoxNjQ0MjQ5MTY2LCJkYXRhIjp7InVpZCI6IlRvYndGUmdjS3pOckNzZXhuUnozWnk3a3JwNEJ3T2VUTGxpRmtBNzR1czQ9In19.ob8UfpN1WMPTagamtSsoco1cllnNTSRwNeKrbI2Q9oc2HNjUL56VVEUc8vuz_jUtYEQ3RZAJZJLf5vSV_wXT6V9dkrGP2L7BilB3JamV56muHBqOjA3Xhii6qGFQePgAvNoZaCrw75-pIDSwzfqEzXDYoNqxkBQfVvTG-FCKTKtbVZqcTNXAcNjA80dp9AydpkCi712NxGITCnigZVFaktWl3B3NuBFd44oE-qKI5JCp6IwbM0ptgBHjAT8i43AmLsdS_-JCPcBjbBTFgS_27KNQ26dvuuZjdSwzKHYSxvUO2rI7Vz_4TVFHdaNA-MSfPksfzZT_B_HnX7FkC-imoA; PHPSESSID=7hslf6eioumlusjacjolnn8cf1; ccw=2 3 61 94 147; usrfgpt=367359160001371644248354");
+      headers.put("Cookie", session.getOptions().optString("cookie"));
 
       Request request = Request.RequestBuilder.create()
          .setUrl(url)
