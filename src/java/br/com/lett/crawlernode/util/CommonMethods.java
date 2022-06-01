548223367816d24fbdee3a2652c8e8e133263b58
vagner sampaio <vagnersampaio92@gmail.com>
2022-06-01 13:21:14 -0300

    CSI cobassi
    update titulo e descrição

--- src/java/br/com/lett/crawlernode/util/CommonMethods.java
+++ src/java/br/com/lett/crawlernode/util/CommonMethods.java
@@ -560,7 +560,7 @@ public class CommonMethods {
       StringBuilder sb = new StringBuilder();
       for (int i = 0; i < str.length(); i++) {
          char c = str.charAt(i);
-         if (c >= 'A' && c <= 'Z') {
+         if ((c >= 'A' && c <= 'Z' )|| (c >= 'À' && c <= 'Ü') ) {
             sb.append(" ");
          }
          sb.append(c);
