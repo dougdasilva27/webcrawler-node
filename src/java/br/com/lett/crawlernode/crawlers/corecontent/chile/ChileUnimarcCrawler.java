e3f5d6fcadc7bfda12736190231cb4580a58e5d2
vagner sampaio <vagnersampaio92@gmail.com>
2022-06-15 10:08:45 -0300

    unimarc

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/chile/ChileUnimarcCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/chile/ChileUnimarcCrawler.java
@@ -2,8 +2,10 @@ package br.com.lett.crawlernode.crawlers.corecontent.chile;
 
 import br.com.lett.crawlernode.core.fetcher.FetchMode;
 import br.com.lett.crawlernode.core.fetcher.ProxyCollection;
+import br.com.lett.crawlernode.core.fetcher.methods.ApacheDataFetcher;
+import br.com.lett.crawlernode.core.fetcher.methods.FetcherDataFetcher;
+import br.com.lett.crawlernode.core.fetcher.methods.JsoupDataFetcher;
 import br.com.lett.crawlernode.core.fetcher.models.Request;
-import br.com.lett.crawlernode.core.fetcher.models.Response;
 import br.com.lett.crawlernode.core.models.Card;
 import br.com.lett.crawlernode.core.models.CategoryCollection;
 import br.com.lett.crawlernode.core.models.Product;
@@ -14,7 +16,6 @@ import br.com.lett.crawlernode.util.CrawlerUtils;
 import br.com.lett.crawlernode.util.JSONUtils;
 import br.com.lett.crawlernode.util.Logging;
 import com.google.common.collect.Sets;
-import com.google.common.net.HttpHeaders;
 import exceptions.MalformedPricingException;
 import exceptions.OfferException;
 import models.Offer;
@@ -22,7 +23,6 @@ import models.Offers;
 import models.pricing.*;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.util.*;
@@ -40,8 +40,10 @@ public class ChileUnimarcCrawler extends Crawler {
       //super.extractInformation(doc);
       Document doc = getProduct(this.session.getOriginalURL());
       List<Product> products = new ArrayList<>();
-      String script = CrawlerUtils.scrapScriptFromHtml(doc, "#__NEXT_DATA__");
-      JSONArray dataArray = JSONUtils.stringToJsonArray(script);
+      //String script = CrawlerUtils.scrapScriptFromHtml(doc, "#__NEXT_DATA__");
+    //  JSONArray dataArray = JSONUtils.stringToJsonArray(script);
+
+      JSONArray dataArray = CrawlerUtils.selectJsonArrayFromHtml(doc, "#__NEXT_DATA__",null,null, false,false,false);
       if (JSONUtils.getValueRecursive(dataArray, "0.isFallback", Boolean.class)) {
          Logging.printLogDebug(logger, session, "Not a product page" + session.getOriginalURL());
          return products;
@@ -175,20 +177,19 @@ public class ChileUnimarcCrawler extends Crawler {
 
    private Document getProduct(String url) {
 
-      Map<String, String> headers = new HashMap<>();
-      headers.put(HttpHeaders.CONTENT_TYPE,"application/json");
+//      Map<String, String> headers = new HashMap<>();
+//      headers.put(HttpHeaders.,"application/json");
 
       Request request = Request.RequestBuilder.create()
          .setUrl(url)
-
+         .mustSendContentEncoding(false)
+         .setSendUserAgent(false)
          .setProxyservice(Arrays.asList(ProxyCollection.BUY,ProxyCollection.NETNUT_RESIDENTIAL_BR,ProxyCollection.NETNUT_RESIDENTIAL_BR_HAPROXY, ProxyCollection.NETNUT_RESIDENTIAL_AR_HAPROXY))
          .build();
 
-
+      return CrawlerUtils.retryRequestDocument(request, List.of(new ApacheDataFetcher(), new JsoupDataFetcher(), new FetcherDataFetcher()), session);
      // Response response = this.dataFetcher.get(session, request);
-      Response response = CrawlerUtils.retryRequest( request, session, dataFetcher, true);
-
-      return Jsoup.parse(response.getBody());
+      // return CrawlerUtils.retryRequestWithListDataFetcher(request, List.of(new ApacheDataFetcher(), new JsoupDataFetcher(), new FetcherDataFetcher()), session);
 
    }
 
