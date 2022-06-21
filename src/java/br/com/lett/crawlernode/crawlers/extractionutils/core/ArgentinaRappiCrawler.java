b393ead97c3ee76dca72291dac8b284743c75005
Thales Rodrigues <thalessantos30@gmail.com>
2022-06-21 20:38:38 -0300

    overriding fetch token and product methods to properly use argentina api endpoints

--- src/java/br/com/lett/crawlernode/crawlers/extractionutils/core/ArgentinaRappiCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/extractionutils/core/ArgentinaRappiCrawler.java
@@ -1,7 +1,19 @@
 package br.com.lett.crawlernode.crawlers.extractionutils.core;
 
 import br.com.lett.crawlernode.core.fetcher.FetchMode;
+import br.com.lett.crawlernode.core.fetcher.methods.FetcherDataFetcher;
+import br.com.lett.crawlernode.core.fetcher.methods.JsoupDataFetcher;
+import br.com.lett.crawlernode.core.fetcher.models.Request;
+import br.com.lett.crawlernode.core.fetcher.models.Response;
 import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.JSONUtils;
+import org.apache.tika.metadata.HttpHeaders;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ArgentinaRappiCrawler extends RappiCrawler {
 
@@ -9,7 +21,7 @@ public class ArgentinaRappiCrawler extends RappiCrawler {
 
    public ArgentinaRappiCrawler(Session session) {
       super(session);
-      super.config.setFetcher(FetchMode.JSOUP);
+      super.config.setFetcher(FetchMode.APACHE);
    }
 
    @Override
@@ -36,5 +48,82 @@ public class ArgentinaRappiCrawler extends RappiCrawler {
    public boolean shouldVisit() {
       String href = this.session.getOriginalURL().toLowerCase();
       return !FILTERS.matcher(href).matches() && (href.startsWith(HOME_PAGE));
+   }
+
+   @Override
+   protected String fetchToken() {
+      String guestPassportUrl = "https://services." + getHomeDomain() + "/api/rocket/v2/guest/passport/";
+      Map<String, String> headers = new HashMap<>();
+      headers.put("accept", "application/json, text/plain, */*");
+      headers.put("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36");
+      headers.put(HttpHeaders.CONTENT_TYPE, "application/json");
+      headers.put("deviceid", "255a806f-25a2-4026-8584-d63dfe7464b2");
+      headers.put("needAppsFlyerId", "false");
+
+      Request request = Request.RequestBuilder.create()
+         .setUrl(guestPassportUrl)
+         .setHeaders(headers)
+         .mustSendContentEncoding(false)
+         .build();
+
+
+      JSONObject jsonGuestPassport = JSONUtils.stringToJson(this.dataFetcher.get(session, request).getBody());
+
+      String xGuestApiKey = jsonGuestPassport.optString("token");
+
+      String url = "https://services." + getHomeDomain() + "/api/rocket/v2/guest";
+
+      headers.put("x-guest-api-key", xGuestApiKey);
+
+      String payload = "{}";
+
+      request = Request.RequestBuilder.create()
+         .setUrl(url)
+         .setHeaders(headers)
+         .setPayload(payload)
+         .mustSendContentEncoding(false)
+         .build();
+
+      JSONObject json = JSONUtils.stringToJson(this.dataFetcher.post(session, request).getBody());
+
+      String token = json.optString("access_token");
+      String tokenType = json.optString("token_type");
+
+      if (tokenType.equals("Bearer")) {
+         token = tokenType + " " + token;
+      }
+
+      return token;
+   }
+
+   @Override
+   protected JSONObject fetchProduct(String productId, String storeId, String token) {
+      String url = "https://services." + getHomeDomain() + "/api/ms/web-proxy/dynamic-list/cpgs";
+
+      Map<String, String> headers = new HashMap<>();
+      headers.put("accept", "application/json, text/plain, */*");
+      headers.put("language", "es");
+      headers.put("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36");
+      headers.put("content-type", "application/json");
+      headers.put("authorization", token);
+
+      String productFriendlyUrl = storeId + "_" + productId;
+
+      String payload = "{\"dynamic_list_request\":{\"context\":\"product_detail\",\"state\":{\"lat\":\"1\",\"lng\":\"1\"},\"limit\":100,\"offset\":0},\"dynamic_list_endpoint\":\"context/content\",\"proxy_input\":{\"product_friendly_url\":\"" + productFriendlyUrl + "\"}}";
+
+
+      Request request = Request.RequestBuilder.create()
+         .setUrl(url)
+         .setHeaders(headers)
+         .setPayload(payload)
+         .build();
+
+
+      Response response = CrawlerUtils.retryRequestWithListDataFetcher(request, List.of(new JsoupDataFetcher(), new FetcherDataFetcher()), session, "post");
+      String body = response.getBody();
+
+      JSONObject jsonObject = CrawlerUtils.stringToJSONObject(body);
+
+      return jsonObject.optJSONObject("dynamic_list_response");
    }
 }
