acb738a77c49bb41d272a8dfe99dbac395448623
Lua <danielsantos@lett.digital>
2022-06-24 11:53:08 -0300

    Finish core

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/mexico/MexicoModatelasCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/mexico/MexicoModatelasCrawler.java
@@ -1,4 +1,134 @@
 package br.com.lett.crawlernode.crawlers.corecontent.mexico;
 
-public class MexicoModatelasCrawler {
+import br.com.lett.crawlernode.core.models.Card;
+import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.models.ProductBuilder;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.Crawler;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.Logging;
+import com.google.common.collect.Sets;
+import exceptions.MalformedPricingException;
+import exceptions.OfferException;
+import models.Offer;
+import models.Offers;
+import models.pricing.*;
+import org.jsoup.nodes.Document;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+public class MexicoModatelasCrawler extends Crawler {
+   public MexicoModatelasCrawler(Session session) {
+      super(session);
+   }
+   private static final Set<String> cards = Sets.newHashSet(Card.VISA.toString(), Card.MASTERCARD.toString(),
+      Card.DINERS.toString(), Card.AMEX.toString(), Card.ELO.toString());
+   @Override
+   public List<Product> extractInformation(Document doc) throws Exception {
+      super.extractInformation(doc);
+      List<Product> products = new ArrayList<>();
+
+      if (isProductPage(doc)) {
+         Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
+
+         String internalId = CrawlerUtils.scrapStringSimpleInfoByAttribute(doc, ".product-add-form > #product_addtocart_form > input[name=item]", "value");
+         String name = CrawlerUtils.scrapStringSimpleInfo(doc, ".page-title-wrapper.product > h1 > span", false);
+         String primaryImage = CrawlerUtils.scrapSimplePrimaryImage(doc, ".gallery-placeholder._block-content-loading > img",
+            Arrays.asList("src"), "http://", "https://www.modatelas.com.mx/");
+         String description = CrawlerUtils.scrapSimpleDescription(doc, Arrays.asList("#description > .product.attribute.description > .value > p"));
+         boolean available = CrawlerUtils.scrapStringSimpleInfo(doc,".product-info-stock-sku > div.stock.available > span:last-child", false).contains("Disponible");
+         Offers offers = available ? scrapOffers(doc) : new Offers();
+
+         // Creating the product
+         Product product = ProductBuilder.create()
+            .setUrl(session.getOriginalURL())
+            .setInternalId(internalId)
+            .setInternalPid(internalId)
+            .setName(name)
+            .setPrimaryImage(primaryImage)
+            .setDescription(description)
+            .setOffers(offers)
+            .build();
+
+         products.add(product);
+
+
+      } else {
+         Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
+      }
+
+      return products;
+   }
+
+   private boolean isProductPage(Document doc) {
+      return doc.selectFirst(".page-title-wrapper.product > h1 > span") != null;
+   }
+
+   private Offers scrapOffers(Document doc) throws OfferException, MalformedPricingException {
+      Offers offers = new Offers();
+      Pricing pricing = scrapPricing(doc);
+
+      if (pricing != null) {
+         offers.add(Offer.OfferBuilder.create()
+            .setUseSlugNameAsInternalSellerId(true)
+            .setSellerFullName("modatelas")
+            .setSellersPagePosition(1)
+            .setIsBuybox(false)
+            .setIsMainRetailer(true)
+            .setPricing(pricing)
+            .build());
+      }
+
+      return offers;
+   }
+
+   private Pricing scrapPricing(Document doc) throws MalformedPricingException {
+
+      Double priceFrom = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".old-price > .price-container.price-final_price > .price-wrapper", "data-price-amount", false, ',', session);
+      Double spotlightPrice = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".special-price > .price-container.price-final_price > .price-wrapper", null, false, ',', session);
+
+      if (priceFrom == null){
+         priceFrom = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".price-container.price-final_price > .price-wrapper", "data-price-amount", false, ',', session);
+      }
+      if (spotlightPrice == null){
+         spotlightPrice = priceFrom;
+      }
+
+
+      BankSlip bankSlip = CrawlerUtils.setBankSlipOffers(spotlightPrice, null);
+      CreditCards creditCards = scrapCreditCards(spotlightPrice);
+
+      return Pricing.PricingBuilder.create()
+         .setSpotlightPrice(spotlightPrice)
+         .setPriceFrom(priceFrom)
+         .setCreditCards(creditCards)
+         .setBankSlip(bankSlip)
+         .build();
+   }
+
+   private CreditCards scrapCreditCards(Double spotlightPrice) throws MalformedPricingException {
+      CreditCards creditCards = new CreditCards();
+
+      Installments installments = new Installments();
+      if (installments.getInstallments().isEmpty()) {
+         installments.add(Installment.InstallmentBuilder.create()
+            .setInstallmentNumber(1)
+            .setInstallmentPrice(spotlightPrice)
+            .build());
+      }
+
+      for (String card : cards) {
+         creditCards.add(CreditCard.CreditCardBuilder.create()
+            .setBrand(card)
+            .setInstallments(installments)
+            .setIsShopCard(false)
+            .build());
+      }
+
+      return creditCards;
+
+   }
 }
