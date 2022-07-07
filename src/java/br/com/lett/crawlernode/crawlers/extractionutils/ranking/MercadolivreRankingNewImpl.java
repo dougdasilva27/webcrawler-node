4aa18c2b7b373380991f53b3ba4b372a66a5b6b2
Thainá Jardim <67081271+ThainaJardim@users.noreply.github.com>
2022-07-07 14:40:50 -0300

    fix ranking pid latam

--- src/java/br/com/lett/crawlernode/crawlers/extractionutils/ranking/MercadolivreRankingNewImpl.java
+++ src/java/br/com/lett/crawlernode/crawlers/extractionutils/ranking/MercadolivreRankingNewImpl.java
@@ -5,11 +5,13 @@ import br.com.lett.crawlernode.core.session.Session;
 public class MercadolivreRankingNewImpl extends MercadolivreCrawler{
 
    private final String URL = session.getOptions().optString("homePage") + this.keywordWithoutAccents.replace(" ", "-") + session.getOptions().optString("id");
+   private final String PRODUCT_URL_HOST = session.getOptions().optString("product_url_host", "produto.mercadolivre.com.br");
+   private final String NEXT_URL_HOST = session.getOptions().optString("next_url_host", "lista.mercadolivre.com.br");
 
    public MercadolivreRankingNewImpl(Session session) {
       super(session);
       super.setUrl(URL);
-      super.setProductUrlHost("produto.mercadolivre.com.br");
-      super.setNextUrlHost("lista.mercadolivre.com.br");
+      super.setProductUrlHost(PRODUCT_URL_HOST);
+      super.setNextUrlHost(NEXT_URL_HOST);
    }
 }
