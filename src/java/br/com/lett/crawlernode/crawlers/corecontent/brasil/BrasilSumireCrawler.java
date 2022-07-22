4465e95d8ac50bda9bceda9c5a7542162a703fcf
Lua <danielsantos@lett.digital>
2022-07-22 11:27:12 -0300

    End core

---
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilSumireCrawler.java
@@ -0,0 +1,126 @@
+package br.com.lett.crawlernode.crawlers.corecontent.brasil;
+
+import br.com.lett.crawlernode.core.models.Card;
+import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.models.ProductBuilder;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.Crawler;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.Logging;
+import com.google.common.collect.Sets;
+import exceptions.MalformedPricingException;
+import exceptions.OfferException;
+import models.Offer;
+import models.Offers;
+import models.pricing.*;
+import org.jsoup.nodes.Document;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+public class BrasilSumireCrawler extends Crawler {
+
+   private static final Set<String> cards = Sets.newHashSet(Card.VISA.toString(), Card.MASTERCARD.toString(),
+      Card.DINERS.toString(), Card.AMEX.toString(), Card.ELO.toString());
+
+   public BrasilSumireCrawler(Session session) {
+      super(session);
+   }
+   @Override
+   public List<Product> extractInformation(Document doc) throws Exception {
+      super.extractInformation(doc);
+      List<Product> products = new ArrayList<>();
+
+      if (isProductPage(doc)) {
+         Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
+
+         String internalId = CrawlerUtils.scrapStringSimpleInfoByAttribute(doc, "#product_addtocart_form > input[type=hidden]:first-child", "value");
+         String name = CrawlerUtils.scrapStringSimpleInfo(doc, ".title-sku-wrapper > div.page-title-wrapper.product > h1 > span", false);
+         String description = CrawlerUtils.scrapStringSimpleInfo(doc, ".product-info-main > div.product.attribute.overview > div > p", false);
+         String primaryImage = CrawlerUtils.scrapSimplePrimaryImage(doc, ".product.media > div.gallery-placeholder._block-content-loading > img", Arrays.asList("src"), "https", "://www.perfumariasumire.com.br/");
+         boolean available = doc.selectFirst(".product-info-main > div.product.alert.stock > a") == null;
+         Offers offers = available ? scrapOffers(doc) : new Offers();
+
+         // Creating the product
+         Product product = ProductBuilder.create()
+            .setUrl(session.getOriginalURL())
+            .setInternalId(internalId)
+            .setInternalPid(internalId)
+            .setName(name)
+            .setPrimaryImage(primaryImage)
+            .setDescription(description)
+            .setOffers(offers)
+            .build();
+
+         products.add(product);
+
+
+      } else {
+         Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
+      }
+
+      return products;
+   }
+   private boolean isProductPage(Document doc) {
+      return doc.selectFirst(".title-sku-wrapper > div.page-title-wrapper.product > h1") != null;
+   }
+
+   private Offers scrapOffers(Document doc) throws OfferException, MalformedPricingException {
+      Offers offers = new Offers();
+      Pricing pricing = scrapPricing(doc);
+
+      if (pricing != null) {
+         offers.add(Offer.OfferBuilder.create()
+            .setUseSlugNameAsInternalSellerId(true)
+            .setSellerFullName("sumire")
+            .setSellersPagePosition(1)
+            .setIsBuybox(false)
+            .setIsMainRetailer(true)
+            .setPricing(pricing)
+            .build());
+      }
+
+      return offers;
+   }
+
+   private Pricing scrapPricing(Document doc) throws MalformedPricingException {
+
+      Double priceFrom = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".product-info-main > div.product-info-price > div.price-box.price-final_price > span > span > meta:nth-child(odd)", "content", false, '.', session);
+      Double spotlightPrice = priceFrom;
+
+      BankSlip bankSlip = CrawlerUtils.setBankSlipOffers(spotlightPrice, null);
+      CreditCards creditCards = scrapCreditCards(spotlightPrice);
+
+      return Pricing.PricingBuilder.create()
+         .setSpotlightPrice(spotlightPrice)
+         .setPriceFrom(priceFrom)
+         .setCreditCards(creditCards)
+         .setBankSlip(bankSlip)
+         .build();
+   }
+
+   private CreditCards scrapCreditCards(Double spotlightPrice) throws MalformedPricingException {
+      CreditCards creditCards = new CreditCards();
+
+      Installments installments = new Installments();
+      if (installments.getInstallments().isEmpty()) {
+         installments.add(Installment.InstallmentBuilder.create()
+            .setInstallmentNumber(1)
+            .setInstallmentPrice(spotlightPrice)
+            .build());
+      }
+
+      for (String card : cards) {
+         creditCards.add(CreditCard.CreditCardBuilder.create()
+            .setBrand(card)
+            .setInstallments(installments)
+            .setIsShopCard(false)
+            .build());
+      }
+
+      return creditCards;
+
+   }
+}
