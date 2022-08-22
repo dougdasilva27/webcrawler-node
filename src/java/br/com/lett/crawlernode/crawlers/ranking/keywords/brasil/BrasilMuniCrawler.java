dbf4668af42841256fb78d0383d7089d3b1b5988
Lua <danielsantos@lett.digital>
2022-08-22 16:50:26 -0300

    End development

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilMuniCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilMuniCrawler.java
@@ -28,11 +28,11 @@ import java.util.List;
 import java.util.Map;
 
 public class BrasilMuniCrawler extends CrawlerRankingKeywords {
+   Integer pageNumber = 0;
    public static final String PRODUCTS_API_URL = "https://a60vkx00hn-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia%20for%20JavaScript%20(4.10.5)%3B%20Browser%20(lite)%3B%20JS%20Helper%20(3.10.0)%3B%20react%20(16.14.0)%3B%20react-instantsearch%20(6.12.1)&x-algolia-api-key=671ae10cfea41e8580445bd850aa9d9c&x-algolia-application-id=A60VKX00HN";
 
    public BrasilMuniCrawler(Session session) {
       super(session);
-      super.fetchMode = FetchMode.JSOUP;
    }
 
    protected void extractProductsFromCurrentPage() throws UnsupportedEncodingException, MalformedProductException {
@@ -40,9 +40,9 @@ public class BrasilMuniCrawler extends CrawlerRankingKeywords {
       this.log("Página " + this.currentPage);
 
       JSONObject search = fetchProductsFromAPI();
-      JSONArray arraySkus = search.has("hits") ? search.getJSONArray("hits/") : new JSONArray();
+      JSONArray arraySkus = JSONUtils.getValueRecursive(search, "results.0.hits", JSONArray.class);
 
-      if (arraySkus.length() > 0) {
+      if (arraySkus != null && arraySkus.length() > 0) {
 
          if (this.totalProducts == 0) {
             setTotalProducts(search);
@@ -55,7 +55,7 @@ public class BrasilMuniCrawler extends CrawlerRankingKeywords {
             String productUrl = "https://shop.munitienda.com.br/BR-SAO/mp/" + internalId + "/" + name;
             String imgUrl = JSONUtils.getStringValue(jsonSku, "image");
             Integer price = crawlPrice(jsonSku);
-            boolean isAvailable = JSONUtils.getIntegerValueFromJSON(jsonSku, "in_stock", 0) >= 1 ? true : false;
+            boolean isAvailable = jsonSku.optBoolean("isActive");
             RankingProduct productRanking = RankingProductBuilder.create()
                .setUrl(productUrl)
                .setInternalId(internalId)
@@ -72,7 +72,7 @@ public class BrasilMuniCrawler extends CrawlerRankingKeywords {
                break;
             }
          }
-
+         hasNextPage(search);
       } else {
 
          this.result = false;
@@ -96,17 +96,23 @@ public class BrasilMuniCrawler extends CrawlerRankingKeywords {
    }
 
    private void setTotalProducts(JSONObject search) {
-      this.totalProducts = JSONUtils.getIntegerValueFromJSON(search, "nbHits", 0);
-      this.log("Total: " + this.totalProducts);
+      JSONArray arraySkus = JSONUtils.getValueRecursive(search, "results", JSONArray.class);
+      String holder = arraySkus.optQuery("/0/nbHits").toString();
+      if (holder != null){
+         this.totalProducts = Integer.parseInt(holder);
+         this.log("Total: " + this.totalProducts);
+      }else {
+         this.totalProducts = 0;
+         this.log("Total: " + this.totalProducts);
+      }
    }
 
    private JSONObject fetchProductsFromAPI() {
-      JSONObject products = new JSONObject();
 
-      String payload = "{\"requests\":[{\"indexName\":\"BR-SAO__product_index__br-production\",\"params\":\"clickAnalytics=true&facets=%5B%5D&highlightPostTag=%3C%2Fais-highlight-0000000000%3E&highlightPreTag=%3Cais-highlight-0000000000%3E&page=" + (currentPage - 1) + "&query=" + keywordEncoded + "&tagFilters=\"}]}";
+      String payload = "{\"requests\":[{\"indexName\":\"BR-SAO__product_index__br-production\",\"params\":\"clickAnalytics=true&facets=%5B%5D&highlightPostTag=%3C%2Fais-highlight-0000000000%3E&highlightPreTag=%3Cais-highlight-0000000000%3E&page=" + pageNumber + "&query=" + keywordEncoded + "&tagFilters=\"}]}";
 
       Map<String, String> headers = new HashMap<>();
-      headers.put(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded");
+      headers.put(HttpHeaders.CONTENT_TYPE, "application/json");
       headers.put("Origin", "https://shop.munitienda.com.br/");
       headers.put("Connection", "keep-alive");
       headers.put("Referer", "https://shop.munitienda.com.br/");
@@ -118,16 +124,36 @@ public class BrasilMuniCrawler extends CrawlerRankingKeywords {
          .setUrl(PRODUCTS_API_URL)
          .setHeaders(headers)
          .setProxyservice(Arrays.asList(
+            ProxyCollection.NETNUT_RESIDENTIAL_BR,
             ProxyCollection.NETNUT_RESIDENTIAL_BR_HAPROXY,
             ProxyCollection.NETNUT_RESIDENTIAL_MX_HAPROXY,
             ProxyCollection.NETNUT_RESIDENTIAL_AR_HAPROXY,
-            ProxyCollection.NETNUT_RESIDENTIAL_ES
+            ProxyCollection.NETNUT_RESIDENTIAL_ES,
+            ProxyCollection.NETNUT_RESIDENTIAL_MX
          ))
          .setPayload(payload)
          .mustSendContentEncoding(true)
+         .setSendUserAgent(true)
          .build();
-      Response response = CrawlerUtils.retryRequestWithListDataFetcher(request, List.of(new ApacheDataFetcher(), new JsoupDataFetcher(), new FetcherDataFetcher()), session);
+      Response response = CrawlerUtils.retryRequestWithListDataFetcher(request, List.of(new ApacheDataFetcher(), new JsoupDataFetcher(), new FetcherDataFetcher()), session, "post");
 
       return CrawlerUtils.stringToJson(response.getBody());
+   }
+
+
+   protected boolean hasNextPage(JSONObject search) {
+      JSONArray arraySkus = JSONUtils.getValueRecursive(search, "results", JSONArray.class);
+      String holder = arraySkus.optQuery("/0/nbPages").toString();
+      if (holder != null) {
+         Integer quantity = Integer.parseInt(holder);
+         if (pageNumber <= quantity) {
+            pageNumber++;
+            return true;
+         } else {
+            return false;
+         }
+      } else {
+         return false;
+      }
    }
 }
