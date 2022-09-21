c8070cdff9eb8efe63bf87d107402f328b1c9fef
doferreira <douglassilva@lett.digital>
2022-09-21 09:21:27 -0300

    create constructor and others method's

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilAlegrapetCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilAlegrapetCrawler.java
@@ -1,4 +1,53 @@
 package br.com.lett.crawlernode.crawlers.corecontent.brasil;
 
-public class BrasilAlegrapetCrawler {
+import br.com.lett.crawlernode.core.fetcher.FetchMode;
+import br.com.lett.crawlernode.core.models.Parser;
+import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.models.ProductBuilder;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.Crawler;
+import org.jsoup.nodes.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BrasilAlegrapetCrawler extends Crawler {
+   protected BrasilAlegrapetCrawler(Session session) {
+      super(session);
+      super.config.setFetcher(FetchMode.JSOUP);
+      super.config.setParser(Parser.HTML);
+   }
+
+   @Override
+   public List<Product> extractInformation(Document document) throws Exception {
+      List<Product> products = new ArrayList<>();
+
+      if (isProductPage(document)) {
+
+         String internalId = crawlInternalId(document);
+
+         Product product = ProductBuilder.create()
+            .setUrl(session.getOriginalURL())
+            .setInternalId(internalId)
+            .build();
+         products.add(product);
+      }
+
+      return products;
+   }
+
+   private boolean isProductPage(Document document) {
+      return document.selectFirst("") != null;
+   }
+
+   private String crawlInternalId(Document json) {
+      String internalId = null;
+
+      if (json.hasAttr("id_item")) {
+         internalId = json.attributes().get("id_item");
+      }
+
+      return internalId;
+   }
+
 }
