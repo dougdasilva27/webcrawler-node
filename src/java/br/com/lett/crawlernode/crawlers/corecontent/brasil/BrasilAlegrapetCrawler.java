131b4bc54d9153644b1efd33cf3c3a117dab8bfb
doferreira <douglassilva@lett.digital>
2022-09-25 16:37:40 -0300

    create fetch response

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilAlegrapetCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilAlegrapetCrawler.java
@@ -1,21 +1,50 @@
 package br.com.lett.crawlernode.crawlers.corecontent.brasil;
 
 import br.com.lett.crawlernode.core.fetcher.FetchMode;
+import br.com.lett.crawlernode.core.fetcher.ProxyCollection;
+import br.com.lett.crawlernode.core.fetcher.methods.ApacheDataFetcher;
+import br.com.lett.crawlernode.core.fetcher.methods.FetcherDataFetcher;
+import br.com.lett.crawlernode.core.fetcher.models.Request;
+import br.com.lett.crawlernode.core.fetcher.models.Response;
 import br.com.lett.crawlernode.core.models.Parser;
 import br.com.lett.crawlernode.core.models.Product;
 import br.com.lett.crawlernode.core.models.ProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.Crawler;
+import br.com.lett.crawlernode.util.CrawlerUtils;
 import org.jsoup.nodes.Document;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BrasilAlegrapetCrawler extends Crawler {
-   protected BrasilAlegrapetCrawler(Session session) {
+   public BrasilAlegrapetCrawler(Session session) {
       super(session);
       super.config.setFetcher(FetchMode.JSOUP);
       super.config.setParser(Parser.HTML);
+   }
+
+   @Override
+   protected Response fetchResponse() {
+      Map<String,String> headers = new HashMap<>();
+      headers.put("authority","www.alegrapet.com.br");
+      headers.put("accept","*/*");
+      headers.put("accept-language","pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7");
+
+      Request request = Request.RequestBuilder.create()
+         .setUrl(session.getOriginalURL())
+         .setProxyservice(List.of(
+            ProxyCollection.NETNUT_RESIDENTIAL_BR,
+            ProxyCollection.NETNUT_RESIDENTIAL_BR_HAPROXY,
+            ProxyCollection.LUMINATI_RESIDENTIAL_BR))
+         .setHeaders(headers)
+         .build();
+
+      Response response = CrawlerUtils.retryRequestWithListDataFetcher(request, List.of(this.dataFetcher, new ApacheDataFetcher(), new FetcherDataFetcher()), session, "get");
+
+      return response;
    }
 
    @Override
@@ -37,7 +66,7 @@ public class BrasilAlegrapetCrawler extends Crawler {
    }
 
    private boolean isProductPage(Document document) {
-      return document.selectFirst("") != null;
+      return document.selectFirst(".page-content") != null;
    }
 
    private String crawlInternalId(Document json) {
