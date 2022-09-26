7ee41e27ff499660f32da768aa6456124ec1ad9b
doferreira <douglassilva@lett.digital>
2022-09-26 15:30:39 -0300

    create extraction information

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilAlegrapetCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilAlegrapetCrawler.java
@@ -6,20 +6,37 @@ import br.com.lett.crawlernode.core.fetcher.methods.ApacheDataFetcher;
 import br.com.lett.crawlernode.core.fetcher.methods.FetcherDataFetcher;
 import br.com.lett.crawlernode.core.fetcher.models.Request;
 import br.com.lett.crawlernode.core.fetcher.models.Response;
+import br.com.lett.crawlernode.core.models.Card;
 import br.com.lett.crawlernode.core.models.Parser;
 import br.com.lett.crawlernode.core.models.Product;
 import br.com.lett.crawlernode.core.models.ProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.Crawler;
 import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.JSONUtils;
+import br.com.lett.crawlernode.util.Logging;
+import com.google.common.collect.Sets;
+import exceptions.MalformedPricingException;
+import exceptions.OfferException;
+import models.Offer;
+import models.Offers;
+import models.pricing.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class BrasilAlegrapetCrawler extends Crawler {
+
+   private final String SELLER_NAME = "Alegra pet";
+
+   public Set<String> cards = Sets.newHashSet(Card.VISA.toString(), Card.MASTERCARD.toString(),
+      Card.ELO.toString(), Card.DINERS.toString(), Card.HIPER.toString(), Card.AMEX.toString(),
+      Card.HIPERCARD.toString());
+
    public BrasilAlegrapetCrawler(Session session) {
       super(session);
       super.config.setFetcher(FetchMode.JSOUP);
@@ -28,10 +45,10 @@ public class BrasilAlegrapetCrawler extends Crawler {
 
    @Override
    protected Response fetchResponse() {
-      Map<String,String> headers = new HashMap<>();
-      headers.put("authority","www.alegrapet.com.br");
-      headers.put("accept","*/*");
-      headers.put("accept-language","pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7");
+      Map<String, String> headers = new HashMap<>();
+      headers.put("authority", "www.alegrapet.com.br");
+      headers.put("accept", "*/*");
+      headers.put("accept-language", "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7");
 
       Request request = Request.RequestBuilder.create()
          .setUrl(session.getOriginalURL())
@@ -52,14 +69,31 @@ public class BrasilAlegrapetCrawler extends Crawler {
       List<Product> products = new ArrayList<>();
 
       if (isProductPage(document)) {
+         Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
 
-         String internalId = crawlInternalId(document);
+         String productName = CrawlerUtils.scrapStringSimpleInfo(document, "div > h1", true);
+         String internalPid = CrawlerUtils.scrapStringSimpleInfoByAttribute(document, "#form_comprar", "data-id");
+         String description = CrawlerUtils.scrapStringSimpleInfo(document, "div.section-box.description > div.board_htm", false);
+         List<String> categories = CrawlerUtils.crawlCategories(document, "div.breadcrumb.flex.f-wrap > span");
+         String primaryImage = CrawlerUtils.scrapStringSimpleInfoByAttribute(document, "[property=\"og:image\"]", "content");
+         List<String> secondaryImages = getSecondaryImages(document);
+         boolean available = document.selectFirst("#button-buy") != null;
+         Offers offers = available ? scrapOffers(document) : new Offers();
 
          Product product = ProductBuilder.create()
             .setUrl(session.getOriginalURL())
-            .setInternalId(internalId)
+            .setName(productName)
+            .setInternalPid(internalPid)
+            .setDescription(description)
+            .setCategories(categories)
+            .setPrimaryImage(primaryImage)
+            .setSecondaryImages(secondaryImages)
+            .setOffers(offers)
             .build();
          products.add(product);
+
+      } else {
+         Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
       }
 
       return products;
@@ -69,14 +103,67 @@ public class BrasilAlegrapetCrawler extends Crawler {
       return document.selectFirst(".page-content") != null;
    }
 
-   private String crawlInternalId(Document json) {
-      String internalId = null;
+   private List<String> getSecondaryImages(Document doc) {
+      List<String> secondaryImages = new ArrayList<>();
 
-      if (json.hasAttr("id_item")) {
-         internalId = json.attributes().get("id_item");
+      Elements imagesLi = doc.select("div.nav-images > div > div > div > img");
+      for (Element imageLi : imagesLi) {
+         secondaryImages.add(imageLi.attr("src"));
       }
-
-      return internalId;
+      if (secondaryImages.size() > 0) {
+         secondaryImages.remove(0);
+      }
+      return secondaryImages;
    }
 
+   private Offers scrapOffers(Document doc) throws MalformedPricingException, OfferException {
+      Offers offers = new Offers();
+      Pricing pricing = scrapPricing(doc);
+      List<String> sales = Collections.singletonList(CrawlerUtils.calculateSales(pricing));
+
+      offers.add(new Offer.OfferBuilder()
+         .setUseSlugNameAsInternalSellerId(true)
+         .setSellerFullName(this.SELLER_NAME)
+         .setMainPagePosition(1)
+         .setIsBuybox(false)
+         .setIsMainRetailer(true)
+         .setPricing(pricing)
+         .setSales(sales)
+         .build());
+
+      return offers;
+   }
+
+   private Pricing scrapPricing(Document doc) throws MalformedPricingException {
+      Double spotlightPrice = CrawlerUtils.scrapDoublePriceFromHtml(doc, "#variacaoPreco", null, false, ',', session);
+
+      BankSlip bankSlip = CrawlerUtils.setBankSlipOffers(spotlightPrice, null);
+      CreditCards creditCards = scrapCreditCards(spotlightPrice);
+
+      return Pricing.PricingBuilder.create()
+         .setSpotlightPrice(spotlightPrice)
+         .setCreditCards(creditCards)
+         .setBankSlip(bankSlip)
+         .build();
+   }
+
+   private CreditCards scrapCreditCards(Double price) throws MalformedPricingException {
+      CreditCards creditCards = new CreditCards();
+      Installments installments = new Installments();
+
+      installments.add(Installment.InstallmentBuilder.create()
+         .setInstallmentNumber(1)
+         .setInstallmentPrice(price)
+         .setFinalPrice(price)
+         .build());
+
+      for (String card : cards) {
+         creditCards.add(CreditCard.CreditCardBuilder.create()
+            .setBrand(card)
+            .setInstallments(installments)
+            .setIsShopCard(false)
+            .build());
+      }
+      return creditCards;
+   }
 }
