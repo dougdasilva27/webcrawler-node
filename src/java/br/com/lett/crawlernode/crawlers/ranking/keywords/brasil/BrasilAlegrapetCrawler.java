171b2610006c54f150098e3223f26c50265292ea
doferreira <douglassilva@lett.digital>
2022-09-27 10:31:06 -0300

    create method's class ranking

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilAlegrapetCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilAlegrapetCrawler.java
@@ -1,4 +1,88 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
 
-public class BrasilAlegrapetCrawler {
+import br.com.lett.crawlernode.core.fetcher.FetchMode;
+import br.com.lett.crawlernode.core.fetcher.ProxyCollection;
+import br.com.lett.crawlernode.core.fetcher.models.Request;
+import br.com.lett.crawlernode.core.models.RankingProduct;
+import br.com.lett.crawlernode.core.models.RankingProductBuilder;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.exceptions.MalformedProductException;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BrasilAlegrapetCrawler extends CrawlerRankingKeywords {
+   public BrasilAlegrapetCrawler(Session session) {
+      super(session);
+      super.fetchMode = FetchMode.JSOUP;
+   }
+
+   @Override
+   protected Document fetchDocument(String url) {
+      Map<String,String> headers = new HashMap<>();
+      headers.put("Connection","keep-alive");
+      Request request = Request.RequestBuilder.create()
+         .setUrl(url)
+         .setHeaders(headers)
+         .setProxyservice(Arrays.asList(
+            ProxyCollection.NETNUT_RESIDENTIAL_CO_HAPROXY,
+            ProxyCollection.NETNUT_RESIDENTIAL_AR_HAPROXY,
+            ProxyCollection.NETNUT_RESIDENTIAL_ES,
+            ProxyCollection.NETNUT_RESIDENTIAL_BR
+         ))
+         .build();
+
+      String response = dataFetcher.get(session, request).getBody();
+
+      return Jsoup.parse(response);
+   }
+
+   @Override
+   protected void extractProductsFromCurrentPage() throws UnsupportedEncodingException, MalformedProductException {
+      String url = "https://www.alegrapet.com.br/loja/busca.php?loja=609385&palavra_busca=" + this.keywordWithoutAccents + "&pg=" + this.currentPage;
+      this.currentDoc = fetchDocument(url);
+
+      Elements products = this.currentDoc.select("div.catalog-content > div > ul > li");
+
+      if (!products.isEmpty()) {
+
+         for (Element product : products) {
+            String internalPid = CrawlerUtils.scrapStringSimpleInfoByAttribute(product, "li >div > div.variants.hide-on-mobile > form", "data-id");
+            String productUrl = CrawlerUtils.scrapUrl(product, "div > ul > li > div > div.image > a.href", "", "https", "www.alegrapet.com.br");
+            String productName = CrawlerUtils.scrapStringSimpleInfo(product, "li >div > a > div.product-name", false);
+            String imageUrl = CrawlerUtils.scrapStringSimpleInfoByAttribute(product, "ul > li > div > div.image > a > img", "data-src");
+            Integer price = CrawlerUtils.scrapPriceInCentsFromHtml(product, "div > ul > li > div > a > div.down-line > div > div.price > div > span", null, true, ',', session, null);
+            boolean isAvailable = price != null;
+
+            RankingProduct productRanking = RankingProductBuilder.create()
+               .setUrl(productUrl)
+               .setInternalPid(internalPid)
+               .setName(productName)
+               .setPriceInCents(price)
+               .setAvailability(isAvailable)
+               .setImageUrl(imageUrl)
+               .build();
+
+            saveDataProduct(productRanking);
+            if (this.arrayProducts.size() == productsLimit)
+               break;
+         }
+      } else {
+         this.result = false;
+         this.log("Keyword sem resultado!");
+      }
+      this.log("Finalizando Crawler de produtos da página: " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
+   }
+
+   protected boolean hasNextPage() {
+      return !this.currentDoc.select("span.page-next.page-link").isEmpty();
+   }
 }
