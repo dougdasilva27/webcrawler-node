1e5f2e1867d5a3a1b46ae0016d0d4947b6407842
doferreira <douglassilva@lett.digital>
2022-10-04 12:26:22 -0300

    create response and extractInformation

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilShopcaoCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilShopcaoCrawler.java
@@ -1,4 +1,67 @@
 package br.com.lett.crawlernode.crawlers.corecontent.brasil;
 
-public class BrasilShopcaoCrawler {
+import br.com.lett.crawlernode.core.fetcher.ProxyCollection;
+import br.com.lett.crawlernode.core.fetcher.methods.ApacheDataFetcher;
+import br.com.lett.crawlernode.core.fetcher.methods.FetcherDataFetcher;
+import br.com.lett.crawlernode.core.fetcher.models.Request;
+import br.com.lett.crawlernode.core.fetcher.models.Response;
+import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.models.ProductBuilder;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.Crawler;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.Logging;
+import org.jsoup.nodes.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BrasilShopcaoCrawler extends Crawler {
+   public BrasilShopcaoCrawler(Session session) {
+      super(session);
+   }
+
+   @Override
+   protected Response fetchResponse() {
+      Request request = Request.RequestBuilder.create()
+         .setUrl(session.getOriginalURL())
+         .setProxyservice(List.of(
+            ProxyCollection.NETNUT_RESIDENTIAL_BR,
+            ProxyCollection.NETNUT_RESIDENTIAL_MX_HAPROXY,
+            ProxyCollection.NETNUT_RESIDENTIAL_MX))
+         .build();
+
+      Response response = CrawlerUtils.retryRequestWithListDataFetcher(request, List.of(this.dataFetcher, new ApacheDataFetcher(), new FetcherDataFetcher()), session, "get");
+
+      return response;
+   }
+
+   @Override
+   public List<Product> extractInformation(Document document) throws Exception {
+      super.extractInformation(document);
+      List<Product> products = new ArrayList<>();
+
+      if (isProductPage(document)) {
+         Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
+
+         String productName = CrawlerUtils.scrapStringSimpleInfo(document, "#shopify-section-pr_summary > h1", true);
+         String internalPid = CrawlerUtils.scrapStringSimpleInfoByAttribute(document, "#shopify-ali-review", "product-id");
+         String description = CrawlerUtils.scrapStringSimpleInfo(document, "#tab_pr_deskl > div.sp-tab-content", false);
+
+         Product product = ProductBuilder.create()
+            .setInternalId(internalPid)
+            .setName(productName)
+            .setDescription(description)
+            .build();
+         products.add(product);
+
+      } else {
+         Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
+      }
+      return products;
+   }
+
+   private boolean isProductPage(Document document) {
+      return document.selectFirst("div.container.container_cat.cat_default") != null;
+   }
 }
