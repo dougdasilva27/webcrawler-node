f18e8e77779bff2b72a269e1c8447ef24ba21b05
mazzillio <mateca500@gmail.com>
2022-10-06 10:41:54 -0300

    Adição de Zoolojapet

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilZoolojapetCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilZoolojapetCrawler.java
@@ -1,10 +1,164 @@
 package br.com.lett.crawlernode.crawlers.corecontent.brasil;
 
+import br.com.lett.crawlernode.core.models.Card;
+import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.models.ProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.Crawler;
+import br.com.lett.crawlernode.util.CommonMethods;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.JSONUtils;
+import br.com.lett.crawlernode.util.Logging;
+import com.google.common.collect.Sets;
+import exceptions.MalformedPricingException;
+import exceptions.OfferException;
+import models.Offer;
+import models.Offers;
+import models.pricing.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.jsoup.nodes.Document;
+import ucar.ma2.ArrayObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class BrasilZoolojapetCrawler extends Crawler {
    public BrasilZoolojapetCrawler(Session session) {
       super(session);
    }
+   private String SELLER_NAME="zoolojapet";
+   private String HOME_PAGE = "https://zoolojapet.com.br/";
+   @Override
+   public List<Product> extractInformation(Document doc) throws Exception {
+      List<Product> products = new ArrayList<Product>();
+      JSONObject dataJson = getScriptJson(doc);
+      if(dataJson!=null) {
+         String internalId = dataJson.optString("id",null);
+         String internalPid = dataJson.optString("sku",null);
+         String url = getUrl(dataJson);
+         String name = dataJson.optString("name");
+         String description = dataJson.optString("description");
+         String primaryImage = getPrimaryImage(dataJson);
+         List<String> secondaryImages = getSecondaryImages(dataJson);
+         Offers offers = scrapOffers(dataJson);
+         Product product = ProductBuilder.create()
+            .setInternalId(internalId)
+            .setInternalPid(internalPid)
+            .setUrl(url)
+            .setName(name)
+            .setOffers(offers)
+            .setPrimaryImage(primaryImage)
+            .setSecondaryImages(secondaryImages)
+            .setDescription(description)
+            .build();
+         products.add(product);
+      }else {
+         Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
+      }
+      return products;
+   }
+   private String getUrl(JSONObject json) {
+      if(json!=null) {
+         String slug = json.optString("slug");
+         if(!"".equals(slug) && !slug.isEmpty()) {
+            return HOME_PAGE+slug;
+         }
+      }
+      return null;
+   }
+   private String getPrimaryImage(JSONObject json) {
+      if (json!=null) {
+         String imageUrl = JSONUtils.getValueRecursive(json,"image.url", String.class);
+         if (!imageUrl.isEmpty() && !"".equals(imageUrl)) {
+            return imageUrl;
+         }
+      }
+      return null;
+   }
+   private List<String> getSecondaryImages(JSONObject json) {
+      if (json!=null) {
+         JSONArray arrayImages = JSONUtils.getValueRecursive(json,"images",JSONArray.class);
+         if(!arrayImages.isEmpty() && arrayImages!=null) {
+            List<String> secondaryImages = new ArrayList<String>();
+            for(Integer i=0;i<arrayImages.length();i++) {
+               JSONObject img = arrayImages.optJSONObject(i);
+               String urlImage = img.optString("url");
+               if(!"".equals(urlImage) && !urlImage.isEmpty()) {
+                  secondaryImages.add(urlImage);
+               }
+            }
+            if (secondaryImages.size() > 0) {
+               return secondaryImages;
+            }
+         }
+      }
+      return null;
+   }
+   private Offers scrapOffers(JSONObject data) throws OfferException, MalformedPricingException {
+      Offers offers = new Offers();
+      List<String> sales = new ArrayList<>();
+      Pricing pricing = scrapPricing(data);
+      sales.add(CrawlerUtils.calculateSales(pricing));
+      offers.add(Offer.OfferBuilder.create()
+         .setMainPagePosition(1)
+         .setIsBuybox(false)
+         .setPricing(pricing)
+         .setSales(sales)
+         .setSellerFullName(SELLER_NAME)
+         .setIsMainRetailer(true)
+         .setUseSlugNameAsInternalSellerId(true)
+         .build());
+      return offers;
+   }
+   private Pricing scrapPricing(JSONObject json) throws MalformedPricingException {
+     Double price = json.optDouble("price");
+
+      CreditCards creditCards = scrapCreditCards(price);
+      BankSlip bankSlip = BankSlip.BankSlipBuilder.create()
+         .setFinalPrice(price)
+         .build();
+
+      return Pricing.PricingBuilder.create()
+         .setSpotlightPrice(price)
+         .setBankSlip(bankSlip)
+         .setCreditCards(creditCards)
+         .build();
+   }
+   private CreditCards scrapCreditCards(Double spotlightPrice) throws MalformedPricingException {
+      CreditCards creditCards = new CreditCards();
+      Installments installments = new Installments();
+      installments.add(Installment.InstallmentBuilder.create()
+         .setInstallmentNumber(1)
+         .setInstallmentPrice(spotlightPrice)
+         .build());
+
+      Set<String> cards = Sets.newHashSet(Card.VISA.toString(), Card.MASTERCARD.toString(), Card.HIPERCARD.toString(),
+         Card.DINERS.toString(),Card.HIPER.toString(), Card.ELO.toString(), Card.SOROCRED.toString(),Card.AMEX.toString());
+
+      for (String card : cards) {
+         creditCards.add(CreditCard.CreditCardBuilder.create()
+            .setBrand(card)
+            .setInstallments(installments)
+            .setIsShopCard(false)
+            .build());
+      }
+
+      return creditCards;
+   }
+
+
+   private JSONObject getScriptJson(Document doc) {
+      String dataScript = CrawlerUtils.scrapScriptFromHtml(doc,"#__NEXT_DATA__");
+      if(dataScript != null && !dataScript.isEmpty()) {
+         JSONArray arrayJson = CrawlerUtils.stringToJsonArray(dataScript);
+         if (arrayJson!=null && !arrayJson.isEmpty()) {
+            return JSONUtils.getValueRecursive(arrayJson,"0.props.pageProps.resource.data",JSONObject.class);
+         }
+
+      }
+      return new JSONObject();
+   }
+
 }
--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilZoolojapetCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilZoolojapetCrawler.java
@@ -3,10 +3,15 @@ package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
 import br.com.lett.crawlernode.core.fetcher.models.Request;
 import br.com.lett.crawlernode.core.fetcher.models.Response;
 import br.com.lett.crawlernode.core.models.Product;
+import br.com.lett.crawlernode.core.models.RankingProduct;
+import br.com.lett.crawlernode.core.models.RankingProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
 import br.com.lett.crawlernode.exceptions.MalformedProductException;
+import br.com.lett.crawlernode.util.CommonMethods;
 import br.com.lett.crawlernode.util.JSONUtils;
+import org.apache.http.HttpHeaders;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
@@ -18,24 +23,91 @@ public class BrasilZoolojapetCrawler extends CrawlerRankingKeywords {
    public BrasilZoolojapetCrawler(Session session) {
       super(session);
    }
-   Integer pageSize = 36;
-      @Override
-      protected void extractProductsFromCurrentPage() throws UnsupportedEncodingException, MalformedProductException {
-         JSONObject json = fetchJSONObject("https://m2.zoolojapet.com.br/graphql/V1/products");
 
+   Integer pageSize = 36;
+   String homePage = "https://zoolojapet.com.br/";
+
+   @Override
+   protected void extractProductsFromCurrentPage() throws UnsupportedEncodingException, MalformedProductException {
+      JSONObject json = fetchJSONObject("https://m2.zoolojapet.com.br/graphql/V1/products");
+      totalProducts = JSONUtils.getValueRecursive(json, "data.products.total_count", Integer.class);
+      JSONArray products = JSONUtils.getValueRecursive(json, "data.products.items", JSONArray.class);
+      if (products != null && !products.isEmpty()) {
+         for (Object obj : products) {
+            JSONObject product = (JSONObject) obj;
+            String internalId = product.optString("id");
+            String internalPid = product.optString("sku", null);
+            String name = product.optString("name");
+            Boolean available = checkAvailability(product.optString("stock_status", null));
+            String productUrl = getUrl(product);
+            String imageUrl = getImage(product);
+            Integer price = getPrice(product);
+            RankingProduct productRanking = RankingProductBuilder.create()
+               .setInternalId(internalId)
+               .setInternalPid(internalPid)
+               .setName(name)
+               .setAvailability(available)
+               .setUrl(productUrl)
+               .setImageUrl(imageUrl)
+               .setPriceInCents(price)
+               .build();
+            saveDataProduct(productRanking);
+            if (this.arrayProducts.size() == productsLimit) {
+               break;
+            }
+         }
       }
+   }
+
+   private String getUrl(JSONObject product) {
+      String urlKey = product.optString("url_key");
+      String urlSuffix = product.optString("url_suffix");
+      if (!"".equals(urlKey) && !"".equals(urlSuffix) && !urlKey.isEmpty() && !urlSuffix.isEmpty()) {
+         return homePage + urlKey + urlSuffix;
+      }
+      return null;
+   }
+
+   private String getImage(JSONObject product) {
+      String imageUrl = JSONUtils.getValueRecursive(product, "thumbnail.url", String.class);
+      if (!"".equals(imageUrl) && !imageUrl.isEmpty()) {
+         return imageUrl;
+      }
+      return null;
+   }
+
+   private Integer getPrice(JSONObject product) {
+      Double priceDouble = JSONUtils.getValueRecursive(product, "price_range.minimum_price.final_price.value", Double.class);
+      if (priceDouble != null) {
+         return CommonMethods.doublePriceToIntegerPrice(priceDouble, 0);
+      }
+      Integer priceInteger = JSONUtils.getValueRecursive(product, "price_range.minimum_price.final_price.value", Integer.class);
+      if(priceInteger != null) {
+         return  priceInteger*100;
+      }
+      return null;
+   }
+
+   private Boolean checkAvailability(String stock) {
+      if (stock != null && !stock.isEmpty()) {
+         return stock.equals("IN_STOCK");
+      }
+      return false;
+   }
+
    @Override
    protected JSONObject fetchJSONObject(String url) {
-      String payload = "{\"query\":\"\\n        query($page: Int!, $pageSize: Int!) {\\n          products(\\n            search: \\\""+this.keywordEncoded+"\\\"\\n            currentPage: $page\\n            pageSize: $pageSize\\n            sort: { relevance: DESC }\\n            \\n          ) {\\n            \\n  items {\\n    id\\n    name\\n    sku\\n    stock_status\\n    url_key\\n    url_suffix\\n    recorrente\\n    price_range {\\n      minimum_price {\\n        regular_price { value }\\n        final_price { value }\\n        discount { amount_off, percent_off }\\n      }\\n    }\\n    thumbnail { url, label }\\n  }\\n  total_count\\n  page_info { current_page, total_pages }\\n\\n            aggregations {\\n              attribute_code\\n              count\\n              label\\n              options { count, label, value }\\n            }\\n          }\\n        }\\n      \",\"variables\":{\"page\":"+this.currentPage+",\"pageSize\":"+this.pageSize+"}}";
+      String payload = "{\"query\":\"\\n        query($page: Int!, $pageSize: Int!) {\\n          products(\\n            search: \\\"" + this.keywordEncoded + "\\\"\\n            currentPage: $page\\n            pageSize: $pageSize\\n            sort: { relevance: DESC }\\n            \\n          ) {\\n            \\n  items {\\n    id\\n    name\\n    sku\\n    stock_status\\n    url_key\\n    url_suffix\\n    recorrente\\n    price_range {\\n      minimum_price {\\n        regular_price { value }\\n        final_price { value }\\n        discount { amount_off, percent_off }\\n      }\\n    }\\n    thumbnail { url, label }\\n  }\\n  total_count\\n  page_info { current_page, total_pages }\\n\\n            aggregations {\\n              attribute_code\\n              count\\n              label\\n              options { count, label, value }\\n            }\\n          }\\n        }\\n      \",\"variables\":{\"page\":" + this.currentPage + ",\"pageSize\":" + this.pageSize + "}}";
       Map<String, String> headers = new HashMap<>();
       headers.put("origin", "https://zoolojapet.com.br/");
       headers.put("referer", "https://zoolojapet.com.br/");
+      headers.put(HttpHeaders.CONTENT_TYPE, "application/json");
       Request request = Request.RequestBuilder.create()
          .setUrl(url)
          .setHeaders(headers)
          .setPayload(payload)
          .build();
-      Response response = this.dataFetcher.get(session, request);
+      Response response = this.dataFetcher.post(session, request);
       return JSONUtils.stringToJson(response.getBody());
    }
 
