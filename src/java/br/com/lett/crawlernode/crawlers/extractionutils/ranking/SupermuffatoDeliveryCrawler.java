35391b6e6a09ca134e147db004acc25dc2c37a6f
mazzillio <mateca500@gmail.com>
2022-10-14 17:59:43 -0300

    Primeira analise de bugs do supermufato

--- src/java/br/com/lett/crawlernode/crawlers/extractionutils/ranking/SupermuffatoDeliveryCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/extractionutils/ranking/SupermuffatoDeliveryCrawler.java
@@ -1,5 +1,7 @@
 package br.com.lett.crawlernode.crawlers.extractionutils.ranking;
 
+import br.com.lett.crawlernode.core.fetcher.DynamicDataFetcher;
+import br.com.lett.crawlernode.core.fetcher.ProxyCollection;
 import br.com.lett.crawlernode.core.models.RankingProduct;
 import br.com.lett.crawlernode.core.models.RankingProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
@@ -7,14 +9,22 @@ import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
 import br.com.lett.crawlernode.exceptions.MalformedProductException;
 import br.com.lett.crawlernode.util.CommonMethods;
 import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.Logging;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SupermuffatoDeliveryCrawler extends CrawlerRankingKeywords {
 
    private static final String BASE_URL = "delivery.supermuffato.com.br/";
-
+   private String HOME_PAGE = "https://delivery.supermuffato.com.br";
    public SupermuffatoDeliveryCrawler(Session session) {
       super(session);
    }
@@ -22,20 +32,57 @@ public class SupermuffatoDeliveryCrawler extends CrawlerRankingKeywords {
    protected String getCityCode() {
       return session.getOptions().optString("cityCode");
    }
+   public static void waitForElement(WebDriver driver, String cssSelector) {
+      WebDriverWait wait = new WebDriverWait(driver, 10);
+      wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(cssSelector)));
+   }
+   @Override
+   protected Document fetchDocument(String url) {
+      String city = getCityCode();
+
+      try {
+         webdriver = DynamicDataFetcher.fetchPageWebdriver(HOME_PAGE, ProxyCollection.BUY_HAPROXY, session);
+
+         waitForElement(webdriver.driver, ".form-control optgroup option[value='13']");
+         webdriver.findAndClick(".form-control optgroup option[value='13']", 10000);
+
+         waitForElement(webdriver.driver, "#s-ch-change-channel");
+         webdriver.findAndClick("#s-ch-change-channel", 10000);
+         webdriver.waitLoad(10000);
+         waitForElement(webdriver.driver, ".fulltext-search-box.ui-autocomplete-input");
+         WebElement pass = webdriver.driver.findElement(By.cssSelector(".fulltext-search-box.ui-autocomplete-input"));
+         pass.sendKeys(this.keywordEncoded);
+
+         waitForElement(webdriver.driver, ".icon.icon-search");
+         webdriver.findAndClick(".icon.icon-search", 10000);
+
+//         webdriver.loadUrl(url);
+//         webdriver.waitLoad(10000);
+
+         webdriver.waitForElement(".ProductList--grid div.ProductItem", 30);
+
+         return Jsoup.parse(webdriver.getCurrentPageSource());
+
+      } catch (Exception e) {
+         Logging.printLogInfo(logger, session, CommonMethods.getStackTrace(e));
+
+      } finally {
+         if (webdriver != null) {
+            webdriver.terminate();
+         }
+      }
+      return null;
+   }
 
    @Override
    protected void extractProductsFromCurrentPage() throws MalformedProductException {
       this.pageSize = 24;
       this.log("Página " + this.currentPage);
-
-      String url = "https://delivery.supermuffato.com.br/buscapagina?" +
-         "ft=" + this.keywordEncoded +
-         "&sc=" + getCityCode() +
-         "&PS=48" +
-         "&sl=d85149b5-097b-4910-90fd-fa2ce00fe7c9" +
-         "&cc=48" +
-         "&sm=0" +
-         "&PageNumber=" + this.currentPage;
+      // https://delivery.supermuffato.com.br/cerveja?sc=13&utmi_cp=14912022153927552
+      // &utmi_cp=14912022154028856
+      String url = "https://delivery.supermuffato.com.br/" + this.keywordEncoded ;
+         //"?&sc=" + getCityCode() +
+         //"#" + this.currentPage;
 
       this.log("Link onde são feitos os crawlers: " + url);
 
