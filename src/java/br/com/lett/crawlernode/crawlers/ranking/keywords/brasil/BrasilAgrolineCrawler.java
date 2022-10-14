6f609df700b47d02811586bd08601a5753e15977
doferreira <douglassilva@lett.digital>
2022-10-14 12:07:36 -0300

    internal pid and total products

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilAgrolineCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilAgrolineCrawler.java
@@ -5,7 +5,9 @@ import br.com.lett.crawlernode.core.models.RankingProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
 import br.com.lett.crawlernode.exceptions.MalformedProductException;
+import br.com.lett.crawlernode.util.CommonMethods;
 import br.com.lett.crawlernode.util.CrawlerUtils;
+import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
@@ -13,7 +15,7 @@ import java.io.UnsupportedEncodingException;
 
 public class BrasilAgrolineCrawler extends CrawlerRankingKeywords {
 
-   private final String HOME_PAGE = "https://www.agroline.com.br";
+   private final String HOME_PAGE = "https://www.agroline.com.br/";
 
    public BrasilAgrolineCrawler(Session session) {
       super(session);
@@ -21,23 +23,26 @@ public class BrasilAgrolineCrawler extends CrawlerRankingKeywords {
 
    @Override
    protected void extractProductsFromCurrentPage() throws UnsupportedEncodingException, MalformedProductException {
-      String url = "https://www.agroline.com.br/busca?busca=" + this.keywordWithoutAccents + "&pagina=" + this.currentPage;
+      String url = HOME_PAGE + "busca?busca=" + this.keywordWithoutAccents + "&pagina=" + this.currentPage;
       this.currentDoc = fetchDocument(url);
 
       Elements products = this.currentDoc.select(".spotContent");
 
-      if(!products.isEmpty()){
-
-         for(Element product : products){
-            String productName = CrawlerUtils.scrapStringSimpleInfo(product,".spotTitle",true);
-            String productUrl = CrawlerUtils.scrapUrl(product,".spot-parte-um","href","https","www.agroline.com.br");
-            String imageUrl = CrawlerUtils.scrapStringSimpleInfoByAttribute(product,".jsImgSpot.imagem-primaria","src");
-            Integer price = CrawlerUtils.scrapPriceInCentsFromHtml(product,".fbits-valor",null,false,',',session,null);
+      if (!products.isEmpty()) {
+         if (this.totalProducts == 0) {
+            setTotalProducts(this.currentDoc);
+         }
+         for (Element product : products) {
+            String productName = CrawlerUtils.scrapStringSimpleInfo(product, ".spotTitle", true);
+            String productUrl = CrawlerUtils.scrapUrl(product, ".spot-parte-um", "href", "https", "www.agroline.com.br");
+            String imageUrl = CrawlerUtils.scrapStringSimpleInfoByAttribute(product, ".jsImgSpot.imagem-primaria", "src");
+            Integer price = CrawlerUtils.scrapPriceInCentsFromHtml(product, ".fbits-valor", null, false, ',', session, null);
+            String internalPid = CommonMethods.getLast(productUrl.split("-"));
             boolean isAvailable = price != null;
 
             RankingProduct productRanking = RankingProductBuilder.create()
                .setUrl(productUrl)
-               //.setInternalPid(internalPid)
+               .setInternalPid(internalPid)
                .setName(productName)
                .setPriceInCents(price)
                .setAvailability(isAvailable)
@@ -53,5 +58,10 @@ public class BrasilAgrolineCrawler extends CrawlerRankingKeywords {
          this.log("Keyword sem resultado!");
       }
       this.log("Finalizando Crawler de produtos da página: " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
+   }
+
+   private void setTotalProducts(Document doc) {
+      this.totalProducts = CrawlerUtils.scrapIntegerFromHtml(doc, ".fbits-qtd-produtos-pagina", true, 0);
+      this.log("Total da busca: " + this.totalProducts);
    }
 }
