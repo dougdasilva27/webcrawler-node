2eb8fc340ee1a2cb9be78ecadab1ea4573c17cc7
Thainá Jardim <67081271+ThainaJardim@users.noreply.github.com>
2022-10-17 21:13:36 -0300

    esalpet to api

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilEsalpetCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilEsalpetCrawler.java
@@ -1,24 +1,20 @@
 package br.com.lett.crawlernode.crawlers.corecontent.brasil;
 
+import br.com.lett.crawlernode.core.fetcher.models.Request;
+import br.com.lett.crawlernode.core.fetcher.models.Response;
 import br.com.lett.crawlernode.core.models.Card;
-import br.com.lett.crawlernode.core.models.CategoryCollection;
+import br.com.lett.crawlernode.core.models.Parser;
 import br.com.lett.crawlernode.core.models.Product;
 import br.com.lett.crawlernode.core.models.ProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.Crawler;
-import br.com.lett.crawlernode.crawlers.extractionutils.core.TrustvoxRatingCrawler;
-import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.JSONUtils;
 import br.com.lett.crawlernode.util.Logging;
 import com.google.common.collect.Sets;
 import exceptions.MalformedPricingException;
 import exceptions.OfferException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
 import models.Offer.OfferBuilder;
 import models.Offers;
-import models.RatingsReviews;
 import models.pricing.BankSlip;
 import models.pricing.BankSlip.BankSlipBuilder;
 import models.pricing.CreditCard.CreditCardBuilder;
@@ -27,9 +23,12 @@ import models.pricing.Installment.InstallmentBuilder;
 import models.pricing.Installments;
 import models.pricing.Pricing;
 import models.pricing.Pricing.PricingBuilder;
+import org.json.JSONArray;
 import org.json.JSONObject;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class BrasilEsalpetCrawler extends Crawler {
 
@@ -39,44 +38,73 @@ public class BrasilEsalpetCrawler extends Crawler {
 
    public BrasilEsalpetCrawler(Session session) {
       super(session);
+      super.config.setParser(Parser.JSON);
    }
 
    @Override
-   public List<Product> extractInformation(Document doc) throws Exception {
-      super.extractInformation(doc);
+   protected Response fetchResponse() {
+      String id = getId();
+      String url = "https://esal.api.alleshub.com.br/api_ecommerce/item/getById/" + id;
+
+      Map<String, String> headers = new HashMap<>();
+      headers.put("BaseConn", "{\"base\":\"erp_esalpet\",\"emp_id\":1}");
+      headers.put("content-type", "application/json");
+
+      Request request = Request.RequestBuilder.create().setUrl(url)
+         .setHeaders(headers)
+         .build();
+
+      return this.dataFetcher.get(session, request);
+   }
+
+   private String getId() {
+      String id = null;
+
+      Pattern pattern = Pattern.compile("id=([0-9]+)", Pattern.MULTILINE);
+      final Matcher matcher = pattern.matcher(session.getOriginalURL());
+
+      if (matcher.find()) {
+         id = matcher.group(1);
+
+      }
+
+      return id;
+   }
+
+   @Override
+   public List<Product> extractInformation(JSONObject jsonObject) throws Exception {
       List<Product> products = new ArrayList<>();
 
-      if (isProductPage(doc)) {
+      JSONObject data = jsonObject != null ? jsonObject.optJSONObject("data") : null;
+
+      if (data != null && !data.has("error")) {
          Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
 
-         JSONObject json = CrawlerUtils.selectJsonFromHtml(doc, "script[type=\"text/javascript\"]", "window.dooca = " , ";", false, true);
-         JSONObject data = json.optJSONObject("product");
-
-         String internalId = data != null ? data.optString("id") : null;
-         String internalPid = data != null ? data.optJSONObject("variation").optString("sku") : null;
-         String name = CrawlerUtils.scrapStringSimpleInfo(doc, "h1.h1.m-0", true);
+         String internalId = data.optString("id");
+         String internalPid = JSONUtils.getValueRecursive(data, "item_linkWeb.ecommerce_id", String.class);
+         String name = JSONUtils.getValueRecursive(data, "item_linkWeb.linkWeb.titulo", String.class);
          // Site has only one category
-         CategoryCollection categories = CrawlerUtils.crawlCategories(doc, "div:not(:first-child) a.breadcrumb-link", true);
-         String primaryImage =  crawlPrimaryImage(doc);
-         String description = CrawlerUtils.scrapElementsDescription(doc, Arrays.asList("div.cms.p-3.p-sm-0"));
-         Integer stock = data != null ? data.optInt("balance") : null;
-         boolean available = !doc.select(".product-action-buy-loader .loader").isEmpty();
-         RatingsReviews ratingsReviews = scrapRating(internalId, doc);
-         Offers offers = available ? scrapOffers(doc) : new Offers();
+         List<String> images = crawlImages(data);
+         String primaryImage = images != null && !images.isEmpty() ? images.remove(0) : null;
+         String description = JSONUtils.getValueRecursive(data, "item_linkWeb.linkWeb.descricaoHtml", String.class);
+         Integer stock = JSONUtils.getValueRecursive(data, "estoque.estAtual", Integer.class, 0);
+         boolean available = stock > 0;
+         List<String> eans = crawlEan(data);
+         Offers offers = available ? scrapOffers(data) : new Offers();
 
          // Creating the product
          Product product = ProductBuilder.create()
-               .setUrl(session.getOriginalURL())
-               .setInternalId(internalId)
-               .setInternalPid(internalPid)
-               .setName(name)
-               .setCategory1(categories.getCategory(0))
-               .setPrimaryImage(primaryImage)
-               .setDescription(description)
-               .setStock(stock)
-               .setRatingReviews(ratingsReviews)
-               .setOffers(offers)
-               .build();
+            .setUrl(session.getOriginalURL())
+            .setInternalId(internalId)
+            .setInternalPid(internalPid)
+            .setName(name)
+            .setPrimaryImage(primaryImage)
+            .setSecondaryImages(images)
+            .setDescription(description)
+            .setStock(stock)
+            .setOffers(offers)
+            .setEans(eans)
+            .build();
 
          products.add(product);
 
@@ -87,82 +115,85 @@ public class BrasilEsalpetCrawler extends Crawler {
       return products;
    }
 
-   private boolean isProductPage(Document doc) {
-      return doc.selectFirst(".product") != null;
+   private List<String> crawlEan(JSONObject data) {
+      String ean = data.optString("ean");
+      if (ean != null && !ean.isEmpty()) {
+         return Collections.singletonList(ean);
+      }
+      return Collections.emptyList();
    }
 
-   private String crawlPrimaryImage(Document doc) {
-      String primaryImage = null;
-      Element primaryImageElement = doc.selectFirst(".thumbs .img-fluid");
+   private List<String> crawlImages(JSONObject data) {
+      List<String> images = new ArrayList<>();
 
-      if (primaryImageElement != null) {
-         primaryImage = primaryImageElement.attr("src").trim();
-
-         if (primaryImage.contains("?")) {
-            primaryImage = primaryImage.split("\\?")[0];
+      JSONArray imagesArray = data.optJSONArray("fotos");
+      if (imagesArray != null) {
+         for (Object obj : imagesArray) {
+            if (obj instanceof String) {
+               String image = (String) obj;
+               String url = "https://esal.api.alleshub.com.br/images/erp_esalpet/item/" + image + ".jpg";
+               images.add(url);
+            } else {
+               break;
+            }
          }
       }
 
-      return primaryImage;
+      return images;
+
    }
 
-   private RatingsReviews scrapRating(String internalId, Document doc) {
-      TrustvoxRatingCrawler trustVox = new TrustvoxRatingCrawler(session, "109465", logger);
-      return trustVox.extractRatingAndReviews(internalId, doc, dataFetcher);
-   }
-
-   private Offers scrapOffers(Document doc) throws OfferException, MalformedPricingException {
+   private Offers scrapOffers(JSONObject data) throws OfferException, MalformedPricingException {
       Offers offers = new Offers();
-      Pricing pricing = scrapPricing(doc);
+      Pricing pricing = scrapPricing(data);
       List<String> sales = new ArrayList<>();
 
       offers.add(OfferBuilder.create()
-            .setUseSlugNameAsInternalSellerId(true)
-            .setSellerFullName(SELLER_FULL_NAME)
-            .setMainPagePosition(1)
-            .setIsBuybox(false)
-            .setIsMainRetailer(true)
-            .setPricing(pricing)
-            .setSales(sales)
-            .build());
+         .setUseSlugNameAsInternalSellerId(true)
+         .setSellerFullName(SELLER_FULL_NAME)
+         .setMainPagePosition(1)
+         .setIsBuybox(false)
+         .setIsMainRetailer(true)
+         .setPricing(pricing)
+         .setSales(sales)
+         .build());
 
       return offers;
 
    }
 
-   private Pricing scrapPricing(Document doc) throws MalformedPricingException {
-      Double spotlightPrice = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".product-price-final span.total", null, true, ',', session);
-      Double priceFrom = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".product-price-discount.mr-4 span", null, true, ',', session);
+   private Pricing scrapPricing(JSONObject data) throws MalformedPricingException {
+      Double spotlightPrice = JSONUtils.getValueRecursive(data, "tabela.vVenda", Double.class, null);
+      //  Double priceFrom = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".product-price-discount.mr-4 span", null, true, ',', session);
 
-      CreditCards creditCards = scrapCreditCards(doc, spotlightPrice);
+      CreditCards creditCards = scrapCreditCards(spotlightPrice);
       BankSlip bankSlip = BankSlipBuilder.create()
-            .setFinalPrice(spotlightPrice)
-            .build();
+         .setFinalPrice(spotlightPrice)
+         .build();
 
       return PricingBuilder.create()
-            .setSpotlightPrice(spotlightPrice)
-            .setPriceFrom(priceFrom)
-            .setCreditCards(creditCards)
-            .setBankSlip(bankSlip)
-            .build();
+         .setSpotlightPrice(spotlightPrice)
+         .setCreditCards(creditCards)
+         .setBankSlip(bankSlip)
+         .build();
    }
 
-   private CreditCards scrapCreditCards(Document doc, Double spotlightPrice) throws MalformedPricingException {
+   private CreditCards scrapCreditCards(Double spotlightPrice) throws MalformedPricingException {
       CreditCards creditCards = new CreditCards();
 
       Installments installments = new Installments();
-         installments.add(InstallmentBuilder.create()
-               .setInstallmentNumber(1)
-               .setInstallmentPrice(spotlightPrice)
-               .build());
+      installments.add(InstallmentBuilder.create()
+         .setInstallmentNumber(1)
+         .setInstallmentPrice(spotlightPrice)
+         .build());
 
 
       for (String card : cards) {
          creditCards.add(CreditCardBuilder.create()
-               .setBrand(card)
-               .setInstallments(installments)
-               .setIsShopCard(false)
-               .build());
+            .setBrand(card)
+            .setInstallments(installments)
+            .setIsShopCard(false)
+            .build());
       }
 
       return creditCards;
--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilEsalpetCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilEsalpetCrawler.java
@@ -1,101 +1,122 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
 
+import br.com.lett.crawlernode.core.fetcher.ProxyCollection;
+import br.com.lett.crawlernode.core.fetcher.methods.ApacheDataFetcher;
+import br.com.lett.crawlernode.core.fetcher.methods.FetcherDataFetcher;
+import br.com.lett.crawlernode.core.fetcher.methods.JsoupDataFetcher;
 import br.com.lett.crawlernode.core.fetcher.models.Request;
 import br.com.lett.crawlernode.core.fetcher.models.Request.RequestBuilder;
+import br.com.lett.crawlernode.core.fetcher.models.Response;
+import br.com.lett.crawlernode.core.models.RankingProduct;
+import br.com.lett.crawlernode.core.models.RankingProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.exceptions.MalformedProductException;
+import br.com.lett.crawlernode.util.CommonMethods;
 import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.JSONUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class BrasilEsalpetCrawler extends CrawlerRankingKeywords {
-  
-  private static final String HOME_PAGE = "www.esalpet.com.br";
-  private static String searchUrl = "";
 
-  public BrasilEsalpetCrawler(Session session) {
-    super(session);
-  }
-  
-  private void fetchSeachUrl() {
-    
-    String url = "https://www.esalpet.com.br/cart/search";
-    
-    Map<String, String> headers = new HashMap<>();
-    headers.put("content-type", "application/x-www-form-urlencoded");
-    
-    String payload = "term=" + this.keywordEncoded;
-    
-    Request request = RequestBuilder.create()
-        .setUrl(url)
-        .setHeaders(headers)
-        .setCookies(cookies)
-        .setPayload(payload)
-        .build();
-    
-    
-    String redirectUrl = this.dataFetcher.post(session, request).getRedirectUrl();
-    if(redirectUrl != null && redirectUrl.endsWith("/0")) {
-      searchUrl = redirectUrl.substring(0, redirectUrl.length() - "/0".length());
-    }
-  }
-  
-  @Override
-  protected void extractProductsFromCurrentPage() {
-    this.pageSize = 24;
-    this.log("Página " + this.currentPage);
-    
-    fetchSeachUrl();
+   public BrasilEsalpetCrawler(Session session) {
+      super(session);
+   }
 
-    String url = searchUrl + "/" + ((this.currentPage-1) * 24);
-    
-    this.log("Link onde são feitos os crawlers: " + url);
-    this.currentDoc = fetchDocument(url);
-    
-    JSONObject idsJson = CrawlerUtils.selectJsonFromHtml(this.currentDoc, "script", "fbq('track', 'Search', ", ");", false, true);
-    JSONArray ids = new JSONArray();
-    Elements products = this.currentDoc.select(".prod-list .prod-wrapper");
-    
-    if(idsJson.has("content_ids") && idsJson.get("content_ids") instanceof JSONArray) {
-      ids = idsJson.getJSONArray("content_ids");
-    }
+   private JSONObject fetchApi() {
 
-    if (!products.isEmpty() && ids.length() == products.size()) {
-      for(int i = 0; i < products.size(); i++) {
-        Element e = products.get(i);
+      String url = "https://esal.api.alleshub.com.br/api_ecommerce/getItensBySearchTerm";
 
-        String internalId = ids.getString(i);
-        String productUrl = CrawlerUtils.scrapUrl(e, "> a[href]", "href", "https", HOME_PAGE);
+      Map<String, String> headers = new HashMap<>();
+      headers.put("BaseConn", "{\"base\":\"erp_esalpet\",\"emp_id\":1}");
+      headers.put("Content-Type", "application/json");
+      headers.put("Host", "esal.api.alleshub.com.br");
+      headers.put("Origin", "https://www.esalpet.com.br");
+      headers.put("Referer", "https://www.esalpet.com.br/");
+      headers.put("x-requested-with", "XMLHttpRequest");
 
-        saveDataProduct(internalId, null, productUrl);
+      String payload = "{\"term\":\"racao\",\"exactMatch\":false}";
 
-        this.log(
-            "Position: " + this.position + 
-            " - InternalId: " + internalId +
-            " - InternalPid: " + null + 
-            " - Url: " + productUrl);
-        
-        if (this.arrayProducts.size() == productsLimit)
-          break;
+      Request request = RequestBuilder.create()
+         .setUrl(url)
+         .setHeaders(headers)
+         .setPayload(payload)
+         .setProxyservice(List.of(ProxyCollection.BUY, ProxyCollection.NETNUT_RESIDENTIAL_BR, ProxyCollection.SMART_PROXY_BR))
+         .build();
+
+      Response response = CrawlerUtils.retryRequestWithListDataFetcher(request, List.of(new JsoupDataFetcher(), new FetcherDataFetcher(), new ApacheDataFetcher()), session, "post");
+      return CrawlerUtils.stringToJson(response.getBody());
+
+   }
+
+   @Override
+   protected void extractProductsFromCurrentPage() throws MalformedProductException {
+
+      JSONObject json = fetchApi();
+      JSONArray products = json != null ? json.optJSONArray("data") : null;
+
+      if (products != null && !products.isEmpty()) {
+         for (Object obj : products) {
+            if (obj instanceof JSONObject) {
+               JSONObject product = (JSONObject) obj;
+               String internalId = integerToString(JSONUtils.getValueRecursive(product, "item_linkWeb.item_id", Integer.class));
+               String internalPid = integerToString(JSONUtils.getValueRecursive(product, "item_linkWeb.ecommerce_id", Integer.class));
+               String name = JSONUtils.getValueRecursive(product, "item_linkWeb.linkWeb.titulo", String.class);
+               String productUrl = crawlUrl(name, internalId);
+               String imageUrl = crawlImage(product);
+               boolean available = product.opt("estoque") != null;
+               Integer price = available ? JSONUtils.getValueRecursive(product, "tabela.Vvenda", Integer.class, 0) : null;
+
+               RankingProduct productRanking = RankingProductBuilder.create()
+                  .setUrl(productUrl)
+                  .setInternalId(internalId)
+                  .setInternalPid(internalPid)
+                  .setName(name)
+                  .setPriceInCents(price)
+                  .setAvailability(available)
+                  .setImageUrl(imageUrl)
+                  .build();
+
+               saveDataProduct(productRanking);
+
+               if (this.arrayProducts.size() == productsLimit)
+                  break;
+            }
+         }
+      } else {
+         this.result = false;
+         this.log("Keyword sem resultado!");
       }
-      
-    } else {
-      this.result = false;
-      this.log("Keyword sem resultado!");
-    }
 
-    this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora "
-        + this.arrayProducts.size() + " produtos crawleados");
-  }
-  
-  @Override
-  protected boolean hasNextPage() {
-    Element pagination = this.currentDoc.selectFirst(".pagination > li:last-child");
-    return pagination != null && !pagination.hasClass("active");
-  }
+      this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora "
+         + this.arrayProducts.size() + " produtos crawleados");
+   }
+
+   private String crawlUrl(String name, String internalId) {
+      String url = "https://www.esalpet.com.br/produto/" + CommonMethods.toSlug(name) + "?id=" + internalId;
+
+      return url;
+   }
+
+   private String integerToString(Integer value) {
+      return value != null ? value.toString() : null;
+   }
+
+   private String crawlImage(JSONObject data) {
+
+      JSONArray imagesArray = data.optJSONArray("fotos");
+
+      if (imagesArray != null) {
+         return "https://esal.api.alleshub.com.br/images/erp_esalpet/item/" + imagesArray.get(0) + ".jpg";
+      }
+
+      return null;
+
+   }
+
 }
