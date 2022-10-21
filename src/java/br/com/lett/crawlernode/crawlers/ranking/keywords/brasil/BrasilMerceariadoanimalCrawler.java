11685d68fe631c3eaa157abb7a0edf6beebde1a0
doferreira <douglassilva@lett.digital>
2022-10-21 10:11:03 -0300

    fix class ranking

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilMerceariadoanimalCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilMerceariadoanimalCrawler.java
@@ -1,6 +1,12 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
+
+import br.com.lett.crawlernode.core.models.RankingProduct;
+import br.com.lett.crawlernode.core.models.RankingProductBuilder;
+import br.com.lett.crawlernode.exceptions.MalformedProductException;
+import br.com.lett.crawlernode.util.CommonMethods;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import br.com.lett.crawlernode.core.session.Session;
@@ -8,53 +14,60 @@ import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
 import br.com.lett.crawlernode.util.CrawlerUtils;
 
 public class BrasilMerceariadoanimalCrawler extends CrawlerRankingKeywords {
-  
-  private static final String HOME_PAGE = "merceariadoanimal.com.br";
 
-  public BrasilMerceariadoanimalCrawler(Session session) {
-    super(session);
-  }
+   private static final String HOME_PAGE = "https://www.merceariadoanimal.com.br/";
 
-  @Override
-  protected void extractProductsFromCurrentPage() {
-    this.pageSize = 18;
-    this.log("Página " + this.currentPage);
+   public BrasilMerceariadoanimalCrawler(Session session) {
+      super(session);
+   }
 
-    String url = "https://www.merceariadoanimal.com.br/" 
-        + this.keywordEncoded + "/?page=" + this.currentPage;
-    
-    this.log("Link onde são feitos os crawlers: " + url);
-    this.currentDoc = fetchDocument(url);
-    Elements products = this.currentDoc.select(".ProductList  > li > .ProductImage");
+   @Override
+   protected void extractProductsFromCurrentPage() throws UnsupportedEncodingException, MalformedProductException {
+      this.pageSize = 18;
+      this.log("Página " + this.currentPage);
 
-    if (!products.isEmpty()) {
-      for (Element e : products) {
-        String internalPid = e.hasAttr("data-product") ? e.attr("data-product") : null;
-        String productUrl = CrawlerUtils.scrapUrl(e, "a", Arrays.asList("href"), "https:", HOME_PAGE);
+      String url = HOME_PAGE + "pesquisa.ecm?search_query=" + this.keywordWithoutAccents + "&x=0&y=0&page=" + this.currentPage + "&section=product#results";
 
-        saveDataProduct(null, internalPid, productUrl);
+      this.log("Link onde são feitos os crawlers: " + url);
+      this.currentDoc = fetchDocument(url);
+      Elements products = this.currentDoc.select(".ProductList.Clear > li");
 
-        this.log(
-            "Position: " + this.position + 
-            " - InternalId: " + null +
-            " - InternalPid: " + internalPid + 
-            " - Url: " + productUrl);
-        
-        if (this.arrayProducts.size() == productsLimit)
-          break;
+      if (!products.isEmpty()) {
+
+         for (Element product : products) {
+            String internalPid = CrawlerUtils.scrapStringSimpleInfoByAttribute(product, ".ProductImage.QuickView", "data-product");
+            String productUrl = CrawlerUtils.scrapStringSimpleInfoByAttribute(product, ".ProductImage.QuickView > a", "href");
+            String productName = CrawlerUtils.scrapStringSimpleInfoByAttribute(product, "a.TrackLink > img", "alt");
+            String imageUrl = CrawlerUtils.scrapStringSimpleInfoByAttribute(product, "div.ProductImage.QuickView > a > img", "src");
+            Integer price = CrawlerUtils.scrapPriceInCentsFromHtml(product, "div.ProductPriceRating > em:nth-child(1)", null, false, ',', session, 0);
+            String buttonAvailable = CrawlerUtils.scrapStringSimpleInfoByAttribute(product, ".ProdutoNaoDisponivel", "style");
+            boolean isAvailable = buttonAvailable != null && buttonAvailable.contains("display:none;");
+            if(!isAvailable){
+               price = null;
+            }
+
+            RankingProduct productRanking = RankingProductBuilder.create()
+               .setUrl(productUrl)
+               .setInternalPid(internalPid)
+               .setName(productName)
+               .setPriceInCents(price)
+               .setAvailability(isAvailable)
+               .setImageUrl(imageUrl)
+               .build();
+
+            saveDataProduct(productRanking);
+            if (this.arrayProducts.size() == productsLimit)
+               break;
+         }
+      } else {
+         this.result = false;
+         this.log("Keyword sem resultado!");
       }
-      
-    } else {
-      this.result = false;
-      this.log("Keyword sem resultado!");
-    }
+      this.log("Finalizando Crawler de produtos da página: " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
+   }
 
-    this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora "
-        + this.arrayProducts.size() + " produtos crawleados");
-  }
-  
-  @Override
-  protected boolean hasNextPage() {
-    return this.currentDoc.selectFirst(".CategoryPagination .FloatRight > a") != null;
-  }
+   @Override
+   protected boolean hasNextPage() {
+      return this.currentDoc.selectFirst(".CategoryPagination .FloatRight > a") != null;
+   }
 }
