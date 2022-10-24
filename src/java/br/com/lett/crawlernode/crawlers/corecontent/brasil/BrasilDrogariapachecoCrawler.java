c4b3cdeb23e32ddab7e7a767eff6a86f8d9cf8c2
Luiza <lrsofal@sga.pucminas.br>
2022-10-24 11:13:19 -0300

    [Pacheco] Miranha config

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilDrogariapachecoCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilDrogariapachecoCrawler.java
@@ -1,20 +1,14 @@
 package br.com.lett.crawlernode.crawlers.corecontent.brasil;
 
-import br.com.lett.crawlernode.core.fetcher.CrawlerWebdriver;
-import br.com.lett.crawlernode.core.fetcher.DynamicDataFetcher;
-import br.com.lett.crawlernode.core.fetcher.ProxyCollection;
+import br.com.lett.crawlernode.core.fetcher.FetchMode;
 import br.com.lett.crawlernode.core.fetcher.models.Request;
 import br.com.lett.crawlernode.core.models.Card;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.crawlers.extractionutils.core.VTEXOldNewImpl;
-import br.com.lett.crawlernode.util.CommonMethods;
 import br.com.lett.crawlernode.util.CrawlerUtils;
-import br.com.lett.crawlernode.util.Logging;
 import cdjd.com.google.common.collect.Sets;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,28 +19,9 @@ public class BrasilDrogariapachecoCrawler extends VTEXOldNewImpl {
    protected Set<String> cards = Sets.newHashSet(Card.VISA.toString(), Card.MASTERCARD.toString(),
       Card.AURA.toString(), Card.DINERS.toString(), Card.HIPER.toString(), Card.AMEX.toString());
 
-
    public BrasilDrogariapachecoCrawler(Session session) {
       super(session);
-   }
-
-   @Override
-   protected Object fetch() {
-
-      Document document = null;
-      CrawlerWebdriver webdriver;
-      try {
-         webdriver = DynamicDataFetcher.fetchPageWebdriver(session.getOriginalURL(), ProxyCollection.BUY_HAPROXY, session);
-         webdriver.waitLoad(12000);
-
-         if (webdriver != null) {
-            document = Jsoup.parse(webdriver.getCurrentPageSource());
-            webdriver.terminate();
-         }
-      } catch (Exception e) {
-         Logging.printLogInfo(logger, session, CommonMethods.getStackTrace(e));
-      }
-      return document;
+      super.config.setFetcher(FetchMode.MIRANHA);
    }
 
    @Override
