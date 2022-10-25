6404d3ff134b95e529902d827e4faa5e0fc63517
Caroline <cdsevangelista@inf.ufpel.edu.br>
2022-10-25 19:20:50 -0300

    Updating core and ranking super irmao

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/caratinga/CaratingaSuperirmaoCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/caratinga/CaratingaSuperirmaoCrawler.java
@@ -1,10 +1,15 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.caratinga;
 
+import br.com.lett.crawlernode.core.models.RankingProduct;
+import br.com.lett.crawlernode.core.models.RankingProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.exceptions.MalformedProductException;
 import br.com.lett.crawlernode.util.CrawlerUtils;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import java.util.Collections;
 
 public class CaratingaSuperirmaoCrawler extends CrawlerRankingKeywords {
 
@@ -13,7 +18,7 @@ public class CaratingaSuperirmaoCrawler extends CrawlerRankingKeywords {
    }
 
    @Override
-   protected void extractProductsFromCurrentPage() {
+   protected void extractProductsFromCurrentPage() throws MalformedProductException {
       //número de produtos por página do market
       this.pageSize = 12;
 
@@ -28,17 +33,31 @@ public class CaratingaSuperirmaoCrawler extends CrawlerRankingKeywords {
          if (this.totalProducts == 0) setTotalProducts();
          for (Element e : products) {
 
-            // InternalPid
-            String internalId = CrawlerUtils.scrapStringSimpleInfoByAttribute(e, ".btn", "href").split("add/")[1];
-            // InternalId
+            String urlId = CrawlerUtils.scrapStringSimpleInfoByAttribute(e, "input", "name");
+            String internalId = urlId.substring(urlId.indexOf("[")+1, urlId.indexOf("]"));
+
             String internalPid = internalId;
 
-            // Url do produto
-            String productUrl = CrawlerUtils.scrapStringSimpleInfoByAttribute(e, ".col-auto a", "href");
+            String productUrl = CrawlerUtils.scrapStringSimpleInfoByAttribute(e, "a", "href");
+            String name = CrawlerUtils.scrapStringSimpleInfo(e, ".titulo b", true);
+            String imgUrl = CrawlerUtils.scrapSimplePrimaryImage(e, "img", Collections.singletonList("src"), "https", "superirmao.loji.com.br");
+            Integer price = CrawlerUtils.scrapPriceInCentsFromHtml(e, ".preco b", null, false, ',', session, null);
+            boolean isAvailable = price != null;
 
-            saveDataProduct(internalId, internalPid, productUrl);
+            RankingProduct productRanking = RankingProductBuilder.create()
+               .setUrl(productUrl)
+               .setInternalId(internalId)
+               .setInternalPid(internalPid)
+               .setImageUrl(imgUrl)
+               .setName(name)
+               .setPriceInCents(price)
+               .setAvailability(isAvailable)
+               .build();
 
-            if (this.arrayProducts.size() == productsLimit) break;
+            saveDataProduct(productRanking);
+            if (this.arrayProducts.size() == productsLimit) {
+               break;
+            }
 
          }
       } else {
