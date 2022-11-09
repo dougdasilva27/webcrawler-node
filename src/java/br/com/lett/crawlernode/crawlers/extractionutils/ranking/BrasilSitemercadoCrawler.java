ff600ae4d2a7236e3a8c026b1fd8bfba814fffb5
mazzillio <mateca500@gmail.com>
2022-11-09 13:19:48 -0300

    Fix api url

--- src/java/br/com/lett/crawlernode/crawlers/extractionutils/ranking/BrasilSitemercadoCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/extractionutils/ranking/BrasilSitemercadoCrawler.java
@@ -171,7 +171,8 @@ public class BrasilSitemercadoCrawler extends CrawlerRankingKeywords {
       }
 
 
-      String apiUrl = apiSearchUrl(lojaId) + this.keywordEncoded.replace("+", "%20");
+      String apiUrl = apiSearchUrl(lojaId) + this.keywordEncoded.replace("+", "%20") + "&enable_new_search=false";
+      //String apiUrl = apiSearchUrl(lojaId) + this.keywordEncoded.replace("+", "%20");
       Request requestApi = RequestBuilder.create()
          .setUrl(apiUrl)
          .setCookies(cookies)
