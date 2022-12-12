de5951811ef30324056c1abf311cc9ba11ef9eaf
doferreira <douglassilva@lett.digital>
2022-12-12 13:52:59 -0300

    change selector description

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilWebcontinentalCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilWebcontinentalCrawler.java
@@ -1,29 +1,27 @@
 package br.com.lett.crawlernode.crawlers.corecontent.brasil;
 
-import br.com.lett.crawlernode.core.fetcher.methods.FetcherDataFetcher;
-import br.com.lett.crawlernode.core.fetcher.models.Request;
-import br.com.lett.crawlernode.core.fetcher.models.Request.RequestBuilder;
 import br.com.lett.crawlernode.core.models.Card;
-import br.com.lett.crawlernode.core.models.CategoryCollection;
 import br.com.lett.crawlernode.core.models.Product;
 import br.com.lett.crawlernode.core.models.ProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.Crawler;
-import br.com.lett.crawlernode.util.*;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.JSONUtils;
+import br.com.lett.crawlernode.util.Logging;
 import com.google.common.collect.Sets;
 import exceptions.MalformedPricingException;
 import exceptions.OfferException;
-import models.*;
-import models.prices.Prices;
+import models.Offer;
+import models.Offers;
 import models.pricing.*;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 public class BrasilWebcontinentalCrawler extends Crawler {
 
@@ -58,7 +56,7 @@ public class BrasilWebcontinentalCrawler extends Crawler {
             String internalId = jsonObject.optString("sku");
             String internalPid = jsonObject.optString("productId");
             String name = jsonObject.optString("name");
-            String description = jsonObject.optString("description");
+            String description = getDescription(doc);
             String image = jsonObject.optString("image");
             JSONObject offerJson = jsonObject.optJSONObject("offers");
             boolean available = offerJson.optString("availability", "").equals("https://schema.org/InStock");
@@ -121,5 +119,12 @@ public class BrasilWebcontinentalCrawler extends Crawler {
       return offers;
    }
 
+   private String getDescription(Document doc) {
+      String simpleDescription = CrawlerUtils.scrapStringSimpleInfo(doc, "article > div > .txt-cabecalho", false);
+      if (simpleDescription == null) {
+         simpleDescription = CrawlerUtils.scrapSimpleDescription(doc, Arrays.asList(".ProductLongDescription"));
+      }
+      return simpleDescription;
+   }
 
 }
