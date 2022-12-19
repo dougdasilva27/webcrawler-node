fa27d9d93abd31fa1d372462d19d8f1bc32182a5
Thales Rodrigues <thalessantos30@gmail.com>
2022-12-19 17:58:03 -0300

    updating carrefour espana crawler

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/espana/EspanaCarrefourCrawler.kt
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/espana/EspanaCarrefourCrawler.kt
@@ -1,10 +1,13 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.espana
 
+import br.com.lett.crawlernode.core.fetcher.ProxyCollection
 import br.com.lett.crawlernode.core.fetcher.models.Request.RequestBuilder
+import br.com.lett.crawlernode.core.models.RankingProductBuilder
 import br.com.lett.crawlernode.core.session.Session
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords
 import br.com.lett.crawlernode.util.toJson
 import org.json.JSONObject
+import java.util.*
 
 class EspanaCarrefourCrawler(session: Session) : CrawlerRankingKeywords(session) {
   var row = 0;
@@ -12,7 +15,13 @@ class EspanaCarrefourCrawler(session: Session) : CrawlerRankingKeywords(session)
   override fun extractProductsFromCurrentPage() {
     val url = "https://www.carrefour.es/search-api/query/v1/search?query=$keywordEncoded&scope=desktop&lang=es&rows=${row + 24}&start=$row&origin=default&f.op=OR"
 
-    val jsonBody = dataFetcher.get(session, RequestBuilder.create().setUrl(url).build()).body.toJson()
+    val jsonBody = dataFetcher.get(session, RequestBuilder.create()
+       .setUrl(url)
+       .setProxyservice(Arrays.asList(
+          ProxyCollection.NETNUT_RESIDENTIAL_ES_HAPROXY,
+          ProxyCollection.NETNUT_RESIDENTIAL_ES
+       ))
+       .build()).body.toJson()
       .optJSONObject("content")
     if (row == 0) {
       totalProducts = jsonBody.optInt("numFound")
@@ -23,12 +32,21 @@ class EspanaCarrefourCrawler(session: Session) : CrawlerRankingKeywords(session)
       if (jsonProduct is JSONObject) {
   		   val productUrl =  "https://www.carrefour.es${jsonProduct.optString("url")}"
          val internalId = jsonProduct.optString("catalog_ref_id")
-         val internalPid = jsonProduct.optString("product_id")
-        
-		     saveDataProduct(internalId, internalPid, productUrl)
-		     log("Position: $position - InternalId: $internalId - InternalPid: $internalPid - Url: $productUrl")
-		     
-		     if (arrayProducts.size == productsLimit) {
+         val productName = jsonProduct.optString("display_name")
+         val price = if (jsonProduct.optDouble("active_price") != 0.0) (jsonProduct.optDouble("active_price") * 100.0).toInt() else null
+         val isAvailable = price != null
+
+         val productRanking = RankingProductBuilder.create()
+            .setUrl(productUrl)
+            .setInternalId(internalId)
+            .setName(productName)
+            .setPriceInCents(price)
+            .setAvailability(isAvailable)
+            .build()
+
+         saveDataProduct(productRanking)
+
+        if (arrayProducts.size == productsLimit) {
             break;
          }
       }
