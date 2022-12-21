826bc8cbcd494723e501c889291cd4b509e19efb
Luiza <lrsofal@sga.pucminas.br>
2022-12-21 09:27:51 -0300

    [PoliShop] Crawl Variant name

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilPolishopCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilPolishopCrawler.java
@@ -1,14 +1,15 @@
 package br.com.lett.crawlernode.crawlers.corecontent.brasil;
 
-import java.util.Arrays;
-import java.util.List;
-import org.json.JSONObject;
-import org.jsoup.nodes.Document;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.crawlers.extractionutils.core.VTEXNewScraper;
 import br.com.lett.crawlernode.crawlers.extractionutils.core.YourreviewsRatingCrawler;
 import models.AdvancedRatingReview;
 import models.RatingsReviews;
+import org.json.JSONObject;
+import org.jsoup.nodes.Document;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class BrasilPolishopCrawler extends VTEXNewScraper {
 
@@ -55,4 +56,27 @@ public class BrasilPolishopCrawler extends VTEXNewScraper {
       return ratingReviews;
    }
 
+   @Override
+   protected String scrapName(Document doc, JSONObject productJson, JSONObject jsonSku) {
+      String name = null;
+
+      if (jsonSku.has("nameComplete")) {
+         name = jsonSku.optString("nameComplete");
+      }
+
+      if (name == null && productJson.has("productName") && productJson.opt("productName") != null) {
+         name = productJson.optString("productName");
+
+      } else if (name == null && jsonSku.has("name")) {
+         name = jsonSku.optString("name");
+      }
+
+      if (name != null && !name.isEmpty() && productJson.has("brand")) {
+         String brand = productJson.optString("brand");
+         if (brand != null && !brand.isEmpty() && !checkIfNameHasBrand(brand, name)) {
+            name = name + " - " + brand;
+         }
+      }
+      return name;
+   }
 }
