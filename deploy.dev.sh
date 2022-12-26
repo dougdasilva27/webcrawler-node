a31088fc27ddc9cc1b9fbb3e9e1af336894db7a1
marcosmoura <marcosmourabro@gmail.com>
2022-12-26 15:06:20 -0300

    change some stuff

--- deploy.dev.sh
+++ deploy.dev.sh
@@ -3,13 +3,17 @@ set -e
 
 $(aws ecr get-login --no-include-email)
 
-export ENVIROMENT_DEPLOY=$1
-export ECR_IMAGE_URL=868884350453.dkr.ecr.us-east-1.amazonaws.com/team-data-capture/webcrawler:$1
+export ECR_IMAGE_URL=868884350453.dkr.ecr.us-east-1.amazonaws.com/team-data-capture/webcrawler:dev
 
-npx json -I -f Dockerrun.aws.dev.json -e "this.containerDefinitions[0].image"="'$ECR_IMAGE_URL'"
+echo "building docker image"
+
+docker build -f Dockerfile build .
 
 docker-compose -f build.yml run --rm maven
 docker-compose -f build.yml build webcrawler
 
+echo "pushing docker image to $ECR_IMAGE_URL"
 docker push $ECR_IMAGE_URL
+
+echo "cleaning up $ECR_IMAGE_URL older versions..."
 docker rmi $(docker image ls -q $ECR_IMAGE_URL)
