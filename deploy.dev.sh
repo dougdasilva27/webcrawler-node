c03ce8c2e4de18c89d6cd651f9cd4560b1ec3e62
marcosmoura <marcosmourabro@gmail.com>
2022-12-27 09:38:37 -0300

    change deploy.dev

--- deploy.dev.sh
+++ deploy.dev.sh
@@ -5,11 +5,14 @@ $(aws ecr get-login --no-include-email)
 
 export ECR_IMAGE_URL=868884350453.dkr.ecr.us-east-1.amazonaws.com/team-data-capture/webcrawler:dev
 
+echo "Run Maven"
+
+docker-compose -f build.yml run --rm maven
+
 echo "building docker image"
 
 docker build -f Dockerfile --no-cache --force-rm -t $ECR_IMAGE_URL .
 
-docker-compose -f build.yml run --rm maven
 docker-compose -f build.yml build webcrawler
 
 echo "pushing docker image to $ECR_IMAGE_URL"
