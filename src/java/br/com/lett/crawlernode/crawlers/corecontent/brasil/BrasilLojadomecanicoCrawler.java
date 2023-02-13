02b7e32cf995c0b6e4e2b32f2b6844024b12b469
Luiza <lrsofal@sga.pucminas.br>
2023-02-13 09:43:25 -0300

    fix blocking

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilLojadomecanicoCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilLojadomecanicoCrawler.java
@@ -1,30 +1,30 @@
 package br.com.lett.crawlernode.crawlers.corecontent.brasil;
 
-import br.com.lett.crawlernode.core.models.Card;
-import br.com.lett.crawlernode.core.models.CategoryCollection;
-import br.com.lett.crawlernode.core.models.Product;
-import br.com.lett.crawlernode.core.models.ProductBuilder;
+import br.com.lett.crawlernode.core.fetcher.FetchMode;
+import br.com.lett.crawlernode.core.fetcher.ProxyCollection;
+import br.com.lett.crawlernode.core.fetcher.models.Request;
+import br.com.lett.crawlernode.core.fetcher.models.Response;
+import br.com.lett.crawlernode.core.models.*;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.Crawler;
 import br.com.lett.crawlernode.util.CrawlerUtils;
 import br.com.lett.crawlernode.util.JSONUtils;
 import br.com.lett.crawlernode.util.Logging;
-import br.com.lett.crawlernode.util.Pair;
-
-import java.util.*;
-
 import com.google.common.collect.Sets;
 import exceptions.MalformedPricingException;
 import exceptions.OfferException;
 import models.Offer;
 import models.Offers;
 import models.RatingsReviews;
-import models.prices.Prices;
 import models.pricing.*;
 import org.json.JSONObject;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 
 public class BrasilLojadomecanicoCrawler extends Crawler {
@@ -35,12 +35,21 @@ public class BrasilLojadomecanicoCrawler extends Crawler {
 
    public BrasilLojadomecanicoCrawler(Session session) {
       super(session);
+      super.config.setParser(Parser.HTML);
+      super.config.setFetcher(FetchMode.JSOUP);
    }
 
    @Override
-   public boolean shouldVisit() {
-      String href = this.session.getOriginalURL().toLowerCase();
-      return !FILTERS.matcher(href).matches() && (href.startsWith(HOME_PAGE));
+   protected Response fetchResponse() {
+      Request request = Request.RequestBuilder.create()
+         .setUrl(session.getOriginalURL())
+         .setFollowRedirects(true)
+         .setProxyservice(List.of(
+            ProxyCollection.BUY,
+            ProxyCollection.LUMINATI_SERVER_BR,
+            ProxyCollection.NETNUT_RESIDENTIAL_BR))
+         .build();
+      return this.dataFetcher.get(session, request);
    }
 
    @Override
