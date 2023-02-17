572f11b415451b8a48670563be4f42600941cdec
mazzillio <mateca500@gmail.com>
2023-02-16 23:44:04 -0300

    Fix refactor crawler for JSONObject

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilSvicenteCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilSvicenteCrawler.java
@@ -1,54 +1,101 @@
 package br.com.lett.crawlernode.crawlers.corecontent.brasil;
 
-import br.com.lett.crawlernode.core.models.Card;
-import br.com.lett.crawlernode.core.models.CategoryCollection;
-import br.com.lett.crawlernode.core.models.Product;
-import br.com.lett.crawlernode.core.models.ProductBuilder;
+import br.com.lett.crawlernode.core.fetcher.methods.ApacheDataFetcher;
+import br.com.lett.crawlernode.core.fetcher.models.Request;
+import br.com.lett.crawlernode.core.fetcher.models.Response;
+import br.com.lett.crawlernode.core.models.*;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.Crawler;
+import br.com.lett.crawlernode.util.CommonMethods;
 import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.JSONUtils;
 import br.com.lett.crawlernode.util.Logging;
 import com.google.common.collect.Sets;
+import com.google.common.net.HttpHeaders;
 import exceptions.MalformedPricingException;
 import exceptions.OfferException;
 import models.Offer;
 import models.Offers;
 import models.pricing.*;
+import org.apache.http.impl.cookie.BasicClientCookie;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
 public class BrasilSvicenteCrawler extends Crawler {
+   private final HashMap<String, String> headers = new HashMap<>();
+
    public BrasilSvicenteCrawler(Session session) {
+
       super(session);
+      super.config.setParser(Parser.JSON);
+      setHeaders();
+   }
+
+   private void setHeaders() {
+      this.headers.put(HttpHeaders.ACCEPT, "application/json, text/javascript, */*; q=0.01");
+      this.headers.put(HttpHeaders.REFERER, "https://www.svicente.com.br/pagina-inicial");
+      this.headers.put("authority", "www.svicente.com.br");
    }
 
    private final static String SELLER_FULL_NAME = "SVicente";
 
+   private String getId() {
+      String split = CommonMethods.getLast(session.getOriginalURL().split("-"));
+      if (split != null && !split.isEmpty()) {
+         return CommonMethods.substring(split, "", ".", true);
+      }
+      return null;
+   }
+
    @Override
-   public List<Product> extractInformation(Document doc) throws Exception {
-      super.extractInformation(doc);
+   protected Response fetchResponse() {
+
+      BasicClientCookie cookieSid = new BasicClientCookie("sid", "ZdgS92V9jyexhnhyBzftwYcvNodFVi0wLhw");
+      cookieSid.setDomain("www.svicente.com.br");
+      cookieSid.setPath("/");
+      this.cookies.add(cookieSid);
+      BasicClientCookie cookieDwsid = new BasicClientCookie("dwsid", "fIaJ9ooIVwT_eaPNcUmwdp5xrYaqI_UTYOM1LQMSE4DyJbSo51Y-DoDMrC9-NtYaW7K84V_VZYZgwg_D_Z9hxw==");
+      cookieDwsid.setDomain("www.svicente.com.br");
+      cookieDwsid.setPath("/");
+      this.cookies.add(cookieDwsid);
+      Request reqForProduct = Request.RequestBuilder.create()
+         .setUrl("https://www.svicente.com.br/on/demandware.store/Sites-SaoVicente-Site/pt_BR/Product-ShowQuickView?pid=" + getId())
+         .setHeaders(this.headers)
+         .setCookies(this.cookies)
+         .build();
+      return new ApacheDataFetcher().get(session, reqForProduct);
+   }
+
+   @Override
+   public List<Product> extractInformation(JSONObject json) throws Exception {
       List<Product> products = new ArrayList<>();
-
-      if (isProductPage(doc)) {
+      if (json != null && json.has("product")) {
          Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
-
-         String internalId = CrawlerUtils.scrapStringSimpleInfoByAttribute(doc, ".productQuantity", "data-pid");
-         String name = CrawlerUtils.scrapStringSimpleInfo(doc, "h1", true);
-         CategoryCollection categories = CrawlerUtils.crawlCategories(doc, ".breadCrumb__option a");
-         String primaryImage = CrawlerUtils.scrapStringSimpleInfoByAttribute(doc, ".productDetails__images__principal__img", "src");
-         boolean availableToBuy = scrapAvailability(doc);
-         Offers offers = availableToBuy ? scrapOffer(doc) : new Offers();
-
+         String internalId = getId();
+         JSONObject productObject = json.optJSONObject("product");
+         String name = productObject.optString("productName");
+         List<String> images = scrapImages(productObject);
+         String primaryImage = images.size() > 0 ? images.remove(0) : null;
+         String description = scrapDescription(productObject);
+         boolean available = productObject.optBoolean("available");
+         Offers offers = available ? scrapOffer(productObject) : new Offers();
          Product product = ProductBuilder.create()
             .setUrl(session.getOriginalURL())
             .setInternalId(internalId)
             .setInternalPid(internalId)
             .setName(name)
-            .setCategories(categories)
+            .setCategories(scrapCategories())
             .setPrimaryImage(primaryImage)
+            .setSecondaryImages(images)
+            .setDescription(description)
             .setOffers(offers)
             .build();
 
@@ -57,22 +104,37 @@ public class BrasilSvicenteCrawler extends Crawler {
       } else {
          Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
       }
-
       return products;
    }
 
-   private boolean scrapAvailability(Document doc) {
-      String availableString = CrawlerUtils.scrapStringSimpleInfo(doc, ".svAddToCartButton.primaryButton", true);
-      if (availableString != null && !availableString.isEmpty()) {
-         return availableString.equals("Comprar");
+   private List<String> scrapImages(JSONObject json) {
+      List<String> images = new ArrayList<>();
+      JSONArray objectsImages = JSONUtils.getValueRecursive(json, "images.large", JSONArray.class, new JSONArray());
+      for (Object o : objectsImages) {
+         JSONObject objImage = (JSONObject) o;
+         String imagePath = JSONUtils.getValueRecursive(objImage, "src.disUrl", String.class, null);
+         if (imagePath != null && !imagePath.isEmpty()) {
+            images.add(imagePath);
+         }
       }
-      return false;
+      return images;
    }
 
-   private Offers scrapOffer(Document doc) throws OfferException, MalformedPricingException {
+   protected String scrapDescription(JSONObject json) throws UnsupportedEncodingException {
+      StringBuilder description = new StringBuilder();
+      description.append("<div>");
+      String longDescription = json.optString("longDescription");
+      if (longDescription != null && !longDescription.isEmpty()) {
+         description.append("<p>" + longDescription + "</p>");
+      }
+      description.append("</div>");
+      return description.toString();
+   }
+
+   private Offers scrapOffer(JSONObject json) throws OfferException, MalformedPricingException {
       Offers offers = new Offers();
-      Pricing pricing = scrapPricing(doc);
-      String saleDiscount = CrawlerUtils.scrapStringSimpleInfo(doc, ".productPrice.productPrice--flag", false);
+      Pricing pricing = scrapPricing(json);
+      String saleDiscount = JSONUtils.getValueRecursive(json, "price.priceDiff.formatted", String.class, null);
       offers.add(Offer.OfferBuilder.create()
          .setUseSlugNameAsInternalSellerId(true)
          .setSellerFullName(SELLER_FULL_NAME)
@@ -86,14 +148,9 @@ public class BrasilSvicenteCrawler extends Crawler {
       return offers;
    }
 
-   private Pricing scrapPricing(Document doc) throws MalformedPricingException {
-      Double spotlightPrice = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".productPrice__lineThrough", null, true, ',', session);
-      Double priceFrom = CrawlerUtils.scrapDoublePriceFromHtml(doc, ".productPrice__price", null, true, ',', session);
-
-      if (spotlightPrice == null) {
-         spotlightPrice = priceFrom;
-         priceFrom = null;
-      }
+   private Pricing scrapPricing(JSONObject json) throws MalformedPricingException {
+      Double spotlightPrice = JSONUtils.getValueRecursive(json, "price.sales.value", Double.class, null);
+      Double priceFrom = JSONUtils.getValueRecursive(json, "price.list.value", Double.class, null);
 
       CreditCards creditCards = scrapCreditCards(spotlightPrice);
 
@@ -126,7 +183,18 @@ public class BrasilSvicenteCrawler extends Crawler {
       return creditCards;
    }
 
-   private boolean isProductPage(Document doc) {
-      return doc.selectFirst(".productDetails") != null;
+   private CategoryCollection scrapCategories() {
+      Request request = Request.RequestBuilder.create()
+         .setUrl(session.getOriginalURL())
+         .setHeaders(this.headers)
+         .setCookies(this.cookies)
+         .build();
+      Response response = new ApacheDataFetcher().get(session, request);
+      Document doc = Jsoup.parse(response.getBody());
+      CategoryCollection categories = CrawlerUtils.crawlCategories(doc, ".breadCrumb__option a");
+      if (categories != null && !categories.isEmpty()) {
+         return categories;
+      }
+      return new CategoryCollection();
    }
 }
