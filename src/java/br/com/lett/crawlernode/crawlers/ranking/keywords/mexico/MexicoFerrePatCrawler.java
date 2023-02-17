840114e92063b0826d9110be7d94b4f64ba3dd49
mazzillio <mateca500@gmail.com>
2023-02-17 10:31:22 -0300

    feat init ranking crawler

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/mexico/MexicoFerrePatCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/mexico/MexicoFerrePatCrawler.java
@@ -1,11 +1,16 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.mexico;
 
 
+import br.com.lett.crawlernode.core.fetcher.ProxyCollection;
 import br.com.lett.crawlernode.core.fetcher.models.Request;
 import br.com.lett.crawlernode.core.fetcher.models.Response;
+import br.com.lett.crawlernode.core.models.RankingProduct;
+import br.com.lett.crawlernode.core.models.RankingProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
 import br.com.lett.crawlernode.exceptions.MalformedProductException;
+import br.com.lett.crawlernode.util.CommonMethods;
+import br.com.lett.crawlernode.util.CrawlerUtils;
 import br.com.lett.crawlernode.util.JSONUtils;
 import com.google.common.net.HttpHeaders;
 import org.json.JSONArray;
@@ -15,6 +20,7 @@ import org.jsoup.nodes.Element;
 
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
+import java.util.List;
 
 public class MexicoFerrePatCrawler extends CrawlerRankingKeywords {
    public MexicoFerrePatCrawler(Session session) {
@@ -26,13 +32,18 @@ public class MexicoFerrePatCrawler extends CrawlerRankingKeywords {
    @Override
    protected JSONObject fetchJSONObject(String url) {
 
-      String payload = "pagina=" + this.keywordEncoded;
+      String payload = "pagina=" + this.currentPage;
       HashMap<String, String> headers = new HashMap<>();
       headers.put(HttpHeaders.ORIGIN, HOME_PAGE);
       headers.put(HttpHeaders.REFERER, url);
       Request request = Request.RequestBuilder.create()
          .setUrl(url)
          .setHeaders(headers)
+         .setProxyservice(List.of(
+            ProxyCollection.BUY_HAPROXY,
+            ProxyCollection.SMART_PROXY_MX_HAPROXY,
+            ProxyCollection.NETNUT_RESIDENTIAL_MX_HAPROXY
+         ))
          .setPayload(payload)
          .build();
       Response response = this.dataFetcher.post(session, request);
@@ -49,6 +60,24 @@ public class MexicoFerrePatCrawler extends CrawlerRankingKeywords {
       if (json != null && !json.isEmpty() && json.has("products")) {
          for (Object object : json.optJSONArray("products")) {
             Element productHtml = Jsoup.parse(object.toString());
+            String productUrl = CrawlerUtils.scrapStringSimpleInfoByAttribute(productHtml,".product-card a","href");
+            String internalId = CommonMethods.getLast(productUrl.split("/"));
+            String name = CrawlerUtils.scrapStringSimpleInfo(productHtml,".brand",true);
+            Integer price = CrawlerUtils.scrapPriceInCentsFromHtml(productHtml,".new-price",null,true,'.',session,null);
+            String image = CrawlerUtils.scrapStringSimpleInfoByAttribute(productHtml,"img","src");
+
+            RankingProduct productRanking = RankingProductBuilder.create()
+               .setInternalPid(internalId)
+               .setName(name)
+               .setAvailability(true)
+               .setUrl(productUrl)
+               .setImageUrl(image)
+               .setPriceInCents(price)
+               .build();
+            saveDataProduct(productRanking);
+            if (this.arrayProducts.size() == productsLimit) {
+               break;
+            }
 
          }
       }
