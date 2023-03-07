ab2d21bb8359e2353fc0c9c5667107d8b06742bc
doferreira <douglassilva@lett.digital>
2023-03-06 21:31:37 -0300

    refactoring pr

--- src/java/br/com/lett/crawlernode/crawlers/extractionutils/core/GPACrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/extractionutils/core/GPACrawler.java
@@ -8,7 +8,6 @@ import br.com.lett.crawlernode.core.fetcher.models.Request;
 import br.com.lett.crawlernode.core.fetcher.models.Request.RequestBuilder;
 import br.com.lett.crawlernode.core.fetcher.models.Response;
 import br.com.lett.crawlernode.core.models.Card;
-import br.com.lett.crawlernode.core.models.CategoryCollection;
 import br.com.lett.crawlernode.core.models.Product;
 import br.com.lett.crawlernode.core.models.ProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
@@ -35,7 +34,6 @@ import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.cookie.BasicClientCookie;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.net.URISyntaxException;
@@ -141,14 +139,13 @@ public class GPACrawler extends Crawler {
                .setCategories(categories)
                .setPrimaryImage(primaryImage)
                .setSecondaryImages(secondaryImages)
-               .setDescription(description.toString())
+               .setDescription(description)
                .setRatingReviews(ratingsReviews)
                .build();
 
          products.add(product);
 
-      }
-      if (jsonSku.isEmpty()) {
+      } else if (jsonSku.isEmpty()) {
          Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
          JSONObject pageJson = CrawlerUtils.selectJsonFromHtml(doc, "#__NEXT_DATA__", null, null, false, false);
          JSONObject product = JSONUtils.getValueRecursive(pageJson, "props.initialProps.componentProps.product", JSONObject.class, new JSONObject());
@@ -174,7 +171,7 @@ public class GPACrawler extends Crawler {
                   .setCategories(categories)
                   .setPrimaryImage(primaryImage)
                   .setSecondaryImages(secondaryImages)
-                  .setDescription(description.toString())
+                  .setDescription(description)
                   .setRatingReviews(ratingsReviews)
                   .build();
 
