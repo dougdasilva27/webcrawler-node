3267ab9746bb0f7378442b437e097ff0259e55f3
doferreira <douglassilva@lett.digital>
2023-03-14 15:49:50 -0300

    delete headers

--- src/java/br/com/lett/crawlernode/crawlers/extractionutils/ranking/AmericanasmaisCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/extractionutils/ranking/AmericanasmaisCrawler.java
@@ -40,21 +40,6 @@ public class AmericanasmaisCrawler extends CrawlerRankingKeywords {
    }
    private Document fetchPage() {
 
-      Map<String, String> headers = new HashMap<>();
-
-      headers.put("authority", "www.americanas.com.br");
-      headers.put("sec-ch-ua", " \" Not A;Brand\";v=\"99\", \"Chromium\";v=\"90\", \"Google Chrome\";v=\"90\"");
-      headers.put("sec-ch-ua-mobile", "?0");
-      headers.put("upgrade-insecure-requests", "1");
-      headers.put("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.9");
-      headers.put("sec-fetch-site", "none");
-      headers.put("sec-fetch-mode", "navigate");
-      headers.put("sec-fetch-user", "?1");
-      headers.put("sec-fetch-dest", "document");
-      headers.put("user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36");
-      headers.put("accept-language", "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7,es;q=0.6");
-
-
       String url = HOME_PAGE + storeId + "?ordenacao=topSelling&conteudo=" +
          this.keywordEncoded.replace("+", "%20") +
          "&limite=24&offset=" + (this.currentPage - 1) * pageSize;
@@ -62,7 +47,6 @@ public class AmericanasmaisCrawler extends CrawlerRankingKeywords {
       Request request = Request.RequestBuilder.create()
          .setUrl(url)
          .mustSendContentEncoding(false)
-         .setHeaders(headers)
          .setFetcheroptions(
             FetcherOptions.FetcherOptionsBuilder.create()
                .mustUseMovingAverage(false)
