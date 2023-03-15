4d163ebfe8ff665c74b4e177f4cb4a208b123674
mazzillio <mateca500@gmail.com>
2023-03-15 16:08:25 -0300

    fix: url for request api

--- src/java/br/com/lett/crawlernode/crawlers/extractionutils/ranking/CarrefourMercadoRanking.java
+++ src/java/br/com/lett/crawlernode/crawlers/extractionutils/ranking/CarrefourMercadoRanking.java
@@ -50,11 +50,11 @@ public class CarrefourMercadoRanking extends CrawlerRankingKeywords {
       }
    }
 
-   protected  String getHomePage(){
+   protected String getHomePage() {
       return HOME_PAGE;
    }
 
-   protected String getLocation(){
+   protected String getLocation() {
       return session.getOptions().optString("vtex_segment");
    }
 
@@ -73,14 +73,18 @@ public class CarrefourMercadoRanking extends CrawlerRankingKeywords {
       headers.put("accept", "*/*");
       headers.put("authority", "mercado.carrefour.com.br");
       headers.put("referer", "https://mercado.carrefour.com.br/");
-
-      String url = "https://mercado.carrefour.com.br/api/graphql?operationName=ProductsQuery&variables={\"first\":20,\"after\":\"" + (this.currentPage - 1) * this.pageSize + "\",\"sort\":\"score_desc\",\"term\":\"" + this.keywordWithoutAccents + "\",\"selectedFacets\":[{\"key\":\"region-id\",\"value\":\"" + regionId + "\"},{\"key\":\"channel\",\"value\":\"{\\\"salesChannel\\\":\\\"2\\\",\\\"regionId\\\":\\\"" + regionId + "\\\"}\"},{\"key\":\"locale\",\"value\":\"pt-BR\"}]}";
-
+      headers.put("accept-encoding", "gzip, deflate, br");
+      //String url = "https://mercado.carrefour.com.br/api/graphql?operationName=ProductsQuery&variables={\"first\":20,\"after\":\"" + (this.currentPage - 1) * this.pageSize + "\",\"sort\":\"score_desc\",\"term\":\"" + this.keywordWithoutAccents + "\",\"selectedFacets\":[{\"key\":\"region-id\",\"value\":\"" + regionId + "\"},{\"key\":\"channel\",\"value\":\"{\\\"salesChannel\\\":\\\"2\\\",\\\"regionId\\\":\\\"" + regionId + "\\\"}\"},{\"key\":\"locale\",\"value\":\"pt-BR\"}]}";
+      String url = "https://mercado.carrefour.com.br/api/graphql?operationName=ProductsQuery&variables=%7B%22first%22%3A20%2C%22after%22%3A%22" + (this.currentPage - 1) * this.pageSize + "%22%2C%22sort%22%3A%22score_desc%22%2C%22term%22%3A%22" + this.keywordEncoded + "%22%2C%22selectedFacets%22%3A%5B%7B%22key%22%3A%22region-id%22%2C%22value%22%3A%22" + regionId + "%22%7D%2C%7B%22key%22%3A%22channel%22%2C%22value%22%3A%22%7B%5C%22salesChannel%5C%22%3A%5C%222%5C%22%2C%5C%22regionId%5C%22%3A%5C%22v2.BF5CB0A0B2E63790F00315745EDC0A60%5C%22%7D%22%7D%2C%7B%22key%22%3A%22locale%22%2C%22value%22%3A%22pt-BR%22%7D%5D%7D";
       Request request = Request.RequestBuilder.create()
          .setUrl(url)
          .setHeaders(headers)
          .setCookies(this.cookies)
          .setProxyservice(Arrays.asList(
+            ProxyCollection.LUMINATI_SERVER_BR_HAPROXY,
+            ProxyCollection.LUMINATI_SERVER_BR,
+            ProxyCollection.LUMINATI_RESIDENTIAL_BR_HAPROXY,
+            ProxyCollection.SMART_PROXY_BR,
             ProxyCollection.BUY,
             ProxyCollection.BUY_HAPROXY,
             ProxyCollection.NETNUT_RESIDENTIAL_BR,
@@ -92,22 +96,22 @@ public class CarrefourMercadoRanking extends CrawlerRankingKeywords {
    }
 
    @Override
-   protected void extractProductsFromCurrentPage()  {
+   protected void extractProductsFromCurrentPage() {
       this.pageSize = 20;
       this.log("Página " + this.currentPage);
 
       JSONObject jsonResponse = fetchResponse();
 
       if (currentPage == 1) {
-         this.totalProducts = JSONUtils.getValueRecursive(jsonResponse, "data.search.products.pageInfo.totalCount",  ".", Integer.class, 0);
+         this.totalProducts = JSONUtils.getValueRecursive(jsonResponse, "data.search.products.pageInfo.totalCount", ".", Integer.class, 0);
       }
 
-      JSONArray products = JSONUtils.getValueRecursive(jsonResponse, "data.search.products.edges",  JSONArray.class);
+      JSONArray products = JSONUtils.getValueRecursive(jsonResponse, "data.search.products.edges", JSONArray.class);
 
       if (products != null && !products.isEmpty()) {
          for (Object object : products) {
 
-         JSONObject product = ((JSONObject) object).optJSONObject("node");
+            JSONObject product = ((JSONObject) object).optJSONObject("node");
             String productUrl = CrawlerUtils.completeUrl(product.optString("slug"), "https",
                this.getHomePage().replace("https://", "".replace("http://", ""))) + "/p";
             String internalId = product.optString("id");
