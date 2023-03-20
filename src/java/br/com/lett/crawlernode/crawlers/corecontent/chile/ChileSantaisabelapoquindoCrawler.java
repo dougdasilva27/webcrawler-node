92cc81efb88ae925d1803804fb12798d8a5051dd
doferreira <douglassilva@lett.digital>
2023-03-20 10:58:57 -0300

    fix crawler

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/chile/ChileSantaisabelapoquindoCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/chile/ChileSantaisabelapoquindoCrawler.java
@@ -6,9 +6,7 @@ import br.com.lett.crawlernode.core.fetcher.methods.FetcherDataFetcher;
 import br.com.lett.crawlernode.core.fetcher.methods.JsoupDataFetcher;
 import br.com.lett.crawlernode.core.fetcher.models.Request;
 import br.com.lett.crawlernode.core.fetcher.models.Response;
-import br.com.lett.crawlernode.core.models.Card;
-import br.com.lett.crawlernode.core.models.Product;
-import br.com.lett.crawlernode.core.models.ProductBuilder;
+import br.com.lett.crawlernode.core.models.*;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.Crawler;
 import br.com.lett.crawlernode.util.CommonMethods;
@@ -21,17 +19,13 @@ import exceptions.OfferException;
 import models.Offer;
 import models.Offers;
 import models.pricing.*;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.impl.cookie.BasicClientCookie;
-import org.json.JSONException;
+import org.json.JSONArray;
 import org.json.JSONObject;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.io.UnsupportedEncodingException;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -39,162 +33,166 @@ public class ChileSantaisabelapoquindoCrawler extends Crawler {
 
    public ChileSantaisabelapoquindoCrawler(Session session) {
       super(session);
+      super.config.setParser(Parser.JSONARRAY);
    }
 
    private static final String SELLER_FULL_NAME = "Santa Isabel";
    protected Set<String> cards = Sets.newHashSet(Card.VISA.toString(), Card.MASTERCARD.toString(),
       Card.AURA.toString(), Card.DINERS.toString(), Card.HIPER.toString(), Card.AMEX.toString());
 
-   private String seller = session.getOptions().optString("seller","");
-   private  String storeName = session.getOptions().optString("store-name","");
-
-   protected Document fetchDoc() {
-      BasicClientCookie cookie = new BasicClientCookie("seller", seller);
-      cookie.setDomain("www.santaisabel.cl");
-      cookie.setPath("/");
-      this.cookies.add(cookie);
-      BasicClientCookie cookie2 = new BasicClientCookie("store-name", storeName);
-      cookie2.setDomain("www.santaisabel.cl");
-      cookie2.setPath("/");
-      this.cookies.add(cookie2);
-
-      Request request = new Request.RequestBuilder()
-         .setUrl(session.getOriginalURL())
-         .setCookies(cookies)
-         .setProxyservice(List.of(
-            ProxyCollection.NETNUT_RESIDENTIAL_ES_HAPROXY,
-            ProxyCollection.BUY_HAPROXY,
-            ProxyCollection.NETNUT_RESIDENTIAL_CO_HAPROXY,
-            ProxyCollection.NETNUT_RESIDENTIAL_BR_HAPROXY,
-            ProxyCollection.NETNUT_RESIDENTIAL_BR
-         ))
-         .build();
-      Response response = CrawlerUtils.retryRequestWithListDataFetcher(request, List.of(new FetcherDataFetcher(), this.dataFetcher, new JsoupDataFetcher(), new ApacheDataFetcher()), session, "get");
-      return Jsoup.parse(response.getBody());
+   private String storeSaleChannel() {
+      return session.getOptions().optString("store-sale-channel");
    }
 
    @Override
-   public List<Product> extractInformation(Document doc) throws Exception {
-      super.extractInformation(doc);
-      doc = fetchDoc();
+   protected Response fetchResponse() {
+      String linkText = scrapUrlProductName();
+      Map<String, String> headers = new HashMap<>();
+      headers.put("apiKey", "WlVnnB7c1BblmgUPOfg");
+      headers.put("x-account", "apoquindo");
+      headers.put("x-consumer", "santaisabel");
+      String apiUrl = "https://sm-web-api.ecomm.cencosud.com/catalog/api/v1/apoquindo/product/" + linkText + "?sc=" + storeSaleChannel();
+      Request request = new Request.RequestBuilder()
+         .setUrl(apiUrl)
+         .setCookies(cookies)
+         .setHeaders(headers)
+         .setProxyservice(List.of(
+            ProxyCollection.BUY,
+            ProxyCollection.LUMINATI_RESIDENTIAL_BR,
+            ProxyCollection.NETNUT_RESIDENTIAL_CO_HAPROXY
+         ))
+         .build();
+      Response response = CrawlerUtils.retryRequestWithListDataFetcher(request, List.of(new FetcherDataFetcher(), this.dataFetcher, new JsoupDataFetcher(), new ApacheDataFetcher()), session, "get");
+      return response;
+   }
 
-
+   @Override
+   public List<Product> extractInformation(JSONArray responseJson) throws Exception {
+      super.extractInformation(responseJson);
       List<Product> products = new ArrayList<>();
+      JSONObject json = (JSONObject) responseJson.get(0);
 
-      Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
-      if (doc != null) {
-         JSONObject json = scrapJSOMFromHTML(doc);
+      if (json.has("productId")) {
+         Logging.printLogDebug(logger, session, "Product page identified: " + this.session.getOriginalURL());
 
-         if (!json.isEmpty()) {
+         String internalId = JSONUtils.getValueRecursive(json, "items.0.itemId", String.class);
+         String internalPid = json.optString("productId");
+         String name = scrapName(json);
+         CategoryCollection categories = scrapCategories(json);
+         String description = scrapDescription(json);
+         String primaryImage = JSONUtils.getValueRecursive(json, "items.0.images.0.imageUrl", String.class);
+         List<String> secondaryImages = getSecondaryImages(json);
+         Integer stock = JSONUtils.getValueRecursive(json, "items.0.sellers.0.commertialOffer.AvailableQuantity", Integer.class);
+         boolean available = stock > 0 ? true : false;
+         Offers offers = available ? scrapOffers(json) : new Offers();
 
-            String internalId = json.optString("sku");
-            String internalPid = internalId;
-            String name = scrapNameWithBrand(json);
-            List<String> images = getImages(doc);
-            String primaryImage = !images.isEmpty() ? images.remove(0) : json.optString("image");
-            String description = json.optString("description");
-            Integer stock = null;
-            boolean availableToBuy = JSONUtils.getValueRecursive(json, "offers.availability", String.class).contains("InStock");
-            Offers offers = availableToBuy ? scrapOffer(json, doc) : new Offers();
+         Product product = ProductBuilder.create()
+            .setUrl(session.getOriginalURL())
+            .setInternalId(internalId)
+            .setInternalPid(internalPid)
+            .setName(name)
+            .setCategories(categories)
+            .setDescription(description)
+            .setPrimaryImage(primaryImage)
+            .setSecondaryImages(secondaryImages)
+            .setOffers(offers)
+            .build();
 
-            // Creating the product
-            Product product = ProductBuilder.create()
-               .setUrl(session.getOriginalURL())
-               .setInternalId(internalId)
-               .setInternalPid(internalPid)
-               .setName(name)
-               .setPrimaryImage(primaryImage)
-               .setSecondaryImages(images)
-               .setDescription(description)
-               .setStock(stock)
-               .setOffers(offers)
-               .build();
+         products.add(product);
 
-            products.add(product);
-
-
-         } else {
-            Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
-         }
+      } else {
+         Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
       }
+
       return products;
    }
 
-   private String scrapNameWithBrand(JSONObject json) {
-      String name = json.optString("name");
+   private List<String> getSecondaryImages(JSONObject json) {
+      List<String> secondaryImages = new ArrayList<>();
+      JSONObject images = JSONUtils.getValueRecursive(json, "items.0", JSONObject.class);
+      JSONArray imagesList = JSONUtils.getJSONArrayValue(images, "images");
+      if (imagesList != null && !imagesList.isEmpty()) {
+         for (Object e : imagesList) {
+            JSONObject obj = (JSONObject) e;
+            String imageUrl = obj.optString("imageUrl");
+            if (imageUrl != null && !imageUrl.isEmpty()) {
+               secondaryImages.add(imageUrl);
+            }
+         }
+         if (secondaryImages.size() > 0) {
+            secondaryImages.remove(0);
+         }
+      }
+      return secondaryImages;
+   }
 
-      JSONObject o = json.optJSONObject("brand");
-      if (o != null) {
-         String brand = o.optString("name");
+   private String scrapUrlProductName() {
+      String regex = "cl\\/(.*)\\/p";
+      Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+      Matcher matcher = pattern.matcher(session.getOriginalURL());
 
-         if (name != null && brand != null) {
-            return name + " - " + brand;
+      if (matcher.find()) {
+         return matcher.group(1);
+      }
+      return null;
+   }
+
+   protected String scrapName(JSONObject productJson) {
+      String name = null;
+      if (productJson.has("productName")) {
+         name = productJson.optString("productName");
+      }
+      if (name != null && !name.isEmpty() && productJson.has("brand")) {
+         String brand = productJson.optString("brand");
+         if (brand != null && !brand.isEmpty() && !checkIfNameHasBrand(brand, name)) {
+            name = name + " " + brand;
          }
       }
       return name;
    }
 
-   private String selectJsonFromHtml(Document doc, String cssElement, String token, String finalIndex) throws JSONException, ArrayIndexOutOfBoundsException, IllegalArgumentException {
+   private CategoryCollection scrapCategories(JSONObject product) {
+      CategoryCollection categories = new CategoryCollection();
+      JSONArray categoriesArray = JSONUtils.getJSONArrayValue(product, "categories");
+      for (int i = categoriesArray.length() - 1; i >= 0; i--) {
+         String path = categoriesArray.get(i).toString();
 
-      String object = null;
-      Elements scripts = doc.select(cssElement);
-
-      for (Element e : scripts) {
-         String script = e.html();
-
-         if (script.contains(token) && (finalIndex == null || script.contains(finalIndex))) {
-            object = script.replace(token, "").replace("\\", "");
-            break;
+         if (path.contains("/")) {
+            categories.add(CommonMethods.getLast(path.split("/")));
          }
       }
 
-      return object;
+      return categories;
    }
 
-   private List<String> getImages(Document doc) {
-      List<String> imageList = new ArrayList<>();
-      String infoProduct = selectJsonFromHtml(doc, "script", "window.__renderData = ", ";");
+   private String scrapDescription(JSONObject productJson) throws UnsupportedEncodingException {
+      String description = JSONUtils.getValueRecursive(productJson, "description", ".", String.class, "");
+      if (description.equals("")) {
+         String complementName = JSONUtils.getValueRecursive(productJson, "items.0.complementName", ".", String.class, "");
+         String nameComplete = JSONUtils.getValueRecursive(productJson, "items.0.nameComplete", ".", String.class, "");
 
-      if (infoProduct != null) {
-         Pattern pattern = Pattern.compile("imageUrl\":\"(.+?)\",\"imageTag");
-         Matcher matcher = pattern.matcher(infoProduct);
-         while (matcher.find()) {
-            imageList.add(matcher.group(1));
-         }
-      }
-      return imageList;
-   }
-
-   private JSONObject getPriceDoc(Document doc) {
-      Element script = doc.selectFirst("script:containsData(window)");
-      String varString = script.toString();
-      String sanitizedString = varString.replace("\\", "");
-      String extractedString = CommonMethods.substring(sanitizedString, "sellers\":[", "]}]", true);
-      return JSONUtils.stringToJson(extractedString);
-   }
-
-   private JSONObject scrapJSOMFromHTML(Document doc) {
-      JSONObject json = new JSONObject();
-      Elements scripts = doc.select("script[type=\"application/ld+json\"]");
-      for (Element s : scripts) {
-         String script = s.html();
-         if (script.contains("Product")) {
-            json = CrawlerUtils.stringToJson(script);
+         if (!complementName.isEmpty() && !complementName.equals(nameComplete)) {
+            description = description.concat(complementName);
          }
       }
 
-      return json;
+      return description;
    }
 
-   private Offers scrapOffer(JSONObject json, Document doc) throws OfferException, MalformedPricingException {
+   private boolean checkIfNameHasBrand(String brand, String name) {
+      String brandStripAccents = StringUtils.stripAccents(brand);
+      String nameStripAccents = StringUtils.stripAccents(name);
+      return nameStripAccents.toLowerCase(Locale.ROOT).contains(brandStripAccents.toLowerCase(Locale.ROOT));
+   }
+
+   private Offers scrapOffers(JSONObject skuInfo) throws MalformedPricingException, OfferException {
       Offers offers = new Offers();
-      JSONObject price = getPriceDoc(doc);
-      Pricing pricing = scrapPricing(price);
-      List<String> sales = new ArrayList<>();
+      Pricing pricing = scrapPricing(skuInfo);
+      List<String> sales = Collections.singletonList(CrawlerUtils.calculateSales(pricing));
 
-      offers.add(Offer.OfferBuilder.create()
+      offers.add(new Offer.OfferBuilder()
          .setUseSlugNameAsInternalSellerId(true)
-         .setSellerFullName(SELLER_FULL_NAME)
+         .setSellerFullName(this.SELLER_FULL_NAME)
          .setMainPagePosition(1)
          .setIsBuybox(false)
          .setIsMainRetailer(true)
@@ -203,35 +201,46 @@ public class ChileSantaisabelapoquindoCrawler extends Crawler {
          .build());
 
       return offers;
-
    }
 
+   private Pricing scrapPricing(JSONObject skuInfo) throws MalformedPricingException {
+      Integer spotlightPriceInt = JSONUtils.getValueRecursive(skuInfo, "items.0.sellers.0.commertialOffer.Price", Integer.class);
+      if (spotlightPriceInt == null) {
+         Double priceDouble = JSONUtils.getValueRecursive(skuInfo, "items.0.sellers.0.commertialOffer.Price", Double.class);
+         spotlightPriceInt = priceDouble.intValue();
+      }
+      spotlightPriceInt = spotlightPriceInt * 100;
+      Integer priceFromInt = JSONUtils.getValueRecursive(skuInfo, "items.0.sellers.0.commertialOffer.ListPrice", Integer.class);
+      if (priceFromInt == null) {
+         Double priceDouble = JSONUtils.getValueRecursive(skuInfo, "items.0.sellers.0.commertialOffer.ListPrice", Double.class);
+         priceFromInt = priceDouble.intValue();
+      }
+      priceFromInt = priceFromInt * 100;
+      Double spotlightPrice = spotlightPriceInt / 100.0;
+      Double priceFrom = priceFromInt / 100.0;
 
-   private Pricing scrapPricing(JSONObject json) throws MalformedPricingException {
 
-      Double rawPrice = 1.0 * JSONUtils.getValueRecursive(json, "commertialOffer.PriceWithoutDiscount", Integer.class, 0);
-      rawPrice = rawPrice == 0 ? null : rawPrice;
-      Double spotlightPrice = 1.0 * JSONUtils.getValueRecursive(json, "commertialOffer.Price", Integer.class, 0);
+      BankSlip bankSlip = CrawlerUtils.setBankSlipOffers(spotlightPrice, null);
       CreditCards creditCards = scrapCreditCards(spotlightPrice);
 
       return Pricing.PricingBuilder.create()
-         .setPriceFrom(rawPrice)
          .setSpotlightPrice(spotlightPrice)
+         .setPriceFrom(priceFrom)
          .setCreditCards(creditCards)
+         .setBankSlip(bankSlip)
          .build();
+
    }
 
-
-   private CreditCards scrapCreditCards(Double spotlightPrice) throws MalformedPricingException {
+   private CreditCards scrapCreditCards(Double price) throws MalformedPricingException {
       CreditCards creditCards = new CreditCards();
-
       Installments installments = new Installments();
-      if (installments.getInstallments().isEmpty()) {
-         installments.add(Installment.InstallmentBuilder.create()
-            .setInstallmentNumber(1)
-            .setInstallmentPrice(spotlightPrice)
-            .build());
-      }
+
+      installments.add(Installment.InstallmentBuilder.create()
+         .setInstallmentNumber(1)
+         .setInstallmentPrice(price)
+         .setFinalPrice(price)
+         .build());
 
       for (String card : cards) {
          creditCards.add(CreditCard.CreditCardBuilder.create()
@@ -243,5 +252,4 @@ public class ChileSantaisabelapoquindoCrawler extends Crawler {
 
       return creditCards;
    }
-
 }
