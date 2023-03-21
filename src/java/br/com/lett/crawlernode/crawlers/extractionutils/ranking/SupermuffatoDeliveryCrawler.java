b86d1a834ff6077e5850743c0cdd1ffdc4a3e8f7
doferreira <douglassilva@lett.digital>
2023-03-21 09:54:43 -0300

    fix ranking webdriver

--- src/java/br/com/lett/crawlernode/crawlers/extractionutils/ranking/SupermuffatoDeliveryCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/extractionutils/ranking/SupermuffatoDeliveryCrawler.java
@@ -2,9 +2,6 @@ package br.com.lett.crawlernode.crawlers.extractionutils.ranking;
 
 import br.com.lett.crawlernode.core.fetcher.DynamicDataFetcher;
 import br.com.lett.crawlernode.core.fetcher.ProxyCollection;
-import br.com.lett.crawlernode.core.fetcher.methods.JsoupDataFetcher;
-import br.com.lett.crawlernode.core.fetcher.models.Request;
-import br.com.lett.crawlernode.core.fetcher.models.Response;
 import br.com.lett.crawlernode.core.models.RankingProduct;
 import br.com.lett.crawlernode.core.models.RankingProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
@@ -23,14 +20,13 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.HashMap;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
-import java.util.Map;
 
 public class SupermuffatoDeliveryCrawler extends CrawlerRankingKeywords {
 
    private static final String BASE_URL = "delivery.supermuffato.com.br/";
-   private String HOME_PAGE = "https://delivery.supermuffato.com.br";
+   private String HOME_PAGE = "https://www.supermuffato.com.br/";
    private String vtexSegment = getVtexSegment();
 
    public SupermuffatoDeliveryCrawler(Session session) {
@@ -50,7 +46,8 @@ public class SupermuffatoDeliveryCrawler extends CrawlerRankingKeywords {
       wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(cssSelector)));
    }
 
-   protected Document fetchDocumentWebDriver(String url) {
+   @Override
+   protected Document fetchDocumentWithWebDriver(String url) {
       String city = getCityCode();
       Document doc = null;
 
@@ -60,18 +57,13 @@ public class SupermuffatoDeliveryCrawler extends CrawlerRankingKeywords {
       do {
 
          try {
-            webdriver = DynamicDataFetcher.fetchPageWebdriver(HOME_PAGE, proxies.get(attempts), session);
+            webdriver = DynamicDataFetcher.fetchPageWebdriver(url, proxies.get(attempts), session);
 
             waitForElement(webdriver.driver, ".form-control optgroup option[value='" + city + "']");
             webdriver.findAndClick(".form-control optgroup option[value='" + city + "']", 30000);
 
             waitForElement(webdriver.driver, "#s-ch-change-channel");
             webdriver.findAndClick("#s-ch-change-channel", 30000);
-            webdriver.waitLoad(10000);
-
-            waitForElement(webdriver.driver, "#s-ch-change-channel");
-            webdriver.findAndClick("#s-ch-change-channel", 30000);
-            webdriver.waitLoad(10000);
 
             waitForElement(webdriver.driver, ".fulltext-search-box.ui-autocomplete-input");
             WebElement pass = webdriver.driver.findElement(By.cssSelector(".fulltext-search-box.ui-autocomplete-input"));
@@ -86,86 +78,54 @@ public class SupermuffatoDeliveryCrawler extends CrawlerRankingKeywords {
          } catch (Exception e) {
             Logging.printLogInfo(logger, session, CommonMethods.getStackTrace(e));
 
-         } finally {
-            if (webdriver != null) {
-               webdriver.terminate();
-            }
          }
       } while (doc == null && attempts++ < 3);
-
+      waitForElement(webdriver.driver, "div.prd-list-item");
       return doc;
    }
 
    @Override
-   protected Document fetchDocument(String url) {
-      Map<String, String> headers = new HashMap<>();
-      headers.put("cookie", vtexSegment);
-
-      Request request = Request.RequestBuilder.create().setUrl(url).setHeaders(headers)
-         .build();
-
-      Response response = new JsoupDataFetcher().get(session, request);
-      return Jsoup.parse(response.getBody());
-
-   }
-
-   @Override
-   protected void extractProductsFromCurrentPage() throws MalformedProductException {
+   protected void extractProductsFromCurrentPage() throws UnsupportedEncodingException, MalformedProductException {
       this.pageSize = 48;
       this.log("Página " + this.currentPage);
-      String url = null;
-
-      if (currentPage == 1) {
-         url = "https://delivery.supermuffato.com.br/" + this.keywordEncoded;
-
-         this.currentDoc = fetchDocumentWebDriver(url);
-
+      if (this.currentPage == 1) {
+         this.currentDoc = fetchDocumentWithWebDriver(HOME_PAGE);
+      } else {
+         this.currentDoc = fetchNextPage();
       }
+      this.log("Link onde são feitos os crawlers: " + HOME_PAGE);
 
-      if (currentPage != 1 || this.currentDoc == null) {
-         url = " https://delivery.supermuffato.com.br/buscapagina?ft=" + this.keywordWithoutAccents + "&PS=48&sl=d85149b5-097b-4910-90fd-fa2ce00fe7c9&cc=48&sm=0&PageNumber=" + this.currentPage;
 
-         this.currentDoc = fetchDocument(url);
-      }
-
-      this.log("Link onde são feitos os crawlers: " + url);
-
-      Elements products = this.currentDoc != null ? this.currentDoc.select("div.prd-list-item") : null;
+      Elements products = this.currentDoc.select("div.prd-list-item");
 
       if (products != null && !products.isEmpty()) {
-
-         if (this.totalProducts == 0)
-            setTotalProducts();
-
-         for (int index = 0; index < products.size(); index++) {
-            Element product = products.get(index);
-
+         for (int i = 0; i < products.size(); i++) {
+            Element product = products.get(i);
             String internalPid = product.attr("data-product-id");
+            String name = CrawlerUtils.scrapStringSimpleInfo(product, ".prd-list-item-name", true);
+            String imageUrl = CrawlerUtils.scrapStringSimpleInfoByAttribute(product, "li[layout] .prd-list-item-img img", "src");
             String urlProduct = CrawlerUtils.scrapUrl(product, ".prd-list-item-desc > a", "href", "https", BASE_URL);
             Integer price = crawlPrice(product);
             boolean available = price != null;
 
             RankingProduct rankingProduct = RankingProductBuilder.create()
-               .setName(CrawlerUtils.scrapStringSimpleInfo(product, ".prd-list-item-name", true))
+               .setName(name)
                .setUrl(urlProduct)
                .setInternalPid(internalPid)
-               .setImageUrl(CrawlerUtils.scrapStringSimpleInfoByAttribute(product, "li[layout] .prd-list-item-img img", "src"))
+               .setImageUrl(imageUrl)
                .setAvailability(available)
                .setPriceInCents(price)
                .build();
 
             saveDataProduct(rankingProduct);
-
-            if (this.arrayProducts.size() == productsLimit) break;
+            if (this.arrayProducts.size() == productsLimit)
+               break;
          }
       } else {
          this.result = false;
          this.log("Keyword sem resultado!");
-         setTotalProducts();
       }
-
-      this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
-      if (!(hasNextPage())) setTotalProducts();
+      this.log("Finalizando Crawler de produtos da página: " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
    }
 
    private Integer crawlPrice(Element product) {
@@ -180,10 +140,18 @@ public class SupermuffatoDeliveryCrawler extends CrawlerRankingKeywords {
    }
 
    @Override
-   protected void setTotalProducts() {
-      this.totalProducts = this.currentDoc != null ? CrawlerUtils.scrapIntegerFromHtml(this.currentDoc, ".resultado-busca-numero span.value", true, 0) : 0;
-      this.log("Total da busca: " + this.totalProducts);
+   protected boolean hasNextPage() {
+      return !this.currentDoc.select(".pager.bottom > ul > li.next").isEmpty();
    }
 
+   private Document fetchNextPage() {
+      Logging.printLogDebug(logger, session, "fetching next page...");
+      webdriver.waitForElement(".pager.bottom > ul > li.next", 5);
+      WebElement button = webdriver.driver.findElement(By.cssSelector(".pager.bottom > ul > li.next"));
+      webdriver.clickOnElementViaJavascript(button);
+      webdriver.waitLoad(10000);
+
+      return Jsoup.parse(webdriver.getCurrentPageSource());
+   }
 
 }
