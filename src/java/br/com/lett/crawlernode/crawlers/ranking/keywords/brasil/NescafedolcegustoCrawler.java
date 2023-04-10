5eff6817e2bf8e950bb553af715236f95d117326
doferreira <douglassilva@lett.digital>
2023-04-10 15:35:37 -0300

    add extract for product redirect

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/NescafedolcegustoCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/NescafedolcegustoCrawler.java
@@ -6,6 +6,8 @@ import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
 import br.com.lett.crawlernode.exceptions.MalformedProductException;
 import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.JSONUtils;
+import org.json.JSONArray;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -25,6 +27,7 @@ public class NescafedolcegustoCrawler extends CrawlerRankingKeywords {
       this.log("Link onde são feitos os crawlers: " + url);
 
       Elements products = this.currentDoc.select(".products-listing__list.products > li");
+      Elements productsRedirect = this.currentDoc.select(".product__section--top");
 
       if (!products.isEmpty()) {
 
@@ -34,6 +37,30 @@ public class NescafedolcegustoCrawler extends CrawlerRankingKeywords {
             String productName = CrawlerUtils.scrapStringSimpleInfo(product, ".product-card__name--link", false);
             String imageUrl = CrawlerUtils.scrapStringSimpleInfoByAttribute(product, ".product-card__image", "data-hover-image");
             Integer price = CrawlerUtils.scrapPriceInCentsFromHtml(product, ".product-card__price-container > div > div.product-card__price--current > span > span > span", null, false, ',', session, null);
+            boolean isAvailable = price != null;
+
+            RankingProduct productRanking = RankingProductBuilder.create()
+               .setUrl(productUrl)
+               .setInternalPid(internalId)
+               .setInternalId(internalId)
+               .setName(productName)
+               .setPriceInCents(price)
+               .setAvailability(isAvailable)
+               .setImageUrl(imageUrl)
+               .build();
+
+            saveDataProduct(productRanking);
+            if (this.arrayProducts.size() == productsLimit)
+               break;
+         }
+
+      } else if (!productsRedirect.isEmpty()) {
+         for (Element productInfo : productsRedirect) {
+            String internalId = getInternalId(productInfo);
+            String productUrl = getProductUrl(this.currentDoc);
+            String productName = CrawlerUtils.scrapStringSimpleInfo(productInfo, ".product__title > h1 > span", false);
+            String imageUrl = CrawlerUtils.scrapStringSimpleInfoByAttribute(productInfo, ".gallery-placeholder__image > img", "src");
+            Integer price = CrawlerUtils.scrapPriceInCentsFromHtml(productInfo, ".price-container.price-final_price > span >span", null, false, ',', session, null);
             boolean isAvailable = price != null;
 
             RankingProduct productRanking = RankingProductBuilder.create()
@@ -68,6 +95,22 @@ public class NescafedolcegustoCrawler extends CrawlerRankingKeywords {
          internalId = CrawlerUtils.scrapStringSimpleInfoByAttribute(element, ".amxnotif-container", "data-product-id");
       }
       return internalId;
+   }
+
+   private String getProductUrl(Document doc) {
+      String productUrl = null;
+      Elements scripts = doc.select("script[type=\"application/ld+json\"]");
+      for (Element e : scripts) {
+         String script = CrawlerUtils.scrapScriptFromHtml(e, "script[type=\"application/ld+json\"]");
+         if (script != null && !script.isEmpty() && script.contains("availability")) {
+            JSONArray object = JSONUtils.stringToJsonArray(script);
+            if (object != null) {
+               productUrl = JSONUtils.getValueRecursive(object, "0.offers.url", String.class);
+            }
+            break;
+         }
+      }
+      return productUrl;
    }
 
 }
