1750e5b6c572d0755971e8e5b9848e923db93370
mazzillio <mateca500@gmail.com>
2023-04-25 09:07:43 -0300

    init ranking

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilIfoodAppCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilIfoodAppCrawler.java
@@ -5,6 +5,8 @@ import br.com.lett.crawlernode.core.fetcher.ProxyCollection;
 import br.com.lett.crawlernode.core.fetcher.methods.FetcherDataFetcher;
 import br.com.lett.crawlernode.core.fetcher.models.Request;
 import br.com.lett.crawlernode.core.fetcher.models.Response;
+import br.com.lett.crawlernode.core.models.RankingProduct;
+import br.com.lett.crawlernode.core.models.RankingProductBuilder;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
 import br.com.lett.crawlernode.exceptions.MalformedProductException;
@@ -25,192 +27,54 @@ public class BrasilIfoodAppCrawler extends CrawlerRankingKeywords {
       super(session);
    }
 
+   private static final String baseImageUrl = "https://static.ifood-static.com.br/image/upload/t_low/pratos";
+
    @Override
    protected JSONObject fetchJSONObject(String url) {
-      String payload = "{" +
-         "\"supported-cards\": [" +
-         "\"IMAGE_BANNER\"," +
-         "\"MINI_BANNER\"," +
-         "\"SMALL_BANNER_CAROUSEL\"," +
-         "\"MEDIUM_BANNER_CAROUSEL\"," +
-         "\"MEDIUM_BANNER_LIST\"," +
-         "\"BIG_BANNER_CAROUSEL\"," +
-         "\"MERCHANT_LIST\"," +
-         "\"MERCHANT_LIST_V2\"," +
-         "\"COMPACTED_MERCHANT_LIST\"," +
-         "\"MERCHANT_CAROUSEL\"," +
-         "\"COMPACTED_MERCHANT_CAROUSEL\"," +
-         "\"CATALOG_ITEM_LIST\"," +
-         "\"CATALOG_ITEM_LIST_V2\"," +
-         "\"CATALOG_ITEM_CAROUSEL\"," +
-         "\"CATALOG_ITEM_CAROUSEL_NO_DELIVERY\"," +
-         "\"CATALOG_ITEM_CAROUSEL_V2\"," +
-         "\"SIMPLE_CATALOG_ITEM_CAROUSEL\"," +
-         "\"CATALOG_ITEM_LIST_V3\"," +
-         "\"CATALOG_ITEM_CAROUSEL_V3\"," +
-         "\"CATALOG_ITEM_QUICK_ADD_LIST\"," +
-         "\"CATALOG_ITEM_QUICK_ADD_CAROUSEL\"," +
-         "\"SOCIAL_IMAGE_CARD_V2\"," +
-         "\"SOCIAL_POST\"," +
-         "\"ROUND_IMAGE_CAROUSEL\"," +
-         "\"ROUND_IMAGE_CAROUSEL_V2\"," +
-         "\"MERCHANT_LIST_WITH_CATALOG_ITEMS\"," +
-         "\"FEATURED_MERCHANT_LIST\"," +
-         "\"FEATURED_MERCHANT_CAROUSEL\"," +
-         "\"SIMPLE_MERCHANT_CAROUSEL\"," +
-         "\"SIMPLE_MERCHANT_LIST\"," +
-         "\"BANNER_GRID\"," +
-         "\"NEXT_CONTENT\"," +
-         "\"INFO_CARD\"," +
-         "\"MERCHANT_TILE_CAROUSEL\"," +
-         "\"MERCHANT_LIST_WITH_ITEMS_CAROUSEL\"," +
-         "\"CATALOG_ITEM_CAROUSEL_WITH_TIMER\"," +
-         "\"MERCHANT_GROUPED_CAROUSEL\"," +
-         "\"MERCHANT_GROUPED_CAROUSEL_V2\"," +
-         "\"WAITING_LEGACY_IMAGE_BANNER\"," +
-         "\"WAITING_LEGACY_INFO_BANNER\"," +
-         "\"INFO_POSTER_CAROUSEL\"," +
-         "\"MERCHANT_MENU_ITEM_LIST\"," +
-         "\"MERCHANT_MENU_WITHOUT_RATING\"," +
-         "\"MERCHANT_MENU_OVERALL_INFO\"," +
-         "\"MERCHANT_MENU_DELIVERY_METHODS_CLOSED_STATE\"," +
-         "\"MERCHANT_MENU_DELIVERY_METHODS_OUT_OF_AREA_STATE\"," +
-         "\"MERCHANT_MENU_DELIVERY_METHODS_SCHEDULE_ONLY_STATE\"," +
-         "\"MERCHANT_MENU_DELIVERY_METHODS_TIME_SLOT_STATE\"," +
-         "\"MERCHANT_MENU_MINIMUM_ORDER\"," +
-         "\"MERCHANT_MENU_RATING\"," +
-         "\"IMAGE_GRID\"," +
-         "\"IMAGE_FOLDER_GRID\"," +
-         "\"MEDIUM_IMAGE_BANNER\"," +
-         "\"SMALL_CATALOG_ITEM_LIST\"," +
-         "\"SMALL_CATALOG_ITEM_CAROUSEL\"," +
-         "\"AWARENESS_BANNER_CAROUSEL\"," +
-         "\"SMALL_ICON_GRID\"," +
-         "\"RELATED_SEARCH_CAROUSEL\"," +
-         "\"ACTION_BUTTON\"," +
-         "\"ACTION_BUTTON_V2\"," +
-         "\"SIMPLE_HEADER\"," +
-         "\"MERCHANT_WITH_CATALOG_ITEMS_CAROUSEL\"," +
-         "\"AWARENESS_MERCHANT_CAROUSEL\"," +
-         "\"SEARCH_BAR\"," +
-         "\"MOSAIC\"," +
-         "\"TRENDING_MERCHANT_CAROUSEL\"," +
-         "\"VOUCHER_WALLET_LIST\"," +
-         "\"CATALOG_PROMOTIONAL_ITEMS_CAROUSEL\"," +
-         "\"SIMILAR_MERCHANT_CAROUSEL\"," +
-         "\"SMALL_ICON_GRID_V2\"," +
-         "\"SHOPPING_LIST_CAROUSEL\"," +
-         "\"VERTICAL_BANNER_CAROUSEL\"," +
-         "\"BANNER_SLIDESHOW\"," +
-         "\"INFO_CARD_CAROUSEL\"," +
-         "\"FEATURED_ITEM_CAROUSEL\"," +
-         "\"COMPARATOR_MERCHANT_CAROUSEL\"," +
-         "\"COMPARATOR_MERCHANT_CAROUSEL_V2\"," +
-         "\"HIGHLIGHTED_ITEM_CAROUSEL\"," +
-         "\"GROCERIES_REORDER_CAROUSEL\"," +
-         "\"VERTICAL_CONTENT_LIST_CAROUSEL\"," +
-         "\"FEATURED_SQUARE_IMAGE_CAROUSEL\" " +
-         "]," +
-         "\"supported-headers\": [ " +
-         "\"OPERATION_HEADER\"," +
-         "\"SIMPLE_SECTION_HEADER\" " +
-         "]," +
-         "\"supported-actions\": [ " +
-         "\"payment-details\"," +
-         "\"create-favorites-folder\"," +
-         "\"feed\"," +
-         "\"grocery-categories\"," +
-         "\"merchant\"," +
-         "\"feed-profile\"," +
-         "\"page\"," +
-         "\"reorder\"," +
-         "\"donations-results\"," +
-         "\"voucher-wallet\"," +
-         "\"shopping-details\"," +
-         "\"merchant-menu-rating\"," +
-         "\"open-search\"," +
-         "\"search\"," +
-         "\"purchase-ifood-card\"," +
-         "\"merchant-menu-details\"," +
-         "\"groceries-details\"," +
-         "\"catalog-item\"," +
-         "\"donations-checkout\"," +
-         "\"register-new-payment\"," +
-         "\"donations-detail\"," +
-         "\"merchant-menu-delivery-time-slot\"," +
-         "\"favorites\"," +
-         "\"redeem-ifood-card\"," +
-         "\"home\"," +
-         "\"showcase-aisle\"," +
-         "\"fullscreen-video\"," +
-         "\"voucher-details\"," +
-         "\"shoppinglist\"," +
-         "\"merchant-menu-search\"," +
-         "\"post-details\"," +
-         "\"quick-commerce\"," +
-         "\"grocery-categories-list\"," +
-         "\"donations-list\"," +
-         "\"loyalty\"," +
-         "\"payment-transaction-details\"," +
-         "\"shorts\"," +
-         "\"card-content\"," +
-         "\"display_express_status_component\"," +
-         "\"home-tab\"," +
-         "\"merchant-page\"," +
-         "\"payment-list\"," +
-         "\"webmiddleware\"," +
-         "\"groceries\"," +
-         "\"last-restaurants\"," +
-         "\"webview\"," +
-         "\"payment-method\"," +
-         "\"card-content-fallback\"," +
-         "\"payment-page\" " +
-         "]," +
-         "\"faster-overrides\": \"{ \\\"storeTypeFilterEnabled\\\": true }\"," +
-         "\"feature-availability\": { " +
-         "\"home_feed_feature\": \"{ \\\"enabled\\\": true }\"," +
-         "\"quick_journey_availability\": \"{ \\\"enabled\\\": false }\"," +
-         "\"groceries_ordering_itens_tico_enabled\": \"{\\\"default\\\": {\\\"model_id\\\": \\\"ifood-ml-groceries-tico-v2-endpoint\\\"}}\"}}";
       Map<String, String> headers = new HashMap<>();
-      //headers.put("alias", "SEARCH_RESULTS_ITEM_TAB_GLOBAL");
-      //headers.put("latitude", "-16.724496702061654");
-      //headers.put("longitude", "-43.81544463336468");
-      //headers.put("zip_code", "39404301");
-      //headers.put("channel", "IFOOD");
-      //headers.put("term", "cerveja");
-      headers.put("content-type", "application/json; charset=UTF-8");
-      headers.put("host", "marketplace.ifood.com.br");
-      headers.put("user-agent", "okhttp/4.10.0");
-      //headers.put("search_method", "Term");
-      headers.put("Accept-Encoding", "gzip,deflate,br");
-//      headers.put("search_id","84e1d1c6-d598-4905-a335-9074e3310521");
-//      headers.put("experiment_details","{\"default_item\":{\"is_discovery_search_service_enabled\": true,\"is_app_experiment_enabled\": true,\"model_id\":\"search-optimus-prime-endpoint\",\"engine\":\"sagemaker\",\"backend_experiment_id\":\"v4\",\"query_rewriter_rule\":\"items-synonyms\",\"force_similar_search_disabled\":true}}");
-      //headers.put("authorization","Bearer eyJraWQiOiJiNjRjNjZmZS00ZWY5LTExZTktODY0Ny1kNjYzYmQ4NzNkOTMiLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzUxMiJ9.eyJzdWIiOiI2MzcxMWZiZi1kMWRlLTQ3MjAtYTJjYi0zOWRmMTg4ZTljZTAiLCJpc3MiOiJpRm9vZCIsInRlbmFudElkIjoiSUZPIiwidHlwZSI6ImFjY2Vzc190b2tlbiIsImV4cCI6MTY4MTk5MjQwOSwiaWF0IjoxNjgxOTg1MjA5LCJkaWQiOiJmMDUzNWI0MzQ5MDRmOGM5NmJkNDQyZTMyMzMzOGRjYTFjOTk1MTlkOTc4NjljYTZmZTA0NjlhNTk5ZDZjNTI2ZDdkODU5NDg2NzA5OTU4YWRjYjE1MDI2NjliNjQ0ZmY4YmE2YmM0OTg4Y2E3MmM0MTA5YTkwNDg5OGNkYmYxNyJ9.CZHq6-OncwtZQo3o07jkuQjFeRaDRPasTMOdLb7LEUaQ7Nt5U4cn2yy_cAmk1p9HHgplCvVvjIHf3jLX__ZbGz9dYn3uZ6jlyoca2oiw-NxchLxMD5UjO0FWXBZgeY7D_RMBDiRYVXCA6_rLLRZ-Hw5ukQ5nglSIxt9axW4PEXDXsEtW5mIzMdKjqd30DK5U8xMHHuROQgnFhQHzC-sbwzpW0xtnumRPswkAm35NAF5ReN7oWbuHPdsNeZGBVG_2S9YW_IGjNxD-BFxvHobPzoXOVh8s0SUBcGDT7ZZUdMi0CYTXVpZ56X0XGDBUgqrPYD7wiKKHILsAZNGGxmMRGQ");
+      headers.put("item_experiment_details", "{\"default_internal\":{\"recommendation_filter\":\"internal_search\",\"model_id\":\"ifood-ml-discovery-default-sort-items\",\"engine\":\"sagemaker\",\"backend_experiment_id\":\"rank_exact\",\"force_recommendation_disabled\":true},\"market_internal\":{\"recommendation_filter\":\"internal_search\",\"model_id\":\"search-bumblebee-endpoint\",\"engine\":\"sagemaker\",\"query_rewriter_model_id\":\"search-r5d4-serve-endpoint\",\"backend_experiment_id\":\"v5\",\"query_rewriter_rule\":\"groceries-context\",\"force_recommendation_disabled\":false,\"similar_search\":{\"backend_experiment_id\":\"v5\",\"query_rewriter_model_id\":\"search-marvin-curated-endpoint\",\"force_recommendation_disabled\":false, \"model_id\": \"search-marvin-filter-endpoint\"}}}");
+      headers.put("item_experiment_enabled", "true");
+      headers.put("item_experiment_variant", "market_internal");
       Request request = Request.RequestBuilder.create()
          .setUrl(url)
          .setHeaders(headers)
-         .setPayload(payload)
          .setProxyservice(Arrays.asList(
-            ProxyCollection.SMART_PROXY_BR_HAPROXY,
-            ProxyCollection.SMART_PROXY_BR,
+            ProxyCollection.BUY_HAPROXY,
             ProxyCollection.NETNUT_RESIDENTIAL_BR_HAPROXY,
             ProxyCollection.NETNUT_RESIDENTIAL_BR
          ))
+         .setFollowRedirects(false)
          .build();
-      Response response = new FetcherDataFetcher().post(session, request);
+      Response response = new FetcherDataFetcher().get(session, request);
       return CrawlerUtils.stringToJson(response.getBody());
    }
 
    @Override
    protected void extractProductsFromCurrentPage() throws UnsupportedEncodingException, MalformedProductException {
-      String url = "https://marketplace.ifood.com.br/v2/cardstack/search/results?alias=SEARCH_RESULTS_ITEM_TAB_GLOBAL&latitude=-16.724496702061654&longitude=-43.81544463336468&zip_code=39404301&channel=IFOOD&term=cerveja";
+      String url = "https://marketplace.ifood.com.br/v2/search/catalog-items?latitude=-23.62143488232774&longitude=-46.683871038258076&zip_code=04555020&channel=IFOOD&term=cerveja&categories=&item_from_merchant_ids=1827a055-b0f2-4f3b-a9eb-c876c456be7d&size=100&page=0";
       JSONObject jsonObject = fetchJSONObject(url);
+
       if (jsonObject != null && jsonObject.has("id")) {
-         String idM = jsonObject.optString("id");
-         String baseImageUrl = jsonObject.optString("baseImageUrl");
-         JSONArray items = JSONUtils.getValueRecursive(jsonObject, "sections.0.cards.0.data.contents", JSONArray.class, new JSONArray());
+         if (this.totalProducts == 0) {
+            this.totalProducts = JSONUtils.getValueRecursive(jsonObject, "items.total", Integer.class, 0);
+         }
+         JSONArray items = JSONUtils.getValueRecursive(jsonObject, "items.data", JSONArray.class, new JSONArray());
          for (Object o : items) {
             JSONObject item = (JSONObject) o;
+            String id = item.optString("id");
+            String name = item.optString("name");
+            String suffixImage = JSONUtils.getValueRecursive(item, "resources.fileName", String.class, "");
+            double priceDouble = item.optDouble("price", 0.0);
+            int price = (int) (priceDouble * 100);
+            RankingProduct objProducts = RankingProductBuilder.create()
+               .setUrl(id)
+               .setImageUrl(baseImageUrl + suffixImage)
+               .setName(name)
+               .setPriceInCents(price)
+               .setInternalId(id)
+               .setAvailability(price != 0)
+               .build();
+            saveDataProduct(objProducts);
          }
       }
    }
