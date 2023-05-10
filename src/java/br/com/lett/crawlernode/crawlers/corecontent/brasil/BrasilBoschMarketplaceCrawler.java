e40680e56ef938e0bcf1c45afb3a4984f5c5d060
Luiza <lrsofal@sga.pucminas.br>
2023-05-10 10:16:48 -0300

    [Bosch Marketplace] core fix

---
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilBoschMarketplaceCrawler.java
@@ -0,0 +1,46 @@
+package br.com.lett.crawlernode.crawlers.corecontent.brasil;
+
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.crawlers.extractionutils.core.VTEXNewScraper;
+import br.com.lett.crawlernode.util.JSONUtils;
+import org.json.JSONObject;
+import org.jsoup.nodes.Document;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Collections;
+import java.util.List;
+
+public class BrasilBoschMarketplaceCrawler extends VTEXNewScraper {
+   public BrasilBoschMarketplaceCrawler(Session session) {
+      super(session);
+   }
+
+   @Override
+   protected String getHomePage() {
+      return session.getOptions().optString("homePage");
+   }
+
+   @Override
+   protected List<String> getMainSellersNames() {
+      return Collections.singletonList(session.getOptions().optString("seller"));
+   }
+
+   protected String scrapDescription(Document doc, JSONObject productJson) throws UnsupportedEncodingException {
+      String description = JSONUtils.getValueRecursive(productJson, "description", ".", String.class, "");
+      if (description.equals("")) {
+         String complementName = JSONUtils.getValueRecursive(productJson, "items.0.complementName", ".", String.class, "");
+         String nameComplete = JSONUtils.getValueRecursive(productJson, "items.0.nameComplete", ".", String.class, "");
+
+         if (!complementName.isEmpty() && !complementName.equals(nameComplete)) {
+            description = description.concat(complementName);
+         }
+      }
+
+      if (productJson.has("Descrição do Produto (A+)")) {
+         String descriptionComplete = JSONUtils.getValueRecursive(productJson, "Descrição do Produto (A+).0", ".", String.class, "");
+         return description.concat(descriptionComplete);
+      }
+
+      return description;
+   }
+}
