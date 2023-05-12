5a7501a7570ad5e9e922152ba3f95d69a4e90200
andressa <andressaleal@lett.digital>
2023-05-12 13:27:35 -0300

    criando crawler

--- src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilAppRappiCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/corecontent/brasil/BrasilAppRappiCrawler.java
@@ -4,10 +4,7 @@ import br.com.lett.crawlernode.core.fetcher.ProxyCollection;
 import br.com.lett.crawlernode.core.fetcher.methods.JsoupDataFetcher;
 import br.com.lett.crawlernode.core.fetcher.models.Request;
 import br.com.lett.crawlernode.core.fetcher.models.Response;
-import br.com.lett.crawlernode.core.models.Card;
-import br.com.lett.crawlernode.core.models.CategoryCollection;
-import br.com.lett.crawlernode.core.models.Product;
-import br.com.lett.crawlernode.core.models.ProductBuilder;
+import br.com.lett.crawlernode.core.models.*;
 import br.com.lett.crawlernode.core.session.Session;
 import br.com.lett.crawlernode.core.task.impl.Crawler;
 import br.com.lett.crawlernode.exceptions.MalformedProductException;
@@ -31,11 +28,13 @@ import org.jsoup.nodes.Element;
 import java.util.*;
 
 public class BrasilAppRappiCrawler extends Crawler{
-   //private static final String SELLER_FULL_NAME = "starbucksathome";
+   private static final String SELLER_FULL_NAME = "rappi app";
    protected Set<String> cards = Sets.newHashSet(Card.VISA.toString(), Card.MASTERCARD.toString(), Card.ELO.toString());
+   private static final String baseImageUrl = "https://images.rappi.com.br/products/";
 
    public BrasilAppRappiCrawler(Session session) {
       super(session);
+      config.setParser(Parser.JSON);
    }
 
    private JSONObject fetchDocument() {
@@ -43,22 +42,23 @@ public class BrasilAppRappiCrawler extends Crawler{
       Map<String, String> headers = new HashMap<>();
       headers.put("content-type", "application/json; charset=UTF-8");
       headers.put("Authorization", "Bearer ft.gAAAAABkXTUqW4WZs3GQ4TwL7iXdRd_6_So4YqGDeJTbK_pLq25-mj1SYmoVpEcqC2FiRufXyE8VA265rUbYNUTK_hn7TV6Su13OyZEXjobm9Dxs1VshF1tbUPEFW_H0qhre7e4YnnZ5j1wamsRb228mzuAz2PCdjit0576uNDTGDJd-vXET3g0Qe1h_d86gIk6fFCmiHRA0fdHQ-jjS3DndmVFBo36_H4-HKISZ9Pohe1NcOPgr-CvO6cqJWj2zp6qqpENxwA2rbFpfzx2L4ESo-kVOk8Adpgr2sAM5WKW8etCVrhBMcbZVE75pSZHifxXIyMDITaxhdgDzWtRMlmlHvo5KVAZcrH_CoCeTTGHZ0Bo7NHzk9GI=");
-      headers.put("host", "services.rappi.com.br");
+      headers.put("Host", "services.rappi.com.br");
       headers.put("app-version-name", "7.48.20230420-72418");
       headers.put(HttpHeaders.ACCEPT, "*/*");
 
-      String payload = "{\"context\":\"product_detail\",\"stores\":[900595776],\"offset\":0,\"limit\":5,\"state\":{\"store_type\":\"atacadao\",\"parent_store_type\":\"market\",\"product_id\":\"2141719064\",\"sessions\":\"0\",\"is_prime\":\"false\",\"zone_ids\":\"[]\",\"unlimited_shipping\":\"false\",\"lat\":\"-27.57929014867\",\"lng\":\"-48.588943853974\"},\"store_type\":\"atacadao\"}";
+      String payload = "{\"context\":\"product_detail\",\"stores\":[900595776],\"offset\":0,\"limit\":5,\"state\":{\"store_type\":\"atacadao\",\"parent_store_type\":\"market\",\"product_id: " + session.getOriginalURL() + ",\"sessions\":\"0\",\"is_prime\":\"false\",\"zone_ids\":\"[]\",\"unlimited_shipping\":\"false\",\"lat\":\"-27.57929014867\",\"lng\":\"-48.588943853974\"},\"store_type\":\"atacadao\"}";
 
       Request request = Request.RequestBuilder.create()
          .setUrl("https://services.rappi.com.br/api/dynamic/context/content")
          .setHeaders(headers)
-         /*.setProxyservice(Arrays.asList(
+         .setPayload(payload)
+         .setProxyservice(Arrays.asList(
             ProxyCollection.NETNUT_RESIDENTIAL_BR,
             ProxyCollection.LUMINATI_SERVER_BR,
-            ProxyCollection.BUY_HAPROXY))*/
+            ProxyCollection.BUY_HAPROXY))
          .build();
 
-      Response response = CrawlerUtils.retryRequest(request, session, new JsoupDataFetcher(), true);
+      Response response = CrawlerUtils.retryRequest(request, session, new JsoupDataFetcher(), false);
 
       return JSONUtils.stringToJson(response.getBody());
    }
@@ -68,7 +68,7 @@ public class BrasilAppRappiCrawler extends Crawler{
       List<String> secondaryImages = new ArrayList<>();
       JSONArray images = JSONUtils.getJSONArrayValue(product, "images");
       for (Integer i=0; i<JSONUtils.getJSONArrayValue(product, "images").length(); i++){
-         secondaryImages.add(images.getString(i));
+         secondaryImages.add(baseImageUrl+images.getString(i));
       }
       return secondaryImages;
    }
@@ -80,7 +80,7 @@ public class BrasilAppRappiCrawler extends Crawler{
          String internalId = Integer.toString(JSONUtils.getIntegerValueFromJSON(jsonProduct, "master_product_id", 0));
          String name = JSONUtils.getStringValue(jsonProduct, "name");
          String category = JSONUtils.getStringValue(jsonProduct, "category_name");
-         String primaryImage = JSONUtils.getStringValue(jsonProduct, "image");
+         String primaryImage = baseImageUrl+JSONUtils.getStringValue(jsonProduct, "image");
          List<String> secondaryImage = getSecondaryImages(jsonProduct);
          String description = JSONUtils.getStringValue(jsonProduct, "description");
          boolean availableToBuy = JSONUtils.getStringValue(jsonProduct, "in_stock") == "true";
@@ -93,11 +93,12 @@ public class BrasilAppRappiCrawler extends Crawler{
             .setName(name)
             .setCategory1(category)
             .setPrimaryImage(primaryImage)
-            //.setSecondaryImages(secondaryImage)
+            .setSecondaryImages(secondaryImage)
             .setDescription(description)
-            //.setOffers(offers)
+            .setOffers(offers)
             .build();
 
+         products.add(product);
 
       } else {
          Logging.printLogDebug(logger, session, "Not a product page " + this.session.getOriginalURL());
--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilAppRappiCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilAppRappiCrawler.java
@@ -1,4 +1,93 @@
 package br.com.lett.crawlernode.crawlers.ranking.keywords.brasil;
 
-public class BrasilAppRappiCrawler {
+import br.com.lett.crawlernode.core.fetcher.ProxyCollection;
+import br.com.lett.crawlernode.core.fetcher.methods.JsoupDataFetcher;
+import br.com.lett.crawlernode.core.fetcher.models.Request;
+import br.com.lett.crawlernode.core.fetcher.models.Response;
+import br.com.lett.crawlernode.core.models.RankingProduct;
+import br.com.lett.crawlernode.core.models.RankingProductBuilder;
+import br.com.lett.crawlernode.core.session.Session;
+import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
+import br.com.lett.crawlernode.exceptions.MalformedProductException;
+import br.com.lett.crawlernode.util.CrawlerUtils;
+import br.com.lett.crawlernode.util.JSONUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BrasilAppRappiCrawler extends CrawlerRankingKeywords {
+   public BrasilAppRappiCrawler(Session session) {
+      super(session);
+   }
+
+   protected JSONObject fetchDocument() {
+      Map<String, String> headers = new HashMap<>();
+      headers.put("content-type", "application/json");
+      headers.put("authorization", "Bearer ft.gAAAAABkXTUqW4WZs3GQ4TwL7iXdRd_6_So4YqGDeJTbK_pLq25-mj1SYmoVpEcqC2FiRufXyE8VA265rUbYNUTK_hn7TV6Su13OyZEXjobm9Dxs1VshF1tbUPEFW_H0qhre7e4YnnZ5j1wamsRb228mzuAz2PCdjit0576uNDTGDJd-vXET3g0Qe1h_d86gIk6fFCmiHRA0fdHQ-jjS3DndmVFBo36_H4-HKISZ9Pohe1NcOPgr-CvO6cqJWj2zp6qqpENxwA2rbFpfzx2L4ESo-kVOk8Adpgr2sAM5WKW8etCVrhBMcbZVE75pSZHifxXIyMDITaxhdgDzWtRMlmlHvo5KVAZcrH_CoCeTTGHZ0Bo7NHzk9GI=");
+
+      String payload = "{\"query\":" +this.keywordWithoutAccents+ ",\"lat\":-27.57929014867,\"lng\":-48.588943853974,\"options\":{\"sort\":{}},\"tiered_stores\":[\"default\",\"standard\",\"premium\"]}";
+
+      Request request = Request.RequestBuilder.create()
+         .setUrl("https://services.rappi.com.br/api/pns-global-search-api/v1/unified-search?is_prime=false&unlimited_shipping=false")
+         .setHeaders(headers)
+         .setPayload(payload)
+         .setProxyservice(Arrays.asList(
+            ProxyCollection.BUY_HAPROXY,
+            ProxyCollection.LUMINATI_SERVER_BR,
+            ProxyCollection.NETNUT_RESIDENTIAL_BR))
+         .build();
+
+      Response response = CrawlerUtils.retryRequest(request, session, new JsoupDataFetcher(), false);
+
+      return JSONUtils.stringToJson(response.getBody());
+   }
+
+   @Override
+   protected void extractProductsFromCurrentPage() throws MalformedProductException {
+      this.pageSize = 12;
+      this.log("Página " + this.currentPage);
+
+      JSONObject json = fetchDocument();
+
+      if (json != null && !json.isEmpty()) {
+         JSONArray productsArray = JSONUtils.getJSONArrayValue(json, "products");
+         this.totalProducts = productsArray.length();
+
+         for (Object product : productsArray) {
+            JSONObject productJson = (JSONObject) product;
+
+            if(productJson != null) {
+               String internalId = Integer.toString(JSONUtils.getIntegerValueFromJSON(productJson, "master_product_id", 0));
+               //String productUrl = JSONUtils.getStringValue(productJson, "url");
+               String name = JSONUtils.getStringValue(productJson, "name");
+               String imageUrl = JSONUtils.getStringValue(productJson, "image");
+               Integer price = JSONUtils.getPriceInCents(productJson, "price");
+               boolean isAvailable = price != null;
+
+               RankingProduct productRanking = RankingProductBuilder.create()
+                  //.setUrl(productUrl)
+                  .setInternalId(internalId)
+                  .setName(name)
+                  .setPriceInCents(price)
+                  .setAvailability(isAvailable)
+                  .setImageUrl(imageUrl)
+                  .build();
+
+               saveDataProduct(productRanking);
+            }
+            if (this.arrayProducts.size() == productsLimit) {
+               break;
+            }
+         }
+      } else {
+         this.result = false;
+         this.log("Keyword sem resultado!");
+      }
+
+      this.log("Finalizando Crawler de produtos da página " + this.currentPage + " - até agora " + this.arrayProducts.size() + " produtos crawleados");
+   }
+
 }
