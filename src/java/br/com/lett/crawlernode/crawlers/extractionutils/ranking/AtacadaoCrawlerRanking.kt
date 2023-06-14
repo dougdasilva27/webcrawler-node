ccc1d58a520e1f621ce06769d209211d2fdf0b76
Luiza <lrsofal@sga.pucminas.br>
2023-06-13 23:21:20 -0300

    [Atacadão] Fix price SOS

--- src/java/br/com/lett/crawlernode/crawlers/extractionutils/ranking/AtacadaoCrawlerRanking.kt
+++ src/java/br/com/lett/crawlernode/crawlers/extractionutils/ranking/AtacadaoCrawlerRanking.kt
@@ -2,6 +2,7 @@ package br.com.lett.crawlernode.crawlers.extractionutils.ranking
 
 import br.com.lett.crawlernode.core.fetcher.FetchMode
 import br.com.lett.crawlernode.core.fetcher.ProxyCollection
+import br.com.lett.crawlernode.core.fetcher.methods.ApacheDataFetcher
 import br.com.lett.crawlernode.core.fetcher.models.Request
 import br.com.lett.crawlernode.core.models.RankingProductBuilder
 import br.com.lett.crawlernode.core.session.Session
@@ -17,7 +18,11 @@ import org.json.JSONObject
 class AtacadaoCrawlerRanking(session: Session) : CrawlerRankingKeywords(session) {
 
    init {
-         fetchMode = FetchMode.FETCHER
+      fetchMode = FetchMode.FETCHER
+   }
+
+   private fun getNeighborhood(): String? {
+      return session.options.optString("cb_user_neighborhood")
    }
 
    fun setCookies(): List<Cookie> {
@@ -29,49 +34,79 @@ class AtacadaoCrawlerRanking(session: Session) : CrawlerRankingKeywords(session)
 
    private fun fetchProducts(): JSONObject {
 
+      var url = "https://algolia.cotabest.com.br/catalogo?displayFacets=true&queryString=${getKeyword()}&page=${currentPage - 1}&order_by=-relevance"
+      val regionIds = requestRegionIds()
+      if (regionIds != null) {
+         url = "https://algolia.cotabest.com.br/catalogo?displayFacets=true&queryString=${getKeyword()}&commaSeparatedRegionIds=${regionIds}&page=${currentPage - 1}&order_by=-relevance"
+      }
       setCookies()
-      val url = "https://www.atacadao.com.br/catalogo/search/?q=${getKeyword()}&page=${currentPage}&order_by=-relevance"
+
       val headers = HashMap<String, String>()
       headers["Accept"] = "*/*"
+      headers["x-requested-with"] = "XMLHttpRequest"
       headers["User-Agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"
       headers["cookie"] = CommonMethods.cookiesToString(cookies)
 
       val request = Request.RequestBuilder.create()
          .setUrl(url)
+         .setHeaders(headers)
          .setProxyservice(
             listOf(
                ProxyCollection.BUY,
                ProxyCollection.LUMINATI_SERVER_BR,
                ProxyCollection.NETNUT_RESIDENTIAL_BR
             )
-         ).setHeaders(headers)
+         )
          .build()
 
-      val response = dataFetcher.get(session, request)
+      val response = CrawlerUtils.retryRequest(request, session, ApacheDataFetcher(), true);
 
       return response.body.toJson()
+   }
+
+   private fun requestRegionIds(): String? {
+      val headers = HashMap<String, String>()
+      headers["Accept"] = "*/*"
+      headers["Connection"] = "keep-alive"
+      headers["Origin"] = "https://www.atacadao.com.br"
+      headers["Referer"] = "https://www.atacadao.com.br/"
+
+      val request = Request.RequestBuilder.create()
+         .setUrl("https://apis.cotabest.com.br/logistic/logistics/neighborhoods/${getNeighborhood()}/PJ/available-logistics")
+         .setHeaders(headers)
+         .setProxyservice(
+            listOf(
+               ProxyCollection.BUY,
+               ProxyCollection.LUMINATI_SERVER_BR,
+               ProxyCollection.NETNUT_RESIDENTIAL_BR
+            )
+         )
+         .build()
+
+      val response = CrawlerUtils.retryRequest(request, session, ApacheDataFetcher(), true);
+      val result = response.body
+      return result.replace(Regex("[\\[\\]]"), "")
    }
 
    override fun extractProductsFromCurrentPage() {
       pageSize = 20
 
       val data = fetchProducts()
+      val products = JSONUtils.getJSONArrayValue(data, "results")
 
       if (this.totalProducts == 0) {
-         this.totalProducts = data.optInt("total_results", 0)
+         products.length()
       }
-
-      val products = JSONUtils.getJSONArrayValue(data, "results")
 
       for (product in products) {
 
          if (product is JSONObject) {
-            val internalId = product.optString("pk", null)
-            val productUrl = CrawlerUtils.completeUrl(product.optString("url"), "https", "www.atacadao.com.br");
-            val name = product.optString("full_display")
+            val internalId = product.optString("id", null)
+            val productUrl = CrawlerUtils.completeUrl(product.optString("slug"), "https", "www.atacadao.com.br");
+            val name = product.optString("name")
             val imageUrl = getImage(product)
             val price = getPrice(product)
-            val isAvailable = price != 0
+            val isAvailable = price != null
 
             val productRanking = RankingProductBuilder.create()
                .setUrl(productUrl)
@@ -99,19 +134,31 @@ class AtacadaoCrawlerRanking(session: Session) : CrawlerRankingKeywords(session)
    }
 
    private fun getPrice(product: JSONObject): Int? {
-      val price = product.optQuery("/price/price")
-      if (price != null) {
-         return CommonMethods.stringPriceToIntegerPrice(price as String?, ',', 0)
+      val providers = product.optJSONArray("providers")
+      var minimumPrice: Double = Double.MAX_VALUE
+
+      for (i in 0 until providers.length()) {
+         val provider = providers.getJSONObject(i)
+         val prices = provider.getJSONArray("prices")
+
+         for (j in 0 until prices.length()) {
+            val price = prices.getJSONObject(j).getDouble("price")
+
+            if (price < minimumPrice) {
+               minimumPrice = price
+            }
+         }
       }
-      return null
+
+      return CommonMethods.doublePriceToIntegerPrice(minimumPrice, null)
    }
 
    private fun getImage(product: JSONObject): String? {
-      val imageUrl = product.optQuery("/photo_url/0")
-      if (imageUrl != null) {
-         return imageUrl as String?
+      val imageSlug = product.optString("photo")
+      if (imageSlug != null) {
+         return "https://media.cotabest.com.br/media/sku/$imageSlug"
       }
+
       return null
    }
-
 }
