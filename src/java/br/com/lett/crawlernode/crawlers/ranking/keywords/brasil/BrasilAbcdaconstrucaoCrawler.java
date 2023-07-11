faed364b4dddecc7d201864ee36b04ae23e6907a
doferreira <douglassilva@lett.digital>
2023-07-11 10:16:47 -0300

    [Abc da Construção] fix request

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilAbcdaconstrucaoCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilAbcdaconstrucaoCrawler.java
@@ -7,9 +7,15 @@ import br.com.lett.crawlernode.core.task.impl.CrawlerRankingKeywords;
 import br.com.lett.crawlernode.exceptions.MalformedProductException;
 import br.com.lett.crawlernode.util.CommonMethods;
 import br.com.lett.crawlernode.util.CrawlerUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Collections;
 
 public class BrasilAbcdaconstrucaoCrawler extends CrawlerRankingKeywords {
@@ -19,23 +25,32 @@ public class BrasilAbcdaconstrucaoCrawler extends CrawlerRankingKeywords {
    }
 
    @Override
+   protected Document fetchDocument(String url) {
+      try {
+         HttpClient client = HttpClient.newBuilder().followRedirects(HttpClient.Redirect.NORMAL).build();
+         HttpRequest request = HttpRequest.newBuilder()
+            .GET()
+            .uri(URI.create(url))
+            .build();
+         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+         return Jsoup.parse(response.body());
+      } catch (Exception e) {
+         throw new RuntimeException("Failed in load document: " + session.getOriginalURL(), e);
+      }
+   }
+
+   @Override
    protected void extractProductsFromCurrentPage() throws MalformedProductException {
 
-      this.pageSize = 24;
-
+      this.pageSize = 12;
       this.log("Página " + this.currentPage);
-
-      String url = "https://www.abcdaconstrucao.com.br/busca?busca=" + this.keywordEncoded + "&pagina=" + this.currentPage;
+      String url = "https://www.abcdaconstrucao.com.br/busca?busca=" + this.keywordWithoutAccents.replace(" ", "%20") + "&pagina=" + this.currentPage + "&tamanho=" + pageSize;
       this.log("Link onde são feitos os crawlers: " + url);
-
-
       this.currentDoc = fetchDocument(url);
+
       Elements products = this.currentDoc.select(".spot");
 
       if (!products.isEmpty()) {
-         if (this.totalProducts == 0) {
-            setTotalProducts();
-         }
 
          for (Element e : products) {
             String productUrl = CrawlerUtils.scrapUrl(e, ".spot__image-wrapper > a", "href", "https", "www.abcdaconstrucao.com.br");
@@ -69,16 +84,7 @@ public class BrasilAbcdaconstrucaoCrawler extends CrawlerRankingKeywords {
    }
 
    @Override
-   protected void setTotalProducts() {
-      this.totalProducts = CrawlerUtils.scrapIntegerFromHtml(this.currentDoc, ".fbits-qtd-produtos-pagina", true, 0);
-      this.log("Total: " + this.totalProducts);
+   protected boolean hasNextPage() {
+      return !this.currentDoc.select("#next-page").isEmpty();
    }
-
-   private Integer getPrice(Element e) {
-      Integer price = CrawlerUtils.scrapIntegerFromHtml(e, ".fbits-preco-calculado-spot", true, null);
-      if (price == null) price = CrawlerUtils.scrapIntegerFromHtml(e, ".fbits-valor", true, null);
-
-      return price;
-   }
-
 }
