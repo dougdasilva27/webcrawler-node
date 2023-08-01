591ad95190c0f99b30724bdfdfd1d356fb6a56e3
andressa <andressaleal@lett.digital>
2023-08-01 11:09:55 -0300

    changing proxies order

--- src/java/br/com/lett/crawlernode/crawlers/extractionutils/core/MexicoSamsclubCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/extractionutils/core/MexicoSamsclubCrawler.java
@@ -62,8 +62,8 @@ public class MexicoSamsclubCrawler extends Crawler {
             .setUrl(apiUrl)
             .setCookies(cookies)
             .setProxyservice(Arrays.asList(
-               ProxyCollection.BUY,
                ProxyCollection.NETNUT_RESIDENTIAL_MX_HAPROXY,
+               ProxyCollection.BUY,
                ProxyCollection.NETNUT_RESIDENTIAL_CO_HAPROXY))
             .build();
 
