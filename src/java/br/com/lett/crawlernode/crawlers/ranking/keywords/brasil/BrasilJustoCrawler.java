d0c7d17fb0a1007364c0c87d469700433cd7a432
Wellington-Lett <wellingtonsilva@lett.digital>
2023-08-03 15:57:07 -0300

    Manutenção Brasil Justo no Ranking

--- src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilJustoCrawler.java
+++ src/java/br/com/lett/crawlernode/crawlers/ranking/keywords/brasil/BrasilJustoCrawler.java
@@ -29,6 +29,8 @@ public class BrasilJustoCrawler extends CrawlerRankingKeywords {
    private final String HOME_PAGE = "https://soujusto.com.br";
    private final String POSTAL_CODE = getPostalCode();
 
+   private final String idStore = session.getOptions().optString("store_id", "10");
+
    public BrasilJustoCrawler(Session session) {
       super(session);
       super.fetchMode = FetchMode.FETCHER;
@@ -46,28 +48,28 @@ public class BrasilJustoCrawler extends CrawlerRankingKeywords {
 
    @Override
    protected void extractProductsFromCurrentPage() throws UnsupportedEncodingException, MalformedProductException {
-      this.pageSize = 10;
-      String url = HOME_PAGE + "/graphql/";
+      this.pageSize = 15;
+//      String url = HOME_PAGE + "/graphql/";
 
-      JSONObject json = fetchJSON(url);
-      JSONObject apiResp = (JSONObject) json.optQuery("/data/search");
+      JSONObject json = fetchJSON();
 
       if (this.currentPage == 1) {
-         this.totalProducts = apiResp.optInt("total");
+         this.totalProducts = JSONUtils.getValueRecursive(json,"results.0.nbHits",Integer.class, 0);
       }
 
-      JSONArray products = JSONUtils.getValueRecursive(apiResp, "products/edges", "/", JSONArray.class, new JSONArray());
+
+      JSONArray products = JSONUtils.getValueRecursive(json, "results.0.hits", JSONArray.class, new JSONArray());
       for (int i = 0; i < products.length(); i++) {
-         JSONObject product = products.getJSONObject(i).optJSONObject("node");
-         String urlPath = product != null ? product.optString("url") : null;
-         String internalId = product != null ? product.optString("sku") : null;
+         JSONObject product = products.getJSONObject(i);
+         String internalId = JSONUtils.getValueRecursive(product, "sku", String.class, "");
          String internalPid =internalId;
-         String productUrl = HOME_PAGE + urlPath;
-         String name = product != null ? product.optString("name") : null;;
-         String imageUrl = product != null ? product.optJSONObject("thumbnail").optString("url") : null;
-         Double price = product != null ? product.optJSONObject("availability").optJSONObject("priceRange").optJSONObject("start").optJSONObject("gross").optDouble("amount") * 100 : null;
-         Integer priceInCents = price != null ? price.intValue() : null;
-         boolean isAvailable = product != null && product.optBoolean("isAvailable");
+         String productUrl = HOME_PAGE + JSONUtils.getValueRecursive(product, "url", String.class, "");
+         String name = JSONUtils.getValueRecursive(product, "full_name.shortName", String.class, "");
+         String imageUrl = JSONUtils.getValueRecursive(product, "image_thumbnail_url", String.class, "");
+         Double price = JSONUtils.getValueRecursive(product, "stores.10.filter_price", Double.class, 0d);
+         boolean isAvailable = JSONUtils.getValueRecursive(product, "stores.10.available", Boolean.class);
+         Integer priceInCents = isAvailable ? (int)(price * 100) : null;
+
 
          RankingProduct productRanking = RankingProductBuilder.create()
             .setUrl(productUrl)
@@ -88,22 +90,16 @@ public class BrasilJustoCrawler extends CrawlerRankingKeywords {
 
    }
 
-   private JSONObject fetchJSON(String url) {
-      int offset = this.currentPage == 1 ? 0 : (this.currentPage - 1) * this.pageSize;
+   private JSONObject fetchJSON() {
+      int offset = this.currentPage - 1;
 
       String payload = "{\n" +
-         "    \"operationName\": \"search\",\n" +
-         "    \"variables\": {\n" +
-         "        \"first\": " + this.pageSize + ",\n" +
-         "        \"query\": \"" + this.location + "\",\n" +
-         "        \"offset\": " + offset + ",\n" +
-         "        \"filter\": {\n" +
-         "            \"postalCode\": \"" + POSTAL_CODE + "\",\n" +
-         "            \"categories\": null\n" +
-         "        },\n" +
-         "        \"onlyEnabledVariants\": true\n" +
-         "    },\n" +
-         "    \"query\": \"fragment CategoryFragment on Category {\\n  id\\n  name\\n  __typename\\n}\\n\\nfragment TaxedMoneyFragment on TaxedMoney {\\n  gross {\\n    amount\\n    currency\\n    localized\\n    __typename\\n  }\\n  __typename\\n}\\n\\nfragment ShoppingListFragment on ShoppingList {\\n  id\\n  name\\n  __typename\\n}\\n\\nfragment MoneyFragment on Money {\\n  localized\\n  amount\\n  currency\\n  __typename\\n}\\n\\nfragment ProductFragment on Product {\\n  id\\n  name\\n  isAvailable\\n  url\\n  sku\\n  maxQuantityAllowed\\n  label\\n  labelFontColor\\n  labelBackgroundColor\\n  showPriceWeightUnit\\n  variants {\\n    id\\n    name\\n    stockQuantity\\n    weightUnit\\n    isPiece\\n    bundle {\\n      discountPrice {\\n        ...MoneyFragment\\n        __typename\\n      }\\n      discountMinQuantity\\n      discountLabel\\n      __typename\\n    }\\n    maturationOptions {\\n      description\\n      name\\n      type\\n      __typename\\n    }\\n    __typename\\n  }\\n  shoppingList {\\n    ...ShoppingListFragment\\n    __typename\\n  }\\n  category {\\n    ...CategoryFragment\\n    __typename\\n  }\\n  thumbnail {\\n    url\\n    __typename\\n  }\\n  availability {\\n    discountPercentage\\n    lineMaturationOptions\\n    quantityOnCheckout\\n    variantOnCheckout\\n    priceRange {\\n      start {\\n        ...TaxedMoneyFragment\\n        __typename\\n      }\\n      stop {\\n        ...TaxedMoneyFragment\\n        __typename\\n      }\\n      __typename\\n    }\\n    priceRangeUndiscounted {\\n      start {\\n        ...TaxedMoneyFragment\\n        __typename\\n      }\\n      stop {\\n        ...TaxedMoneyFragment\\n        __typename\\n      }\\n      __typename\\n    }\\n    __typename\\n  }\\n  __typename\\n}\\n\\nquery search($query: String!, $first: Int, $offset: Int, $orderOptions: String, $filter: ProductFilterInput, $onlyEnabledVariants: Boolean) {\\n  search(query: $query) {\\n    pages\\n    total\\n    queryId\\n    categories {\\n      ...CategoryFragment\\n      __typename\\n    }\\n    promotionalRule {\\n      promotionalContent\\n      urlContent\\n      imageContent\\n      __typename\\n    }\\n    products(\\n      first: $first\\n      offset: $offset\\n      orderOptions: $orderOptions\\n      filter: $filter\\n      onlyEnabledVariants: $onlyEnabledVariants\\n    ) {\\n      edges {\\n        node {\\n          ...ProductFragment\\n          searchIndex\\n          __typename\\n        }\\n        __typename\\n      }\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\"\n" +
+         "    \"requests\": [\n" +
+         "        {\n" +
+         "            \"indexName\": \"arcade_storefront\",\n" +
+         "            \"params\": \"analyticsTags=%5B%22desktop%22%2C%22anonymous%22%2C%22search%22%2C%22short%20query%22%5D&clickAnalytics=true&facets=%5B%22brand%22%2C%22categories.lvl0%22%2C%22first_publication_timestamp%22%2C%22same_day_shipping_available%22%2C%22stores." + this.idStore + ".filter_price%22%2C%22stores." + this.idStore + ".has_discount%22%5D&filters=is_published%3A%20true%20AND%20stores." + this.idStore + ".active%3A%20true&highlightPostTag=%2Fais-highlight&highlightPreTag=ais-highlight&hitsPerPage=" + this.pageSize +"&maxValuesPerFacet=30&page=" + offset + "&query=" + this.keywordEncoded + "&ruleContexts=%5B%22user_store%3A" + this.idStore + "%22%5D&tagFilters=&userToken=2c2f950f-2e53-47f5-9846-82a8168e0a24\"\n" +
+         "        }\n" +
+         "    ]\n" +
          "}";
 
       Map<String, String> headers = new HashMap<>();
@@ -112,7 +108,7 @@ public class BrasilJustoCrawler extends CrawlerRankingKeywords {
       headers.put("origin", "https://soujusto.com.br");
 
       Request request = Request.RequestBuilder.create()
-         .setUrl("https://soujusto.com.br/graphql/")
+         .setUrl("https://cnn0corslw-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia%20for%20JavaScript%20(4.13.0)%3B%20Browser%20(lite)%3B%20instantsearch.js%20(4.54.0)%3B%20react%20(17.0.2)%3B%20react-instantsearch%20(6.38.1)%3B%20react-instantsearch-hooks%20(6.38.1)%3B%20JS%20Helper%20(3.14.0)&x-algolia-api-key=d544ee233b452e57766306b328901df5&x-algolia-application-id=CNN0CORSLW")
          .setHeaders(headers)
          .setCookies(this.cookies)
          .setProxyservice(Arrays.asList(
@@ -130,6 +126,5 @@ public class BrasilJustoCrawler extends CrawlerRankingKeywords {
 
       return CrawlerUtils.stringToJson(response.getBody());
    }
-
 
 }
